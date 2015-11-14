ff1b5f57dd018501367de083055ca6916ce75d59
mistakevin <kevincolten@gmail.com>
2015-11-14 15:28:11 -0600

    added test kata

--- katas/02KataTwo.js
+++ katas/02KataTwo.js
@@ -2,10 +2,11 @@
 
 var assert = require('assert');
 
-function findOdd(A) {
-    // Given an array A, find the int that appears an odd number of times.
-    // There will always be only one integer that appears an odd number of times.
+// Given an array A, find the int that appears an odd number of times.
+// There will always be only one integer that appears an odd number of times.
 
+function findOdd(arr) {
+    // Your code here
 }
 
 
