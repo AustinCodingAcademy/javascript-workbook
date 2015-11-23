e2c023091b74c153569a2aa12bf8633a0685b34e
mistakevin <kevincolten@gmail.com>
2015-11-23 11:14:35 -0600

    solved mastermind

--- apps/05Mastermind.js
+++ apps/05Mastermind.js
@@ -26,12 +26,55 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateHint() {
-    // your code here
+function generateHint(solution, guess) {
+    var solutionArray = solution.split(''); // ['c', 'b, 'a', 'b' ]
+    var guessArray = guess.split(''); // ['a', 'b', 'c', 'b']
+
+    var correctLetterLocations = 0;
+
+    // write a loop that loops over each item
+    // in solutionArray
+    for (var i = 0; i < solutionArray.length; i++) {
+        // if solutionArray at index i is equal to
+        // guessArray at index i, then increment correctLetterLocations,
+        // and cancel out solutionArra at that index
+        if (solutionArray[i] === guessArray[i]) {
+            correctLetterLocations++;
+            solutionArray[i] = null;
+        }
+    }
+
+    var correctLetters = 0;
+    for (var i = 0; i < solutionArray.length; i++) {
+        // if solution array contains guessArray at index i,
+        // increment correctLetters and cancel out index i of
+        // solutionArray
+
+        // must save the index just in case it does exist
+        var targetIndex = solutionArray.indexOf(guessArray[i]);
+        if ( targetIndex > -1 ) {
+            correctLetters++;
+            // so that we can "target" it for nullification
+            solutionArray[targetIndex] = null;
+        }
+    }
+
+    return colors.red(correctLetterLocations) + ' - ' + correctLetters;
 }
 
 function mastermind(guess) {
-    // your code here
+    if (guess === solution) {
+        return 'You guessed it!';
+    }
+
+    var hint = generateHint(solution, guess);
+    board.push(guess + ' ' + hint);
+
+    if (board.length === 10) {
+        return 'You ran out of turns! The solution was ' + solution;
+    }
+
+    return "Guess again.";
 }
 
 
