5f7023f5473698587086f176417ec40846795264
Demetrius Thomas <t.metrous@outlook.com>
2016-02-17 19:05:41 -0600

    2Lesson

--- test/02LessonTwoTest.js
+++ test/02LessonTwoTest.js
@@ -3,18 +3,21 @@
 var assert = require('assert');
 
 function whichSpecies(character) {
-    // should return "dog" when character is 'scooby'
-    // should return "cat" when character is 'garfield'
-    // should return "fish" when character is 'nemo'
-    // should return false if character is anything else
-
+    if( character === 'scooby');
+    return "dog";}
+    else if character === 'garfield';
+    return "cat";
+    else if (character === 'nemo');
+    return 'fish';
+    else return false;
 }
 
 function isEven(number) {
-    // should return true is number is even (divisible by 2)
-
+    if(
+        number % 2)
+        return true;
+        
 }
-
 
 // Tests
 
