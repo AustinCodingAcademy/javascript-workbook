e4ff9dd00a451bd2283dd352f393d258f0517e61
TheeGrassHopper <almortada88@gmail.com>
2016-02-22 14:31:46 -0600

    commiting app 02nd & 03ed problemThree and LessonThreeTest

--- apps/02RockPaperScissors.js
+++ apps/02RockPaperScissors.js
@@ -4,6 +4,7 @@ var assert = require('assert');
 var prompt = require('prompt');
 prompt.start();
 
+// solution 1
 
 // var userChoice = prompt("Do you choose rock, paper or scissors?");
 // var computerChoice = Math.random();
@@ -15,55 +16,149 @@ prompt.start();
 //     computerChoice = "scissors";
 // } console.log("Computer: " + computerChoice);
 
-function rockPaperScissors(hand1, hand2){
-    if(hand1 == hand2){
-        return "It's a tie!";
+// function rockPaperScissors(hand1, hand2){
+//     if(hand1 == hand2){
+//         return "It's a tie!";
+//     }
+//     else if (hand1 === "rock"){
+//         if(hand2 == "scissors"){
+//             return "Hand one wins!";
+//         }
+//         else {
+//             return "Hand two wins!";
+//         }
+//     }
+//     else if (hand1 === "paper"){
+//         if(hand2 === "rock"){
+//             return "Hand one wins!";   
+//         }
+//         else {
+//             return "Hand two wins!";
+//         }
+//     }
+//     else if (hand1 === "scissors"){
+//         if(hand2 === "rock"){
+//             return "Hand one wins!";
+//         }
+//         else {
+//             return "Hand two wins!"; 
+//         }
+//     }
+// }
+
+// Solution 2
+
+var computerChoice = function (low, high){
+  return Math.floor(Math.random() * (high - low) + low);
+};
+
+var random1 = computerChoice(1, 3);
+var random2 = computerChoice(1, 3);
+
+function choiceRockPaperScissors(random1, random2){
+    if(random1 === 1){
+       return  "rock";
+    } 
+    else if (random2 === 2){
+      return  "paper";
     }
-    else if (hand1 === "rock"){
-        if(hand2 == "scissors"){
-            return "Hand one wins!";
-        }
-        else {
-            return "Hand two wins!";
-        }
-    }
-    else if (hand1 === "paper"){
-        if(hand2 === "rock"){
-            return "Hand one wins!";   
-        }
-        else {
-            return "Hand two wins!";
-        }
-    }
-    else if (hand1 === "scissors"){
-        if(hand2 === "rock"){
-            return "Hand one wins!";
-        }
-        else {
-            return "Hand two wins!"; 
-        }
+    else {
+       return "scissors";
     }
 }
 
+//  console.log(choiceRockPaperScissors(random1, random2));
 
+function rockPaperScissors(hand1, hand2){
+    if( hand1 === null){
+       hand1 = choiceRockPaperScissors(random1);
+      
+    }
+    if (hand2 === null) {
+      hand2 = choiceRockPaperScissors(random2);
+      
+    }
+  
+  if( hand1 === hand2){
+    return "It's a tie!";
+  }
+  else if(hand1 === "rock" && hand2 === "paper"){
+          return "Hand two wins!";
+        }
+  else if(hand1 === "rock" && hand2 === "scissors") {
+          return "Hand one wins!";
+        }
+  
+  
+  else if(hand1 === "paper" && hand2 === "rock") {
+          return "Hand one wins!";
+        }
+  else if(hand1 === "paper" && hand2 === "scissors") {
+          return "Hand two wins!";
+        }
+  
+  
+  else if(hand1 === "scissors" && hand2 === "paper") {
+          return "Hand one wins!";
+        }
+  else if(hand1 === "scissors" && hand2 === "rock") {
+          return "Hand two wins!";
+        } 
+}
+rockPaperScissors(null, null);
 
-// function rockPaperScissors(hand1, hand2) {
+// Solution 3
+function computerChoice(low, high){
+  return Math.floor(Math.random() * (high - low) + low);
+}
 
-//      if(hand2==hand1){
-//         return "It's a tie!";
-//      }
-//      else if( hand1 == "rock" && hand2 == "scissors"){
-//         return "Hand one wins!";
-//      }
-//      else if ( hand1 == "paper" && hand2 =="scissors"){
-//         return "Hand two wins!";
-//      }
-//      else if ( hand1 == "paper" && hand2 == "rock"){
-//         return "Hand two wins!";
-//     }
-//     rockPaperScissors("paper", "rock");
-// };
+function choiceRockPaperScissors(random){
+   random = computerChoice(1,4);
 
+    if(random === 1){
+       return  "rock";
+    } 
+    else if (random === 2){
+      return  "paper";
+    }
+    else {
+       return "scissors";
+    }
+  return choiceRockPaperScissors();
+}
+
+function rockPaperScissors(hand1, hand2){
+  
+  hand1 = choiceRockPaperScissors();
+  hand2 = choiceRockPaperScissors();
+
+  if( hand1 === hand2){
+    return "it's a tie!";
+  }
+  else if(hand1 === "rock" && hand2 === "paper"){
+          return "hand2 wins";
+        }
+  else if(hand1 === "rock" && hand2 === "scissors") {
+          return "hand1 wins";
+        }
+  
+  
+  else if(hand1 === "paper" && hand2 === "rock") {
+          return "hand1 wins";
+        }
+  else if(hand1 === "paper" && hand2 === "scissors") {
+          return "hand2 wins";
+        }
+  
+  
+  else if(hand1 === "scissors" && hand2 === "paper") {
+          return "hand1 wins";
+        }
+  else if(hand1 === "scissors" && hand2 === "rock") {
+          return "hand2 wins";
+        }
+}
+ console.log(rockPaperScissors());
 
 function getPrompt() {
     prompt.get(['hand1', 'hand2'], function (error, result) {
