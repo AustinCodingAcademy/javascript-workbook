c3795ad7ce7ac9ca478653eecde6d0b3955cdf8e
Richard Davis <laura@Lauras-MacBook-Pro.local>
2016-02-22 18:47:34 -0600

    03TicTacToe

--- apps/03TicTacToe.js
+++ apps/03TicTacToe.js
@@ -13,51 +13,63 @@ var board = [
 var playerTurn = 'X';
 
 function printBoard() {
-    console.log('   0  1  2');
-    console.log('0 ' + board[0].join(' | '));
-    console.log('  ---------');
-    console.log('1 ' + board[1].join(' | '));
-    console.log('  ---------');
-    console.log('2 ' + board[2].join(' | '));
+	console.log('   0  1  2');
+	console.log('0 ' + board[0].join(' | '));
+	console.log('  ---------');
+	console.log('1 ' + board[1].join(' | '));
+	console.log('  ---------');
+	console.log('2 ' + board[2].join(' | '));
 }
 
 function horizontalWin() {
-    
-if ((board[0][0] === playerTurn && board[0][1] === playerTurn && board[0][2] === playerTurn) || (board[1][0] === playerTurn && board[1][1] === playerTurn && board[1][2] === playerTurn) || (board[2][0] === playerTurn && board[2][1] === playerTurn && board[2][2] === playerTurn)){
+
+	if ((board[0][0] === playerTurn && board[0][1] === playerTurn && board[0][2] === playerTurn) || (board[1][0] === playerTurn && board[1][1] === playerTurn && board[1][2] === playerTurn) || (board[2][0] === playerTurn && board[2][1] === playerTurn && board[2][2] === playerTurn)) {
 		return true;
-}
+	}
 
 }
 
 function verticalWin() {
-if ((board[0][0] === playerTurn && board[1][0] === playerTurn && board[2][0] === playerTurn) || (board[0][1] === playerTurn && board[1][1] === playerTurn && board[2][1] === playerTurn) || (board[0][2] === playerTurn && board[1][2] === playerTurn && board[2][2] === playerTurn)){ 
-	return true;
+	if ((board[0][0] === playerTurn && board[1][0] === playerTurn && board[2][0] === playerTurn) || (board[0][1] === playerTurn && board[1][1] === playerTurn && board[2][1] === playerTurn) || (board[0][2] === playerTurn && board[1][2] === playerTurn && board[2][2] === playerTurn)) {
+		return true;
+	}
 }
-}
+
 function diagonalWin() {
-if ((board[0][0] === playerTurn && board[1][1] === playerTurn && board[2][2] === playerTurn) || (board[0][2] === playerTurn && board[1][1] === playerTurn && board[2][0] === playerTurn)){
-	return true;
+	if ((board[0][0] === playerTurn && board[1][1] === playerTurn && board[2][2] === playerTurn) || (board[0][2] === playerTurn && board[1][1] === playerTurn && board[2][0] === playerTurn)) {
+		return true;
+	}
 }
-}
+
 function checkForWin() {
-    if (horizontalWin() || verticalWin() || diagonalWin()) {
-return true;    }
+
+	if (horizontalWin() || verticalWin() || diagonalWin()) {
+		return true;
+	}
 }
 
 function ticTacToe(row, column) {
-  
-    board[row][column] = playerTurn;
-    playerTurn = (playerTurn == "X") ? 'O' : 'X';
-    
+
+	board[row][column] = playerTurn;
+	if (checkForWin()) {
+		console.log(playerTurn + " wins");
+		board = [
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' ']
+];
+	}
+	playerTurn = (playerTurn == "X") ? 'O' : 'X';
+
 }
 
 function getPrompt() {
-    printBoard();
-    console.log("It's Player " + playerTurn + "'s turn.");
-    prompt.get(['row', 'column'], function (error, result) {
-        ticTacToe(result['row'], result['column']);
-        getPrompt();
-    });
+	printBoard();
+	console.log("It's Player " + playerTurn + "'s turn.");
+	prompt.get(['row', 'column'], function (error, result) {
+		ticTacToe(result['row'], result['column']);
+		getPrompt();
+	});
 }
 
 
@@ -66,33 +78,33 @@ function getPrompt() {
 
 if (typeof describe !== 'undefined') {
 
-    describe('#ticTacToe()', function () {
-        it('should place mark on the board', function () {
-            ticTacToe(1, 1);
-            assert.deepEqual(board, [ [' ', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' '] ]);
-        });
-        it('should alternate between players', function () {
-            ticTacToe(0, 0);
-            assert.deepEqual(board, [ ['O', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' '] ]);
-        });
-        it('should check for vertical wins', function () {
-            board = [ [' ', 'X', ' '], [' ', 'X', ' '], [' ', 'X', ' '] ];
-            assert.equal(verticalWin(), true);
-        });
-        it('should check for horizontal wins', function () {
-            board = [ ['X', 'X', 'X'], [' ', ' ', ' '], [' ', ' ', ' '] ];
-            assert.equal(horizontalWin(), true);
-        });
-        it('should check for diagonal wins', function () {
-            board = [ ['X', ' ', ' '], [' ', 'X', ' '], [' ', ' ', 'X'] ];
-            assert.equal(diagonalWin(), true);
-        });
-        it('should detect a win', function () {
-            assert.equal(checkForWin(), true);
-        });
-    });
+	describe('#ticTacToe()', function () {
+		it('should place mark on the board', function () {
+			ticTacToe(1, 1);
+			assert.deepEqual(board, [[' ', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' ']]);
+		});
+		it('should alternate between players', function () {
+			ticTacToe(0, 0);
+			assert.deepEqual(board, [['O', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' ']]);
+		});
+		it('should check for vertical wins', function () {
+			board = [[' ', 'X', ' '], [' ', 'X', ' '], [' ', 'X', ' ']];
+			assert.equal(verticalWin(), true);
+		});
+		it('should check for horizontal wins', function () {
+			board = [['X', 'X', 'X'], [' ', ' ', ' '], [' ', ' ', ' ']];
+			assert.equal(horizontalWin(), true);
+		});
+		it('should check for diagonal wins', function () {
+			board = [['X', ' ', ' '], [' ', 'X', ' '], [' ', ' ', 'X']];
+			assert.equal(diagonalWin(), true);
+		});
+		it('should detect a win', function () {
+			assert.equal(checkForWin(), true);
+		});
+	});
 } else {
 
-    getPrompt();
+	getPrompt();
 
 }
