19c74f1583e07c5ef92700ea7f20f1752bbd2762
PieroAPretto <pretto417@gmail.com>
2016-02-28 17:13:20 -0800

    All tests passed

--- apps/04TowersOfHanoi.js
+++ apps/04TowersOfHanoi.js
@@ -10,36 +10,71 @@ var stacks = {
     c: []
 };
 
+var moveCount = 0;
+
 function printStacks() {
     console.log("a: " + stacks.a);
     console.log("b: " + stacks.b);
     console.log("c: " + stacks.c);
 }
 
-function movePiece() {
-    // Your code here
+function restartGame() {
+    moveCount = 0;
+    var stacks = {
+    a: [4, 3, 2, 1],
+    b: [],
+    c: []
+    };
+    return stacks;
+}
+
+function movePiece(startStack, endStack) {
+    if (isLegal(startStack,endStack)) {
+        stacks[endStack].push(stacks[startStack].pop());
+        moveCount++;
+    }
+    else {
+        return false;
+    }
+}
+
+function isLegal(startStack, endStack) {
+    if (stacks[endStack].length === 0) {
+        return true;
+    }
+    if (stacks[startStack].pop() < stacks[endStack].pop()) {
+        return true;
+    }
+    else {
+        console.log("Invalid move! Try again!");
+        return false;
+    }
 
 }
 
-function isLegal() {
-    // Your code here
-
-}
-
-function checkForWin() {
-    // Your code here
+function checkForWin(startStack, endStack) {
+    if (stacks.b.length === 4) {
+        console.log("You won with " + moveCount + " moves!\n" 
+        + "The miminum amount of moves possible is 15.\n" 
+        + "Restarting game.." + "\n");
+        restartGame();
+        return true;
+    }
+    else {
+        return false;
+    }
 
 }
 
 function towersOfHanoi(startStack, endStack) {
-    // Your code here
-
+    movePiece(startStack, endStack);
+    checkForWin();
 }
 
 function getPrompt() {
     printStacks();
-    prompt.get(['start stack', 'end stack'], function (error, result) {
-        towersOfHanoi(result['start stack'], result['end stack']);
+    prompt.get(['startStack', 'endStack'], function (error, result) {
+        towersOfHanoi(result['startStack'], result['endStack']);
         getPrompt();
     });
 }
@@ -57,12 +92,23 @@ if (typeof describe !== 'undefined') {
 
     describe('#isLegal()', function () {
         it('should not allow an illegal move', function () {
+            stacks = {
+              a: [4, 3, 2],
+              b: [1],
+              c: []
+            };
             assert.equal(isLegal('a', 'b'), false);
         });
         it('should allow a legal move', function () {
+            stacks = {
+              a: [4, 3, 2, 1],
+              b: [],
+              c: []
+            };
             assert.equal(isLegal('a', 'c'), true);
         });
     });
+
     describe('#checkForWin()', function () {
         it('should detect a win', function () {
             stacks = { a: [], b: [4, 3, 2, 1], c: [] }
