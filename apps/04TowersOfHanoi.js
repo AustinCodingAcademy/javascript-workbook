a0881398e13edfba121083df3b7fa154d075568a
Rampaggio <rampyatx@gmail.com>
2016-02-29 18:50:50 -0600

    What a wonderful Game

--- apps/04TowersOfHanoi.js
+++ apps/04TowersOfHanoi.js
@@ -16,30 +16,51 @@ function printStacks() {
     console.log("c: " + stacks.c);
 }
 
-function movePiece() {
+function movePiece(startStack, endStack) {
     // Your code here
+    stacks[endStack].push(stacks[startStack].pop());
 
 }
 
-function isLegal() {
+function isLegal(startStack, endStack) {
     // Your code here
+    var arrayStart = stacks[startStack];
+    
+    var arrayEnd = stacks[endStack];
+    
+    if (arrayStart[arrayStart.length -1] < arrayEnd[arrayEnd.length -1] || arrayEnd.length === 0) {
+        return true;
+    }
+    else {
+        return false;
+    };
 
 }
 
 function checkForWin() {
     // Your code here
+    if (stacks.b.length === 4 || stacks.c.length === 4) {
+        console.log("You Win!");
+        return true;
+    } else {
+        return false;
+    };
 
 }
 
 function towersOfHanoi(startStack, endStack) {
-    // Your code here
+   isLegal(startStack, endStack);
+    movePiece(startStack, endStack);
+    checkForWin();
 
 }
 
 function getPrompt() {
     printStacks();
-    prompt.get(['start stack', 'end stack'], function (error, result) {
-        towersOfHanoi(result['start stack'], result['end stack']);
+    prompt.get(['start_stack', 'end_stack'], function (error, result) {
+        if ((result['start_stack'] === 'a' || result['start_stack'] === 'b' || result['start_stack'] === 'c') && (result['end_stack'] === 'a' || result['end_stack'] === 'b' || result['end_stack'] === 'c')) { 
+            towersOfHanoi(result['start_stack'], result['end_stack']);
+        };
         getPrompt();
     });
 }
@@ -55,11 +76,21 @@ if (typeof describe !== 'undefined') {
         });
     });
 
-    describe('#isLegal()', function () {
+     describe('#isLegal()', function () {
         it('should not allow an illegal move', function () {
+            stacks = {
+              a: [4, 3, 2],
+              b: [1],
+              c: []
+            };
             assert.equal(isLegal('a', 'b'), false);
         });
         it('should allow a legal move', function () {
+            stacks = {
+              a: [4, 3, 2, 1],
+              b: [],
+              c: []
+            };
             assert.equal(isLegal('a', 'c'), true);
         });
     });
