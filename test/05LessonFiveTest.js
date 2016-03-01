1068e3a603190207c4a1045ba05470ec34f1a2fb
Guillermo Hernandez Jr <guillermohernandez@users.noreply.github.com>
2016-03-01 00:53:22 -0600

    Turn Lesson 05

--- test/05LessonFiveTest.js
+++ test/05LessonFiveTest.js
@@ -3,12 +3,24 @@
 var assert = require('assert');
 
 function loopIt() {
-    // should return an array with numbers 0 - 99
+    var arr = [];
+    for (var i = 0; i < 100; i++){
+        arr.push(i);
+    }
+    
+    return arr;
 
 }
 
 function onlyEvens() {
-    // should return an array with all even numbers between 1 - 99
+    var arr = [];
+    for (var i = 1; i < 100; i++){
+        if (i % 2== 0){
+        arr.push(i);
+        }
+    }
+    
+    return arr;
 
 }
 
@@ -17,9 +29,25 @@ function fizzBuzz() {
     // divisible by 3 are replaced by the string "fizz", numbers divisible by 5
     // are replaced by the string "buzz", and the numbers divisble by both 3 and
     // 5 are replaced by fizzbuzz
+    var arr = [];
+    for (var i = 0; i < 100; i++){
+        if (i % 5 === 0 && i % 3 === 0){
+            arr.push("fizzbuzz");
+        }
+        else if (i % 3 === 0){
+            arr.push("fizz");    
+        }
+        else if (i % 5 === 0){
+            arr.push("buzz");           
+        }
+        else {
+            return arr;
+        }
+    }
+    console.log(arr);
+    //return arr;
 
 }
-
 
 // Tests
 
@@ -43,4 +71,3 @@ describe('#fizzBuzz', function () {
         assert.deepEqual(fizzBuzz(), array);
     });
 });
-
