327fbd530755aee9402cd12c03d2468ad6e6df02
rzmunson <rzmunson@hotmail.com>
2016-02-29 18:41:09 -0600

    Towers of Annoyed

--- apps/04TowersOfHanoi.js
+++ apps/04TowersOfHanoi.js
@@ -16,23 +16,39 @@ function printStacks() {
     console.log("c: " + stacks.c);
 }
 
-function movePiece() {
-    // Your code here
+function movePiece(startStack, endStack) {
+    stacks[endStack].push(stacks[startStack].pop());
 
 }
 
-function isLegal() {
-    // Your code here
-
-}
+function isLegal(startStack, endStack) {
+    var arrayStart = stacks[startStack];
+    var arrayEnd = stacks[endStack];
+    
+    if(arrayStart[arrayStart.length - 1] < arrayEnd[arrayEnd.length -1] || arrayEnd.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
+    }
 
 function checkForWin() {
     // Your code here
-
+    if (stacks.b.length === 4 || stacks.c.length === 4) {
+        console.log("You win!");
+        return true;
+    } else {
+        return false;
+    };
 }
+
 
 function towersOfHanoi(startStack, endStack) {
     // Your code here
+    isLegal(startStack, endStack);
+    
+    movePiece(startStack, endStack);
+    checkForWin();
 
 }
 
@@ -57,9 +73,19 @@ if (typeof describe !== 'undefined') {
 
     describe('#isLegal()', function () {
         it('should not allow an illegal move', function () {
+            stacks = {
+              a: [4, 3, 2],
+              b: [1],
+              c: []
+            };
             assert.equal(isLegal('a', 'b'), false);
         });
         it('should allow a legal move', function () {
+            stacks = {
+              a: [4, 3, 2, 1],
+              b: [],
+              c: []
+            };
             assert.equal(isLegal('a', 'c'), true);
         });
     });
