06d002fd682a454e30d30a51f78a0a5155a9c38d
alonzoalayon <alonzo@campgladiator.com>
2016-03-01 22:22:17 -0600

    Lesson Five passing

--- test/05LessonFiveTest.js
+++ test/05LessonFiveTest.js
@@ -4,12 +4,22 @@ var assert = require('assert');
 
 function loopIt() {
     // should return an array with numbers 0 - 99
-
+    var points = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99 ];
+    for (var i = 0; i < points.length; i++) {
+        console.log(points[i]); 
+       
+    }
+    return points;
 }
 
 function onlyEvens() {
     // should return an array with all even numbers between 1 - 99
-
+    var points = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99 ];
+    var b = [];
+    for (var i = 2; i < points.length; i+=2) {
+        b.push(points[i]);
+    }
+    return b;
 }
 
 function fizzBuzz() {
@@ -17,7 +27,23 @@ function fizzBuzz() {
     // divisible by 3 are replaced by the string "fizz", numbers divisible by 5
     // are replaced by the string "buzz", and the numbers divisble by both 3 and
     // 5 are replaced by fizzbuzz
-
+    var points = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99 ];
+    var b = [];
+    for (var i = 0; i < points.length; i++) {
+        if(((points[i] % 5) === 0) && ((points[i] % 3) === 0)){   
+            b.push('fizzbuzz');
+        }
+        else if((points[i] % 5) === 0){
+            b.push('buzz');
+        }
+        else if((points[i] % 3) === 0){
+            b.push('fizz');
+        }        
+        else{    
+            b.push(points[i]);
+        }
+    }
+    return b;
 }
 
 
