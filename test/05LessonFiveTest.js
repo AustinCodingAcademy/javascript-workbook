27b5fb2862d2f3a340e21cecda8812eb798ba6d2
rzmunson <rzmunson@hotmail.com>
2016-03-02 19:17:40 -0600

    Lesson 5 Test not all passing

--- test/05LessonFiveTest.js
+++ test/05LessonFiveTest.js
@@ -4,12 +4,23 @@ var assert = require('assert');
 
 function loopIt() {
     // should return an array with numbers 0 - 99
+    var arr = [];
+    for (var i = 0; i <= 99; i++) {
+        arr.push(i);
+    }
+    return arr;
+    
 
 }
 
 function onlyEvens() {
     // should return an array with all even numbers between 1 - 99
-
+    var arr = [];
+    for (var i = 1; i < 99; i += 2) {
+        arr.push(i + 1);
+        
+    }
+    return arr;
 }
 
 function fizzBuzz() {
@@ -17,7 +28,20 @@ function fizzBuzz() {
     // divisible by 3 are replaced by the string "fizz", numbers divisible by 5
     // are replaced by the string "buzz", and the numbers divisble by both 3 and
     // 5 are replaced by fizzbuzz
+    var arr = [];
+    
+    for (var i = 0; i < 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0 && i === 0) {
+            return "fizzbuzz";
+        } else if (i % 5 === 0) {
+            return "buzz";
+        } else if (i % 3 === 0) {
+            return "fizz";
+        } else {
+            return arr.push(i);
+        }
 
+}
 }
 
 
