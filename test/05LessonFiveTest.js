9adc329db0a0a89611bcf34913d57c0632b7d75a
Rampaggio <rampyatx@gmail.com>
2016-03-02 19:05:41 -0600

    It Is Clicking

--- test/05LessonFiveTest.js
+++ test/05LessonFiveTest.js
@@ -4,11 +4,24 @@ var assert = require('assert');
 
 function loopIt() {
     // should return an array with numbers 0 - 99
-
+    var arr = [];
+    
+    for (var i = 0; i < 100; i++) {
+        arr.push(i);
+    }
+    
+    return arr;
 }
 
 function onlyEvens() {
     // should return an array with all even numbers between 1 - 99
+    var arr = [];
+    
+    for (var i = 2; i <= 99; i += 2) {
+        arr.push(i);
+    }
+
+    return arr;
 
 }
 
@@ -17,11 +30,23 @@ function fizzBuzz() {
     // divisible by 3 are replaced by the string "fizz", numbers divisible by 5
     // are replaced by the string "buzz", and the numbers divisble by both 3 and
     // 5 are replaced by fizzbuzz
+    var arr = [];
+    
+    for (var i = 0; i < 100; i++) {
+        if ((i % 3 === 0) && (i % 5 === 0)) {
+            arr.push('fizzbuzz');
+        } else if (i % 5 === 0) {
+            arr.push('buzz');
+        } else if (i % 3 === 0) {
+            arr.push('fizz');
+        } else {
+            arr.push(i);
+        }
+    } 
+    
+    return arr;
 
 }
-
-
-// Tests
 
 describe('#loopIt', function () {
     it('should return an array with numbers 0 - 99', function () {
