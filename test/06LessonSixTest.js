9ad1b892f7e557cca94a45dcbfd1bf9565ec9452
Richard Davis <laura@Lauras-MacBook-Pro.local>
2016-03-09 20:02:15 -0600

    06LessonTest

--- test/06LessonSixTest.js
+++ test/06LessonSixTest.js
@@ -11,59 +11,84 @@ var assert = require('assert');
 // .remove should also take one argument, car. look for the index of the car in this.cars. If it is found, splice it out of this.cars. If it isn't found, return "That car isn't here!"
 
 // Your code here
+function Car(color) {
+	this.color = color;
+}
 
+function Garage(size) {
+	this.size = size;
+	this.cars = [];
+	this.add = function (car) {
+		if (this.cars.length < this.size) {
+			this.cars.push(car);
+		} else {
+			return "Not enough space!";
+		}
+	}
+	this.remove = function (car) {
+		for (var i = 0; i <= this.cars.length; i++) {
+
+
+			if (this.cars[i] === car) {
+				this.cars.splice(i, 1);
+			} else {
+				return "That car isn't here!";
+			}
+		}
+	}
+}
 // Tests
 
 var redCar, blueCar, greenCar, yellowCar, twoCarGarage, threeCarGarage
 
 it('should be able to instatiate car objects', function () {
-    redCar = new Car('red');
-    assert.equal(redCar.color, 'red');
+	redCar = new Car('red');
+	assert.equal(redCar.color, 'red');
 
-    blueCar = new Car('blue');
-    assert.equal(blueCar.color, 'blue');
+	blueCar = new Car('blue');
+	assert.equal(blueCar.color, 'blue');
 
-    greenCar = new Car('green');
-    assert.equal(greenCar.color, 'green');
+	greenCar = new Car('green');
+	assert.equal(greenCar.color, 'green');
 
-    yellowCar = new Car('yellow');
-    assert.equal(yellowCar.color, 'yellow');
+	yellowCar = new Car('yellow');
+	assert.equal(yellowCar.color, 'yellow');
 });
 
 it('should be able to instatiate garage objects', function () {
-    twoCarGarage = new Garage(2);
-    assert.equal(twoCarGarage.size, 2);
+	twoCarGarage = new Garage(2);
+	assert.equal(twoCarGarage.size, 2);
 
-    threeCarGarage = new Garage(3);
-    assert.equal(threeCarGarage.size, 3);
+	threeCarGarage = new Garage(3);
+	assert.equal(threeCarGarage.size, 3);
 });
 
 it('should be able to add cars to a garage', function () {
-    twoCarGarage.add(redCar);
-    twoCarGarage.add(blueCar);
-    assert.equal(twoCarGarage.cars.length, 2);
+	twoCarGarage.add(redCar);
+	twoCarGarage.add(blueCar);
+	assert.equal(twoCarGarage.cars.length, 2);
 
-    threeCarGarage.add(greenCar);
-    threeCarGarage.add(yellowCar);
-    assert.equal(threeCarGarage.cars.length, 2);
+	threeCarGarage.add(greenCar);
+	threeCarGarage.add(yellowCar);
+	assert.equal(threeCarGarage.cars.length, 2);
 });
 
 it('should be able to remove cars from a garage', function () {
-    twoCarGarage.remove(redCar);
-    assert.equal(twoCarGarage.cars.length, 1);
+	twoCarGarage.remove(redCar);
+	assert.equal(twoCarGarage.cars.length, 1);
 
-    threeCarGarage.add(redCar);
-    assert.equal(threeCarGarage.cars.length, 3);
+	threeCarGarage.add(redCar);
+	assert.equal(threeCarGarage.cars.length, 3);
 });
 
 it('should be able to detect of a garage is full', function () {
-    twoCarGarage.remove(blueCar);
-    assert.equal(twoCarGarage.cars.length, 0);
+	twoCarGarage.remove(blueCar);
+	assert.equal(twoCarGarage.cars.length, 0);
 
-    threeCarGarage.add(blueCar);
-    assert.equal(threeCarGarage.add(blueCar), 'Not enough space!');
+	threeCarGarage.add(blueCar);
+	assert.equal(threeCarGarage.add(blueCar), 'Not enough space!');
 });
 
 it('should be able to detect if a car is not in a garage', function () {
-    assert.equal(twoCarGarage.remove(blueCar), "That car isn't here!");
+	assert.equal(twoCarGarage.remove(blueCar), "That car isn't here!");
 });
