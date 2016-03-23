1fd40e841edef82edba1f58d9d81e6231747e805
andrew albrecht <andrewalbrecht@utexas.edu>
2016-03-23 18:33:36 -0500

    {Checkers}

--- apps/06Checkers.js
+++ apps/06Checkers.js
@@ -5,14 +5,54 @@ var prompt = require('prompt');
 prompt.start();
 
 
-function Checker() {
+function Checker(color) {
     // Your code here
+
+    if (color == 'white') {
+        this.symbol = String.fromCharCode(0x125CB)
+    } else {
+        this.symbol = String.fromCharCode(0x125CF)
+    }
+
 }
 
 function Board() {
+    this.checkers = []
+
+    this.createCheckers = function () {
+        var whitePositions = [[0, 1], [0, 3], [0, 5], [0, 7],
+                              [1, 0], [1, 2], [1, 4], [1, 6],
+                              [2, 1], [2, 3], [2, 5], [2, 7]];
+
+        var blackPositions = [[5, 0], [5, 2], [5, 4], [5, 6],
+                              [6, 1], [6, 3], [6, 5], [6, 7],
+                              [7, 0], [7, 2], [7, 4], [7, 6]];
+
+        for (var i = 0; i <= 11; i++) {
+
+            var white = new Checker('white');
+            this.grid[whitePositions[i][0]][whitePositions[i][1]] = white;
+            this.checkers.push(white);
+        }
+
+        for (var i = 0; i <= 11; i++) {
+
+            var black = new Checker('black');
+            this.grid[blackPositions[i][0]][blackPositions[i][1]] = black;
+            this.checkers.push(black);
+        }
+
+    }
+
+    this.selectChecker = function (row, column) {
+
+        return this.grid[row, column];
+
+    }
+
     this.grid = [];
     // creates an 8x8 array, filled with null values
-    this.createGrid = function() {
+    this.createGrid = function () {
         // loop to create the 8 rows
         for (var row = 0; row < 8; row++) {
             this.grid[row] = [];
@@ -24,7 +64,7 @@ function Board() {
     }
 
     // prints out the board
-    this.viewGrid = function() {
+    this.viewGrid = function () {
         // add our column numbers
         var string = "  0 1 2 3 4 5 6 7\n";
         for (var row = 0; row < 8; row++) {
@@ -49,15 +89,34 @@ function Board() {
         console.log(string);
     }
 
+    
+    this.killChecker = function (position) {
+        this.selectChecker[position]
+        this.checker.indexOf(position)
+    }
+    
+    
     // Your code here
 }
+
 function Game() {
 
     this.board = new Board();
 
-    this.start = function() {
+    this.start = function () {
         this.board.createGrid();
         // Your code here
+        this.board.createCheckers();
+    }
+
+    this.moveChecker = function (start, end) {
+
+        //60, 70
+        //[6, 0]
+
+        var checker = this.board.selectChecker(start.split('')[0], start.split('')[1]);
+        this.board.grid[start.split('')[0]][start.split('')[1]] = null;
+        this.board.grid[end.split('')[0]][end.split('')[1]] = checker;
     }
 }
 
@@ -76,11 +135,11 @@ game.start();
 // Tests
 
 if (typeof describe !== 'undefined') {
-    describe('Game', function() {
-        it('should have a board', function() {
+    describe('Game', function () {
+        it('should have a board', function () {
             assert.equal(game.board.constructor.name, 'Board');
         });
-        it('board should have 24 checkers', function() {
+        it('board should have 24 checkers', function () {
             assert.equal(game.board.checkers.length, 24);
         });
     });
@@ -95,7 +154,7 @@ if (typeof describe !== 'undefined') {
             game.moveChecker('52', '43');
             assert(game.board.grid[4][3]);
         });
-        it('should be able to jump over and kill another checker', function() {
+        it('should be able to jump over and kill another checker', function () {
             game.moveChecker('30', '52');
             assert(game.board.grid[5][2]);
             assert(!game.board.grid[4][1]);
