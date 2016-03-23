a37e639634c1e6a7a260eaa550ae679132413922
andrew albrecht <andrewalbrecht@utexas.edu>
2016-03-23 18:37:15 -0500

    {lesson 6 test}

--- test/06LessonSixTest.js
+++ test/06LessonSixTest.js
@@ -3,6 +3,32 @@
 var assert = require('assert');
 
 // Create two classes, Car and Garage
+function Car(color) {
+
+    this.color = color;
+}
+
+function Garage(size) {
+
+    this.size = size;
+    this.cars = [];
+    this.add = function (car) {
+        if (this.cars.length < this.size) {
+            this.cars.push(car);
+        } else {
+            return "Not enough space!";
+        }
+    }
+    this.remove = function (car) {
+        if (this.cars.indexOf(car) != -1) //if not -1, then it exists in array 
+        {
+            var locationOfCar = this.cars.indexOf(car);
+            this.cars.splice(locationOfCar, 1); //starts with car and splices 1 element, which is the car
+        } else {
+            return "That car isn't here!";
+        }
+    }
+}
 // The Car class should take one parameter, color. Set the passed in color to be an attribute of the object
 // The Garage should also take one parameter, size. The size will be an integer, and set it to an attribute
 // Give the Garage object an attribute, this.cars, and set it to an empty array
