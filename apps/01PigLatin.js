40058b0615945f23de02507ae0a181c7c2d127b9
Alex Vazquez <alexvazquez1220@gmail.com>
2016-05-08 11:49:34 -0500

    gh-pages-0001*pig latin homework

--- apps/01PigLatin.js
+++ apps/01PigLatin.js
@@ -5,12 +5,41 @@ var prompt = require('prompt');
 prompt.start();
 
 
-function pigLatin(word) {
+var pigLatin = function(word) {
+// Make lowercase
+word = word.toLowerCase();
+// Make vowel index equivalent to no vowels
+var vowelIndex = -1;
 
-    // Your code here
-
+ //identify vowels
+ if ( ( word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('a');
 }
-
+  if ( ( word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('e');
+}
+if ( ( word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('i');
+}
+if ( ( word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex ) || vowelIndex === -1 ) {
+   vowelIndex = word.indexOf('o');
+}
+ if ( ( word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex ) || vowelIndex === -1 ) {
+   vowelIndex = word.indexOf('u');
+}
+if ( ( word.indexOf('y') > -1 && word.indexOf('y') < vowelIndex ) || vowelIndex === -1 ) {
+   vowelIndex = word.indexOf('y');
+}
+// If "word" begins with a vowel. Add 'yay'
+if (vowelIndex === 0) {
+  return word + 'yay'
+;
+}
+// Slice the word according to vowelIndex
+var firstPart = word.slice(0, vowelIndex);
+var restWord = word.slice(vowelIndex, word.length);
+// Return the piglatin word
+return restWord + firstPart + 'ay';
 
 function getPrompt() {
     prompt.get(['word'], function (error, result) {
