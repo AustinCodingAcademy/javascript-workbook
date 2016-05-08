dda523d7be5fc4c6d4221c2dc199f568273eb507
kieranypyne <kieranypyne@gmail.com>
2016-05-08 16:43:38 -0500

    Fixed the error from fixing my error

--- apps/01PigLatin.js
+++ apps/01PigLatin.js
@@ -17,19 +17,19 @@ function pigLatin(word) {
     if ( ( word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
     vowelIndex = word.indexOf('a');
     }
-    if ( ( word.indexOf('i') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
+    if ( ( word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex ) || vowelIndex === -1 ) {
     vowelIndex = word.indexOf('i');
     }
-    if ( ( word.indexOf('e') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
+    if ( ( word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex ) || vowelIndex === -1 ) {
     vowelIndex = word.indexOf('e');
     }
-    if ( ( word.indexOf('o') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
+    if ( ( word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex ) || vowelIndex === -1 ) {
     vowelIndex = word.indexOf('o');
     }
-    if ( ( word.indexOf('u') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
+    if ( ( word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex ) || vowelIndex === -1 ) {
     vowelIndex = word.indexOf('u');
     }
-    if ( ( word.indexOf('y') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
+    if ( ( word.indexOf('y') > -1 && word.indexOf('y') < vowelIndex ) || vowelIndex === -1 ) {
     vowelIndex = word.indexOf('y');
     }
     //in the event of the word having a vowel first letter, or no vowels
