0c6317885dbf58b927fa53dfff45f367bf8181bd
proPerry <perry.protzmann@gmail.com>
2016-05-11 20:46:57 -0500

    Teacher's way (not working)

--- apps/01PigLatin.js
+++ apps/01PigLatin.js
@@ -4,14 +4,56 @@ var assert = require('assert');
 var prompt = require('prompt');
 prompt.start();
 
-
 function pigLatin(word) {
 
-    // Your code here
+word = word.toLowerCase();
 
+    var vowelIndex = 0;
+
+    var vowel = ['a', 'e', 'i', 'o', 'u'];
+    /*if ( ( word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('a');
 }
+    if ( ( word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('a');
+}
+if ( ( word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('e');
+}
+if ( ( word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('i');
+}
+if ( ( word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('o');
+}
+if ( ( word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('u');
+}*/
 
+//var begin = word.slice(0, vowelIndex);
+//var end = word.slice(vowelIndex, word.length);
 
+    //if (word[0] === 'a' || word[0] === 'e' || word[0] === 'i' || word[0] === 'o' || word[0] === 'u' ) {}
+    /*if (isVowel(word[0])) {
+        return word + "yay";
+    } else {
+        return restWord + firstPart + 'ay';
+        vowel.indexOf(letter) !== -1
+    }*/
+    function isVowel(letter) {
+            while(!isVowel(word[vowelIndex])) {
+                vowelIndex ++;
+            }
+            if (vowelIndex === 0) {
+                return (word + "yay").toLowerCase();
+            } else {
+                var begin = word.slice(0, vowelIndex);
+                var end = word.slice(vowelIndex, word.length);
+
+                return (end + begin + 'ay').toLowerCase();
+            }
+        }
+}
 function getPrompt() {
     prompt.get(['word'], function (error, result) {
 
@@ -21,7 +63,7 @@ function getPrompt() {
     });
 }
 
-// Tests
+    // Tests
 
 if (typeof describe !== 'undefined') {
 
