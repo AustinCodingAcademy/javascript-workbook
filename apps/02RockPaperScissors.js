1cf74e94a362de80243f585775482b5762f1b784
kelleymccauley <klmccauley74@gmail.com>
2016-05-12 19:04:53 -0500

    Rock Paper Scissors Submission - thanks!

--- apps/02RockPaperScissors.js
+++ apps/02RockPaperScissors.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 var assert = require('assert');
@@ -7,10 +8,47 @@ prompt.start();
 
 function rockPaperScissors(hand1, hand2) {
 
-    // Write code here
 
+  hand1 = hand1.toLowerCase();
+  hand2 = hand2.toLowerCase();
+
+  if (hand1 === hand2){
+    return "It's a tie!";
+  }
+
+  if (hand1 ==='rock'){
+    if (hand2 === 'scissors'){
+      return "Hand one wins!";
+      }
+  }
+  if (hand1 ==='rock'){
+    if (hand2 ==='paper'){
+      return "Hand two wins!";
+     }
+  }
+  if (hand1 === 'paper'){
+    if (hand2 === 'rock'){
+      return "Hand one wins!";
+      }
+  }
+  if (hand1 ==='paper'){
+    if (hand2 ==='scissors'){
+      return "Hand two wins!";
+      }
+  }
+
+  if (hand1 ==='scissors'){
+      if (hand2 === 'paper'){
+        return "Hand one wins!";
+        }
+      }
+
+  if (hand1 ==='scissors'){
+    if (hand2 ==='paper'){
+      return "Hand two wins!";
+      }
+    }
 }
-
 function getPrompt() {
     prompt.get(['hand1', 'hand2'], function (error, result) {
 
