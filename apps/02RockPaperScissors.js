a12cfa749936a0e6c3c32fd4568cd8e83f5f3c08
MrGgit7Gro <growmatthew3000@gmail.com>
2016-05-15 13:50:26 -0400

    02RockPaperScissors

--- apps/02RockPaperScissors.js
+++ apps/02RockPaperScissors.js
@@ -6,9 +6,21 @@ prompt.start();
 
 
 function rockPaperScissors(hand1, hand2) {
-
-    // Write code here
-
+  if(hand1===hand2){
+    return "It's a tie!"
+  }else if(hand1==="paper" && hand2==="rock"){ //"hand1===paper + hand2===rock"
+    return "Hand one wins!";
+  }else if (hand1==="paper" && hand2==="scissors"){
+      return "Hand two wins!" ;
+  }else if(hand1==="rock" && hand2==="paper"){
+    return "Hand two wins!";
+  }else if (hand1==="rock" && hand2==="scissors"){
+    return "Hand one wins!";
+  }else if (hand1==="scissors" && hand2==="rock"){
+    return "Hand two wins!";
+  }else if (hand1==="scissors" && hand2==="paper"){
+        return "Hand one wins!";
+  }
 }
 
 function getPrompt() {
