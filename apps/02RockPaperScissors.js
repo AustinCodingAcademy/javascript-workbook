10d99ee4940a18eb259654c9e1d8761ad42e81a2
kelleymccauley <klmccauley74@gmail.com>
2016-05-15 13:56:10 -0500

    Resubmiting RockPaperScissors with minor changes

--- apps/02RockPaperScissors.js
+++ apps/02RockPaperScissors.js
@@ -19,36 +19,28 @@ function rockPaperScissors(hand1, hand2) {
   if (hand1 ==='rock'){
     if (hand2 === 'scissors'){
       return "Hand one wins!";
-      }
-  }
-  if (hand1 ==='rock'){
-    if (hand2 ==='paper'){
-      return "Hand two wins!";
-     }
-  }
-  if (hand1 === 'paper'){
-    if (hand2 === 'rock'){
-      return "Hand one wins!";
-      }
-  }
-  if (hand1 ==='paper'){
-    if (hand2 ==='scissors'){
-      return "Hand two wins!";
-      }
-  }
-
-  if (hand1 ==='scissors'){
-      if (hand2 === 'paper'){
-        return "Hand one wins!";
-        }
-      }
-
-  if (hand1 ==='scissors'){
-    if (hand2 ==='paper'){
-      return "Hand two wins!";
-      }
+      }  else {
+        return "Hand two wins!"
     }
+  }
+
+  if (hand1 ==='paper'){
+    if (hand2 ==='rock'){
+      return "Hand one wins!";
+      } else {
+        return "Hand two wins!"
+    }
+  }
+
+  if (hand1 === 'scissors'){
+    if (hand2 === 'paper'){
+      return "Hand one wins!";
+      } else {
+        return "Hand two wins!"
+    }
+  }
 }
+
 function getPrompt() {
     prompt.get(['hand1', 'hand2'], function (error, result) {
 
