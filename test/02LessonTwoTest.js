c7e81b15aed7affac184353b97ea2130cfc40442
Joshua Reola <jreola@cisco.com>
2016-05-16 18:19:46 -0400

    Fixed 02LessonTwoTest.js

--- test/02LessonTwoTest.js
+++ test/02LessonTwoTest.js
@@ -3,6 +3,20 @@
 var assert = require('assert');
 
 function whichSpecies(character) {
+  if ((character) === 'scooby'){
+    return 'dog';
+  }
+  else if((character)=== 'garfield'){
+    return 'cat';
+  }
+  else if((character) === 'nemo'){
+    return 'fish';
+  }
+  else{
+    return false;
+  }
+
+
     // should return "dog" when character is 'scooby'
     // should return "cat" when character is 'garfield'
     // should return "fish" when character is 'nemo'
@@ -11,6 +25,12 @@ function whichSpecies(character) {
 }
 
 function isEven(number) {
+    if(number %2 === 0){
+      return true;
+    }
+    else{
+      return false;
+    }
     // should return true is number is even (divisible by 2)
 
 }
