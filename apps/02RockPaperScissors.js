1c59a0b7963593ff029ca1420706e19da93512c9
Walker <jwalkerlyle@gmail.com>
2016-05-16 19:17:55 -0500

    rps in class

--- apps/02RockPaperScissors.js
+++ apps/02RockPaperScissors.js
@@ -6,39 +6,39 @@ prompt.start();
 
 
 function rockPaperScissors(hand1, hand2) {
-
+hand1 = hand1.toLowerCase();
+hand2 = randomizeHand();
     // Write code here
-    if (hand1 === hand2) {
-      return "It's a tie!";
-    }
-    else {
+      if (hand1 === hand2) {
+        return "It's a tie!";
+      }
 
-      if (hand1 === 'rock') {
-        if (hand2 === 'scissors') {
-          return 'Hand one wins!';
+      else if (hand1 === 'rock') {
+          if (hand2 === 'scissors') {
+            return "Hand one wins!";
+          }
+          else if (hand2 ==='paper') {
+            return "Hand two wins!";
+          }
         }
-        else {
-          return 'Hand two wins!';
+      else if (hand1 === 'paper') {
+          if (hand2 === 'rock') {
+            return 'Hand one wins!';
+          }
+          else if (hand2 ==='scissors') {
+            return 'Hand two wins!';
+          }
+        }
+
+      else if(hand1 === 'scissors') {
+          if(hand2==='rock') {
+            return "Hand two wins!";
+          }
+          else if (hand2==='paper') {
+            return "Hand two wins";
+          }
         }
       }
-      if (hand1 === 'paper') {
-        if (hand2 === 'rock') {
-          return 'Hand one wins!';
-        }
-        else {
-          return 'Hand two wins!';
-        }
-      }
-      if (hand1 === 'scissors') {
-        if (hand2 === 'paper') {
-          return 'Hand one wins!';
-        }
-        else {
-          return 'Hand two wins!';
-        }
-      }
-    }
-}
 
 function getPrompt() {
     prompt.get(['hand1', 'hand2'], function (error, result) {
