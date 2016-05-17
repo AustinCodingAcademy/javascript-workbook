46dbb92dc5c7f78ad8f95f4aac930fdfc87f7531
bojangle5 <zpearce2000@yahoo.com>
2016-05-17 01:06:00 -0500

    ZP_int1_02LessonTwo_commit

--- test/02LessonTwoTest.js
+++ test/02LessonTwoTest.js
@@ -25,7 +25,7 @@ function whichSpecies(character) {
 
 function isEven(number) {
     // should return true is number is even (divisible by 2)
-    if (number % 2 == 0) {
+    if (number % 2 === 0) {
       return true;
   	}
     else {
