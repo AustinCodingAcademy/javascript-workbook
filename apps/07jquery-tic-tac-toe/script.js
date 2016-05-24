aa2b982b687c52a3e53a2964b8ba31f30350d5ea
Walker <jwalkerlyle@gmail.com>
2016-05-23 21:00:18 -0500

    class work

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -1,5 +1,55 @@
 'use strict';
+var playerTurn = 'x';
+function checkForWin() {
+  if($('[data-cell="0"]').text()===playerTurn&&$('[data-cell="1"]').text()===playerTurn&&$('[data-cell="2"]').text()===playerTurn) {
+       $('#announce-winner').text('"Player " + playerTurn + "Wins!" ')
+     } //copy this ^^^ for these vvv
+  else if
+       ($('[data-cell = "3"]').text()===playerTurn)&&
+       $('[data-cell = "4"]').text()===playerTurn)&&
+       $('[data-cell = "5"]').text()===playerTurn)) {
+         $('#announce-winner').text('"Player " + playerTurn + "Wins!" ')
+     }
+  else if
+       ($('[data-cell = "0"]').text()===playerTurn)&&
+       $('[data-cell = "3"]').text()===playerTurn)&&
+       $('[data-cell = "6"]').text()===playerTurn)) {
+         $('#announce-winner').text('"Player " + playerTurn + "Wins!" ')
+       }
+  else if
+        ($('[data-cell = "1"]').text()===playerTurn)&&
+        $('[data-cell = "4"]').text()===playerTurn)&&
+        $('[data-cell = "7"]').text()===playerTurn)) {
+         $('#announce-winner').text('"Player " + playerTurn + "Wins!" ')
+            }
+  else if
+        ($('[data-cell = "2"]').text()===playerTurn)&&
+         $('[data-cell = "5"]').text()===playerTurn)&&
+         $('[data-cell = "8"]').text()===playerTurn)) {
+              $('#announce-winner').text('"Player " + playerTurn + "Wins!" ')
+        }
+  else if
+        ($('[data-cell = "2"]').text()===playerTurn)&&
+        $('[data-cell = "5"]').text()===playerTurn)&&
+        $('[data-cell = "8"]').text()===playerTurn)) {
+            $('#announce-winner').text('"Player " + playerTurn + "Wins!" ')
+        }
+  else if
+        ($('[data-cell = "2"]').text()===playerTurn)&&
+        $('[data-cell = "5"]').text()===playerTurn)&&
+        $('[data-cell = "8"]').text()===playerTurn)) {
+            $('#announce-winner').text('"Player " + playerTurn + "Wins!" ')
+        }
+   }
+
 
 $(document).on('ready', function() {
     // Put app logic in here
+
+    $('[data-cells]').click(function() {
+      $(this).text(playerTurn); //place mark
+      checkForWin();
+      playerTurn=(playerTurn=== 'x') ? 'O' : 'X';
+    });
+
 });
