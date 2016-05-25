0d6d207e6ccc5932e95a0c9589803d22bae878b4
Walker <jwalkerlyle@gmail.com>
2016-05-25 11:26:40 -0500

    removed extra parentheses

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -5,39 +5,27 @@ function checkForWin() {
        $('#announce-winner').text('"Player " + playerTurn + "Wins!" ')
      } //copy this ^^^ for these vvv
   else if
-       ($('[data-cell = "3"]').text()===playerTurn)&&
-       $('[data-cell = "4"]').text()===playerTurn)&&
-       $('[data-cell = "5"]').text()===playerTurn)) {
+       ($('[data-cell = "3"]').text()===playerTurn)&&$('[data-cell = "4"]').text()===playerTurn)&&$('[data-cell = "5"]').text()===playerTurn) {
          $('#announce-winner').text('"Player " + playerTurn + "Wins!" ')
      }
   else if
-       ($('[data-cell = "0"]').text()===playerTurn)&&
-       $('[data-cell = "3"]').text()===playerTurn)&&
-       $('[data-cell = "6"]').text()===playerTurn)) {
+       ($('[data-cell = "0"]').text()===playerTurn)&&$('[data-cell = "3"]').text()===playerTurn)&&$('[data-cell = "6"]').text()===playerTurn) {
          $('#announce-winner').text('"Player " + playerTurn + "Wins!" ')
        }
   else if
-        ($('[data-cell = "1"]').text()===playerTurn)&&
-        $('[data-cell = "4"]').text()===playerTurn)&&
-        $('[data-cell = "7"]').text()===playerTurn)) {
+        ($('[data-cell = "1"]').text()===playerTurn)&&$('[data-cell = "4"]').text()===playerTurn)&&$('[data-cell = "7"]').text()===playerTurn) {
          $('#announce-winner').text('"Player " + playerTurn + "Wins!" ')
             }
   else if
-        ($('[data-cell = "2"]').text()===playerTurn)&&
-         $('[data-cell = "5"]').text()===playerTurn)&&
-         $('[data-cell = "8"]').text()===playerTurn)) {
+        ($('[data-cell = "2"]').text()===playerTurn)&&$('[data-cell = "5"]').text()===playerTurn)&&$('[data-cell = "8"]').text()===playerTurn) {
               $('#announce-winner').text('"Player " + playerTurn + "Wins!" ')
         }
   else if
-        ($('[data-cell = "2"]').text()===playerTurn)&&
-        $('[data-cell = "5"]').text()===playerTurn)&&
-        $('[data-cell = "8"]').text()===playerTurn)) {
+        ($('[data-cell = "2"]').text()===playerTurn)&&$('[data-cell = "5"]').text()===playerTurn)&&$('[data-cell = "8"]').text()===playerTurn) {
             $('#announce-winner').text('"Player " + playerTurn + "Wins!" ')
         }
   else if
-        ($('[data-cell = "2"]').text()===playerTurn)&&
-        $('[data-cell = "5"]').text()===playerTurn)&&
-        $('[data-cell = "8"]').text()===playerTurn)) {
+        ($('[data-cell = "2"]').text()===playerTurn)&&$('[data-cell = "5"]').text()===playerTurn)&&$('[data-cell = "8"]').text()===playerTurn) {
             $('#announce-winner').text('"Player " + playerTurn + "Wins!" ')
         }
    }
