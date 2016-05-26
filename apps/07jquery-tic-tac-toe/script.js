cd7b318b3fb12227aaeb372b89b0ed389a9912b2
Walker <jwalkerlyle@gmail.com>
2016-05-25 20:12:58 -0500

    all wins now return message

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -9,6 +9,10 @@ function checkForWin() {
          $('#announce-winner').text("Player " + playerTurn + " Wins!")
      }
   else if
+       ($('[data-cell = "6"]').text()===playerTurn&& $('[data-cell = "7"]').text()===playerTurn&& $('[data-cell = "8"]').text()===playerTurn) {
+         $('#announce-winner').text("Player " + playerTurn + " Wins!")
+        }
+  else if
        ($('[data-cell = "0"]').text()===playerTurn&& $('[data-cell = "3"]').text()===playerTurn&& $('[data-cell = "6"]').text()===playerTurn) {
          $('#announce-winner').text("Player " + playerTurn + " Wins!")
        }
@@ -21,11 +25,11 @@ function checkForWin() {
               $('#announce-winner').text("Player " + playerTurn + " Wins!")
         }
   else if
-        ($('[data-cell = "2"]').text()===playerTurn&& $('[data-cell = "5"]').text()===playerTurn&& $('[data-cell = "8"]').text()===playerTurn) {
+        ($('[data-cell = "0"]').text()===playerTurn&& $('[data-cell = "4"]').text()===playerTurn&& $('[data-cell = "8"]').text()===playerTurn) {
             $('#announce-winner').text("Player " + playerTurn + " Wins!")
         }
   else if
-        ($('[data-cell = "2"]').text()===playerTurn&& $('[data-cell = "5"]').text()===playerTurn&& $('[data-cell = "8"]').text()===playerTurn) {
+        ($('[data-cell = "2"]').text()===playerTurn&& $('[data-cell = "4"]').text()===playerTurn&& $('[data-cell = "6"]').text()===playerTurn) {
             $('#announce-winner').text("Player " + playerTurn + " Wins!" )
         }
    }
