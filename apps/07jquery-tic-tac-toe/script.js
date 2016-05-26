3a8b0f4f020a275a7e85d5c310913f778fd721c3
Walker <jwalkerlyle@gmail.com>
2016-05-25 19:57:36 -0500

    jquery tictactoe working

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -1,32 +1,32 @@
 'use strict';
-var playerTurn = 'x';
+var playerTurn = 'X';
 function checkForWin() {
-  if($('[data-cell="0"]').text()===playerTurn&&$('[data-cell="1"]').text()===playerTurn&&$('[data-cell="2"]').text()===playerTurn) {
-       $('#announce-winner').text('"Player " + playerTurn + "Wins!" ')
+  if($('[data-cell="0"]').text()===playerTurn&& $('[data-cell="1"]').text()===playerTurn&& $('[data-cell="2"]').text()===playerTurn) {
+       $('#announce-winner').text("Player " + playerTurn + " Wins!")
      } //copy this ^^^ for these vvv
   else if
-       ($('[data-cell = "3"]').text()===playerTurn)&&$('[data-cell = "4"]').text()===playerTurn)&&$('[data-cell = "5"]').text()===playerTurn) {
-         $('#announce-winner').text('"Player " + playerTurn + "Wins!" ')
+       ($('[data-cell = "3"]').text()===playerTurn&& $('[data-cell = "4"]').text()===playerTurn&& $('[data-cell = "5"]').text()===playerTurn) {
+         $('#announce-winner').text("Player " + playerTurn + " Wins!")
      }
   else if
-       ($('[data-cell = "0"]').text()===playerTurn)&&$('[data-cell = "3"]').text()===playerTurn)&&$('[data-cell = "6"]').text()===playerTurn) {
-         $('#announce-winner').text('"Player " + playerTurn + "Wins!" ')
+       ($('[data-cell = "0"]').text()===playerTurn&& $('[data-cell = "3"]').text()===playerTurn&& $('[data-cell = "6"]').text()===playerTurn) {
+         $('#announce-winner').text("Player " + playerTurn + " Wins!")
        }
   else if
-        ($('[data-cell = "1"]').text()===playerTurn)&&$('[data-cell = "4"]').text()===playerTurn)&&$('[data-cell = "7"]').text()===playerTurn) {
-         $('#announce-winner').text('"Player " + playerTurn + "Wins!" ')
+        ($('[data-cell = "1"]').text()===playerTurn&& $('[data-cell = "4"]').text()===playerTurn&& $('[data-cell = "7"]').text()===playerTurn) {
+         $('#announce-winner').text("Player " + playerTurn + " Wins!")
             }
   else if
-        ($('[data-cell = "2"]').text()===playerTurn)&&$('[data-cell = "5"]').text()===playerTurn)&&$('[data-cell = "8"]').text()===playerTurn) {
-              $('#announce-winner').text('"Player " + playerTurn + "Wins!" ')
+        ($('[data-cell = "2"]').text()===playerTurn&& $('[data-cell = "5"]').text()===playerTurn&& $('[data-cell = "8"]').text()===playerTurn) {
+              $('#announce-winner').text("Player " + playerTurn + " Wins!")
         }
   else if
-        ($('[data-cell = "2"]').text()===playerTurn)&&$('[data-cell = "5"]').text()===playerTurn)&&$('[data-cell = "8"]').text()===playerTurn) {
-            $('#announce-winner').text('"Player " + playerTurn + "Wins!" ')
+        ($('[data-cell = "2"]').text()===playerTurn&& $('[data-cell = "5"]').text()===playerTurn&& $('[data-cell = "8"]').text()===playerTurn) {
+            $('#announce-winner').text("Player " + playerTurn + " Wins!")
         }
   else if
-        ($('[data-cell = "2"]').text()===playerTurn)&&$('[data-cell = "5"]').text()===playerTurn)&&$('[data-cell = "8"]').text()===playerTurn) {
-            $('#announce-winner').text('"Player " + playerTurn + "Wins!" ')
+        ($('[data-cell = "2"]').text()===playerTurn&& $('[data-cell = "5"]').text()===playerTurn&& $('[data-cell = "8"]').text()===playerTurn) {
+            $('#announce-winner').text("Player " + playerTurn + " Wins!" )
         }
    }
 
@@ -34,10 +34,10 @@ function checkForWin() {
 $(document).on('ready', function() {
     // Put app logic in here
 
-    $('[data-cells]').click(function() {
+    $('[data-cell]').click(function() {
       $(this).text(playerTurn); //place mark
       checkForWin();
-      playerTurn=(playerTurn=== 'x') ? 'O' : 'X';
+      playerTurn=(playerTurn=== 'X') ? 'O' : 'X';
     });
 
 });
