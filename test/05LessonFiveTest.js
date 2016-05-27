bd89e04323bb14298d523d0ec7360c1da24d5014
Neville Medhora <nevmed@gmail.com>
2016-05-26 20:46:28 -0500

    test 5

--- test/05LessonFiveTest.js
+++ test/05LessonFiveTest.js
@@ -4,19 +4,56 @@ var assert = require('assert');
 
 function loopIt() {
     // should return an array with numbers 0 - 99
+    
+    var array = [];
+    for (var i = 0; i < 100; i++){
+        array.push(i);
+    }
+        return array;
 
 }
 
 function onlyEvens() {
     // should return an array with all even numbers between 1 - 99
-
+    
+    var array = [];
+    for (var i = 2; i < 100; i+=2){
+        array.push(i);
+    }
+        return array;
 }
+
 
 function fizzBuzz() {
     // should return an array of numbers between 0 - 99 where the numbers
     // divisible by 3 are replaced by the string "fizz", numbers divisible by 5
     // are replaced by the string "buzz", and the numbers divisble by both 3 and
     // 5 are replaced by fizzbuzz
+
+    var array = [];
+    for (var i = 0; i < 100; i++){
+        
+        //if they are divisble by 3 then output fizz
+        
+        if ((i % 3 === 0) && (i % 5 === 0))   {
+            array.push('fizzbuzz');
+        }
+
+        else if (i % 3 === 0){
+            array.push('fizz');
+        }        
+
+        //if they are divisble by 5 then output buzz
+        else if (i % 5 === 0){
+            array.push('buzz');
+        }
+
+        else { array.push(i); }
+    
+    }
+
+    console.log(array);
+    return array;
 
 }
 
