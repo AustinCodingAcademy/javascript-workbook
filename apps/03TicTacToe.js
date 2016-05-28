d04188808668b190e132ee71957d504ff746e037
proPerry <perry.protzmann@gmail.com>
2016-05-28 18:43:53 -0500

    TicTacToe done

--- apps/03TicTacToe.js
+++ apps/03TicTacToe.js
@@ -12,6 +12,10 @@ var board = [
 
 var playerTurn = 'X';
 
+function togglePlayer() {
+    playerTurn = (playerTurn === 'X') ?  'O' : 'X';
+}
+
 function printBoard() {
     console.log('   0  1  2');
     console.log('0 ' + board[0].join(' | '));
@@ -21,24 +25,44 @@ function printBoard() {
     console.log('2 ' + board[2].join(' | '));
 }
 
+
 function horizontalWin() {
     // Your code here
+    return (board[0][0] === playerTurn && board[0][1] === playerTurn && board[0][2] === playerTurn) ||
+            (board[1][0] === playerTurn && board[1][1] === playerTurn && board[1][2] === playerTurn) ||
+            (board[2][0] === playerTurn && board[2][1] === playerTurn && board[2][2] === playerTurn);
+
 }
 
 function verticalWin() {
     // Your code here
+    return (board[0][0] === playerTurn && board[1][0] === playerTurn && board[2][0] === playerTurn) ||
+            (board[0][1] === playerTurn && board[1][1] === playerTurn && board[2][1] === playerTurn) ||
+            (board[2][0] === playerTurn && board[1][2] === playerTurn && board[2][2] === playerTurn);
 }
 
 function diagonalWin() {
     // Your code here
+    return (board[0][0] === playerTurn && board[1][1] === playerTurn && board[2][2] === playerTurn) ||
+            (board[2][2] === playerTurn && board[1][1] === playerTurn && board[0][0] === playerTurn);
 }
 
 function checkForWin() {
     // Your code here
+        if (verticalWin() || horizontalWin() || diagonalWin()) {
+            console.log('Player ' + playerTurn + ' Won!');
+            return true;
+        } else {
+            return false;
+        }
 }
 
 function ticTacToe(row, column) {
     // Your code here
+
+    board[row][column] = playerTurn;
+    checkForWin();
+    togglePlayer();
 }
 
 function getPrompt() {
