5490d8ba6119ffa4d098ad172b54b96c744c08d6
MrGgit7Gro <growmatthew3000@gmail.com>
2016-06-01 19:17:54 -0400

    Lesson 07 jquery tic-tac-toe

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -1,5 +1,40 @@
 'use strict';
 
-$(document).on('ready', function() {
-    // Put app logic in here
+var playerTurn = 'X';
+
+
+function diagonalWin(){
+  return ($('[data-cell = "0"]').text() === playerTurn && $('[data-cell = "4" ]').text() === playerTurn && $('[data-cell = "8"]').text() === playerTurn)
+  || ($('[data-cell = "2"]').text() === playerTurn && $('[data-cell = "4"]').text() === playerTurn && $('[data-cell = "6"]').text() === playerTurn);
+}
+
+function horizontalWin(){
+  return ($('[data-cell="0"]').text() === playerTurn && $('[data-cell = "1"]').text() === playerTurn && $('[data-cell = "2"]').text() === playerTurn)
+  || ($('[data-cell = "3"]').text() === playerTurn && $('[data-cell = "4"]').text() === playerTurn && $('[data-cell ="5"]').text() === playerTurn)
+  || ($('[data-cell = "6"]').text() === playerTurn && $('[data-cell = "7"]').text() === playerTurn && $('[data-cell ="8"]').text() === playerTurn);
+}
+
+function verticalWin(){
+  return ($('[data-cell = "0"]').text() === playerTurn && $('[data-cell = "3"]').text() === playerTurn && $('[data-cell = "6"]').text() === playerTurn)
+  || ($('[data-cell = "1"]').text() === playerTurn && $('[data-cell = "4"]').text() === playerTurn && $('[data-cell = "7"]').text() === playerTurn)
+  || ($('[data-cell = "2"]').text() === playerTurn && $('[data-cell = "5"]').text() === playerTurn && $('[data-cell = "8"]').text() === playerTurn);
+  }
+
+function checkForWin(){
+  if(verticalWin() === true || horizontalWin() === true || diagonalWin() === true) {
+      $('#announce-winner').text('Player ' + playerTurn + ' Wins!');
+    return true;
+  }
+}
+
+function togglePlayerTurn() {
+  playerTurn = (playerTurn = (playerTurn ==='X') ? 'O' : 'X');
+}
+
+$(document).on('ready', function(){
+  $('[data-cell]').click(function(){
+    $(this).text(playerTurn);
+    checkForWin()
+    togglePlayerTurn();
+});
 });
