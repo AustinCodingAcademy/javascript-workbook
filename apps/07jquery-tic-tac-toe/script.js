ac578ae1409cc6998bdcda69647a04add7a5fc7a
bojangle5 <zpearce2000@yahoo.com>
2016-06-01 18:47:27 -0500

    ZP_Int1_jQueryTTT_commit

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -1,5 +1,42 @@
 'use strict';
+var playerTurn = 'X';
+
+function horizontalWin() {
+    return ($('[data-cell="0"]').text() === playerTurn && $('[data-cell="1"]').text() === playerTurn && $('[data-cell="2"]').text() === playerTurn) ||
+        ($('[data-cell="3"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="5"]').text() === playerTurn) ||
+        ($('[data-cell="6"]').text() === playerTurn && $('[data-cell="7"]').text() === playerTurn && $('[data-cell="8"]').text() === playerTurn);
+}
+
+function verticalWin() {
+    return ($('[data-cell="0"]').text() === playerTurn && $('[data-cell="3"]').text() === playerTurn && $('[data-cell="6"]').text() === playerTurn) ||
+        ($('[data-cell="1"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="7"]').text() === playerTurn) ||
+        ($('[data-cell="2"]').text() === playerTurn && $('[data-cell="5"]').text() === playerTurn && $('[data-cell="8"]').text() === playerTurn);
+}
+
+function diagonalWin() {
+    return ($('[data-cell="0"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="8"]').text() === playerTurn) ||
+        ($('[data-cell="2"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="6"]').text() === playerTurn);
+}
+
+function checkForWin() {
+    if (horizontalWin() === true || verticalWin() === true || diagonalWin() === true) {
+
+        $('#announce-winner').text('Player ' + playerTurn + ' Wins!');
+        return true;
+    }
+}
+
+function togglePlayer() {
+    playerTurn = (playerTurn = (playerTurn === 'X') ? 'O' : 'X');
+}
 
 $(document).on('ready', function() {
     // Put app logic in here
+
+    $('[data-cell]').click(function() {
+        //var playerTurn = 'X';
+        $(this).text(playerTurn);
+        checkForWin()
+        togglePlayer();
+    });
 });
