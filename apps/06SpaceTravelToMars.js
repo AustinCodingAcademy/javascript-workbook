c0590b91b9aa9468ae65b6b3a0a6ed617f85df65
Will <williamm.littler@gmail.com>
2016-06-14 21:06:26 -0500

    gh-pages-0030
    * working on spaceship to mars

--- apps/06SpaceTravelToMars.js
+++ apps/06SpaceTravelToMars.js
@@ -11,6 +11,27 @@ var jobTypes = {
 
 // Your code here
 
+function CrewMember(name, job, specialSkill) {
+    this.name = name;
+    this.job = job;
+    this.specialSkill = specialSkill;
+    this.ship = null;
+
+    this.enterShip = function (ship) {
+      this.ship = ship;
+      ship.crew.push(this);
+    }
+}
+
+function Ship(name, type, ability) {
+    this.name = name;
+    this.type = type;
+    this.ability = ability;
+    this.crew = [];
+}
+
+
+
 //tests
 if (typeof describe !== 'undefined'){
 describe('CrewMember', function(){
