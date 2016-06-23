9a4fe02ee1828801f0fbeb311143731f67873bb8
ekora87 <ekora87@gmail.com>
2016-06-22 20:24:51 -0500

    Master Mind and Space Travel

--- apps/05Mastermind.js
+++ apps/05Mastermind.js
@@ -15,23 +15,58 @@ function printBoard() {
     }
 }
 
-function generateSolution() {
-    for (var i = 0; i < 4; i++) {
-        var randomIndex = getRandomInt(0, letters.length);
-        solution += letters[randomIndex];
-    }
-}
+ function generateSolution() {
+     for (var i = 0; i < 4; i++) {
+         var randomIndex = getRandomInt(0, letters.length);
+         solution += letters[randomIndex];
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
+     }
+ }
 
-function generateHint() {
+ function getRandomInt(min, max) {
+   return Math.floor(Math.random() * (max - min)) + min;
+ }
+
+function generateHint(solution, guess) {
     // your code here
+    var solutionArray = solution.split('');
+    var guessArray = guess.split('');
+    var correctLetterLocation = 0;
+    for (var i = 0; i <solutionArray.length; i++){
+      if (solutionArray[i] === guessArray[i]){
+          correctLetterLocation ++;
+          solutionArray[i] = null;
+      }
+    }
+    var correctLetter = 0;
+
+      for (var y = 0; y < solutionArray.length; y++){
+      var targetIndex = solutionArray.indexOf(guessArray[y]);
+      if (targetIndex > -1){
+        correctLetter++;
+        solutionArray[targetIndex] = null;
+      }
+    }
+    return colors.red(correctLetterLocation) + "-" + colors.white(correctLetter);
 }
 
 function mastermind(guess) {
     // your code here
+
+    if (guess === solution){
+      return "You guessed it!";
+    }
+    else {
+      if (board.length < 10){
+        var hint = generateHint(solution, guess);
+        board.push(guess + " " + hint);
+        console.log ("Guess Again!");
+      }
+        else {
+        printBoard();
+        return "You ran out of turns. The solution is: " + solution;
+      }
+  }
 }
 
 
@@ -67,7 +102,7 @@ if (typeof describe !== 'undefined') {
         });
 
     });
-        
+
 } else {
 
     generateSolution();
--- apps/06SpaceTravelToMars.js
+++ apps/06SpaceTravelToMars.js
@@ -10,6 +10,40 @@ var jobTypes = {
 };
 
 // Your code here
+function CrewMember (name, job, specialSkill){
+  this.name = name;
+  this.job = job;
+  this.specialSkill = specialSkill;
+  this.ship = null;
+
+  this.enterShip = function(ship){
+    this.ship = ship;
+    ship.crew.push(this);
+  }
+}
+
+function Ship(name, type, ability){
+  this.name = name;
+  this.type = type;
+  this.ability = ability;
+  this.crew = [];
+
+  this.missionStatement = function(){
+    for (var i = 0; i < this.crew.length; i++){
+      var key = this.crew[i].job;
+      var value = jobTypes[key];
+      if (value === this.type){
+        return this.ability;
+      }
+    }
+
+        return "Can't perform a mission yet.";
+      }
+    }
+
+
+
+
 
 //tests
 if (typeof describe !== 'undefined'){
