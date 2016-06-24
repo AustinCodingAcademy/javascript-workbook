2d6cf6f94445878d996ff33b6101d76aa9682b6c
dorianpenaloza <dorianpenaloza@yahoo.com>
2016-06-23 19:36:58 -0500

    Initial commit 07jquery-TicTacToe

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -1,5 +1,39 @@
 'use strict';
 
 $(document).on('ready', function() {
-    // Put app logic in here
+
+    var playerTurn = 'X';
+
+    function horizontalWin() {
+     return ( ( $('[data-cell="0"]').text() === playerTurn && $('[data-cell="1"]').text() === playerTurn && $('[data-cell="2"]').text() === playerTurn) ||
+              ( $('[data-cell="3"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="5"]').text() === playerTurn) ||
+              ( $('[data-cell="6"]').text() === playerTurn && $('[data-cell="7"]').text() === playerTurn && $('[data-cell="8"]').text() === playerTurn)
+            );
+    }
+    function verticalWin() {
+      return ( ( $('[data-cell="0"]').text() === playerTurn && $('[data-cell="3"]').text() === playerTurn && $('[data-cell="6"]').text() === playerTurn) ||
+               ( $('[data-cell="1"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="7"]').text() === playerTurn) ||
+               ( $('[data-cell="2"]').text() === playerTurn && $('[data-cell="5"]').text() === playerTurn && $('[data-cell="8"]').text() === playerTurn)
+             );
+    }
+    function diagonalWin() {
+      return ( ( $('[data-cell="0"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="8"]').text() === playerTurn) ||
+               ( $('[data-cell="2"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="6"]').text() === playerTurn)
+             );
+    }
+    function checkForWin() {
+        return (horizontalWin() || verticalWin() || diagonalWin());
+    }
+
+    $('[data-cell]').click (function() {
+      if ($(this).text().length === 0) {
+            $(this).text(playerTurn);
+            if (checkForWin()) {
+              $('#announce-winner').text('Player ' + playerTurn + ' Wins!');
+            }
+        playerTurn = (playerTurn === 'X') ? 'O' : 'X';
+      }
+    });
+
+//end of document.
 });
