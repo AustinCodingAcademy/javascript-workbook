305b878aa35c16850a925695e8332c0a6c2e7641
Neville Medhora <nevmed@gmail.com>
2016-06-23 21:04:33 -0500

    TicToeee

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -1,5 +1,63 @@
 'use strict';
 
 $(document).on('ready', function() {
-    // Put app logic in here
+
+	var playerTurn = "X";
+
+	 
+	function changePlayer() {
+	  playerTurn = (playerTurn === "X") ? "O" : "X";
+	}
+
+
+	function horizontalWin() {
+	    return ( $('[data-cell="0"]').text() === $('[data-cell="1"]').text() && $('[data-cell="1"]').text() === $('[data-cell="2"]').text() ) || 
+	    ( $('[data-cell="3"]').text() === $('[data-cell="4"]').text() && $('[data-cell="4"]').text() === $('[data-cell="5"]').text() ) ||
+	    ( $('[data-cell="6"]').text() === $('[data-cell="7"]').text() && $('[data-cell="7"]').text() === $('[data-cell="8"]').text() );
+	}
+
+
+	function verticalWin() {
+	    return ( $('[data-cell="0"]').text() === $('[data-cell="3"]').text() && $('[data-cell="3"]').text() === $('[data-cell="6"]').text() ) || 
+		( $('[data-cell="1"]').text() === $('[data-cell="4"]').text() && $('[data-cell="4"]').text() === $('[data-cell="7"]').text() ) || 
+		( $('[data-cell="2"]').text() === $('[data-cell="5"]').text() && $('[data-cell="5"]').text() === $('[data-cell="8"]').text() );
+	}
+
+
+	function diagonalWin() {
+	    return ( $('[data-cell="0"]').text() === $('[data-cell="4"]').text() && $('[data-cell="0"]').text() === $('[data-cell="8"]').text() ) || 
+	    ( $('[data-cell="2"]').text() === $('[data-cell="4"]').text() && $('[data-cell="2"]').text() === $('[data-cell="6"]').text() );
+	}
+
+
+	function checkForWin() {
+	    return diagonalWin() || verticalWin()  || horizontalWin();
+	}
+
+	window.checkForWin = checkForWin;
+	window.diagonalWin = diagonalWin;
+	window.horizontalWin = horizontalWin;
+	window.verticalWin = verticalWin;
+
+
+	//set all the click functions
+	$('[data-cell]').click(function () {
+		$(this).text(playerTurn);
+		
+		changePlayer();
+
+		//checkForWin announce
+		if (checkForWin()){
+			$('#announce-winner').text('Player ' + playerTurn + 'Wins!!! OMG OMG OMG!!');
+		}
+
+
+		
+	});
+
+		
+		
+
+
+
 });
