bcf3b1812e92a089d471b5f9d7aad617aaefc269
Will Littler <williamm.littler@gmail.com>
2016-06-26 16:51:06 -0500

    gh-pages-0036
    trying my hand @ check for win... not working yet.

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -11,8 +11,7 @@ $(document).ready(function() {
       $block = lastChild.detach();
       blockSize1 = parseInt($block.data('block'));
       // console.log($block);
-    }
-    else {
+    } else {
       var targetStack = $(this).children().last();
       if ($(this).children().length > 0) {
         blockSize2 = parseInt(targetStack.data('block'));
@@ -20,11 +19,21 @@ $(document).ready(function() {
           $(this).append($block);
           $block = null;
         }
-       }
-       else {
+       } else {
          $(this).append($block);
          $block = null;
        }
     }
+    checkForWin();
   });
+
+  function checkForWin(){
+    $('[data-stack]').each(function(index, '[data-block]') {
+      if (index === 3 && (($(this).data("stack") === "2") || ($(this).data("stack") === "3")))
+      {
+        $('#announce-game-won').text('You Won!');
+      }
+    });
+  };
+
 });
