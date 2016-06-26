16e42b6cb645b30f6798e8b9fbaca0635276ef3b
Will Littler <williamm.littler@gmail.com>
2016-06-26 15:13:21 -0500

    gh-pages-0034
    towers of hanoi 2/3 done

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -2,4 +2,27 @@
 
 $(document).ready(function() {
   // Put app logic here
-}
+  var $block = null;
+  var blockSize1 = 0;
+  var blockSize2 = 0;
+  $('[data-stack]').click(function(){
+    if ($block === null) {
+      var lastChild = $(this).children().last();
+      $block = lastChild.detach();
+      blockSize1 = parseInt($block.data('block'));
+      // console.log($block);
+    } else {
+      var targetStack = $(this).children().last();
+      if ($(this).children().length > 0) {
+        blockSize2 = parseInt(targetStack.data('block'));
+        if (blockSize1 < blockSize2) {
+        $(this).append($block);
+        $block = null;
+        }
+       } else {
+         $(this).append($block);
+         $block = null;
+       }
+    }
+  });
+});
