7f5595e4d1be0e2d2700916bbd46aeba221910cf
Walker <jwalkerlyle@gmail.com>
2016-06-27 20:36:16 -0500

    list items working and append correctly

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -2,4 +2,13 @@
 
 $(document).ready(function() {
     // App logic goes here
+    $('form').submit(function (event) {
+      event.preventDefault();
+      var todoText = $(this).find('#todo').val();
+      $('#todo-list').append('<li>' + todoText + '</li>');
+      $('#todo-list').sortable();
+      $(this).find('#todo').val('')
+
+    });
+
 });
