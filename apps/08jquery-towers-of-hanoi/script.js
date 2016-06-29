c469115ed433dfb21a6bb456993802908b9497fa
Jason Burks <jason_burks@mac.com>
2016-06-28 20:26:06 -0500

    gh-pages 017
    * Wrote JQUERY Towers of Hanoi App

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -2,4 +2,34 @@
 
 $(document).ready(function() {
   // Put app logic here
-}
+  var allStacks = 'div[data-stack]';
+  var block = null;
+
+  function checkForWin(){
+    if (($(this).data('stack') > 1) && ($(this).children().length) === 4){
+      $('#announce-game-won').text('You Won!');
+    }
+  }
+
+  $(allStacks).click(function(){
+    var $children = $(this).children();
+    if (block === null){
+      if ($(this).children().length > 0) {
+        block = $(this).children().last().detach();
+      }
+    } else {
+      var $blockData = $(block).data('block');
+      var $targetBlockData = $(this).children().last().data('block');
+
+      if ($children.length === 0 || ($blockData < $targetBlockData)){
+
+        $('#announce-game-won').text('');
+        $(this).append(block);
+        checkForWin();
+        block = null;
+      } else {
+        $('#announce-game-won').text('Invalid Move. Try again...');
+      }
+    }
+  });
+});
