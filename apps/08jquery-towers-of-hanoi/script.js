d8fb7fe2bd9fd5a8519c1eaf8e413797182cd8a9
dorianpenaloza <dorianpenaloza@yahoo.com>
2016-06-30 19:41:13 -0500

    08jequery-towers-of-hanoi

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -1,5 +1,28 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
-}
+  var $block = null;
+  $('[data-stack]').click (function () {
+    var $children = $(this).children();
+    if ($block === null) {
+      if ($children.length > 0) {
+        $block = $children.last().detach();
+      }
+    }
+    else {
+      if ($children.length === 0 || $children.last().data('block') > $block.data('block')) {
+        $(this).append($block);
+        $block = null;
+      }
+    }
+    checkForWin();
+  });
+
+  function checkForWin() {
+     $('[data-stack]').each(function() {
+      if (($(this).data('stack') > 1) && ($(this).children().length === 4)) {
+            $('#announce-game-won').text('You Won!');
+      }
+    });
+  }
+});
