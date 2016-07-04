c318460850b34cdac41ed81c8aa65cc5123f8ca3
Will <williamm.littler@gmail.com>
2016-07-04 14:13:05 -0500

    gh-pages-0044
    * working on app 10...

--- apps/10address-book-ajax/script.js
+++ apps/10address-book-ajax/script.js
@@ -2,4 +2,11 @@
 
 $(document).ready(function() {
     // You code here
+    $.ajax('https://reqres-api.herokuapp.com/api/users', {
+      success: function(response) {
+        response.forEach(function(user){
+          $('div.output').text(user.first_name);
+  });
+}
+});
 });
