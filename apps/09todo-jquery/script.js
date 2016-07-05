cdb67fc274664a0f718be6e30c54979fc464cdd7
dorianpenaloza <dorianpenaloza@yahoo.com>
2016-07-05 18:20:16 -0500

    09todo0jquery complete

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -1,5 +1,15 @@
 'use strict';
 
 $(document).ready(function() {
-    // App logic goes here
+
+    $('form').submit(function (event) {
+      event.preventDefault();
+      var todoText = $(this).find('#todo').val();
+      $('#todo-list').append('<li>' + todoText + '</li>');
+    });
+
+    $('#todo-list').click (function() {
+      $(this).sortable();
+    });
+//end of document.ready.
 });
