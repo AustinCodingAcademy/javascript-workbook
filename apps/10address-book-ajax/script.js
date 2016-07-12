787c1b28a97f37dcc509656e101a7d75d5b30b49
Walker <jwalkerlyle@gmail.com>
2016-07-11 20:29:43 -0500

    address book beginning

--- apps/10address-book-ajax/script.js
+++ apps/10address-book-ajax/script.js
@@ -2,4 +2,14 @@
 
 $(document).ready(function() {
     // You code here
-});
+    $.ajax('https://reqres-api.herokuapp.com/api/users', {
+      success: function(users) {
+        users.forEach(function(user) {
+          var markUp = "<tr>";
+          markUp += "<td>" + user.id + "</td>";
+          markUp += "<td>" + user.firstName + "</td>";
+          markUp += "<td>" + user.lastName + "</td>";
+        })
+      }
+    }
+  });
