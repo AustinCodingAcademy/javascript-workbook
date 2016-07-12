2ea881722438597cc893b9938a88f493bfd10c68
Joshua Reola <josh.reola@gmail.com>
2016-07-12 18:27:36 -0500

    Update script.js
    manually just added it.

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -1,5 +1,15 @@
 'use strict';
 
 $(document).ready(function() {
-    // App logic goes here
+  // App logic goes here
+
+  $('form').submit(function (event) { // target the form, and put a submit event listener on it. We can capture and pass in the event for us to use
+    var todoText = $(this).find('input[type="text"]').val();
+    event.preventDefault(); // this tells the browser to "prevent" the "default" "event" from occuring (sending and refreshing)
+    $('#todo-list').append('<li>' + todoText + '</li>');
+
+  });
+
+  $('#todo-list').sortable();
+
 });
