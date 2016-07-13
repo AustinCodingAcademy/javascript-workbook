25b268ca1311491860b0de4265cd371a8b6d4d20
Neville Medhora <nevmed@gmail.com>
2016-07-12 20:58:07 -0500

    Thingy

--- apps/10address-book-ajax/script.js
+++ apps/10address-book-ajax/script.js
@@ -1,5 +1,27 @@
 'use strict';
 
 $(document).ready(function() {
-    // You code here
+    
+	$.ajax('https://reqres-api.herokuapp.com/api/users/', {
+		success: function(users) {
+			event.preventDefault();
+			user.forEach(function(users){ 
+  			
+				var str = '<th>' + user.id + '</th><th>' + user.first_name + '</th><th>' + user.last_name + '</th>';
+
+	  			console.log(str);
+	  			$('tbody').append(str);
+
+	  			var url = 'https://reqres-api.herokuapp.com/api/users/' + $(this).data('id', 0);
+
+	  			$.ajax(url, {
+	  				success: function(user) {
+	  					var str = '<h3>' + user.first_name + ' ' + user.last_name + '</h3>' + 
+	  					'<h4>' + user.occupation + '</h4>' + 
+	  					'<p>' + user.phone + '</p><p>' + user.address + '</p>' + '<img src="' + user.avatar + ">;
+	  				}
+	  			}
+  			}
+  		}
+	});
 });
