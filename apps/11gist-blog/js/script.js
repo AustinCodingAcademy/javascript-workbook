4cb543a34ee845855345b0a50943f90ebce04583
Will <williamm.littler@gmail.com>
2016-07-14 20:40:09 -0500

    gh-pages-0049
    * app 11 working

--- apps/11gist-blog/js/script.js
+++ apps/11gist-blog/js/script.js
@@ -2,32 +2,35 @@
 
 $(document).ready(function() {
   // You code here
-  $.ajax('http://localhost:8080/api/gists.json', {
+  // my gist https://api.github.com/users/wlittler/gists
+  // local test http://localhost:8080/api/gists.json
+  $.ajax('https://api.github.com/users/wlittler/gists', {
     success: function(response) {
       response.forEach(function(posts){
         var description = posts.description;
         if (description.indexOf('#post') > -1) {
           var str = description.slice(5);
           $('#posts').append('<li><a href="#" data-url="' + posts.url + '">' + str + '</a></li>');
-
         };
-      });
       $('a').click(function(event){
         event.preventDefault();
         console.log("clicky");
         $.ajax($(this).data('url'), {
           success: function(postData) {
             $('#post').html(marked(postData.files['post.md'].content));
-            console.log(postData.files['post.md'].content);
+            $('#blogtitle').html(postData.description.slice(5));
+            $('#postTime').html(postData.created_at);
             $.ajax(postData['comments_url'], {
-              success: function(getComments) {
-                console.log(getComments);
-                $('#comments').append(getComments['user']['login'] + ' :' + getComments.body);
+              success: function(response) {
+                  response.forEach(function(getComments){
+                    $('#comments').html(getComments['user']['login'] + ': ' + getComments.body);
+                  });
               }
             });
           }
         });
       });
-    }
-  });
+    });
+  }
+});
 });
