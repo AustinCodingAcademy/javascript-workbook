9f47ac45c2793b9bf69cd1bd7b0936c1f07665ba
Will Littler <williamm.littler@gmail.com>
2016-07-21 22:36:12 -0500

    Revert "gh-pages-0049"
    This reverts commit 4cb543a34ee845855345b0a50943f90ebce04583.

--- apps/11gist-blog/script.js
+++ apps/11gist-blog/script.js
@@ -2,35 +2,32 @@
 
 $(document).ready(function() {
   // You code here
-  // my gist https://api.github.com/users/wlittler/gists
-  // local test http://localhost:8080/api/gists.json
-  $.ajax('https://api.github.com/users/wlittler/gists', {
+  $.ajax('http://localhost:8080/api/gists.json', {
     success: function(response) {
       response.forEach(function(posts){
         var description = posts.description;
         if (description.indexOf('#post') > -1) {
           var str = description.slice(5);
           $('#posts').append('<li><a href="#" data-url="' + posts.url + '">' + str + '</a></li>');
+
         };
+      });
       $('a').click(function(event){
         event.preventDefault();
         console.log("clicky");
         $.ajax($(this).data('url'), {
           success: function(postData) {
             $('#post').html(marked(postData.files['post.md'].content));
-            $('#blogtitle').html(postData.description.slice(5));
-            $('#postTime').html(postData.created_at);
+            console.log(postData.files['post.md'].content);
             $.ajax(postData['comments_url'], {
-              success: function(response) {
-                  response.forEach(function(getComments){
-                    $('#comments').html(getComments['user']['login'] + ': ' + getComments.body);
-                  });
+              success: function(getComments) {
+                console.log(getComments);
+                $('#comments').append(getComments['user']['login'] + ' :' + getComments.body);
               }
             });
           }
         });
       });
-    });
-  }
-});
+    }
+  });
 });
