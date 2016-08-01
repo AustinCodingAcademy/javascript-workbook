1db36d9d4f31c56cb70a7c1fd4d7c8e46dfc7451
DMcCal79 <dave_mccalmont@hotmail.com>
2016-07-31 23:15:30 -0500

    Answered the Pig Latin App concept checks

--- apps/01PigLatin.js
+++ apps/01PigLatin.js
@@ -7,11 +7,80 @@ prompt.start();
 
 function pigLatin(word) {
 
-    // Your code here
+  // Concept Check
+  //
+  // Please explain why we are using String.prototype.toLowerCase() here.
+  // For more info:
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLowerCase
+  //
+  // Your Answer:
+  // This will convert the string to all lowercase letters.  This is important because the conditionals are case-sensitive.  For example, if you were trying to find the index of 'a' in the word 'Apple', then the result would be -1.  This is becuase the conditional is looking for a lowercase 'a' and the one in 'Apple' is capitalized.
+  word  = word.toLowerCase();
 
+  // Concept Check
+  //
+  // What is the significance of setting vowelIndex to -1?
+  // This might help:
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/lastIndexOf
+  //
+  // Your Answer:
+  // Setting the vowelIndex to -1 will allow us to know if there weren't any vowels in the string.  If we end up with a value of -1 after all of the conditionals have been run, then this tells us that there weren't any vowels found in the string.
+  var vowelIndex = -1; // Set it to assume there are no vowels
+
+  if ( ( word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
+      vowelIndex = word.indexOf('a');
+  }
+
+  if ( ( word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex ) || vowelIndex === -1 ) {
+      vowelIndex = word.indexOf('e');
+  }
+
+  if ( ( word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex ) || vowelIndex === -1 ) {
+      vowelIndex = word.indexOf('i');
+  }
+
+  if ( ( word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex ) || vowelIndex === -1 ) {
+      vowelIndex = word.indexOf('o');
+  }
+
+  if ( ( word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex ) || vowelIndex === -1 ) {
+      vowelIndex = word.indexOf('u');
+  }
+
+  if ( ( word.indexOf('y') > -1 && word.indexOf('y') < vowelIndex ) || vowelIndex === -1 ) {
+      vowelIndex = word.indexOf('y');
+  }
+
+  // Concept Check
+  //
+  // Why are we using String.prototype.slice() here? What role does vowelIndex play?
+  // This might help:
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/slice
+  //
+  // Your Answer:
+  // The slice method allows us to "crop" the string into smaller sections.  In the (), the first index is where the slice begins and the second index is where it ends.  The slice will include the first index and ends up until the second index.  In the firstPart vaiable, the slice begins at the begining of the word and ends right before the first vowel.  The vowelIndex is the first vowel of the word which was found using the preceeding conditional. The slice for the restWord variable begins on the first vowel (vowelIndex) and ends after the final letter of the word.
+  var firstPart = word.slice(0, vowelIndex);
+  var restWord = word.slice(vowelIndex, word.length);
+
+  if (vowelIndex === 0) {
+    return word + 'yay';
+  }
+  else {
+    return restWord + firstPart + 'ay';
+  }
 }
 
 
+// Concept Check
+//
+// Make sure you run this app with node in the console before answering this question.
+// Also, read Lesson 1 in the textbook.
+// http://intermediate.austincodingacademy.com/_book/lesson_one.html
+//
+// What does prompt.get() do for us?
+//
+// Your Answer:
+// This will prompt the user for input. In this case, it's asking the user to enter a word that they would like to be translated into Pig Latin. 
 function getPrompt() {
     prompt.get(['word'], function (error, result) {
 
