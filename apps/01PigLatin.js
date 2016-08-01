8d1f76deb03d5339e80e0f9e534863a7d6c2b529
Charlie Sears <Charlie Sears>
2016-08-01 14:40:23 -0500

    updated 01PigLatin.js added comments

--- apps/01PigLatin.js
+++ apps/01PigLatin.js
@@ -7,40 +7,82 @@ prompt.start();
 
 function pigLatin(word) {
 
-var firstLetter = word[0];
-return word.replace(firstLetter, '') + firstLetter + 'ay';
-console.log( 'crazy'.indexOf('a') );
+  // Concept Check
+  //
+  // Please explain why we are using String.prototype.toLowerCase() here.
+  // For more info:
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLowerCase
+  //
+  // Your Answer:
+  //We are using the lowercase command in order to make our output look right.
+  // If we didn't and we had a user input 'Spear' than the program would return
+  // 'earSpay'. That doesn't look good and it is kind of confusing.
+  word  = word.toLowerCase();
 
-console.log( 'crazy'.indexOf('e') );
-var word = 'crazy';
-var vowelIndex = -1; // Set it to assume there are no vowels
+  // Concept Check
+  //
+  // What is the significance of setting vowelIndex to -1?
+  // This might help:
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/lastIndexOf
+  //
+  // Your Answer:
+  //We are setting our code to assume there are no vowels.
+  var vowelIndex = -1; // Set it to assume there are no vowels
 
-if ( ( word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
-    vowelIndex = word.indexOf('a');
+  if ( ( word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
+      vowelIndex = word.indexOf('a');
+  }
+
+  if ( ( word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex ) || vowelIndex === -1 ) {
+      vowelIndex = word.indexOf('e');
+  }
+
+  if ( ( word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex ) || vowelIndex === -1 ) {
+      vowelIndex = word.indexOf('i');
+  }
+
+  if ( ( word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex ) || vowelIndex === -1 ) {
+      vowelIndex = word.indexOf('o');
+  }
+
+  if ( ( word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex ) || vowelIndex === -1 ) {
+      vowelIndex = word.indexOf('u');
+  }
+
+  if ( ( word.indexOf('y') > -1 && word.indexOf('y') < vowelIndex ) || vowelIndex === -1 ) {
+      vowelIndex = word.indexOf('y');
+  }
+
+  // Concept Check
+  //
+  // Why are we using String.prototype.slice() here? What role does vowelIndex play?
+  // This might help:
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/slice
+  //
+  // Your Answer: We are using the slice to slice the word. We need the vowelIndex
+  // to know where to slice the word.
+  var firstPart = word.slice(0, vowelIndex);
+  var restWord = word.slice(vowelIndex, word.length);
+
+  if (vowelIndex === 0) {
+    return word + 'yay';
+  }
+  else {
+    return restWord + firstPart + 'ay';
+  }
 }
 
-if ( ( word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex ) || vowelIndex === -1 ) {
-    vowelIndex = word.indexOf('e');
-}
 
-if ( ( word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex ) || vowelIndex === -1 ) {
-    vowelIndex = word.indexOf('i');
-}
-
-console.log( 'hello'.slice(0, 2) );
-var firstPart = word.slice(0, vowelIndex);
-var restWord = word.slice(vowelIndex, word.length);
-
-return restWord + firstPart + 'ay';
-
-console.log( pigLatin('egg') );
-if vowelIndex(0)
-  return word + 'yay'
-
-console.log( 'HeLlO'.toLowerCase() );
-}
-
-
+// Concept Check
+//
+// Make sure you run this app with node in the console before answering this question.
+// Also, read Lesson 1 in the textbook.
+// http://intermediate.austincodingacademy.com/_book/lesson_one.html
+//
+// What does prompt.get() do for us?
+//
+// Your Answer: It provides a prompt to the user. In this case it spits out
+//'word:__'.
 function getPrompt() {
     prompt.get(['word'], function (error, result) {
 
