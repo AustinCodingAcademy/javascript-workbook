79db853965e7d263d4281b895984912b343fb62c
Stephrod <stephanie_rodriguez5@yahoo.com>
2016-08-01 19:36:54 -0500

    Lesson two homework

--- homework/02LessonTwo.js
+++ homework/02LessonTwo.js
@@ -29,7 +29,11 @@ function equals(argument1, argument2) {
   if (argument1 === argument2) {
     return true;
   }
+  else {
+    return false;
+  }
 }
+
 
 // Problem 2:
 // lessThanOrEqualTo(parameter1, parameter2) is an empty function
@@ -37,6 +41,12 @@ function equals(argument1, argument2) {
 // parameter1 is less than or equal to parameter2
 
 function lessThanOrEqualTo(parameter1, parameter2) {
+  if (parameter1 <= parameter2 ) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 // Problem 3:
@@ -46,9 +56,7 @@ function lessThanOrEqualTo(parameter1, parameter2) {
 function add(number1, number2) {
   return number1 + number2;
 }
- var x = add (2 + 4);
 
- x;
 // Problem 4:
 // write a function named addThree(number1, number2, number3)
 // this function will add three numbers
@@ -57,9 +65,6 @@ function add(number1, number2) {
 function addThree (number1, number2, number3) {
   return number1 + number2 + number3;
 }
-var x = addThree;
-
-x;
 
 // ****
 // Concept Checkpoint
@@ -99,14 +104,14 @@ x;
 // complete isEven() by returning a boolean expression
 
 function isEven(number) {
-  if (isEven % 2 === 0) {
+  if (number % 2 === 0) {
     return true;
-  } else {
+  }
+  else {
     return false;
   }
 }
 
-isEven(2);
 
 // Problem 6:
 // isDivisibleByThree(number) is a function that
@@ -115,6 +120,12 @@ isEven(2);
 // complete isDivisibleByThree() by returning a boolean expression
 
 function isDivisibleByThree(number) {
+  if (number % 3 === 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 // ****
@@ -130,6 +141,18 @@ function isDivisibleByThree(number) {
 // should return false if character is anything else
 
 function whichSpecies(character) {
+  if (character === 'scooby') {
+    return 'dog';
+  }
+  else if (character === 'garfield') {
+    return 'cat';
+  }
+  else if (character === 'nemo') {
+    return 'fish';
+  }
+  else {
+    return false;
+  }
 }
 
 // Problem 8:
@@ -140,6 +163,20 @@ function whichSpecies(character) {
 // return the string "divisible by 3" when number % 3 === 0
 // return the string "divisible by 5" when number % 5 === 0
 
+function testNumber(number) {
+  if (number % 4 === 0) {
+    return ('divisible by 4');
+  }
+  else if (number % 2 === 0) {
+    return ('divisible by 2');
+  }
+  else if (number % 3 === 0) {
+    return ('divisible by 3');
+  }
+  else if (number % 5 === 0) {
+    return ('divisible by 5');
+  }
+}
 
 // ****
 // Concept Checkpoint
