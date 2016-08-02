6ccb2565024d552f219285331e06c3426037be17
Salvador <salvadorrod13@gmail.com>
2016-08-01 19:06:25 -0500

    rockPaperScissors assigment completed

--- apps/02RockPaperScissors.js
+++ apps/02RockPaperScissors.js
@@ -7,7 +7,39 @@ prompt.start();
 
 function rockPaperScissors(hand1, hand2) {
 
-    // Write code here
+    // // Write code here
+    // ONE OPTIONS
+    if (hand1 === hand2){
+      return "It's a tie!";
+    }
+
+    if ( (hand1 === 'rock' &&  hand2 === 'scissors') ||
+       (hand1 === 'paper' &&  hand2 === 'rock') ||
+       (hand1 === 'scissors' &&  hand2 === 'paper') )
+       {
+      return "Hand one wins!";
+    } else {
+      return "Hand two wins!";
+    }
+
+
+        // if (hand1 === 'rock'){
+        //   if (hand2 === 'scissors'){
+        //     return "Hand one wins!";
+        //   }
+        // }
+        // if (hand1 === 'paper'){
+        //   if (hand2 === 'rock'){
+        //     return "Hand one wins!";
+        //   }
+        // }
+        // if (hand1 === 'scissors'){
+        //   if (hand2 === 'paper'){
+        //     return "Hand one wins!";
+        //   }
+        // }
+        //
+        // return "Hand two wins!";
 
 }
 
