5612bd1b85f3a8b70b4d02b8e04cd926a1f3554a
hcgrace05 <hcgrace.05@gmail.com>
2016-08-01 21:00:36 -0500

    Another way to do Rock, Paper, Scissors where it tells you if your word is wrong

---
+++ apps/02RockPaperScissors0.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var assert = require('assert');
+var prompt = require('prompt');
+// this is what starts the app
+prompt.start();
+
+// this is where the code test one hand verse another hand.
+function rockPaperScissors(hand1, hand2) {
+     hand1 = hand1.toLowerCase();
+     hand2 = hand2.toLowerCase();
+
+    if (hand1===hand2) {
+      return "It's a tie!";
+    };
+
+    if ((hand1 === 'rock') && (hand2 === 'scissors')){
+      return "Hand one wins!";
+    }
+    else if ((hand1 === 'paper') && (hand2 ==='rock')){
+      return 'Hand one wins!';
+    }
+    else if ((hand1 === 'scissors')&& (hand2 ==='paper')){
+      return 'Hand one wins!';
+    }
+    else if ((hand1 === 'rock')&& (hand2 ==='paper')){
+      return 'hand two wins!';
+    }
+    else if ((hand1 === 'paper') && (hand2 ==='scissors')){
+      return 'Hand two wins!';
+    }
+    else if ((hand1 === 'scissors')&& (hand2==='rock')){
+      return 'Hand two wins!';
+    }
+    else {
+      return 'please enter rock, paper, or scissors';
+    };
+};
+
+function getPrompt() {
+    prompt.get(['hand1', 'hand2'], function (error, result) {
+
+        console.log( rockPaperScissors(result['hand1'], result['hand2']) );
+
+        getPrompt();
+    });
+}
+
+// Tests
+
+if (typeof describe !== 'undefined') {
+
+    describe('#rockPaperScissors()', function () {
+        it('should detect a tie', function () {
+            assert.equal(rockPaperScissors('rock', 'rock'), "It's a tie!");
+            assert.equal(rockPaperScissors('paper', 'paper'), "It's a tie!");
+            assert.equal(rockPaperScissors('scissors', 'scissors'), "It's a tie!");
+        });
+        it('should detect which hand won', function () {
+            assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
+            assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
+            assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
+        });
+    });
+} else {
+
+    getPrompt();
+
+}
