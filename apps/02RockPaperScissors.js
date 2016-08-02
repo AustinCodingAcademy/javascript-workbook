8fadbedf70944095cf7909f7729c45b0a89b44ae
Matthew Holman <mateoholman@gmail.com>
2016-08-01 20:58:05 -0500

    Finished v1.0 of the RockPaperScissors app

--- apps/02RockPaperScissors.js
+++ apps/02RockPaperScissors.js
@@ -7,10 +7,55 @@ prompt.start();
 
 function rockPaperScissors(hand1, hand2) {
 
-    // Write code here
+    // Spec 3: Input scrubbing
+    // We're going to change the inputs to lower case.
+    hand1.toLowerCase();
+    hand2.toLowerCase();
+
+    // Spec 1: Is it a tie?
+    if (hand1 === hand2) {
+      return "It's a tie!";
+    }
+    // Now that we know it's not a tie:
+    // Spec 2: Who is the winner?
+    // First we'll determine the winner if hand1 is rock.
+    else  if (hand1 === 'rock'){
+      if (hand2 === 'scissors') {
+        return "Hand one wins!";
+      }
+      // We already know the other hand isn't rock, otherwise it would be
+      // a tie. Therefore, the other hand must be paper.
+      else {
+        return "Hand two wins!";
+      }
+    }
+    // Next, we'll determine the winner if hand1 is paper.
+    else if(hand1 === 'paper') {
+      if (hand2 === 'rock') {
+        return "Hand one wins!";
+      }
+      else {
+        return "Hand two wins!";
+      }
+    }
+    // Next, we'll determine the winner if hand1 is scissors.
+    else if (hand1 === 'scissors'){
+      if (hand2 === 'paper') {
+        return "Hand one wins!";
+      }
+      else {
+        return "Hand two wins!";
+      }
+    }
+    // Finally, if we get to this point, the user must have input something
+    // invalid. We'll let them know.
+    else {
+      return "Hey user. You suck. Your inputs need to be rock, paper or scissors!";
+    }
 
 }
 
+//We need user input for hand1 & hand2
 function getPrompt() {
     prompt.get(['hand1', 'hand2'], function (error, result) {
 
