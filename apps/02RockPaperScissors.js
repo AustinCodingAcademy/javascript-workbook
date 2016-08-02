48de4fdd857a387f84ef0fc88a97620bda22f2c3
Sandra Killebrew <sandra@nextconsulting-llc.com>
2016-08-02 11:05:14 -0500

    completed app 02 RPS

--- apps/02RockPaperScissors.js
+++ apps/02RockPaperScissors.js
@@ -5,11 +5,36 @@ var prompt = require('prompt');
 prompt.start();
 
 
-function rockPaperScissors(hand1, hand2) {
+function rockPaperScissors (hand1, hand2) {
 
-    // Write code here
+  //check for a tie
+    if (hand1 === hand2) {
+      return "It's a tie!";
+    }
 
-}
+    if (hand1 === 'paper') {
+      if (hand2 === 'rock') {
+        return 'Hand one wins!';
+      } else if (hand2 === 'scissors') {
+        return 'Hand two wins!';
+      }
+    }
+    if (hand1 === 'rock') {
+      if (hand2 === 'paper') {
+        return 'Hand two wins!';
+      } else if (hand2 === 'scissors') {
+        return 'Hand one wins!';
+      }
+    }
+    if (hand1 === 'scissors') {
+      if (hand2 === 'paper') {
+        return 'Hand one wins!';
+      } else if (hand2 === 'rock') {
+        return 'Hand two wins!';
+      }
+    }
+  }
+
 
 function getPrompt() {
     prompt.get(['hand1', 'hand2'], function (error, result) {
