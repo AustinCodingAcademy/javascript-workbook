844225e3ae3a145c5e85bc148361f8924a9ad4cf
Harry Wada <harrywada@gmail.com>
2016-08-03 15:52:42 -0500

    finished lesson 3 homework

--- homework/03LessonThree.js
+++ homework/03LessonThree.js
@@ -20,16 +20,15 @@ var firstName = 'susan';
 
 // Problem 1:
 // What is the value of firstName at this point in the file?
-var valueOfFirstName;
+var valueOfFirstName = "susan";
 
 // Problem 2:
 // What is the value of lastName at this point in the file?
-var valueOfLastName;
+var valueOfLastName = "plaid";
 
 // Problem 3:
 // Fix the following function so that it returns the first argument
 function returnTheFirstArgument(firstArgument) {
-  var firstArgument = 'bob';
   return firstArgument;
 }
 
@@ -41,7 +40,7 @@ function returnTheFirstArgument(firstArgument) {
 // What is scope?
 //
 // Your Answer Goes Here:
-//
+// Scope is the context in which a variable or function can be used. For instance, a variable created in a function can only be used in that function, its scope does not include anything beyond that function
 //
 
 
@@ -57,19 +56,19 @@ var ternaryResult4 = ""     ? 'first' : 'second';
 
 // Problem 4:
 // What is the value of ternaryResult1
-var problemFourAnswer;
+var problemFourAnswer = "first";
 
 // Problem 5:
 // What is the value of ternaryResult2
-var problemFiveAnswer;
+var problemFiveAnswer = "second";
 
 // Problem 6:
 // What is the value of ternaryResult3
-var problemSixAnswer;
+var problemSixAnswer = "first";
 
 // Problem 7:
 // What is the value of ternaryResult4
-var problemSevenAnswer;
+var problemSevenAnswer = "second";
 
 // ****
 // Concept Checkpoint
@@ -79,7 +78,7 @@ var problemSevenAnswer;
 // What is the ternary operator?  How does it differ from normal conditional statements?
 //
 // Your Answer Goes Here:
-//
+// The ternary operator allows you to evaluate a boolean statement and provide two values, one to be returned if the statement is true, and one to be returned if the statement is false. This is also much easier than using "if" statements because it only takes one line.
 //
 
 
@@ -89,17 +88,19 @@ var problemSevenAnswer;
 
 // Problem 8:
 // Create an empty array
-var emptyArray;
+var emptyArray = [];
 
 // Problem 9:
 // Create an array with 5 elements in it
-var lengthFiveArray;
+var lengthFiveArray = [1, 2, 3, 4, 5];
 
 // DO NOT MODIFY
 var nameArray = ['bob', 'fred', 'susan'];
 
 // Problem 10:
 // Replace the value 'fred' in nameArray with 'george'
+var index = nameArray.indexOf("fred");
+nameArray[index] = "george";
 
 // DO NOT MODIFY
 var threeByThreeArray = [
@@ -110,10 +111,14 @@ var threeByThreeArray = [
 
 // Problem 11:
 // Replace the center element of threeByThreeArray, which is 5, with something else.
+threeByThreeArray[1][1] = 6;
 
 // Problem 12:
 // create a 2 by 2 (2 rows, 2 columns) nested array
-var twoByTwoArray;
+var twoByTwoArray = [
+  [1, 2],
+  [3, 4]
+];
 
 // ****
 // Tests
