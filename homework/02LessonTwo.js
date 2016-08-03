5fe5c3e37e92cd11d5eecf8e2ac529bd6f8ba0bf
Charlie Sears <Charlie Sears>
2016-08-03 14:10:25 -0500

    I know that the whichSpecies code is broken. I cannot figure it out.

--- homework/02LessonTwo.js
+++ homework/02LessonTwo.js
@@ -26,6 +26,16 @@ var alwaysTrue = function () {
 // argument1 is equal to argument2
 
 function equals(argument1, argument2) {
+  if (argument1 === argument2)
+  {
+    return true;
+}
+  else
+  {
+
+      return false;
+  }
+
 }
 
 // Problem 2:
@@ -34,16 +44,33 @@ function equals(argument1, argument2) {
 // parameter1 is less than or equal to parameter2
 
 function lessThanOrEqualTo(parameter1, parameter2) {
+  if (parameter1 <= parameter2) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 // Problem 3:
 // write a function named add(number1, number2)
 // add will add two numbers and return the result
 
+function add(number1, number2) {
+  return number1 + number2;
+  }
+
 // Problem 4:
 // write a function named addThree(number1, number2, number3)
 // this function will add three numbers
 // you must call your function add() in addThree()
+
+function addThree(number1, number2, number3)
+  {
+    var firstAdd = add(number1, number2);
+
+    return firstAdd + number3;
+  }
 
 
 // ****
@@ -54,22 +81,29 @@ function lessThanOrEqualTo(parameter1, parameter2) {
 // What is a function? How do you define a function in Javascript?
 //
 // Your Answer Goes Here:
-//
+//A function is code that takes one or more arguments and returns a new value
+//based on what the function is set up to do. The way you define a function is
+//by stating 'function_space_nameoffunction(argument1, argument2)'
+//Following this code you can add if statements and conditionals.
 //
 // What is a return value?
 //
 // Your Answer Goes Here:
-//
+//A return value is what the function outputs at the end of a function.
 //
 // How do you define a named function?
 //
-// Your Answer Goes Here:
+// Your Answer Goes Here:  A named function is one action you want your code to
+// do. You name the function whatever you like and then you name two parameters and
+// tell the fuction what to do with those parameters.
 //
 //
 // What is a parameter? What is an argument? Is there a difference between the two?
 //
 // Your Answer Goes Here:
-//
+//A parameter is a data point or possibly user input that your function will
+// change in some way.  An argument is the actual value that gets passed to the
+// parameter in the function.
 // ****
 
 
@@ -84,6 +118,13 @@ function lessThanOrEqualTo(parameter1, parameter2) {
 // complete isEven() by returning a boolean expression
 
 function isEven(number) {
+  if (number % 2 === 0)
+
+    {return true;}
+
+  else {
+    return false;
+  }
 }
 
 // Problem 6:
@@ -93,6 +134,14 @@ function isEven(number) {
 // complete isDivisibleByThree() by returning a boolean expression
 
 function isDivisibleByThree(number) {
+  if (number % 3 === 0)
+
+    {return true;}
+
+  else {
+    return false;
+  }
+
 }
 
 // ****
@@ -108,6 +157,31 @@ function isDivisibleByThree(number) {
 // should return false if character is anything else
 
 function whichSpecies(character) {
+
+  if ( character = 'scooby')
+  {
+
+    return 'dog';
+  }
+
+  if ( character = 'garfield')
+  {
+
+    return 'cat';
+  }
+
+  if ( character = 'nemo')
+  {
+
+    return 'fish';
+  }
+
+  else (character)
+
+    return false;
+
+
+
 }
 
 // Problem 8:
@@ -118,6 +192,27 @@ function whichSpecies(character) {
 // return the string "divisible by 3" when number % 3 === 0
 // return the string "divisible by 5" when number % 5 === 0
 
+function testNumber(number)  {
+
+  if ( number % 4 === 0) {
+
+    return "divisible by 4";
+  }
+  if ( number % 2 === 0) {
+
+    return "divisible by 2";
+  }
+  if ( number % 3 === 0) {
+
+    return "divisible by 3";
+  }
+  if ( number % 5 === 0) {
+
+    return "divisible by 5";
+  }
+
+
+}
 
 // ****
 // Concept Checkpoint
@@ -126,9 +221,9 @@ function whichSpecies(character) {
 //
 // In your own words, explain what conditionals do.
 //
-// Your Answer Goes Here:
-//
-//
+// Your Answer Goes Here: They turn raw user data into what the programmer
+//thinks the user is wanting to do based on the program that the programmer
+//has set up.
 // ****
 
 // ****
