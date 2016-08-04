88ffeb52149fcf541047698dd4f2396e05a75605
Harry Wada <harrywada@gmail.com>
2016-08-03 19:01:41 -0500

    finished towers of hanoi app

--- apps/04TowersOfHanoi.js
+++ apps/04TowersOfHanoi.js
@@ -16,24 +16,38 @@ function printStacks() {
     console.log("c: " + stacks.c);
 }
 
-function movePiece() {
-    // Your code here
-
+function movePiece(startStack, endStack) {
+  // Your code here
+  if(isLegal(startStack, endStack)){
+    stacks[endStack].push(stacks[startStack].pop());
+  }
 }
 
-function isLegal() {
-    // Your code here
-
+function isLegal(startStack, endStack) {
+  // Your code here
+  if(stacks[startStack].length === 0){
+    return false;
+  }
+  if(stacks[endStack].length === 0){
+    return true;
+  }
+  if(stacks[startStack][stacks[startStack].length - 1] > stacks[endStack][stacks[endStack].length - 1]){
+    return false;
+  }
+  return true;
 }
 
 function checkForWin() {
-    // Your code here
-
+  // Your code here
+  if(stacks.b.length === 4 || stacks.c.length === 4){
+    return true;
+  }
+  return false;
 }
 
 function towersOfHanoi(startStack, endStack) {
-    // Your code here
-
+  // Your code here
+  movePiece(startStack, endStack);
 }
 
 function getPrompt() {
