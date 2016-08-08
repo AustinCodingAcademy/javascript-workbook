0a077af45dedd04214c795e8098e865ec57a3cb9
Charlie Sears <Charlie Sears>
2016-08-07 23:54:17 -0500

    Finished homework 3!

--- homework/03LessonThree.js
+++ homework/03LessonThree.js
@@ -20,16 +20,17 @@ var firstName = 'susan';
 
 // Problem 1:
 // What is the value of firstName at this point in the file?
-var valueOfFirstName;
+var valueOfFirstName = 'susan';
 
 // Problem 2:
 // What is the value of lastName at this point in the file?
-var valueOfLastName;
+var valueOfLastName = 'plaid';
 
 // Problem 3:
 // Fix the following function so that it returns the first argument
+var firstArgument = 'bob';
 function returnTheFirstArgument(firstArgument) {
-  var firstArgument = 'bob';
+
   return firstArgument;
 }
 
@@ -41,7 +42,9 @@ function returnTheFirstArgument(firstArgument) {
 // What is scope?
 //
 // Your Answer Goes Here:
-//
+//It is the area of the code that contains the arguments and variables a person
+// is currently manipulating. It is a bucket of code that cannot reference
+// the contents of other buckets.
 //
 
 
@@ -57,19 +60,19 @@ var ternaryResult4 = ""     ? 'first' : 'second';
 
 // Problem 4:
 // What is the value of ternaryResult1
-var problemFourAnswer;
+var problemFourAnswer = true ? 'first' : 'second';
 
 // Problem 5:
 // What is the value of ternaryResult2
-var problemFiveAnswer;
+var problemFiveAnswer = false ? 'first' : 'second';
 
 // Problem 6:
 // What is the value of ternaryResult3
-var problemSixAnswer;
+var problemSixAnswer = 4      ? 'first' : 'second';
 
 // Problem 7:
 // What is the value of ternaryResult4
-var problemSevenAnswer;
+var problemSevenAnswer = ""     ? 'first' : 'second';
 
 // ****
 // Concept Checkpoint
@@ -80,7 +83,10 @@ var problemSevenAnswer;
 //
 // Your Answer Goes Here:
 //
-//
+//The ternary operator (?) is like a toggle. It outputs one result and then
+// another, back and forth. It differs from normal conditional statements because
+// it will always give one or the other answer, independent of any math reasoning
+// or other logic.
 
 
 // ****
@@ -89,18 +95,18 @@ var problemSevenAnswer;
 
 // Problem 8:
 // Create an empty array
-var emptyArray;
+var emptyArray = [ ];
 
 // Problem 9:
 // Create an array with 5 elements in it
-var lengthFiveArray;
+var lengthFiveArray = [ "Yoda", "Luke", "Leia", "Darth Vader", "Obi-Wan"];
 
 // DO NOT MODIFY
 var nameArray = ['bob', 'fred', 'susan'];
 
 // Problem 10:
 // Replace the value 'fred' in nameArray with 'george'
-
+var nameArray = ['bob', 'george', 'susan'];
 // DO NOT MODIFY
 var threeByThreeArray = [
   [1, 2, 3],
@@ -110,10 +116,20 @@ var threeByThreeArray = [
 
 // Problem 11:
 // Replace the center element of threeByThreeArray, which is 5, with something else.
-
+var threeByThreeArray = [
+  [1, 2, 3],
+  [4, "Star Warts", 6],
+  [7, 8, 9]
+];
 // Problem 12:
 // create a 2 by 2 (2 rows, 2 columns) nested array
-var twoByTwoArray;
+var twoByTwoArray = [
+
+[[3, 5],[5,6]],
+[[7, 84], [6,7]]
+
+
+];
 
 // ****
 // Tests
