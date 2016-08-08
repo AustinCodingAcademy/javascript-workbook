d7bd3b4a4346eb312b960f96f55c145fa5140d4c
hcgrace05 <hcgrace.05@gmail.com>
2016-08-08 16:21:39 -0500

    don't understand #3 100%

--- homework/03LessonThree.js
+++ homework/03LessonThree.js
@@ -20,11 +20,11 @@ var firstName = 'susan';
 
 // Problem 1:
 // What is the value of firstName at this point in the file?
-var valueOfFirstName;
+var valueOfFirstName ='susan';
 
 // Problem 2:
 // What is the value of lastName at this point in the file?
-var valueOfLastName;
+var valueOfLastName='plaid';
 
 // Problem 3:
 // Fix the following function so that it returns the first argument
@@ -32,7 +32,6 @@ function returnTheFirstArgument(firstArgument) {
   var firstArgument = 'bob';
   return firstArgument;
 }
-
 // ****
 // Concept Checkpoint
 //
@@ -41,7 +40,7 @@ function returnTheFirstArgument(firstArgument) {
 // What is scope?
 //
 // Your Answer Goes Here:
-//
+//Scope is the area within which the variable is active.
 //
 
 
@@ -57,19 +56,19 @@ var ternaryResult4 = ""     ? 'first' : 'second';
 
 // Problem 4:
 // What is the value of ternaryResult1
-var problemFourAnswer;
+var problemFourAnswer='first'
 
 // Problem 5:
 // What is the value of ternaryResult2
-var problemFiveAnswer;
+var problemFiveAnswer='second'
 
 // Problem 6:
 // What is the value of ternaryResult3
-var problemSixAnswer;
+var problemSixAnswer='first'
 
 // Problem 7:
 // What is the value of ternaryResult4
-var problemSevenAnswer;
+var problemSevenAnswer='second'
 
 // ****
 // Concept Checkpoint
@@ -79,7 +78,7 @@ var problemSevenAnswer;
 // What is the ternary operator?  How does it differ from normal conditional statements?
 //
 // Your Answer Goes Here:
-//
+//Ternary operator is similar to an if statement but does it in a shorter way by using a condition, which is something that expresses to true or false, and expr1, expr2, which are values of any type.
 //
 
 
@@ -89,17 +88,18 @@ var problemSevenAnswer;
 
 // Problem 8:
 // Create an empty array
-var emptyArray;
+var emptyArray=[]
 
 // Problem 9:
 // Create an array with 5 elements in it
-var lengthFiveArray;
+var lengthFiveArray=[1, 2, 3, 4, 5]
 
 // DO NOT MODIFY
 var nameArray = ['bob', 'fred', 'susan'];
 
 // Problem 10:
 // Replace the value 'fred' in nameArray with 'george'
+nameArray[1]= 'george';
 
 // DO NOT MODIFY
 var threeByThreeArray = [
@@ -110,10 +110,14 @@ var threeByThreeArray = [
 
 // Problem 11:
 // Replace the center element of threeByThreeArray, which is 5, with something else.
+threeByThreeArray[1][1]='5';
 
 // Problem 12:
 // create a 2 by 2 (2 rows, 2 columns) nested array
-var twoByTwoArray;
+var twoByTwoArray = [
+  [1 ,2],
+  [1,2]
+]
 
 // ****
 // Tests
