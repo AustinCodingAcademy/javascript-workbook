b04854c8172b52b90398e208d16c8be4ad7ea6aa
hcgrace05 <hcgrace.05@gmail.com>
2016-08-08 19:36:24 -0500

    final Commit found a typo

--- homework/03LessonThree.js
+++ homework/03LessonThree.js
@@ -109,13 +109,13 @@ var threeByThreeArray = [
 
 // Problem 11:
 // Replace the center element of threeByThreeArray, which is 5, with something else.
-threeByThreeArray[1][1]='5';
+threeByThreeArray[1][1]='grace';
 
 // Problem 12:
 // create a 2 by 2 (2 rows, 2 columns) nested array
 var twoByTwoArray = [
-  [1 ,2],
-  [1,2]
+  [1, 2],
+  [1, 2]
 ]
 
 // ****
