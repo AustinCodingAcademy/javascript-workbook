2bd2ad6769c6005fc12470613e5c83021d1684df
zacacollier <zacacollier@gmail.com>
2016-08-09 14:07:14 -0500

    Basic TicTacToe complete. Added some basic input scrubbing.

--- apps/03TicTacToe.js
+++ apps/03TicTacToe.js
@@ -1,9 +1,11 @@
 'use strict';
 
+
 var assert = require('assert');
 var prompt = require('prompt');
 prompt.start();
 
+var valid = 0 || 1 || 2;
 var board = [
     [' ', ' ', ' '],
     [' ', ' ', ' '],
@@ -22,31 +24,96 @@ function printBoard() {
 }
 
 function horizontalWin() {
-    // Your code here
+  // how to make the array selection slimmer?
+  // this format simply spits out a winning result into the top level
+  return  playerTurn === board[0][0] &&
+          playerTurn === board[0][1] &&
+          playerTurn === board[0][2] ||
+          playerTurn === board[1][0] &&
+          playerTurn === board[1][1] &&
+          playerTurn === board[1][2] ||
+              playerTurn === board[2][0] &&
+              playerTurn === board[2][1] &&
+              playerTurn === board[2][2];
+
+
+
 }
 
 function verticalWin() {
-    // Your code here
-}
+  return playerTurn === board[0][0] &&
+      playerTurn === board[1][0] &&
+      playerTurn === board[2][0] ||
+          playerTurn === board[0][1] &&
+          playerTurn === board[1][1] &&
+          playerTurn === board[2][1] ||
+              playerTurn === board[0][2] &&
+              playerTurn === board[1][2] &&
+              playerTurn === board[2][2];
+
+    }
 
 function diagonalWin() {
-    // Your code here
+  if (playerTurn === board[0][0] &&
+      playerTurn === board[1][1] &&
+      playerTurn === board[2][2] ||
+        playerTurn === board[0][2] &&
+        playerTurn === board[1][1] &&
+        playerTurn === board[2][0] )
+      {
+        return true;
+      }
+
+    }
+
+function ticTacToe(row, column) {
+
+  board[row][column] = playerTurn;
+  checkForWin();
+  playerTurn = (playerTurn === 'X') ? 'O' : 'X';
 }
 
 function checkForWin() {
-    // Your code here
+  if (
+    horizontalWin() || verticalWin() || diagonalWin()
+  )
+  {console.log(playerTurn + " " + "is the winner!");}
+  return true;
+  // playAgain();
 }
 
-function ticTacToe(row, column) {
-    // Your code here
-}
+// function playAgain() {
+//   console.log("Would you like to play again?");
+//   prompt.get(['y', 'n']), function (error, result)  {
+//   (result['y', 'n'] === 'y') ? getPrompt() : console.log("thanks for playing!");
+// }
+// }
 
 function getPrompt() {
     printBoard();
     console.log("It's Player " + playerTurn + "'s turn.");
     prompt.get(['row', 'column'], function (error, result) {
-        ticTacToe(result['row'], result['column']);
-        getPrompt();
+        // ticTacToe(result['row'], result['column']);
+        // getPrompt();
+
+        if ((result['row', 'column'] > 2) || (result['row', 'column'].length > 1)) {
+          console.log("Please enter a valid input");
+          getPrompt();
+        }
+        else {
+          ticTacToe(result['row'], result['column']);
+          getPrompt();
+        }
+
+        // if ((result['row'] !== valid) || (result['column'] !== valid))
+        //   {
+        //     console.log("Please enter a valid input:");
+        //     getPrompt();
+        //   }
+        // else {
+        //   ticTacToe(result['row'], result['column']);
+        //   // getPrompt();
+        // }
     });
 }
 
