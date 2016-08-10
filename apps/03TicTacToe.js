81009e355ce53f73ac4627620b14df421b5e90b4
Stephrod <stephanie_rodriguez5@yahoo.com>
2016-08-09 20:42:15 -0500

    TTT code

--- apps/03TicTacToe.js
+++ apps/03TicTacToe.js
@@ -23,24 +23,65 @@ function printBoard() {
 
 function horizontalWin() {
     // Your code here
-}
+    if (board[0][0] === playerTurn && board[0][1] === playerTurn && board[0][2] === playerTurn) {
+      return true;
+    }
+    if (board[1][0] === playerTurn && board[1][1] === playerTurn && board[1][2] === playerTurn) {
+      return true;
+    }
+    if (board[2][0] === playerTurn && board[2][1] === playerTurn && board[2][2] === playerTurn) {
+      return true;
+    }
+    else {
+      return false;
+    }
+};
 
 function verticalWin() {
     // Your code here
-}
+    if (board[0][0] === playerTurn && board[1][0] === playerTurn && board[2][0] === playerTurn) {
+      return true;
+    }
+    if (board[0][1] === playerTurn && board[1][1] === playerTurn && board[2][1] === playerTurn) {
+      return true;
+    }
+    if (board[0][2] === playerTurn && board[1][2] === playerTurn && board[2][2] === playerTurn) {
+      return true;
+    }
+    else {
+      return false;
+    }
+};
 
 function diagonalWin() {
     // Your code here
-}
+    if (board[0][0] === playerTurn && board[1][1] === playerTurn && board[2][2] === playerTurn) {
+      return true;
+    }
+    if (board[0][2] === playerTurn && board[1][1] === playerTurn && board[2][0] === playerTurn) {
+      return true;
+    }
+    else {
+      return false;
+    }
+};
 
 function checkForWin() {
     // Your code here
-}
+    if(horizontalWin || virticalWin || diagonalWin ){
+      console.log('Player ' + playerTurn + ' Won!');
+    }
+    else {
+      return false;
+    }
+};
 
 function ticTacToe(row, column) {
     // Your code here
-    board[row][column] === 'playerTurn';
-}
+    board[row][column] = playerTurn;
+    playerTurn = (playerTurn === 'X') ? 'O' : 'X';
+};
+
 
 function getPrompt() {
     printBoard();
