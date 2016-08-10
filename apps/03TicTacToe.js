82a9f1a6673ddd469bdcc7cb0931c98aad4b8618
Judah M. Ritterman <judah29@gmail.com>
2016-08-10 16:39:32 -0500

    scope question regarding tic-tac-toe in comments at top of page

--- apps/03TicTacToe.js
+++ apps/03TicTacToe.js
@@ -1,8 +1,12 @@
-//cleanup code
-//re-write program on whiteboard
-//re-write program in code
-//flesh out program in html/css
-//colt solution
+//question
+  //scope
+    //how does printBoard() have access to board variable inside of resetBoard()
+      //would think printBoard() would only have access to global 'var board'
+        //such that it would not work to call resetBoard()
+          //and then call printBoard()
+            //and expect printBoard()
+              //to print a fresh board
+                //rather than an old board that has been played on already
 
 'use strict';
 
@@ -10,8 +14,8 @@ var assert = require('assert');
 var prompt = require('prompt');
 prompt.start();
 
-var validValues = [0,1,2];
 var playerTurn = 'X';
+var validValues = [0,1,2];
 var counter = 0;
 
 var board = [
@@ -40,10 +44,38 @@ function printBoard() {
   console.log('2 ' + board[2].join(' | '));
 }
 
-function ticTacToe(row, column) {
+function getPrompt() {
+  console.log("===============================");
+  console.log("It's Player " + playerTurn + "'s turn.");
+  console.log();
+  printBoard();
+  prompt.get(['row', 'column'], function(error, result) {
+    var row = parseInt(result['row']);
+    var column = parseInt(result['column']);
+    //check for validity of row and column player move
+    if(validVal(row) && validVal(column) ){
+      ticTacToe(row, column);
+      getPrompt();
+    }
+    else{
+      console.log('invalid entry, try again');
+      getPrompt();
+    }
 
+    //prompt.get closes
+  });
+  //getPrompt() closes
+}
+
+function validVal(row_or_column){
+  return (validValues.indexOf(row_or_column) > -1 && validValues.indexOf(row_or_column) > -1)
+}
+
+function ticTacToe(row, column) {
   //as long as there is space available
     //let's play the game
+      //could also write
+        //if board[row][col] === " ";
   if (board[row][column] !== "X" && board[row][column] !== "O") {
     board[row][column] = playerTurn;
     counter++;
@@ -89,32 +121,6 @@ function ticTacToe(row, column) {
 
 }
 
-function getPrompt() {
-  console.log("===============================");
-  console.log("It's Player " + playerTurn + "'s turn.");
-  console.log();
-  printBoard();
-  prompt.get(['row', 'column'], function(error, result) {
-    var row = parseInt(result['row']);
-    var column = parseInt(result['column']);
-    //check for validity of row and column player move
-    if(validVal(row) && validVal(column) ){
-        ticTacToe(row, column);
-        getPrompt();
-    }
-    else{
-      console.log('invalid entry, try again');
-      getPrompt();
-    }
-
-  });
-  //prompt.get closes
-}
-//getPrompt() closes
-
-function validVal(row_or_column){
-  return (validValues.indexOf(row_or_column) > -1 && validValues.indexOf(row_or_column) > -1)
-}
 
 
 function checkForWin() {
