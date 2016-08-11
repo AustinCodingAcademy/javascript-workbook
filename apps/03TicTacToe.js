a7be56a8961a0c2cf1c400bb2f151335b406ddd2
Giovanna <g.c1@me.com>
2016-08-10 19:03:10 -0500

    It works

--- apps/03TicTacToe.js
+++ apps/03TicTacToe.js
@@ -22,24 +22,73 @@ function printBoard() {
 }
 
 function horizontalWin() {
-    // Your code here
-}
+  if (board[0][0] === playerTurn && board[0][1] === playerTurn && board[0][2] === playerTurn) {
+    return true;
+  }
+  if (board[1][0] === playerTurn && board[1][1] === playerTurn && board[1][2] === playerTurn) {
+    return true;
+  }
+  if (board[2][0] === playerTurn && board[2][1] === playerTurn && board[2][2] === playerTurn) {
+  return true;
+  }
+  else {
+      return false;
+  }
+};
 
 function verticalWin() {
-    // Your code here
+  if (board[0][0] === playerTurn && board[1][0] === playerTurn && board[2][0] === playerTurn) {
+    return true;
+  }
+  if (board[0][1] === playerTurn && board[1][1] === playerTurn && board[2][1] === playerTurn) {
+    return true;
+  }
+  if (board[0][2] === playerTurn && board[1][2] === playerTurn && board[2][2] === playerTurn) {
+  return true;
 }
+  else {
+      return false;
+  }
+};
 
 function diagonalWin() {
-    // Your code here
+  if (board[0][0] === playerTurn && board[1][1] === playerTurn && board[2][2] === playerTurn) {
+    return true;
+  }
+  if (board[0][2] === playerTurn && board[1][1] === playerTurn && board[2][0] === playerTurn) {
+    return true;
+}
+  else {
+      return false;
+  }
 }
 
 function checkForWin() {
-    // Your code here
+  if (horizontalWin() || verticalWin() || diagonalWin()) {
+    cleanBoard();
+    return true;
+  }
+  else {
+      return false;
+  }
+};
+
+function cleanBoard() {
+  board = [
+   [' ', ' ', ' '],
+   [' ', ' ', ' '],
+   [' ', ' ', ' ']
+  ];
+    return board;
 }
 
 function ticTacToe(row, column) {
-    // Your code here
-}
+  board [row][column] = playerTurn;
+    if (checkForWin()) {
+      console.log('Player ' + playerTurn + ' Won!');
+    }
+    playerTurn = (playerTurn === 'X') ? 'O' : 'X';
+};
 
 function getPrompt() {
     printBoard();
@@ -48,8 +97,7 @@ function getPrompt() {
         ticTacToe(result['row'], result['column']);
         getPrompt();
     });
-}
-
+};
 
 
 // Tests
