40e028b626ab559f596aaca744a5c8a6d69a9c89
Gabe Amaya <GabeAmaya2@gmail.com>
2016-08-16 09:32:44 -0500

    Finished Towers of Hanoi with huge help from mentors, I learned a ton in more detail, about objects.

--- apps/04TowersOfHanoi.js
+++ apps/04TowersOfHanoi.js
@@ -10,30 +10,60 @@ var stacks = {
     c: []
 };
 
+
 function printStacks() {
     console.log("a: " + stacks.a);
     console.log("b: " + stacks.b);
     console.log("c: " + stacks.c);
 }
 
-function movePiece() {
-    // Your code here
-
+function movePiece(startStack, endStack) {
+    // assigned a variable so that it is easier to read when combined with pushinng
+    // or moving the game piece to the next spot.
+    var piece = stacks[startStack].pop();
+    // the variable piece is used instead of that long start stack expression.
+    stacks[endStack].push(piece);
 }
 
-function isLegal() {
-    // Your code here
+function isLegal(startStack, endStack) {
+
+    // assigning a local variable for the length of the starting and ending stack so that
+    // we may reference it elsewhere in this function easier.
+    var startStackLength = stacks[startStack].length;
+    var endStackLength = stacks[endStack].length;
+
+    // ** used a Boolean expression to run through scenerios for illegal moves **
+
+    // took the length and checked to see if it was empty, if there is nothing in the first tower
+    // then it is an illegal move.
+    if (startStackLength === 0) {
+      return false;
+    }
+    else if (endStackLength === 0) {
+      return true;
+    }
+    // checking to see if the last index of startStack is less than the last index of endStack.
+    else {
+      return stacks[startStack][startStackLength - 1] < stacks[endStack][endStackLength - 1];
+    }
 
 }
 
 function checkForWin() {
-    // Your code here
-
+    // Pretty simple function here, simply checking to see if stacks b or c have
+    // a length of 4, meaning that the stack is 4 blocks high, if they do then a win is detected.
+    if (stacks['b'].length === 4 || stacks['c'].length === 4) {
+      console.log('Congrats, you win!' + '\nCan you beat it in less turns?')
+      return true;
+    }
+    else {
+      return false;
+    }
 }
 
 function towersOfHanoi(startStack, endStack) {
-    // Your code here
-
+    // calling the function to move the piece from user
+    movePiece(startStack, endStack);
 }
 
 function getPrompt() {
