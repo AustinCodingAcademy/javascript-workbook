096309260d122195fa74373aca24583023355300
madelineolsen <mloveolsen@gmail.com>
2016-08-17 19:17:11 -0500

    Lesson 4 Towers of Hanoi, finished

--- apps/04TowersOfHanoi.js
+++ apps/04TowersOfHanoi.js
@@ -10,37 +10,94 @@ var stacks = {
     c: []
 };
 
+//NEW STUFF
+function legalStack(startStack, endStack) {
+  if((startStack == "a"|| startStack == "b"|| startStack == "c")
+    &&
+    (endStack == "a"|| endStack == "b"|| endStack == "c"))  {
+    return true;
+  }
+  else {
+    return false;
+  }
+    }
+
+
+
+//NEW STUFF ENDS
+
 function printStacks() {
     console.log("a: " + stacks.a);
     console.log("b: " + stacks.b);
     console.log("c: " + stacks.c);
 }
 
-function movePiece() {
+function movePiece(startStack, endStack) {
     // Your code here
+
+    var piece = stacks[startStack].pop();
+    stacks[endStack].push(piece);
 
 }
 
-function isLegal() {
+function isLegal(startStack, endStack) {
     // Your code here
+    var startPiece = stacks[startStack].length - 1;
+    var endPiece = stacks[endStack].length - 1;
+
+    console.log("startpiece: "+ stacks[startStack][startPiece]);
+    console.log("endpiece: " + stacks[endStack][endPiece]);
+
+
+    if(stacks[startStack][startPiece] < stacks[endStack][endPiece] || stacks[endStack].length === 0) {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
 
 }
 
 function checkForWin() {
     // Your code here
-
+    if(stacks.b.length === 4 || stacks.c.length === 4) {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
 }
 
 function towersOfHanoi(startStack, endStack) {
     // Your code here
-
+    if(!legalStack(startStack, endStack)) {
+      console.log("The stack must be a, b, or c");
+      getPrompt();
+    }
+    else {
+    if(isLegal(startStack,endStack)) {
+    movePiece(startStack, endStack);
+      if(checkForWin()) {
+        console.log("You win!");
+      }
+      else
+      {
+        getPrompt();
+      }
+  }
+  else {
+    console.log("That is not a legal move!");
+    getPrompt();
+  }
 }
-
+}
 function getPrompt() {
     printStacks();
     prompt.get(['start stack', 'end stack'], function (error, result) {
         towersOfHanoi(result['start stack'], result['end stack']);
-        getPrompt();
+        // getPrompt();
     });
 }
 
