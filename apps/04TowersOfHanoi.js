b952562fe695b16ec9786a16eac519105929c54c
madelineolsen <mloveolsen@gmail.com>
2016-08-17 19:58:42 -0500

    Lesson 4 Towers of Hanoi I MADE IT PRINT HOW MANY MOVES YOU USED WHEN YOU WIN, I DID IT ALL BY MYSELFgit status

--- apps/04TowersOfHanoi.js
+++ apps/04TowersOfHanoi.js
@@ -10,19 +10,23 @@ var stacks = {
     c: []
 };
 
-//NEW STUFF
+
 function legalStack(startStack, endStack) {
-  if((startStack == "a"|| startStack == "b"|| startStack == "c")
-    &&
-    (endStack == "a"|| endStack == "b"|| endStack == "c"))  {
-    return true;
-  }
-  else {
-    return false;
-  }
+    if ((startStack == "a" || startStack == "b" || startStack == "c") &&
+        (endStack == "a" || endStack == "b" || endStack == "c")) {
+        return true;
+    } else {
+        return false;
     }
+}
 
+//NEW STUFF
 
+var counter = 0;
+
+function add() {
+    counter += 1;
+}
 
 //NEW STUFF ENDS
 
@@ -45,57 +49,51 @@ function isLegal(startStack, endStack) {
     var startPiece = stacks[startStack].length - 1;
     var endPiece = stacks[endStack].length - 1;
 
-    console.log("startpiece: "+ stacks[startStack][startPiece]);
+    console.log("startpiece: " + stacks[startStack][startPiece]);
     console.log("endpiece: " + stacks[endStack][endPiece]);
 
 
-    if(stacks[startStack][startPiece] < stacks[endStack][endPiece] || stacks[endStack].length === 0) {
-      return true;
-    }
-    else
-    {
-      return false;
+    if (stacks[startStack][startPiece] < stacks[endStack][endPiece] || stacks[endStack].length === 0) {
+        return true;
+    } else {
+        return false;
     }
 
 }
 
 function checkForWin() {
     // Your code here
-    if(stacks.b.length === 4 || stacks.c.length === 4) {
-      return true;
-    }
-    else
-    {
-      return false;
+    if (stacks.b.length === 4 || stacks.c.length === 4) {
+        return true;
+    } else {
+        return false;
     }
 }
 
 function towersOfHanoi(startStack, endStack) {
     // Your code here
-    if(!legalStack(startStack, endStack)) {
-      console.log("The stack must be a, b, or c");
-      getPrompt();
-    }
-    else {
-    if(isLegal(startStack,endStack)) {
-    movePiece(startStack, endStack);
-      if(checkForWin()) {
-        console.log("You win!");
-      }
-      else
-      {
+    if (!legalStack(startStack, endStack)) {
+        console.log("The stack must be a, b, or c");
         getPrompt();
-      }
-  }
-  else {
-    console.log("That is not a legal move!");
-    getPrompt();
-  }
+    } else {
+        if (isLegal(startStack, endStack)) {
+            movePiece(startStack, endStack);
+            add();
+            if (checkForWin()) {
+                console.log("You won in " + counter + " moves!");
+            } else {
+                getPrompt();
+            }
+        } else {
+            console.log("That is not a legal move!");
+            getPrompt();
+        }
+    }
 }
-}
+
 function getPrompt() {
     printStacks();
-    prompt.get(['start stack', 'end stack'], function (error, result) {
+    prompt.get(['start stack', 'end stack'], function(error, result) {
         towersOfHanoi(result['start stack'], result['end stack']);
         // getPrompt();
     });
@@ -105,36 +103,48 @@ function getPrompt() {
 
 if (typeof describe !== 'undefined') {
 
-    describe('#towersOfHanoi()', function () {
-        it('should be able to move a block', function () {
+    describe('#towersOfHanoi()', function() {
+        it('should be able to move a block', function() {
             towersOfHanoi('a', 'b');
-            assert.deepEqual(stacks, { a: [4, 3, 2], b: [1], c: [] });
+            assert.deepEqual(stacks, {
+                a: [4, 3, 2],
+                b: [1],
+                c: []
+            });
         });
     });
 
-    describe('#isLegal()', function () {
-        it('should not allow an illegal move', function () {
+    describe('#isLegal()', function() {
+        it('should not allow an illegal move', function() {
             stacks = {
-              a: [4, 3, 2],
-              b: [1],
-              c: []
+                a: [4, 3, 2],
+                b: [1],
+                c: []
             };
             assert.equal(isLegal('a', 'b'), false);
         });
-        it('should allow a legal move', function () {
+        it('should allow a legal move', function() {
             stacks = {
-              a: [4, 3, 2, 1],
-              b: [],
-              c: []
+                a: [4, 3, 2, 1],
+                b: [],
+                c: []
             };
             assert.equal(isLegal('a', 'c'), true);
         });
     });
-    describe('#checkForWin()', function () {
-        it('should detect a win', function () {
-            stacks = { a: [], b: [4, 3, 2, 1], c: [] }
+    describe('#checkForWin()', function() {
+        it('should detect a win', function() {
+            stacks = {
+                a: [],
+                b: [4, 3, 2, 1],
+                c: []
+            }
             assert.equal(checkForWin(), true);
-            stacks = { a: [1], b: [4, 3, 2], c: [] }
+            stacks = {
+                a: [1],
+                b: [4, 3, 2],
+                c: []
+            }
             assert.equal(checkForWin(), false);
         });
     })
