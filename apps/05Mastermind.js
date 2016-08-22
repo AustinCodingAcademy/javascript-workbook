d8f74d48e41f013a9513a2346047a3266a27229a
Sandra Killebrew <sandra@nextconsulting-llc.com>
2016-08-22 18:41:15 -0500

    lesson 05 app in progress

--- apps/05Mastermind.js
+++ apps/05Mastermind.js
@@ -11,7 +11,7 @@ var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
 function printBoard() {
     for (var i = 0; i < board.length; i++) {
-        console.log(board[i])
+        console.log(board[i]);
     }
 }
 
@@ -26,13 +26,41 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateHint() {
-    // your code here
+function generateHint(solution, guess) {
+    var solutionArray = solution.split('');
+    var guessArray = guess.split('');
+    var correctLetterLocations = 0;
+    for (var i=0; i < 4; i++) {
+      if (solutionArray[i] === guessArray[i]) {
+        correctLetterLocations ++ ;
+        solutionArray[i] = null;
+      }
+    };
+    var correctLetters = 0;
+    for (var i=0; i < 4; i++) {
+	     var targetIndex = solutionArray.indexOf(guessArray[i])
+//detect if the letter in guessArray matches a letter in solutionArray.  If it does,
+//add to correctLetters and set the index to null.
+	   if (targetIndex > -1) {
+		    correctLetters ++ ;
+		    targetIndex = null;
+	    }
+    }
+    return colors.red(correctLetterLocations) + '-' + colors.white(correctLetters);
 }
 
 function mastermind(guess) {
-    // your code here
+    if (solution === guess){
+//not console.log the string?
+      return "You guessed it!";
+    }
+    var hint = generateHint(solution, guess);
+    board.push(guess + hint);
+    if (board.length === 10) {
+      return "You ran out of turns! The solution was " + solution;
+    }
 }
+
 
 
 function getPrompt() {
@@ -60,14 +88,14 @@ if (typeof describe !== 'undefined') {
 
     describe('#generateHint()', function () {
         it('should generate hints', function () {
-            assert.equal(generateHint('abcd', 'abdc'), '2-2');
+            assert.equal(generateHint('abcd', 'abdc'), colors.red('2') + '-' + colors.white('2'));
         });
         it('should generate hints if solution has duplicates', function () {
-            assert.equal(generateHint('abcd', 'aabb'), '1-1');
+            assert.equal(generateHint('abcd', 'aabb'), colors.red('1') + '-' + colors.white('1'));
         });
 
     });
-        
+
 } else {
 
     generateSolution();
