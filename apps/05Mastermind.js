a3c51a5313fbbd6f0851514fd12578c0e5074e76
Gabe Amaya <GabeAmaya2@gmail.com>
2016-08-23 16:55:12 -0500

    Started on app 5 Mastermind, pretty hard at first but starting to understand about implementing for loops.

--- apps/05Mastermind.js
+++ apps/05Mastermind.js
@@ -16,22 +16,48 @@ function printBoard() {
 }
 
 function generateSolution() {
+    // this generates a random index number and adds it to the empty string varialbe to produce a solution
     for (var i = 0; i < 4; i++) {
         var randomIndex = getRandomInt(0, letters.length);
         solution += letters[randomIndex];
     }
 }
 
+// this function is called in the generateSolution funtion to get a random number
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateHint() {
-    // your code here
+function generateHint(solution,guess) {
+
+    // creating a variable so we can split the solution and guess into an array from a string.
+    var solutionArray = solution.split('');
+    var guessArray = guess.split('');
+    var correctLetterLocations = 0;
+    var correctLetters = 0
+
+    // a for loop is created to show iterate through the solution to see if any guesses match the solution.
+    for (i = 0, i < solutionArray.length, i++) {
+      if (solutionArray[i] === guessArray[i]) {
+        solutionArray[i] = null;
+        correctLetterLocations++;
+      }
+    }
+
+    // using indexof to determine if there are correct letters in the guess.
+    for (i = 0, i < solutionArray.length, i++) {
+      var targetIndex = solutionArray.indexof(guessArray[i])
+      if (targetIndex > -1) {
+        correctLetters++;
+        solutionArray[targetIndex] = null;
+      }
+    }
 }
 
 function mastermind(guess) {
-    // your code here
+  if (guess === solution) {
+    console.log('Congrats son, you guessed it.')
+  }
 }
 
 
@@ -67,7 +93,7 @@ if (typeof describe !== 'undefined') {
         });
 
     });
-        
+
 } else {
 
     generateSolution();
