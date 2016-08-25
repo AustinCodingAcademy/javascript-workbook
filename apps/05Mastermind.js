043c60612da38743368da3fb46ec4e852e2d9c08
Aaron <aaronaustintx@gmail.com>
2016-08-24 19:54:10 -0500

    Commit for Final Version of Mastermind.Js

--- apps/05Mastermind.js
+++ apps/05Mastermind.js
@@ -10,18 +10,18 @@ var solution = '';
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 var hint = [];
 function printBoard() {
-    for (var i = 0; i < board.length; i++) {
-        console.log(board[i])
-    }
+  for (var i = 0; i < board.length; i++) {
+    console.log(board[i])
+  }
 }
 
 function generateSolution() {
-    for (var i = 0; i < 4; i++) {
-        var randomIndex = getRandomInt(0, letters.length);
-        solution += letters[randomIndex];
-    }
+  for (var i = 0; i < 4; i++) {
+    var randomIndex = getRandomInt(0, letters.length);
+    solution += letters[randomIndex];
+  }
 
-    return solution;
+  return solution;
 }
 
 function getRandomInt(min, max) {
@@ -29,90 +29,92 @@ function getRandomInt(min, max) {
 }
 
 function generateHint(solution, guess) {
-    // your code here
-    var solutionArray = solution.split("");
-    var guessArray = guess.split("");
-    var correctLetterLocations = 0;
-    // determine correct letter location
-    for(var i=0; i<=3; i++){
-      if(guessArray[i] === solutionArray[i]){
-          correctLetterLocations++;
-          solutionArray[i] = null;
-    //        return correctLetterLocations;
-      }
+  // your code here
+  var solutionArray = solution.split("");
+  var guessArray = guess.split("");
+  var correctLetterLocations = 0;
+  // determine correct letter location
+  for(var i=0; i<=3; i++){
+    if(guessArray[i] === solutionArray[i]){
+      correctLetterLocations++;
+      solutionArray[i] = null;
+      //        return correctLetterLocations;
     }
-      // determine correct letters
-      var correctLetters = 0;
+  }
+  // determine correct letters
+  var correctLetters = 0;
 
-      // for if solution i
-      //    correctLetters
-      //for(var )
+  // for if solution i
+  //    correctLetters
+  //for(var )
 
-      for(var j=0; j<=solutionArray.length; j++){
+  for(var j=0; j<=solutionArray.length; j++){
 
-        var targetIndex = guessArray.indexOf(solutionArray[j]);
-        console.log(targetIndex);
-        if(targetIndex > -1){
-                correctLetters++;
-                solutionArray[j] = null;
-
-        }
+    var targetIndex = guessArray.indexOf(solutionArray[j]);
+  //  console.log(targetIndex);
+    if(targetIndex > -1){
+      correctLetters++;
+      solutionArray[j] = null;
     }
-    hint = correctLetterLocations + "-" + correctLetters;
-    return hint;
+  }
+  hint = correctLetterLocations + "-" + correctLetters;
+  return hint;
 }
 
 
 function mastermind(guess) {
-    // your code here
+  // your code here
   if(guess === solution){
-    console.log('You guessed it!');
+      console.log('You guessed it!');
+      var hint = generateHint(solution, guess);
+      board.push(hint + " " + guess );
     return 'You guessed it!';
   } else {
-      generateHint(solution, guess);
+      var hint = generateHint(solution, guess);
+      board.push(hint + " " + guess );
   }
-  board.push(hint + " " + guess );
+
   return guess;
 
 }
 
 
 function getPrompt() {
-    prompt.get(['guess'], function (error, result) {
-        console.log( mastermind(result['guess']) );
-        printBoard();
-        console.log(solution);
-        getPrompt();
-    });
+  prompt.get(['guess'], function (error, result) {
+    console.log( mastermind(result['guess']) );
+    printBoard();
+  // console.log(solution);
+    getPrompt();
+  });
 }
 
 // Tests
 
 if (typeof describe !== 'undefined') {
 
-    describe('#mastermind()', function () {
-        it('should register a guess and generate hints', function () {
-            solution = 'abcd';
-            mastermind('aabb');
-            assert.equal(board.length, 1);
-        });
-        it('should be able to detect a win', function () {
-            assert.equal(mastermind(solution), 'You guessed it!');
-        });
+  describe('#mastermind()', function () {
+    it('should register a guess and generate hints', function () {
+      solution = 'abcd';
+      mastermind('aabb');
+      assert.equal(board.length, 1);
+    });
+    it('should be able to detect a win', function () {
+      assert.equal(mastermind(solution), 'You guessed it!');
+    });
+  });
+
+  describe('#generateHint()', function () {
+    it('should generate hints', function () {
+      assert.equal(generateHint('abcd', 'abdc'), '2-2');
+    });
+    it('should generate hints if solution has duplicates', function () {
+      assert.equal(generateHint('abcd', 'aabb'), '1-1');
     });
 
-    describe('#generateHint()', function () {
-        it('should generate hints', function () {
-            assert.equal(generateHint('abcd', 'abdc'), '2-2');
-        });
-        it('should generate hints if solution has duplicates', function () {
-            assert.equal(generateHint('abcd', 'aabb'), '1-1');
-        });
-
-    });
+  });
 
 } else {
 
-    generateSolution();
-    getPrompt();
+  generateSolution();
+  getPrompt();
 }
