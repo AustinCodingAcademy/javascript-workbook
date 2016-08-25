d93aed58c5388bf8ddfaba8d2b71c9d12ca1fbef
Aaron <aaronaustintx@gmail.com>
2016-08-24 19:27:56 -0500

    Commit to get Mastermind.js committed

--- apps/05Mastermind.js
+++ apps/05Mastermind.js
@@ -8,7 +8,7 @@ prompt.start();
 var board = [];
 var solution = '';
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-
+var hint = [];
 function printBoard() {
     for (var i = 0; i < board.length; i++) {
         console.log(board[i])
@@ -20,18 +20,60 @@ function generateSolution() {
         var randomIndex = getRandomInt(0, letters.length);
         solution += letters[randomIndex];
     }
+
+    return solution;
 }
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateHint() {
+function generateHint(solution, guess) {
     // your code here
+    var solutionArray = solution.split("");
+    var guessArray = guess.split("");
+    var correctLetterLocations = 0;
+    // determine correct letter location
+    for(var i=0; i<=3; i++){
+      if(guessArray[i] === solutionArray[i]){
+          correctLetterLocations++;
+          solutionArray[i] = null;
+    //        return correctLetterLocations;
+      }
+    }
+      // determine correct letters
+      var correctLetters = 0;
+
+      // for if solution i
+      //    correctLetters
+      //for(var )
+
+      for(var j=0; j<=solutionArray.length; j++){
+
+        var targetIndex = guessArray.indexOf(solutionArray[j]);
+        console.log(targetIndex);
+        if(targetIndex > -1){
+                correctLetters++;
+                solutionArray[j] = null;
+
+        }
+    }
+    hint = correctLetterLocations + "-" + correctLetters;
+    return hint;
 }
+
 
 function mastermind(guess) {
     // your code here
+  if(guess === solution){
+    console.log('You guessed it!');
+    return 'You guessed it!';
+  } else {
+      generateHint(solution, guess);
+  }
+  board.push(hint + " " + guess );
+  return guess;
+
 }
 
 
@@ -39,6 +81,7 @@ function getPrompt() {
     prompt.get(['guess'], function (error, result) {
         console.log( mastermind(result['guess']) );
         printBoard();
+        console.log(solution);
         getPrompt();
     });
 }
@@ -67,7 +110,7 @@ if (typeof describe !== 'undefined') {
         });
 
     });
-        
+
 } else {
 
     generateSolution();
