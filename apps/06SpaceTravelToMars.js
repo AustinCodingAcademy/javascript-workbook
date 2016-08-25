d831f79edf8f6492b9bf4271d23ea4b5667ced56
Salvador <salvadorrod13@gmail.com>
2016-08-24 20:32:20 -0500

    apps6 SpacetravelToMars passing!

--- apps/06SpaceTravelToMars.js
+++ apps/06SpaceTravelToMars.js
@@ -10,6 +10,35 @@ var jobTypes = {
 };
 
 // Your code here
+function CrewMember (name, job, specialSkill) {
+  this.name = name;
+  this.job = job;
+  this.specialSkill = specialSkill;
+  this.ship = null;
+
+  this.enterShip = function(ship){
+    this.ship = ship;
+    ship.crew.push(this)
+  }
+}
+
+function Ship(name, type, ability){
+  this.name = name;
+  this.type = type;
+  this.ability = ability;
+  this.crew = [];
+
+  this.missionStatement = function(name) {
+    for (var i=0;i<this.crew.length;i++){
+      var job = this.crew[i].job;
+      if ( jobTypes[job] === this.type ) {
+        return this.ability;
+      }
+    }
+    return "Can't perform a mission yet.";
+  };
+
+}
 
 //tests
 if (typeof describe !== 'undefined'){
