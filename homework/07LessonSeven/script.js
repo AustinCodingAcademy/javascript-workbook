601d8badb3bd3fc6a4f1423d68a2337e78c4d865
cjclay <Outlier@carlas-mbp.attlocal.net>
2016-09-07 18:12:51 -0500

    HomeworkSeven Update

--- homework/07LessonSeven/script.js
+++ homework/07LessonSeven/script.js
@@ -69,22 +69,22 @@ $(document).on('ready', function() {
   // Problem 9:
   // select the element with id="insert-text"
   // then set the text of this element to "Text Has Been Inserted"
-  $('#insert-text').text('Text Has Been Inserted')
+  $('#insert-text').text('Text Has Been Inserted');
 
   // Problem 10:
   // select the element with id="add-attribute"
   // add the attribute data-color="red" to that element
-  $('#add-attribute').attr('data-color','red')
+  $('#add-attribute').attr('data-color','red');
 
   // Problem 11:
   // select the element with id="data-element"
   // and set window.dataIdValue to the value of the data-id attribute of the element
   // you can do this by calling .data() on the selected element
-  window.dataIdValue = $('data-element').data('id')
+  window.dataIdValue = $('data-element').data('id');
 
   // Problem 12:
   // select the element with id="data-element"
   // and set its text (.text()) to the value of the data-content attribute
-$('#data-element').text($('#data-content').data('content'))  
+$('#data-element').text($('#data-content').data('content'));  
 
 });
