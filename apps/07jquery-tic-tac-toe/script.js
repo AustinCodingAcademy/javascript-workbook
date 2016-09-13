8b80d2ab36dc17500cf3a6eeff675df4b5ab0546
Aaron <aaronaustintx@gmail.com>
2016-09-12 21:18:01 -0500

    Commit to get  version #3 of App7 tictac toe in

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -1,21 +1,55 @@
 'use strict';
-
+var playerTurn = 'X';
 $(document).on('ready', function() {
     // Put app logic in here
-    var playerTurn = 'X';
     // .on CLICK
+
+    function checkForWin(){
+
+      if (horizontalWin() === true || verticalWin() === true){
+        // checkForHorizontalWin
+
+      }
+      function horizontalWin(){
+      if( ($('[data-cell = "0"]').text() === playerTurn &&
+          $('[data-cell = "1"]').text() === playerTurn &&
+          $('[data-cell = "2"]').text() === playerTurn )||
+
+          ($('[data-cell = "3"]').text() === playerTurn &&
+          $('[data-cell = "4"]').text() === playerTurn &&
+          $('[data-cell = "5"]').text() === playerTurn )||
+
+          ($('[data-cell = "6"]').text() === playerTurn &&
+          $('[data-cell = "7"]').text() === playerTurn &&
+          $('[data-cell = "8"]').text() === playerTurn ) ){
+
+        return true;
+        // CHECK for verticalWin
+      }
+      } else if ( ($('[data-cell = "0"]').text() === playerTurn &&
+          $('[data-cell = "3"]').text() === playerTurn &&
+          $('[data-cell = "6"]').text() === playerTurn )||
+
+          ($('[data-cell = "1"]').text() === playerTurn &&
+          $('[data-cell = "4"]').text() === playerTurn &&
+          $('[data-cell = "7"]').text() === playerTurn )||
+
+          ($('[data-cell = "2"]').text() === playerTurn &&
+          $('[data-cell = "5"]').text() === playerTurn &&
+          $('[data-cell = "8"]').text() === playerTurn ) ){
+        return true;
+      }
+
+      return true;
+    }
+
+
     $('[data-cell]').on('click', function(){
       $(this).text(playerTurn);
-      if(checkForWin){
-        return true;
-      }
-      playerTurn === playerTurn ? 'X' : 'O' ;
+      //checkForWin();
 
-      checkForWin =  function(){
-        return true;
-      }
-
-
-    })
+      // toggle
+        playerTurn =  (playerTurn === 'X') ?  'O' : 'X';
+    });
 
 });
