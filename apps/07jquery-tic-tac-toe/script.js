865fa86d6f570a3c255d5beb7a1e916ba06592e2
Aaron <aaronaustintx@gmail.com>
2016-09-12 19:05:06 -0500

    Commit to get  version #2 of App7 tictac toe in

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -2,4 +2,20 @@
 
 $(document).on('ready', function() {
     // Put app logic in here
+    var playerTurn = 'X';
+    // .on CLICK
+    $('[data-cell]').on('click', function(){
+      $(this).text(playerTurn);
+      if(checkForWin){
+        return true;
+      }
+      playerTurn === playerTurn ? 'X' : 'O' ;
+
+      checkForWin =  function(){
+        return true;
+      }
+
+
+    })
+
 });
