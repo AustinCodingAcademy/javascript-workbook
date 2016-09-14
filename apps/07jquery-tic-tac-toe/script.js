0b5aa3fdc1e196b40fe21e0ebe3237542ca68a8d
Judah M. Ritterman <judah29@gmail.com>
2016-09-14 18:26:30 -0500

    diff version of game

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -1,47 +1,74 @@
-console.log('hello');
-console.log('yo');
 'use strict';
+var counter=0;
+var winningCombos = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [6,4,2],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+];
 
 $(document).on('ready', function() {
-  // $('#announce-winner').html('won');
-    // Put app logic in here
+    $('#clear').click(function(){
+      clearBoard();
+      // ('[data-cell]').bind();
+    });
     var playerTurn = 'X';
     $('[data-cell]').click(function(){
-      console.log(playerTurn);
-      // if(playerTurn === 'X'){
+      $('#announce-winner').text('');
+      if( $(this).text() === ''){
+        counter++;
         $(this).text(playerTurn);
-      //   playerTurn = 'O';
-        // console.log($(this).text());
-      // }
-      // else{
-      //   $(this).text(playerTurn);
-      //   playerTurn = 'X';
-      //   console.log($(this).text());
-      //   // console.log( $('[data-cell="0"]').text() === 'X');
-      // }
-      if(checkForWin()){
-        $('#announce-winner').html(playerTurn + ' is the winner!');
-      }
-      playerTurn = (playerTurn === 'X')?  'O': 'X';
+
+        if( checkForWin() ){
+          $('#announce-winner').html('player ' + playerTurn + ' wins!');
+          // resetBoard();
+          // $('[data-cell]').unbind();
+
+        }
+
+        else if (checkForTie()) {
+          $('#announce-winner').html('tie game, click Clear Board button');
+        }
+        playerTurn = (playerTurn === 'X')?  'O': 'X';
+        }
+        else{
+          $('#announce-winner').text('invalid move');
+        }
     });
 
+    function resetBoard(){
+      $(document).on('click', function(){
+        clearBoard();
+      });
+    }
 
+    function clearBoard(){
+      // $('[data-cell]').bind();
+
+      $('[data-cell]').each(function(){
+        $(this).empty();
+      });
+      $('#announce-winner').html('');
+      playerTurn = 'X';
+      counter=0;
+
+    }
     function checkForWin() {
-      return(horizontalWin() || verticalWin() || diagonalWin());
-      // return(horizontalWin() || verticalWin() || diagonalWin());
+        for(var i = 0; i < winningCombos.length; i++) {
+          if($('[data-cell="'  + winningCombos[i][0] +  '"]').text() === playerTurn &&
+          $('[data-cell="'  + winningCombos[i][1] +  '"]').text() === playerTurn &&
+          $('[data-cell="'  + winningCombos[i][2] +  '"]').text() === playerTurn
+        ){
+            return true;
+          }
+        }
     }
-
-    function horizontalWin() {
-      return($('[data-cell="0"]').text() === playerTurn && $('[data-cell="1"]').text() === playerTurn && $('[data-cell="2"]').text() === playerTurn || $('[data-cell="3"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="5"]').text() === playerTurn || $('[data-cell="6"]').text() === playerTurn && $('[data-cell="7"]').text() === playerTurn && $('[data-cell="8"]').text() === playerTurn);
+    function checkForTie(){
+      return(counter === 9);
     }
-
-    function verticalWin() {
-      return($('[data-cell="0"]').text() === playerTurn && $('[data-cell="3"]').text() === playerTurn && $('[data-cell="6"]').text() === playerTurn || $('[data-cell="1"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="7"]').text() === playerTurn || $('[data-cell="2"]').text() === playerTurn && $('[data-cell="5"]').text() === playerTurn && $('[data-cell="8"]').text() === playerTurn);
-    }
-
-    function diagonalWin() {
-      return($('[data-cell="0"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="8"]').text() === playerTurn || $('[data-cell="2"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="6"]').text() === playerTurn);
-    }
-
 
 });
