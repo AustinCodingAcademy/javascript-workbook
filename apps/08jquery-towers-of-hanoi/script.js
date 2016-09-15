b2508f9b296efd7b737c9cd358accb8f2f1834e6
Kimberly Cook <kcook@zenoss.com>
2016-09-14 22:14:46 -0500

    partial app 8

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -1,5 +1,21 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
-}
+
+var $block = null;
+
+  $('[data-stack]').click(function() {
+    if ($block === null) {
+      $block = $(this).children().last().detach();
+    }
+    else {
+      $(this).append($block);
+      $block = null;
+    }
+  });
+
+});
+
+
+
+//for next step remember Piero's notes in Slack and isLegal function from original ToH app
