ef8524dd95746ae2d58e1850f75f0eef65ab015e
Aaron <aaronaustintx@gmail.com>
2016-09-14 21:00:50 -0500

    Commit to get  version #1 of App/08jquery-towers-of-hanoi Eight in

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -2,4 +2,23 @@
 
 $(document).ready(function() {
   // Put app logic here
-}
+  var block = null;
+
+  $('[data-stack]').on('click', function(){
+  //   var $dataStack = $('[data-stack]');
+
+      if(!block){
+      block = $(this).children().last().detach();
+    } else {
+      console.log(block);
+      $(this).append(block);
+        block = null;
+    }
+  });
+
+  // if block
+  // put it down
+  // else pick it up
+
+
+});
