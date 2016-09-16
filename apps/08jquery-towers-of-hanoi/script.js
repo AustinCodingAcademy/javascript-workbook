d901395bbf1a459c2d91ece1dd1b709245d035e6
Salvador <salvadorrod13@gmail.com>
2016-09-15 21:27:08 -0500

    Towers of Hanoi GUI App better quality!

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -5,7 +5,11 @@ $(document).ready(function() {
   var $block = null;
   var maxBlock =4;
 
-  $('[data-stack]').click( function(){
+  function checkForWin() {
+    return ( $("[data-stack='2']").children().length >= maxBlock || $("[data-stack='3']").children().length >= maxBlock) ;
+  }
+
+  $("[data-stack]").click( function(){
     // Get the children and lastBlock of active stack
     var $children = $(this).children();
     var $lastBlock = $children.last();
@@ -15,24 +19,14 @@ $(document).ready(function() {
       $block = $lastBlock.detach();
     } else {
       // Verify the move.. with the last block of active stack and the block picked up
-      if ( $lastBlock.data('block') <= $block.data('block') ) {
-        alert('Invalid move');
+      if ( $lastBlock.data("block") <= $block.data("block") ) {
+        alert("Invalid move");
       } else {
         // Insert the detach block into the active stack
         $(this).append($block);
         $block = null;
       }
-
-      if (checkForWin()) {
-        $('#announce-game-won').text('You Won!');
-      }
-
+      if (checkForWin()) { $("#announce-game-won").text("You Won!"); }
     }
-
-  } );
-
-  function checkForWin() {
-    return ( $('[data-stack="2"]').children().length >= maxBlock || $('[data-stack="3"]').children().length >= maxBlock) ;
-  }
-
+  } );  // Click event
 } );
