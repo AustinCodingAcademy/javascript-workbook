93dab932d3b3442cc0d1d9c5cb16c0c67e50f4a9
Jon Woo <jw56578@gmail.com>
2016-09-15 20:13:03 -0500

    finish homework tests

--- homework/07LessonSeven/test.js
+++ homework/07LessonSeven/test.js
@@ -16,8 +16,7 @@ beforeEach(function (done) {
     {
       file: path.resolve(__dirname, 'index.html'),
       scripts: [
-          path.resolve(__dirname, '../../vendor/jquery.js'),
-          path.resolve(__dirname, 'script.js')
+          path.resolve(__dirname, '../../vendor/jquery.js')
       ],
       virtualConsole,
       done(err, jsdomWindow) {
@@ -29,6 +28,8 @@ beforeEach(function (done) {
         window = jsdomWindow;
         $(document).on('ready', function () {
           done();
+          testFunctions();
+          
         });
       }
     }
@@ -130,3 +131,93 @@ describe('Lesson 7 Homework', function () {
   });
 
 });
+
+
+ function testFunctions() {
+
+  // ****
+  // jQuery
+  // ****
+
+  // ****
+  // element selectors
+  // ****
+
+  // Problem 1:
+  // select the h1 element
+  // assign the selected jQuery object to the property window.h1
+  window.h1 = $('h1');
+
+  // Problem 2:
+  // select the p element
+  // assign the selected jQuery object to the the property window.p
+  window.p = $('p');
+
+  // ****
+  // id selectors
+  // ****
+
+  // Problem 3:
+  // select the element with id="main"
+  // assign the selected jQuery object to the property window.idMain
+  window.idMain = $('#main');
+
+  // Problem 4:
+  // select the element with id="secondary"
+  // assign the selected jQuery object to the the property window.idSecondary
+  window.idSecondary = $('#secondary');
+
+  // ****
+  // class selectors
+  // ****
+
+  // Problem 5:
+  // select the element with class="container"
+  // assign the selected jQuery object to the property window.container
+  window.container = $('.container');
+
+  // Problem 6:
+  // select the elements with class="item"
+  // assign the selected jQuery object to the the property window.items
+  window.items = $('.item');
+
+  // ****
+  // attribute selectors
+  // ****
+
+  // Problem 7:
+  // select the element with the data-id attribute
+  // assign the selected jQuery object to the property window.dataId
+  window.dataId = $('[data-id]');
+
+  // Problem 8:
+  // select the element with the data-price attribute
+  // assign the selected jQuery object to the the property window.dataPrice
+  window.dataPrice = $('[data-price]');
+
+  // ****
+  // dom manipulation
+  // ****
+
+  // Problem 9:
+  // select the element with id="insert-text"
+  // then set the text of this element to "Text Has Been Inserted"
+  $('#insert-text').text('Text Has Been Inserted');
+
+  // Problem 10:
+  // select the element with id="add-attribute"
+  // add the attribute data-color="red" to that element
+  $('#add-attribute').attr('data-color','red');
+
+  // Problem 11:
+  // select the element with id="data-element"
+  // and set window.dataIdValue to the value of the data-id attribute of the element
+  // you can do this by calling .data() on the selected element
+  window.dataIdValue = $('#data-element').attr('data-id');
+
+  // Problem 12:
+  // select the element with id="data-element"
+  // and set its text (.text()) to the value of the data-content attribute
+  $('#data-element').text($('#data-element').attr('data-content'));
+
+}
