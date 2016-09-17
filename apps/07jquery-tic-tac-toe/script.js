480f5522a2bb7b253404ded0d06b02e25453f83d
nicolemichelle88 <daugereaux@gmail.com>
2016-09-17 15:10:20 -0500

    tic tac toe

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -1,20 +1,43 @@
 'use strict';
 
 $(document).on('ready', function() {
-var playerTurn = 'X';
-$('[data-cell]').click(function(){
-  $(this).text(playerTurn = (playerTurn === 'X') ? 'O' : 'X');
-});
-var horizontalWin = (function(){
-  if ()
-});
-var verticalWin = (function(){
+    var playerTurn = 'X';
+    $('[data-cell]').click(function() {
+        $(this).text(playerTurn = (playerTurn === 'X') ? 'O' : 'X');
+    });
+    var horizontalWin = (function() {
+        if (( < div data - cell = "0" > === playerTurn && < div data - cell = "1" > === playerTurn && < div data - cell = "2" > === playerTurn) || ( < div data - cell = "3" > === playerTurn && < div data - cell = "4" > === playerTurn && < div data - cell = "5" > === playerTurn) || ( < div data - cell = "6" > === playerTurn && < div data - cell = "7" > === playerTurn && < div data - cell = "8" > === playerTurn)) {
+            return true;
+        } else {
+            return false;
+        }
 
-});
-var diagonalWin = (function(){
+    });
 
-});
-var checkForWin = (function(){
-  if
-});
+
+    var verticalWin = (function() {
+        if (( < div data - cell = "0" > === playerTurn && < div data - cell = "3" > === playerTurn && < div data - cell = "6" > === playerTurn) || ( < div data - cell = "1" > === playerTurn && < div data - cell = "4" > === playerTurn && < div data - cell = "7" > === playerTurn) || ( < div data - cell = "2" > === playerTurn && < div data - cell = "5" > === playerTurn && < div data - cell = "8" > === playerTurn)) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+
+
+    var diagonalWin = (function() {
+        if (( < div data - cell = "0" > === playerTurn && < div data - cell = "4" > === playerTurn && < div data - cell = "8" > === playerTurn) || ( < div data - cell = "2" > === playerTurn && < div data - cell = "4" > === playerTurn && < div data - cell = "6" > === playerTurn)) {
+          return true;
+        } else {
+          return false;
+        }
+
+    });
+    var checkForWin = (function() {
+        ifif ((horizontalWin() === true) || (verticalWin() === true) || (diagonalWin() === true)) {
+            console.log('Player ' + playerTurn + 'won!');
+            return true;
+        } else {
+            return false;
+        }
+    });
 });
