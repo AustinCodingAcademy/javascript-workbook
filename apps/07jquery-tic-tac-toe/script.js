af2453c0bb2fd5b1cb41dc763693d4a3d9006b91
Manny Hagman <manny.hagman@gmail.com>
2016-09-19 11:02:05 -0500

    07tictactoe complete
    having to do this the old fashioned way because my repository is acting strange. 
    
    correct many issues I missed during initial submission.

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -1,39 +1,22 @@
-'use strict';
 
-var playerTurn = 'X';
+playerTurn = 'X';
 
-$(document).ready(function() {
-  $('[data-cell]').click(function() {
- $(this).text(playerTurn);
- checkForWin();
- if (checkForWin() === true) {
-   $("#announce-winner").text('please select clear board to start over');
-}
+$(document).on('ready', function() {
 
-playerTurn = (playerTurn === 'X') ? 'O' : 'X'; // this teirnery operator allows us to switch between x and o when clicking.
-});
+$('[data-cell]').click(function() {
+  $(this).text(playerTurn)
+  checkForWin();
+  playerTurn = (playerTurn === 'X') ?  'O' : 'X';
+})
 
 $('#clear').click(function() {
-$('[data-cell]').empty() //we do not have to use each here since the data cell selector performs its own implicit iteration. //
- $('#announce-winner').empty(); // this code clears the message displayed when a winner is detected
- playerTurn = 'X';
+  $('[data-cell]').empty()
+  $("#announce-winner").empty()
+  playerTurn = 'X';
+})
 });
-});
-
-
-//if statements execute when the return value === true//
-// using the "or" concditional operator
-// or "or" conditional will return true if any of the conditions are met.
-// Once this is detected the script moves to the next functional pat of the code.
-
-function checkForWin() {
-if (horizontalWin() || verticalWin()|| diagonalWin()) {
-    $("#announce-winner").text('player ' + playerTurn + ' wins!');
-};
-}
 
 function horizontalWin() {
-console.log('horizontalWin is working');
   return
   ($('[data-cell = "0"]').text() === playerTurn &&
   $('[data-cell = "1"]').text() === playerTurn &&
@@ -48,12 +31,11 @@ console.log('horizontalWin is working');
   $('[data-cell = "8"]').text() === playerTurn);
 }
 
-horizontalWin();
+// horizontalWin();
 
 function verticalWin() {
-console.log('verticalWin is working');
-  	return
-    ($('[data-cell = "0"]').text() === playerTurn &&
+
+	return ($('[data-cell = "0"]').text() === playerTurn &&
 	  $('[data-cell = "3"]').text() === playerTurn &&
 	  $('[data-cell = "6"]').text() === playerTurn) ||
 
@@ -66,11 +48,11 @@ console.log('verticalWin is working');
 	  $('[data-cell = "8"]').text() === playerTurn);
 }
 
-verticalWin();
+// verticalWin();
 
 function diagonalWin() {
-return
-  ($('[data-cell = "0"]').text() === playerTurn &&
+
+  return ($('[data-cell = "0"]').text() === playerTurn &&
   $('[data-cell = "4"]').text() === playerTurn &&
   $('[data-cell = "8"]').text() === playerTurn) ||
 
@@ -79,16 +61,10 @@ return
 	$('[data-cell = "6"]').text() === playerTurn);
 }
 
-diagonalWin();
+// diagonalWin();
 
-function preventMove() {
-console.log('this function will prevent a move')
+function checkForWin() {
+  if (horizontalWin() || verticalWin() || diagonalWin()) {
+    $("#announce-winner").text('player ' + playerTurn + ' wins!');
+  }
 }
-
-preventMove(); //these funcitons are place holders for the bonus spec. they also serve as troubleshooting devices to see exactly where my mode might be running into isses.
-
-function preventMoveChange() {
-console.log('this function will prevent a prevous cell from being over written with a click');
-}
-
-preventMoveChange();
