5e5416a7b2b9db518db6b212816586ca95d372a6
nicolemichelle88 <daugereaux@gmail.com>
2016-09-19 04:42:36 -0500

    tic tac toe final

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -1,43 +1,57 @@
 'use strict';
 
 $(document).on('ready', function() {
-    var playerTurn = 'O';
-    $('[data-cell]').click(function() {
-        $(this).text(playerTurn = (playerTurn === 'X') ? 'O' : 'X');
-    });
-    var horizontalWin = (function() {
-        if (( $('[data-cell="0"]').text()  === playerTurn && $('[data-cell="1"]').text() === playerTurn && $('[data-cell="2"]').text() === playerTurn) || ( $('[data-cell="3"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="5"]').text() === playerTurn) || ($('[data-cell="6"]').text() === playerTurn && $('[data-cell="7"]').text() === playerTurn && $('[data-cell="8"]').text() === playerTurn)) {
+  var playerTurn = 'X';
+
+$('[data-cell]').click(function() {
+    $(this).text(playerTurn);
+    if (!checkForWin()){
+      playerTurn = (playerTurn === 'X') ? 'O' : 'X';
+    }
+});
+
+$('#clear').click(function() {
+    $('[data-cell]').text('');
+    playerTurn = "X";
+});
+
+
+    function horizontalWin() {
+        if (( $('div[data-cell="0"]').text()  === playerTurn && $('div[data-cell="1"]').text() === playerTurn && $('div[data-cell="2"]').text() === playerTurn) || ( $('div[data-cell="3"]').text() === playerTurn && $('div[data-cell="4"]').text() === playerTurn && $('div[data-cell="5"]').text() === playerTurn) || ($('div[data-cell="6"]').text() === playerTurn && $('div[data-cell="7"]').text() === playerTurn && $('div[data-cell="8"]').text() === playerTurn)) {
             return true;
         } else {
             return false;
         }
 
-    });
+    };
 
 
-    var verticalWin = (function() {
-        if (($('[data-cell="0"]').text() === playerTurn && $('[data-cell="3"]').text() === playerTurn && $('[data-cell="6"]').text() === playerTurn) || ( $('[data-cell="1"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="7"]').text() === playerTurn) || ( $('[data-cell="2"]').text() === playerTurn && $('[data-cell="5"]').text() === playerTurn && $('[data-cell="8"]').text() === playerTurn)) {
+    function verticalWin() {
+        if (($('[data-cell="0"]').text() === playerTurn && $('div[data-cell="3"]').text() === playerTurn && $('div[data-cell="6"]').text() === playerTurn) || ( $('div[data-cell="1"]').text() === playerTurn && $('div[data-cell="4"]').text() === playerTurn && $('div[data-cell="7"]').text() === playerTurn) || ( $('div[data-cell="2"]').text() === playerTurn && $('div[data-cell="5"]').text() === playerTurn && $('div[data-cell="8"]').text() === playerTurn)) {
             return true;
         } else {
             return false;
         }
-    });
+    };
 
 
-    var diagonalWin = (function() {
-        if (( $('[data-cell="0"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="8"]').text() === playerTurn) || ($('[data-cell="2"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="6"]').text() === playerTurn)) {
+    function diagonalWin() {
+        if (( $('div[data-cell="0"]').text() === playerTurn && $('div[data-cell="4"]').text() === playerTurn && $('div[data-cell="8"]').text() === playerTurn) || ($('div[data-cell="2"]').text() === playerTurn && $('div[data-cell="4"]').text() === playerTurn && $('div[data-cell="6"]').text() === playerTurn)) {
           return true;
         } else {
           return false;
         }
 
-    });
-    var checkForWin = (function() {
-        if ((horizontalWin() === true) || (verticalWin() === true) || (diagonalWin() === true)) {
-            console.log('Player ' + playerTurn + 'won!');
-            return true;
-        } else {
-            return false;
-        }
-    });
+    };
+
+    function checkForWin(){
+     if (horizontalWin() || verticalWin() || diagonalWin()) {
+       $('#announce-winner').text('Player ' + playerTurn + ' Wins!');
+       return true;
+     }
+     else {
+       return false;
+     }
+   }
+
 });
