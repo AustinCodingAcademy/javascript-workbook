6b477adb957b3a04f2408d603d20520722d09862
Aaron <aaronaustintx@gmail.com>
2016-09-19 19:52:10 -0500

    Commit to get  version #2 of App/08jquery-towers-of-hanoi Eight in

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -2,17 +2,41 @@
 
 $(document).ready(function() {
   // Put app logic here
-  var block = null;
+  var $block = null;
+  var lastBlock = $(this).children().last().data('block');
 
   $('[data-stack]').on('click', function(){
   //   var $dataStack = $('[data-stack]');
-
-      if(!block){
-      block = $(this).children().last().detach();
+      // grabs block
+      if(!$block){
+        $block = $(this).children().last().detach();
     } else {
-      console.log(block);
-      $(this).append(block);
-        block = null;
+
+        var $children = $(this).children();
+        var blockValue = $block.data('block');
+
+
+        if($children.length === 0 || $children.last().data('block') > blockValue){
+            $(this).append($block);
+            $block = null;
+        } else {
+          console.log("invalid move!!" );
+        }
+
+
+
+
+      // if(lastBlock.data('block') >= block.data('block')){
+      //   console.log("invalid move!!");
+      // }
+    //  lastBlock = $(this).children().last().data('block');
+    //  $lastBlock = $lastStack.data('block');
+      // console.log(lastBlock);
+
+// this.lastblock >= l
+
+
+    //  }
     }
   });
 
