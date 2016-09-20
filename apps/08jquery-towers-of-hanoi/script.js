da6861469c5d1874e2bf0d895fa4e9606983d8e2
zacacollier <zacacollier@gmail.com>
2016-09-19 21:44:52 -0500

    GUI Towers of Hanoi complete and all tests passing

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -1,5 +1,34 @@
 'use strict';
 
-$(document).ready(function() {
-  // Put app logic here
+var $block = null;
+function checkForWin()  {
+  //maximum size is 4
+return ( $("[data-stack='2']").children().length >= 4 || $("[data-stack='3']").children().length >= 4) ;
 }
+
+$(document).ready( function() {
+
+  $('[data-stack]').click( function (){
+    var $children = $(this).children();
+    var $last = $children.last();
+
+    if  ($block === null) {
+      //Pop the block off the stack and save it in memory
+      //(**IF that memory space is 'null')
+      $block = $last.detach();
+    }
+    else {
+      if ($last.data('block') <= $block.data('block'))  {
+        $('div#announce-game-won').text('(☞ ಥ益ಥ)☞ invalid move buddy').fadeOut(2000);
+      }
+      else {
+        $(this).append($block);
+        $('div#announce-game-won').text('');
+        $block = null;
+      }
+    }
+    if (checkForWin()) {
+      $('div#announce-game-won').text('You Won!');
+    }
+  });
+});
