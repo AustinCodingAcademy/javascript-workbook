3366d0e8268a45cde0cde35b0704e6d65bf0e3a8
Matthew Holman <mateoholman@gmail.com>
2016-09-19 19:56:49 -0500

    Finsihed App 9 w/o the bonuses

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -1,5 +1,22 @@
 'use strict';
 
 $(document).ready(function() {
-    // App logic goes here
-});
+    //SPEC 4: Make the list sortable
+    $('ul').sortable();
+
+    //Bonus Spec 5: Add a check box to each list item.
+    
+
+    $('form').submit(function (event) { // target the form, and put a submit event listener on it. We can capture and pass in the event for us to use
+      event.preventDefault(); // this tells the browser to "prevent" the "default" "event" from occuring (sending and refreshing)
+      console.log( $(this) ); // let's log out our "this" context (our form element) as a jQuery object
+
+      //SPEC 2:
+      var todoText = $(this).find('#todo').val();
+
+      //SPEC 3:
+      $('#todo-list').append('<li>' + todoText + '</li>');
+
+    });//!('form').submit()
+
+});//!(document).ready()
