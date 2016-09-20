39edc1502bc8d6928355bef60ea453d101da9ba0
Charlie Sears <Charlie Sears>
2016-09-20 09:34:44 -0500

    I completed the Towers of Hanoi App but the last test is not passing. I will push an updated app soon.

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -2,4 +2,26 @@
 
 $(document).ready(function() {
   // Put app logic here
-}
+  var block = null
+  $( "[data-stack]" ).click(function() {
+
+      if ( !block ) {
+        block = $(this).children().last().detach();
+      }
+      else {
+          if ($(this).children().length === 0 || block.data("block") < $(this).children().last().data("block") ) {
+            $(this).append(block);
+            block = null;
+          }
+      function checkForWin() {
+        if ($( "[data-stack]").each([2]).length === 4 || $( "[data-stack]").each([3]).length === 4 )  {
+          $( "div#announce-game-won" ).text('You Won!')
+        }
+      }
+      }
+
+});
+//($("[data-stack]= 2").length === 4 || $("[data-stack]= 3").length === 4 )
+
+
+});
