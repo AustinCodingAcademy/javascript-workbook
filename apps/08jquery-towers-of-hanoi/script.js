22a931020cd9d16d0fd3d9f5a794708a0fe0a378
Kimberly Cook <kcook@zenoss.com>
2016-09-19 20:09:36 -0500

    App 8 complete.

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -2,20 +2,34 @@
 
 $(document).ready(function() {
 
-var $block = null;
+  function checkForWin() {
+    if ($('[data-stack="2"]').children().length === 4 || $('[data-stack="3"]').children().length === 4) {
+      $('#announce-game-won').text('You won!');
+    }
+  }
+
+  var $block = null;
 
   $('[data-stack]').click(function() {
+
+    var $children = $(this).children();
+
     if ($block === null) {
-      $block = $(this).children().last().detach();
+      $block = $children.last().detach();
     }
     else {
-      $(this).append($block);
-      $block = null;
+
+      var blockValue = $block.data('block');
+
+      if ($children.length === 0 || $children.last().data('block') > blockValue) {
+        $(this).append($block);
+        $block = null;
+      }
+      else {
+        console.log("That's not a legal move. Please try again.");
+      }
     }
+    checkForWin();
   });
 
 });
-
-
-
-//for next step remember Piero's notes in Slack and isLegal function from original ToH app
