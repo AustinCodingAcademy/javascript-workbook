5a80b70fbbfdcb0d4bf4b42b427107fc925f5777
zacacollier <zacacollier@gmail.com>
2016-09-20 00:02:44 -0500

    Added check boxes with jQueryUI .checkboxradio()

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -1,15 +1,28 @@
 'use strict';
 
 $(document).ready(function() {
-    function li(input) {
+    //add radio to pre-existing items
+    $('li').prepend("<label><input type='radio'></label>")
+    $('input[type="radio"]').checkboxradio()
+    $('#todo').attr('placeholder', "Add tasks here...")
+    function liAdd(input) {
         return "<li>" + input + "</li>";
     }
+
     $('form').on('submit', function(event) {
         event.preventDefault();
+
+        //Select input
         var toDoText = $(this).find('input[type="text"]').val();
         //Void the input form
         $(this).find('input[type="text"]').val('');
-        $('#todo-list').append(li(toDoText));
+
+        $('#todo-list').append(liAdd(toDoText));
+        //add radio to newly-created item
+        $('#todo-list').children().last().prepend("<label><input type='radio'></label>")
+        $('input[type="radio"]').checkboxradio();
+        // $('#todo-list').children().last().fadeOut(1500);
+        
     });
     $('#todo-list').sortable();
 });
