11c81b47a7afd9f5c4f9fb5f63af56a6c586ed3a
Aaron <aaronaustintx@gmail.com>
2016-09-21 20:33:26 -0500

    Commit to get  version #1 of 09 app todolist09js

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -2,4 +2,16 @@
 
 $(document).ready(function() {
     // App logic goes here
+
+    $('form').submit(function (event) {
+       event.preventDefault();
+       var todoText = $(this).find('input[type="text"]').val('#todo');
+
+       console.log( $(this) );
+       $('#todo').append('<li>' + todoText + '</li>');
+       $(event.currentTarget).find('input[type="text"]'.val(''));
+
+
+      });
+        $('ul').sortable();
 });
