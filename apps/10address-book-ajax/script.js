1a80c3053260140d0b3959ff4ba5a6e0f717ee2a
Aaron <aaronaustintx@gmail.com>
2016-09-21 21:07:06 -0500

    Commit to get  version #1 of 10 app 10address-book-ajax scriptjs

--- apps/10address-book-ajax/script.js
+++ apps/10address-book-ajax/script.js
@@ -2,4 +2,13 @@
 
 $(document).ready(function() {
     // You code here
+    $.ajax('https://reqres-api.herokuapp.com/api/users', {
+      success: function(users) {
+        for (var i = 0; i < users.length; i++) {
+        //  var str = "<li> + 'https://reqres-api.herokuapp.com/api/users/' + + </li>";
+          console.log(users[i]['name']);
+        }
+      }
+    });
+
 });
