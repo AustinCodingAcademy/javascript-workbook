48ae6d52bae5c0ea4c36ff3be7e25d293be5a2eb
Giovanna <g.c1@me.com>
2016-09-23 10:09:55 -0500

    08 App Complete

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -1,5 +1,39 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
-}
+
+var block = null;
+
+$("[data-stack]").on("click", function() {
+  if (block === null) {
+    block = $(this).children().last().detach();
+    console.log("1st Down!");
+  }
+  else {
+
+var blockValue = Number(block.attr("data-block"));
+var move2Value = Number($(this).children().last().attr("data-block"));
+      if (blockValue < move2Value || !move2Value) {
+        $(this).append(block);
+        block = null;
+        console.log("2nd Down!");
+      }
+      else {
+        alert("Not so fast! Can't put bigger blocks on top of smaller ones.");
+      };
+      checkForWin();
+      console.log("3rd Down!");
+  };
+
+
+});
+
+function checkForWin() {
+  if ($('[data-stack="2"]').children().length === 4 || $('[data-stack="3"]').children().length === 4)
+  {
+    $("div#announce-game-won").text("You Won!");
+  }
+};
+
+
+}); /*Don't Delete Me* *Closing tags for the whole app.*/
