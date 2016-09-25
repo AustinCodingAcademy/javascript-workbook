51bfbc86243c57e77893c6cfbd8e79108de698e5
madelineolsen <mloveolsen@gmail.com>
2016-09-25 16:55:28 -0500

    Lesson 9 to-do list app, finished

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -2,4 +2,13 @@
 
 $(document).ready(function() {
     // App logic goes here
+    $('form').submit(function (event) {
+            event.preventDefault();
+            console.log( $(this) );
+            var todoText = $(this).find('input[type="text"]').val();
+            $('#todo-list').append('<li>' + todoText + '</li>');
+            $(this).find('input[type="text"]').val('')
+            $('#todo-list').sortable();
+          });
+    //End here
 });
