3d700dd6c2c90c93447842456da2de0921e3d7a1
DMcCal79 <dave_mccalmont@hotmail.com>
2016-09-25 13:11:45 -0500

    Completed JQuery To-do List App

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -1,5 +1,13 @@
 'use strict';
 
 $(document).ready(function() {
-    // App logic goes here
+    $('form').submit(function (event) {
+      event.preventDefault();
+      console.log($(this));
+      var todoText = $(this).find('input[type="text"]').val()
+      $('#todo-list').append('<li>' + '<input type="checkbox">' + todoText + '</li>') /*Added checkbox to each item listed*/
+      $(this).find('input[type="text"]').val('');
+      $('ul').sortable();
+      $('ul').disableSelection();
+    })
 });
