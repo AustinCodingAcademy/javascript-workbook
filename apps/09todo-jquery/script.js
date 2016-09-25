94efe946c06ac48cb24db75d93c8873de1cedcc6
Stephrod <stephanie_rodriguez5@yahoo.com>
2016-09-25 14:27:06 -0500

    09 Todo list

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -2,4 +2,10 @@
 
 $(document).ready(function() {
     // App logic goes here
+    $('form').submit(function(event) {
+      event.preventDefault();
+      console.log(this);
+      var toDoText = $(this).find('#todo').val();
+      $('#todo-list').append('<li>' + toDoText + '<li>').sortable();
+    });
 });
