09884853f29bc964baab1efb44f3482759ea1964
Colin Clarkson <colinsclarkson@gmail.com>
2016-09-25 13:37:37 -0500

    completed

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -2,4 +2,12 @@
 
 $(document).ready(function() {
     // App logic goes here
+    $('form').submit(function (event) {
+      event.preventDefault();
+      var todoText = $(this).find('#todo').val();
+      $('#todo-list').append('<li>' + todoText + '</li>');
+      $(this).find('#todo').val('');
+    });
+    $('#todo-list').sortable();
+
 });
