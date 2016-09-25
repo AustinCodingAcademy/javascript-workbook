ce5c7a1d61156d1ad9f24f721084dcc8ff631757
madelineolsen <mloveolsen@gmail.com>
2016-09-25 17:08:47 -0500

    Lesson 9 App To Do list, added missing semi-colon, finished

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -1,14 +1,12 @@
 'use strict';
 
 $(document).ready(function() {
-    // App logic goes here
-    $('form').submit(function (event) {
-            event.preventDefault();
-            console.log( $(this) );
-            var todoText = $(this).find('input[type="text"]').val();
-            $('#todo-list').append('<li>' + todoText + '</li>');
-            $(this).find('input[type="text"]').val('')
-            $('#todo-list').sortable();
+  $('form').submit(function(event){
+      event.preventDefault();
+      console.log($(this));
+      var todoText = $(this).find('input[type="text"]').val();
+      $('#todo-list').append('<li>' + todoText + '</li>');
+      $(this).find('input[type="text"]').val('');
+      $('#todo-list').sortable();
           });
-    //End here
 });
