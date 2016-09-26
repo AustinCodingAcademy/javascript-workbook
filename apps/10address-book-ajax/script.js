e4e44a0da6c02ea8563e279dc84665de40d38a10
Gabe Amaya <GabeAmaya2@gmail.com>
2016-09-26 16:37:30 -0500

    partial app 10 complete, doesn't do anything. Need help.

--- apps/10address-book-ajax/script.js
+++ apps/10address-book-ajax/script.js
@@ -1,5 +1,22 @@
 'use strict';
 
 $(document).ready(function() {
-    // You code here
+  $.ajax('https://reqres-api.herokuapp.com/api/users', {
+    success: function(user) {
+      for (var i = 0; i < user.length; i++) {
+        var $str = $('<tr><a href="#" data-id="' + user[i].id + '">' + user[i].id + '</a></tr>');
+        console.log($str);
+        $('tbody').append($str);
+
+        $str.click(function(event) {
+          event.preventDefault();
+          $.ajax('https://reqres-api.herokuapp.com/api/users/' + $(this).find('a').data('id'), {
+            success: function(response) {
+              $('div#details').html(response['name']);
+            }
+          })
+        });
+      }
+    }
+  });
 });
