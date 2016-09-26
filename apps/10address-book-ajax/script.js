94aa7687daf2324b3b41f2b2445997432e7041ba
Aaron <aaronaustintx@gmail.com>
2016-09-26 17:50:09 -0500

    Commit to get  version #1 of App 10 AddressBook-ajaxjs

--- apps/10address-book-ajax/script.js
+++ apps/10address-book-ajax/script.js
@@ -4,10 +4,17 @@ $(document).ready(function() {
     // You code here
     $.ajax('https://reqres-api.herokuapp.com/api/users', {
       success: function(users) {
-        for (var i = 0; i < users.length; i++) {
+        // for (var i = 0; i < users.length; i++) {
+        users.forEach(function(user){
+           var str = "<li> + 'https://reqres-api.herokuapp.com/api/users/' + 'id' + </li>";
+
+
+
+        })
+
         //  var str = "<li> + 'https://reqres-api.herokuapp.com/api/users/' + + </li>";
           console.log(users[i]['name']);
-        }
+      //  }
       }
     });
 
