24623cf7f06621503194db9b6015d355356b384b
Salvador <salvadorrod13@gmail.com>
2016-09-26 19:48:31 -0500

    Lesson 10 Address-book-ajax Done

--- apps/10address-book-ajax/script.js
+++ apps/10address-book-ajax/script.js
@@ -4,43 +4,46 @@ $(document).ready(function() {
 
   $.ajax('https://reqres-api.herokuapp.com/api/users', {
     success: function(users) {
-      users.forEach(function(user){
+
+      users.forEach(function(response){
+        var id = response.id.toString();
+        // console.log(id);
 
         var $tbody = $("tbody");
-        var $str = $('<tr><td>'+user.id+'</td>'+
-                     '<td>'+user.first_name+'</td>'+
-                     '<td>'+user.last_name+'</td>'+
-                     '<td><a href="#" data-id="'+user.id+'">'+'view'+user.id+'</a></td><tr>' );
-
+        var $str = $('<tr><td>'+response.id+'</td>'+
+                     '<td>'+response.first_name+'</td>'+
+                     '<td>'+response.last_name+'</td>'+
+                     '<td><a href="#" data-id="'+response.id+'">'+'view'+response.id+'</a></td><tr>' );
         $tbody.append($str);
-
-        var $children = $tbody.children();
-        var $lastBlock = $children.last();
-
-        $lastBlock.find("a").on("click", function() {
-          var url = 'https://reqres-api.herokuapp.com/api/users/';
-
-          var id = $(this).data('id');
-          $(this).find("href").val(url+"/"+id);
-
-          $.ajax(url, {
-           success: function(response) {
-             var $str2  = $('<div id="'+id+'"'+
-                          "<h3>first last</h3>"+
-                          "<h4>student</h4>"+
-                          "<p>5551234567</p>"+
-                          "<p> 123 Whatever Street, Austin, TX 78701 </p>"+
-                          "<img src='https://s3.amazonaws.com/uifaces/faces/twitter/marcoramires/128.jpg'>"+
-                          "</div>");
-
-           }     // success
-          } ) ;  // Ajax
-
-        } ); // Click
-
       }); // for each users
+
+      $("td > a").on("click", function(event) {
+        event.preventDefault();
+        var url = 'https://reqres-api.herokuapp.com/api/users/';
+        // var id =  $(this).data('id');
+        url= url + $(this).data('id');
+
+        // $(this).find("href").val(url);
+
+        $.ajax(url, {
+         success: function(user) {
+           var lstr  = '<div id="'+user.id+'">'+
+                        "<h3>"+user.first_name+" "+user.last_name+"</h3>"+
+                        "<h4>"+user.occupation+"</h4>"+
+                        "<p>"+user.number+"</p>"+
+                        "<p>"+user.address+"</p>"+
+                        "<img src='"+user.avatar+"'/>"+
+                        "</div>";
+            $('div#details').html(lstr);
+         }     // success
+        } ) ;  // Ajax
+
+      } ); // Click
 
     }  // success
   });  // Ajax users
+
+
+
 
 });
