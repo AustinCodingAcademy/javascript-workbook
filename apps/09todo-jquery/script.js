955c19c9d99865a7c504528789d235a6a70dcbaf
Charlie Sears <Charlie Sears>
2016-09-26 21:03:36 -0500

    This is my Todo app. I intend on doing some of the bonus specs but I have not yet done so.

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -2,4 +2,15 @@
 
 $(document).ready(function() {
     // App logic goes here
+    $('#todo-list').sortable();
+    $('form').submit(function (event) {
+      // I got rid of .on('submit', function(event) ... which I got from Kevins code
+      event.preventDefault();
+      var todoText = $(this).find('#todo').val();
+      var newListItem = '<li>' + todoText + '</li>';
+      $('#todo-list').append(newListItem);
+
+    }
+  )
+
 });
