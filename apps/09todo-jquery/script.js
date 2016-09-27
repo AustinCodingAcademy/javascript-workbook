930bcf418ac4718323b5113bff31c4b3ba117804
nicolemichelle88 <daugereaux@gmail.com>
2016-09-26 20:47:44 -0500

    todo

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -1,5 +1,14 @@
 'use strict';
 
 $(document).ready(function() {
-    // App logic goes here
+  $('form').submit(function (event) { // target the form, and put a submit event listener on it. We can capture and pass in the event for us to use
+         event.preventDefault();
+
+         var todoText = $(this).find('#todo').val();
+
+         $('#todo-list').append('<li>' + todoText + '</li>');
+
+         $('ul').sortable();
+         $('input[type="text"]').val('');
+});
 });
