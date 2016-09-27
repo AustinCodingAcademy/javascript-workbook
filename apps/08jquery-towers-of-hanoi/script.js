3e4f3da0a6acb5d1648a900769656b97d3bf9a5c
Sandra Killebrew <sandra@nextconsulting-llc.com>
2016-09-26 20:03:28 -0500

    less 08 app complete

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -5,16 +5,19 @@ $(document).ready(function() {
   //Put app logic here
   function checkForWin() {
     console.log("checking for win");
-    //each is running three times
-    $('[data-stack]').each(function(){
+    //$('[data-stack]').each(function()
       //console.log($('[data-stack]').children().length + " how many children");
       //$('[data-stack]').children().length is returning 4
-      if($('[data-stack]').children().length === 4) {
-        $("#announce-game-won").text('You Won!');
-        console.log("I won");
-      }
-    });
-  };
+      //my jquery object is incorrect
+      //or my each is wrong
+      //$(this).data('stack')
+      //  if($('[data-stack]').children().length === 4)
+      //($(this).data('stack') === 3).children().length === 4)
+      if ($('[data-stack="2"]').children().length === 4 || $('[data-stack="3"]').children().length === 4) {
+          $("#announce-game-won").text('You Won!');
+          console.log("I won");
+        }
+    };
   //set variable block equal to nothing.  It will store the blocks you want to move.
 
   var block = null;
