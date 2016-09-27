edbb6377ea00eee638779dece2c863fa1d8f151c
Matthew Holman <mateoholman@gmail.com>
2016-09-26 19:48:56 -0500

    Finished App 10

--- apps/10address-book-ajax/script.js
+++ apps/10address-book-ajax/script.js
@@ -1,5 +1,46 @@
 'use strict';
 
 $(document).ready(function() {
-    // You code here
-});
+  $.ajax('https://reqres-api.herokuapp.com/api/users', {
+    //Spec 2:
+    success: function(response) {
+      response.forEach(function(user){
+        var str =
+          '<tr>' +
+            '<td>' + user.id + '</td>' +
+            '<td>' + user.first_name + '</td>' +
+            '<td>' + user.last_name + '</td>' +
+            '<td>' + '<a href="#"' + 'data-id=' + user.id + '>' + 'view' + '</a>' + '</td>' +
+          '</tr>';
+        //Append the string to the body below here:
+
+        var $tableRow = $(str);
+
+        $('tbody').append($tableRow);
+
+        //Spec 3:
+        $('a', $tableRow).on('click', function(){
+          event.preventDefault();
+          var url = 'https://reqres-api.herokuapp.com/api/users/' + $(this).data('id');
+
+          $.ajax(url, {
+            success: function(response) {
+              var user = response;
+              var divStr =
+              '<h3>' + user.first_name + ' ' + user.last_name + '</h3>'+
+              '<h4>' + user.occupation + '</h4>' +
+              '<p>' + user.phone + '</p>' +
+              '<p>' + user.address + '</p>' +
+              '<img src=' + user.avatar + '>';
+
+              $('#details').html(divStr);
+            }
+          });//!(ajax)
+        });//!('a').click()
+      })
+    }
+  });//!(ajax)
+
+
+
+});//!(document).ready()
