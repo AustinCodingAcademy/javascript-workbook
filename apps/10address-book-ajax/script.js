9ef2f02f8546b44808a872a29b143b4d1958629a
Manny Hagman <manny.hagman@gmail.com>
2016-09-28 16:22:30 -0500

    app 10 address book complete

--- apps/10address-book-ajax/script.js
+++ apps/10address-book-ajax/script.js
@@ -1,5 +1,34 @@
 'use strict';
 
 $(document).ready(function() {
-    // You code here
-});
+    $.ajax('https://reqres-api.herokuapp.com/api/users', {
+		success: function(users) {
+			users.forEach(function(user) {
+				var $tr = $('<tr></tr>'); // parent tr // we have to create something before we can append it
+				$tr.append('<td>' + user.id + '</td>');
+				$tr.append('<td>' + user.first_name + '</td>');
+				$tr.append('<td>' + user.last_name + '</td>');
+				var $a = $('<td><a href="/" data-id="' + user.id + '">view</a></td>');
+				$tr.append($a);
+				$('tbody').append($tr);
+			}); // end of loop
+
+
+			$('a').on('click', function(event) {
+				event.preventDefault();
+				var id = $(this).data('id');
+				$.ajax('https://reqres-api.herokuapp.com/api/users/' + id, {
+					success: function(user) {
+						var $details = $('#details');
+						$details.empty();
+						$details.append('<h3>' + user.first_name + ' ' + user.last_name + '<h3>');
+						$details.append('<h4>' + user.occupation + '</h4>');
+						$details.append('<p>' + user.phone + '</p>');
+						$details.append('<p>' + user.address + '</p>');
+						$details.append('<img src="' + user.avatar + '"/>');
+					} // end of success call back
+				}); // end of second ajax call
+			}); // end of on click
+		}
+	})
+}); // end of document ready
