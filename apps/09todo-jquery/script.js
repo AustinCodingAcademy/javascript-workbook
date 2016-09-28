dd0a347dd2cc5ac9e7b96fa771e4e0030941a604
cjclay <Outlier@carlas-mbp.attlocal.net>
2016-09-28 13:15:32 -0500

    ToDo-jquery

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -1,5 +1,15 @@
 'use strict';
 
 $(document).ready(function() {
-    // App logic goes here
+  $('form').submit(function (event) {
+    event.preventDefault();
+    var todoText = $(this).find('#todo').val();
+    $('#todo-list').append('<li><input type="checkbox">' + todoText + ' <button type="button">Delete</button></li>');
+    $('button').click(function() {
+      $(this).parent().remove();
+    });
+    $('#todo').val('');
+    $('#todo').attr("placeholder","Enter some text...").placeholder();
+  });
+  $('#todo-list').sortable();
 });
