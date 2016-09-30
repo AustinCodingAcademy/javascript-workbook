86ffb8f88260497aee7866cb848f6b54b4ec4bc4
cjclay <Outlier@carlas-mbp.attlocal.net>
2016-09-30 01:28:53 -0500

    Address Book-AJAX

--- apps/10address-book-ajax/script.js
+++ apps/10address-book-ajax/script.js
@@ -1,5 +1,36 @@
 'use strict';
 
 $(document).ready(function() {
-    // You code here
-});
+
+  $.ajax('https://reqres-api.herokuapp.com/api/users',{
+
+    success: function(users) {
+
+      users.forEach(function(user){
+        var str = '<tr>' + '<td>' + user.id + '</td>' +
+                  '<td>' + user.first_name + '</td>' +
+                  '<td>' + user.last_name + '</td>' +
+                  '<td><a href="#" data-id="'+ user.id +
+                  '">view</a></td>'
+        $('tbody').append(str);
+      })
+
+      $('a').on('click',function(event) {
+        event.preventDefault();
+        var url = 'https://reqres-api.herokuapp.com/api/users/';
+        url += $(this).data('id');
+        console.log(this);
+        console.log(url);
+        $.ajax(url, {
+          success: function(user) {
+            var view = '<div id="'+user.id+'">' +
+                    "<h3>" + user.first_name + " " + user.last_name + "</h3>" +
+                    "<h4>" + user.occupation + "</h4>" +
+                    "<p>" + user.phone + "</p>" +
+                    "<p>" + user.address + "</p>" +
+                    "<img src='" + user.avatar + "'/>" + "</div>";
+            $('#details').html(view);
+          }});
+        })
+      }})
+  });
