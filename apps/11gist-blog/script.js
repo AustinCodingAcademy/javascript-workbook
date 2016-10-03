325de7cdefd107ad4cfb5cc748746d572d727782
madelineolsen <mloveolsen@gmail.com>
2016-10-03 16:23:55 -0500

    Final Project gist blog, having trouble making it connect to Github

--- apps/11gist-blog/script.js
+++ apps/11gist-blog/script.js
@@ -1,5 +1,24 @@
 'use strict';
 
 $(document).ready(function() {
-    // You code here
+    $.ajax('http://localhost:8080/apps/11gist-blog/api/gists.json', {
+      success: function(gists) {
+        gists.forEach(function(gist) {
+          if (gist.description.indexOf('#post') > -1) {
+            var $a = $('<a href=" ' + gist.url + ' ">' + gist.description.slice(6) + '</a>');
+            $('#posts').append($('<li></li>').append($a));
+            $a.on('click', function(event) {
+              event.preventDefault();
+              var href = $(this).attr('href');
+              console.log(href);
+              $.ajax(href,{
+                success:function(gist) {
+                  $('#post').html(marked(gist['files']['post.md']['content']));
+                }
+              })
+            });
+          }
+        });
+      }
+    });
 });
