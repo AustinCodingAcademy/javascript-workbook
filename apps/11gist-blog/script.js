862b18606e955d8abadd057f821b358f8f321745
idlemindss <kristinavan23@yahoo.com>
2016-10-02 19:37:17 -0500

    completed specs 0 - 3.1

--- apps/11gist-blog/script.js
+++ apps/11gist-blog/script.js
@@ -1,5 +1,34 @@
 'use strict';
 
 $(document).ready(function() {
-    // You code here
+    $.ajax('http://localhost:8080/apps//11gist-blog/api/gists.json', {
+      success: function(gists) {
+        gists.forEach(function(gist) {
+          if (gist.description.indexOf('#post') > -1) {
+          // console.log(gist.url);
+          // console.log(gist.description);
+          var $a = $('<a href="' + gist.url + '">' + gist.description.slice(6) + '</a>');
+          $('#posts').append($('<li></li>').append($a));
+          $a.on('click', function(event) {
+            event.preventDefault();
+            var href = $(this).attr('href');
+            console.log(href);
+            $.ajax(href, {
+              success: function(gist) {
+                console.log(gist);
+                $('#post').html(marked(gist['files']['post.md']['content']));
+              }
+            })
+            $.ajax(gist.comments_url, {
+              success: function(comments) {
+                console.log(gist.comments_url);
+                //
+              }
+            })
+          });
+          }
+        });
+      }
+
+    });
 });
