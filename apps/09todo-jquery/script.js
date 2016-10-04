47955d506d46ac46930455f4cb7fed03db8e08cb
Giovanna <g.c1@me.com>
2016-10-04 11:26:39 -0500

    09Todo App Complete

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -1,22 +1,20 @@
 'use strict';
 
 $(document).ready(function() {
-  $("form").on("submit", function (event) {
-    event.preventDefault();
 
-    var todoText = $(this).find('#todo').val();
-    var checkBox =
-      $('#checks').on("click", function(){
-        $(this).prop().change();
-    }); //Closes .click() var function
+    $("form").on("submit", function (event) {
+        event.preventDefault();
+            var todoText = $(this).find('#todo').val();
+            var checkBox =
+            $('#checks').on("click", function(){
+                $(this).prop().change();
+            }); //Closes .click() var function
 
-    $('#todo-list').append($('<li>' + todoText + '</li>')).sortable();
-      console.log("Append & sortable");
-    $(this).find('#todo').val('');
-      console.log("find & value");
-
-    $('checkBox');
-      console.log("checkbox");
-
-  }); //Closes .submit()
+            $('#todo-list').append($('<li>' + todoText + '</li>')).sortable();
+                console.log("Append & sortable");
+            $(this).find('#todo').val('');
+                console.log("find & value");
+            $('checkBox');
+                console.log("checkbox");
+        }); //Closes .submit()
 }); // Closes .ready()
