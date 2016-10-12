369660d6d146c7ce4132a24d0371f9acc6329679
Kendra Moore <kendra.j.moore3443@gmail.com>
2016-10-12 18:14:24 -0500

    first

--- practice-problems/lesson1.js
+++ practice-problems/lesson1.js
@@ -2,44 +2,44 @@
 
 //Answer:
 
-
+Booleans
 
 
 //2. What is a null value? Provide one example.
 
-//Answer:
+//Answer: Serves as a placeholder to create varible
 
 
 
 
 //3. List and describe 3 more data types used in JS.
 
-//Answer:
+//Answer: Undefined, Null, String
 
 
 //4. Why do we use comments in JS?
 
-//Answer:
+//Answer: Notes to programmer
 
 
 //5. Why do we use 'strict mode'?
 
-//Answer:
+//Answer: Restrict javascript functions that were created by accidents
 
 
 //6. Write the code to log the output "Hello World".
 
-//Answer:
+//Answer: console.log("Hello World");
 
 
 //7. What is the difference between the "and" and the "or" operators? Explain why you would use each of them.
 
-//Answer:
+//Answer: For "or" it will determine if at least one side is true or false while and will determine if both sides are true or false
 
 
 //8. Explain truthiness and falsiness in your own words. Provide an example for each.
 
-//Answer:
+//Answer:Falsey in javascript is a value that is NaN, undefines, 0, null, or false
 
 
 
@@ -53,70 +53,70 @@
 
 //9. console.log(true && false);
 
-//Answer:
+//Answer: false
 
 
 
 
 //10. console.log(false && true);
 
-//Answer:
+//Answer: false
 
 
 
 
 //11. console.log(true && true && false);
 
-//Answer:
+//Answer: false
 
 
 
 
 //12. console.log(true || false);
 
-//Answer:
+//Answer: true
 
 
 
 
 //13. console.log(false || true);
 
-//Answer:
+//Answer: true
 
 
 
 
 //14. console.log(true || true || false);
 
-//Answer:
+//Answer: true
 
 
 
 
 //15. console.log(!true);
 
-//Answer:
+//Answer: false
 
 
 
 
 //16. console.log(!undefined);
 
-//Answer:
+//Answer: true
 
 
 
 
 //17. console.log(!!"Hello");
 
-//Answer:
+//Answer: true
 
 
 
 
 //18. console.log(!!null);
 
-//Answer:
+//Answer: false
 
 
 
@@ -133,7 +133,7 @@
 
 //20. What’s the difference between == and ===?
 
-//Answer:
+//Answer: == means that two things are equal while === is comparing
 
 
 
@@ -147,4 +147,5 @@
 
 //22. What does (7 - 4 && 6 + 3) || (7 < 5 || 8 > 4) evaluate to? Explain how you solved your answer.
 
-//Answer:
+
+//Answer: 9 used node in the terminal
