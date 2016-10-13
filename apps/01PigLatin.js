db0fdc2d0776f500c37ef460652e837555a1c03e
jerryjoebenson <jerryjoebenson@gmail.com>
2016-10-12 21:07:29 -0500

    Lesson 2 In Class Project

--- apps/01PigLatin.js
+++ apps/01PigLatin.js
@@ -9,9 +9,39 @@ var rl = readline.createInterface({
 
 
 function pigLatin(word) {
+  word=word.toLowerCase();
+var vowelIndex = -1;
 
-  // Your code here
+  if ( ( word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
+      vowelIndex = word.indexOf('a');
+  }
 
+  if ( ( word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex ) || vowelIndex === -1 ) {
+      vowelIndex = word.indexOf('e');
+  }
+
+  if ( ( word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex ) || vowelIndex === -1 ) {
+      vowelIndex = word.indexOf('i');
+  }
+
+  if ( ( word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex ) || vowelIndex === -1 ) {
+      vowelIndex = word.indexOf('o');
+  }
+
+  if ( ( word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex ) || vowelIndex === -1 ) {
+        vowelIndex = word.indexOf('u');
+    }
+ if ( vowelIndex === 0){
+  return word + 'yay';
+}
+ else {
+
+    var firstPart = word.slice(0, vowelIndex);
+    var restWord = word.slice(vowelIndex, word.length);
+    return restWord + firstPart + 'ay';
+
+
+}
 }
 
 
