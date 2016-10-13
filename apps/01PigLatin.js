79690ced753947809e87ba0d3c35362ad2659303
Greg Pfaff <greg.pfaff@razorfish.com>
2016-10-12 21:07:36 -0500

    adding solve for Pig Latin Problem

--- apps/01PigLatin.js
+++ apps/01PigLatin.js
@@ -10,8 +10,40 @@ var rl = readline.createInterface({
 
 function pigLatin(word) {
 
-  // Your code here
+  var vowelIndex = -1;
 
+  if ( ( word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
+      vowelIndex = word.indexOf('a');
+  }
+
+  if ( ( word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex ) || vowelIndex === -1 ) {
+      vowelIndex = word.indexOf('e');
+  }
+
+  if ( ( word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex ) || vowelIndex === -1 ) {
+      vowelIndex = word.indexOf('i');
+  }
+  if ( ( word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex ) || vowelIndex === -1 ) {
+      vowelIndex = word.indexOf('o');
+  }
+  if ( ( word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex ) || vowelIndex === -1 ) {
+      vowelIndex = word.indexOf('u');
+  }
+
+  var firstPart = word.slice(0, vowelIndex);
+  var restWord = word.slice(vowelIndex, word.length)
+
+  if(vowelIndex === -1) {
+    return (word.substring(0, word.length) + 'ay').toLowerCase();
+  }
+
+  if(vowelIndex === 0){
+    return (word + "yay").toLowerCase();
+  }
+
+  if(vowelIndex) {
+    return (restWord + firstPart + 'ay').toLowerCase(); 
+  }
 }
 
 
