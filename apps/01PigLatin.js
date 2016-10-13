922da824765ba34871ebe36aa263dd57c1cc0fa8
dmencha <dmencha@rMB.local>
2016-10-12 21:06:22 -0500

    Class 2 In Class - Pig Latin

--- apps/01PigLatin.js
+++ apps/01PigLatin.js
@@ -12,6 +12,45 @@ function pigLatin(word) {
 
   // Your code here
 
+word = word.toLowerCase();
+
+var vowelIndex = -1; // Set it to assume there are no vowels
+
+if ( ( word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('a');
+}
+
+if ( ( word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('e');
+}
+
+if ( ( word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('i');
+}
+
+if ( ( word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('o');
+}
+
+if ( ( word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('u');
+}
+
+if ( ( word.indexOf('y') > -1 && word.indexOf('y') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('y');
+}
+
+var firstPart = word.slice(0, vowelIndex);
+var restWord = word.slice(vowelIndex, word.length);
+
+if (vowelIndex === 0) {
+  return word + 'yay';
+}
+else {
+  return restWord + firstPart + 'ay';
+}
+
+
 }
 
 
