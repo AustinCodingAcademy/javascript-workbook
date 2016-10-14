a477ae2d6f92d5397aa593c3c0736e87cb147cf9
Jonathan Soape <jonathansoape@gmail.com>
2016-10-13 20:56:37 -0500

    Lesson 1 Pig Latin

--- apps/01PigLatin.js
+++ apps/01PigLatin.js
@@ -11,13 +11,55 @@ var rl = readline.createInterface({
 function pigLatin(word) {
 
   // Your code here
+  word = word.toLowerCase();
+  var firstLetter = word[0];
+
+
+  //  Step 2.2 - Find first vowel
+  var vowelIndex = -1; // Set it to assume there are no vowels
+
+  if ((word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex) || vowelIndex === -1) {
+    vowelIndex = word.indexOf('a');
+
+  }
+
+  if ((word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex) || vowelIndex === -1) {
+    vowelIndex = word.indexOf('e');
+  }
+
+  if ((word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex) || vowelIndex === -1) {
+    vowelIndex = word.indexOf('i');
+  }
+
+  if ((word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex) || vowelIndex === -1) {
+    vowelIndex = word.indexOf('o');
+  }
+
+  if ((word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex) || vowelIndex === -1) {
+    vowelIndex = word.indexOf('u');
+  }
+
+  var firstPart = word.slice(0, vowelIndex);
+  var restWord = word.slice(vowelIndex, word.length);
+
+  if (vowelIndex === 0) {
+    return word + 'yay';
+  }
+
+
+  return restWord + firstPart + 'ay';
+
+
+  // return word.replace(firstLetter, '') + firstLetter + 'ay';
+
+
 
 }
 
 
 function getPrompt() {
   rl.question('word ', (answer) => {
-    console.log( pigLatin(answer) );
+    console.log(pigLatin(answer));
     getPrompt();
   });
 }
@@ -26,20 +68,20 @@ function getPrompt() {
 
 if (typeof describe === 'function') {
 
-  describe('#pigLatin()', function () {
-    it('should translate a simple word', function () {
+  describe('#pigLatin()', function() {
+    it('should translate a simple word', function() {
       assert.equal(pigLatin('car'), 'arcay');
       assert.equal(pigLatin('dog'), 'ogday');
     });
-    it('should translate a complex word', function () {
+    it('should translate a complex word', function() {
       assert.equal(pigLatin('create'), 'eatecray');
       assert.equal(pigLatin('valley'), 'alleyvay');
     });
-    it('should attach "yay" if word begins with vowel', function () {
+    it('should attach "yay" if word begins with vowel', function() {
       assert.equal(pigLatin('egg'), 'eggyay');
       assert.equal(pigLatin('emission'), 'emissionyay');
     });
-    it('should auto lowercase word before translation', function () {
+    it('should auto lowercase word before translation', function() {
       assert.equal(pigLatin('HeLlO'), 'ellohay');
       assert.equal(pigLatin('RoCkEt'), 'ocketray');
     });
