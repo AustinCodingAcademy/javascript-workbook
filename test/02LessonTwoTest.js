28331d769f7dc9e5a7a43316f7ee8b2d7d8f73ad
jamesku <jameskuhr@gmail.com>
2016-10-15 14:23:36 -0500

    Lesson 2 test, 6 passing

--- test/02LessonTwoTest.js
+++ test/02LessonTwoTest.js
@@ -7,12 +7,25 @@ function whichSpecies(character) {
   // should return "cat" when character is 'garfield'
   // should return "fish" when character is 'nemo'
   // should return false if character is anything else
-  
+ switch(character){
+	 case "scooby":
+		 return ("dog");
+		 break;
+	case "garfield":
+		 return ("cat");
+		break;
+	case "nemo":
+		return ("fish");
+		break;
+	default:
+		return(false);
+ }
+
 }
 
 function isEven(number) {
   // should return true is number is even (divisible by 2)
-
+return (number%2 ==0);
 }
 
 
