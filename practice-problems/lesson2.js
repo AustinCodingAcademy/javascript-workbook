c0ada21e92ff87778f39bc1f1402a36f2cabe7fd
pricedrp9 <pricedrp9@gmail.com>
2016-10-16 18:13:42 -0500

    Lesson 2 practice problems... Need help with 5 & 6

--- practice-problems/lesson2.js
+++ practice-problems/lesson2.js
@@ -1,6 +1,6 @@
 //1. In your own words, explain what conditionals do.
 
-//Answer:
+//Answer: Conditionals are a cornerstone in the logic of programming. if something then, do this, else do something else, unless if something else is true
 
 
 
@@ -9,28 +9,68 @@
 
 //Answer:
 
+var test = 1;
+
+if (test < 10) {
+console.log("good job");
+} else {
+  console.log("try again");
+}
+
 
 
 
 //3. Write a conditional statement that prints out “divisible by 2”, “divisible by 3”, “divisible by 4”, or “divisible by 5” if a number is divisible by each of these numbers, respectively.
 
 //Answer:
+var divide = 27
 
-
+if (divide % 2 === 0 && divide % 3 ===0 && divide % 4 ===0 && divide %5 === 0) {
+  console.log("divisible by 2,3,4,5");
+}
+else if (divide % 2 ===0 && divide % 3 ===0 && divide % 4 ===0  ) {
+console.log("divisible by 2,3,4");
+}
+else if (divide % 2 ===0 && divide % 5 ===0 && divide % 4 ===0  ) {
+console.log("divisible by 2,4,5");
+}
+else if (divide % 2 ===0 && divide % 3 ===0) {
+console.log("divisible by 2,3");
+}
+else if (divide % 2 ===0 && divide % 4 ===0) {
+console.log("divisible by 2,4");
+}
+else if (divide % 2 ===0 && divide % 5 ===0) {
+console.log("divisible by 2,5");
+}
+else if (divide % 2 ===0) {
+console.log("divisible by 2");
+}
+else if (divide % 3 ===0) {
+console.log("divisible by 3");
+}
+else if (divide % 4 ===0) {
+console.log("divisible by 4");
+}
+else if (divide % 5 ===0) {
+console.log("divisible by 5");
+}
+else {
+  console.log("Try Again");
+}
 
 
 //4. Write the code for a function that prints out the text passed into the function as an argument.
 
 //Answer:
-
-
-
+function myFunction(){
+  return "Testing, Testing, 1...2...3...";
+}
+console.log(myFunction());
 
 //5. Create a function that prints out the average of a set of numbers.
 
 //Answer:
-
-
 
 
 //6. Call your above function and write the code to make the average print out.
