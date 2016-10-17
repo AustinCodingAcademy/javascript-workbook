805ed0b8089cdd01215ea15b35d61be886ba9f2f
Alex <alexanderjgaw@gmail.com>
2016-10-16 21:32:25 -0500

    Lesson One homework and test, Lesson 2 homework and practice problem

--- practice-problems/lesson2.js
+++ practice-problems/lesson2.js
@@ -1,6 +1,6 @@
 //1. In your own words, explain what conditionals do.
 
-//Answer:
+//Answer: Conditionals tell the program what to do based on certain rules.
 
 
 
@@ -9,19 +9,39 @@
 
 //Answer:
 
+if (answer) {
+  console.log('good job');
+} else {
+  console.log('try again');
+}
+
 
 
 
 //3. Write a conditional statement that prints out “divisible by 2”, “divisible by 3”, “divisible by 4”, or “divisible by 5” if a number is divisible by each of these numbers, respectively.
 
 //Answer:
-
+if (num % 2 === 0) {
+  console.log('divisible by 2');
+}
+if (num % 3 === 0) {
+  console.log('divisible by 3');
+}
+if (num % 4 === 0) {
+  console.log('divisible by 4');
+}
+if (num % 5 === 0) {
+  console.log('divisible by 5');
+}
 
 
 
 //4. Write the code for a function that prints out the text passed into the function as an argument.
 
 //Answer:
+var printIt = function(someString) {
+  console.log(someString);
+}
 
 
 
@@ -29,6 +49,9 @@
 //5. Create a function that prints out the average of a set of numbers.
 
 //Answer:
+var printAverage = function(num1, num2) {
+  console.log((num1 + num2) / 2);
+}
 
 
 
@@ -36,3 +59,4 @@
 //6. Call your above function and write the code to make the average print out.
 
 //Answer:
+printAverage(6,2);
