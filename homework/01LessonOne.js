71232a406a420711c2b6bd398553bfe0defd2c4d
ernietrevino <trevinoernie@gmail.com>
2016-10-18 18:22:31 -0500

    Lesson 2 test passing

--- homework/01LessonOne.js
+++ homework/01LessonOne.js
@@ -9,7 +9,7 @@ var assert = require('assert');
 // Problem 0:
 // To start out we are declaring a variable named myUndefined
 // Note that we are not assigning a value, so it is undefined.
-
+// undefined means that the variable has not been declared, or has not been given a value. null is a special object because typeof null returns 'object'. The value of a variable with no value is undefined
 var myUndefined;
 
 // Problem 1:
@@ -17,27 +17,27 @@ var myUndefined;
 //
 // What is the difference between null and undefined?
 
-var myNull;
+var myNull = null;
 
 // Problem 2:
 // Let's create a new variable named myTrue and give it the value true.
 
-var myTrue;
+var myTrue = true;
 
 // Problem 3:
 // Let's create a new variable named myFalse and give it the value false.
 
-var myFalse;
+var myFalse = false;
 
 // Problem 4:
 // Let's create a new variable named myNumber. Assign it a number.
 
-var myNumber;
+var myNumber = 3;
 
 // Problem 5:
 // Let's create a new variable named myString. Assign it a string.
 
-var myString;
+var myString = 'Horse';
 
 // ****
 // Boolean Operators
@@ -46,57 +46,57 @@ var myString;
 // Problem 6:
 // What is the value of true && true
 
-var trueAndTrue;
+var trueAndTrue=true;
 
 // Problem 7:
 // What is the value of false && true
 
-var falseAndTrue;
+var falseAndTrue=false;
 
 // Problem 8:
 // What is the value of true && false
 
-var trueAndFalse;
+var trueAndFalse=false;
 
 // Problem 9:
 // What is the value of false && false
 
-var falseAndFalse;
+var falseAndFalse=true;
 
 // Problem 10:
 // What is the value of true || true
 
-var trueOrTrue;
+var trueOrTrue=true;
 
 // Problem 11:
 // What is the value of false || true
 
-var falseOrTrue;
+var falseOrTrue=false;
 
 // Problem 12:
 // What is the value of true || false
 
-var trueOrFalse;
+var trueOrFalse=true;
 
 // Problem 13:
 // What is the value of false || false
 
-var falseOrFalse;
+var falseOrFalse=;
 
 // Problem 14:
 // What is the value of !false
 
-var notFalse;
+var notFalse=true;
 
 // Problem 15:
 // What is the value of !true
 
-var notTrue;
+var notTrue=false;
 
 // Problem 16:
 // What is the value of !!true
 
-var notNotTrue;
+var notNotTrue=true;
 
 // ****
 // Concept Checkpoint
@@ -106,7 +106,7 @@ var notNotTrue;
 // What is the difference between the "and" and the "or" operators? Explain why you would use each of them.
 //
 // Your Answer Goes Here:
-//
+//And means both must be equal to each other and or means the first one has to be true
 // ****
 
 // ****
@@ -119,22 +119,22 @@ var emptyString = '';
 // Problem 17:
 // What is the value of !!bob
 
-var notNotBob;
+var notNotBob=true;
 
 // Problem 18:
 // What is the value of !!emptyString
 
-var notNotEmptyString;
+var notNotEmptyString=True;
 
 // Problem 19:
 // What is the value of !null
 
-var notNull;
+var notNull=True;
 
 // Problem 20:
 // What is the value of !!undefined
 
-var notNotUndefined;
+var notNotUndefined=false;
 
 // ****
 // Concept Checkpoint
@@ -144,7 +144,7 @@ var notNotUndefined;
 // Explain truthiness and falsiness in your own words. Provide an example for each.
 //
 // Your Answer Goes Here:
-//
+//a truthy value is a value that translates to true when evaluated in a Boolean context. All values are truthy unless they are defined. its all truthy unless it is defined as falsey
 // ****
 
 // ****
@@ -153,47 +153,47 @@ var notNotUndefined;
 
 // Problem 21
 // What is the value of 4 === 4
-var fourEqualTofour;
+var fourEqualTofour=true;
 
 // Problem 22:
 // What is the value of 4 !== 4
-var fourNotEqualTofour;
+var fourNotEqualTofour=false;
 
 // Problem 23:
 // What is the value of -3 === 10
-var negativeThreeEqualToTen;
+var negativeThreeEqualToTen=false;
 
 // Problem 24:
 // What is the value of -3 < 10
-var negativeThreeLessThanTen;
+var negativeThreeLessThanTen=true;
 
 // Problem 25:
 // What is the value of -3 <= 10
-var negativeThreeLessThanOrEqualToTen;
+var negativeThreeLessThanOrEqualToTen=true;
 
 // Problem 26:
 // What is the value of -3 > 10
-var negativeThreeGreaterThanTen;
+var negativeThreeGreaterThanTen=false;
 
 // Problem 27:
 // What is the value of -3 >= 10
-var negativeThreeGreaterThanOrEqualToTen;
+var negativeThreeGreaterThanOrEqualToTen=false;
 
 // Problem 28:
 // What is the value of '4' == 4
-var stringFourEqualsNumberFour;
+var stringFourEqualsNumberFour=true;
 
 // Problem 29:
 // What is the value of '4' != 4
-var stringFourNotEqualsNumberFour;
+var stringFourNotEqualsNumberFour=false;
 
 // Problem 30:
 // What is the value of '4' === 4
-var stringFourStrictEqualsNumberFour;
+var stringFourStrictEqualsNumberFour=true;
 
 // Problem 31:
 // What is the value of '4' !== 4
-var stringFourStrictNotEqualsNumberFour;
+var stringFourStrictNotEqualsNumberFour=false;
 
 // ****
 // Concept Checkpoint
@@ -216,207 +216,207 @@ var stringFourStrictNotEqualsNumberFour;
 // Tests
 // ****
 
-describe('Lesson 1 Homework', function () {
+describe('Lesson 1 Homework', function() {
 
-  describe('Data Type Problems', function () {
-    describe('Problem 0: undefined variable myUndefined', function () {
-      it('should be undefined', function () {
-        assert.equal(typeof myUndefined, 'undefined');
-      });
+    describe('Data Type Problems', function() {
+        describe('Problem 0: undefined variable myUndefined', function() {
+            it('should be undefined', function() {
+                assert.equal(typeof myUndefined, 'undefined');
+            });
+        });
+
+        describe('Problem 1: null variable myNull', function() {
+            it('should be null', function() {
+                assert(myNull === null);
+            });
+        });
+
+        describe('Problem 2: true variable myTrue', function() {
+            it('should be true', function() {
+                assert.equal(myTrue, true);
+            });
+        });
+
+        describe('Problem 3: false variable myFalse', function() {
+            it('should be false', function() {
+                assert.equal(myFalse, false);
+            });
+        });
+
+        describe('Problem 4: number variable myNumber', function() {
+            it('should be a number', function() {
+                assert.equal(typeof myNumber, 'number');
+            });
+        });
+
+        describe('Problem 5: string variable myString', function() {
+            it('should be a string', function() {
+                assert.equal(typeof myString, 'string');
+            });
+        });
     });
 
-    describe('Problem 1: null variable myNull', function () {
-      it('should be null', function () {
-        assert(myNull === null);
-      });
+    describe('Boolean Operators', function() {
+        describe('Problem 6: true && true', function() {
+            it('should be true', function() {
+                assert.equal(trueAndTrue, true && true);
+            });
+        });
+
+        describe('Problem 7: false && true', function() {
+            it('should be false', function() {
+                assert.equal(falseAndTrue, false && true);
+            });
+        });
+
+        describe('Problem 8: true && false', function() {
+            it('should be false', function() {
+                assert.equal(trueAndFalse, true && false);
+            });
+        });
+
+        describe('Problem 9: false && false', function() {
+            it('should be false', function() {
+                assert.equal(falseAndFalse, false && false);
+            });
+        });
+
+        describe('Problem 10: true || true', function() {
+            it('should be true', function() {
+                assert.equal(trueOrTrue, true || true);
+            });
+        });
+
+        describe('Problem 11: false || true', function() {
+            it('should be true', function() {
+                assert.equal(falseOrTrue, false || true);
+            });
+        });
+
+        describe('Problem 12: true || false', function() {
+            it('should be true', function() {
+                assert.equal(trueOrFalse, true || false);
+            });
+        });
+
+        describe('Problem 13: false || false', function() {
+            it('should be false', function() {
+                assert.equal(falseOrFalse, false || false);
+            });
+        });
+
+        describe('Problem 14: !false', function() {
+            it('should be true', function() {
+                assert.equal(notFalse, !false);
+            });
+        });
+
+        describe('Problem 15: !true', function() {
+            it('should be false', function() {
+                assert.equal(notTrue, !true);
+            });
+        });
+
+        describe('Problem 16: !!true', function() {
+            it('should be true', function() {
+                assert.equal(notNotTrue, !!true);
+            });
+        });
     });
 
-    describe('Problem 2: true variable myTrue', function () {
-      it('should be true', function () {
-        assert.equal(myTrue, true);
-      });
+    describe('Truthiness', function() {
+
+        describe('Problem 17: !!bob', function() {
+            it('should be true', function() {
+                assert.equal(notNotBob, !!bob);
+            });
+        });
+
+        describe('Problem 18: !!emptyString', function() {
+            it('should be false', function() {
+                assert.equal(notNotEmptyString, !!emptyString);
+            });
+        });
+
+        describe('Problem 19: !null', function() {
+            it('should be true', function() {
+                assert.equal(notNull, !null);
+            });
+        });
+
+        describe('Problem 20: !!undefined', function() {
+            it('should be false', function() {
+                assert.equal(notNotUndefined, !!undefined);
+            });
+        });
     });
 
-    describe('Problem 3: false variable myFalse', function () {
-      it('should be false', function () {
-        assert.equal(myFalse, false);
-      });
-    });
+    describe('Comparison Operators', function() {
+        describe('Problem 21: 4 === 4', function() {
+            it('should be true', function() {
+                assert.equal(fourEqualTofour, 4 === 4);
+            });
+        });
 
-    describe('Problem 4: number variable myNumber', function () {
-      it('should be a number', function () {
-        assert.equal(typeof myNumber, 'number');
-      });
-    });
+        describe('Problem 22: 4 !== 4', function() {
+            it('should be false', function() {
+                assert.equal(fourNotEqualTofour, 4 !== 4);
+            });
+        });
 
-    describe('Problem 5: string variable myString', function () {
-      it('should be a string', function () {
-        assert.equal(typeof myString, 'string');
-      });
-    });
-  });
+        describe('Problem 23: -3 === 10', function() {
+            it('should be false', function() {
+                assert.equal(negativeThreeEqualToTen, -3 === 10);
+            });
+        });
 
-  describe('Boolean Operators', function () {
-    describe('Problem 6: true && true', function () {
-      it('should be true', function () {
-        assert.equal(trueAndTrue, true && true);
-      });
-    });
+        describe('Problem 24: -3 < 10', function() {
+            it('should be true', function() {
+                assert.equal(negativeThreeLessThanTen, -3 < 10);
+            });
+        });
 
-    describe('Problem 7: false && true', function () {
-      it('should be false', function () {
-        assert.equal(falseAndTrue, false && true);
-      });
-    });
+        describe('Problem 25: -3 <= 10', function() {
+            it('should be true', function() {
+                assert.equal(negativeThreeLessThanOrEqualToTen, -3 <= 10);
+            });
+        });
 
-    describe('Problem 8: true && false', function () {
-      it('should be false', function () {
-        assert.equal(trueAndFalse, true && false);
-      });
-    });
+        describe('Problem 26: -3 > 10', function() {
+            it('should be false', function() {
+                assert.equal(negativeThreeGreaterThanTen, -3 > 10);
+            });
+        });
 
-    describe('Problem 9: false && false', function () {
-      it('should be false', function () {
-        assert.equal(falseAndFalse, false && false);
-      });
-    });
+        describe('Problem 27: -3 >= 10', function() {
+            it('should be false', function() {
+                assert.equal(negativeThreeGreaterThanOrEqualToTen, -3 >= 10);
+            });
+        });
 
-    describe('Problem 10: true || true', function () {
-      it('should be true', function () {
-        assert.equal(trueOrTrue, true || true);
-      });
-    });
+        describe('Problem 28: \'4\' == 4', function() {
+            it('should be true', function() {
+                assert.equal(stringFourEqualsNumberFour, '4' == 4);
+            });
+        });
 
-    describe('Problem 11: false || true', function () {
-      it('should be true', function () {
-        assert.equal(falseOrTrue, false || true);
-      });
-    });
+        describe('Problem 29: \'4\' != 4', function() {
+            it('should be false', function() {
+                assert.equal(stringFourNotEqualsNumberFour, '4' != 4);
+            });
+        });
 
-    describe('Problem 12: true || false', function () {
-      it('should be true', function () {
-        assert.equal(trueOrFalse, true || false);
-      });
-    });
+        describe('Problem 30: \'4\' === 4', function() {
+            it('should be false', function() {
+                assert.equal(stringFourStrictEqualsNumberFour, '4' === 4);
+            });
+        });
 
-    describe('Problem 13: false || false', function () {
-      it('should be false', function () {
-        assert.equal(falseOrFalse, false || false);
-      });
+        describe('Problem 31: \'4\' !== 4', function() {
+            it('should be true', function() {
+                assert.equal(stringFourStrictNotEqualsNumberFour, '4' !== 4);
+            });
+        });
     });
-
-    describe('Problem 14: !false', function () {
-      it('should be true', function () {
-        assert.equal(notFalse, !false);
-      });
-    });
-
-    describe('Problem 15: !true', function () {
-      it('should be false', function () {
-        assert.equal(notTrue, !true);
-      });
-    });
-
-    describe('Problem 16: !!true', function () {
-      it('should be true', function () {
-        assert.equal(notNotTrue, !!true);
-      });
-    });
-  });
-
-  describe('Truthiness', function () {
-
-    describe('Problem 17: !!bob', function () {
-      it('should be true', function () {
-        assert.equal(notNotBob, !!bob);
-      });
-    });
-
-    describe('Problem 18: !!emptyString', function () {
-      it('should be false', function () {
-        assert.equal(notNotEmptyString, !!emptyString);
-      });
-    });
-
-    describe('Problem 19: !null', function () {
-      it('should be true', function () {
-        assert.equal(notNull, !null);
-      });
-    });
-
-    describe('Problem 20: !!undefined', function () {
-      it('should be false', function () {
-        assert.equal(notNotUndefined, !!undefined);
-      });
-    });
-  });
-
-  describe('Comparison Operators', function () {
-    describe('Problem 21: 4 === 4', function () {
-      it('should be true', function () {
-        assert.equal(fourEqualTofour, 4 === 4);
-      });
-    });
-
-    describe('Problem 22: 4 !== 4', function () {
-      it('should be false', function () {
-        assert.equal(fourNotEqualTofour, 4 !== 4);
-      });
-    });
-
-    describe('Problem 23: -3 === 10', function () {
-      it('should be false', function () {
-        assert.equal(negativeThreeEqualToTen, -3 === 10);
-      });
-    });
-
-    describe('Problem 24: -3 < 10', function () {
-      it('should be true', function () {
-        assert.equal(negativeThreeLessThanTen, -3 < 10);
-      });
-    });
-
-    describe('Problem 25: -3 <= 10', function () {
-      it('should be true', function () {
-        assert.equal(negativeThreeLessThanOrEqualToTen, -3 <= 10);
-      });
-    });
-
-    describe('Problem 26: -3 > 10', function () {
-      it('should be false', function () {
-        assert.equal(negativeThreeGreaterThanTen, -3 > 10);
-      });
-    });
-
-    describe('Problem 27: -3 >= 10', function () {
-      it('should be false', function () {
-        assert.equal(negativeThreeGreaterThanOrEqualToTen, -3 >= 10);
-      });
-    });
-
-    describe('Problem 28: \'4\' == 4', function () {
-      it('should be true', function () {
-        assert.equal(stringFourEqualsNumberFour, '4' == 4);
-      });
-    });
-
-    describe('Problem 29: \'4\' != 4', function () {
-      it('should be false', function () {
-        assert.equal(stringFourNotEqualsNumberFour, '4' != 4);
-      });
-    });
-
-    describe('Problem 30: \'4\' === 4', function () {
-      it('should be false', function () {
-        assert.equal(stringFourStrictEqualsNumberFour, '4' === 4);
-      });
-    });
-
-    describe('Problem 31: \'4\' !== 4', function () {
-      it('should be true', function () {
-        assert.equal(stringFourStrictNotEqualsNumberFour, '4' !== 4);
-      });
-    });
-  });
 
 });
