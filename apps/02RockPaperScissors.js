c6b587b3f887a7293807a6064afe8bb484cfbfcb
akshayasaraf <AkshayaSaraf@Akshaya-Sarafs-Mac.local>
2016-10-18 18:59:20 -0500

    Rock, Paper and Scissors app

--- apps/02RockPaperScissors.js
+++ apps/02RockPaperScissors.js
@@ -11,7 +11,36 @@ var rl = readline.createInterface({
 function rockPaperScissors(hand1, hand2) {
 
   // Write code here
-
+  hand1=hand1.toLowerCase();
+  hand2=hand2.toLowerCase();
+ if(hand1 === hand2){
+ return ( "It's a tie!");
+ }
+ else if(hand1 === 'rock')
+ {
+   if (hand2 === 'scissors') {
+     return ("Hand one wins!");
+   }
+   else{
+     return ("Hand two wins!");
+   }
+ }
+ else if (hand1 === 'paper') {
+   if (hand2 === 'rock') {
+     return ("Hand one wins!");
+   }
+   else {
+      return ("Hand two wins!");
+   }
+ }
+ else if (hand1 === 'scissors') {
+   if (hand2 ==='paper') {
+     return ("Hand one wins!");
+   }
+   else {
+      return ("Hand two wins!");
+   }
+ }
 }
 
 function getPrompt() {
