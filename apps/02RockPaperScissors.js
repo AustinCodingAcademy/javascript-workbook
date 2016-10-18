eec88cc9a0386b81ce5544c443186c40d2eeb3c0
mcplunket <maryclaireplunket@Marys-MacBook-Pro-2.local>
2016-10-18 18:37:13 -0500

    App Two Rock Paper Scissors

--- apps/02RockPaperScissors.js
+++ apps/02RockPaperScissors.js
@@ -9,9 +9,35 @@ var rl = readline.createInterface({
 
 
 function rockPaperScissors(hand1, hand2) {
-
+ hand1 = hand1.toLowerCase();
+ hand2 = hand2.toLowerCase();
   // Write code here
+  if (hand1 === hand2) {
+    return "It's a tie!";
+  }
+  if (hand1 === 'rock') {
+    if (hand2 === 'scissors') {
+      return 'Hand one wins!';
+    }
+    // If we reach here, player 2 must have dealt paper
+    return 'Hand two wins!';
+  }
 
+  if (hand1 === 'paper') {
+    // fill this in using the logic above
+    if (hand2 === 'rock'){
+      return 'Hand one wins!';
+    }
+    return 'Hand two wins!';
+  }
+
+  if (hand1 === 'scissors') {
+    // fill this in using the logic above
+    if (hand2 === 'paper') {
+      return 'Hand one wins';
+    }
+    return 'Hand two wins';
+  }
 }
 
 function getPrompt() {
