c71a8fbb5118420bf5930482a9a01ed34e18f9a4
Jordan Mitchell <mordanjitchell@gmail.com>
2016-10-17 19:54:11 -0500

    homework lesson 2 working

--- homework/02LessonTwo.js
+++ homework/02LessonTwo.js
@@ -26,7 +26,7 @@ var alwaysTrue = function () {
 // argument1 is equal to argument2
 
 function equals(argument1, argument2) {
-  return argument1 === argument;
+  return argument1 === argument2;
 }
 
 // Problem 2:
@@ -35,17 +35,22 @@ function equals(argument1, argument2) {
 // parameter1 is less than or equal to parameter2
 
 function lessThanOrEqualTo(parameter1, parameter2) {
+  return parameter1 <= parameter2;
 }
 
 // Problem 3:
 // write a function named add(number1, number2)
 // add will add two numbers and return the result
-
+function add(number1, number2){
+  return number1 + number2;
+}
 // Problem 4:
 // write a function named addThree(number1, number2, number3)
 // this function will add three numbers
 // you must call your function add() in addThree()
-
+function addThree(number1, number2, number3){
+  return number1 + number2 + number3;
+}
 
 // ****
 // Concept Checkpoint
@@ -55,10 +60,10 @@ function lessThanOrEqualTo(parameter1, parameter2) {
 // What is a function? How do you define a function in Javascript?
 //
 // Your Answer Goes Here:
-//
+//functions can do small bits of code to simplify larger functions later on.  They check the values of input and output the arguments
 //
 // What is a return value?
-//
+//return values stop the code from executing and
 // Your Answer Goes Here:
 //
 //
@@ -85,8 +90,14 @@ function lessThanOrEqualTo(parameter1, parameter2) {
 // complete isEven() by returning a boolean expression
 
 function isEven(number) {
+if(number %  2 === 0){
+  return true;
 }
-
+else{
+  return false;
+}
+}
+isEven();
 // Problem 6:
 // isDivisibleByThree(number) is a function that
 // returns true if number is divisible by 3,
@@ -94,6 +105,12 @@ function isEven(number) {
 // complete isDivisibleByThree() by returning a boolean expression
 
 function isDivisibleByThree(number) {
+  if (number % 3 === 0){
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 // ****
@@ -109,6 +126,18 @@ function isDivisibleByThree(number) {
 // should return false if character is anything else
 
 function whichSpecies(character) {
+  if(character === "scooby"){
+    return "dog";
+  }
+  else if(character === "garfield"){
+    return "cat";
+  }
+  else if (character === "nemo"){
+    return "fish";
+  }
+  else{
+    return false;
+  }
 }
 
 // Problem 8:
@@ -118,7 +147,20 @@ function whichSpecies(character) {
 // return the string "divisible by 2" when number % 2 === 0
 // return the string "divisible by 3" when number % 3 === 0
 // return the string "divisible by 5" when number % 5 === 0
-
+function testNumber(number){
+if (number % 4 === 0){
+  return "divisible by 4";
+}
+else if(number % 2 === 0){
+  return "divisible by 2";
+}
+else if (number % 3 === 0){
+  return "divisible by 3";
+}
+  else if(number % 5 === 0){
+    return "divisible by 5";
+  }
+}
 
 // ****
 // Concept Checkpoint
@@ -246,5 +288,3 @@ describe('Lesson 2 Homework', function () {
   });
 
 });
-Contact GitHub API Training Shop Blog About
-© 2016 GitHub, Inc. Terms Privacy Security Status Help
