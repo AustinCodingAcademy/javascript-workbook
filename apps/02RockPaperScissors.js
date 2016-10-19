d4536fb2c873308d30b601295f806971150f4e7a
dsolis421 <snapshot8d@gmail.com>
2016-10-18 19:51:55 -0500

    02 rock paper scissors add validation passing

--- apps/02RockPaperScissors.js
+++ apps/02RockPaperScissors.js
@@ -13,7 +13,15 @@ function rockPaperScissors(hand1, hand2) {
   // Write code here
   hand1.toLowerCase();
   hand2.toLowerCase();
-  
+
+  if (hand1 !== "rock" && hand1 !== "paper" && hand1 !== "scissors") {
+    return "Hand one selection is not valid!";
+  };
+
+  if (hand2 !== "rock" && hand2 !== "paper" && hand2 !== "scissors") {
+    return "Hand two selection is not valid!";
+  };
+
   if (hand1 === hand2) {
     return "It's a tie!";
   } else if (hand1 === "rock") {
