7e7b0468fbef2973f8f6a9fbbd95b30967d48ed2
unknown <willeo.smith@gmail.com>
2016-10-19 18:34:54 -0500

    Done 10/19

--- apps/02RockPaperScissors.js
+++ apps/02RockPaperScissors.js
@@ -12,7 +12,35 @@ function rockPaperScissors(hand1, hand2) {
 
   // Write code here
 
+if (hand1 === hand2){
+  return "It's a tie!";
 }
+
+if (hand1 === 'rock') {
+  if (hand2 === 'scissors') {
+    return 'Hand one wins!';
+  }
+}
+
+if (hand1 === 'scissors') {
+  if (hand2 === 'paper') {
+    return 'Hand one wins!';
+  }
+}
+
+if (hand1 === 'paper') {
+  if (hand2 === 'rock') {
+    return 'Hand one wins!';
+  }
+}
+  else {
+  return "Hand two wins!";
+}
+
+}
+
+
+
 
 function getPrompt() {
   rl.question('hand1: ', (answer1) => {
