82d06a379813668aba1067fd5b48a98c518491af
chrisperk <christopherperkins.512@gmail.com>
2016-10-18 20:52:14 -0500

    Rock Paper Scissors app in progress

--- apps/02RockPaperScissors.js
+++ apps/02RockPaperScissors.js
@@ -8,46 +8,68 @@ var rl = readline.createInterface({
 });
 
 
-function rockPaperScissors(userHand1, userHand2) {
+function rockPaperScissors(hand1, hand2) {
     // Write code here
+    hand1 = hand1.trim();
+    hand2 = hand2.trim();
 
-    var hand1 = userHand1.toLowerCase();
-    var hand2 = userHand2.toLowerCase();
+    hand1 = hand1.toLowerCase();
+    hand2 = hand2.toLowerCase();
 
-    if (hand1 === hand2) {
-        return "It's a tie!";
-    } else if (hand1 === 'rock') {
-        if (hand2 === 'paper') {
-            return 'Hand two wins!';
+    if (((hand1 === 'rock') || (hand1 === 'paper') || (hand1 === 'scissors')) && ((hand2 === 'rock') || (hand2 === 'paper') || (hand2 === 'scissors'))) {
+        if (hand1 === hand2) {
+            return "It's a tie!";
+        } else if (hand1 === 'rock') {
+            if (hand2 === 'paper') {
+                return 'Hand two wins!';
+            } else {
+                return 'Hand one wins!';
+            }
+        } else if (hand1 === 'paper') {
+            if (hand2 === 'scissors') {
+                return 'Hand two wins!';
+            } else {
+                return 'Hand one wins!';
+            }
+        } else if (hand1 === 'scissors') {
+            if (hand2 === 'rock') {
+                return 'Hand two wins!';
+            } else {
+                return 'Hand one wins!';
+            }
         } else {
-            return 'Hand one wins!';
-        };
-    } else if (hand1 === 'paper') {
-        if (hand2 === 'scissors') {
-            return 'Hand two wins!';
-        } else {
-            return 'Hand one wins!';
-        };
-    } else if (hand1 === 'scissors') {
-        if (hand2 === 'rock') {
-            return 'Hand two wins!';
-        } else {
-            return 'Hand one wins!';
-        };
+            return 'Invalid entry!';
+        }
     } else {
         return 'Invalid entry!';
-    };
-};
+    }
+}
+
+// var iterate = true;
+// var userContinue = true;
 
 function getPrompt() {
     rl.question('hand1: ', (answer1) => {
         rl.question('hand2: ', (answer2) => {
             console.log(rockPaperScissors(answer1, answer2));
             getPrompt();
-        });
-    });
+        })
+    })
 }
 
+// var userIterate = true;
+//
+// function iterate() {
+//     while (userIterate === true) {
+//         if (userInput === 'n') {
+//             console.log('Thank you for playing! Come back again.');
+//             userIterate = false;
+//         } else {
+//             console.log('Awesome, here we go!');
+//             getPrompt();
+//         }
+//     }
+// }
 // Tests
 
 if (typeof describe === 'function') {
@@ -67,5 +89,9 @@ if (typeof describe === 'function') {
 } else {
 
     getPrompt();
+    // var userInput = prompt("Would you like to continue?", "<Enter y/n>");
+    // var trimmedUserInput = userInput.trim();
+    // var scrubbedInput = trimmedUserInput.toLowerCase();
+    // iterate();
 
-}
+};
