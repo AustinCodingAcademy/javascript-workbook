d69021f479ed8d4c0fe89ef39cdc3ebbb29161d5
Joe King <realkalel@yahoo.com>
2016-10-20 18:22:02 -0500

    02RockPaperScissors.js

--- homework/03LessonThree.js
+++ homework/03LessonThree.js
@@ -21,11 +21,11 @@ var firstName = 'susan';
 // Problem 1:
 // What is the value of firstName at this point in the file?
 var valueOfFirstName;
-
+susan
 // Problem 2:
 // What is the value of lastName at this point in the file?
 var valueOfLastName;
-
+plaid
 // Problem 3:
 // Fix the following function so that it returns the first argument
 function returnTheFirstArgument(firstArgument) {
@@ -41,7 +41,7 @@ function returnTheFirstArgument(firstArgument) {
 // What is scope?
 //
 // Your Answer Goes Here:
-//
+// scope is the set of variables, objects, and functions you have access to.
 //
 
 
@@ -57,11 +57,11 @@ var ternaryResult4 = "" ? 'first' : 'second';
 
 // Problem 4:
 // What is the value of ternaryResult1
-var problemFourAnswer;
+var problemFourAnswer = "first";
 
 // Problem 5:
 // What is the value of ternaryResult2
-var problemFiveAnswer;
+var problemFiveAnswer = "first";
 
 // Problem 6:
 // What is the value of ternaryResult3
@@ -79,7 +79,9 @@ var problemSevenAnswer;
 // What is the ternary operator?  How does it differ from normal conditional statements?
 //
 // Your Answer Goes Here:
-//
+// it assigns a value to a variable based on some condition.
+
+
 //
 
 
@@ -89,18 +91,18 @@ var problemSevenAnswer;
 
 // Problem 8:
 // Create an empty array
-var emptyArray;
+var emptyArray = [];
 
 // Problem 9:
 // Create an array with 5 elements in it
-var lengthFiveArray;
+var lengthFiveArray = ["joe", "tim", "ray" "allen" "john"];
 
 // DO NOT MODIFY
 var nameArray = ['bob', 'fred', 'susan'];
 
 // Problem 10:
 // Replace the value 'fred' in nameArray with 'george'
-
+var nameArray = ['bob', 'george', 'susan'];
 // DO NOT MODIFY
 var threeByThreeArray = [
   [1, 2, 3],
@@ -110,10 +112,17 @@ var threeByThreeArray = [
 
 // Problem 11:
 // Replace the center element of threeByThreeArray, which is 5, with something else.
-
+var threeByThreeArray = [
+  [1, 2, 3],
+  [4, 3, 6],
+  [7, 8, 9]
+];
 // Problem 12:
 // create a 2 by 2 (2 rows, 2 columns) nested array
-var twoByTwoArray;
+var twoByTwoArray = [
+  [1,2],
+  [2,4]
+];
 
 // ****
 // Tests
