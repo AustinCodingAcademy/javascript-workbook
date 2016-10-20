c4d1f6fbf808522fb76cb1d5e71a8be8f9e0c1b7
dsolis421 <snapshot8d@gmail.com>
2016-10-20 18:23:04 -0500

    pig latin code cleanup

--- apps/01PigLatin.js
+++ apps/01PigLatin.js
@@ -11,43 +11,41 @@ var rl = readline.createInterface({
 function pigLatin(word) {
 
     // Your code here
-    word = word.toLowerCase();
-    var firstLetter = word[0];
+  word = word.toLowerCase();
+  var firstLetter = word[0];
 
 
-    var vowelIndex = -1; // Set it to assume there are no vowels
+  var vowelIndex = -1; // Set it to assume there are no vowels
 
-    if ((word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex) || vowelIndex === -1) {
-        vowelIndex = word.indexOf('a');
-    }
+  if ((word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex) || vowelIndex === -1) {
+      vowelIndex = word.indexOf('a');
+  }
 
-    if ((word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex) || vowelIndex === -1) {
-        vowelIndex = word.indexOf('e');
-    }
+  if ((word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex) || vowelIndex === -1) {
+      vowelIndex = word.indexOf('e');
+  }
 
-    if ((word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex) || vowelIndex === -1) {
-        vowelIndex = word.indexOf('i');
-    }
+  if ((word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex) || vowelIndex === -1) {
+      vowelIndex = word.indexOf('i');
+  }
+
+  if ((word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex) || vowelIndex === -1) {
+      vowelIndex = word.indexOf('o');
+  }
 
 
-    if ((word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex) || vowelIndex === -1) {
-        vowelIndex = word.indexOf('o');
-    }
+  if ((word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex) || vowelIndex === -1) {
+      vowelIndex = word.indexOf('u');
+  }
 
+  if (vowelIndex === 0) {
+      // special snowflake
+      return word + 'yay';
+  }
+  var firstPart = word.slice(0, vowelIndex);
+  var restWord = word.slice(vowelIndex, word.length);
 
-    if ((word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex) || vowelIndex === -1) {
-        vowelIndex = word.indexOf('u');
-    }
-
-    if (vowelIndex === 0) {
-        // special snowflake
-        return word + 'yay';
-    }
-    var firstPart = word.slice(0, vowelIndex);
-    var restWord = word.slice(vowelIndex, word.length);
-
-    return restWord + firstPart + 'ay';
-
+  return restWord + firstPart + 'ay';
 }
 
 
