198d81e91f3cdb6da25def76abd290953f10ec3a
Alex <alexanderjgaw@gmail.com>
2016-10-19 20:51:50 -0500

    Tic Tac Toe app

--- apps/03TicTacToe.js
+++ apps/03TicTacToe.js
@@ -13,6 +13,7 @@ var board = [
 ];
 
 var playerTurn = 'X';
+var stopGame = false;
 
 function printBoard() {
   console.log('   0  1  2');
@@ -25,22 +26,60 @@ function printBoard() {
 
 function horizontalWin() {
   // Your code here
+  return  (board[0][0] === playerTurn && board[0][1] === playerTurn && board[0][2] === playerTurn) ||
+          (board[1][0] === playerTurn && board[1][1] === playerTurn && board[1][2] === playerTurn) ||
+          (board[2][0] === playerTurn && board[2][1] === playerTurn && board[2][2] === playerTurn);
 }
 
 function verticalWin() {
   // Your code here
+  return  (board[0][0] === playerTurn && board[1][0] === playerTurn && board[2][0] === playerTurn) ||
+          (board[0][1] === playerTurn && board[1][1] === playerTurn && board[2][1] === playerTurn) ||
+          (board[0][2] === playerTurn && board[1][2] === playerTurn && board[2][2] === playerTurn);
 }
 
 function diagonalWin() {
   // Your code here
+  return  (board[0][0] === playerTurn && board[1][1] === playerTurn && board[2][2] === playerTurn) ||
+          (board[0][2] === playerTurn && board[1][1] === playerTurn && board[2][0] === playerTurn);
 }
 
 function checkForWin() {
   // Your code here
+  if (horizontalWin() || verticalWin() || diagonalWin()) {
+    console.log('Player ' + playerTurn + ' Won!');
+    stopGame = true;
+    return true;
+  }
+}
+
+function checkForTie() {
+  var isEmpty = false;
+  for(var i = 0; i < board.length; i++) {
+    for(var j = 0; j < board[i].length; j++) {
+      isEmpty = (board[i][j] === ' ') ? true : false;
+      if (isEmpty) {
+        return;
+      }
+    }
+  }
+  console.log("It's a tie! Try again!");
+  stopGame = true;
 }
 
 function ticTacToe(row, column) {
   // Your code here
+  if (!row || !column || !board[row][column]) {
+    console.log('Use valid input, asshole!');
+  } else if (board[row][column] === ' ') {
+    board[row][column] = playerTurn;
+    checkForWin();
+    checkForTie();
+    playerTurn = (playerTurn === 'X') ? 'O' : 'X';
+  } else {
+    console.log('That spot is taken, try again!');
+  }
+
 }
 
 function getPrompt() {
@@ -49,7 +88,7 @@ function getPrompt() {
   rl.question('row: ', (row) => {
     rl.question('column: ', (column) => {
       ticTacToe(row, column);
-      getPrompt();
+      if (!stopGame) {getPrompt();}
     });
   });
 
