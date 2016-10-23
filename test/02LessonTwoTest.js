b6fff6d7f7a5a0d26c041930dd1748af5ab8e0c3
James Puckett <jamesjaypuckett@gmail.com>
2016-10-23 07:57:25 -0500

    lesson two test using switch

--- test/02LessonTwoTest.js
+++ test/02LessonTwoTest.js
@@ -7,12 +7,26 @@ function whichSpecies(character) {
   // should return "cat" when character is 'garfield'
   // should return "fish" when character is 'nemo'
   // should return false if character is anything else
-  
+  switch (character) {
+    case 'scooby':
+      return 'dog';
+      break;
+    case 'garfield':
+      return 'cat';
+      break;
+    case 'nemo':
+      return 'fish';
+      break;
+    default:
+      return false;
+
+  }
 }
 
 function isEven(number) {
   // should return true is number is even (divisible by 2)
-
+  var ternary = (number % 2 === 0) ? true : false;
+  return ternary
 }
 
 
