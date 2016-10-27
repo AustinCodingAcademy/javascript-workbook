744bbca170db704c17f7f99dbea43284e05f793b
mhow8809 <mhow_22000@yahoo.com>
2016-10-27 17:25:11 -0500

    03TicTacToe passing with extras

--- apps/03TicTacToe.js
+++ apps/03TicTacToe.js
@@ -6,13 +6,14 @@ var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
 var board = [
   [' ', ' ', ' '],
   [' ', ' ', ' '],
   [' ', ' ', ' ']
 ];
-
 var playerTurn = 'X';
+
 
 function printBoard() {
   console.log('   0  1  2');
@@ -25,34 +26,62 @@ function printBoard() {
 
 function horizontalWin() {
   // Your code here
+  return ((board[0][0] === playerTurn && board[0][1] === playerTurn && board[0][2] === playerTurn) ||
+  (board[1][0] === playerTurn && board[1][1] === playerTurn && board[1][2] === playerTurn) ||
+  (board[2][0] === playerTurn && board[2][1] === playerTurn && board[2][2] === playerTurn));
 }
 
 function verticalWin() {
   // Your code here
+  return ((board[0][0] === playerTurn && board[1][0] === playerTurn && board[2][0] === playerTurn) ||
+  (board[0][1] === playerTurn && board[1][1] === playerTurn && board[2][1] === playerTurn) ||
+  (board[0][2] === playerTurn && board[1][2] === playerTurn && board[2][2] === playerTurn));
 }
 
 function diagonalWin() {
   // Your code here
+  return ((board[0][0] === playerTurn && board[1][1] === playerTurn && board[2][2] === playerTurn) ||
+  (board[0][2] === playerTurn && board[1][1] === playerTurn && board[2][0] === playerTurn));
 }
 
 function checkForWin() {
   // Your code here
+  return ( (horizontalWin()) || (verticalWin()) || (diagonalWin()) );
 }
 
 function ticTacToe(row, column) {
   // Your code here
+  if (board[row][column] != ' ' ) {
+    console.log('Square has already been played!');
+    console.log('Player ' + playerTurn + ' Go Again!');
+  } else {
+      board[row][column] = playerTurn;
+      if (checkForWin() != true ) {
+        playerTurn = (playerTurn === 'X') ? 'O' : 'X';
+        getPrompt();
+    } else {
+      console.log('Player ' + playerTurn + ' Wins!!!!');
+      rl.question('Would you like to play again? Yes or No: ' , (playAgain) => {
+        if (playAgain === 'Yes') {
+          board = [
+            [' ', ' ', ' '],
+            [' ', ' ', ' '],
+            [' ', ' ', ' ']
+          ]; 
+          getPrompt();
+        }
+      });
+    }
+  }
 }
-
 function getPrompt() {
   printBoard();
   console.log("It's Player " + playerTurn + "'s turn.");
   rl.question('row: ', (row) => {
     rl.question('column: ', (column) => {
       ticTacToe(row, column);
-      getPrompt();
     });
   });
-
 }
 
 
