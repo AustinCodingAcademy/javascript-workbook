f16f519049b5121d2c82d71627dca7b6c7b2f075
umbreen-deweb <umbreen.ahmad@gmail.com>
2016-11-01 12:51:17 -0500

    Towers of Hanoi

--- apps/04TowersOfHanoi.js
+++ apps/04TowersOfHanoi.js
@@ -19,23 +19,47 @@ function printStacks() {
   console.log("c: " + stacks.c);
 }
 
-function movePiece() {
-  // Your code here
+function movePiece(startStack, endStack) {
+  //I am saying that from the variable stacks, pop off the smallest block
+  //There are the stacks, inside the stacks are the blocks
+  // end stack is where the block is going or pushed
 
+var pickedblock = stacks[startStack].pop();
+stacks[endStack].push(pickedblock);
+  //push on to end
 }
 
-function isLegal() {
-  // Your code here
-
+function isLegal(startStack, endStack) {
+  // This is saying if the block moving from start stack is smaller than the one on the end stack then it is legal
+  // Or if there are no blocks on the end stack as in it is equal to zera the block is okay to move from the starting stack to the end stack
+  if (stacks[endStack].length === 0){
+    return true;
+  }
+  if (stacks[startStack].length < stacks[endStack].length){
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function checkForWin() {
+function checkForWin(startStack, endStack) {
   // Your code here
-
+if(stacks.b.length === 4 || stacks.c.length === 4){
+  console.log('Good Job!!')
+  return true;
+} else {
+  return false;
+}
 }
 
 function towersOfHanoi(startStack, endStack) {
   // Your code here
+if (isLegal(startStack, endStack)) {
+  movePiece(startStack, endStack);
+} else {
+  console.log('This move is a no go, try again!');
+}
+ checkForWin();
 
 }
 
