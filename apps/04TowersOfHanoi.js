70f0567384b31d79e6b37ee72df67430cd00940f
dmencha <dmencha@rMB.local>
2016-11-02 18:35:34 -0500

    Towers of Hanoi

--- apps/04TowersOfHanoi.js
+++ apps/04TowersOfHanoi.js
@@ -19,25 +19,107 @@ function printStacks() {
   console.log("c: " + stacks.c);
 }
 
-function movePiece() {
+function movePiece(startStack, endStack) {
   // Your code here
+  // Get the piece off the top of the start stack and save it (var)
+  // Move it to the endStack
+//  var towertoremovefrom = stacks[startStack];
+//  var block = towertoremovefrom.pop();
 
+//  var towertoaddto = stacks[endStack];
+//  towertoaddto.push(block);
+
+
+
+
+  var piece = stacks[startStack].pop();
+  stacks[endStack].push(piece);
 }
 
-function isLegal() {
+function isLegal(startStack, endStack) {
   // Your code here
+  // Get a reference to the actual start stack ans save it
+  // Does start stack have anything in it?
+  // Get a reference to the actual end stack and save it
+  // Is the piece from the start stack bigger or smaller than the piece on the top of the end stack
+var startStackArray = stacks[startStack];
+var endStackArray   = stacks[endStack];
+
+if (startStackArray.length  === 0){
+  return false;
+}
+else if (endStackArray.length === 0) {
+  return true;
+}
+else {
+  return startStackArray[startStackArray.length-1]
+  < endStackArray[endStackArray.length - 1];
+}
 
 }
 
 function checkForWin() {
   // Your code here
+  // Does Stack C have a length of 4? (.length -- to check the length of the array)
+if((stacks.c.length === 4) || (stacks.b.length === 4)){
+  console.log("You Won!")
+  return true;
+}
+else{
+  return false;
+}
 
 }
+
+// var towers = {
+
+// var tower1 : [4,3,2,1],
+// var tower2 : [],
+// var tower3 : []
+// }
 
 function towersOfHanoi(startStack, endStack) {
   // Your code here
+  // MAIN FUNCTION
+  //  Check if the move is okay
+  //  If its okay, then make the move
+
+
+
+  if (isLegal(startStack, endStack)){
+    movePiece(startStack, endStack);
+  }
+  else {
+    console.log("Invalid move please try again.")
+  }
+  if (checkForWin()) {
+  process.exit();
+  }
 
 }
+
+//if(start === "tower1"){
+//  var block = tower1.pop();
+//}
+//
+//if(start === "tower2"){
+//  var block = tower2.pop();
+//}
+//
+//if(start === "tower3"){
+//  var block = tower3.pop();
+//}
+//
+//if(end === "tower2"){
+//  tower1.push(block);
+//}
+//
+//var block = tower1.pop();
+//tower2.push(block);
+
+
+  //  Did the player win yet?
+
 
 function getPrompt() {
   printStacks();
