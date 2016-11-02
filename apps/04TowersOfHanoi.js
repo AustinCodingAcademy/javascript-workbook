9e8f13c387587f6bda53a21f25442c32e03f93e7
akshayasaraf <AkshayaSaraf@Akshaya-Sarafs-Mac.local>
2016-11-01 19:38:37 -0500

    Towers of Hanoi

--- apps/04TowersOfHanoi.js
+++ apps/04TowersOfHanoi.js
@@ -8,35 +8,57 @@ var rl = readline.createInterface({
 });
 
 var stacks = {
-  a: [4, 3, 2, 1],
-  b: [],
-  c: []
+  'a': [4, 3, 2, 1],
+  'b': [],
+  'c': []
 };
-
+var startStack='';
+var endStack='';
+// stacks are also called associative array or objects.
 function printStacks() {
   console.log("a: " + stacks.a);
   console.log("b: " + stacks.b);
   console.log("c: " + stacks.c);
 }
 
-function movePiece() {
-  // Your code here
+function movePiece(startStack, endStack) {
+  // In movePiece(), pass in two arguments, startStack and endStack. Visualizing playing in a physical game, you .pop() off a block from the the startStack and .push() it on the endStack. Call this function from towersOfHanoi(), passing in your arguments.
+  var q =stacks[startStack].pop();
+  stacks[endStack].push(q);
+}
+
+function isLegal(startStack, endStack) {
+  // isLegal() takes two arguments, startStack and endStack, and will check to see if the block being moved, from startStack is smaller than last block in endStack. return true if it is allowed, otherwise, return false. Also, don't forget to think about if the endStack is empty, you may put any block there. Put this check before your movePiece() function.
+  var move = stacks[startStack];
+  var stackblock= stacks[endStack];
+  var pos=move.length - 1;
+  var pos1=stackblock.length -1;
+  if(( move[pos] < stackblock[pos1] )||(stacks[endStack].length === 0)){
+    return true;
+  }
+  else {
+    return false;
+  }
 
 }
 
-function isLegal() {
-  // Your code here
-
-}
-
-function checkForWin() {
-  // Your code here
+function checkForWin(startStack,endStack) {
+  // In checkForWin(), you can simply check if the b stack or c stack has a .length of 4, then consoleing out a message like "You Won!!!" and returning true if a win is detected, or a false if not.
+  if (stacks['c'].length === 4 || stacks['b'].length === 4 ){
+    console.log("You Won !!!");
+    return true;
+  }
+  else {
+    return false;
+  }
 
 }
 
 function towersOfHanoi(startStack, endStack) {
   // Your code here
-
+ isLegal(startStack,endStack);
+ movePiece(startStack,endStack);
+ checkForWin(startStack,endStack);
 }
 
 function getPrompt() {
