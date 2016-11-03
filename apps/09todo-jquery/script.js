b8611ac409f5f6ea94248780dc935f31c5bfc1e9
Alex <alexanderjgaw@gmail.com>
2016-11-03 12:37:02 -0500

    Lesson 9 jQuery To Do app

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -2,4 +2,23 @@
 
 $(document).ready(function() {
   // App logic goes here
+  $('form').submit(function(event) {
+    event.preventDefault();
+
+    if ($('#default-li')) {
+      $('#default-li').remove();
+    }
+
+    var todoText = $(this).find('#todo').val();
+    $('#todo-list').append('<li><a class="done" href="#">Done</a><span class="todo-text">' + todoText + '</span></li>');
+    $('#todo-list').sortable();
+    $('#todo').val('');
+  });
+
+  $('#todo-list').on('click', '.done', function (e) {
+    $(e.target).parent().remove();
+    if ($('#todo-list').children().length === 0) {
+      $('#todo-list').append('<li id="default-li"><span class="todo-text">You finished your list! Feel free to start over!</span></li>');
+    }
+  });
 });
