d4bc51fbdc3ca606f50abb2da6c0c518b0f33b51
mcplunket <maryclaireplunket@Marys-MacBook-Pro-2.local>
2016-11-03 19:03:10 -0500

    Towers of Hanoi App and Lesson 5 HW

--- apps/04TowersOfHanoi.js
+++ apps/04TowersOfHanoi.js
@@ -19,24 +19,46 @@ function printStacks() {
   console.log("c: " + stacks.c);
 }
 
-function movePiece() {
+function movePiece(startStack, endStack) {
   // Your code here
-
+  var piece =  stacks[startStack].pop();
+  stacks[endStack].push(piece);
 }
 
-function isLegal() {
+function isLegal(startStack, endStack) {
   // Your code here
-
+  var topStartStack = stacks[startStack][stacks[startStack].length - 1];
+  var topEndStack = stacks[endStack][stacks[endStack].length - 1];
+  if (stacks[endStack].length === 0){
+    return true;
+  } else if (stacks[startStack].length === 0) {
+    console.log("This is an empty stack")
+    return false;
+  } else {
+    console.log("Value of topStart Stack: " + topStartStack + ", Value of topEndStack: " + topEndStack);
+    return (topStartStack <= topEndStack);
+  }
 }
 
 function checkForWin() {
   // Your code here
-
+ if ((stacks.b.length === 4) || (stacks.c.length === 4)) {
+   console.log("You won!!");
+   return true;
+ } else {
+   return false;
+ }
 }
 
-function towersOfHanoi(startStack, endStack) {
+function towersOfHanoi(startStack,endStack) {
   // Your code here
+  if (isLegal(startStack,endStack)) {
+      movePiece(startStack,endStack);
 
+      checkForWin();
+  } else {
+    return false;
+  }
 }
 
 function getPrompt() {
