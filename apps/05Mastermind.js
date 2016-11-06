4114356f5ea594521fae9b9fa56fa85a449d2e99
Greg Pfaff <greg.pfaff@razorfish.com>
2016-11-06 13:52:49 -0600

    adding working mastermind app

--- apps/05Mastermind.js
+++ apps/05Mastermind.js
@@ -11,6 +11,8 @@ var rl = readline.createInterface({
 var board = [];
 var solution = '';
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+var turns = 10;
+var hint;
 
 function printBoard() {
   for (var i = 0; i < board.length; i++) {
@@ -35,55 +37,44 @@ function generateHint(solution, guess) {
   var solutionArray = solution.split('');
   var guessArray = guess.split('');
   var correctLetters = 0;
-  var correctLetterLocations;
-  var solutionContainer = [];
-  var guessContainer = [];
-  var match = null;
-  var temp;
-
+  var correctLetterLocations = 0;
 
   for (var i = 0; i < solutionArray.length; i++) {
-  /* if (solutionArray.indexOf() === guessArray.indexOf()) {
-      solutionArray = null;}*/
-    console.log("solution array", solutionArray[i]);
-    solutionContainer.push(solutionArray[i]);
-    console.log(solutionContainer);
+    if (solutionArray[i] === guessArray[i]) {
+      correctLetterLocations++;
+      solutionArray[i] = null;
+    }
   }
-
   for (var i = 0; i < guessArray.length; i++) {
-    console.log("guess array", guessArray[i]);
-    guessContainer.push(guessArray[i]);
-    console.log(guessContainer);
+    var match = solutionArray.indexOf(guessArray[i]);
+    console.log('match', match)
+    if ( match > -1 ) {
+      correctLetters++;
+      solutionArray[match] = null;
+    }
   }
 
-  for (var i = 0; i < guessArray.length; i++) {
-    temp = solutionArray[i].indexOf(guessArray[i]);
-    console.log('temp', temp)
-    if ( temp === -1 )
-      match = false;
-  }
-  console.log(match);
-  return match;
+  console.log('correctLetters', correctLetters)
+  console.log('correctLetterLocations', correctLetterLocations);
+  return correctLetters + "-" + correctLetterLocations;
 
-  /*while (guessArray !== solutionArray) {
-    correctLetters++
-    console.log('correctLetters', correctLetters);
-  }*/
 }
 
 function mastermind(guess) {
   // your code here
-  var counter = 0;
-
+  hint;
   console.log(guess);
   console.log(solution);
+  console.log(hint);
 
   if (guess !== solution) {
     
-    board.push(guess);
-    console.log('board', board);
-
-    generateHint(solution, guess);
+    var hint = generateHint(solution, guess);
+    board.push(hint);
+    turns--;
+    console.log("Turns remaining " + turns);
+    console.log('boards', board);
+    return "Guess again!";
 
     if (board.length > 10) {
       return "Game over!"
@@ -91,21 +82,6 @@ function mastermind(guess) {
   } else if (guess === solution) {
       return "You guessed it!";
   }
-  return guess;
-  /*var correctLetterLocations = 0;
-  var correctLetters = 0;
-
-  if (guessArray contains any correctLetters) that werent in correct location
-
-  for (var i = 0; i < solutionArray.length; i++) {
-    results.push(solutionArray[i]);
-
-    var targetIndex;
-
-    if (targetIndex > -1) {
-      correctLetters++
-    }
-  }*/
 }
 
 
