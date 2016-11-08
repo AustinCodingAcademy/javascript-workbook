1d1143632809a7d6611473de833c6444b5c9997d
ssfiero <ssfiero@yahoo.com>
2016-11-07 21:48:33 -0600

    Lesson 06 - Homework - Complete

--- homework/06LessonSix.js
+++ homework/06LessonSix.js
@@ -12,8 +12,9 @@ var assert = require('assert');
 // Associative Array Refresh
 // Add properties x: 1, y: 2 to point to make it a point
 // in two dimensional space.
-var point = {
-};
+
+// var point = {
+// };
 
 var point = {
   x: 1,
@@ -26,8 +27,9 @@ var point = {
 // In order to do this, you should add parameters x and y
 // as properties to "this".
 // example: this.propName = propValue;
-function Point(x, y) {
-}
+
+// function Point(x, y) {
+// }
 
 function Point(x, y) {
   this.x = x;
@@ -38,8 +40,10 @@ function Point(x, y) {
 // Create a new point using the class constructor Point.
 // Set x to 5, and y to -3
 // This can be done as follows: new Point(someX, someY)
-var anotherPoint;
 
+// var anotherPoint;
+
+var anotherPoint = new Point(5, -3);
 
 
 // ****
@@ -50,6 +54,8 @@ var anotherPoint;
 // What is the difference between point (an object literal, or associative array)
 // and anotherPoint (a point object constructed from the Point class)?
 //
+// An array is used to store multiple values in a single variable (this is point).  An object has characteristics and can be considered part of a class (this is anotherPoint).  These are both basically the same, since the array is essentially an object in js.
+// The difference between point and anotherPoint is that they have different (x, y) values.
 //
 
 // ****
@@ -87,13 +93,25 @@ function ConferenceRoom() {
 // Problem 4:
 // finish the definition of class Person()
 // Add a method called sayHi() that returns "Hi, I'm " + this.name + '!';
+
+// function Person(name) {
+//   this.name = name;
+// }
+
 function Person(name) {
   this.name = name;
+  this.sayHi = function() {
+    return "Hi, I'm " + this.name + '!';
+  }
 }
 
 // Problem 5:
 // create a new Person named Jen
-var jen;
+
+// var jen;
+
+var jen = new Person('Jen');
+
 
 // Problem 6:
 // add jen to the conferenceRoom
@@ -101,6 +119,8 @@ var conferenceRoom = new ConferenceRoom();
 // you can do this by calling the method enter() as follows:
 // conferenceRoom.enter(somePerson);
 // add your code for Problem 6 here
+
+conferenceRoom.enter(jen);
 
 
 // ****
@@ -110,6 +130,7 @@ var conferenceRoom = new ConferenceRoom();
 //
 // What is a method?
 //
+// A method is a object property that contains a function definition.
 //
 
 // ****
