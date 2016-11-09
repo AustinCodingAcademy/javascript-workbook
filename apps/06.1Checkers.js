17db39e226c5b7e48c2af02e0b8007457becb08b
Greg Pfaff <greg.pfaff@razorfish.com>
2016-11-09 11:22:35 -0600

    adding scoping

--- apps/06.1Checkers.js
+++ apps/06.1Checkers.js
@@ -58,8 +58,18 @@ function Board() {
 
   this.checkers = [];
   this.createCheckers = function (checker) {
+
+    var whitePositions;
+    var blackPositions;
+    var whiteRow;
+    var whiteColumn;
+    var blackRow;
+    var blackColumn;
+    
     whitePositions = [[0, 1], [0, 3], [0, 5], [0, 7], [1, 0], [1, 2], [1, 4], [1, 6], [2, 1], [2, 3], [2, 5], [2, 7]];
     blackPositions = [[5, 0], [5, 2], [5, 4], [5, 6], [6, 1], [6, 3], [6, 5], [6, 7], [7, 0], [7, 2], [7, 4], [7, 6]];
+
+
 
     for (var i = 0; i < whitePositions.length; i++) {
 
@@ -81,7 +91,7 @@ function Board() {
       console.log("black position", this.checkers.push(this.grid[blackRow][blackColumn]));
     }
     return console.log(this.checkers);
-  }
+  };
 }
 
 function Game() {
