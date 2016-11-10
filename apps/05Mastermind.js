fa5590a6f455b95a2a569c025011e91eeea0c31a
Clint Lucio <clintlucio@gmail.com>
2016-11-10 15:48:51 -0600

    apps/05Mastermind.js

--- apps/05Mastermind.js
+++ apps/05Mastermind.js
@@ -4,8 +4,8 @@ var assert = require('assert');
 var colors = require('colors/safe');
 var readline = require('readline');
 var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
 var board = [];
@@ -13,68 +13,98 @@ var solution = '';
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
 function printBoard() {
-  for (var i = 0; i < board.length; i++) {
-    console.log(board[i]);
-  }
+    for (var i = 0; i < board.length; i++) {
+        console.log(board[i]);
+    }
 }
 
 function generateSolution() {
-  for (var i = 0; i < 4; i++) {
-    var randomIndex = getRandomInt(0, letters.length);
-    solution += letters[randomIndex];
-  }
+    for (var i = 0; i < 4; i++) {
+        var randomIndex = getRandomInt(0, letters.length);
+        solution += letters[randomIndex];
+    }
 }
 
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateHint(solution, guess) {//added solution and guess
-  // your code here
-  var solutionArray = solution.split('');
-  var guessArray = guess.split('');
+function generateHint(solution, guess) { //added solution and guess
+    // your code here
+    var solutionArray = solution.split('');
+    var guessArray = guess.split('');
+    var correctLetterLocations = 0;
+    for (var i = 0; i < 4; i++) {
+        if (solutionArray[i] === guessArray[i]) {
+            correctLetterLocations++;
+            solutionArray[i] = null;
+        }
+    }
+    var correctLetters = 0;
+    var targetIndex = 0;
+    for (var i = 0; i < 4; i++) {
+        targetIndex = solutionArray.indexof(guessArray[i]);
+        if (targetIndex > -1) {
+            correctLetters++;
+            solutionArray[targetIndex] = null;
+        }
+    }
+    return correctLetterLocations + "-" + correctLetters;
 }
 
 function mastermind(guess) {
-  // your code here
+    // your code here
+    solution = 'abcd';
+    if (guess === solution) {
+        winCheck = true;
+        return "You guessed correctly!";
+    } else {
+        var hint = generateHint(solution, guess);
+        board.push(guess + hint);
+    }
 }
 
-
 function getPrompt() {
-  rl.question('guess: ', (guess) => {
-    console.log( mastermind(guess) );
-    printBoard();
-    getPrompt();
-  });
+    if (winCheck === false) {
+        promt.get(['guess'], function(error, result) {
+            if (board.length === 10) {
+                console.log('You are out of turns! The correct solution was ' + solution)
+            } else {
+                console.log(mastermind(result['guess']));
+                printBoard();
+                getPrompt();
+            }
+        });
+    }
 }
 
 // Tests
 
 if (typeof describe === 'function') {
 
-  describe('#mastermind()', function () {
-    it('should register a guess and generate hints', function () {
-      solution = 'abcd';
-      mastermind('aabb');
-      assert.equal(board.length, 1);
-    });
-    it('should be able to detect a win', function () {
-      assert.equal(mastermind(solution), 'You guessed it!');
-    });
-  });
-
-  describe('#generateHint()', function () {
-    it('should generate hints', function () {
-      assert.equal(generateHint('abcd', 'abdc'), '2-2');
-    });
-    it('should generate hints if solution has duplicates', function () {
-      assert.equal(generateHint('abcd', 'aabb'), '1-1');
+    describe('#mastermind()', function() {
+        it('should register a guess and generate hints', function() {
+            solution = 'abcd';
+            mastermind('aabb');
+            assert.equal(board.length, 1);
+        });
+        it('should be able to detect a win', function() {
+            assert.equal(mastermind(solution), 'You guessed it!');
+        });
     });
 
-  });
+    describe('#generateHint()', function() {
+        it('should generate hints', function() {
+            assert.equal(generateHint('abcd', 'abdc'), '2-2');
+        });
+        it('should generate hints if solution has duplicates', function() {
+            assert.equal(generateHint('abcd', 'aabb'), '1-1');
+        });
+
+    });
 
 } else {
 
-  generateSolution();
-  getPrompt();
+    generateSolution();
+    getPrompt();
 }
