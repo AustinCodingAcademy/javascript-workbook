eaff9804498238b4d212ef942125f50d8c76f3a4
Jonathan Soape <jonathansoape@gmail.com>
2016-11-11 22:31:34 -0600

    Mastermind all tests passed

--- apps/05Mastermind.js
+++ apps/05Mastermind.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var assert = require('assert');
-// var colors = require('colors/safe');
+var colors = require('colors/safe');
 var readline = require('readline');
 var rl = readline.createInterface({
   input: process.stdin,
@@ -11,8 +11,6 @@ var rl = readline.createInterface({
 var board = [];
 var solution = '';
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-
-
 
 function printBoard() {
   for (var i = 0; i < board.length; i++) {
@@ -35,39 +33,41 @@ function generateHint(solution, guess) {
   // your code here
   var solutionArray = solution.split('');
   var guessArray = guess.split('');
+
   var correctLetterLocations = 0;
   var correctLetters = 0;
+
   for (var i = 0; i < solutionArray.length; i++){
     if (solutionArray[i] === guessArray[i]){
       correctLetterLocations++;
       solutionArray[i] = null;
     }
   }
+
   for (var i = 0; i < solutionArray.length; i++){
     var targetIndex = solutionArray.indexOf(guessArray[i]);
-    if (targetIndex > -1){
+    if(targetIndex != -1) {
       correctLetters++;
-      solutionArray[i] = null;
+      solutionArray[targetIndex] = null;
     }
-  };
 
-
-  return (correctLetterLocations,'-', correctLetters);
-  var hint = generateHint(guess);
-  board.push(guess, hint);
-  if (board.length === 10){
-    return ("You ran out of turns! The solution was ", solution);
-  } else{
-    return ("Guess again.");
   }
+
+  return correctLetterLocations + '-' + correctLetters;
 }
 
-function mastermind(solution, guess) {
+
+function mastermind(guess) {
   // your code here
-  if (guess === solution) {
-    return("You guessed it!");
+  if (guess === solution){
+    return ("You guessed it!");
+  }
+  var hint = generateHint(solution, guess);
+  board.push(guess + ' ' + hint);
+  if (board.length === 10){
+    return ('You ran out of turns! The solution was', solution);
   } else {
-    generateHint(solution, guess);
+    return ('Guess again.');
   }
 }
 
