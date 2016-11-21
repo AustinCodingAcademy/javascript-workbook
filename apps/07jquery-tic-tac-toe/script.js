c3b9245c47d1136ab49eee3ba15e5eed983124db
Greg Pfaff <greg.pfaff@razorfish.com>
2016-11-21 00:01:03 -0600

    fixing broken jquery script

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -30,6 +30,7 @@ $(document).on('ready', function() {
   }
   function maxMoves() {
     if (turns >= 8) {
+      alert("Game is a tie, resetting")
       $('#announce-winner').text('Game is a draw, resetting board');
       resetGame();
     }
@@ -51,15 +52,15 @@ $(document).on('ready', function() {
     console.log('Check for win');
     //var wins = [[0,1,2], [3,4,5] [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
-    var val0 = $('[data-cell='0']').text();
-    var val1 = $('[data-cell='1']').text();
-    var val2 = $('[data-cell='2']').text();
-    var val3 = $('[data-cell='3']').text();
-    var val4 = $('[data-cell='4']').text();
-    var val5 = $('[data-cell='5']').text();
-    var val6 = $('[data-cell='6']').text();
-    var val7 = $('[data-cell='7']').text();
-    var val8 = $('[data-cell='8']').text();
+    var val0 = $('[data-cell="0"]').text();
+    var val1 = $('[data-cell="1"]').text();
+    var val2 = $('[data-cell="2"]').text();
+    var val3 = $('[data-cell="3"]').text();
+    var val4 = $('[data-cell="4"]').text();
+    var val5 = $('[data-cell="5"]').text();
+    var val6 = $('[data-cell="6"]').text();
+    var val7 = $('[data-cell="7"]').text();
+    var val8 = $('[data-cell="8"]').text();
 
     console.log($playerTurn);
 
