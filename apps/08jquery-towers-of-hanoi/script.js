b7d2e7eff02a559a0da7b9e6711d3a1db71f7f5e
James Puckett <jamesjaypuckett@gmail.com>
2016-11-21 11:00:27 -0600

    TOH

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -2,4 +2,10 @@
 
 $(document).ready(function() {
   // Put app logic here
+  $('data-stack').click(function(){
+    var block = $('this').children().last();
+    // $('data-stack').click(function(){
+    //   if(block)
+    // });
+  });
 }
