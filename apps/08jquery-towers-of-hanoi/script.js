3ae4d26bc80819341f1fcff43ea8c5dfb7e6c97c
Greg Pfaff <greg.pfaff@razorfish.com>
2016-11-26 08:23:31 -0600

    adding WIP hanoi app

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -2,4 +2,26 @@
 
 $(document).ready(function() {
   // Put app logic here
-}
+  var $dataStack = $("[data-stack]");
+  var $clickedStack;
+  var $block = null;
+
+  $dataStack.click(function(e){
+
+    $clickedStack = $(this);
+
+    if (!$block) {
+      $block = $clickedStack.children().last().detach();
+    } else {
+       $clickedStack.append($block);
+       $block = null;
+       checkForWin();
+    }
+
+    function checkForWin(){
+      if ($('[data-stack="2"]').children().length === 4 || $('[data-stack="3"]').children().length === 4) {
+       $('#announce-game-won').text('You Won!');
+      }
+    }
+  });
+});
