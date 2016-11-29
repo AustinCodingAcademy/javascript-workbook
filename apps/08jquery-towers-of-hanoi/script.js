f273095d0ef3fb97a3935b5b26b958fe246e5467
Greg Pfaff <greg.pfaff@razorfish.com>
2016-11-29 09:10:46 -0600

    adding fixes

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -10,18 +10,44 @@ $(document).ready(function() {
 
     $clickedStack = $(this);
 
+    // check to see if block exists and is true if so set the variable.
     if (!$block) {
       $block = $clickedStack.children().last().detach();
-    } else {
-       $clickedStack.append($block);
-       $block = null;
+    } else if (isLegal($block, $(this).children().last())){
        checkForWin();
-    }
-
-    function checkForWin(){
-      if ($('[data-stack="2"]').children().length === 4 || $('[data-stack="3"]').children().length === 4) {
-       $('#announce-game-won').text('You Won!');
-      }
-    }
+     }
   });
+
+  function isLegal($start, $end) {
+
+    console.log('start', $start.get(0));
+    console.log('end', $end.get(0));
+    console.log($clickedStack.get(0));
+    console.log($clickedStack.children().length);
+    
+    if ($start.attr('data-block') < $end.attr('data-block') || $clickedStack.children().length === 0) {
+      $block.detach();
+
+      $clickedStack.append($block);
+    
+      $block = null;
+      console.log("if block", $block);
+    } else {
+        alert("You cannot make that move");
+    }
+    $block = null;
+    console.log("global block", $block);
+
+  }
+
+  function checkForWin(){
+    console.log("Check for win");
+    /*$dataStack.each(function(e) {
+      console.log(e.get(0));
+    })*/
+    /*if ($('[data-stack="2"]').children().length === 4 || $('[data-stack="3"]').children().length === 4) {
+     return $('#announce-game-won').text('You Won!');
+    }*/
+  }
+
 });
