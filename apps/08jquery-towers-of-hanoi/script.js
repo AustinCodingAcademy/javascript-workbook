0dc1104bb108803edda6c9ebbbfc37d713272747
Greg Pfaff <greg.pfaff@razorfish.com>
2016-11-30 09:35:40 -0600

    removing comments

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -49,34 +49,3 @@ $(document).ready(function() {
 
 });
 
-
-/*'use strict';
-
-$(document).ready(function() {
-  // Put app logic here
-  var $block = null;
-  var play = true;
-
-  function isLegal($blockToMove, $destinationBlock) {
-    return !($blockToMove.data('block') > $destinationBlock.data('block'));
-  }
-
-  function checkForWin() {
-    if ($('[data-stack="2"]').children().length === 4 || $('[data-stack="3"]').children().length === 4) {
-      $('#announce-game-won').text('You Won!');
-      play = false;
-    }
-  }
-  $('div[data-stack]').click(function () {
-    if (!$block && play) {
-      $block = $(this).children().last().detach();
-    } else if (isLegal($block, $(this).children().last()) && play) {
-      $(this).append($block);
-      $block = null;
-      checkForWin();
-    }
-
-  })
-
-
-});*/
