353f9e6b9b717c6b798788bd4df2de187ea89ca0
jamesku <jameskuhr@gmail.com>
2016-11-30 18:24:46 -0600

    one test fails because i made it so that if you click for an illegal play, it replaces the block.  also, there is a cheat code: type cheat on a new screen and it replicates a stack in the 3rd column for a win

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -1,5 +1,48 @@
 'use strict';
+    var $block = null;
+    var $oldstack = null;
+$(document).ready(function () {
+    $('[data-stack]').click(function () {
+        var $this = $(this);
+        if ($block === null) {
+            $oldstack = $this;
+            $block = $oldstack.children().last().detach();
+        } 
+        else {
+            var $lastblock = $this.children().last().data("block");
+            if ($block.data("block") < $lastblock || !$lastblock) {
+                $this.append($block);
+                $block = null;
+                 checkForWin($this);
+            } else {
+                alert("please play a legal play");
+                $oldstack.append($block);
+                $block = null;
+               
+            }
+        }
+    });
+});
 
-$(document).ready(function() {
-  // Put app logic here
-}
+function checkForWin($this){
+            if ($this.data("stack")>1){
+            if ($this.children().length === 4){
+            $("#announce-game-won").text('You Won!');
+            }
+        }
+};
+
+$(function() {
+    var total = 0;
+   $(window).keypress(function(e) {
+       var key = e.which;
+       total += parseInt(key);
+       if(total === 517){
+           $('#winnerStack').append('<div data-block="100"></div>');
+           $('#winnerStack').append('<div data-block="75"></div>');
+           $('#winnerStack').append('<div data-block="50"></div>');
+           $('#winnerStack').append('<div data-block="25"></div>');
+        checkForWin($('#winnerStack'));
+       }
+   });
+});
