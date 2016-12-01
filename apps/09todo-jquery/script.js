207a8aa197ec30f8eca1fa28899eee8baf2ab097
Greg Pfaff <greg.pfaff@razorfish.com>
2016-11-30 18:54:21 -0600

    adding working app

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -1,5 +1,22 @@
 'use strict';
 
 $(document).ready(function() {
-  // App logic goes here
+  var $form = $('form');
+  var $this = $(this);
+  var $value;
+  var $toDoList = $('#todo-list');
+  var $listItem = $('li');
+
+  $form.submit(function(event) {
+    event.preventDefault();
+    $value = $this.find('input[type="text"]').val();
+    $toDoList.append('<li>' + $value + '</li>');
+    $toDoList.sortable();
+  })
+
+  $listItem.click(function(event) {
+    event.preventDefault();
+    console.log(this)
+    $this.find().remove();
+  })
 });
