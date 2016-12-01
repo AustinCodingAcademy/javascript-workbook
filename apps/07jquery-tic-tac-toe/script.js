eda9bfa343e5945bb66112d25a23851f70cd1467
ssfiero <ssfiero@yahoo.com>
2016-12-01 00:24:19 -0600

    Lesson 07 JQuery - Tic-Tac-Toe App

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -1,5 +1,23 @@
 'use strict';
 
+
 $(document).on('ready', function() {
-  // Put app logic in here
-});
+
+  // Spec 1
+  var $playerTurn = $('X');
+
+  $('[data-cell]').click(function(){
+    $('this').text($playerTurn);
+
+    // Spec 2
+    // This is the switch to next player, ternary operator
+    function $toggleplayerTurn() {
+      $playerTurn = ($playerTurn === 'X') ? 'O' : 'X';
+    }
+
+
+
+    })
+
+
+  });
