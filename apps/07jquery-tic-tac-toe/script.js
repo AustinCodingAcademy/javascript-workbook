d28cc0d33c131a2a42921d1e74590ef5dae8b9c3
RodrigoPaquot <rodrigopaquot@Rodrigos-MacBook-Pro.local>
2016-12-01 20:03:12 -0600

    Created tic tac toe app, using jqueary and with the help of specs from the workbook

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -1,5 +1,100 @@
 'use strict';
 
 $(document).on('ready', function() {
-  // Put app logic in here
+
+var $datacell = $('[data-cell]');
+var playerTurn = "X";
+var $button = $('#clear');
+
+
+
+
+
+
+var datacells0 = $('[data-cell="0"]').text();
+var datacells1 = $('[data-cell="1"]').text();
+var datacells2 = $('[data-cell="2"]').text();
+var datacells3 = $('[data-cell="3"]').text();
+var datacells4 = $('[data-cell="4"]').text();
+var datacells5 = $('[data-cell="5"]').text();
+var datacells6 = $('[data-cell="6"]').text();
+var datacells7 = $('[data-cell="7"]').text();
+var datacells8 = $('[data-cell="8"]').text();
+
+
+
+$datacell.click(function() {
+
+$(this).text(playerTurn)
+
+checkForWin();
+
+playerTurn = (playerTurn === "X") ? "O" : "X";
+});
+
+
+$button.click(function() {
+$datacell.empty();
+$("#announce-winner").empty();
+playerTurn = "X";
+});
+
+
+//
+
+function horizontalWin() {
+
+if (datacells0 === playerTurn && datacells1 === playerTurn && datacells2 === playerTurn ||
+datacells3 === playerTurn && datacells4 === playerTurn && datacells5 === playerTurn ||
+datacells6 === playerTurn && datacells7 === playerTurn && datacells8 === playerTurn) {
+
+return true;
+
+}
+}
+
+function verticalWin() {
+if (datacells0 === playerTurn && datacells3 === playerTurn && datacells6 === playerTurn ||
+datacells1 === playerTurn && datacells4 === playerTurn && datacells7 === playerTurn ||
+datacells2 === playerTurn && datacells5 === playerTurn && datacells8 === playerTurn) {
+
+return true;
+}
+}
+
+function diagonalWin() {
+console.log('test2: ' + datacells8);
+if (datacells0 === playerTurn && datacells4 === playerTurn && datacells8 === playerTurn ||
+datacells2 === playerTurn && datacells4 === playerTurn && datacells6 === playerTurn) {
+
+return true;
+}
+
+}
+
+function checkForWin() {
+
+datacells0 = $('[data-cell="0"]').text();
+datacells1 = $('[data-cell="1"]').text();
+datacells2 = $('[data-cell="2"]').text();
+datacells3 = $('[data-cell="3"]').text();
+datacells4 = $('[data-cell="4"]').text();
+datacells5 = $('[data-cell="5"]').text();
+datacells6 = $('[data-cell="6"]').text();
+datacells7 = $('[data-cell="7"]').text();
+datacells8 = $('[data-cell="8"]').text();
+
+if (horizontalWin() || verticalWin() || diagonalWin()) {
+
+
+
+$('#announce-winner').text('Player ' + playerTurn + ' Wins!');
+
+return true;
+
+}
+
+}
+
+
 });
