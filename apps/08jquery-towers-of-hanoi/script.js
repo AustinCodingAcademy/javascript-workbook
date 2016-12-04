da0ac490b775dc7d8e46e662c8c2634012b1aedf
chrisperk <christopherperkins.512@gmail.com>
2016-12-04 02:09:22 -0600

    Towers of Hanoi (jQuery), all tests passing, WIP

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -1,5 +1,23 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
-}
+    // Put app logic here
+    var $sendingStackTopBlock = null;
+
+    function checkForWin() {
+        return ($('[data-stack=2]').children().length === 4) || ($('[data-stack=3]').children().length === 4)
+    }
+
+    $('[data-stack]').click(function() {
+        if (!$sendingStackTopBlock) {
+            $sendingStackTopBlock = $(this).children().last().detach();
+        } else {
+            if ($(this).children().last().data('block') > $sendingStackTopBlock.data('block') || $(this).children().last().data('block') == undefined) {
+                $(this).append($sendingStackTopBlock);
+                $sendingStackTopBlock = null;
+            }
+        } if (checkForWin()) {
+            $('#announce-game-won').text('you won!');
+        }
+    })
+})
