9b8e3dd4bcc0229d415abea1c50f119971894cf7
jamesku <jameskuhr@gmail.com>
2016-12-04 18:10:29 -0600

    Battleship works, with an AI that plays against you, i tested it a bit and it passes all the unique situations i could think of clustered ships, ships along the border, etc..

--- apps/battleship/setUpEnemyShips.js
+++ apps/battleship/setUpEnemyShips.js
@@ -1,9 +1,12 @@
+//this sets up the enemy ships, we dont need them to be visually represented anywhere, so we will set them up in a javascript array and refer to this array everytime the player shoots at the game to determine a hit or a miss.
+
 var ebattleship;
 var eaircraft;
 var edestroyer;
 var esubmarine;
 var patrol;
 
+//sets up the array
 var enemyBoard = new Array(10);
 
 for (i = 0; i < 10; i++) {
@@ -28,71 +31,77 @@ placeShip(2, 'D');
 function placeShip(numberOfSpots, symbol) {
     var placed = false;
 
+    //we try to place a ship starting at a random spot, if that ship hits the end of the board or another ship, we try again.
     while (!placed) {
 
+        //get random start point and which way the shop is going to go
         var direction = getRandomInt(0, 3);
         var xCoordinate = getRandomInt(0, 9);
         var yCoordinate = getRandomInt(0, 9);
-        
+
+        //depening on the direction we try to place the ship, the if statement ensures that the ship will not go off the board
         switch (direction) {
         case 0:
-            if(yCoordinate+numberOfSpots <10){
-            placed = tryToPlace0(numberOfSpots, symbol,xCoordinate,yCoordinate);
+            if (yCoordinate + numberOfSpots < 10) {
+                placed = tryToPlace0(numberOfSpots, symbol, xCoordinate, yCoordinate);
             }
             break;
         case 1:
-            if(xCoordinate+numberOfSpots <10){
-            placed = tryToPlace1(numberOfSpots, symbol,xCoordinate,yCoordinate);
+            if (xCoordinate + numberOfSpots < 10) {
+                placed = tryToPlace1(numberOfSpots, symbol, xCoordinate, yCoordinate);
             }
             break;
         case 2:
-            if(xCoordinate-numberOfSpots > -1){
-            placed = tryToPlace2(numberOfSpots, symbol,xCoordinate,yCoordinate);
+            if (xCoordinate - numberOfSpots > -1) {
+                placed = tryToPlace2(numberOfSpots, symbol, xCoordinate, yCoordinate);
             }
             break;
         case 3:
-            if(yCoordinate-numberOfSpots > -1){
-            placed = tryToPlace3(numberOfSpots, symbol,xCoordinate,yCoordinate);
+            if (yCoordinate - numberOfSpots > -1) {
+                placed = tryToPlace3(numberOfSpots, symbol, xCoordinate, yCoordinate);
             }
             break;
         default:
-        placed = false;
+            //if we dont place a ship, do it again.
+            placed = false;
         }
     }
 }
 
 
-
-function tryToPlace0(numberOfSpots, symbol,xCoordinate,yCoordinate) {
+//i separated out these functions because they were easy to replicate and genearlly very self contained, i could have made it one big switch statement, but these actually play the ships by filling out the array with the appropriate letter - passed through by the symbol parameter- in the right configuration for each ship, depending on transformation of the x and y coordinates 
+function tryToPlace0(numberOfSpots, symbol, xCoordinate, yCoordinate) {
 
     //save the initial board
     var initialBoard = enemyBoard;
 
-    //try to place a ship in the direction randomly selected
+    //try to place a ship using the right number of spots
     for (i = 0; i < numberOfSpots; i++) {
+        //here is where the ship direction comes into play, transforming the coordinates
         if (enemyBoard[xCoordinate][yCoordinate + i] === "-") {
             enemyBoard[xCoordinate][yCoordinate + i] = symbol;
-            if (i === numberOfSpots-1) {
+            if (i === numberOfSpots - 1) {
                 return true;
             }
         } else {
+            //if we cant place it, put back the inital board and return false so we can try again.
             enemyBoard = initialBoard;
             return false;
         }
     }
 }
 
-                
-function tryToPlace1(numberOfSpots, symbol,xCoordinate,yCoordinate) {
+
+function tryToPlace1(numberOfSpots, symbol, xCoordinate, yCoordinate) {
 
     //save the initial board
     var initialBoard = enemyBoard;
-    
+
     //try to place a ship in the direction randomly selected
     for (i = 0; i < numberOfSpots; i++) {
-        if (enemyBoard[xCoordinate+i][yCoordinate] === "-") {
-            enemyBoard[xCoordinate+i][yCoordinate] = symbol;
-            if (i === numberOfSpots-1) {
+        if (enemyBoard[xCoordinate + i][yCoordinate] === "-") {
+            enemyBoard[xCoordinate + i][yCoordinate] = symbol;
+            if (i === numberOfSpots - 1) {
                 return true;
             }
         } else {
@@ -101,17 +110,17 @@ function tryToPlace1(numberOfSpots, symbol,xCoordinate,yCoordinate) {
         }
     }
 }
-                
-function tryToPlace2(numberOfSpots, symbol,xCoordinate,yCoordinate) {
+
+function tryToPlace2(numberOfSpots, symbol, xCoordinate, yCoordinate) {
 
     //save the initial board
     var initialBoard = enemyBoard;
-    
+
     //try to place a ship in the direction randomly selected
     for (i = 0; i < numberOfSpots; i++) {
-        if (enemyBoard[xCoordinate-i][yCoordinate] === "-") {
-            enemyBoard[xCoordinate-i][yCoordinate] = symbol;
-            if (i === numberOfSpots-1) {
+        if (enemyBoard[xCoordinate - i][yCoordinate] === "-") {
+            enemyBoard[xCoordinate - i][yCoordinate] = symbol;
+            if (i === numberOfSpots - 1) {
                 return true;
             }
         } else {
@@ -120,17 +129,17 @@ function tryToPlace2(numberOfSpots, symbol,xCoordinate,yCoordinate) {
         }
     }
 }
-                
-function tryToPlace3(numberOfSpots, symbol,xCoordinate,yCoordinate) {
+
+function tryToPlace3(numberOfSpots, symbol, xCoordinate, yCoordinate) {
 
     //save the initial board
     var initialBoard = enemyBoard;
-    
+
     //try to place a ship in the direction randomly selected
     for (i = 0; i < numberOfSpots; i++) {
         if (enemyBoard[xCoordinate][yCoordinate - i] === "-") {
             enemyBoard[xCoordinate][yCoordinate - i] = symbol;
-            if (i === numberOfSpots-1) {
+            if (i === numberOfSpots - 1) {
                 return true;
             }
         } else {
@@ -139,9 +148,9 @@ function tryToPlace3(numberOfSpots, symbol,xCoordinate,yCoordinate) {
         }
     }
 }
-                
-                
-                
+
+
+//random integer function
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
--- apps/battleship/setUpYourShips.js
+++ apps/battleship/setUpYourShips.js
@@ -1,3 +1,5 @@
+//this lets the user set up their own ships
+
 var shipWeArePlacingCurrently = null;
 var startPoint = "null";
 var secondPoint = null;
@@ -5,12 +7,14 @@ var symbol = null;
 var totalPlaced = 0;
 $(document).ready(function () {
 
+    //when they click on the button, explain to them how it works, and how many spaces they get
     $("#carrier").click(function () {
         shipWeArePlacingCurrently = 5;
         symbol = "A";
-        alert("OK lets place a 5 spot AIRCRATCARRIER, click on the tile you want the front of the ship to be on, and then click on the tile just above, below, to the left or the right to determine the direction of placement");
+        alert("OK lets place a 5 spot AIRCRAFT CARRIER, click on the tile you want the front of the ship to be on, and then click on the tile just above, below, to the left or the right to determine the direction of placement");
         $('#carrier').prop('disabled', true);
-        startPoint = "null";
+        //make sure the startpoint is reset before doing the 2 click loop below
+        startPoint = null;
     });
 
     $("#battleship").click(function () {
@@ -18,7 +22,7 @@ $(document).ready(function () {
         symbol = "B";
         alert("OK lets place a 4 spot BATTLESHIP, click on the tile you want the front of the ship to be on, and then click on the tile just above, below, to the left or the right to determine the direction of placement");
         $('#battleship').attr('disabled', 'disabled');
-        startPoint = "null";
+        startPoint = null;
     });
 
     $("#cruiser").click(function () {
@@ -26,7 +30,7 @@ $(document).ready(function () {
         symbol = "C";
         alert("OK lets place a 3 spot CRUISER, click on the tile you want the front of the ship to be on, and then click on the tile just above, below, to the left or the right to determine the direction of placement");
         $('#cruiser').attr('disabled', 'disabled');
-        startPoint = "null";
+        startPoint = null;
     });
 
     $("#sub").click(function () {
@@ -34,42 +38,46 @@ $(document).ready(function () {
         symbol = "S";
         alert("OK lets place a 3 spot SUBMARINE, click on the tile you want the front of the ship to be on, and then click on the tile just above, below, to the left or the right to determine the direction of placement");
         $('#sub').attr('disabled', 'disabled');
-        startPoint = "null";
+        startPoint = null;
     });
 
     $("#destroyer").click(function () {
         shipWeArePlacingCurrently = 2;
         symbol = "D";
-        alert("OK lets place a 5 spot carrier, click on the tile you want the front of the ship to be on, and then click on the tile just above, below, to the left or the right to determine the direction of placement");
+        alert("OK lets place a 2 spot DESTROYER, click on the tile you want the front of the ship to be on, and then click on the tile just above, below, to the left or the right to determine the direction of placement");
         $('#destroyer').attr('disabled', 'disabled');
-        startPoint = "null";
+        startPoint = null;
     });
 
-
+    //this makes the first click be the startPoint and the second click be the secondPoint, so we can determine where and which direction the ship will go
     $('.innerRow').click(function () {
         var $this = $('this');
-        if (startPoint === "null") {
+        if (!startPoint) {
             startPoint = this.id.toString();
         } else {
             secondPoint = this.id.toString();
-            runchecks(startPoint, secondPoint);
+            if (totalPlaced < 5) {
+                runchecks(startPoint, secondPoint);
+            }
         }
     });
 
 });
 
+//this function breaks out the coordiantes from the elements and determines which direction the ship is going.
 function runchecks(startPoint, secondPoint) {
-    //    alert(startPoint);
-    //    alert(secondPoint);
+
     var x1 = parseInt(startPoint[0]);
     var y1 = parseInt(startPoint[2]);
     var x2 = parseInt(secondPoint[0]);
     var y2 = parseInt(secondPoint[2]);
     var direction = null;
     if (x1 - x2 > 0) {
+        //this does two things, determines the direction and ensures that the ship will fit on the board
         if (x1 - shipWeArePlacingCurrently > -1) {
             direction = "left";
         } else {
+            //reset the button if the ship doesnt fit on the board
             reButton();
             return;
         }
@@ -99,35 +107,20 @@ function runchecks(startPoint, secondPoint) {
             return;
         }
     }
-
-
-    if (x1 - x2 < 0) {
-        direction = "right";
-    }
-    if (y1 - y2 > 0) {
-        direction = "down";
-    }
-    if (y1 - y2 < 0) {
-        direction = "up";
-    }
+    direction = "up";
 
     switch (direction) {
     case "left":
         tryToPlaceleft(x1, y1);
-
         break;
     case "right":
         tryToPlaceRight(x1, y1);
-
         break;
     case "up":
         tryToPlaceUp(x1, y1);
-
         break;
-
     case "down":
         tryToPlaceDown(x1, y1);
-
         break;
     default:
         return;
@@ -145,7 +138,6 @@ function tryToPlaceleft(x1, y1) {
     var necessarySquares = new Array();
     for (i = 0; i < shipWeArePlacingCurrently; i++) {
         var currentId = (x1 - i) + "," + y1;
-        //jquery was being annoying, used vanilla javascript
         var temp = document.getElementById(currentId).innerText;
         if (temp !== '-') {
             reButton();
@@ -155,7 +147,7 @@ function tryToPlaceleft(x1, y1) {
     for (i = 0; i < shipWeArePlacingCurrently; i++) {
         var currentId = (x1 - i) + "," + y1;
         document.getElementById(currentId).innerText = symbol;
-        
+
     }
     totalPlacedCounter();
     return true;
@@ -166,7 +158,6 @@ function tryToPlaceRight(x1, y1) {
     var necessarySquares = new Array();
     for (i = 0; i < shipWeArePlacingCurrently; i++) {
         var currentId = (x1 + i) + "," + y1;
-        //jquery was being annoying, used vanilla javascript
         var temp = document.getElementById(currentId).innerText;
         if (temp !== '-') {
             reButton();
@@ -176,9 +167,8 @@ function tryToPlaceRight(x1, y1) {
     for (i = 0; i < shipWeArePlacingCurrently; i++) {
         var currentId = (x1 + i) + "," + y1;
         document.getElementById(currentId).innerText = symbol;
-   
     }
-     totalPlacedCounter();
+    totalPlacedCounter();
     return true;
 }
 
@@ -187,7 +177,7 @@ function tryToPlaceDown(x1, y1) {
     var necessarySquares = new Array();
     for (i = 0; i < shipWeArePlacingCurrently; i++) {
         var currentId = (x1) + "," + (y1 - i);
-        //jquery was being annoying, used vanilla javascript
+
         var temp = document.getElementById(currentId).innerText;
         if (temp !== '-') {
             reButton();
@@ -197,7 +187,7 @@ function tryToPlaceDown(x1, y1) {
     for (i = 0; i < shipWeArePlacingCurrently; i++) {
         var currentId = (x1) + "," + (y1 - i);
         document.getElementById(currentId).innerText = symbol;
-        }
+    }
     totalPlacedCounter();
     return true;
 }
@@ -207,7 +197,6 @@ function tryToPlaceUp(x1, y1) {
     var necessarySquares = new Array();
     for (i = 0; i < shipWeArePlacingCurrently; i++) {
         var currentId = (x1) + "," + (y1 + i);
-        //jquery was being annoying, used vanilla javascript
         var temp = document.getElementById(currentId).innerText;
         if (temp !== '-') {
             reButton();
@@ -242,12 +231,11 @@ function reButton() {
         break;
     default:
         return;
-
     }
 }
 
 function totalPlacedCounter() {
-    startPoint=null;
+    startPoint = null;
     totalPlaced++;
-    
+
 }
