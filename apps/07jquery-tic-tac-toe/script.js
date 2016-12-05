da8310b571dd8bcc44695f183eee5160a438d692
ssfiero <ssfiero@yahoo.com>
2016-12-04 18:02:00 -0600

    Lesson 07 - Tic-Tac-Toe App - All Tests Passing

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -4,27 +4,27 @@
 $(document).on('ready', function() {
 
   // Spec 1
-  var $playerTurn = $('X');
+  var $playerTurn = 'X';
   var $divDataCell = $('[data-cell]');
 
   // Spec 3
   // Variables for each cell on the board
   // Used .text() to get the text content of the data-cell
-  var dataCell0 = $('[data-cell="0"]').text();
-  var dataCell1 = $('[data-cell="1"]').text();
-  var dataCell2 = $('[data-cell="2"]').text();
-  var dataCell3 = $('[data-cell="3"]').text();
-  var dataCell4 = $('[data-cell="4"]').text();
-  var dataCell5 = $('[data-cell="5"]').text();
-  var dataCell6 = $('[data-cell="6"]').text();
-  var dataCell7 = $('[data-cell="7"]').text();
-  var dataCell8 = $('[data-cell="8"]').text();
+  var $dataCell0 = $('[data-cell="0"]');
+  var $dataCell1 = $('[data-cell="1"]');
+  var $dataCell2 = $('[data-cell="2"]');
+  var $dataCell3 = $('[data-cell="3"]');
+  var $dataCell4 = $('[data-cell="4"]');
+  var $dataCell5 = $('[data-cell="5"]');
+  var $dataCell6 = $('[data-cell="6"]');
+  var $dataCell7 = $('[data-cell="7"]');
+  var $dataCell8 = $('[data-cell="8"]');
 
   // Spec 2
-  // Toggles between both players
-  function $toggleplayerTurn() {
+  // This is the switch to next player, ternary operator
+  function toggleplayerTurn() {
     $playerTurn = ($playerTurn === 'X') ? 'O' : 'X';
-  }
+  };
 
 
   // Board
@@ -33,58 +33,55 @@ $(document).on('ready', function() {
   // 6, 7, 8
 
   // Spec 3
-  function $horizontalWin() {
-    if (($playerTurn === dataCell0 && $playerTurn === dataCell1 && $playerTurn === dataCell2) ||
-    ($playerTurn === dataCell3 && $playerTurn === dataCell4 && $playerTurn === dataCell5) ||
-    ($playerTurn === dataCell6 && $playerTurn === dataCell7 && $playerTurn === dataCell8)) {
+  function horizontalWin() {
+    if (($playerTurn === $dataCell0.text() && $playerTurn === $dataCell1.text() && $playerTurn === $dataCell2.text()) ||
+    ($playerTurn === $dataCell3.text() && $playerTurn === $dataCell4.text() && $playerTurn === $dataCell5.text()) ||
+    ($playerTurn === $dataCell6.text() && $playerTurn === $dataCell7.text() && $playerTurn === $dataCell8.text())) {
       return true;
     }
-  }
+  };
 
-  function $verticalWin() {
-    if (($playerTurn === dataCell0 && $playerTurn === dataCell3 && $playerTurn === dataCell6) ||
-    ($playerTurn === dataCell1 && $playerTurn === dataCell4 && $playerTurn === dataCell7) ||
-    ($playerTurn === dataCell2 && $playerTurn === dataCell5 && $playerTurn === dataCell8)) {
+  function verticalWin() {
+    if (($playerTurn === $dataCell0.text() && $playerTurn === $dataCell3.text() && $playerTurn === $dataCell6.text()) ||
+    ($playerTurn === $dataCell1.text() && $playerTurn === $dataCell4.text() && $playerTurn === $dataCell7.text()) ||
+    ($playerTurn === $dataCell2.text() && $playerTurn === $dataCell5.text() && $playerTurn === $dataCell8.text())) {
       return true;
     }
-  }
+  };
 
-  function $diagonalWin() {
-    if (($playerTurn === dataCell0 && $playerTurn === dataCell4 && $playerTurn === dataCell8) ||
-    ($playerTurn === dataCell2 && $playerTurn === dataCell4 && $playerTurn === dataCell6)) {
+  function diagonalWin() {
+    if (($playerTurn === $dataCell0.text() && $playerTurn === $dataCell4.text() && $playerTurn === $dataCell8.text()) ||
+    ($playerTurn === $dataCell2.text() && $playerTurn === $dataCell4.text() && $playerTurn === $dataCell6.text())) {
       return true;
     }
-  }
-
-
-  // Spec 1 - When a cell is clicked, run function and get text from the cell the player clicked
-  // Spec 3 - Check combinations for a win (before switching to next player)
-  // Spec 2 - Toggle: This is the switch to next player, ternary operator
-  $divDataCell.click(function() {
-    $('this').text($playerTurn);
-    $checkForWin();
-    $toggleplayerTurn();
-  })
+  };
 
 
   // Spec 3
   // Check combinations for a win, and announce winner if true
-  function $checkForWin() {
-
-    var dataCell0 = $('[data-cell="0"]').text();
-    var dataCell1 = $('[data-cell="1"]').text();
-    var dataCell2 = $('[data-cell="2"]').text();
-    var dataCell3 = $('[data-cell="3"]').text();
-    var dataCell4 = $('[data-cell="4"]').text();
-    var dataCell5 = $('[data-cell="5"]').text();
-    var dataCell6 = $('[data-cell="6"]').text();
-    var dataCell7 = $('[data-cell="7"]').text();
-    var dataCell8 = $('[data-cell="8"]').text();
-
-    if ($horizontalWin() || $verticalWin() || $diagonalWin()) {
+  function checkForWin() {
+    if (horizontalWin() || verticalWin() || diagonalWin()) {
       $('#announce-winner').text('Player ' + $playerTurn + ' Wins!');
       return true;
     }
-  }
+  };
+
+
+  // Spec 1 - When a cell is clicked, run function and get text from the cell the player clicked
+  // Spec 3 - Check combinations for a win (before switching to next player)
+  // Spec 2 - Toggles between both players
+  $divDataCell.click(function() {
+      $(this).text($playerTurn);
+      checkForWin();
+      toggleplayerTurn();
+  });
+
+
+  // Spec 4
+  // Added a button with an id="clear" that clears the board, and resets the player to player 'X'
+  $('#clear').click(function() {
+    $divDataCell.empty();
+    $playerTurn = 'X';
+  });
 
 });
