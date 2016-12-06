294fbfd8877803367a63c848fb89330ef6fbedab
James Puckett <jamesjaypuckett@gmail.com>
2016-12-05 20:39:17 -0600

    tod list complete

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -2,10 +2,38 @@
 
 $(document).ready(function() {
   // Put app logic here
-  $('data-stack').click(function(){
-    var block = $('this').children().last();
-    // $('data-stack').click(function(){
-    //   if(block)
-    // });
+  //var children = $('data-stack').children();
+  //var lastChild = children.last();
+  var dataStack = $('[data-stack]');
+  var block = null;
+
+  dataStack.click(function(){
+    if (block === null){
+      block = $(this).children().last().detach();
+      //block.append(dataStack);
+      // if (isLegal){
+      //
+      // }
+    }
+    else {
+        $(this).children().append(block);
+    }
   });
-}
+  // function isLegal(start, end) {
+  //   if (start.data('block') > end.data('block')) {
+  //     return true;
+  //   }
+  // }
+
+});
+
+// On click() of a [data-stack], detach() the last() of the children() and set it equal to a variable called block. Then, when another [data-stack] is click()ed, check if the block variable is assigned, if so, append the block to the stack and set block to null.
+
+//Only move the block if the value of the data- block attribute is less than the last block of the stack, or if the stack is empty.
+
+//Create a function checkForWin() that checks .forEach() stack and determines if one of the last two stacks has four [data-block]s. Run this function after every move. If you won, put the .text 'You Won! inside the div#announce-game-won element.
+
+
+
+
+  //var selected = = $(this);
--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -2,4 +2,19 @@
 
 $(document).ready(function() {
   // App logic goes here
+  $('form').submit(function (event) {
+    event.preventDefault();
+    var todoText = $(this).find('#todo').val();
+    $(this).find('#todo').val('');
+    $('#todo-list').append('<li>' + todoText + '<button></button>'+'</li>');
+    $('#todo-list').sortable();
+
+    //Inside your callback prevent the default event from occuring when you submit. Then within the $(this) context, .find() the value of #todo and assign it to a variable called todoText.
+  });
+  // $('button').click(function(){
+  //   $(this).parent().remove();
+  // });
+  $('body').on('click', 'button', function(){
+    $(this).parent().remove();
+  });
 });
