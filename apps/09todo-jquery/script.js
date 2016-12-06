421a43ae47003576e42b59f25966a65f953a4250
Greg Pfaff <greg.pfaff@razorfish.com>
2016-12-05 19:09:12 -0600

    adding to-do-list with checkboxes

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -10,8 +10,14 @@ $(document).ready(function() {
   $form.submit(function(event) {
     event.preventDefault();
     $value = $this.find('input[type="text"]').val();
-    $toDoList.append('<li>' + $value + '</li>');
+    $toDoList.append('<div class="to-do-list-item"> <li>' + $value + '</li>' + '<input id="finishTask" type="checkbox" value="Finish Task"/>' + '<span id="complete-checkbox">Mark Complete</span>' + '</div>');
     $toDoList.sortable();
+  });
+
+  $('#todo-list').on('change', 'input', function(event) {
+    if ($(this).is(":checked")) {
+      $(this).closest('.to-do-list-item').remove();
+    }  
   });
 
   $("#todo-list").on('click', 'li', function(event) {
