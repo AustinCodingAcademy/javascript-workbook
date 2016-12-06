db56a100f77b221fd7bf5000b6c81207bb914d0c
Jordan Mitchell <mordanjitchell@gmail.com>
2016-12-05 19:31:23 -0600

    jquery to do list

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -1,5 +1,20 @@
 'use strict';
 
 $(document).ready(function() {
-  // App logic goes here
+    // App logic goes here
+    $('form').submit(function(event) {
+        event.preventDefault();
+        //nside your callback prevent the default event from occuring when you submit.
+        //Then within the $(this) context, .find() the value of #todo and assign it to a variable called todoText.
+        var todoText = $(this).find('#todo').val();
+        $('#todo-list').append('<li>' + todoText + '</li>');
+        var todoText = $(this).find('#todo').val(" ");
+        $('ul').sortable();
+    });
+
+    var $listitem = $('li');
+    $listitem.click(function() {
+        $(this).remove();
+    });
+
 });
