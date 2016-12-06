5a73f4b4300bc954d1a98348db97ef42b017ed2d
jonnapatrice <jonna.palermo@gmail.com>
2016-12-05 20:32:18 -0600

    To Do JQuery File - Lesson 9

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -1,5 +1,16 @@
 'use strict';
 
 $(document).ready(function() {
-  // App logic goes here
+    // App logic goes here
+    //Using jQuery, put a submit event listener on the form.
+    $('form').submit(function(event) {
+        event.preventDefault();
+        var todoText = $(this).find('#todo').val();
+        $('#todo-list').append('<li><input type="checkbox">' + todoText + '<button class ="remove">remove</button></li>').sortable();
+        todoText = $(this).find('#todo').val("");
+        $('body').on('click', 'button', function() {
+            $(this).parent().remove();
+        });
+
+    });
 });
