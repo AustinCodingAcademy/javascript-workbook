adca53975b43bede845ee75061f21446bf017244
pricedrp9 <pricedrp9@gmail.com>
2016-12-05 20:01:15 -0600

    Todo List

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -1,5 +1,23 @@
 'use strict';
 
 $(document).ready(function() {
-  // App logic goes here
+    $('form').submit(function(event) { // target the form, and put a submit event listener on it. We can capture and pass in the event for us to use
+        event.preventDefault(); // this tells the browser to "prevent" the "default" "event" from occuring (sending and refreshing)
+        console.log($(this)); // let's log out our "this" context (our form element) as a jQuery object
+        var todoText = $(this).find('input[type="text"]').val();
+        $('#todo-list').append('<li> <input type ="checkbox" name="test" value="test"/> ' + todoText + '</li>');
+        $(this).find('input[type="text"]').val('');
+        $('ul').sortable(); //could also put draggable here. Interacting with the DOM
+        // $("#todo-list").children('li').each(function() {
+        // //     $(this).append('<input type="checkbox" name="test" value="test" />');
+        // });
+        // // $('li').click(function(){
+        // //   $(this).remove();
+        // });
+    });
+
+
 });
+
+
+//.val only works for inputs. Specific for inputs in general. Whatever you type that is the value of the textbox of the form or whatever you are working with.
