1f81f01a64040a6355d7108c8d6bb5cf07d4c60d
chrisperk <christopherperkins.512@gmail.com>
2016-12-06 20:49:05 -0600

    To-Do jQuery, all tests passing, need bonus spec6,  WIP

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -1,5 +1,17 @@
 'use strict';
 
 $(document).ready(function() {
-  // App logic goes here
+    // App logic goes here
+    $('form').submit(function(event) {
+        event.preventDefault();
+        var todoText = $(this).find('#todo').val();
+        $('#todo-list').append('<li><input type="checkbox">  ' + todoText + '  </input><a class="remove-item" href="#">Remove</a></li>');
+        $('#todo-list').sortable();
+        $('#todo').val('');
+    });
+
+    $('a.remove-item').click(function(event) {
+        event.preventDefault();
+        $(this).parent().remove();
+    });
 });
