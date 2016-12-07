1329eea7d49656508662321e2317df40df612499
chrisperk <christopherperkins.512@gmail.com>
2016-12-07 00:50:32 -0600

    To-Do jQuery, all bonus specs complete, COMPLETE

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -5,13 +5,13 @@ $(document).ready(function() {
     $('form').submit(function(event) {
         event.preventDefault();
         var todoText = $(this).find('#todo').val();
-        $('#todo-list').append('<li><input type="checkbox">  ' + todoText + '  </input><a class="remove-item" href="#">Remove</a></li>');
+        $('#todo-list').append('<li><input type="checkbox"></input>  ' + todoText + '  <button class="remove-button" type="button">Remove</button></li>');
         $('#todo-list').sortable();
         $('#todo').val('');
     });
+});
 
-    $('a.remove-item').click(function(event) {
-        event.preventDefault();
-        $(this).parent().remove();
-    });
+$('body').on('click', '.remove-button', function(event) {
+    event.preventDefault();
+    $(this).parent().detach();
 });
