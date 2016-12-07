138d750d333fac28f323d636bfa567a31032c3ce
dsolis421 <snapshot8d@gmail.com>
2016-12-06 20:03:48 -0600

    11gist-blog styled

--- apps/11gist-blog/script.js
+++ apps/11gist-blog/script.js
@@ -4,18 +4,20 @@
 
 $(document).ready(function() {
   // You code here
+  //change AJAX calls to work w local host when testing
+  //   '/apps/11gist-blog/api/gists.json'
 
   function formatDate(date) {
     var arrDate = date.split('-');
     return arrDate[1] + '/' + arrDate[2] + '/'
  + arrDate[0]  }
 
-  $.ajax('/apps/11gist-blog/api/gists.json', {
+  $.ajax('https://api.github.com/users/dsolis421/gists.json', {
     success: function getGists(response) {
       response.forEach(function(post){
         if (post.description.match('^#post')) {
           var postDesc = post.description.replace(/#post /g, '');
-          var postURL = post.url.replace(/http:\/\/127.0.0.1:8080/g,'');
+          var postURL = post.url.replace(/http:\/\/127.0.0.1:808/g,'https://api.github.com/users/dsolis421/gists');
           var postDate = formatDate(post.created_at.split('T')[0]);
           var str = '<li><a href="#" data-url="' + postURL + '">' + postDesc + '</a> | ' + postDate + '</li>';
           console.log(str);
@@ -28,14 +30,15 @@ $(document).ready(function() {
         console.log(url);
         $.ajax(url, {
           success: function postHandler(postResponse) {
-            var commURL = postResponse.comments_url.replace(/http:\/\/127.0.0.1:8080/g,'');
+            var commURL = postResponse.comments_url.replace(/http:\/\/127.0.0.1:8080/g,'https://api.github.com/users/dsolis421/gists');
             $("#post").empty();
+            $('#post').css("text-align","left");
             $("#comments").empty();
             $("#post").append(marked(postResponse.files["post.md"].content));
             console.log(commURL);
             $.ajax(commURL, {
               success: function getComments(commResponse) {
-                $("#post").append("<h4 class='playfair'>Comments</h4>");
+                $("#post").append("<h4 class='playfair'>Comments <span class='fa fa-comments'></span></h4>");
                 commResponse.forEach(function(comment){
                   if (comment.user.login != null) {
                     $("#comments").append("<li>" + comment.user.login + " - " + comment.body + "</li>");
