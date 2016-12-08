bb53eb24a887fa5a07e87b3ce781758ea6e3711d
Greg Pfaff <greg.pfaff@razorfish.com>
2016-12-07 22:39:55 -0600

    adding WIP gist blog

--- apps/11gist-blog/script.js
+++ apps/11gist-blog/script.js
@@ -1,5 +1,31 @@
 'use strict';
 
 $(document).ready(function() {
-  // You code here
+
+  $.ajax("http://127.0.0.1:8080/apps/11gist-blog/api/gists.json", {
+    success(gists) {
+      gists.forEach(function(gist) {
+
+        var post = "<a href='#' data-url='" + gist.url + "'>" + gist.description + "</a>";
+
+        if (post.indexOf('#post') !== -1) {
+          post = post.replace("#post", " ");
+          $('#posts').append(post);
+        }
+      });
+      
+      $('#posts').on('click', 'a', function(e){
+        e.preventDefault();
+        console.log($(this).data('url'));
+        /*var $self = this;
+        $.ajax($self, {
+
+        })*/
+      })
+    },
+    error(request, status, error) {
+      alert("Request Failed");
+    }
+  });
 });
+
