007b638a8c30d7870b6be072144502736ba127a5
Clint Lucio <clintlucio@gmail.com>
2016-12-08 13:04:24 -0600

    apps/08jquery-towers-of-hanoi/

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -2,4 +2,26 @@
 
 $(document).ready(function() {
   // Put app logic here
-}
+  var block = null;
+  $('[data-stack]').click(function() {
+      if(!block) {
+        block= $(this).children().last().detach();
+      }
+      else {
+        var stackedBlock = $(this).children().last().data('block');
+        if ($(block).data('block') < stackedBlock || !stackedBlock) {
+          $(this).append(block);
+          block = null;
+          checkForWin();
+        };
+      };
+  });
+});
+//check for win
+function checkForWin() {
+  if ($('[data-stack="2"]').children().length === 4 ||
+  $('[data-stack="3"]').children().length === 4) {
+   $('#announce-game-won').text('You Won!');
+   console.log("You Rock!");
+ };
+};
