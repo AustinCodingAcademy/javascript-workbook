7bb90fd2c494ac0090078b31a2aa4d694008d9fe
Jonathan Soape <jonathansoape@gmail.com>
2016-12-08 16:23:13 -0600

    Lesson 9 To Do List app - all tests passed, but didn't include bonus specs

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -2,4 +2,12 @@
 
 $(document).ready(function() {
   // App logic goes here
+
+  $('form').submit(function (event) {
+      event.preventDefault();
+      var todoText = $(this).find('input[id="todo"]').val();
+      $('#todo-list').append('<li>' + todoText + '</li>');
+    });
+
+  $('#todo-list').sortable();
 });
