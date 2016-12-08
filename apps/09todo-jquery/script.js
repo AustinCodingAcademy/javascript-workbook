9b97db4e1ecb91a2b56d841f1bdde2ad1f40f750
ssfiero <ssfiero@yahoo.com>
2016-12-08 00:21:15 -0600

    Lesson 09 JQuery - ToDo App - All tests passing

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -1,5 +1,29 @@
 'use strict';
 
 $(document).ready(function() {
-  // App logic goes here
+
+  // Spec 1
+  // Using jQuery, put a submit event listener on the form.
+  $('form').submit(function (event) {
+    // Spec 2
+    // Inside your callback prevent the default event from occuring when you submit.
+    event.preventDefault();
+
+    // Then within the $(this) context, .find() the value of #todo and assign it to a variable called todoText.
+    var todoText = $(this).find('input[type="text"]').val();
+
+    // Spec 3
+    // Construct a string containing a list item <li></li> with your todoText in the middle. .append() the html string to the end of your #todo-list.
+    $('#todo-list').append('<li>' + todoText + '<li>');
+
+    // clears the input value in the form
+    $(this).find('input[type="text"]').val('');
+
+    // Spec 4
+    // Now make your list .sortable().
+    $('#todo-list').sortable();
+
+  })
+
+
 });
