6573f2b9fa9d57d8a25b401f7a416534214369c5
pricedrp9 <pricedrp9@gmail.com>
2016-12-07 20:30:09 -0600

    ToDo Updates

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -2,11 +2,27 @@
 
 $(document).ready(function() {
     $('form').submit(function(event) { // target the form, and put a submit event listener on it. We can capture and pass in the event for us to use
+
         event.preventDefault(); // this tells the browser to "prevent" the "default" "event" from occuring (sending and refreshing)
         console.log($(this)); // let's log out our "this" context (our form element) as a jQuery object
         var todoText = $(this).find('input[type="text"]').val();
-        $('#todo-list').append('<li> <input type ="checkbox" name="test" value="test"/> ' + todoText + '</li>');
+        //.val if empty will be "" or could have taken the falsey approach --> !todoText
+        if (todoText === "") {
+          return;
+        }
+
+        $('#todo-list').append('<li> <input type ="checkbox" name="test" value="test"/>' + todoText + '<button class ="done"></button></li>');
+
+        $('body').on('click', 'button', function(event) {
+            $(this).parent().remove();
+
+
+        });
+
+        //this is the button in the scenario above.
+
         $(this).find('input[type="text"]').val('');
+
         $('ul').sortable(); //could also put draggable here. Interacting with the DOM
         // $("#todo-list").children('li').each(function() {
         // //     $(this).append('<input type="checkbox" name="test" value="test" />');
