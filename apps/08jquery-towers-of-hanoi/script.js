98c0b905be62763353f38cf38532ae72e4646d5a
ssfiero <ssfiero@yahoo.com>
2016-12-08 18:13:59 -0600

    Lesson 08 JQuery - Towers of Hanoi - All Tests Passing

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -11,17 +11,13 @@ $(document).ready(function() {
   $dataStack.click(movePiece);
 
   function movePiece() {
-    // Run this function (the checkForWin function) after every move.
-    // The checkForWin function is run after every click event movePiece
-    checkForWin();
     // Then, when another [data-stack] is click()ed, check if there is anything in the $block variable...
     // i.e. (if the block variable is assigned)...
     // if so, append the block to the stack and set block to null.
-    if (!$block) {
+    if ($block === null) {
       // $(this) gets the actual div that was clicked
       // detach() the last() of the children() and set it equal to a variable called block.
       $block = $(this).children().last().detach();
-      console.log($block);
 
     } else {
 
@@ -35,6 +31,9 @@ $(document).ready(function() {
       // the last block of the stack, or if the stack is empty.
       if (dataBlockValue1 < dataBlockValue2 || $(this).children().length === 0) {
         $(this).append($block);
+        // Run this function (the checkForWin function) after every move.
+        // The checkForWin function is run after every time the block is moved to a new stack
+        checkForWin();
         $block = null;
       };
     };
@@ -48,11 +47,10 @@ $(document).ready(function() {
   var $dataStack3 = $('[data-stack="3"]');
 
   function checkForWin() {
-      console.log('is the function running?');
     if (($dataStack2.children().length === 4) || ($dataStack3.children().length === 4)) {
-      console.log('is it a win');
       // If you won, put the .text 'You Won! inside the div#announce-game-won element.
-      $('[#announce-game-won]').text('You Won!');
+      // You won will be announced when 4 blocks are in stack 2 or 3, and after user clicks on another stack
+      $('#announce-game-won').text('You Won!');
       return true;
     };
   };
