80a8bdb8a4e8b361bc6c0727259c62cdd52525a5
Jonathan Soape <jonathansoape@gmail.com>
2016-12-08 18:52:21 -0600

    App 08 Hanoi Jquery - all tests passing

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -2,4 +2,33 @@
 
 $(document).ready(function() {
   // Put app logic here
-}
+
+  var block = null;
+  var towerHeight = 4;
+
+  $('[data-stack]').click(function() {
+
+    if (block && ($(this).children().last().data('block') > block.data('block') || $(this).children().length === 0)) {
+      //Put down
+      $(this).append(block);
+      block = null;
+      checkForWin();
+    } else {
+      if (block === null) {
+        //Pick up
+        block = $(this).children().last().detach();
+      }
+    }
+  });
+
+
+
+  function checkForWin() {
+    if ($('[data-stack=2]').children().length === towerHeight) {
+      $('#announce-game-won').text("You Won!");
+    }
+    if ($('[data-stack=3]').children().length === towerHeight) {
+      $('#announce-game-won').text("You Won!");
+    }
+  };
+});
