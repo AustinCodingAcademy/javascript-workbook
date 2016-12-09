e638611eab1a36c0e37e91d3964156e263c3a5cf
mcplunket <maryclaireplunket@Marys-MacBook-Pro-2.local>
2016-12-08 20:15:59 -0600

    Towers WIP and Todo

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -1,5 +1,18 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
-}
+    // Put app logic here
+
+    $('[data-stack]').click(
+        function() {
+            var block = $(this).children().last().detach()
+                //where should i be defining this variable?
+            console.log(block)
+            if (block.length > 0) {
+                $(this).append(block)
+            } else {
+                console.log('hi');
+            }
+        }
+    );
+});
