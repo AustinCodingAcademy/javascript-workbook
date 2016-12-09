5368119291f3585338424030c0c4f7010e6638c6
mhow8809 <mhow_22000@yahoo.com>
2016-12-08 20:03:43 -0600

    09 todo passing

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -2,4 +2,15 @@
 
 $(document).ready(function() {
   // App logic goes here
+  $('form').submit(function (event) { // target the form, and put a submit event listener on it. We can capture and pass in the event for us to use
+    event.preventDefault(); // this tells the browser to "prevent" the "default" "event" from occuring (sending and refreshing)
+    var todoText = $(this).find('#todo').val();
+    console.log(todoText);
+    $('#todo-list').append('<li><input type="checkbox" id="complete">' + todoText + '</li>');
+    $('#todo-list').sortable();
+  });
+
+  function toDone() {
+
+  }
 });
