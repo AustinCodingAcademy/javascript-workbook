7435010518fb65be59edf01eea8ff2c138b80c1f
Austin Shipp <austinshipp@yahoo.com>
2016-12-10 14:56:04 -0600

    Finished Lesson 9 To-Do App

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -1,5 +1,9 @@
 'use strict';
 
 $(document).ready(function() {
-  // App logic goes here
+  $('form').submit(function(event){
+    event.preventDefault();
+    var $todoText = $(this).find('#todo').val();
+    $('#todo-list').append('<li><label class="checkbox-inline"><input type="checkbox" />' +$todoText + '</label></li>').sortable();
+  })
 });
