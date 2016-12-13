c724b1aae83e7a57cfcfc80b5670784639e60a2e
Alex <alexanderjgaw@gmail.com>
2016-12-12 21:02:09 -0600

    fixing my damn tic tac toe app, wanna fight?

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -7,6 +7,11 @@ $(document).on('ready', function() {
   var playerTurn = 'X';
   var play = true;
   var turns = 0;
+  var winCombinations = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ];
 
   // Set up #announce-winner with text so it takes up space. Visibility is set to hidden until someone wins.
   $('#announce-winner').html('Player <span id="winner">X</span> Wins!');
@@ -16,96 +21,37 @@ $(document).on('ready', function() {
     play = false;
   }
 
+  function getText(cell) {
+    return $('div[data-cell="' + cell + '"]').text();
+  }
+
+
+
   function checkForWin() {
-    // Check for horizontal win
-    if (($("div[data-cell='0']").text() === playerTurn && $("div[data-cell='1']").text() === playerTurn && $("div[data-cell='2']").text() === playerTurn) || ($("div[data-cell='3']").text() === playerTurn && $("div[data-cell='4']").text() === playerTurn && $("div[data-cell='5']").text() === playerTurn) || ($("div[data-cell='6']").text() === playerTurn && $("div[data-cell='7']").text() === playerTurn && $("div[data-cell='8']").text() === playerTurn)) {
-      return true;
+    var winArray = [];
+    for (var i = 0; i < 8; i++) {
+      for (var j = 0; j < 3; j++) {
+        if (getText(winCombinations[i][j]) === playerTurn) {
+          winArray.push($('div[data-cell="' + winCombinations[i][j] + '"]'));
+        }
+      }
+      if (winArray.length === 3) {
+        lightEmUp(winArray);
+        return true;
+      } else {
+        winArray = [];
+      }
     }
-    // Check for vertical win
-    if (($("div[data-cell='0']").text() === playerTurn && $("div[data-cell='3']").text() === playerTurn && $("div[data-cell='6']").text() === playerTurn) || ($("div[data-cell='1']").text() === playerTurn && $("div[data-cell='4']").text() === playerTurn && $("div[data-cell='7']").text() === playerTurn) || ($("div[data-cell='2']").text() === playerTurn && $("div[data-cell='5']").text() === playerTurn && $("div[data-cell='8']").text() === playerTurn)) {
-      return true;
-    }
-    // Check for diagonal win
-    if (($("div[data-cell='0']").text() === playerTurn && $("div[data-cell='4']").text() === playerTurn && $("div[data-cell='8']").text() === playerTurn) || ($("div[data-cell='2']").text() === playerTurn && $("div[data-cell='4']").text() === playerTurn && $("div[data-cell='6']").text() === playerTurn)) {
-      return true;
-    }
-    // If it gets here, no win yet so return false
-    return false;
   }
 
   function checkForTie() {
     return turns === 8;
   }
 
-//  This ugly function finds the specific squares that were responsible for the win and turns their background old-computer green.
-  function lightEmUp() {
-    var $divH = '', cellNumberH = 0, winArrayH = []; // Variables for horizontal check
-    var $divV = '', cellNumberV = 0, winArrayV = []; // Variables for vertical check
-    var $divD = '', cellNumberD = 0, winArrayD = []; // Variables for diagonal check
-    var $divD2 = '', cellNumberD2 = 0, winArrayD2 = []; // Variables for other diagonal check
-    for(var i = 0; i < 3; i++) {
-      for(var j = 0; j < 3; j++) {
-        cellNumberH = j + (i * 3);
-        cellNumberV = i + (j * 3);
-        cellNumberD = (2 * i) + (2 * j);
-        cellNumberD2 = 4 * j;
-        $divH = $('div[data-cell="' + cellNumberH + '"]');
-        $divV = $('div[data-cell="' + cellNumberV + '"]');
-        $divD = $('div[data-cell="' + cellNumberD + '"]');
-        $divD2 = $('div[data-cell="' + cellNumberD2 + '"]');
-        if($divH.text() === playerTurn) {
-          winArrayH.push($divH);
-        }
-        if($divV.text() === playerTurn) {
-          winArrayV.push($divV);
-        }
-        if($divD.text() === playerTurn) {
-          winArrayD.push($divD);
-        }
-        if($divD2.text() === playerTurn) {
-          winArrayD2.push($divD2);
-        }
-
-      }
-      // Check for horizontal win
-      if(winArrayH.length === 3) {
-        for(var i = 0; i < 3; i++) {
-          winArrayH[i].css('color', '#33ff66');
-        }
-        return;
-      } else {
-        winArrayH = [];
-      }
-
-      // Check for vertical win
-      if(winArrayV.length === 3) {
-        for(var i = 0; i < 3; i++) {
-          winArrayV[i].css('color', '#33ff66');
-        }
-        return;
-      } else {
-        winArrayV = [];
-      }
-
-      // Check for diagonal win
-      if(winArrayD.length === 3) {
-        for(var i = 0; i < 3; i++) {
-          winArrayD[i].css('color', '#33ff66');
-        }
-        return;
-      } else {
-        winArrayD = [];
-      }
-
-      // Check for OTHER diagonal win. There's gotta be a better way.
-      if(winArrayD2.length === 3) {
-        for(var i = 0; i < 3; i++) {
-          winArrayD2[i].css('color', '#33ff66');
-        }
-        return;
-      } else {
-        winArrayD2 = [];
-      }
+//This function adds a class to the winning squares so we can style them
+  function lightEmUp(winners) {
+    for (var i = 0; i < 3; i++) {
+      winners[i].addClass('winner');
     }
   }
 
@@ -114,7 +60,6 @@ $(document).on('ready', function() {
       $(this).text(playerTurn);
       if (checkForWin()) {
         stopGame();
-        lightEmUp();
         $('#winner').text(playerTurn);
         $('#announce-winner').css('visibility', 'visible');
       } else if (checkForTie()) {
@@ -126,7 +71,7 @@ $(document).on('ready', function() {
   })
 
   $('#clear').click( function() {
-      $('div[data-cell]').text('').css('color', 'black');
+      $('div[data-cell]').text('').removeClass('winner');
       playerTurn = 'X';
       $('#announce-winner').css('visibility', 'hidden').html('Player <span id="winner">X</span> Wins!');
       play = true;
