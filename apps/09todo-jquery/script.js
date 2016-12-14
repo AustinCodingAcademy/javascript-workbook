d05d06a2215d523a42babe5f9ced1841e4258002
Jonathan Soape <jonathansoape@gmail.com>
2016-12-13 18:26:20 -0600

    App 09 To Do List - all tests passing, but didn't do bonus spec

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -6,8 +6,11 @@ $(document).ready(function() {
   $('form').submit(function (event) {
       event.preventDefault();
       var todoText = $(this).find('input[id="todo"]').val();
-      $('#todo-list').append('<li>' + todoText + '</li>');
+      $('#todo-list').append('<li><input type="checkbox">' + todoText + '</li>');
     });
 
   $('#todo-list').sortable();
+
+$('[checked]').css('text-decoration', 'line-through');
+
 });
