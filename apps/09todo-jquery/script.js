ba5c81e277ef4f2e50c1fca3a0edeb3e6c82d663
trevorlujan123 <trevorlujan@live.com>
2016-12-14 19:12:19 -0600

    Todo app final

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -9,38 +9,21 @@ $(document).ready(function() {
         // this finds the value of the id "todo", and saves it in  a variable.
         var todoText = $(this).find('#todo').val();
 
-        // variables for the open circle and closed circle.
-        var checkMark = "<i class='check fa fa-circle' aria-hidden='true'></i>";
-        var noCheckMark = "<i class='check fa fa-circle-thin' aria-hidden='true'></i>"
-        var mark = noCheckMark;
-
-
         // .sortable() allows the user to re-sort the unordered list.
         // this adds the li into the unordered list.
         // you can add full html using jquery by putting it into a string.
-        $("#todo-list").append("<li><i class='check fa fa-circle-thin' aria-hidden='true'></i>" + todoText + "<i class='button fa fa-times' aria-hidden='true'></i></li>").sortable();
+        $("#todo-list").append("<li><input type='checkbox'>" + todoText + "<i class='button fa fa-times' aria-hidden='true'></i></li>").sortable();
 
         // this makes the submit box empty after you click the button
         todoText = $(this).find("#todo").val("");
 
         // this allows the user to delete an item when they click on the button
-        $("body").on('click','.button', function() {
+        $("body").on('click', '.button', function() {
             $(this).parent().remove();
-        });
-
-
-        $("body").on('click','i', function() {
-            if (mark === noCheckMark){
-                $(this).replaceWith(checkMark);
-            }
-        });
-        $("body").on('click','i', function() {
-            if (mark === checkMark){
-                $(this).replaceWith(noCheckMark);
-
-            }
 
         });
+
+
 
     });
 });
