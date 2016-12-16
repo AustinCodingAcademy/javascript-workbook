96bf6d098058247d7d74f77ff808eea689dbab4e
zekeg <zechariah.guadiana@gmail.com>
2016-12-15 20:19:53 -0600

    App 11 - Gist Blog: All Specifications Met, No CSS Yet

--- apps/11gist-blog/script.js
+++ apps/11gist-blog/script.js
@@ -1,5 +1,72 @@
 'use strict';
 
 $(document).ready(function() {
-  // You code here
+    // You code here
+    $.ajax('http://127.0.0.1:8080/apps/11gist-blog/api/gists.json', {
+        success: function(response) {
+
+            response.forEach(function(item) {
+
+                //create formatted link item in variable
+                if (item.description.indexOf("#post") >= 0) {
+                    console.log("#post detected!");
+
+                    //create a string with the formatted <a> element and create a new data attribute with the item's url
+                    //add br tag (temp?)
+                    var str = "<li><a href = \"#\" data-url=" + item.url + ">" + item.description.replace("#post", "") + "</a></li>";
+                    //check value in console
+                    console.log(str);
+                    //populate list
+                    $("#posts").append(str);
+                }
+
+                console.log($("body").html());
+
+                $("a[data-url]").click(function(event) {
+                    console.log("click register");
+                    //Prevent default action
+                    event.preventDefault();
+
+                    $.ajax($(this).data("url"), {
+                        success: function(response) {
+                            console.log("success!" + response.url)
+
+                            console.log(response.files["post.md"]["content"]);
+                            var content = response.files["post.md"]["content"];
+                            $("#post").html(marked(content));
+
+                            //GET COMMENTS
+                            $.ajax(response.comments_url, {
+                                success: function(response) {
+
+                                  //iterate through comment items
+                                    response.forEach(function(comment) {
+                                        console.log("comment: " + comment.user.login);
+                                        var str = "<li>" + comment.user.login + "</li>" +
+                                                  "<li>" + comment.body + "</li>";
+                                        $("#comments").html(str);
+                                    })
+
+
+
+                                }
+                            })
+                        }
+                    });
+
+
+
+
+                })
+
+
+
+
+
+            })
+
+
+        }
+
+    })
 });
