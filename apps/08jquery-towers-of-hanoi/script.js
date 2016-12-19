7f74e2e1fe772066e151be73ce07e6f07f520210
James Puckett <jamesjaypuckett@gmail.com>
2016-12-19 15:22:08 -0600

    minor tweak hanoi test

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -20,14 +20,3 @@ $(document).ready(function() {
 function checkForWin() {
   return ($('[data-stack=2]').children().length === 4) || ($('[data-stack=3]').children().length === 4)
 }
-
-// On click() of a [data-stack], detach() the last() of the children() and set it equal to a variable called block. Then, when another [data-stack] is click()ed, check if the block variable is assigned, if so, append the block to the stack and set block to null.
-
-//Only move the block if the value of the data- block attribute is less than the last block of the stack, or if the stack is empty.
-
-//Create a function checkForWin() that checks .forEach() stack and determines if one of the last two stacks has four [data-block]s. Run this function after every move. If you won, put the .text 'You Won! inside the div#announce-game-won element.
-
-
-
-
-//var selected = = $(this);
