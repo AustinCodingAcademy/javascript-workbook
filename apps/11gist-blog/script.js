c0079c039d3389852f37f6bd92003fc6aedd09ba
Clint Lucio <clintlucio@gmail.com>
2016-12-19 23:44:19 -0600

    apps/11gist-blog/

--- apps/11gist-blog/script.js
+++ apps/11gist-blog/script.js
@@ -1,5 +1,30 @@
 'use strict';
 
 $(document).ready(function() {
-  // You code here
+    $.ajax('http://127.0.0.1:8080/apps/11gist-blog/api/gists.json', {
+        success: function(gists) {
+            gists.forEach(function(gist) {
+                if (gist.description.indexOf('#post') > -1) {
+                    var $a = $('<a href="' + gist.url + '">' + gist.description.slice(6) + '</a>');
+                    $('#posts').append($('<li></li>').append($a));
+                    $a.on('click', function(event) {
+                        event.preventDefault();
+                        var href = $(this).attr('href');
+                        console.log(href);
+                        $.ajax(href, {
+                            success: function(gist) {
+                                console.log(gist);
+                                $('#post').html(marked(gist['files']['post.md']['content']));
+                            }
+                        });
+                        $.ajax(gist.comments_url, {
+                            success: function(comments) {
+                                console.log(gist.comments_url);
+                            }
+                        });
+                    });
+                }
+            });
+        }
+    });
 });
