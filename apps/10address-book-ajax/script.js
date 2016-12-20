39f0799a55e8b56cca73e1d4cbff943e223b0e26
dmencha <dmencha@rMB.local>
2016-12-19 23:33:18 -0600

    jQuery Address Book

--- apps/10address-book-ajax/script.js
+++ apps/10address-book-ajax/script.js
@@ -1,5 +1,37 @@
 'use strict';
 
 $(document).ready(function() {
-  // You code here
+//Using jQuery's $.ajax method, make a call
+// to https://reqres-api.herokuapp.com/api/users.
+//  Watch the call happen in your Network tab in
+//  your developer console.
+ $.ajax('https://reqres-api.herokuapp.com/api/users', {
+   success: function(users) {
+     users.forEach(function(user){
+     var str = '<tr><td>'
+     + user.id + '</td><td>'
+     + user.first_name + '</td><td>'
+     + user.last_name + '</td><td><a href="#" data-id="'
+     + user.id + '">view</a></td></tr>';
+      $('tbody').append(str);
+
+     })
+   }
+ })
+
+ $('body').on('click', 'a', function(event) {
+        event.preventDefault();
+        var url = 'https://reqres-api.herokuapp.com/api/users/' + $(this).data("id");
+        $.ajax(url, {
+            success: function(users) {
+                var str =
+                    "<h3>" + users.first_name + " " + users.last_name + "</h3>" +
+                    "<h4>" + users.occupation + "</h4>" +
+                    "<p>" + users.phone + "</p>" +
+                    "<p>" + users.address + "</p>" +
+                    "<img src='" + users.avatar + "'>";
+                $('#details').html(str);
+            }
+        });
+    });
 });
