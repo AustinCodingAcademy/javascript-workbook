1693c5195b3d43079def8cd186536e5dce4902be
Benavidez <Dbenavidez@Benavidezs-MacBook-Air.local>
2016-12-21 17:27:17 -0600

    Towers Complete

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -1,5 +1,22 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
+  var block = null;
+
+  $('[data-stack]').click(function() {
+    if (!block) {
+      block = $(this).children().last().detach();
+    } else {
+      if ($(this).children().last().data('block') > block.data('block') || $(this).children().last().data('block') == undefined) {
+        $(this).append(block);
+        block = null;
+      }
+    }
+    if (checkForWin()) {
+      $('#announce-game-won').text('you won!');
+    }
+  })
+})
+function checkForWin() {
+  return ($('[data-stack=2]').children().length === 4) || ($('[data-stack=3]').children().length === 4)
 }
