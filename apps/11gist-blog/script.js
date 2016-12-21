c0121f8460f94955b175ec108c83e48b5716d998
chrisperk <christopherperkins.512@gmail.com>
2016-12-21 15:47:23 -0600

    Gist Blog, added date created and date edited data

--- apps/11gist-blog/script.js
+++ apps/11gist-blog/script.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
                     var strPostDescription = post.description.slice(5);
 
                     // make an html-encapsulated string that will be inserted into index.html
-                    var $post = $('<a class="collection-item" href="#" data-url="' + post.url + '" data-comments="' + post.comments_url + ' ">' + strPostDescription + '</a>');
+                    var $post = $(`<a class="collection-item" href="#" data-url="${post.url}" data-comments="${post.comments_url}" data-id="${post.id}">${strPostDescription}</a>`);
 
                     // attach string creating list-link element to end of #posts list; lastest post will be at top of list
                     $('ul#posts').append($post);
@@ -30,13 +30,13 @@ $(document).ready(function() {
             // click listener on #posts list-links
             $('ul#posts a').click(function(event) {
 
-                var $that = $(this).data('url').id;
-                console.log($that);
+                var $thatId = $(this).data('url').id;
+                console.log($thatId);
 
                 // prevent page from reloading
                 event.preventDefault();
 
-                // when a post is selected, display the #new-comment entry form
+                // // when a post is selected, display the #new-comment entry form
                 // $('form#new-comment').css('display', 'block');
 
                 // remove active class from all #posts list-links
@@ -50,6 +50,11 @@ $(document).ready(function() {
 
                     // if request is successful run this new post function with returned post as param
                     success: function(post) {
+                        var $postCreatedDate = moment(post.created_at).format("dddd, MMMM Do YYYY, h:mm:ss a");
+                        var $postUpdatedDate = moment(post.updated_at).format("dddd, MMMM Do YYYY, h:mm:ss a");
+                        var $postDate = $(`<p>Posted: ${$postCreatedDate}</p><p>Last edited: ${$postUpdatedDate}</p>`);
+
+                        $('div#post-date').html($postDate);
 
                         // pull the content property from the selected post's 'post.md' file and store it in a var
                         var postContent = post.files['post.md'].content;
@@ -83,39 +88,50 @@ $(document).ready(function() {
                     }
                 })
 
-                // put click listener on submit button after new-comment form and post comments are displayed
+                // // put click listener on submit button after new-comment form and post comments are displayed
                 // $('button#submit-comment').click(function(event) {
                 //     event.preventDefault();
                 //     var $newCommentUsername = $('#user-name').val();
                 //     var $newCommentBody = $('#user-comment').val();
-                //     console.log(`${$newCommentUsername} ${$newCommentBody}`);
-                //     $.post(`http://127.0.0.1:8080/apps/11gist-blog/api/gists/88b4b967cd89b99da6e3/comments.json`, {
+                //     console.log(`${$newCommentUsername}\n${$newCommentBody}`);
+                //     // var $newComment = $(`<blockquote><li><span class="comment-user">${$newCommentUsername}</span> says "<span class="comment-body">${$newCommentBody}"</span></li></blockquote>`);
+                //     // $('#comments').append($newComment);
+                //     // $('input#user-name').val('');
+                //     // $('textarea#user-comment').val('');
+                //     $.ajax('https://api.github.com/user?access_token=cd5e11656049515e0cb90cd14f91bac9e24830fa', {
                 //         success: function(response) {
-                //
                 //             console.log(response);
+                //             $.post(`https://api.github.com/users/chrisperk/gists/${$('a.collection-item.active').data('id')}/comments`, {
+                //                 "body": $newCommentBody
+                //             }, {
+                //                 success: function(response) {
                 //
-                //             // make ajax request for the comments object with this link's data-comments_url key
-                //             $.ajax($(this).data('comments'), {
+                //                     console.log(response);
                 //
-                //                 // if request is successful run this new comments function with returned comments array as param
-                //                 success: function(comments) {
+                //                     // make ajax request for the comments object with this link's data-comments_url key
+                //                     $.ajax($(this).data('comments'), {
                 //
-                //                     // reset the comments list before we loop through and generate each comment
-                //                     $('ul#comments').empty();
+                //                         // if request is successful run this new comments function with returned comments array as param
+                //                         success: function(comments) {
                 //
-                //                     // loop through comment object in array and enter each as the param for this generate comment function
-                //                     comments.forEach(function(comment) {
+                //                             // reset the comments list before we loop through and generate each comment
+                //                             $('ul#comments').empty();
                 //
-                //                         // make an html-encapsulated string that displays and styles new comment
-                //                         var $comment = $('<blockquote><li><span class="comment-user">' + comment.user.login + '</span> says "<span class="comment-body">' + comment.body + '"</span></li></blockquote>');
+                //                             // loop through comment object in array and enter each as the param for this generate comment function
+                //                             comments.forEach(function(comment) {
                 //
-                //                         // attach new comment to the #comments list; latest comment will be at bottom of list
-                //                         $('#comments').append($comment);
+                //                                 // make an html-encapsulated string that displays and styles new comment
+                //                                 var $comment = $('<blockquote><li><span class="comment-user">' + comment.user.login + '</span> says "<span class="comment-body">' + comment.body + '"</span></li></blockquote>');
+                //
+                //                                 // attach new comment to the #comments list; latest comment will be at bottom of list
+                //                                 $('#comments').append($comment);
+                //                             })
+                //                         }
                 //                     })
                 //                 }
                 //             })
                 //         }
-                //     })
+                //     });
                 // })
             });
         }
