3a07408bf401476f7f8c4067047df802082e6b26
Clint Lucio <clintlucio@gmail.com>
2016-12-21 12:19:24 -0600

    apps/11gist-blog/

--- apps/11gist-blog/script.js
+++ apps/11gist-blog/script.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
     $.ajax('https://api.github.com/users/clucio72/gists', {
         success: function(gists) {
             gists.forEach(function(gist) {
-                if (gist.description.indexOf('#post Here is my first post!') > -1) {
+                if (gist.description.indexOf('#post Here is my first post') > -1) {
                     var $a = $('<a href="' + gist.url + '">' + gist.description.slice(6) + '</a>');
-                    $('#post Here is my first post!').append($('<li></li>').append($a));
+                    $('#post Here is my first post').append($('<li></li>').append($a));
                     $a.on('click', function(event) {
                         event.preventDefault();
                         var href = $(this).attr('href');
@@ -14,7 +14,7 @@ $(document).ready(function() {
                         $.ajax(href, {
                             success: function(gist) {
                                 console.log(gist);
-                                $('#post Here is my first post!').html(marked(gist['files']['post.md']['content']));
+                                $('#post Here is my first post').html(marked(gist['files']['post.md']['content']));
                             }
                         });
                         $.ajax(gist.comments_url, {
