41b028f1adefb2a11d66aa5cb0ee0649b74ca8ac
zekeg <zechariah.guadiana@gmail.com>
2016-12-22 16:16:31 -0600

    Minimal CSS Styling, let's just see the gist thing work

--- apps/11gist-blog/script.js
+++ apps/11gist-blog/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 $(document).ready(function() {
-    // You code here
+  
     $.ajax('http://127.0.0.1:8080/apps/11gist-blog/api/gists.json', {
         success: function(response) {
 
