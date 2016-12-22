dcddfb066f69ce16dad5f2f8e503a37077a82f9e
Clint Lucio <clintlucio@gmail.com>
2016-12-22 10:29:08 -0600

    apps/11gist-blog/

--- apps/11gist-blog/script.js
+++ apps/11gist-blog/script.js
@@ -4,24 +4,27 @@ $(document).ready(function() {
     $.ajax('https://api.github.com/users/clucio72/gists', {
         success: function(gists) {
             gists.forEach(function(gist) {
-                if (gist.description.indexOf('#post Here is my first post') > -1) {
+                if (gist.description.indexOf('#post') > -1) {
                     var $a = $('<a href="' + gist.url + '">' + gist.description.slice(6) + '</a>');
-                    $('#post Here is my first post').append($('<li></li>').append($a));
+                    $('#post').append($('<li></li>').append($a));
                     $a.on('click', function(event) {
                         event.preventDefault();
                         var href = $(this).attr('href');
-                        console.log(href);
                         $.ajax(href, {
                             success: function(gist) {
+                                var content = gist['files']['post.md']['content'];
+                                $('#post').html(marked(content));
                                 console.log(gist);
-                                $('#post Here is my first post').html(marked(gist['files']['post.md']['content']));
+                                $.ajax(gist.comments_url, {
+                                    success: function(comments) {
+                                        $('#comments').html('');
+                                        comments.forEach(function(comment) {
+                                            $('#comments').append($('<li>' + comment['user']['login'] + ': ' + comment['body'] + '</li>'));
+                                        })
+                                    }
+                                });
                             }
-                        });
-                        $.ajax(gist.comments_url, {
-                            success: function(comments) {
-                                console.log(gist.comments_url);
-                            }
-                        });
+                        })
                     });
                 }
             });
