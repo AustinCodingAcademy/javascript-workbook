685aa5af9dc514579f6e59fd7ae1baa53830ce52
Bradley Cuccio <bradleycuccio@gmail.com>
2016-12-21 20:21:35 -0600

    Gist Blog

--- apps/11gist-blog/script.js
+++ apps/11gist-blog/script.js
@@ -3,36 +3,12 @@
 //After starting up a server in the app directory, navigate in your browser to http://127.0.0.1:8080/apps/11gist-blog/api/gists.json,
 //here you will see the top level of the api, with all of the gists. In each gist, you will see a "url" key. Navigate to that URL.
 //In that address you will see another //JSON object. Look for a "files" key with an object containing a file. In that file you'll see
-//the "content" of the file. Next look for a "comments_url". At that address, you'll see a collection of comments, with keys such as "user" and "body".
-//Spec 1.1
-//Using jQuery to make an AJAX call, insert a list of links into #posts using JavaScript forEach with the "description" of each gist as the text.
-//Spec 1.2
-//Within each loop, write an if condition to insert only gists that start with a '#post' in the "description". After fitering, remove the '#post ' from the title.
-//Spec 1.3
-//Set the href attribute on each link to "#", and a "data-url" attribute equal to the "url" value.
-//Spec 2
-//After the links are are inserted, add a click listener, and prevent the default event from occuring. Then make an ajax call with the
-//"data-url" value, grabbing it with $.data('url').
-//Spec 3
-//Insert the "content" of the file named post.md into #post.
-
-//Spec 3.1
-//Since our "content" is written in Markdown, we can use the Marked.js library to convert the content to html. Simply call marked()
-//on your content. e.g. marked(content).
-//Spec 4
-//After inserting your content, make another ajax call using the "comments_url", and insert the ["user"]["login"] and "body" in a list
-//in #comments.
-//similar to TODO app
-//Spec 5
-//Make it look nice! This is going to be YOUR blog! Take a look at some CSS frameworks, such as:
-//Materialize - Framework based on Google's Material Design Guidelines
-//Material Design Lite - Google's Official CSS Material Framework
-//Bootstrap - Probably the most popular CSS framework on the web. Made by Twitter
-//Foundation - Industry standard of CSS Frameworks
-//Skeleton - Beautifully simple CSS Framework
+//the "content" of the file. Next look for a "comments_url". At that address, you'll see a collection of comments, 
+//with keys such as "user" and "body".
 //Spec 7
 //After development and styling on your blog dies down, you can plug it into your Github account. If done correctly,
-//you should only have to change one url: http://127.0.0.1:8080/apps/11gist-blog/api/gists.json to https://api.github.com/users/<your github username>/gists.
+//you should only have to change one url: http://127.0.0.1:8080/apps/11gist-blog/api/gists.json to 
+//https://api.github.com/users/<your github username>/gists.
 //Now whenever you create a //gist with "#post" in the title and a file named post.md, it will automatically be pulled into your blog
 'use strict';
 
