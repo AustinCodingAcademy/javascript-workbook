8836e5761d57fdc6e22f7f977f95b26b64e7e4d0
Jonathan Soape <jonathansoape@gmail.com>
2016-12-24 13:03:13 -0600

    address book still in progress, but almost done

--- apps/10address-book-ajax/script.js
+++ apps/10address-book-ajax/script.js
@@ -2,4 +2,27 @@
 
 $(document).ready(function() {
   // You code here
+  $.ajax('https://reqres-api.herokuapp.com/api/users', {
+    success: function(users){
+      users.forEach(function(user){
+        var str = "`<tr><td>${user.id}</td><td>${user.first_name}</td><td>${user.last_name}</td><td><a href='#' data-id='${user.id}'>View</a></tr>`";
+        $('tbody').append(str)
+      })
+
+      $('a').click(function(event){
+        event.preventDefault;
+        var url = 'https://reqres-api.herokuapp.com/api/users/';
+        url += $(this).data('id');
+        $.ajax(url, {
+          success: function(user){
+            
+          }
+        })
+      }
+    }
+
+
+  });
+
+
 });
