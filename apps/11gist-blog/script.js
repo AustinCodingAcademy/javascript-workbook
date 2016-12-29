ed276963e5e5478cca69201dc869fc63f06cd182
dmencha <dmencha@rMB.local>
2016-12-28 23:23:45 -0600

    Gist Blog

--- apps/11gist-blog/script.js
+++ apps/11gist-blog/script.js
@@ -1,5 +1,47 @@
+
 'use strict';
 
 $(document).ready(function() {
-  // You code here
+
+  $.ajax("http://127.0.0.1:8080/apps/11gist-blog/api/gists.json", {
+    success(gists) {
+      gists.forEach(function(gist) {
+
+        var post = "<a href='#' data-url='" + gist.url + "'>" + gist.description + "</a>";
+
+        if (post.indexOf("#post") !== -1) {
+          post = post.replace("#post", " ");
+          $("#posts").append("<li>" + post + "</li>");
+        }
+      });
+    },
+  });
+
+  $("#posts").on("click", "a", function(element){
+    element.preventDefault();
+    var $self = $(this).data('url');
+    $.ajax($self, {
+      success(url) {
+        var $post = url.files["post.md"].content;
+        var $marked = marked($post);
+
+        $("#post").append($marked);
+        getComments(url);
+      },
+    });
+  });
+
+  function getComments(url) {
+    $.ajax(url.comments_url, {
+      success(url) {
+        $.each(url, function(index, value) {
+          var $comments = value.user.login;
+          var $body = value.body;
+          $("#comments").append(" " + $comments + " ");
+          $('#comments').append(" " + $body + " ");
+        })
+      }
+    })
+  }
+
 });
