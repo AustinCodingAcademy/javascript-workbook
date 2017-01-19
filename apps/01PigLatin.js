7ec52b168cbe1b71081d1c7a16a2985896880132
James Puckett <jamesjaypuckett@gmail.com>
2017-01-19 15:31:54 -0600

    pig latin complete

--- apps/01PigLatin.js
+++ apps/01PigLatin.js
@@ -13,92 +13,85 @@ function pigLatin(word) {
   var vowelIndex = -1;
   var firstPart = '';
   var restWord = '';
-  if (word.slice(0, 1) === 'a' || word.slice(0, 1) === 'e' || word.slice(0, 1) === 'i' || word.slice(0, 1) === 'o' || word.slice(0, 1) === 'u' || word.slice(0, 1) === 'y' ) {
+  var vowel = '';
+  if (word.slice(0, 1) === 'a' || word.slice(0, 1) === 'e' || word.slice(0, 1) === 'i' || word.slice(0, 1) === 'o' || word.slice(0, 1) === 'u' || word.slice(0, 1) === 'y') {
     return word + 'yay';
   }
-  if ((word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex) || vowelIndex === -1) {
-    vowelIndex = word.indexOf('a');
-    console.log('VI: ' + vowelIndex);
-    if (vowelIndex === 0) {
-      return word + 'yay';
-    } else {
-      firstPart = word.slice(0, vowelIndex);
-      restWord = word.slice(vowelIndex, word.length);
-      console.log('first part: ' + firstPart);
-      console.log('restWord: ' + restWord);
-      return restWord + firstPart + 'ay';
-    }
-  }
+  for (var i = 0; i < word.length; i++) {
+    if (word[i] === 'a' || word[i] === 'e' || word[i] === 'i' || word[i] === 'o' || word[i] === 'u' || word[i] === 'y') {
+      vowel = word.slice(i, i + 1);
+      console.log('vowel: ' + vowel);
+      if ((word.indexOf(vowel) > -1 && word.indexOf(vowel) < vowelIndex) || vowelIndex === -1) {
+        vowelIndex = word.indexOf(vowel);
+        console.log('VI: ' + vowelIndex);
+        firstPart = word.slice(0, vowelIndex);
+        restWord = word.slice(vowelIndex, word.length);
+        console.log('first part: ' + firstPart);
+        console.log('restWord: ' + restWord);
+        return restWord + firstPart + 'ay';
+      }
 
-  if ((word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex) || vowelIndex === -1) {
-    vowelIndex = word.indexOf('e');
-    console.log('VI: ' + vowelIndex);
-    if (vowelIndex === 0) {
-      return word + 'yay';
-    } else {
-      firstPart = word.slice(0, vowelIndex);
-      restWord = word.slice(vowelIndex, word.length);
-      console.log('first part: ' + firstPart);
-      console.log('restWord: ' + restWord);
-      return restWord + firstPart + 'ay';
     }
   }
+  // if ((word.indexOf(vowel) > -1 && word.indexOf(vowel) < vowelIndex) || vowelIndex === -1) {
+  //   vowelIndex = word.indexOf(vowel);
+  //   console.log('VI: ' + vowelIndex);
+  //   firstPart = word.slice(0, vowelIndex);
+  //   restWord = word.slice(vowelIndex, word.length);
+  //   console.log('first part: ' + firstPart);
+  //   console.log('restWord: ' + restWord);
+  //   return restWord + firstPart + 'ay';
+  // }
 
-  if ((word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex) || vowelIndex === -1) {
-    vowelIndex = word.indexOf('i');
-    console.log('VI: ' + vowelIndex);
-    if (vowelIndex === 0) {
-      return word + 'yay';
-    } else {
-      firstPart = word.slice(0, vowelIndex);
-      restWord = word.slice(vowelIndex, word.length);
-      console.log('first part: ' + firstPart);
-      console.log('restWord: ' + restWord);
-      return restWord + firstPart + 'ay';
-    }
-  }
+  // if ((word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex) || vowelIndex === -1) {
+  //   vowelIndex = word.indexOf('e');
+  //   console.log('VI: ' + vowelIndex);
+  //   firstPart = word.slice(0, vowelIndex);
+  //   restWord = word.slice(vowelIndex, word.length);
+  //   console.log('first part: ' + firstPart);
+  //   console.log('restWord: ' + restWord);
+  //   return restWord + firstPart + 'ay';
+  // }
 
-  if ((word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex) || vowelIndex === -1) {
-    vowelIndex = word.indexOf('o');
-    console.log('VI: ' + vowelIndex);
-    if (vowelIndex === 0) {
-      return word + 'yay';
-    } else {
-      firstPart = word.slice(0, vowelIndex);
-      restWord = word.slice(vowelIndex, word.length);
-      console.log('first part: ' + firstPart);
-      console.log('restWord: ' + restWord);
-      return restWord + firstPart + 'ay';
-    }
-  }
+  // if ((word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex) || vowelIndex === -1) {
+  //   vowelIndex = word.indexOf('i');
+  //   console.log('VI: ' + vowelIndex);
+  //   firstPart = word.slice(0, vowelIndex);
+  //   restWord = word.slice(vowelIndex, word.length);
+  //   console.log('first part: ' + firstPart);
+  //   console.log('restWord: ' + restWord);
+  //   return restWord + firstPart + 'ay';
+  // }
 
-  if ((word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex) || vowelIndex === -1) {
-    vowelIndex = word.indexOf('u');
-    console.log('VI: ' + vowelIndex);
-    if (vowelIndex === 0) {
-      return word + 'yay';
-    } else {
-      firstPart = word.slice(0, vowelIndex);
-      restWord = word.slice(vowelIndex, word.length);
-      console.log('first part: ' + firstPart);
-      console.log('restWord: ' + restWord);
-      return restWord + firstPart + 'ay';
-    }
-  }
+  // if ((word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex) || vowelIndex === -1) {
+  //   vowelIndex = word.indexOf('o');
+  //   console.log('VI: ' + vowelIndex);
+  //   firstPart = word.slice(0, vowelIndex);
+  //   restWord = word.slice(vowelIndex, word.length);
+  //   console.log('first part: ' + firstPart);
+  //   console.log('restWord: ' + restWord);
+  //   return restWord + firstPart + 'ay';
+  // }
 
-  if ((word.indexOf('y') > -1 && word.indexOf('y') < vowelIndex) || vowelIndex === -1) {
-    vowelIndex = word.indexOf('y');
-    console.log('VI: ' + vowelIndex);
-    if (vowelIndex === 0) {
-      return word + 'yay';
-    } else {
-      firstPart = word.slice(0, vowelIndex);
-      restWord = word.slice(vowelIndex, word.length);
-      console.log('first part: ' + firstPart);
-      console.log('restWord: ' + restWord);
-      return restWord + firstPart + 'ay';
-    }
-  }
+  // if ((word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex) || vowelIndex === -1) {
+  //   vowelIndex = word.indexOf('u');
+  //   console.log('VI: ' + vowelIndex);
+  //   firstPart = word.slice(0, vowelIndex);
+  //   restWord = word.slice(vowelIndex, word.length);
+  //   console.log('first part: ' + firstPart);
+  //   console.log('restWord: ' + restWord);
+  //   return restWord + firstPart + 'ay';
+  // }
+
+  // if ((word.indexOf('y') > -1 && word.indexOf('y') < vowelIndex) || vowelIndex === -1) {
+  //   vowelIndex = word.indexOf('y');
+  //   console.log('VI: ' + vowelIndex);
+  //   firstPart = word.slice(0, vowelIndex);
+  //   restWord = word.slice(vowelIndex, word.length);
+  //   console.log('first part: ' + firstPart);
+  //   console.log('restWord: ' + restWord);
+  //   return restWord + firstPart + 'ay';
+  // }
 }
 
 
