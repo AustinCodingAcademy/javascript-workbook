aa80f2b9649217a7b9ddc6c44f393a23e54799f3
nevetsretlaw <nevetsretlaw@Stevens-MacBook-Pro-2.local>
2017-01-18 20:42:04 -0600

    Rock Paper Scissors Completed

--- apps/02RockPaperScissors.js
+++ apps/02RockPaperScissors.js
@@ -10,8 +10,30 @@ var rl = readline.createInterface({
 
 function rockPaperScissors(hand1, hand2) {
 
-  // Write code here
+  if (hand1.toLowerCase() === hand2.toLowerCase()) {
+    return "It\'s a tie!";
+  } 
 
+  if (hand1.toLowerCase() === 'rock') {
+    if (hand2.toLowerCase() === 'scissors') {
+      return 'Hand one wins!';}
+  }
+
+  return 'Hand two wins!'
+
+  if (hand1.toLowerCase() === 'paper') {
+    if (hand2.toLowerCase() === 'rock') {
+      return 'Hand one wins!';
+    }
+  return 'Hand two wins!';
+  }
+
+  if (hand1.toLowerCase() === 'scissors') {
+    if (hand2.toLowerCase() === 'paper') {
+      return 'Hand one wins!'
+    }
+    return 'Hand two wins!'
+  }
 }
 
 function getPrompt() {
