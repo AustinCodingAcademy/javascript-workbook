21ec3f69e0902b64d3eb94c46bda60ea50a7fc45
joeycaradonna <joeycaradonna45@gmail.com>
2017-01-19 20:27:58 -0600

    Pyg latin - so cute

--- apps/01PigLatin.js
+++ apps/01PigLatin.js
@@ -7,13 +7,47 @@ var rl = readline.createInterface({
   output: process.stdout
 });
 
+var word = 'car';
 
-function pigLatin(word) {
+var firstLetter = word[0];
+console.log(firstLetter);
+var word = 'crazy';
+var vowelIndex = -1; // Set it to assume there are no vowels
 
-  // Your code here
-
+if ( ( word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('a');
 }
 
+if ( ( word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('e');
+}
+
+if ( ( word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('i');
+}
+
+if ( ( word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('o');
+}
+
+if ( ( word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('u');
+}
+
+if ( ( word.indexOf('y') > -1 && word.indexOf('y') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('y');
+
+var firstPart = word.slice(0, vowelIndex);
+var restWord = word.slice(vowelIndex, word.length);
+}
+console.log(firstPart);
+console.log(restWord);
+
+if (vowelIndex === 0) {
+  return word + 'yay';
+} else {
+  return restWord + firstPart + 'ay';
+}
 
 function getPrompt() {
   rl.question('word ', (answer) => {
