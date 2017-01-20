d8792ebd48593cf73e410cf78a40e9b0fcc9e789
Morgakd <morgakd@sbcglobal.net>
2017-01-19 20:39:40 -0600

    Pig Latin retry

--- apps/01PigLatin.js
+++ apps/01PigLatin.js
@@ -3,51 +3,51 @@
 var assert = require('assert');
 var readline = require('readline');
 var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
-
+s
 
 function pigLatin(word) {
-    word = word.toLowerCase();
-    // var prompt = require('prompt');
-    // prompt.get(["Enter a word"], function(word, ))
-    var firstLetter = word[0];
-    console.log(firstLetter);
-    //
+  word = word.toLowerCase();
+  // var prompt = require('prompt');
+  // prompt.get(["Enter a word"], function(word, ))
+  var firstLetter = word[0];
+  console.log(firstLetter);
+  //
 
-    var vowelIndex = -1;
-    if ((word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex) || vowelIndex === -1) {
-        vowelIndex = word.indexOf('a');
-    }
-    if ((word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex) || vowelIndex === -1) {
-        vowelIndex = word.indexOf('e');
-    }
-    if ((word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex) || vowelIndex === -1) {
-        vowelIndex = word.indexOf('i');
-    }
-    if ((word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex) || vowelIndex === -1) {
-        vowelIndex = word.indexOf('ao');
-    }
-    if ((word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex) || vowelIndex === -1) {
-        vowelIndex = word.indexOf('u');
-    }
-    if ((word.indexOf('y') > -1 && word.indexOf('y') < vowelIndex) || vowelIndex === -1) {
-        vowelIndex = word.indexOf('y');
-    }
-    var firstPart = word.slice(0, vowelIndex);
-    var restWord = word.slice(vowelIndex, word.length);
-    console.log("First word", firstPart);
-    console.log("Rest word", restWord);
-    return restWord + firstPart + 'ay';
+  var vowelIndex = -1;
+  if ((word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex) || vowelIndex === -1) {
+    vowelIndex = word.indexOf('a');
+  }
+  if ((word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex) || vowelIndex === -1) {
+    vowelIndex = word.indexOf('e');
+  }
+  if ((word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex) || vowelIndex === -1) {
+    vowelIndex = word.indexOf('i');
+  }
+  if ((word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex) || vowelIndex === -1) {
+    vowelIndex = word.indexOf('ao');
+  }
+  if ((word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex) || vowelIndex === -1) {
+    vowelIndex = word.indexOf('u');
+  }
+  if ((word.indexOf('y') > -1 && word.indexOf('y') < vowelIndex) || vowelIndex === -1) {
+    vowelIndex = word.indexOf('y');
+  }
+  var firstPart = word.slice(0, vowelIndex);
+  var restWord = word.slice(vowelIndex, word.length);
+  console.log("First word", firstPart);
+  console.log("Rest word", restWord);
+  return restWord + firstPart + 'ay';
 
-    return word.replace(firstLetter, '') + firstLetter + 'ay';
+  return word.replace(firstLetter, '') + firstLetter + 'ay';
 
-    if (vowelIndex === 0) {
-        return word + 'yay';
-    } else {
-        return restWord + firstPart + 'ay'
-    }
+  if (vowelIndex === 0) {
+    return word + 'yay';
+  } else {
+    return restWord + firstPart + 'ay'
+  }
 }
 
 //
@@ -58,37 +58,37 @@ function pigLatin(word) {
 
 
 function getPrompt() {
-    rl.question('word ', (answer) => {
-        console.log(pigLatin(answer));
-        getPrompt();
-    });
+  rl.question('word ', (answer) => {
+    console.log(pigLatin(answer));
+    getPrompt();
+  });
 }
 
 // Tests
 
 if (typeof describe === 'function') {
 
-    describe('#pigLatin()', function() {
-        it('should translate a simple word', function() {
-            assert.equal(pigLatin('car'), 'arcay');
-            assert.equal(pigLatin('dog'), 'ogday');
-        });
-        it('should translate a complex word', function() {
-            assert.equal(pigLatin('create'), 'eatecray');
-            assert.equal(pigLatin('valley'), 'alleyvay');
-        });
-        it('should attach "yay" if word begins with vowel', function() {
-            assert.equal(pigLatin('egg'), 'eggyay');
-            assert.equal(pigLatin('emission'), 'emissionyay');
-        });
-        it('should auto lowercase word before translation', function() {
-            assert.equal(pigLatin('HeLlO'), 'ellohay');
-            assert.equal(pigLatin('RoCkEt'), 'ocketray');
-        });
+  describe('#pigLatin()', function() {
+    it('should translate a simple word', function() {
+      assert.equal(pigLatin('car'), 'arcay');
+      assert.equal(pigLatin('dog'), 'ogday');
     });
+    it('should translate a complex word', function() {
+      assert.equal(pigLatin('create'), 'eatecray');
+      assert.equal(pigLatin('valley'), 'alleyvay');
+    });
+    it('should attach "yay" if word begins with vowel', function() {
+      assert.equal(pigLatin('egg'), 'eggyay');
+      assert.equal(pigLatin('emission'), 'emissionyay');
+    });
+    it('should auto lowercase word before translation', function() {
+      assert.equal(pigLatin('HeLlO'), 'ellohay');
+      assert.equal(pigLatin('RoCkEt'), 'ocketray');
+    });
+  });
 } else {
 
-    getPrompt();
+  getPrompt();
 
 }
 
