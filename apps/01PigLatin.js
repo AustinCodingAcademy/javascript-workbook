fc234d3051a1b1d3f26d80728746ddd577d31bfd
Chris Austin <christopheraustin@Christophers-MacBook-Pro.local>
2017-01-19 20:31:16 -0600

    Pyg latin - so cute

--- apps/01PigLatin.js
+++ apps/01PigLatin.js
@@ -9,8 +9,42 @@ var rl = readline.createInterface({
 
 
 function pigLatin(word) {
+word = word.toLowerCase()
+  var firstLetter = work[0]
+  console.log('firstLetter:'firstLetter);
 
-  // Your code here
+var vowelIndex = -1; // Set it to assume there are no vowels
+
+if ( ( word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('a');
+}
+
+if ( ( word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('e');
+}
+
+if ( ( word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('i');
+}
+if ( ( word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('o');
+}
+
+if ( ( word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('u');
+}
+
+if ( ( word.indexOf('y') > -1 && word.indexOf('y') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('y');
+}
+var fistPart = word.slice(0, vowelIndex);
+var restWord = word.slice(vowelIndex, word.length);
+console.log ("first part",fistPart);
+console.log ('rest word', restworld);
+if (vowelIndex === 0)
+return word.to
+return word.replace(firstLetter, '') + firstLetter + 'ay';
+
 
 }
 
