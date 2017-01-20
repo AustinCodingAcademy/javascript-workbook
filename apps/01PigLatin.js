f95cd9bf5c641b009cb3e2e04d7c19f84d7f0955
samlynch96 <sam@bigbabyagency.com>
2017-01-19 20:34:46 -0600

    Pig Latin

--- apps/01PigLatin.js
+++ apps/01PigLatin.js
@@ -9,8 +9,48 @@ var rl = readline.createInterface({
 
 
 function pigLatin(word) {
-
+  word = word.toLowerCase();
   // Your code here
+  // var word = 'car';
+
+var firstLetter = word[0];
+
+
+
+var vowelIndex = -1; // Set it to assume there are no vowels
+
+if ( ( word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('a');
+}
+
+if ( ( word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('e');
+}
+
+if ( ( word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('i');
+}
+
+if ( ( word.indexOf('o') > -1 && word.indexOf('i') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('o');
+}
+
+if ( ( word.indexOf('u') > -1 && word.indexOf('i') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('u');
+}
+
+if ( ( word.indexOf('y') > -1 && word.indexOf('i') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('y');
+}
+
+var firstPart = word.slice(0, vowelIndex);
+var restWord = word.slice(vowelIndex, word.length);
+
+
+if (vowelIndex === 0) {
+  return word + 'yay';
+} else{
+return restWord.toLowerCase() + firstPart.toLowerCase() + 'ay';
 
 }
 
