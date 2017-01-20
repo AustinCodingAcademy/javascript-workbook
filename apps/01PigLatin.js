5b9ccf6a25c2b971281f75e36de2b1543d87d1c4
Desmond Thomas <d.ishmael.thomas@gmail.com>
2017-01-19 20:26:44 -0600

    Pig Latin App created with no failures

--- apps/01PigLatin.js
+++ apps/01PigLatin.js
@@ -9,8 +9,50 @@ var rl = readline.createInterface({
 
 
 function pigLatin(word) {
+  word = word.toLowerCase();
 
-  // Your code here
+  var firstLetter = word[0];
+
+  console.log('firstLetter:' firstLetter);
+
+  var vowelIndex = -1; // Set it to assume there are no vowels
+
+  if ( ( word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('a');
+  }
+
+  if ( ( word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('e');
+  }
+
+  if ( ( word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('i');
+  }
+
+  if ( ( word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('o');
+  }
+
+  if ( ( word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('u');
+  }
+
+  if ( ( word.indexOf('y') > -1 && word.indexOf('y') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('y');
+  }
+
+  var firstPart = word.slice(0, vowelIndex);
+
+  var restWord = word.slice(vowelIndex, word.length);
+
+  console.log('first part:', firstpart);
+  console.log('last part:', lastpart);
+
+  if (vowelIndex === 0) {
+    return word + 'ay';
+  } else {
+  return restWord + firstPart + 'ay';
+  }
 
 }
 
