59f58821360839034f891d5e498f032d7a6ae5d9
Collind09 <collind09@gmail.com>
2017-01-19 20:26:52 -0600

    pyg Latin - so cut

--- apps/01PigLatin.js
+++ apps/01PigLatin.js
@@ -12,7 +12,44 @@ function pigLatin(word) {
 
   // Your code here
 
+  // var word = 'car';
+
+
+  var firstLetter = word[0];
+
+
+  console.log(crazy);
+
+  var vowelIndex = -1; // Set it to assume there are no vowels
+
+if ( ( word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('a');
 }
+
+if ( ( word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('e');
+}
+
+if ( ( word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('i');
+}
+
+if ( ( word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('i');
+}
+
+if ( ( word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('i');
+}
+
+if ( ( word.indexOf('y') > -1 && word.indexOf('y') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('i');
+}
+
+//... the rest for o, u, y
+}
+
+
 
 
 function getPrompt() {
