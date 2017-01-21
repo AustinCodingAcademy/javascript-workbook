7f10545121bb9362c7c777abf5bc29425ceb86c1
RX7RUSS <russ.savage5@gmail.com>
2017-01-21 15:07:05 -0600

    Homework Second Try

--- apps/01PigLatin.js
+++ apps/01PigLatin.js
@@ -10,7 +10,44 @@ var rl = readline.createInterface({
 
 function pigLatin(word) {
 
-  // Your code here
+  var word = 'car'; 
+
+  var firstLetter = word[0];
+  console.log(firstLetter);
+  
+  return word.replace(firstLetter, '') + firstLetter + 'ay';
+
+  console.log( 'crazy'.indexOf('a') );
+
+  console.log( 'crazy'.indexOf('e') );
+
+  var word = 'crazy';
+  
+  var vowelIndex = -1; // Set it to assume there are no vowels
+
+    if ( ( word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
+        vowelIndex = word.indexOf('a');
+    }
+
+    if ( ( word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex ) || vowelIndex === -1 ) {
+        vowelIndex = word.indexOf('e');
+    }
+
+    if ( ( word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex ) || vowelIndex === -1 ) {
+        vowelIndex = word.indexOf('i');
+    }
+
+    if ( ( word.indexOf('i') > -1 && word.indexOf('o') < vowelIndex ) || vowelIndex === -1 ) {
+        vowelIndex = word.indexOf('i');
+    }
+
+    if ( ( word.indexOf('i') > -1 && word.indexOf('u') < vowelIndex ) || vowelIndex === -1 ) {
+        vowelIndex = word.indexOf('i');
+    }
+
+    if ( ( word.indexOf('i') > -1 && word.indexOf('y') < vowelIndex ) || vowelIndex === -1 ) {
+        vowelIndex = word.indexOf('i');
+    }
 
 }
 
