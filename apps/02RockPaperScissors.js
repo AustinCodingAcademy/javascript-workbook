19ded1a1ac95b06b78130a6391cde47f321e6f7c
aflater <aflater78704@gmail.com>
2017-01-20 20:02:19 -0600

    02RockPaperScissors

--- apps/02RockPaperScissors.js
+++ apps/02RockPaperScissors.js
@@ -10,9 +10,42 @@ var rl = readline.createInterface({
 
 function rockPaperScissors(hand1, hand2) {
 
-  // Write code here
+if (hand1 === hand2) {
+    return "It's a tie!";
+  }
 
+if (hand1 === 'rock') {
+if (hand2 === 'scissors') {
+  return 'Hand one wins!';
+  }
+if (hand2 === 'paper') {
+  return 'Hand two wins!';
+  }
 }
+
+
+if (hand1 === 'paper') {
+if (hand2 === 'scissors') {
+  return 'Hand two wins!';
+  }
+
+if (hand2 === rock) {
+  return 'Hand one wins!';
+  }
+  }
+
+if (hand1 === "scissors") {
+if (hand2 === 'rock') {
+   return 'Hand two wins!';
+  }
+
+if (hand2 === 'paper') {
+    return 'Hand one wins!';
+  }
+} 
+}
+
+
 
 function getPrompt() {
   rl.question('hand1: ', (answer1) => {
@@ -42,5 +75,6 @@ if (typeof describe === 'function') {
 } else {
 
   getPrompt();
+
 
 }
