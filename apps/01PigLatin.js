fec3eb2943f7ec1db77f8b130e32764c7cc47540
avagore <avaeelerog@gmail.com>
2017-01-22 20:01:34 -0600

    Fixed indentation

--- apps/01PigLatin.js
+++ apps/01PigLatin.js
@@ -14,38 +14,39 @@ function pigLatin(word) {
   var vowelIndex = -1; // Set it to assume there are no vowels
 
   if ( ( word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
-      vowelIndex = word.indexOf('a');
+    vowelIndex = word.indexOf('a');
   }
 
   if ( ( word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex ) || vowelIndex === -1 ) {
-      vowelIndex = word.indexOf('e');
+    vowelIndex = word.indexOf('e');
   }
 
   if ( ( word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex ) || vowelIndex === -1 ) {
-      vowelIndex = word.indexOf('i');
+    vowelIndex = word.indexOf('i');
   }
 
   if ( ( word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex ) || vowelIndex === -1 ) {
-      vowelIndex = word.indexOf('o');
+    vowelIndex = word.indexOf('o');
   }
 
   if ( ( word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex ) || vowelIndex === -1 ) {
-      vowelIndex = word.indexOf('u');
+    vowelIndex = word.indexOf('u');
   }
   if ( ( word.indexOf('y') > -1 && word.indexOf('y') < vowelIndex ) || vowelIndex === -1 ) {
-      vowelIndex = word.indexOf('y');
+    vowelIndex = word.indexOf('y');
   }
   
   word = word.toLowerCase();
   var firstPart = word.slice(0, vowelIndex);
   var restWord = word.slice(vowelIndex, word.length);
 
+//making sure first letter isn't a vowel, if so, add 'yay'
   if(vowelIndex > 0) {
     return restWord + firstPart + 'ay';
   } else {
     return word + 'yay';
   }
-}
+};
 
 
 function getPrompt() {
