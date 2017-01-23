80d130a2fdea39de37a079f58fe91d7c1bf0602f
RX7RUSS <russ.savage5@gmail.com>
2017-01-22 18:29:40 -0600

    02HomeworkTwo

--- homework/02LessonTwo.js
+++ homework/02LessonTwo.js
@@ -26,7 +26,12 @@ var alwaysTrue = function () {
 // argument1 is equal to argument2
 
 function equals(argument1, argument2) {
-}
+  if (argument1 === argument2) {
+    return true; 
+  } else {
+    return false;
+  }
+};
 
 // Problem 2:
 // lessThanOrEqualTo(parameter1, parameter2) is an empty function
@@ -34,17 +39,30 @@ function equals(argument1, argument2) {
 // parameter1 is less than or equal to parameter2
 
 function lessThanOrEqualTo(parameter1, parameter2) {
-}
+  if (parameter1 <= parameter2) {
+    return true; 
+  } else {
+    return false;
+  }
+};
 
 // Problem 3:
 // write a function named add(number1, number2)
 // add will add two numbers and return the result
+
+function add(number1, number2) {
+  return number1 + number2;
+};
 
 // Problem 4:
 // write a function named addThree(number1, number2, number3)
 // this function will add three numbers
 // you must call your function add() in addThree()
 
+function addThree(number1, number2, number3) {
+  return number1 + number2 + number3;
+}
+add();
 
 // ****
 // Concept Checkpoint
@@ -53,22 +71,24 @@ function lessThanOrEqualTo(parameter1, parameter2) {
 //
 // What is a function? How do you define a function in Javascript?
 //
-// Your Answer Goes Here:
+// Your Answer Goes Here: A function is a set of statements that performs a task or calculates a value. 
 //
 //
 // What is a return value?
 //
-// Your Answer Goes Here:
+// Your Answer Goes Here: The return statement stops the execution of a function and returns a value from that function.
 //
 //
 // How do you define a named function?
 //
-// Your Answer Goes Here:
+// Your Answer Goes Here: A named function is a function declaration if it appears as a statement.
 //
 //
 // What is a parameter? What is an argument? Is there a difference between the two?
 //
-// Your Answer Goes Here:
+// Your Answer Goes Here: An argument in context with functions is the actual value that is passed to the function
+//( as input) ,when it is called. However parameter refers to the variables that are used in the function declaration/definition
+//to represent those arguments that were send to the function during the function call.
 //
 // ****
 
@@ -84,6 +104,11 @@ function lessThanOrEqualTo(parameter1, parameter2) {
 // complete isEven() by returning a boolean expression
 
 function isEven(number) {
+  if (number % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Problem 6:
@@ -93,6 +118,11 @@ function isEven(number) {
 // complete isDivisibleByThree() by returning a boolean expression
 
 function isDivisibleByThree(number) {
+  if (number % 3 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // ****
@@ -108,6 +138,15 @@ function isDivisibleByThree(number) {
 // should return false if character is anything else
 
 function whichSpecies(character) {
+   if (character === 'scooby') {
+      return "dog";
+  } else if (character === 'garfield' ) {
+      return "cat";
+  } else if (character === 'nemo') {
+      return "fish";
+  } else {
+      return false;
+  }
 }
 
 // Problem 8:
@@ -118,6 +157,20 @@ function whichSpecies(character) {
 // return the string "divisible by 3" when number % 3 === 0
 // return the string "divisible by 5" when number % 5 === 0
 
+function testNumber(number){
+  if(number % 4 === 0){
+    return('divisible by 4');
+  }
+  if(number % 2 === 0){
+    return('divisible by 2');
+  }
+  if(number % 3 === 0){
+    return('divisible by 3');
+  }
+  if(number % 5 === 0){
+    return('divisible by 5');
+  }
+}
 
 // ****
 // Concept Checkpoint
