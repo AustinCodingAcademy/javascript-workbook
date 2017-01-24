cfb2ebe0304019120665f350a3bdea7622052622
aleexga <aleexga@gmail.com>
2017-01-23 18:49:46 -0600

    Rock Paper Scissors app finished

--- apps/02RockPaperScissors.js
+++ apps/02RockPaperScissors.js
@@ -9,14 +9,40 @@ var rl = readline.createInterface({
 
 
 function rockPaperScissors(hand1, hand2) {
-
-  // Write code here
-
+  if ((hand1&&hand2 === 'rock') || (hand1&&hand2 === 'paper') || (hand1&&hand2 === 'scissors')) {
+    if (hand1 === hand2) {
+      return "It's a tie!";
+    }
+    if (hand1 === 'rock') {
+      if (hand2 === 'scissors') {
+        return 'Hand one wins!';
+      }
+      // If we reach here, player 2 must have dealt paper
+      return 'Hand two wins!';
+    }
+    if (hand1 === 'paper') {
+      if (hand2 ==='rock') {
+        return 'Hand one wins!';
+      }
+      //If we reach here, player 2 must have dealt scissors
+      return 'Hand two wins!';
+    }
+    if (hand1 === 'scissors') {
+      if (hand2 === 'rock'){
+        return 'Hand two wins!';
+      }
+      // If we reach here, player 2 must have dealt paper 
+      return 'Hand one wins!';
+    }
+  }
+  else {
+    console.log( 'thats not an input silly! Try again!' );
+  }
 }
 
 function getPrompt() {
   rl.question('hand1: ', (answer1) => {
-    rl.question('hand2: ', (answer2) => {
+    rl.question('hand2: ', (answer2) => { 
       console.log( rockPaperScissors(answer1, answer2) );
       getPrompt();
     });
