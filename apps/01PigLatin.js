aa1af456199457254f39b006d32534e9318d411c
nevetsretlaw <nevetsretlaw@Stevens-MacBook-Pro-2.local>
2017-01-23 21:11:45 -0600

    Pig Latin Mark1

--- apps/01PigLatin.js
+++ apps/01PigLatin.js
@@ -10,9 +10,52 @@ var rl = readline.createInterface({
 
 function pigLatin(word) {
 
-  // Your code here
+  // When the user inputs a word the variable "word" will equal the user's input
+  // "pigLatin" will translate that word into Pig Latin
 
+  /*So the basic idea of Pig Latin is to 
+  
+  take the first letters of the word 
+    up to the first vowel, 
+  move them to the back, 
+  and add 'ay' to the end of it.*/
+
+  word = word.toLowerCase();
+
+  // This will determine if the word has a vowel and return the index of the first vowel
+  var vowelIndex = -1;
+
+  if ( ( word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('a');
+  }
+  if ( ( word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('e');
+  }
+  if ( ( word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('i');
+  }
+  if ( ( word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('o');
+  }
+  if ( ( word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('u');
+  }
+
+  // console.log(vowelIndex);
+  // End of vowelIndex code
+
+  // This will divide the word into two parts with the first part starting at [0] and ending at the first vowel
+  var firstPart = word.slice(0, vowelIndex);
+  var restWord = word.slice(vowelIndex,word.length);
+  
+  if (vowelIndex === 0) {
+    return word + 'yay';
+  }
+  else {
+    return restWord + firstPart + 'ay'
+  };
 }
+  
 
 
 function getPrompt() {
