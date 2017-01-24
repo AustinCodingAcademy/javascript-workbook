089919626ea872c114200a73e1fc7976d43b374b
Leslie Robertson <leslievrobertson@gmail.com>
2017-01-24 15:04:30 -0600

    tests passing

--- apps/02RockPaperScissors.js
+++ apps/02RockPaperScissors.js
@@ -10,8 +10,33 @@ var rl = readline.createInterface({
 
 function rockPaperScissors(hand1, hand2) {
 
-  // Write code here
+  hand1 = hand1.toLowerCase();
+  hand2 = hand2.toLowerCase();
 
+  if (hand1 === hand2) {
+    return 'It\'s a tie!';
+  }
+
+  if (hand1 === 'rock') {
+    if (hand2 === 'scissors') {
+      return 'Hand one wins!';
+    }
+    return 'Hand two wins!';
+  }
+
+  if (hand1 === 'paper') {
+    if (hand2 === 'rock') {
+      return 'Hand one wins!';
+    }
+    return 'Hand two wins!';
+  }
+
+  if (hand1 === 'scissors') {
+    if (hand2 === 'paper') {
+      return 'Hand one wins!';
+    }
+    return 'Hand two wins!';
+  }
 }
 
 function getPrompt() {
