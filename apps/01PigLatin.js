ba763f456f6c1d96406e4cc1b94159893429581d
foxjo <jordan.wharton@yahoo.com>
2017-01-24 17:45:58 -0600

    Pig Latin Test passing maybe

--- apps/01PigLatin.js
+++ apps/01PigLatin.js
@@ -9,7 +9,7 @@ var rl = readline.createInterface({
 
 
 function pigLatin(word) {
-    //var word = 'car';
+  //var word = 'car';
   word = word.toLowerCase();
   var firstLetter = word[0];
   console.log('first letter:', firstLetter);
@@ -19,29 +19,29 @@ function pigLatin(word) {
   var vowelIndex = -1; // Set it to assume there are no vowels
 
   if ((word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex) || vowelIndex === -1) {
-      vowelIndex = word.indexOf('a');
-    }
+    vowelIndex = word.indexOf('a');
+  }
 
   if ((word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex) || vowelIndex === -1) {
-      vowelIndex = word.indexOf('e');
-    }
+    vowelIndex = word.indexOf('e');
+  }
 
-   if ((word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex) || vowelIndex === -1) {
-      vowelIndex = word.indexOf('i');
-    }
+  if ((word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex) || vowelIndex === -1) {
+    vowelIndex = word.indexOf('i');
+  }
 
   if ((word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex) || vowelIndex === -1) {
-      vowelIndex = word.indexOf('o');
-    }
+    vowelIndex = word.indexOf('o');
+  }
 
 
   if ((word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex) || vowelIndex === -1) {
-      vowelIndex = word.indexOf('u');
-    }
+    vowelIndex = word.indexOf('u');
+  }
 
   if ((word.indexOf('y') > -1 && word.indexOf('y') < vowelIndex) || vowelIndex === -1) {
-      vowelIndex = word.indexOf('y');
-    }
+    vowelIndex = word.indexOf('y');
+  }
 
   var firstPart = word.slice(0, vowelIndex);
   var restWord = word.slice(vowelIndex, word.length);
@@ -50,18 +50,18 @@ function pigLatin(word) {
 
 
   if (vowelIndex === 0) {
-      return word + 'yay';
-    } else {
-      return restWord + firstPart + 'ay';
-    }
+    return word + 'yay';
+  } else {
+    return restWord + firstPart + 'ay';
+  }
 }
 
 
 function getPrompt() {
   rl.question('word ', (answer) => {
-      console.log(pigLatin(answer));
-      getPrompt();
-    });
+    console.log(pigLatin(answer));
+    getPrompt();
+  });
 }
 
 // Tests
@@ -69,23 +69,23 @@ function getPrompt() {
 if (typeof describe === 'function') {
 
   describe('#pigLatin()', function() {
-      it('should translate a simple word', function() {
-          assert.equal(pigLatin('car'), 'arcay');
-          assert.equal(pigLatin('dog'), 'ogday');
-        });
-      it('should translate a complex word', function() {
-          assert.equal(pigLatin('create'), 'eatecray');
-          assert.equal(pigLatin('valley'), 'alleyvay');
-        });
-      it('should attach "yay" if word begins with vowel', function() {
-          assert.equal(pigLatin('egg'), 'eggyay');
-          assert.equal(pigLatin('emission'), 'emissionyay');
-        });
-      it('should auto lowercase word before translation', function() {
-          assert.equal(pigLatin('HeLlO'), 'ellohay');
-          assert.equal(pigLatin('RoCkEt'), 'ocketray');
-        });
+    it('should translate a simple word', function() {
+      assert.equal(pigLatin('car'), 'arcay');
+      assert.equal(pigLatin('dog'), 'ogday');
     });
+    it('should translate a complex word', function() {
+      assert.equal(pigLatin('create'), 'eatecray');
+      assert.equal(pigLatin('valley'), 'alleyvay');
+    });
+    it('should attach "yay" if word begins with vowel', function() {
+      assert.equal(pigLatin('egg'), 'eggyay');
+      assert.equal(pigLatin('emission'), 'emissionyay');
+    });
+    it('should auto lowercase word before translation', function() {
+      assert.equal(pigLatin('HeLlO'), 'ellohay');
+      assert.equal(pigLatin('RoCkEt'), 'ocketray');
+    });
+  });
 } else {
 
   getPrompt();
