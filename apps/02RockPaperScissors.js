8b5298e05adfb83567f7b5a61c0df6a371e5ae66
aflater <aflater78704@gmail.com>
2017-01-25 08:03:52 -0600

    Fixed indentation errors

--- apps/02RockPaperScissors.js
+++ apps/02RockPaperScissors.js
@@ -10,43 +10,41 @@ var rl = readline.createInterface({
 
 function rockPaperScissors(hand1, hand2) {
 
-if (hand1 === hand2) {
-    return "It's a tie!";
-  }
+  if (hand1 === hand2) {
+  return "It's a tie!";
+}
 
-if (hand1 === 'rock') {
-if (hand2 === 'scissors') {
+  if (hand1 === 'rock') {
+  if (hand2 === 'scissors') {
   return 'Hand one wins!';
-  }
-if (hand2 === 'paper') {
+}
+  if (hand2 === 'paper') { 
   return 'Hand two wins!';
-  }
 }
 
 
-if (hand1 === 'paper') {
-if (hand2 === 'scissors') {
+  if (hand1 === 'paper') {
+  if (hand2 === 'scissors') {
   return 'Hand two wins!';
-  }
+}
 
-if (hand2 === rock) {
+  if (hand2 === rock) {
   return 'Hand one wins!';
-  }
-  }
-
-if (hand1 === "scissors") {
-if (hand2 === 'rock') {
-   return 'Hand two wins!';
-  }
-
-if (hand2 === 'paper') {
-    return 'Hand one wins!';
-  }
-} 
 }
 
 
+  if (hand1 === "scissors") {
+  if (hand2 === 'rock') {
+  return 'Hand two wins!';
+}
 
+  if (hand2 === 'paper') {
+  return 'Hand one wins!';
+}
+
+
+}
+}
 function getPrompt() {
   rl.question('hand1: ', (answer1) => {
     rl.question('hand2: ', (answer2) => {
