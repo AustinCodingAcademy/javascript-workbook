3c1754c48c3910a6afacc57189e698eb6edc124d
aleexga <aleexga@gmail.com>
2017-01-24 18:49:33 -0600

    PigLatin App finished

--- apps/01PigLatin.js
+++ apps/01PigLatin.js
@@ -7,23 +7,57 @@ var rl = readline.createInterface({
   output: process.stdout
 });
 
+function pigLatin(answer) {
+  //Declaring some variables
+  var word = answer.toLowerCase();
+  var firstLetter = word[0];
+  var vowelIndex;
 
-function pigLatin(word) {
-
-  // Your code here
-
+  //Goes to checkVowel function (below) and checks if the first letter is a vowel
+  if (checkVowel(firstLetter) === -1){
+    //It is not a vowel! 
+    vowelIndex = checkVowel(word);
+    var firstPart = word.slice(0, vowelIndex);
+    var restWord = word.slice(vowelIndex, word.length);
+    return restWord + firstPart + 'ay'; 
+  }
+  else{
+    //It is indeed a vowel! So just add 'ay', as in 'YAY!'
+    return answer + 'ay';
+  }
 }
 
+//Checks if there is a vowel AND where is the vowel
+function checkVowel(word){
+  var vowelIndex = -1; // Set it to assume there are no vowels
 
+  if ( ( word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
+      vowelIndex = word.indexOf('a');
+  }
+  if ( ( word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex ) || vowelIndex === -1 ) {
+      vowelIndex = word.indexOf('e');
+  }
+  if ( ( word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex ) || vowelIndex === -1 ) {
+      vowelIndex = word.indexOf('i');
+  }
+  if ( ( word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex ) || vowelIndex === -1 ) {
+      vowelIndex = word.indexOf('o');
+  }
+  if ( ( word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex ) || vowelIndex === -1 ) {
+      vowelIndex = word.indexOf('u');
+  }
+  return vowelIndex;
+}
+
+//Asks the user for a word and gets the word
 function getPrompt() {
-  rl.question('word ', (answer) => {
-    console.log( pigLatin(answer) );
-    getPrompt();
-  });
-}
+   rl.question('what word do you wish to translate? huhhh? ', (answer) => {
+     console.log( pigLatin(answer) );
+     getPrompt();
+   });
+ }
 
 // Tests
-
 if (typeof describe === 'function') {
 
   describe('#pigLatin()', function () {
