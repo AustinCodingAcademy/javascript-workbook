ae02fe326dafe46333a77e416c917191954e8ee7
James Puckett <jamesjaypuckett@gmail.com>
2017-01-24 20:56:28 -0600

    RPS attempted to add a win tracker and determine who wins 2 out of 3, no luck, commented that section out

--- apps/02RockPaperScissors.js
+++ apps/02RockPaperScissors.js
@@ -7,72 +7,118 @@ var rl = readline.createInterface({
   output: process.stdout
 });
 
+
+
 function rockPaperScissors(hand1, hand2) {
   //converts word to lower case
   var p1 = hand1.toLowerCase();
   var p2 = hand2.toLowerCase();
+  var p1Wins = 0;
+  var p2Wins = 0;
+  var tie = 0;
+  var score = 'Current score-   p1: ' + p1Wins + '    p2: ' + p2Wins + '    tie: ' + tie;
 
   //checks to see if the person at least got the first letter correct. just in the event of a typo. also allows the use to just type r,p, or s
-  if (p1.slice(0,1) === 'r'){
+  if (p1.slice(0, 1) === 'r') {
     p1 = 'rock';
   }
-  if (p1.slice(0,1) === 'p'){
+  if (p1.slice(0, 1) === 'p') {
     p1 = 'paper';
   }
-  if (p1.slice(0,1) === 's'){
+  if (p1.slice(0, 1) === 's') {
     p1 = 'scissors';
   }
 
-  if (p2.slice(0,1) === 'r'){
+  if (p2.slice(0, 1) === 'r') {
     p2 = 'rock';
   }
-  if (p2.slice(0,1) === 'p'){
+  if (p2.slice(0, 1) === 'p') {
     p2 = 'paper';
   }
-  if (p2.slice(0,1) === 's'){
+  if (p2.slice(0, 1) === 's') {
     p2 = 'scissors';
   }
 
   //filters any answers that arent rock paper or scisossors out and anything spelt incorrectly
-  if (p1 !=='rock' && p1 !== 'paper' && p1 !== 'scissors'){
+  if (p1 !== 'rock' && p1 !== 'paper' && p1 !== 'scissors') {
     return 'Invalid entry player 1, please try again.';
   }
-  if (p2 !=='rock' && p2 !== 'paper' && p2 !== 'scissors'){
+  if (p2 !== 'rock' && p2 !== 'paper' && p2 !== 'scissors') {
     return 'Invalid entry player 2, please try again.';
   }
   //checks for tie
-  if (p1 === p2){
+  if (p1 === p2) {
+    // ++tie;
+    // console.log(score);
+    // checkForWin(p1Wins, p2Wins);
     return "It's a tie!";
   }
 
   //checks all three options for p1 vs p2
-  if (p1 === 'rock'){
-    if (p2 === 'paper'){
+  if (p1 === 'rock') {
+    if (p2 === 'paper') {
+      // ++p2Wins;
+      // console.log(score);
+      // checkForWin(p1Wins, p2Wins);
       return 'Hand two wins!';
-    }else {
+    } else {
+      // ++p1Wins;
+      // console.log(score);
+      // checkForWin(p1Wins, p2Wins);
       return 'Hand one wins!';
     }
   }
-  if (p1 === 'paper'){
-    if (p2 === 'rock'){
+  if (p1 === 'paper') {
+    if (p2 === 'rock') {
+      // ++p1Wins;
+      // console.log(score);
+      // checkForWin(p1Wins, p2Wins);
       return 'Hand one wins!';
-    }else {
+    } else {
+      // ++p2Wins;
+      // console.log(score);
+      // checkForWin(p1Wins, p2Wins);
       return 'Hand two wins!';
     }
   }
-  if (p1 === 'scissors'){
-    if (p2 === 'paper'){
+  if (p1 === 'scissors') {
+    if (p2 === 'paper') {
+      // ++p1Wins;
+      // console.log(score);
+      // checkForWin(p1Wins, p2Wins);
       return 'Hand one wins!';
-    }else {
+    } else {
+      // ++p2Wins;
+      // console.log(score);
+      // checkForWin(p1Wins, p2Wins);
       return 'Hand two wins!';
     }
   }
+
+  // function checkForWin(play1, play2) {
+  //   if (play1 === 2) {
+  //     console.log('Hand 1 wins 2 out of 3! Reseting...');
+  //     p1Wins = 0;
+  //     p2Wins = 0;
+  //     tie = 0;
+  //     getPrompt();
+  //   }
+  //   if (play2 === 2) {
+  //     console.log('Hand 2 wins 2 out of 3! Reseting...');
+  //     p1Wins = 0;
+  //     p2Wins = 0;
+  //     tie = 0;
+  //     getPrompt();
+  //   }
+  // }
 }
+
+
 
 function getPrompt() {
   rl.question('hand1: ', (answer1) => {
     rl.question('hand2: ', (answer2) => {
-      console.log( rockPaperScissors(answer1, answer2) );
+      console.log(rockPaperScissors(answer1, answer2));
       getPrompt();
     });
   });
