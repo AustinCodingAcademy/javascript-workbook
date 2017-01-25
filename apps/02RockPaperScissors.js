83a31dbfac6f128c0e8530d0b0957051f14b65ae
foxjo <jordan.wharton@yahoo.com>
2017-01-24 20:49:37 -0600

    RockPaperScissors test passing

--- apps/02RockPaperScissors.js
+++ apps/02RockPaperScissors.js
@@ -41,33 +41,33 @@ function rockPaperScissors(hand1, hand2) {
   }
 }
 
-  function getPrompt() {
-    rl.question('hand1: ', (answer1) => {
-      rl.question('hand2: ', (answer2) => {
-        console.log(rockPaperScissors(answer1, answer2));
-        getPrompt();
-      });
+function getPrompt() {
+  rl.question('hand1: ', (answer1) => {
+    rl.question('hand2: ', (answer2) => {
+      console.log(rockPaperScissors(answer1, answer2));
+      getPrompt();
     });
-  }
+  });
+}
 
-  // Tests
+// Tests
 
-  if (typeof describe === 'function') {
+if (typeof describe === 'function') {
 
-    describe('#rockPaperScissors()', function() {
-      it('should detect a tie', function() {
-        assert.equal(rockPaperScissors('rock', 'rock'), "It's a tie!");
-        assert.equal(rockPaperScissors('paper', 'paper'), "It's a tie!");
-        assert.equal(rockPaperScissors('scissors', 'scissors'), "It's a tie!");
-      });
-      it('should detect which hand won', function() {
-        assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
-        assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
-        assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
-      });
+  describe('#rockPaperScissors()', function() {
+    it('should detect a tie', function() {
+      assert.equal(rockPaperScissors('rock', 'rock'), "It's a tie!");
+      assert.equal(rockPaperScissors('paper', 'paper'), "It's a tie!");
+      assert.equal(rockPaperScissors('scissors', 'scissors'), "It's a tie!");
     });
-  } else {
+    it('should detect which hand won', function() {
+      assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
+      assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
+      assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
+    });
+  });
+} else {
 
-    getPrompt();
+  getPrompt();
 
-  }
+}
