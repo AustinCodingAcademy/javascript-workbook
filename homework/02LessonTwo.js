a9a720a00394eecdd109301c1d7dc3263132c965
aleexga <aleexga@gmail.com>
2017-01-24 19:18:21 -0600

    homework 02 finished

--- homework/02LessonTwo.js
+++ homework/02LessonTwo.js
@@ -26,6 +26,12 @@ var alwaysTrue = function () {
 // argument1 is equal to argument2
 
 function equals(argument1, argument2) {
+  if(argument1 === argument2){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 // Problem 2:
@@ -34,17 +40,30 @@ function equals(argument1, argument2) {
 // parameter1 is less than or equal to parameter2
 
 function lessThanOrEqualTo(parameter1, parameter2) {
+  if(parameter1 <= parameter2){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 // Problem 3:
 // write a function named add(number1, number2)
 // add will add two numbers and return the result
 
+function add(number1, number2){
+  return number1 + number2;
+}
+
 // Problem 4:
 // write a function named addThree(number1, number2, number3)
 // this function will add three numbers
 // you must call your function add() in addThree()
 
+function addThree(number1, number2, number3){
+  return add(number1, number2) + number3;
+}
 
 // ****
 // Concept Checkpoint
@@ -54,22 +73,25 @@ function lessThanOrEqualTo(parameter1, parameter2) {
 // What is a function? How do you define a function in Javascript?
 //
 // Your Answer Goes Here:
-//
+//A function is a building block in JavaScript that peforms a task or calculates a value. You define a function by writing 
+//function functionName (parameters) {}
 //
 // What is a return value?
 //
 // Your Answer Goes Here:
-//
+// It is a statement that stops the execution of a function and returns a value.
 //
 // How do you define a named function?
 //
 // Your Answer Goes Here:
-//
+// You define a function by writing 
+//  function functionName (parameters) {}
+// Then you call it by its name, in this case functionName(parameters);, and input any applicable parameters.
 //
 // What is a parameter? What is an argument? Is there a difference between the two?
 //
 // Your Answer Goes Here:
-//
+//  Parameters are values that might be used to execute a function. They go inside the parenthesis of a function.
 // ****
 
 
@@ -84,6 +106,12 @@ function lessThanOrEqualTo(parameter1, parameter2) {
 // complete isEven() by returning a boolean expression
 
 function isEven(number) {
+  if(number % 2 === 0){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 // Problem 6:
@@ -93,6 +121,12 @@ function isEven(number) {
 // complete isDivisibleByThree() by returning a boolean expression
 
 function isDivisibleByThree(number) {
+  if(number % 3 === 0){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 // ****
@@ -108,6 +142,18 @@ function isDivisibleByThree(number) {
 // should return false if character is anything else
 
 function whichSpecies(character) {
+  if(character === 'scooby'){
+    return "dog";
+  }
+  if(character === 'garfield'){
+    return "cat";
+  }
+  if(character === 'nemo'){
+    return "fish";
+  }
+  else{
+    return false;
+  }
 }
 
 // Problem 8:
@@ -118,6 +164,20 @@ function whichSpecies(character) {
 // return the string "divisible by 3" when number % 3 === 0
 // return the string "divisible by 5" when number % 5 === 0
 
+function testNumber(number){
+  if(number % 4 === 0){
+    return "divisible by 4";
+  }
+  if(number % 2 === 0){
+    return "divisible by 2";
+  }
+  if(number % 3 === 0){
+    return "divisible by 3";
+  }
+  if(number % 5 === 0){
+    return "divisible by 5";
+  }
+}
 
 // ****
 // Concept Checkpoint
@@ -127,7 +187,8 @@ function whichSpecies(character) {
 // In your own words, explain what conditionals do.
 //
 // Your Answer Goes Here:
-//
+// Conditionals are usually use for comparison. They use boolean variables to evaluate expressions.
+// They either compare if one value is equal or not equal, greater than or less than another value.
 //
 // ****
 
