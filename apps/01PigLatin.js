c729a75e722d8fd9247fb3e18c4ed5808fc0a2c9
bttodd11 <bttodd@icloud.com>
2017-01-24 18:30:39 -0600

    New Changes

--- apps/01PigLatin.js
+++ apps/01PigLatin.js
@@ -15,7 +15,7 @@ function pigLatin(word) {
     console.log(firstLetter);
     word = word.toLowerCase();
 
-    var word = 'crazy';
+    // word = 'crazy';
     var vowelIndex = -1; // Set it to assume there are no vowels
 
     if ((word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex) || vowelIndex === -1) {
@@ -28,32 +28,33 @@ function pigLatin(word) {
 
     if ((word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex) || vowelIndex === -1) {
         vowelIndex = word.indexOf('i');
-      }
+    }
     if ((word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex) || vowelIndex === -1) {
-          vowelIndex = word.indexOf('o');
-      }
+        vowelIndex = word.indexOf('o');
+    }
 
-      if ((word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex) || vowelIndex === -1) {
-          vowelIndex = word.indexOf('u');
-      }
+    if ((word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex) || vowelIndex === -1) {
+        vowelIndex = word.indexOf('u');
+    }
 
-      if ((word.indexOf('y') > -1 && word.indexOf('y') < vowelIndex) || vowelIndex === -1) {
-          vowelIndex = word.indexOf('y');
-      }
+    if ((word.indexOf('y') > -1 && word.indexOf('y') < vowelIndex) || vowelIndex === -1) {
+        vowelIndex = word.indexOf('y');
+    }
 
-      var firstPart = word.slice(0, vowelIndex);
-      var restWord = word.slice(vowelIndex, word.length);
-      console.log('first part', firstPart);
-      console.log('rest word', restWord);
+    var firstPart = word.slice(0, vowelIndex);
+    var restWord = word.slice(vowelIndex, word.length);
+    console.log('first part', firstPart);
+    console.log('rest word', restWord);
 
-      if (vowelIndex === 0) {
-          return word + "yay";
-      }
-      else {
-          return restWord.toLowerCase()
-      }
+    if (vowelIndex === 0) {
+      var newBeginsWithVowel = restWord + firstPart + 'yay';
+        return newBeginsWithVowel.toLowerCase();
+    } else {
+      var newWord = restWord + firstPart + 'ay';
+      return newWord.toLowerCase();
+    }
 
-    return restWord + firstPart + 'ay';
+
 }
 
 
