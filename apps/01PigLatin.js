4976c73b1b5684a373efd877ffa4391331222926
RX7RUSS <russ.savage5@gmail.com>
2017-01-25 17:17:03 -0600

    Fix Input Scrubbing

--- apps/01PigLatin.js
+++ apps/01PigLatin.js
@@ -11,45 +11,47 @@ var rl = readline.createInterface({
 
 function pigLatin(word) {
 
+  word = word.toLowerCase();
   var firstLetter = word[0];
 
   var vowelIndex = -1; // Set it to assume there are no vowels
 
-    if ( ( word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
-      vowelIndex = word.indexOf('a');
-    }
+  if ( ( word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('a');
+  }
 
-    if ( ( word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex ) || vowelIndex === -1 ) {
-      vowelIndex = word.indexOf('e');
-    }
+  if ( ( word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('e');
+  }
 
-    if ( ( word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex ) || vowelIndex === -1 ) {
-      vowelIndex = word.indexOf('i');
-    }
+  if ( ( word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('i');
+  }
 
-    if ( ( word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex ) || vowelIndex === -1 ) {
-      vowelIndex = word.indexOf('o');
-    }
+  if ( ( word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('o');
+  }
 
-    if ( ( word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex ) || vowelIndex === -1 ) {
-      vowelIndex = word.indexOf('u');
-    }
+  if ( ( word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('u');
+  }
 
-    if ( ( word.indexOf('y') > -1 && word.indexOf('y') < vowelIndex ) || vowelIndex === -1 ) {
-      vowelIndex = word.indexOf('y');
-    }
+  if ( ( word.indexOf('y') > -1 && word.indexOf('y') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('y');
+  }
 
-    word = word.toLowerCase();
-    var firstPart = word.slice(0, vowelIndex);
-    var restWord  = word.slice(vowelIndex, word.length)
+  
+  
+  var firstPart = word.slice(0, vowelIndex);
+  var restWord  = word.slice(vowelIndex, word.length)
 
-    if(vowelIndex > 0) {
-      return restWord + firstPart + 'ay';
-    } else if (vowelIndex === 0) {
-      return word + 'yay';
-    } else {
-      return 'Invalid User Input';
-    }
+  if(vowelIndex > 0) {
+    return restWord + firstPart + 'ay';
+  } else if (vowelIndex === 0) {
+    return word + 'yay';
+  } else {
+    return 'Invalid User Input';
+  }
 
 };
 
