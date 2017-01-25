c35a1bea4e74ced85bf1875e5a9314d43e50fc07
Morgakd <morgakd@sbcglobal.net>
2017-01-24 20:26:35 -0600

    Lesson One Test

--- test/01LessonOneTest.js
+++ test/01LessonOneTest.js
@@ -5,111 +5,107 @@ var assert = require('assert');
 
 function returnTrue() {
   // should return true
-return(4===4)
+  return (4 === 4)
 }
 
 function returnFalse() {
   // should return false
-return(4===5)
+  return (4 === 5)
 }
 
 function greaterThan(big, small) {
   // should return true if the first argument is greater than the second argument
   // and return false if the first argument is less than the second argument
-if (big > small){
-  return true;
-}
-else{
-  return false;
-}
+  if (big > small) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function lessThan(small, big) {
   // should return true if the first argument is less than the second argument
   // and return false if the first argument is greater than the second argument
-if(small < big){
-  return true;
-}
-else{
-  return false;
-}
+  if (small < big) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function equalTo(a, b) {
   // should return true if the first argument is equal to the second argument
   // and should return true if the first argument is equal to the second argument
-if (a === b){
-  return true;
-}
-else {
-  return false;
-}
+  if (a === b) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function notEqualTo(a, b) {
   // should return true if the first argument is not equal to the second argument
   // and should return false if the first argument is equal to the second argument
-if (a !== b){
-  return true;
-}
-else{
-  return false;
-}
+  if (a !== b) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 
 // Tests
 
-describe('#returnTrue()', function () {
-  it('should return true', function () {
+describe('#returnTrue()', function() {
+  it('should return true', function() {
     assert.equal(returnTrue(), true);
   });
 });
 
-describe('#returnFalse()', function () {
-  it('should return false', function () {
+describe('#returnFalse()', function() {
+  it('should return false', function() {
     assert.equal(returnFalse(), false);
   });
 });
 
-describe('#greaterThan()', function () {
-  it('should return true if the first argument is greater than the second argument', function () {
+describe('#greaterThan()', function() {
+  it('should return true if the first argument is greater than the second argument', function() {
     assert.equal(greaterThan(10, 4), true);
   });
-  it('should return false if the first argument is less than the second argument', function () {
+  it('should return false if the first argument is less than the second argument', function() {
     assert.equal(greaterThan(4, 10), false);
   });
 });
 
-describe('#lessThan()', function () {
-  it('should return true if the first argument is less than the second argument', function () {
+describe('#lessThan()', function() {
+  it('should return true if the first argument is less than the second argument', function() {
     assert.equal(lessThan(3, 5), true);
   });
-  it('should return false if the first argument is greater than the second argument', function () {
+  it('should return false if the first argument is greater than the second argument', function() {
     assert.equal(lessThan(6, 3), false);
   });
 });
 
-describe('#equalTo()', function () {
-  it('should return true if the first argument is equal to the second argument', function () {
+describe('#equalTo()', function() {
+  it('should return true if the first argument is equal to the second argument', function() {
     assert.equal(equalTo(6, 6), true);
   });
-  it('should return false if the first argument is not equal to the second argument', function () {
+  it('should return false if the first argument is not equal to the second argument', function() {
     assert.equal(equalTo(6, 7), false);
   });
-  it('should return false if not using ===', function () {
+  it('should return false if not using ===', function() {
     assert.equal(equalTo(6, '6'), false);
   });
 });
 
-describe('#notEqualTo()', function () {
-  it('should return false if the first argument is equal to the second argument', function () {
+describe('#notEqualTo()', function() {
+  it('should return false if the first argument is equal to the second argument', function() {
     assert.equal(notEqualTo(6, 6), false);
   });
-  it('should return true if the first argument is not equal to the second argument', function () {
+  it('should return true if the first argument is not equal to the second argument', function() {
     assert.equal(notEqualTo(6, 7), true);
   });
-  it('should return true if not using ===', function () {
+  it('should return true if not using ===', function() {
     assert.equal(notEqualTo(6, '6'), true);
   });
 });
