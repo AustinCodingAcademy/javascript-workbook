7b6f205af2726a87f757206e4b38c282ad279786
Tony <arinaldi@vt.edu>
2017-01-26 19:29:57 -0600

    Entered code for the rockPaperScissors function and added tests

--- apps/02RockPaperScissors.js
+++ apps/02RockPaperScissors.js
@@ -7,18 +7,54 @@ var rl = readline.createInterface({
   output: process.stdout
 });
 
-
 function rockPaperScissors(hand1, hand2) {
+  hand1 = hand1.toLowerCase();
+  hand2 = hand2.toLowerCase();
 
-  // Write code here
+  var allowedWords = ['rock', 'paper', 'scissors'];
+  if (allowedWords.indexOf(hand1) < 0 || allowedWords.indexOf(hand2) < 0) {
+    return 'Invalid input';
+  }
 
+  if (hand1 === hand2) {
+    return "It's a tie!";
+  }
+
+  if (hand1 === 'rock') {
+    if (hand2 === 'scissors') {
+      return 'Hand one wins!';
+    }
+    return 'Hand two wins!';
+  }
+
+  if (hand1 === 'paper') {
+    if (hand2 === 'rock') {
+      return 'Hand one wins!';
+    }
+    return 'Hand two wins!';
+  }
+
+  if (hand1 === 'scissors') {
+    if (hand2 === 'paper') {
+      return 'Hand one wins!';
+    }
+    return 'Hand two wins!';
+  }
 }
 
 function getPrompt() {
   rl.question('hand1: ', (answer1) => {
     rl.question('hand2: ', (answer2) => {
-      console.log( rockPaperScissors(answer1, answer2) );
-      getPrompt();
+      //console.log( rockPaperScissors(answer1, answer2) );
+      var output = rockPaperScissors(answer1, answer2);
+      if (output === "It's a tie!" || output === "Invalid input") {
+        console.log(output);
+        getPrompt();
+      } else {
+        console.log(output);
+        process.exit();
+      }
+      //getPrompt();
     });
   });
 }
@@ -37,6 +73,16 @@ if (typeof describe === 'function') {
       assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
       assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
       assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
+    });
+    it('should scrub input', function () {
+      assert.equal(rockPaperScissors('RocK', 'rock'), "It's a tie!");
+      assert.equal(rockPaperScissors('scissors', 'PAPER'), "Hand one wins!");
+      assert.equal(rockPaperScissors('RoCk', 'papER'), "Hand two wins!");
+    });
+    it('should validate input', function () {
+      assert.equal(rockPaperScissors('dog', 'cat'), "Invalid input");
+      assert.equal(rockPaperScissors('paper', 'mouse'), "Invalid input");
+      assert.equal(rockPaperScissors('horse', 'scissors'), "Invalid input");
     });
   });
 } else {
