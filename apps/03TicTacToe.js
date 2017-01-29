ce6a7c9035ce37e21c28a5ab7deb9fffbf9df677
Jonathan Gucciardi <jonmgucciardi@Jonathans-MacBook-Pro.local>
2017-01-29 13:00:26 -0600

    Tic Tac Toe PassinG NPM TEST

--- apps/03TicTacToe.js
+++ apps/03TicTacToe.js
@@ -24,46 +24,44 @@ function printBoard() {
   console.log('  ---------');
   console.log('2 ' + board[2].join(' | '));
 }
-
+/* Test for horizontal win by parsing through each cell in the board[][] array, looking for playerTurn value
+   within that array cells, if the value returns true on three horizontal cells in the same row, exit the 
+   function with return true; The value of horizontallWin() is now the boolean value of true*/
 function horizontalWin() {
   // Your code here
+  
    (board[0][0] === playerTurn && board[0][1] === playerTurn && board[0][2] === playerTurn) ||
    (board[1][0] === playerTurn && board[1][1] === playerTurn && board[1][2] === playerTurn) || 
    (board[2][0] === playerTurn && board[2][1] === playerTurn && board[2][2] === playerTurn)
      
-   
-     
-   return true;
-   }
+ return true;
+}
 
 
-
+/*Do the same test as horizontalWin() here, but this time, change the cells you search to that 
+of the columns, exit the function by returning true when the value of playerTurn exists in the 
+ three matching vertical cells*/
 function verticalWin() {
   // Your code here
    (board[0][0] === playerTurn && board[1][0] === playerTurn && board[2][0] === playerTurn) ||
    (board[0][1] === playerTurn && board[1][1] === playerTurn && board[2][1] === playerTurn) ||
    (board[0][2] === playerTurn && board[1][2] === playerTurn && board[2][2] === playerTurn)
-
    
-        
-   return true;
-    
+ return true;
 }
 
-
+/* Do the same as the first two tests, only change will be the cells we are searching. I searched
+   from top left to bottom right diagonal, and bottom left , top right diagonal. Once again return
+  true to exit the function */
 function diagonalWin() {
   // Your code here
    (board[0][0] === playerTurn && board[1][1] === playerTurn && board[2][2] === playerTurn) ||
    (board[0][2] === playerTurn && board[1][1] === playerTurn && board[2][0] === playerTurn)
 
-   
-
-   return true;
-
-
-
+ return true;
 }
-
+/* Test to see if any of the winning functions above are true, In the event they are, out to the console
+   the player which has won. to exit this function, return the boolean value of true */
 function checkForWin() {
   // Your code here
   if(horizontalWin() === true || verticalWin() === true || diagonalWin() === true){
@@ -73,14 +71,16 @@ function checkForWin() {
 }
 
 
-
+/*This is where the game will be played. playerTurn is default as 'X', assign it to the board and
+  use the check for win function to test if the game has a winner, otherwise, change playerTurn to
+  'O' and continue to play Tic Tac Toe*/
 function ticTacToe(row, column) {
   // Your code here
-    board[row][column] = playerTurn;
+    board[row][column] = playerTurn; // add player to the board
     
-    checkForWin();
+    checkForWin(); //test for a winning combination
     
-    playerTurn = (playerTurn === 'X') ? 'O' : 'X';
+    playerTurn = (playerTurn === 'X') ? 'O' : 'X'; //switch between players
     
 }
 
