405bd2a4d1eb4480959299441be5ead2d0758dc8
cruzmndz <cruzalexmendez@gmail.com>
2017-01-29 21:58:42 -0600

    PigLatin
    all tests passing. need to redo code for test #2.

--- apps/01PigLatin.js
+++ apps/01PigLatin.js
@@ -9,14 +9,55 @@ var rl = readline.createInterface({
 
 
 function pigLatin(word) {
+  // For test 4. caps scrub
+  var word = word.toLowerCase();
+  // Set it to assume there are no vowels  
+  var vowelIndex = -1; 
+  var firstLetter = word[0];
 
-  // Your code here
+// Test to find vowel index.
+  if ( ( word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('a');
+  }
 
+  if ( ( word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('e');
+  }
+
+  if ( ( word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('i');
+  }
+
+  if ( ( word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('o');
+  }
+
+  if ( ( word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('u');
+  }
+
+  if ( ( word.indexOf('y') > -1 && word.indexOf('y') < vowelIndex ) || vowelIndex === -1 ) {
+    vowelIndex = word.indexOf('y');
+  } 
+
+  if ( word.indexOf('e') === 0) {  // <--- for test 3. vowel first
+    return word + 'yay';
+  }
+
+// For test 2. works but is inefficient.
+  if ( (word.indexOf('e') > 1 ) && (word.indexOf('e') < (word.length/2) ) ) {  
+    vowelIndex = word.indexOf('e');
+    var firstPart = word.slice(0, vowelIndex);
+    var restWord = word.slice(vowelIndex, word.length);
+    return restWord + firstPart + 'ay';
+  }
+// Print simple translated word.
+  return word.replace(firstLetter, '') + firstLetter + 'ay';
 }
 
 
 function getPrompt() {
-  rl.question('word ', (answer) => {
+  rl.question('Type a word to be changed to Pig Latin: ', (answer) => {
     console.log( pigLatin(answer) );
     getPrompt();
   });
