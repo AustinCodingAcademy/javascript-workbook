964314913e7dda506ea83f214c3ad4a8e8b657b6
samlynch96 <sam@bigbabyagency.com>
2017-01-31 18:18:49 -0600

    Rock Paper Scissors

--- apps/02RockPaperScissors.js
+++ apps/02RockPaperScissors.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 var assert = require('assert');
 var readline = require('readline');
@@ -11,6 +11,42 @@ var rl = readline.createInterface({
 function rockPaperScissors(hand1, hand2) {
 
   // Write code here
+
+  if (hand1 === hand2) {
+    return "It's a tie!";
+}
+
+if (hand1 === 'rock') {
+  if (hand2 === 'scissors') {
+    return 'Hand one wins!';
+  }
+  // If we reach here, player 2 must have dealt paper
+  if (hand2 === 'paper')
+    return 'Hand two wins!';
+}
+
+
+if (hand1 === 'paper') {
+  if (hand2 === 'rock') {
+    return 'Hand one wins!';
+  }
+
+  if (hand2 === 'scissors')
+    return 'Hand two wins!';
+}
+  // fill this in using the logic above
+
+
+
+if (hand1 === 'scissors') {
+  if (hand2 === 'paper'){
+    return 'Hand one wins!';
+  }
+  // fill this in using the logic above
+  if (hand2 === 'rock')
+    return 'Hand two wins!';
+
+}
 
 }
 
