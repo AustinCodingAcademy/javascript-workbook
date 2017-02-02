217ca992ab2ebb966edfed217019dffa48a4168e
James Puckett <jamesjaypuckett@gmail.com>
2017-02-02 13:01:23 -0600

    TTT with smarter AI, made it take center if available, and told it how to win. need to add blocks and corners, and smart moves

--- apps/TTTwithSmartAI.js
+++ apps/TTTwithSmartAI.js
@@ -97,35 +97,87 @@ function changePlayer() {
   }
 }
 
-function randomAiMove(playerTurn, row, column) {
+function AImove(playerTurn, row, column) {
   if (playerTurn === 'O') {
-    row = Math.floor(Math.random() * 3);
-    column = Math.floor(Math.random() * 3);
-    ticTacToe(row, column);
+    if(board[1][1] === ' '){
+      ticTacToe(1, 1);
+      //take win horizontal
+    }else if (board[0][0] === 'O' && board[0][1] === 'O' && board[0][2] === ' '){
+      ticTacToe(0, 2);
+    }else if (board[0][0] === 'O' && board[0][1] === ' ' && board[0][2] === 'O'){
+      ticTacToe(0, 1);
+    }else if (board[0][0] === ' ' && board[0][1] === 'O' && board[0][2] === 'O'){
+      ticTacToe(0, 0);
+    }else if (board[1][0] === 'O' && board[1][1] === 'O' && board[1][2] === ' '){
+      ticTacToe(1, 2);
+    }else if (board[1][0] === 'O' && board[1][1] === ' ' && board[1][2] === 'O'){
+      ticTacToe(1, 1);
+    }else if (board[1][0] === ' ' && board[1][1] === 'O' && board[1][2] === 'O'){
+      ticTacToe(1, 0);
+    }else if (board[2][0] === 'O' && board[2][1] === 'O' && board[2][2] === ' '){
+      ticTacToe(2, 2);
+    }else if (board[2][0] === 'O' && board[2][1] === ' ' && board[2][2] === 'O'){
+      ticTacToe(2, 1);
+    }else if (board[2][0] === ' ' && board[2][1] === 'O' && board[2][2] === 'O'){
+      ticTacToe(2, 0);
+    }
+    //take win vertical
+    }else if (board[0][0] === 'O' && board[1][0] === 'O' && board[2][0] === ' '){
+      ticTacToe(2, 0);
+    }else if (board[0][0] === 'O' && board[1][0] === ' ' && board[2][0] === 'O'){
+      ticTacToe(1, 0);
+    }else if (board[0][0] === ' ' && board[1][0] === 'O' && board[2][0] === 'O'){
+      ticTacToe(0, 0);
+    }else if (board[0][1] === 'O' && board[1][1] === 'O' && board[2][1] === ' '){
+      ticTacToe(2, 1);
+    }else if (board[0][1] === 'O' && board[1][1] === ' ' && board[2][1] === 'O'){
+      ticTacToe(1, 1);
+    }else if (board[0][1] === ' ' && board[1][1] === 'O' && board[2][1] === 'O'){
+      ticTacToe(0, 1);
+    }else if (board[0][2] === 'O' && board[1][2] === 'O' && board[2][2] === ' '){
+      ticTacToe(2, 2);
+    }else if (board[0][2] === 'O' && board[1][2] === ' ' && board[2][2] === 'O'){
+      ticTacToe(1, 2);
+    }else if (board[0][2] === ' ' && board[1][2] === 'O' && board[2][2] === 'O'){
+      ticTacToe(0, 2);
+    }
+    //take win diaginol
+    else if (board[0][0] === 'O' && board[1][1] === 'O' && board[2][2] === ' '){
+      ticTacToe(2, 2);
+    }else if (board[0][0] === 'O' && board[1][1] === ' ' && board[2][2] === 'O'){
+      ticTacToe(1, 1);
+    }else if (board[0][0] === ' ' && board[1][1] === 'O' && board[2][2] === 'O'){
+      ticTacToe(0, 0);
+    }else if (board[0][0] === 'O' && board[1][0] === 'O' && board[2][0] === ' '){
+      ticTacToe(2, 0);
+    }else if (board[0][0] === 'O' && board[1][0] === ' ' && board[2][0] === 'O'){
+      ticTacToe(1, 0);
+    }else if (board[0][0] === ' ' && board[1][0] === 'O' && board[2][0] === 'O'){
+      ticTacToe(0, 0);
+    }
   }
+  // ((board[0][0] === playerTurn && board[0][1] === playerTurn && board[0][2] === playerTurn) || (board[1][0] === playerTurn && board[1][1] === playerTurn && board[1][2] === playerTurn) || (board[2][0] === playerTurn && board[2][1] === playerTurn && board[2][2] === playerTurn))
 }
 
 function ticTacToe(row, column) {
+  checkForWin();
   if (!(row == 0 || row == 1 || row == 2) || !(column == 0 || column == 1 || column == 2)) {
-    console.log('Invalid entry, please try again');
+    console.log('Invalid entry, please try again')
     return;
   }
   if (board[row][column] !== ' ') {
     console.log('That spot appears taken, please try again');
-    //the following two functions make the AI make a random move until it finds a spot that isnt taken
-    checkForWin();
-    randomAiMove(playerTurn, row, column);
-    getPrompt();
     return;
   } else {
+    checkForWin();
     board[row][column] = playerTurn;
+    checkForWin();
     changePlayer();
-    //calls AI functin to make a random move
-    randomAiMove(playerTurn, row, column);
+    AImove(playerTurn, row, column);
     checkForWin();
   }
-  checkForWin();
-  changePlayer();
+  // checkForWin();
+  // changePlayer();
 }
 
 function getPrompt() {
