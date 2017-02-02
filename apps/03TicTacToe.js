5b2ac7ac9aa5e6c7a3b90438cbc84225313887af
HippErger <berger.clayton@gmail.com>
2017-02-01 20:30:03 -0600

    TicTacToe up and reset

--- apps/03TicTacToe.js
+++ apps/03TicTacToe.js
@@ -12,7 +12,15 @@ var board = [
   [' ', ' ', ' ']
 ];
 
-var playerTurn = 'X';
+var playerTurn = "X";
+
+
+function toggleTurn() {
+  // changes the player's mark after each move. 
+  playerTurn = (playerTurn === 'X') ? 'O' : 'X';
+}
+
+
 
 function printBoard() {
   console.log('   0  1  2');
@@ -22,25 +30,101 @@ function printBoard() {
   console.log('  ---------');
   console.log('2 ' + board[2].join(' | '));
 }
+function checkForWin() {
+  // All Win functions are inside this function.  
+// if    (board[0][0] === playerTurn && board[0][1] === playerTurn && board[0][2] === playerTurn ||
+//       board[1][0] === playerTurn && board[1][1] === playerTurn && board[1][2] === playerTurn ||
+//       board[2][0] === playerTurn && board[2][1] === playerTurn && board[2][2] === playerTurn ||
+//       board[0][0] === playerTurn && board[1][0] === playerTurn && board[2][0] === playerTurn ||
+//       board[0][1] === playerTurn && board[1][1] === playerTurn && board[2][1] === playerTurn ||
+//       board[0][2] === playerTurn && board[1][2] === playerTurn && board[2][2] === playerTurn ||
+//       board[0][0] === playerTurn && board[1][0] === playerTurn && board[2][0] === playerTurn ||
+//       board[0][1] === playerTurn && board[1][1] === playerTurn && board[2][1] === playerTurn ||
+//       board[0][2] === playerTurn && board[1][2] === playerTurn && board[2][2] === playerTurn) {
+//         return true;
+//   }
+//   else { 
+//     return false;
+//   }
+if (horizontalWin() || verticalWin() || diagonalWin()) {
+  return true;
+}
+}
+      
+
+
 
 function horizontalWin() {
-  // Your code here
-}
+  // This chunk checks to see if there is a win on any of the rows. 
+  // Since this statement has a true/false if/else statement it doesn't need to be restated again. 
+  if (board[0][0] === playerTurn && board[0][1] === playerTurn && board[0][2] === playerTurn ||
+      board[1][0] === playerTurn && board[1][1] === playerTurn && board[1][2] === playerTurn ||
+      board[2][0] === playerTurn && board[2][1] === playerTurn && board[2][2] === playerTurn) {
+        return true;
+  }
+  else { 
+    return false;
+  }
+  }
 
+// This checks to see if the there is a win in the columns
 function verticalWin() {
-  // Your code here
+    if (board[0][0] === playerTurn && board[1][0] === playerTurn && board[2][0] === playerTurn ||
+        board[0][1] === playerTurn && board[1][1] === playerTurn && board[2][1] === playerTurn ||
+        board[0][2] === playerTurn && board[1][2] === playerTurn && board[2][2] === playerTurn) {
+          return true;
+  }
+  else { 
+    return false;
+  }
+
 }
 
 function diagonalWin() {
-  // Your code here
+      if (board[0][0] === playerTurn && board[1][1] === playerTurn && board[2][2] === playerTurn ||
+          board[0][2] === playerTurn && board[1][1] === playerTurn && board[2][0] === playerTurn)  {
+            return true;
+  }
+  else { 
+    return false;
+  }
+
 }
 
-function checkForWin() {
-  // Your code here
-}
+
+
 
 function ticTacToe(row, column) {
-  // Your code here
+  // Calls takes input from user and places it into row and column in the original board. 
+  board[row][column] = playerTurn;
+  // the next lines check for wins in the rows, columns and diagonals. 
+  if (checkForWin() === true) {
+    console.log("################################");
+    console.log("################################");
+    console.log("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+    console.log( "Player" + " " + playerTurn + " " + "wins!");
+    console.log("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+    printBoard();
+    console.log("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+    console.log("################################");
+    console.log("################################");
+    
+    board = [
+      [' ', ' ', ' '],
+      [' ', ' ', ' '],
+      [' ', ' ', ' ']
+      ];
+
+    // getPrompt();
+  }
+  else {
+    console.log("Where to next?");
+    toggleTurn();
+  }
+
+  
+
+
 }
 
 function getPrompt() {
