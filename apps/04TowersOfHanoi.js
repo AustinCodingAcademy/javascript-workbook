8116441f20ab40c51a0cc09e28206d69fad891b6
aleexga <aleexga@gmail.com>
2017-02-04 18:24:00 -0600

    Towers of Hanoi finished

--- apps/04TowersOfHanoi.js
+++ apps/04TowersOfHanoi.js
@@ -13,43 +13,80 @@ var stacks = {
   c: []
 };
 
+
 function printStacks() {
   console.log("a: " + stacks.a);
   console.log("b: " + stacks.b);
   console.log("c: " + stacks.c);
 }
 
-function movePiece() {
-  // Your code here
-
+function movePiece(startStack, endStack) {
+  // Pop and push a piece!
+  var piece = stacks[startStack].pop();
+  stacks[endStack].push(piece);
 }
 
-function isLegal() {
-  // Your code here
-
+function isLegal(startStack, endStack) {
+  // Check if endStack has something in it.
+  if (stacks[endStack].length < 1) {
+    return true;
+  }
+  // Check if the last number in endStack is larger than the piece you are moving from startStack. 
+  // Or if this is a legal move.
+  else if ((stacks[startStack][(stacks[startStack].length-1)]) < (stacks[endStack][(stacks[endStack].length-1)])) {
+    return true;
+  }
+  // If not, this is an illegal move.
+  else {
+    console.log("Illegal move! Try again!");
+    return false;
+  }
 }
 
 function checkForWin() {
-  // Your code here
-
+  // Check if the b stack or c stack has a .length of 4, if yes, user wins!
+  if (stacks.c.length > 3){
+    console.log("You Won!!");
+    return true;
+  }
+  else if (stacks.b.length > 3){
+    console.log("You Won!!");
+    return true;
+  }
+  else{
+    console.log("Keep Going!");
+    return false;
+  }
 }
 
 function towersOfHanoi(startStack, endStack) {
-  // Your code here
-
+  // If isLegal returns true, move a piece!
+  if (isLegal(startStack, endStack)){
+    movePiece(startStack,endStack);
+  }
+  // And also check for win every time.
+  checkForWin();
 }
 
 function getPrompt() {
   printStacks();
   rl.question('start stack: ', (startStack) => {
     rl.question('end stack: ', (endStack) => {
-      towersOfHanoi(startStack, endStack);
+      // Check if startStack and EndStack are equal to 'a', 'b', or 'c'.
+      if ((startStack === 'a' || startStack === 'b' || startStack === 'c') && 
+      (endStack === 'a' || endStack === 'b' || endStack === 'c')) {
+        towersOfHanoi(startStack, endStack);
+      }
+      // If not equal, try again!
+      else {
+        console.log ('Not an input silly! Pick a, b, or c!');
+      }
       getPrompt();
     });
   });
 }
 
-// Tests
+// Tests ------------------------------------
 
 if (typeof describe === 'function') {
 
