61c4abc52fca509da1a0ae9a7105a459bf5681c6
foxjo <jordan.wharton@yahoo.com>
2017-02-07 17:44:44 -0600

    Towers of Hanoi first commit

--- apps/04TowersOfHanoi.js
+++ apps/04TowersOfHanoi.js
@@ -19,31 +19,59 @@ function printStacks() {
   console.log("c: " + stacks.c);
 }
 
-function movePiece() {
+function movePiece(startStack, endStack) {
   // Your code here
-
+var poppedItem = stacks[startStack].pop();
+stacks[endStack].push(poppedItem);
 }
 
-function isLegal() {
+function isLegal(startStackKey,endStackKey) {
   // Your code here
 
+  var startStack = stacks[startStack];
+  var endStack = stacks[endStack];
+  var lastElementOfStartStack = startStack[startStack.length -1];
+  var lastElementOfEndStack = endStack[endStack.length -1];
+
+  if (startStack.length === 0){
+    return false;
+  }
+
+  if (endStack.length === 0) {
+    return true;
+  }
+
+if (lastElementOfStartStack < lastElementOfStartStack){
+  return false;
+}
 }
 
 function checkForWin() {
   // Your code here
-
+if(b.length === 4 || c.length ===4) {
+  console.log('You Won!');
+  return true
+}else {
+  return false
+}
 }
 
 function towersOfHanoi(startStack, endStack) {
   // Your code here
-
+if (isLegal(startStackKey,endStackKey)){
+    movePiece(startStack,endStack);
+    checkForWin();
 }
+else {
+  console.log ('Move Now Allowed.')
+}
+  }
 
 function getPrompt() {
   printStacks();
   rl.question('start stack: ', (startStack) => {
     rl.question('end stack: ', (endStack) => {
-      towersOfHanoi(startStack, endStack);
+      towersOfHanoi();
       getPrompt();
     });
   });
