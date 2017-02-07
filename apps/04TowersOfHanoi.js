37dcf887b86cf8eea8d7a140196f73b4bf445006
nevetsretlaw <nevetsretlaw@Stevens-MacBook-Pro-2.local>
2017-02-06 22:02:32 -0600

    comments on TOH

--- apps/04TowersOfHanoi.js
+++ apps/04TowersOfHanoi.js
@@ -13,24 +13,34 @@ var stacks = {
   c: []
 };
 
+// Prints the starting board
 function printStacks() {
   console.log("a: " + stacks.a);
   console.log("b: " + stacks.b);
   console.log("c: " + stacks.c);
 }
 
+// Accepts two inputs: first is the stack where the piece starts and the second is the stack where the piece will end up
 function movePiece(startStack,endStack) {  
+
+// Assigns the variable "poppedPiece" to the value of the piece that has been popped from Stack A  
   var poppedPiece = stacks[startStack].pop();
+
+// Adds the popped piece to the top of the end stack
   stacks[endStack].push(poppedPiece);
 }
 
+// Checks to see if the move is legal or illegal
 function isLegal(startStack, endStack) {
+
+// If the end stack's length is zero or the start stack is less than the end stack than the move is legal 
   if ((stacks[endStack].length === 0 ) || (stacks[startStack] < stacks[endStack])) {
     return true;
   }
   return false;
 }
 
+// Checks to see if all of the pieces are on one stack constituting a win
 function checkForWin() {
   if ((stacks['b'].length === 4) || (stacks['c'].length === 4)) {
     console.log('You Won!');
@@ -50,7 +60,7 @@ function getPrompt() {
   rl.question('start stack: ', (startStack) => {
     rl.question('end stack: ', (endStack) => {
       towersOfHanoi(startStack, endStack);
-      getPrompt();
+      getPrompt ();
     });
   });
 }
