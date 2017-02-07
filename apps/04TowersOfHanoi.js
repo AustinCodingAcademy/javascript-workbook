0bff86b9d8f5fe2dd8e780ff3e5cc3c4753d3081
rbmcintosh <ryanbmcintosh@gmail.com>
2017-02-07 17:45:18 -0600

    Towers with comments

--- apps/04TowersOfHanoi.js
+++ apps/04TowersOfHanoi.js
@@ -7,22 +7,30 @@ var rl = readline.createInterface({
   output: process.stdout
 });
 
+// The object below is three different stacks (arrays) set up how the game starts with all of the pieces on the first stack. 
+
 var stacks = {
   a: [4, 3, 2, 1],
   b: [],
   c: []
 };
 
+// The function below prints all three stacks in their current state. 
+
 function printStacks() {
   console.log("a: " + stacks.a);
   console.log("b: " + stacks.b);
   console.log("c: " + stacks.c);
-}
+};
+
+// The function below moves a piece from one stack to another.
 
 function movePiece(startStack, endStack) {
   var piece = stacks[startStack].pop();
   stacks[endStack].push(piece);
-}
+};
+
+// The function below is an if/else statement checking to see if the user's move is legal. Two different variables were created to store values. 
 
 function isLegal(startStack, endStack) {
   var startArray = stacks[startStack];
@@ -32,40 +40,52 @@ function isLegal(startStack, endStack) {
   } else if (endArray.length === 0) {
     return true;
   } else {
-    return startArray[startArray.length - 1] 
+    return startArray[startArray.length - 1]
     < endArray[endArray.length - 1];
-  }
+  };
 };
+
+// This function is checking to see if there is a win, meaning all four pieces have been moved to another tower.  
+// If not, the function will return false.
 
 function checkForWin() {
   if ((stacks.b.length === 4) || (stacks.c.length === 4)) {
-    console.log("You Won!");
+    console.log("Congratulations! You Won!!!");
     return true;
   } else {
     return false;
-  }
+  };
 };
+
+// This function is an if/else statement first checking to see if the user's move is legal. If it is, the piece is moved to the desired location. 
+// If not, the app prints that it was invalid and the user is prompted to try again. 
+// After a successful legal move, this function will check for a win. If there is no win, the game proceeds.
 
 function towersOfHanoi(startStack, endStack) {
   if (isLegal(startStack, endStack)) {
     movePiece(startStack, endStack);
   } else {
-    console.log('Invalid move please try again');
+    console.log('Invalid move. Please try again.');
   }
   if (checkForWin()) {
     process.exit();
-  }
+  } else {
+    return false;
+  };
 };
+
+// The function below is the start of every move. It prints the stacks in their current state and prompts the user to pick a piece to move and where to move it.
+// After this, it runs the towers function above and then starts over if it wasn't a winning move.
 
 function getPrompt() {
   printStacks();
-  rl.question('start stack: ', (startStack) => {
-    rl.question('end stack: ', (endStack) => {
+  rl.question('Pick a stack to pull a piece from: ', (startStack) => {
+    rl.question('Pick a stack to move your piece to: ', (endStack) => {
       towersOfHanoi(startStack, endStack);
       getPrompt();
     });
   });
-}
+};
 
 // Tests
 
@@ -105,7 +125,5 @@ if (typeof describe === 'function') {
     });
   });
 } else {
-
   getPrompt();
-
-}
+};
