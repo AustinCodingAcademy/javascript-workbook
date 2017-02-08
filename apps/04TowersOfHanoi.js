4d56548fd6b8c858f123d15355453b2f4223a5dc
HippErger <berger.clayton@gmail.com>
2017-02-07 21:57:20 -0600

    npm test throws ERRORgit add . extra ) but can't find it in my code

--- apps/04TowersOfHanoi.js
+++ apps/04TowersOfHanoi.js
@@ -21,83 +21,83 @@ function printStacks() {
 
 function movePiece(startStack, endStack) {
 //this just moves the pieces around from one stack to the other. 
-var garbonzobean = stacks[startStack].pop();
+  var garbonzobean = stacks[startStack].pop();
 
-stacks[endStack].push(garbonzobean);
+  stacks[endStack].push(garbonzobean);
 }
 
 function isLegal(startStack, endStack) {
   // Check to see if the input is a valid stack : a, b or c
-if (( startStack === 'a' || startStack === 'b' || startStack === 'c') && (endStack === 'a' || endStack === 'b' || endStack === 'c')) 
-{
-  // These variables are the length of the stacks being pulled from and the one being stacked on.
-  var lengthOfStartStack = stacks[startStack].length;
-  var lengthOfEndStack = stacks[endStack].length;
+  if (( startStack === 'a' || startStack === 'b' || startStack === 'c') && (endStack === 'a' || endStack === 'b' || endStack === 'c')) 
+  {
+    // These variables are the length of the stacks being pulled from and the one being stacked on.
+    var lengthOfStartStack = stacks[startStack].length;
+    var lengthOfEndStack = stacks[endStack].length;
 
-// Check to see if the stack being pulled from, startStack, has tokens/donuts to pull from. If so, continue.
-  if (lengthOfStartStack > 0) {
-    // this var donut is the value of the token being taken off of the stack. 
-    var donut = stacks[startStack][(lengthOfStartStack - 1)];
-    // this if checks to see whether the length of the stacked-on is < 0 OR|| if the value of the donut is less than 
-    // the value of the last number in the array lengthOfEndStack-1 of endStack of stacks. 
-    // we subtract 1 from the lengthOfEndStack because the index begins with 0 while the length of an array starts with 1
-    // So we have to subract one from the length else we would call on a numbe that isn't in the array, giving us "undefined"
-    if ((lengthOfEndStack === 0) || (donut < stacks[endStack][(lengthOfEndStack - 1)])){
-     return true; 
-    }
-    else {
-      console.log("Try again. Be sure the token moved is smaller.");
-      return false;
-    }
+    // Check to see if the stack being pulled from, startStack, has tokens/donuts to pull from. If so, continue.
+    if (lengthOfStartStack > 0) {
+      // this var donut is the value of the token being taken off of the stack. 
+      var donut = stacks[startStack][(lengthOfStartStack - 1)];
+      // this if checks to see whether the length of the stacked-on is < 0 OR|| if the value of the donut is less than 
+      // the value of the last number in the array lengthOfEndStack-1 of endStack of stacks. 
+      // we subtract 1 from the lengthOfEndStack because the index begins with 0 while the length of an array starts with 1
+      // So we have to subract one from the length else we would call on a numbe that isn't in the array, giving us "undefined"
+      if ((lengthOfEndStack === 0) || (donut < stacks[endStack][(lengthOfEndStack - 1)])){
+      return true; 
+      }
 
-  }
+      else {
+        console.log("Try again. Be sure the token moved is smaller.");
+        return false;
+      }
+
+    }
 
 }
+
 // This ELSE returns if the user inputs a wrong column !== a, b or c
 else {
   console.log("Please input a valid stack, a, b or c.");
   return false;
 }
 
-  // isLegal() takes two arguments, startStack and endStack, and will check to see if the block being moved, 
-  // from startStack is smaller than last block in endStack. return true if it is allowed, otherwise, return false. 
-  // Also, don't forget to think about if the endStack is empty, you may put any block there. Put this check before 
-  // your movePiece() function.
-
 }
 
 function checkForWin() {
   var lengthOfC = stacks.c.length;
+  var lengthOfB = stacks.b.length;
 
-  if (lengthOfC === 4){
+  if (lengthOfC === 4 || lengthOfB === 4){
     console.log('Hooray! You won!');
+    return true;
   }
 
   else {
     console.log('Try again.');
+    return false;
   }
 
 }
-/* In checkForWin(), you can simply check if the b stack or c stack has a .length of 4, then consoling out a 
-message like "You Won!!!" and returning true if a win is detected, or a false if not. */
 
 function towersOfHanoi(startStack, endStack) {
 
-  // Will run to see if the move is legal before the move is made:
-  if (isLegal(startStack, endStack)) 
+    // Will run to see if the move is legal before the move is made: 
+    if (isLegal(startStack, endStack)) 
     {
-  // This runs movePiece 
-  movePiece(startStack, endStack);
-  
-  // This runs a counter of moves to be displayed befor each new prompt.
-  counter ++;
-  
-  }
 
-  else {
-    console.log("pooh");
-  }
-}
+    // This runs movePiece 
+    movePiece(startStack, endStack);
+  
+    // This runs a counter of moves to be displayed befor each new prompt.
+    counter ++;
+   
+  
+    }
+
+    else {
+    console.log("pooh. You didn't enter a legal move.");
+    }
+
 
 var counter = 0;
 
@@ -107,7 +107,6 @@ console.log("then type in the tower you'd like to place it.");
 console.log("Use only a, b or c and the token on top must always be smaller. Good Luck!");
 
 function getPrompt() {
-
   console.log("Moves =" + " " + counter);
   checkForWin();
   printStacks();
