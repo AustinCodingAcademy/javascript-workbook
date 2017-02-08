d463f0dead1784f06e8e031e2ba38423d653c1d3
JoshStephenz <joshstephensla@gmail.com>
2017-02-07 20:42:02 -0600

    Tic Tac Toe

--- apps/03TicTacToe.js
+++ apps/03TicTacToe.js
@@ -42,9 +42,6 @@ function horizontalWin() {
   return (board[0][0] === playerTurn && board[0][1] === playerTurn && board[0][2] === playerTurn) || (board[1][0] === playerTurn && board[1][1] === playerTurn && board[1][2] === playerTurn) || (board[2][0] === playerTurn && board[2][1] === playerTurn && board[2][2] === playerTurn)
 }
 
-function horizontalRowWin(row) {
-  return (board[row][0] === playerTurn && board[row][1] === playerTurn && board[row][2] === playerTurn)
-}
 
 function verticalWin() {
   // Your code here
