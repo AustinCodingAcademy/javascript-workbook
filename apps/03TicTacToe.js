6d543880c2406db257a8efddc7dfc01433ed0e6a
aflater <aflater78704@gmail.com>
2017-02-07 18:56:54 -0600

    Tic Tac Toe App Passing

--- apps/03TicTacToe.js
+++ apps/03TicTacToe.js
@@ -13,6 +13,7 @@ var board = [
 ];
 
 var playerTurn = 'X';
+var stopGame = false;
 
 function printBoard() {
   console.log('   0  1  2');
@@ -24,24 +25,47 @@ function printBoard() {
 }
 
 function horizontalWin() {
-  // Your code here
+   return  (board[0][0] === playerTurn && board[0][1] === playerTurn && board[0][2] === playerTurn) ||
+          (board[1][0] === playerTurn && board[1][1] === playerTurn && board[1][2] === playerTurn) ||
+          (board[2][0] === playerTurn && board[2][1] === playerTurn && board[2][2] === playerTurn);
 }
 
+
 function verticalWin() {
-  // Your code here
+  return  (board[0][0] === playerTurn && board[1][0] === playerTurn && board[2][0] === playerTurn) ||
+          (board[0][1] === playerTurn && board[1][1] === playerTurn && board[2][1] === playerTurn) ||
+          (board[0][2] === playerTurn && board[1][2] === playerTurn && board[2][2] === playerTurn);
 }
 
 function diagonalWin() {
-  // Your code here
+   return  (board[0][0] === playerTurn && board[1][1] === playerTurn && board[2][2] === playerTurn) ||
+          (board[0][2] === playerTurn && board[1][1] === playerTurn && board[2][0] === playerTurn);
 }
 
 function checkForWin() {
-  // Your code here
+    if (horizontalWin() || verticalWin() || diagonalWin()) {
+    console.log('Player ' + playerTurn + ' Won!');
+    stopGame = true;
+    return true;
+  }
 }
 
+
+function checkForWin() {
+  if (horizontalWin() || verticalWin() || diagonalWin()) {
+    console.log('Player ' + playerTurn + ' Won!');
+    return true;
+  } else {
+    return false;
+  }
+};
+
 function ticTacToe(row, column) {
-  // Your code here
-}
+  board[row][column] = playerTurn;
+  checkForWin();
+  playerTurn = (playerTurn === 'X') ? 'O' : 'X';
+  
+};
 
 function getPrompt() {
   printBoard();
@@ -54,6 +78,7 @@ function getPrompt() {
   });
 
 }
+
 
 
 
