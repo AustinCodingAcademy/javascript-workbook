8aa47839959e1a18906b7a1fa1fd5856277f11a8
nick rodriguez <nickinwaco@gmail.com>
2017-02-07 20:49:27 -0600

    updated HorizontalWin

--- apps/03TicTacToe.js
+++ apps/03TicTacToe.js
@@ -26,7 +26,12 @@ function printBoard() {
 function horizontalWin() {
 
 //  Your code here
-  if ((board[0][0] === 'X' && board[0][1] === 'X' && board[0][2] === 'X' ) || (board[0][0] === 'O' && board[0][1] === 'O' && board[0][2] === 'O')) {
+  if ((board[0][0] === 'X' && board[0][1] === 'X' && board[0][2] === 'X' )
+   || (board[1][0] === 'X' && board[1][1] === 'X' && board[1][2] === 'X')
+   || (board[2][0] === 'X' && board[2][1] === 'X' && board[2][2] === 'X')
+   || (board[0][0] === 'O' && board[0][1] === 'O' && board[0][2] === 'O')
+   || (board[1][0] === 'O' && board[1][1] === 'O' && board[1][2] === 'O')
+   || (board[2][0] === 'O' && board[2][1] === 'O' && board[2][2] === 'O')) {
       console.log(playerTurn + " !!! WINS!!!    ...Resetting Game");
 
         board = [
