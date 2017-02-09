ff8672988291fef6052375c93d1b4312500c417e
Jonathan Gucciardi <jonmgucciardi@Jonathans-MacBook-Pro.local>
2017-02-08 20:05:19 -0600

    Towers: with Extra Credit

--- apps/04TowersOfHanoi.js
+++ apps/04TowersOfHanoi.js
@@ -13,15 +13,14 @@ var stacks = {
   c: []
 };
 
-//var numDisks = getPrompt("how many discs would you like: ?");
-//numDisks = stacks['a'].length;
+
 function printStacks() {
   console.log("a: " + stacks.a);
   console.log("b: " + stacks.b);
   console.log("c: " + stacks.c);
 }
 var moveCount = 1; // store the total number of moves played by user;
-
+var legalMoveCount = 0;
 /* function movePiece will handle moving the user selected stacks from startStack to endStack */
 function movePiece(startStack, endStack) {
   // Your code here : 
@@ -41,7 +40,7 @@ function isLegal(startStack, endStack) {
   var end = stacks[endStack].length; // store length of endStack in end;
   var start = stacks[startStack].length; // store length of startStack in start;
   if(stacks[endStack].length === 0 || stacks[startStack][start - 1] < stacks[endStack][end - 1]){
-    
+    legalMoveCount++; // add 1 to legal move counter here
     return true;
   
 }
@@ -75,7 +74,9 @@ return false;
 
 function towersOfHanoi(startStack, endStack) {
   // Your code here
-  isLegal(startStack, endStack); //call isLegal to check if moves done have passed true
+  if(isLegal(startStack, endStack)){
+    console.log("Number of Legal Moves: " + legalMoveCount);  
+  } //call isLegal to check if moves done have passed true
   movePiece(startStack, endStack); // call movePiece to allow user to move stacks
   checkForWin(); // test for a win here
   console.log("Number of Moves: " + moveCount++);
