12e568e2361ead92225da5f016d33cb2a339ed62
Mark Wang <athensbird@Marsbook.local>
2017-02-14 16:22:01 -0600

    HW V - Initial Commit

--- apps/05Mastermind.js
+++ apps/05Mastermind.js
@@ -11,6 +11,7 @@ var rl = readline.createInterface({
 var board = [];
 var solution = '';
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+var colors = require('colors/safe');
 
 function printBoard() {
   for (var i = 0; i < board.length; i++) {
@@ -29,12 +30,53 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateHint() {
-  // your code here
+function generateHint(solution, guess) {
+  // split the string into individual letters for indexing later
+  var solutionArray = solution.split("");
+  var guessArray = guess.split("");
+  // declare a variable to record the number of correct positions in the string
+  var correctLetterLocations = 0;
+  // loop around the solution array to see if there is a match 
+  for (var i = 0; i < solutionArray.length; i++) {
+    //detect a match
+    if (solutionArray[i] === guessArray[i]) {
+      //increment the counter
+      correctLetterLocations ++;
+      //set the list item at index i to null so there is no duplicate 
+      solutionArray[i] = null;
+    }
+  }
+  // declare a variable to record the number of correct letters in the string
+  var correctLetters = 0;
+  for (var j = 0; j < solutionArray.length; j++) {
+    //detect if the target index item exists in the solution array 
+    var targetIndex = solutionArray.indexOf(guessArray[j]);
+    if (targetIndex !== -1) {
+      correctLetters ++;
+      solutionArray[targetIndex] = null;
+    }
+  }
+  //return the hint as a string
+   return correctLetterLocations + "-" + correctLetters;
+}
+
+function endGame() {
+  if (board.length === 10) {
+    console.log("You ran out of turns! The solution was " + solution);
+  } else {
+    console.log("Guess again!");
+  }
 }
 
 function mastermind(guess) {
-  // your code here
+  //collect the hint value 
+  var hint = generateHint(solution, guess);
+  board.push(hint + guess);
+  //detect a win 
+  if (solution === guess) {
+    return "You guessed it!";
+  }
+  endGame();
 }
 
 
