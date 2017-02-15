fef1cd3b05c6ca76bf277604f7a08c01431b3d97
aflater <aflater78704@gmail.com>
2017-02-15 08:28:49 -0600

    HomeworkFivePassin

--- homework/05LessonFive.js
+++ homework/05LessonFive.js
@@ -16,27 +16,27 @@ var six = 6;
 
 // Problem 1:
 // What is the result of applying the increment (++) operator to var one?
-var onePlusPlus;
+var onePlusPlus = one + 1;
 
 // Problem 2:
 // What is the result of applying the decrement (--) operator to var two?
-var twoMinusMinus;
+var twoMinusMinus  = two - 1;
 
 // Problem 3:
 // What is the result of three += 3?
-var threePlusAssignmentThree;
+var threePlusAssignmentThree = 6; 
 
 // Problem 4:
 // What is the result of four -= 3?
-var fourMinusAssignmentThree;
+var fourMinusAssignmentThree = four - 3;
 
 // Problem 5:
 // What is the result of five *= 2?
-var fiveMultiplicationAssignmentTwo;
+var fiveMultiplicationAssignmentTwo = five * 2;
 
 // Problem 6:
 // What is the result of six /= 3?
-var sixDivisionAssignmentThree;
+var sixDivisionAssignmentThree = 6 / 3;
 
 // ****
 // Loops
@@ -52,7 +52,7 @@ while (whileLoopCounter < 10) {
 
 // Problem 7:
 // Fill in whileLoopArrayResult so that it matches whileLoopArray
-var whileLoopArrayResult = [];
+var whileLoopArrayResult = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // DO NOT MODIFY
 var forLoopArray = [];
@@ -63,7 +63,7 @@ for (forLoopCounter = 0; forLoopCounter > -10; forLoopCounter--) {
 
 // Problem 8:
 // Fill in forLoopArrayResult so that it matches forLoopArray
-var forLoopArrayResult = [];
+var forLoopArrayResult = [0, -1, -2, -3, -4, -5, -6, -7, -8, -9];
 
 // ****
 // Concept Checkpoint
@@ -71,7 +71,7 @@ var forLoopArrayResult = [];
 // Write your answer in comments
 //
 // What are loops? Why do we use them?
-//
+//If you want to execute a code mutiple times, using a loop comes in handy.
 
 // ****
 // Tests
