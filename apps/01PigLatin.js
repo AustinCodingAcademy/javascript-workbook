897b469303387c8bbd191ef854b9795e998576e7
aflater <aflater78704@gmail.com>
2017-02-15 20:50:05 -0600

    PigLatinIndentiontake2

--- apps/01PigLatin.js
+++ apps/01PigLatin.js
@@ -17,31 +17,31 @@ function pigLatin(word) {
 
   if ( ( word.indexOf('a') > -1 && word.indexOf('a') < vowelIndex ) || vowelIndex === -1 ) {
     vowelIndex = word.indexOf('a');
-}
+  }
 
   if ( ( word.indexOf('e') > -1 && word.indexOf('e') < vowelIndex ) || vowelIndex === -1 ) {
     vowelIndex = word.indexOf('e');
-}
+  }
 
   if ( ( word.indexOf('i') > -1 && word.indexOf('i') < vowelIndex ) || vowelIndex === -1 ) {
     vowelIndex = word.indexOf('i');
-}
+  }
 
   if ( ( word.indexOf('o') > -1 && word.indexOf('o') < vowelIndex ) || vowelIndex === -1 ) {
     vowelIndex = word.indexOf('o');
-}
+  }
   if ( ( word.indexOf('u') > -1 && word.indexOf('u') < vowelIndex ) || vowelIndex === -1 ) {
     vowelIndex = word.indexOf('u');
-}
+  }
   if ( ( word.indexOf('y') > -1 && word.indexOf('y') < vowelIndex ) || vowelIndex === -1 ) {
     vowelIndex = word.indexOf('y');
-}
+  }
   var firstPart = word.slice(0, vowelIndex);
   var restWord = word.slice(vowelIndex, word.length);
-    if (vowelIndex === 0)
+  if (vowelIndex === 0)
       return restWord + firstPart + 'yay';
       else {
-        return restWord + firstPart + 'ay';
+      return restWord + firstPart + 'ay';
       }
 }
 
