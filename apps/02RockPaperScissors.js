08a7ba39b12763a7fc4ab0a5c8ab510394784926
aflater <aflater78704@gmail.com>
2017-02-16 07:32:45 -0600

    RPSindentions

--- apps/02RockPaperScissors.js
+++ apps/02RockPaperScissors.js
@@ -14,34 +14,34 @@ function rockPaperScissors(hand1, hand2) {
 
   if (hand1 === hand2) {
     return "It's a tie!";
-}
+  }
 
   if (hand1 === 'rock') {
-  if (hand2 === 'scissors') {
+    if (hand2 === 'scissors') {
     return 'Hand one wins!';
-}
-  if(hand2 === 'paper'){
+  }
+    if(hand2 === 'paper'){
     return 'Hand two wins!';
-}
-}
+  }
+  }
 
   if (hand1 === 'paper') {
-  if (hand2 === 'scissors') {
+    if (hand2 === 'scissors') {
     return 'Hand two wins!';
-}
-  if (hand2 === rock) {
+  }
+    if (hand2 === rock) {
     return 'Hand one wins!';
-}
-}
+  }
+  }
 
   if(hand1 === "scissors") {
-  if (hand2 === 'rock') {
+    if (hand2 === 'rock') {
     return 'Hand two wins!';
-}
-  if (hand2 === 'paper') {
+  }
+    if (hand2 === 'paper') {
     return 'Hand one wins!'
-}
-}
+  }
+  }
 }
 
 function getPrompt() {
