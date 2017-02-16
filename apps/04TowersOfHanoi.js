304d478a66c6897df429f7e9a330ec82dea2825d
aflater <aflater78704@gmail.com>
2017-02-15 20:10:39 -0600

    FixingIndentionErrors

--- apps/04TowersOfHanoi.js
+++ apps/04TowersOfHanoi.js
@@ -31,23 +31,24 @@ function isLegal(startStack, endStack) {
   var topStartStack = stacks[startStack][stacks[startStack].length - 1];
   var topEndStack = stacks[endStack][stacks[endStack].length - 1];
   if (stacks[endStack].length === 0) {
-  return true;
+    return true;
   } 
   else if (stacks[startStack].length === 0) {
-  console.log("This is an empty stack");
-  return false;
-} 
+    console.log("This is an empty stack");
+    return false;
+  } 
   else {
-  console.log("Value of topStart Stack: " + topStartStack + ", Value of topEndStack: " + topEndStack);
-  return (topStartStack <= topEndStack);
-}
+    console.log("Value of topStart Stack: " + topStartStack + ", Value of topEndStack: " + topEndStack);
+    return (topStartStack <= topEndStack);
+  }
 }
 
 function checkForWin() {
   if ((stacks.b.length === 4) || (stacks.c.length === 4)) {
     console.log("You won!!");
     return true;
-} else {
+  } 
+  else {
     return false;
   }
 }
@@ -65,7 +66,7 @@ function towersOfHanoi(startStack, endStack) {
 }
 
 function getPrompt() {
-    printStacks();
+  printStacks();
     rl.question('start stack: ', (startStack) => {
         rl.question('end stack: ', (endStack) => {
             towersOfHanoi(startStack, endStack);
