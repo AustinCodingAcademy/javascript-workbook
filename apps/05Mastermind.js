084792d1bc103236b10499dc6ff6dedf5d399fc0
Madeline Olsen <madeline@Madelines-MacBook-Pro.local>
2017-02-16 19:31:52 -0600

    Lesson 2 homework, finished and passing all

--- apps/05Mastermind.js
+++ apps/05Mastermind.js
@@ -11,6 +11,8 @@ var rl = readline.createInterface({
 var board = [];
 var solution = '';
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+var winCheck = false;
+
 
 function printBoard() {
   for (var i = 0; i < board.length; i++) {
@@ -29,21 +31,55 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateHint() {
+function generateHint(solution, guess) {
   // your code here
+  var solutionArray = solution.split('') //splits letters of solution array into separate items
+  var guessArray = guess.split('') //splits letters of guess array into separate items
+  var correctLetterLocations = 0; //correct letter locations starts at 0
+  for (var i = 0; i < 4; i++) {
+    if (solutionArray[i] === guessArray[i]) { //if when counting up from 0, the corresponding solutionarray index and guessaray index are equal...
+      correctLetterLocations++; //then increment correctLetterLocations
+      solutionArray[i] = null; //then set index in solution array to null
+    }
+  }
+  var correctLetters = 0; //correct # letters starts at 0
+  var targetIndex = 0;
+  for (var i = 0; i < 4; i++) {
+  targetIndex = solutionArray.indexOf(guessArray[i]);
+  if(targetIndex > -1) { //iterate up to find if the targetindex item of guess is similar to an item in any index of solution
+    correctLetters++; //increment the correct letters
+    solutionArray[targetIndex] = null; //set that item in the solution at that index to null
+  }
+  }
+  return correctLetterLocations + "-" + correctLetters; //return the correct letter locations + the correct letters for player to see
 }
 
 function mastermind(guess) {
   // your code here
+  solution = 'abcd'; //the solution to the game
+  if (guess === solution) { //if the guess is the same as the solution
+    winCheck = true;
+    return "You guessed it!"; //print that they guessed it
+  }else{
+    var hint = generateHint(solution, guess); //otherwise, give them the hint from their answer
+    board.push(guess + hint);
+
+  }
 }
 
 
 function getPrompt() {
-  rl.question('guess: ', (guess) => {
-    console.log( mastermind(guess) );
-    printBoard();
-    getPrompt();
-  });
+  if(winCheck === false) { //if wincheck doesn't detect a win
+    prompt.get(['guess'], function (error, result) { //prompt user to guess
+      if(board.length === 10) { //if the user has guessed 10, times...
+        console.log('You ran out of turns! The solution was ' + solution) //print that they're out of turns + the solution
+      } else { //otherwise...
+        console.log( mastermind(result['guess']) );
+        printBoard();//print the board for user to see,
+        getPrompt(); //and prompt user to guess
+      }
+    });
+  }
 }
 
 // Tests
