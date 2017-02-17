5d9bc6792af1a3593e9c9b1a59891e7af30c4cdb
joeycaradonna <joeycaradonna45@gmail.com>
2017-02-16 20:57:59 -0600

    Mastermind App

--- apps/05Mastermind.js
+++ apps/05Mastermind.js
@@ -29,18 +29,51 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateHint() {
+function generateHint(solution, guess) {
   // your code here
+  var solutionArray = solution.split('');
+  var guessArray = guess.split('');
+  var correctLetterLocations = 0;
+  for (var i = 0; i < guess.length; i++) {
+    // This is checking for is the correct spot and letter
+    if (guessArray[i] === solutionArray[i]) {
+      solutionArray[i] = null;
+      correctLetterLocations = correctLetterLocations + 1;
+    }
+
+  }
+  var correctLetters = 0;
+  var targetIndex = null;
+  for (var j = 0; j < guess.length; j++) {
+    targetIndex = (solutionArray.indexOf(guessArray[j]));
+    if (targetIndex > -1) {
+      solutionArray[targetIndex] = null;
+      correctLetters++;
+    }
+
+  }
+  return ((correctLetterLocations) + '-' + (correctLetters));
 }
 
 function mastermind(guess) {
   // your code here
+  solution = 'abcd';
+  if (guess === solution) {
+    return "You guessed it!"
+  }
+  var hint = generateHint(solution, guess)
+  board.push(guess + ' ' + hint)
+  if (board.length === 10) {
+    return 'You ran out of turns! The solution was' + solution;
+  } else {
+    return 'Guess again';
+  }
 }
 
 
 function getPrompt() {
   rl.question('guess: ', (guess) => {
-    console.log( mastermind(guess) );
+    console.log(mastermind(guess));
     printBoard();
     getPrompt();
   });
@@ -50,22 +83,22 @@ function getPrompt() {
 
 if (typeof describe === 'function') {
 
-  describe('#mastermind()', function () {
-    it('should register a guess and generate hints', function () {
+  describe('#mastermind()', function() {
+    it('should register a guess and generate hints', function() {
       solution = 'abcd';
       mastermind('aabb');
       assert.equal(board.length, 1);
     });
-    it('should be able to detect a win', function () {
+    it('should be able to detect a win', function() {
       assert.equal(mastermind(solution), 'You guessed it!');
     });
   });
 
-  describe('#generateHint()', function () {
-    it('should generate hints', function () {
+  describe('#generateHint()', function() {
+    it('should generate hints', function() {
       assert.equal(generateHint('abcd', 'abdc'), '2-2');
     });
-    it('should generate hints if solution has duplicates', function () {
+    it('should generate hints if solution has duplicates', function() {
       assert.equal(generateHint('abcd', 'aabb'), '1-1');
     });
 
