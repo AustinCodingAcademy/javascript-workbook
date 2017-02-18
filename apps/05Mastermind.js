94629787c72516c5eb173b6fc11921991576d733
jeffgojeff <jeffgojeff@gmail.com>
2017-02-18 14:51:52 -0600

    mastermin update

--- apps/05Mastermind.js
+++ apps/05Mastermind.js
@@ -1,5 +1,20 @@
 'use strict';
 
+//not sure what this does yet..
+//declared down the list and set to nothing.
+//difference?
+//put INSIDE mastermind bruh 
+//solution = 'abcd';
+
+
+//global stuff?
+//set locally in order to change value as needed
+//var i = 0;
+//set locally because avas wrong ;)
+//var = correctLetterLocations = 0;
+
+
+
 var assert = require('assert');
 var colors = require('colors/safe');
 var readline = require('readline');
@@ -11,6 +26,7 @@ var rl = readline.createInterface({
 var board = [];
 var solution = '';
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
 
 function printBoard() {
   for (var i = 0; i < board.length; i++) {
@@ -29,12 +45,59 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateHint() {
+function generateHint(solution, guess) {
   // your code here
+  //hint me baby ;)
+  var solutionArray = solution.split('');
+  var guessArray = guess.split('');
+  // .split @ '' nothing in quotes is every place. 
+  // will turn each 'step' into an array
+  //console.log(solutionArray);
+  //console.log('solutionArray value ^^');
+
+  var correctLetterLocations = 0;
+  var correctLetter = 0;
+  
+
+  for (var i = 0; i < 4; i++) {
+    if (solutionArray[i] === guessArray[i]){
+      correctLetterLocations++;
+      solutionArray[i] = null;
+    }
+  }
+
+  for (var i = 0; i < 4; i++) {
+    var targetIndex = solutionArray.indexOf(guessArray[i]);
+    if (targetIndex > -1) {
+      correctLetter++;
+      solutionArray[targetIndex] = null;
+    }
+
+  }
+  return correctLetterLocations + '-' + correctLetter;
 }
 
 function mastermind(guess) {
   // your code here
+  //start writing code!!!
+  // -_-
+  solution = 'abcd';
+  var hint = generateHint(solution, guess);
+  board.push(guess+ ' ' + hint);
+  if (guess === solution) {
+    //console.log('*******************');
+    //console.log('* You guessed it! *');
+    //console.log('*******************');
+    return 'You guessed it!';
+  }
+  else if (board.length === 10) {
+    return 'You ran out of turns! The solution was ' + solution;
+  }
+ else {
+   //console.log('hit else');
+   //console.log(board); 
+   return 'Try again';
+ }
 }
 
 
