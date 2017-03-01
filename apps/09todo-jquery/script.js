e723f2b294bc1be9d75c5052f47a5193983cccf1
Tony <arinaldi@vt.edu>
2017-02-28 19:45:33 -0600

    Finished To-Do List app

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -1,5 +1,20 @@
 'use strict';
 
 $(document).ready(function() {
-  // App logic goes here
+	var $todoList = $('#todo-list');
+	$todoList.sortable();
+	$('#todo').focus();
+
+  $('form').submit(function(event) {
+  	event.preventDefault();
+  	var $todoText = $(this).find('input[type="text"]').val();
+
+  	$todoList.append('<li><input type="checkbox" />' + $todoText + '<input type="button" class="delete-button" value="X" /></li>');
+
+  	$(this).find('input[type="text"]').val('');
+
+  	$('.delete-button').click(function(event) {
+  		$(this).parent().remove();
+  	});
+  });
 });
