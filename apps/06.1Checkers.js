a1d62833e029011ee16d823e48cd67f97ba2bc8e
saf0911 <saf0911@gmail.com>
2017-03-01 08:32:25 -0600

    checkers finished

--- apps/06.1Checkers.js
+++ apps/06.1Checkers.js
@@ -8,8 +8,19 @@ var rl = readline.createInterface({
 });
 
 
-function Checker() {
+
   // Your code here
+  //setting the checker class and assigning it a color perameter
+class Checker {
+    constructor(color) {
+      //if the color is white make the checker white. If not make the checker black
+      if (color === 'white') {
+        this.symbol = String.fromCharCode(0x125CB);
+      }
+      else {
+        this.symbol = String.fromCharCode(0x125CF);
+      }
+  }
 }
 
 function Board() {
@@ -53,17 +64,78 @@ function Board() {
   };
 
   // Your code here
-}
-function Game() {
+  // setting a checker array
+  this.checkers = [];
+  //creating the checkers and assigning the position that they need to be placed for white and black checkers
+  this.createCheckers = function() {
+    var whitePositions = [[0, 1], [0, 3], [0, 5], [0, 7],
+      [1, 0], [1, 2], [1, 4], [1, 6],
+      [2, 1], [2, 3], [2, 5], [2, 7]];
 
+    var blackPositions = [[5, 0], [5, 2], [5, 4], [5, 6],
+      [6, 1], [6, 3], [6, 5], [6, 7],
+      [7, 0], [7, 2], [7, 4], [7, 6]]; 
+      // placing the white checkers on the grid
+      for (var i = 0; i <= 11; i++) {
+        var checker = new Checker('white');
+        var p = whitePositions[i];
+        this.checkers.push(checker);
+        this.grid[p[0]][p[1]] = checker;
+        //placing the black checkers on the grid
+        var checkerb = new Checker('black');
+        p = blackPositions[i];
+        this.checkers.push(checkerb);
+        this.grid[p[0]][p[1]] = checkerb;
+      }
+
+    }
+
+    this.selectChecker = function(row, column) { 
+      return this.grid[row][column];
+    }
+    //removing a checker if it gets jumped
+     this.killChecker = function(position) {
+      var checkerPositionKill = this.selectChecker(position[0], position[1]);
+      var checkerIndex = this.checkers.indexOf(checkerPositionKill);
+      if (checkerPositionKill !== null) {
+        this.checkers.splice(checkerIndex, 1);
+        this.grid[position[0]][position[1]] = null;
+    }
+  }
+}
+
+function Game() {
+// setting the board
   this.board = new Board();
 
   this.start = function() {
     this.board.createGrid();
     // Your code here
+    //placing checkers
+    this.board.createCheckers()
   };
-}
+  //making so players can move the checkers
+  this.moveChecker = function(start, end) {
+    //moving the checker and nulling the spot where it was
+    var checker = this.board.selectChecker(start[0], start[1]);
+    this.board.grid[start[0]][start[1]] = null;
+    this.board.grid[end[0]][end[1]] = checker;
+    //setting variables to find the start and end spot of the move to jump a checker
+   var startrow = Number(start[0]);
+   var startcolumn = Number(start[1]);
+   var endrow = Number(end[0]);
+   var endcolumn = Number(end[1]);
+   // finding the spot of the checker that was jumped
+   var cord1 = (startrow + endrow) / 2;
+   var cord2 = (startcolumn + endcolumn) / 2;
+   var killPosition = [cord1, cord2];
+   //setting the absolute value of the start and end to make sure the player moved two spots for a jump. 
+   // then calling the method to remove the jumped checker 
+   if (Math.abs(startrow - endrow) ===  2)
+    this.board.killChecker(killPosition);
 
+  }
+}
 function getPrompt() {
   game.board.viewGrid();
   rl.question('which piece?: ', (whichPiece) => {
@@ -110,3 +182,10 @@ if (typeof describe === 'function') {
 } else {
   getPrompt();
 }
+
+
+
+
+
+
+
