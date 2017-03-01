edcb148d22723c186c6f79ec46afadcd2ca98f67
joeycaradonna <joeycaradonna45@gmail.com>
2017-02-28 20:52:38 -0600

    Jquery Tic Tac Toe unfinished

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -1,5 +1,35 @@
 'use strict';
 
 $(document).on('ready', function() {
-  // Put app logic in here
+  console.log('start');
+
+  var playerTurn = 'X';
+
+  jQuery('div[data-cell]').on('click', function() {
+    $(this).text(playerTurn);
+    checkForWin();
+    playerTurn = (playerTurn === 'X' ? 'O' : 'X');
+
+  });
+
+  function checkForWin() {
+
+    if (
+      $('div[data-cell=0]').text() === playerTurn &&
+      $('div[data-cell=1]').text() === playerTurn &&
+      $('div[data-cell=2]').text() === playerTurn
+    ) {
+      console.log('win in the first row');
+    }
+    if (
+      $('div[data-cell=3]').text() === playerTurn &&
+      $('div[data-cell=4]').text() === playerTurn &&
+      $('div[data-cell=5]').text() === playerTurn
+    )
+  }
+
+  $(":button").click(function() {
+    $('[data-cell]').text('');
+
+});
 });
