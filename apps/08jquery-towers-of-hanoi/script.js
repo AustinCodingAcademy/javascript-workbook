ca96f966dccb70b27f904ff0ab8238fa4eab5ff4
Morgakd <morgakd@sbcglobal.net>
2017-02-28 20:55:08 -0600

    Towers of Hanoi jquery

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -1,5 +1,23 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
+// Put app logic here
+
+$('[data-stack]').click(function() {
+
+  var $block = $(this).children().last(); //set $block to the last item on the clicked data-stack
+  var $lastItemFrom = $(this).length;
+  var $lastItemTo = $(this).length;
+
+  if ($lastItemFrom != '0') {
+    $block.detach();
+  }
+
+  else if ($block != '' && $lastItemTo === '0' || $block.data() < $(this).children().last().data()) {
+    // if ($block != '') {
+    $(this).append($block);
+    $block = null;
+    // }
+  }
+});
 });
