c25e9965efa33ad466dc44b8aa8db4739095e8e8
Chris Austin <christophmaustin@gmail.com>
2017-02-28 20:52:25 -0600

    ticTacToeStart

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -1,5 +1,33 @@
 'use strict';
 
 $(document).on('ready', function() {
-  // Put app logic in here
+console.log('start');
+
+var playerTurn = 'X';
+
+jQuery('div[data-cell]').on('click', function() {
+$(this).text(playerTurn);
+checkForWin();
+playerTurn = (playerTurn === 'X' ? 'O' : 'X');
+
 });
+
+function checkForWin() {
+
+if (
+$('div[data-cell=0]').text() === playerTurn &&
+$('div[data-cell=1]').text() === playerTurn &&
+$('div[data-cell=2]').text() === playerTurn
+) {
+console.log('win in the first row');
+}
+}
+
+  $(':button').click(function(){
+$('[data-cell]').text(' ')
+
+});
+});
+
+
+//$('[data-cell]').text(' ')
