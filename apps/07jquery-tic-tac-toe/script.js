c987903635eb68dd0b5e5430b6432b4660291185
UnimportantWank <Xeromancer@gmail.com>
2017-03-01 19:14:23 -0600

    prevents duplicate moves; logs a win; resets the board and you can play again

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -3,23 +3,33 @@
 $(document).on('ready', function () {
   // Put app logic in here
 
-  var playerTurn = 'X';
-  $('div[data-cell]').on('click', function () {
-    if (isSpaceAvailable(this)) {
-      $(this).text(playerTurn);
+  function gameOn(x) {
+    var $this = x;
+    if (isSpaceAvailable($this)) {
+      console.log("SUCCESS");
+      $this.text(playerTurn);
       //checks for win each time there is a click
       checkForWin();
       playerTurn = (playerTurn === 'X') ? 'O' : 'X'
     }
+  }
+
+  var playerTurn = 'X';
+  $('div[data-cell]').on('click', function () {
+    gameOn($(this));
   });
-  //trying to prevent more clicks after a win
-  // $('div[data-cell]').on('click', function () {
-  //   if ('#announce-winner') {
-  //     $('div[data-cell]').off('click');
-  //     console.log('cheese');
-  //     return false;
-  //   }
+
+  // if (isSpaceAvailable(this)) {
+  //   $(this).text(playerTurn);
+  //   //checks for win each time there is a click
+  //   checkForWin();
+  //   playerTurn = (playerTurn === 'X') ? 'O' : 'X'
+  // }
   // });
+  // //trying to enable clicks after the board is cleared
+  function enableClicks() {
+    $('div[data-cell]').off('click');
+  };
 
   function checkForWin() {
     //checking for that win
@@ -52,16 +62,45 @@ $(document).on('ready', function () {
       winMessage();
     };
   }
+
+  /*
+
+    $("#someElement").click(function(){
+    $(this).unbind('click');
+    $('#anotherElement').show();
+  });
+
+  $('#anotherElement').click(function(){
+    $("#someElement").bind('click');
+  });
+    */
   // returns who won and stops the game from playing
   function winMessage() {
     $('#announce-winner').text('Player ' + playerTurn + ' Wins!');
-    $('div[data-cell]').off('click');
+    $('div[data-cell]').unbind('click');
   }
   //clear and remove the message
   $('#clear').on('click', function clearBoard() {
     $('div[data-cell]').text('');
     $('#announce-winner').text('');
+    console.log('ding');
+    // $('div[data-cell]').bind('click');
+    $('div[data-cell]').on('click', function () {
+      gameOn($(this));
+    });
+    console.log('dong');
   })
+  //checking to see if the space is available
+  function isSpaceAvailable(target) {
+    if ($(target).text() === '') {
+      $('#announce-winner').text(' ');
+      return true;
+    } else {
+      $('#announce-winner').text('That space is taken!');
+      return false;
+    }
+  }
+
   //checking to see if the space is available
   function isSpaceAvailable(target) {
     if ($(target).text() === '') {
