b02814ddbbb9ab164699ea2158eec4ea3f4d2be0
Chris Austin <christophmaustin@gmail.com>
2017-03-01 21:05:36 -0600

    TicTacToefinsihed

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -1,33 +1,66 @@
 'use strict';
 
 $(document).on('ready', function() {
-console.log('start');
+  // Put app logic in here
+  var playerTurn = 'X';
 
-var playerTurn = 'X';
+  $('[data-cell]').on("click", function() {
+    // conditional such that the following runs only if ($(this).text() === "")
+    if ($(this).text() === "") {
 
-jQuery('div[data-cell]').on('click', function() {
-$(this).text(playerTurn);
-checkForWin();
-playerTurn = (playerTurn === 'X' ? 'O' : 'X');
+      $(this).text(playerTurn);
+
+      if (checkForWin() === true) {
+        $('#announce-winner').text('Player ' + playerTurn + ' wins!');
+        return true;
+      }
+
+      playerTurn = (playerTurn === 'X') ? 'O' : 'X';
+    }
+  });
+
+  $('#clear').on("click", function() {
+    $('[data-cell]').empty();
+    $('#announce-winner').empty();
+    playerTurn = 'X';
+  });
+
+
+  function horizontalWin() {
+    if (($('[data-cell="0"]').text() === playerTurn && $('[data-cell="1"]').text() === playerTurn && $('[data-cell="2"]').text() === playerTurn) ||
+      ($('[data-cell="3"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="5"]').text() === playerTurn) ||
+      ($('[data-cell="6"]').text() === playerTurn && $('[data-cell="7"]').text() === playerTurn && $('[data-cell="8"]').text() === playerTurn)) {
+      console.log('horizontalWin');
+      return true;
+    }
+    return;
+  }
+
+  function verticalWin() {
+    if (($('[data-cell="0"]').text() === playerTurn && $('[data-cell="3"]').text() === playerTurn && $('[data-cell="6"]').text() === playerTurn) ||
+      ($('[data-cell="1"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="7"]').text() === playerTurn) ||
+      ($('[data-cell="2"]').text() === playerTurn && $('[data-cell="5"]').text() === playerTurn && $('[data-cell="8"]').text() === playerTurn)) {
+      console.log('verticalWin');
+      return true;
+    }
+    return;
+  }
+
+  function diagonalWin() {
+    if (($('[data-cell="0"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="8"]').text() === playerTurn) ||
+      ($('[data-cell="2"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="6"]').text() === playerTurn)) {
+      console.log('diagonalWin');
+      return true;
+    }
+    return;
+  }
+
+  function checkForWin() {
+    if ((horizontalWin() === true) || (verticalWin() === true) || (diagonalWin() === true)) {
+      return true;
+    }
+    return;
+  }
+
 
 });
-
-function checkForWin() {
-
-if (
-$('div[data-cell=0]').text() === playerTurn &&
-$('div[data-cell=1]').text() === playerTurn &&
-$('div[data-cell=2]').text() === playerTurn
-) {
-console.log('win in the first row');
-}
-}
-
-  $(':button').click(function(){
-$('[data-cell]').text(' ')
-
-});
-});
-
-
-//$('[data-cell]').text(' ')
