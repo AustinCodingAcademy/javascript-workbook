351eea318ca63e073ae97f3b0fbaf88e98a8839f
rbmcintosh <ryanbmcintosh@gmail.com>
2017-03-06 14:18:57 -0600

    Lesson 7 HW

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -1,5 +1,15 @@
 'use strict';
 
-$(document).on('ready', function() {
+$(document).on('ready', function(){
   // Put app logic in here
+  $('[data-cell]').click(function(){
+    var playerTurn = 'X';
+    $(this).text(playerTurn);
+    playerTurn = (playerTurn === 'X') ? 'O' : 'X';
+  });
+
+  $('button').click(function(){
+    $('[data-cell]').text('');
+    playerTurn = 'X';
+  });
 });
