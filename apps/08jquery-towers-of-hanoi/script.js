a2cb29905c8c4d7ff650f9d952419f1151c3df5a
bttodd11 <bttodd@icloud.com>
2017-03-06 21:49:01 -0600

    Tower of Hanoi From Thursday

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -1,5 +1,69 @@
 'use strict';
 
-$(document).ready(function() {
-  // Put app logic here
+var assert = require('assert');
+var readline = require('readline');
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
+
+var stacks = {
+  a: [4, 3, 2, 1],
+  b: [],
+  c: []
+};
+
+function printNewStacks() {
+  console.log("a: " + stacks.a);
+  console.log("b: " + stacks.b);
+  console.log("c: " + stacks.c);
+}
+
+function movePiece(startPile, endPile) {
+  // Moves the Pieces to the Pile
+  stacks[endPile].push(stacks[startPile].pop());
+}
+// Checks to see if the move is legal or not
+function isLegal(startPilek, endPile) {
+  // Your code here
+  var lastItemFrom = stacks[startPile][stacks[startPile].length - 1];
+  var lastItemTo = stacks[endPile][stacks[endPile].length - 1];
+  if (lastItemTo === undefined || lastItemFrom < lastItemTo) {
+    movePiece(startPile, endPile);
+    checkForWin();
+    return true
+  } else if (lastItemFrom > lastItemTo) {
+    console.log("Opps, try again");
+    return false
+  }
+
+}
+
+function checkForWin() {
+  // Checks to see if you won or not
+  if (stacks.c.length === 4 || stacks.b.length === 4) {
+    printStacks();
+    console.log("You Won!!");
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
+function towersOfHanoi(startPile, endPile) {
+  // Your code here
+  isLegal(startPile, endPile);
+
+}
+
+function getPrompt() {
+  printStacks();
+
+  rl.question('start stack: ', (startPile) => {
+    rl.question('end stack: ', (endPile) => {
+      towersOfHanoi(startPile, endPile);
+      getPrompt();
+    });
+  });
+}
