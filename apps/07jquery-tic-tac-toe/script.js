9622fd5bb10298e71bb63883963118f54181058a
RX7RUSS <russ.savage5@gmail.com>
2017-03-07 16:37:37 -0600

    TTT All Tests Pass

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -1,97 +1,62 @@
-'use strict';
-
-$(document).on('ready', function() {
+$(document).on('ready', function () {
   // Put app logic in here
   var playerTurn = 'X';
-  var $dataCell = $('[data-cell]');
-  $('[data-cell]').click(function() {
+  
+  $('[data-cell]').click(function () {
     $(this).text(playerTurn);
-
+    checkForWin();
     playerTurn = (playerTurn === 'X') ? 'O' : 'X';
-      
-
+    
   })
 
-  checkForWin ();
-  clearBoard ();
-  })
+  function checkForWin() {
 
+    //horiz
 
-function checkForWin () {
-  if($('[data-cell="0"]').text() === playerTurn && $('[data-cell="1"]').text() === playerTurn && $('[data-cell="2"]').text() === playerTurn) {
-    $('#announce-winner').text('Player ' + playerTurn + ' Wins!');
+    if($('[data-cell="0"]').text() === playerTurn && $('[data-cell="1"]').text() === playerTurn && $('[data-cell="2"]').text() === playerTurn) {
+      $('#announce-winner').text('Player ' + playerTurn + ' Wins!');
+    }
+    if($('[data-cell="3"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="5"]').text() === playerTurn) {
+      $('#announce-winner').text('Player ' + playerTurn + ' Wins!');
+    }
+    if($('[data-cell="6"]').text() === playerTurn && $('[data-cell="7"]').text() === playerTurn && $('[data-cell="8"]').text() === playerTurn) {
+      $('#announce-winner').text('Player ' + playerTurn + ' Wins!');
+    }
+    //vert
+
+    if($('[data-cell="0"]').text() === playerTurn && $('[data-cell="3"]').text() === playerTurn && $('[data-cell="6"]').text() === playerTurn) {
+      $('#announce-winner').text('Player ' + playerTurn + ' Wins!');
+    }
+    if($('[data-cell="1"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="7"]').text() === playerTurn) {
+      $('#announce-winner').text('Player ' + playerTurn + ' Wins!');
+    }
+    if($('[data-cell="2"]').text() === playerTurn && $('[data-cell="5"]').text() === playerTurn && $('[data-cell="8"]').text() === playerTurn) {
+      $('#announce-winner').text('Player ' + playerTurn + ' Wins!');
+    }
+    //diag
+
+    if($('[data-cell="0"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="8"]').text() === playerTurn) {
+      $('#announce-winner').text('Player ' + playerTurn + ' Wins!');
+    }
+    if($('[data-cell="2"]').text() === playerTurn && $('[data-cell="4"]').text() === playerTurn && $('[data-cell="6"]').text() === playerTurn) {
+      $('#announce-winner').text('Player ' + playerTurn + ' Wins!');
     }
 
-  if(('[data-cell=3]').text() === playerTurn && ('[data-cell=4]').text() === playerTurn && ('[data-cell=5]').text() === playerTurn) {
-    $('#announce-winner').text('Player ' + playerTurn + ' Wins!');
-    }
-
-  if(('[data-cell=6]').text() === playerTurn && ('[data-cell=7]').text() === playerTurn && ('[data-cell=8]').text() === playerTurn) {
-    $('#announce-winner').text('Player ' + playerTurn + ' Wins!');
-    }
-
-
-
-  if(('[data-cell=0]').text() === playerTurn && ('[data-cell=3]').text() === playerTurn && ('[data-cell=6]').text() === playerTurn) {
-    $('#announce-winner').text('Player ' + playerTurn + ' Wins!');
-    }
-
-  if(('[data-cell=1]').text() === playerTurn && ('[data-cell=4]').text() === playerTurn && ('[data-cell=7]').text() === playerTurn) {
-    $('#announce-winner').text('Player ' + playerTurn + ' Wins!');
-    }
-
-  if(('[data-cell=2]').text() === playerTurn && ('[data-cell=5]').text() === playerTurn && ('[data-cell=8]').text() === playerTurn) {
-    $('#announce-winner').text('Player ' + playerTurn + ' Wins!');
-    }
-
-
-  if(('[data-cell=0]').text() === playerTurn && ('[data-cell=4]').text() === playerTurn && ('[data-cell=8]').text() === playerTurn) {
-    $('#announce-winner').text('Player ' + playerTurn + ' Wins!');
-    }
-
-  if(('[data-cell=2]').text() === playerTurn && ('[data-cell=4]').text() === playerTurn && ('[data-cell=6]').text() === playerTurn) {
-    $('#announce-winner').text('Player ' + playerTurn + ' Wins!');
-    }
-
-
-
-
-  function clearBoard () {
-    $('#clear').click(function() {
-      $('[data-cell]').empty();
-      playerTurn = 'X';
-      $('#announce-winner').empty();
-    });
   }
-
-  
-
-
-}
-
-  
-
-
-
-
-
-
-
 
 
 // Add a button with an id="clear" that will not only clear the board, but reset the player to player 'X'
 
+  $('#clear').click(function () {
+    $('[data-cell]').empty();
+    playerTurn = 'X';
+    $('#announce-winner').empty();
+  })
+});
 
-// function setup(){
-//   $('data-cell').click(function(){
-//     this.text(playerTurn);
-//   });
+  
 
-// }
 
-// function runThisCodeOnce(){
-//   $('[data-cell]').click(handleDataCellDivClick);
-//   $(this).text('X');
-// }
 
-// this variable will represent which of the divs are clicked on!!!
+
+
