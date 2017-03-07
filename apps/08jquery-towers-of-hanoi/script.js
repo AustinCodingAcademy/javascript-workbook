3f2ba55946d6f2a3ac8cbe15cc40beeebe780124
joeycaradonna <joeycaradonna45@gmail.com>
2017-03-07 14:20:08 -0600

    Towers of Hanoi passing w/comments

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -1,5 +1,35 @@
 'use strict';
 
+var $block = null;
 $(document).ready(function() {
-  // Put app logic here
+  $('[data-stack]').on('click', function() {
+//This is were the blocks will be moved from one stack to another stack
+      if ($block === null) {
+
+        $block = $(this).children().last().detach();
+
+      } else {
+        if (
+//Moving the blocks to either stack
+          $($block).data('block') < $(this).children().last().data('block') ||
+          $(this).children().length === 0) {
+        $(this).append($block);
+        $block = null;
+
+      }
+    }
+    //This is running after every move to make sure that it's a legal move
+    checkForWin();
+
+  })
+
+function checkForWin() {
+//Declaring a winner if the stacks are on either 2 or 3 and have 4 blocks on them
+  if (
+    $('[data-stack="2"]').children().length === 4 ||
+    $('[data-stack="3"]').children().length === 4
+  ) {
+    $('#announce-game-won').text('You Won!');
+  }
+}
 });
