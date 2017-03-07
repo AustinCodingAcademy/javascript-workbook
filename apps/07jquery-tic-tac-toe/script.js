3dabd3d12b666266af67429bffa036c8893a7be2
cruzmndz <cruzalexmendez@gmail.com>
2017-03-06 21:03:41 -0600

    First push (top horizontal win only)
    need to add all the other winning methods.

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -1,5 +1,39 @@
 'use strict';
 
+var getElement = $;
+var playerTurn = 'X';
+
 $(document).on('ready', function() {
   // Put app logic in here
+  setUp(); //------>function to register a few click handlers
+  clearBoardClick();
 });
+
+function setUp(){
+  getElement('[data-cell]').click(handleClick);
+};
+
+function clearBoardClick(){
+  getElement('#clear').click(clearBoard);
+};
+
+function handleClick(){
+  getElement(this).text(playerTurn);
+  if (checkForWin()){
+    getElement('#announce-winner').text('Player ' + playerTurn + ' Wins!');
+  }
+  playerTurn = (playerTurn === 'X') ? 'O' : 'X';
+};
+
+function clearBoard(){
+  getElement('[data-cell]').empty();
+};
+
+function checkForWin(){
+  if (getElement('[data-cell="0"]').text() === playerTurn && 
+	    getElement('[data-cell="1"]').text() === playerTurn &&
+      getElement('[data-cell="2"]').text() === playerTurn) {
+        return true;
+      }
+
+};
