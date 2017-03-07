a07ce661b7057422fa68104173f9f8d778529338
joeycaradonna <joeycaradonna45@gmail.com>
2017-03-07 15:04:00 -0600

    Towers of hanoi finished fix

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -3,16 +3,16 @@
 var $block = null;
 $(document).ready(function() {
   $('[data-stack]').on('click', function() {
-//This is were the blocks will be moved from one stack to another stack
-      if ($block === null) {
+    //This is were the blocks will be moved from one stack to another stack
+    if ($block === null) {
 
-        $block = $(this).children().last().detach();
+      $block = $(this).children().last().detach();
 
-      } else {
-        if (
-//Moving the blocks to either stack
-          $($block).data('block') < $(this).children().last().data('block') ||
-          $(this).children().length === 0) {
+    } else {
+      if (
+        //Moving the blocks to either stack
+        $($block).data('block') < $(this).children().last().data('block') ||
+        $(this).children().length === 0) {
         $(this).append($block);
         $block = null;
 
@@ -23,13 +23,13 @@ $(document).ready(function() {
 
   })
 
-function checkForWin() {
-//Declaring a winner if the stacks are on either 2 or 3 and have 4 blocks on them
-  if (
-    $('[data-stack="2"]').children().length === 4 ||
-    $('[data-stack="3"]').children().length === 4
-  ) {
-    $('#announce-game-won').text('You Won!');
+  function checkForWin() {
+    //Declaring a winner if the stacks are on either 2 or 3 and have 4 blocks on them
+    if (
+      $('[data-stack="2"]').children().length === 4 ||
+      $('[data-stack="3"]').children().length === 4
+    ) {
+      $('#announce-game-won').text('You Won!');
+    }
   }
-}
 });
