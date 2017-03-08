cadcd6afe700b234ec5d618b9414c03293ecfbd1
Jonathan Gucciardi <jonmgucciardi@Jonathans-MacBook-Pro.local>
2017-03-08 13:47:28 -0600

    Lesson 8 homework

--- homework/08LessonEight/script.js
+++ homework/08LessonEight/script.js
@@ -17,30 +17,30 @@ $(document).on('ready', function() {
 
   // Problem 1:
   // Get the children of #first-list using the .children() method
-  window.firstListChildren = null;
+  window.firstListChildren = $('#first-list').children();
 
   // Problem 2:
   // Get the children of #second-list using the .children() method
-  window.secondListChildren = null;
+  window.secondListChildren = $('#second-list').children();
   
   // Problem 3:
   // Now use the .detach() method on window.firstListChildren to
   // remove those elements from the document
-  window.firstListChildren;
+  window.firstListChildren = $(firstListChildren).detach();
 
   // Problem 4:
   // Now use the .detach() method on window.secondListChildren to
   // remove those elements from the document
-  window.secondListChildren;
+  window.secondListChildren = $(secondListChildren).detach();
 
   // Problem 5:
   // use $finalList.prepend() to put window.firstListChildren
   // at the beggining of #final-list
-  $finalList;
+  $finalList.prepend(firstListChildren);
 
   // Problem 6:
   // use $finalList.append() to put window.secondListChildren
   // at the end of #final-list
-  $finalList;
+  $finalList.append(secondListChildren);
 
 });
