67598d2b7418a8da156658eabdac6649310a4c5b
cruzmndz <cruzalexmendez@gmail.com>
2017-03-07 19:01:40 -0600

    final
    all tests passing plus bonus spec.

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -5,7 +5,7 @@ var playerTurn = 'X';
 
 $(document).on('ready', function() {
   // Put app logic in here
-  setUp(); //------>function to register a few click handlers
+  setUp();
   clearBoardClick();
 });
 
@@ -18,7 +18,12 @@ function clearBoardClick(){
 };
 
 function handleClick(){
-  getElement(this).text(playerTurn);
+  //IF STATEMENT TO PROTECT AGAINST A PREVIOUSLY MARKED CELL
+  if (getElement(this).text() === '') {
+    getElement(this).text(playerTurn);
+  } else {
+    return false;
+  }
   if (checkForWin()){
     getElement('#announce-winner').text('Player ' + playerTurn + ' Wins!');
   }
@@ -30,10 +35,47 @@ function clearBoard(){
 };
 
 function checkForWin(){
+  //ALL HORIZONTAL ROWS
   if (getElement('[data-cell="0"]').text() === playerTurn && 
 	    getElement('[data-cell="1"]').text() === playerTurn &&
       getElement('[data-cell="2"]').text() === playerTurn) {
         return true;
-      }
-
+  }
+  if (getElement('[data-cell="3"]').text() === playerTurn && 
+	    getElement('[data-cell="4"]').text() === playerTurn &&
+      getElement('[data-cell="5"]').text() === playerTurn) {
+        return true;
+  }
+  if (getElement('[data-cell="6"]').text() === playerTurn && 
+	    getElement('[data-cell="7"]').text() === playerTurn &&
+      getElement('[data-cell="8"]').text() === playerTurn) {
+        return true;
+  }
+  //ALL VERTICAL ROWS
+  if (getElement('[data-cell="0"]').text() === playerTurn && 
+	    getElement('[data-cell="3"]').text() === playerTurn &&
+      getElement('[data-cell="6"]').text() === playerTurn) {
+        return true;
+  }
+  if (getElement('[data-cell="1"]').text() === playerTurn && 
+	    getElement('[data-cell="4"]').text() === playerTurn &&
+      getElement('[data-cell="7"]').text() === playerTurn) {
+        return true;
+  }
+  if (getElement('[data-cell="2"]').text() === playerTurn && 
+	    getElement('[data-cell="5"]').text() === playerTurn &&
+      getElement('[data-cell="8"]').text() === playerTurn) {
+        return true;
+  }
+  //DIAGONAL WINS
+  if (getElement('[data-cell="0"]').text() === playerTurn && 
+	    getElement('[data-cell="4"]').text() === playerTurn &&
+      getElement('[data-cell="8"]').text() === playerTurn) {
+        return true;
+  }
+  if (getElement('[data-cell="2"]').text() === playerTurn && 
+	    getElement('[data-cell="4"]').text() === playerTurn &&
+      getElement('[data-cell="6"]').text() === playerTurn) {
+        return true;
+  }
 };
