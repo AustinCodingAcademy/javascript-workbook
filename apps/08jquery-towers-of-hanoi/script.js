45206a8f28b5a6d85c272594a561aff09c9b44ed
bttodd11 <bttodd@icloud.com>
2017-03-07 23:02:36 -0600

    Updated Checkpoint still needs to check for win correctly

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -2,30 +2,30 @@
 
 var $block = null;
 $(document).ready(function() {
-  //On Click for game play
   $('[data-stack]').on('click', function() {
-    if ($block === 'null') {
+    //This moves the codes on click
+    if ($block === null) {
       $block = $(this).children().last().detach();
     } else {
       if (
-        //This will move the blocks
+        //This is what moves blocks around the pile
         $($block).data('block') < $(this).children().last().data('block') ||
         $(this).children().length === 0) {
         $(this).append($block);
         $block = null;
       }
     }
-    //Checks to see if you have won  ..... What checks it each time after you have moved the block
+    //Call the check for win after each block move
     checkForWin();
   })
 
   function checkForWin() {
-    //Will check to see if you have won
+    // This checks for a win
     if (
       $('[data-stack="2"]').children().length === 4 ||
       $('[data-stack="3"]').children().length === 4
     ) {
-      $('#announce-game-won').text('You have Won!');
+      $('#announce-game-won').text('You Won!');
     }
   }
 });
