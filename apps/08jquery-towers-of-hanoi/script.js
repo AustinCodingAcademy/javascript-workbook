b79345aeaad784636b0690551ff269b6c3938c9a
Chris Austin <christophmaustin@gmail.com>
2017-03-07 18:43:50 -0600

    WorkingTowersCheckPoint

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -1,36 +1,32 @@
 'use strict';
 
-//Below is required to start the game and get the document ready
+var $block = null;
+//This below gets the document ready for jQuery
 $(document).ready(function() {
-  //Below is the variable we will use to define the css selector
-  var $dataStacks = $('[data-stack]');
-  var $block = null;
-  //Below is the click method, which allows the user to play the game
-  $dataStacks.click(function() {
-    if ($block) {
-      var $endStack = $(this);
-      var $lastBlock = $(this).children().last();
-
-      //This checks to see if the move is legal and if it is, it will apend the block
-      if ($block.data('block') < $lastBlock.data('block') || $endStack.children().length === 0) {
-        $endStack.append($block);
-      }
-      //This will detach the block and add it to the end of the block
-    } else {
+  $('[data-stack]').on('click', function() {
+    //Code below initiates the game to be played with they click on it
+    if ($block === null) {
       $block = $(this).children().last().detach();
+    } else {
+      if (
+        //this takes a block and appends it to the next 
+        $($block).data('block') < $(this).children().last().data('block') ||
+        $(this).children().length === 0) {
+        $(this).append($block);
+        $block = null;
+      }
     }
-    //This will check for a win after every click (move)
+    //This checks for a win after every time you move a block
     checkForWin();
-  });
-  //Below are the parameters that define a win.
-  function checkForWin() {
-    var middleLength = $('[data-stack="2"]').children().length;
-    var lastLength = $('[data-stack="3"]').children().length;
+  })
 
-    //Display winning message if middle or last stack is four blocks long
-    if (middleLength === 4 || lastLength === 4) {
+  function checkForWin() {
+    //This declares a win when the length equals 4 on stack2 or stack3
+    if (
+      $('[data-stack="2"]').children().length === 4 ||
+      $('[data-stack="3"]').children().length === 4
+    ) {
       $('#announce-game-won').text('You Won!');
     }
   }
-
 });
