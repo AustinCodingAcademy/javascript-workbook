085e294cdef27821499cad9f4eded4b8ec2d1416
Tony <arinaldi@vt.edu>
2017-03-08 09:50:58 -0600

    Finished address book

--- apps/10address-book-ajax/script.js
+++ apps/10address-book-ajax/script.js
@@ -1,5 +1,28 @@
 'use strict';
 
 $(document).ready(function() {
-  // You code here
+  var url = 'https://reqres-api.herokuapp.com/api/users/';
+  $.ajax(url, {
+    success: function(response) {
+      response.forEach(function(user) {
+        var str = '<tr><td>' + user.id + '</td><td>' + user.first_name + '</td><td>' + user.last_name + '</td><td><a data-id="' + user.id + '" href="#">view</a></td></tr>';
+        $('tbody').append(str);
+      });
+      $('a[data-id]').click(function(e) {
+        e.preventDefault();
+        var $detailsDiv = $('#details');
+        var userUrl = url + $(this).data('id');
+
+        $detailsDiv.empty();
+
+        $.ajax(userUrl, {
+          success: function(user) {
+            var details = '<h3>' + user.first_name + ' ' + user.last_name + '</h3><h4>' + user.occupation + '</h4><p>' + user.phone + '</p><p>' + user.address + '</p><img src="' + user.avatar + '"/>';
+            $detailsDiv.append(details);
+          }
+        });
+      });
+      $('a[data-id')[0].click();
+    }
+  });
 });
