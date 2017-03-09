12496635c84ad572c6ca32d300312de2fbd33760
samlynch96 <sam@bigbabyagency.com>
2017-03-08 21:38:36 -0600

    07 tic tac toe incomplete

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -2,4 +2,35 @@
 
 $(document).on('ready', function() {
   // Put app logic in here
+  // where am i?
+  var playerTurn='X';
+
+  jQuery('div[data-cell]').on('click',function(){
+
+    $(this).text(playerTurn);
+    checkForWin();
+    playerTurn = (playerTurn === 'X'? 'O' : 'X');
+
+  });
+
+
+//returna boolean
+function checkForWin(){
+  // get the text for 0, 1, and 2
+  if(
+  $('div[data-cell=0]').text() === playerTurn &&
+  $('div[data-cell=0]').text() === playerTurn &&
+  $('div[data-cell=0]').text() === playerTurn
+) {
+    console.log ('win in first row');
+    clearBoard();
+    }
+
+
+  };
+
+  function clearBoard(){
+    $('[data-cell]').text('');
+
+  }
 });
