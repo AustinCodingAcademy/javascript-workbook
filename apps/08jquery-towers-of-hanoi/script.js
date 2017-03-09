2d1be073fe7d451b5042f62c6399d2a8affa40c0
rbmcintosh <ryanbmcintosh@gmail.com>
2017-03-08 20:55:21 -0600

    Towers Of Hanoi

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -1,5 +1,25 @@
 'use strict';
 
+var block = null;
+
 $(document).ready(function() {
   // Put app logic here
+  $('[data-stack]').click(clickOfDataStack);
 });
+
+function clickOfDataStack(){
+  // this data stack got clicked
+  
+  if (block){
+    var lastBlockOfTheStack = $(this).children().last();
+    var dataAttributeOfLastBlock = lastBlockOfTheStack.data("block");
+    var dataAttributeOfBlockWeAreMoving = block.data("block");
+    
+    if (dataAttributeOfLastBlock > dataAttributeOfBlockWeAreMoving || lastBlockOfTheStack.length === 0){
+      $(this).append(block);
+      block = null;
+    }
+  } else {
+      block = $(this).children().last().detach();
+  }
+}
