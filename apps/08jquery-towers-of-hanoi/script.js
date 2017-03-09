d02d9a22b48fd5e7b9f0a2f8161914c658d42d76
RX7RUSS <russ.savage5@gmail.com>
2017-03-08 20:46:11 -0600

    TOH 3 Tests Passing

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -1,11 +1,47 @@
 'use strict';
 
+var $block = null;
+
+
 $(document).ready(function() {
   // Put app logic here
-  var $block = null; 
+  
+
   $('[data-stack]').click(function () {
-    $block = $(this).children().last().detach();
+    // $block = $(this).children().last().detach();
     
-  })
+    if($block) {
+      var $lastBlock = $(this).children().last();
+      var $lastBlockValue = $lastBlock.data("block");
+      var $blockToBePlaced = $block.data("block");
+
+
+      if($blockToBePlaced < $lastBlockValue || !$lastBlockValue) {
+        //comparing the block that was clicked on to the last block in a stack
+        
+        $(this).append($block);
+        $block = null;
+      }
+              
+    } else {
+      $block = $(this).children().last().detach();
+    }
+  
+
+  });
 
 });
+
+
+// $(document).ready(function() {
+//   // Put app logic here
+   
+//   $('[data-stack]').click(function () {
+//     $block = $(this).children().last().detach();
+                      //the above technique is called a train or training
+    
+//   });
+
+// });
+
+
