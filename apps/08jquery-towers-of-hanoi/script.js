709b7ed2b5bcaceb677b0cad6603b76c771e71e8
cruzmndz <cruzalexmendez@gmail.com>
2017-03-08 21:03:36 -0600

    first push
    blocks can move now.

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -1,5 +1,26 @@
 'use strict';
 
+var block = null;
+
 $(document).ready(function() {
-  // Put app logic here
+  $('[data-stack]').click(stackWasClicked);
 });
+
+function stackWasClicked() {
+  //IF BLOCK HAS SOMETHING IN IT WE ALREADY PICKED UP A BLOCK. 
+  if (block) {    //ADDED  !== NULL) {
+    //BLOCK IS PICKED UP, EXECUTE THIS TO PUT IT BACK DOWN SOMEWHERE.
+    var lastBlockofStack = $(this).children().last();
+    var dataBlockAttrOfLastBlock = lastBlockofStack.data('block');
+    var dataAttrOfRemovedBlock = block.data('block');
+
+    if (dataBlockAttrOfLastBlock > dataAttrOfRemovedBlock || lastBlockofStack.length === 0) {
+      $(this).append(block);
+      block = null;
+    }
+  //ELSE I HAVENT PICKED ANYTHING UP AND I NEED TO PICK UP A BLOCK
+  } 
+  else {
+    block = $(this).children().last().detach();
+  }
+}
