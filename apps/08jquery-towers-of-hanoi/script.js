85fcf300d8b706d89aefad7550a92db12f62980c
rbmcintosh <ryanbmcintosh@gmail.com>
2017-03-10 17:19:16 -0600

    Towers: All Tests Passing

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -1,25 +1,42 @@
 'use strict';
 
+// Stored global variable
 var block = null;
 
 $(document).ready(function() {
-  // Put app logic here
+  // Put app logic here.
   $('[data-stack]').click(clickOfDataStack);
 });
 
 function clickOfDataStack(){
-  // this data stack got clicked
   
+  // This data stack got clicked.
   if (block){
+
+    // Stored local variables
     var lastBlockOfTheStack = $(this).children().last();
     var dataAttributeOfLastBlock = lastBlockOfTheStack.data("block");
     var dataAttributeOfBlockWeAreMoving = block.data("block");
     
+    // This is an if/else statement deciding whether or not the move is legal. If so, the block is moved.
     if (dataAttributeOfLastBlock > dataAttributeOfBlockWeAreMoving || lastBlockOfTheStack.length === 0){
       $(this).append(block);
       block = null;
-    }
-  } else {
-      block = $(this).children().last().detach();
+    } 
   }
-}
+
+  // The block is detached and stored.
+  else {
+    block = $(this).children().last().detach();
+  }
+
+  // After block is moved, we are checking to see if there is a win.
+  checkForWin();
+};
+
+// This checks the length of stack 2 and 3 and whether or not each individually are equal to 4. If so, text is displayed notifying user of a win. 
+function checkForWin(){
+  if ($('[data-stack="2"]').children().length === 4 || $('[data-stack="3"]').children().length === 4){
+    $('#announce-game-won').text('You Won!');
+  }
+};
