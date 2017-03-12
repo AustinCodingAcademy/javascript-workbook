6a357e1f463f992b36481b9ae474ba2119641194
HippErger <berger.clayton@gmail.com>
2017-03-12 14:47:09 -0500

    JS is creating a new div and CSS is stylizig the div on stack one to create multiple playing pieces. Not finished yet but good place to pause.

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -9,8 +9,10 @@ $(document).ready(function() {
   pushCounter();
   // listens for the click on a [data-stack] then runs the function to deal with that click
   $("[data-stack]").click(handleWhatWasClicked);
+  $("[data-stack]").click(playSound);
+  $("#input-Submit").click(generateBlocks);
   // listens for the click on the clear button then runs the function to deal with that click
-  $('#clear').click( clearTowers);
+  $('#clear').click(clearTowers);
 });
 
 // This function takes current vars as arguments to decide what to do with the information.
@@ -22,6 +24,7 @@ function handleWhatWasClicked(){
       // pick one up.
       block = $(this).children().last().detach(); //instead of .detach() just set he block to be there then detach when we click on the second tower. 
          $("#moving-div").append(block);
+        //  playSound();
       // but if there isn't anything in the stack tell them to choose a different stack 
     } else if($(this).children().length === 0){
         alert("Please choose a stack with pieces to play.");
@@ -39,6 +42,7 @@ function handleWhatWasClicked(){
       if(widthOfTopPiece > widthOfMovingPiece ||  topPieceValue === 0 ){
         // puts the block down
         $(this).append(block);
+        // playSound();
         // resets the block var to null
         block = null;
         // adds mark to the move tally
@@ -55,7 +59,7 @@ function handleWhatWasClicked(){
   checkForWIn(); 
 } //<-- last } in handleWhatWasClicked
 
-// Built separately to easily change
+// Built separately to easily change later
 function checkForWIn() {
   // Creates vars to measure the amount of data in the arrays.
   var lengthOfLastStack = $('[data-stack = "3"]').children().length;
@@ -69,30 +73,64 @@ function checkForWIn() {
 }
 
  // needs to be global so it can be used at anytime. 
- function clearTowers() {
-    // Must clear the counter
-    counter = 0;
-    // Must replace all stacks to their original state.
-    $('[data-stack = "1"]').append($('[data-block = "100"]'));
-    $('[data-stack= "1"]').append($('[data-block = "75"]'));
-    $('[data-stack= "1"]').append($('[data-block = "50"]'));
-    $('[data-stack= "1"]').append($('[data-block = "25"]'));
-    // refresh the Move Counter
-    pushCounter();
-    // Must clear the current piece
-    block = null;
-    // Clear the winner banner
-    $("#announce-game-won").empty();
- } 
+function clearTowers() {
+  // Must clear the counter
+  counter = 0;
+  // Must replace all stacks to their original state.
+  $('[data-stack = "1"]').append($('[data-block = "100"]'));
+  $('[data-stack= "1"]').append($('[data-block = "75"]'));
+  $('[data-stack= "1"]').append($('[data-block = "50"]'));
+  $('[data-stack= "1"]').append($('[data-block = "25"]'));
+  // refresh the Move Counter
+  pushCounter();
+  // Must clear the current piece
+  block = null;
+  // Clear the winner banner
+  $("#announce-game-won").empty();
+} 
 
- //  Must be global because its used by multiple functions to display the current move count.
- function pushCounter(){
-   $('#move-count').text("Total moves " + counter);
- }
+//  Must be global because its used by multiple functions to display the current move count.
+function pushCounter(){
+  $('#move-count').text("Total moves " + counter);
+}
 
- $(document).on('mousemove', function(e){
-    $('#moving-div').offset({
-      left:  e.pageX,
-      top:   e.pageY 
-     });
-  });
+// This function handles the moving piece(floating piece). The #moving-div is a stationary div used to placehold the value of what was picked up
+// this function changes the position of that stationary div using the (x,y) coordinates of the cursor as its placement.
+$(document).on('mousemove', function(e){
+  $('#moving-div').offset({
+    left:  e.pageX,
+    top:   e.pageY 
+    });
+});
+
+// This function get the value of sound held in HTML and plays it.
+function playSound() {
+  var sound = document.getElementById("audio");
+  sound.play()
+}
+
+// being called on line 14!!
+// creates new blocks based on the user's input
+function generateBlocks(){
+  // takes the number and subtracts 4 but will need to subtract 3 when I fix the above code.
+  var numberOfBlocks = ($("#number-Value").val() - 4);
+  // takes the number displays it on the screen. Not necessary and will probably be removed later
+  $("#block-Counter").text("number of Blocks " + numberOfBlocks);
+    // Keeps the blocks within legal limits of the game 
+    if (numberOfBlocks >= 1 && numberOfBlocks <= 4){
+      // counts through the numbers and runs for the length of the number the user put in
+      for (var i = 0; i < (numberOfBlocks + 1); i++){
+        // creates a var that creates a new element on the DOM
+        var newDiv = document.createElement('div');
+        // sets the ID of that new element to token + the current number in the loop
+        newDiv.id = "token" + i;
+        // .appends the new element to the first stack. 
+        $('[data-stack = "1"]').append($(newDiv));
+      }
+    } else{
+        // tells the user their number is outside the legal limits of the game
+        alert("Please input a number between 3 and 8.");
+      }
+}
+
+  // john@digitaldownbeat.com
