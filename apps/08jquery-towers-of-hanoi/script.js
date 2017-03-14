aaafaf4ea90fafb71b30de901a9b77bba53e9b21
jeffgojeff <jeffgojeff@gmail.com>
2017-03-13 22:53:12 -0500

    towers GUI completed w/ comments

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -1,5 +1,43 @@
 'use strict';
 
+var $block = null;
+
+
 $(document).ready(function() {
   // Put app logic here
+  
+
+  $('[data-stack]').click(function () {
+    
+    if($block) {
+      var $lastBlock = $(this).children().last();
+      var $lastBlockValue = $lastBlock.data("block");
+      var $blockToBePlaced = $block.data("block");
+      //define variables for clean code throughout
+
+
+      if($blockToBePlaced < $lastBlockValue || !$lastBlockValue) {
+        //comparing the block that was selected, to the last block.
+        
+        $(this).append($block);
+        $block = null;
+        checkForWin();
+      }
+              
+    } else {
+      $block = $(this).children().last().detach();
+    }
+  
+
+  });
+
+  function checkForWin(){
+      //check for win
+      if( $('[data-stack="2"]').children().length === 4 || $('[data-stack="3"]').children().length === 4 ){
+      //check the 2 divs that are correct for winning if they are equal to 4.
+      $('#announce-game-won').text('you won!');
+      //
+    }
+  }
+
 });
