fc77e64b27a45b5bfce36d2869e07266e11cae19
cruzmndz <cruzalexmendez@gmail.com>
2017-03-15 18:41:11 -0500

    all tests passing
    + updated HTML & CSS for proper stacking.

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var block = null;
+var stack = $('[data-stack]');
 
 $(document).ready(function() {
   $('[data-stack]').click(stackWasClicked);
@@ -8,13 +9,13 @@ $(document).ready(function() {
 
 function stackWasClicked() {
   //IF BLOCK HAS SOMETHING IN IT WE ALREADY PICKED UP A BLOCK. 
-  if (block) {    //ADDED  !== NULL) {
+  if (block !== null) {
     //BLOCK IS PICKED UP, EXECUTE THIS TO PUT IT BACK DOWN SOMEWHERE.
     var lastBlockofStack = $(this).children().last();
     var dataBlockAttrOfLastBlock = lastBlockofStack.data('block');
-    var dataAttrOfRemovedBlock = block.data('block');
+    var removedBlock = block.data('block');
 
-    if (dataBlockAttrOfLastBlock > dataAttrOfRemovedBlock || lastBlockofStack.length === 0) {
+    if (dataBlockAttrOfLastBlock > removedBlock || lastBlockofStack.length === 0) {
       $(this).append(block);
       block = null;
     }
@@ -22,5 +23,12 @@ function stackWasClicked() {
   } 
   else {
     block = $(this).children().last().detach();
+  }
+  checkForWin();
+}
+
+function checkForWin() {
+  if ($('[data-stack=2]').children().length === 4 || $('[data-stack=3]').children().length === 4) {
+    $('.rainbow').text('YOU WON!');
   }
 }
