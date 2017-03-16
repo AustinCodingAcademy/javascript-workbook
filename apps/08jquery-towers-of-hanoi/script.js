f8b264e482709ebc09da291a6b04ed11a7f03685
HippErger <berger.clayton@gmail.com>
2017-03-15 19:54:22 -0500

    trying to stop stacks from stacking on one another when user changes number of blocks

--- apps/08jquery-towers-of-hanoi/script.js
+++ apps/08jquery-towers-of-hanoi/script.js
@@ -2,29 +2,39 @@
 // These variable have to be global for all of the functions to access
 var block = null;
 var counter = 0;
+var toWinValue = 3;
+var blocksToCount = 3;//tried using this to hold the value of what was in the stack before the user tried to change the number of pieces
 
 // this function listens for a call back when all scripts have loaded. Once it get called, and only once, the functions inside can run.
 $(document).ready(function() {
   // Calls the pushCounter to display 0 amount of moves
   pushCounter();
+  pushMinimum();
   // listens for the click on a [data-stack] then runs the function to deal with that click
   $("[data-stack]").click(handleWhatWasClicked);
   $("[data-stack]").click(playSound);
-  $("#input-Submit").click(generateBlocks);
+  $("#input-Submit").click(function() {
+    if(blocksToCount === 3){
+      generateBlocks(); 
+    } else {
+        $('[data-stack= "1"]').empty();
+        clearTowers();
+        generateBlocks();
+      } 
+  });
   // listens for the click on the clear button then runs the function to deal with that click
   $('#clear').click(clearTowers);
 });
 
 // This function takes current vars as arguments to decide what to do with the information.
 function handleWhatWasClicked(){
-  // If there are no pieced picked up we should pick one up with this click
+  // If there are no piece picked up we should pick one up with this click( var block = the user's hand)
   if(block === null){
     // if the stack we clicked on has something in it pick it up. We don't want the user to pick up falsey data
     if($(this).children().length > 0){
       // pick one up.
       block = $(this).children().last().detach(); //instead of .detach() just set he block to be there then detach when we click on the second tower. 
          $("#moving-div").append(block);
-        //  playSound();
       // but if there isn't anything in the stack tell them to choose a different stack 
     } else if($(this).children().length === 0){
         alert("Please choose a stack with pieces to play.");
@@ -42,7 +52,6 @@ function handleWhatWasClicked(){
       if(widthOfTopPiece > widthOfMovingPiece ||  topPieceValue === 0 ){
         // puts the block down
         $(this).append(block);
-        // playSound();
         // resets the block var to null
         block = null;
         // adds mark to the move tally
@@ -59,36 +68,35 @@ function handleWhatWasClicked(){
   checkForWIn(); 
 } //<-- last } in handleWhatWasClicked
 
-// Built separately to easily change later
 function checkForWIn() {
   // Creates vars to measure the amount of data in the arrays.
   var lengthOfLastStack = $('[data-stack = "3"]').children().length;
   var lengthOfSecondStack = $('[data-stack = "2"]').children().length;
 
   // compares the values of the arrays to 4 because we have 4 game pieces.
-  if (lengthOfLastStack === 4 || lengthOfSecondStack === 4) {
+  if (lengthOfLastStack === toWinValue || lengthOfSecondStack === toWinValue) {
     // if either are true we congratulate the winner.
-    $('#announce-game-won').height(75);
+    $('#announce-game-won').height(90);
     $('#announce-game-won').text("You Won!");
   }
 //****Need to end the game and reset after a certain time or find a way to jump out of the loop.**** 
 }
 
- // needs to be global so it can be used at anytime. 
 function clearTowers() {
+  toWinValue = 3;
   // Must clear the counter
   counter = 0;
-  // Must replace all stacks to their original state.
-  $('[data-stack = "1"]').append($('[data-block = "100"]'));
+  // Must replace all stacks to their original states
   $('[data-stack= "1"]').append($('[data-block = "75"]'));
   $('[data-stack= "1"]').append($('[data-block = "50"]'));
-  $('[data-stack= "1"]').append($('[data-block = "25"]'));
+  $('[data-stack= "1"]').append($('[data-block = "25"]')); 
   // refresh the Move Counter
   pushCounter();
   // Must clear the current piece
   block = null;
   // Clear the winner banner
   $("#announce-game-won").empty();
+  pushMinimum();
 } 
 
 //  Must be global because its used by multiple functions to display the current move count.
@@ -105,39 +113,39 @@ $(document).on('mousemove', function(e){
     });
 });
 
-// This function get the value of sound held in HTML and plays it.
+// This function gets the value of sound held in HTML and plays it.
 function playSound() {
   var sound = document.getElementById("audio");
   sound.play()
 }
 
-// being called on line 14!!
 // creates new blocks based on the user's input
 function generateBlocks(){
-  // takes the number and subtracts 4 but will need to subtract 3 when I fix the above code.
-  var numberOfBlocks = ($("#number-Value").val() - 4);
-  // takes the number displays it on the screen. Not necessary and will probably be removed later
-  $("#block-Counter").text("number of Blocks " + numberOfBlocks);
-    // Keeps the blocks within legal limits of the game 
-    if (numberOfBlocks >= 1 && numberOfBlocks <= 4){
-      // counts through the numbers and runs for the length of the number the user put in
-      for (var i = 0; i < (numberOfBlocks + 1); i++){
-        // creates a var that creates a new element on the DOM
-        var newDiv = document.createElement('div');
-        // sets the ID of that new element to token + the current number in the loop
-        newDiv.id = "token" + i;
-        newDiv.data = Number(25 + i);
-        //('[data-block = "100"]'));
-        // .appends the new element to the first stack. 
-        $('[data-stack = "1"]').append($(newDiv));
-      }
-    } else{
-        // tells the user their number is outside the legal limits of the game
-        alert("Please input a number between 3 and 8.");
-      }
+    var numberOfBlocks = ($("#number-Value").val());
+    blocksToCount = numberOfBlocks;
+      // Keeps the blocks within legal limits of the game 
+      if (numberOfBlocks > 3 && numberOfBlocks <= 8){
+        // counts through the numbers and runs for the length of the number the user put in
+        var numberToAdd = (numberOfBlocks - 3);
+        for (var i = 1; i <= numberToAdd; i++){
+          // creates a var that creates a new element on the DOM
+          var newDiv = document.createElement('div');
+          // sets the ID of that new element to token + the current number in the loop
+          newDiv.id = "token" + i;
+          // .appends the new element to the first stack. 
+          $('[data-stack = "1"]').prepend($(newDiv));
+          $('#token' + i).attr("data-block", 100 + i);
+          toWinValue++;
+          pushMinimum();
+        }
+       } else{
+            // tells the user their number is outside the legal limits of the game
+            alert("Please input a number between 4 and 8.");
+          }
+}  // <--- last } in generateBlocks  
+
+// returns the value of the number needed to win and pushes the value to a div.
+function pushMinimum() {
+  $("#block-Counter").text("Min # of moves " + (Math.pow(2, toWinValue) - 1));
 }
-
   // john@digitaldownbeat.com
-  // added this line as a test
-
-  // Line 128, added in the data attribute to each new div. I can use this value to compare to find if the move is legal.
