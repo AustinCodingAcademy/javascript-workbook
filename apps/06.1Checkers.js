7eef137f37dee9d1c07ddb8358b8e1fda46c5d1d
Nick Rodriguez <Nick Rodriguez>
2017-03-18 12:22:40 -0500

    updates

--- apps/06.1Checkers.js
+++ apps/06.1Checkers.js
@@ -7,12 +7,67 @@ var rl = readline.createInterface({
   output: process.stdout
 });
 
-
-function Checker() {
-  // Your code here
+class Checker {
+  constructor(symbol) {
+    this.symbol = (color === 'white') ? String.fromCharCode(0x125CB) : String.fromCharCode(0x125CF);
+//white is a circle.   anything else is a solid circle
+  }
 }
 
+
 function Board() {
+  this.checkers = [];
+  this.createCheckers = function(){
+    var whiteCheckers = [  //hard coded the ONLY eligible positons for the WHITE checkers
+      [0, 1], [0, 3], [0, 5], [0, 7],
+      [1, 0], [1, 2], [1, 4], [1, 6],
+      [2, 1], [2, 3], [2, 5], [2, 7]
+    ];
+
+    var blackCheckers = [
+      [5, 0], [5, 2], [5, 4], [5, 6],
+      [6, 1], [6, 3], [6, 5], [6, 7],
+      [7, 0], [7, 2], [7, 4], [7, 6]
+    ];
+
+    for (var spot of whiteCheckers){
+      //what is spot?   aeach array is in 'spot' ex [0,1]
+      //spot is going to be an array
+      //spot may look like [0,1]
+      var row = spot[0];  //nth array ZERO element
+      var column = spot[1]; //same nth array but 1-th element
+    //row may have a 0 in it
+    //column may have a 1 in it
+      var whiteChecker = new Checker('white');
+      this.grid[row][column] = whiteChecker;  // puts the value of circle or solid in the position
+    }
+
+    for (var spot of blackCheckers){
+      //what is spot?   aeach array is in 'spot' ex [0,1]
+      //spot is going to be an array
+      //spot may look like [0,1]
+      var row = spot[0];  //nth array ZERO element
+      var column = spot[1]; //same nth array but 1-th element
+    //row may have a 0 in it
+    //column may have a 1 in it
+      var blackChecker = new Checker('black');
+      this.grid[row][column].push(blackChecker);   // puts the value of circle or solid in the position
+      //this.checkers.push(this.grid[row][column](whiteChecker));
+    }
+// from our class discussion 3/1/2017
+this.killChecker = function (position){  // in class we discussed that you could pass "row" & "column"
+  var row = position [0];  //get rid of these if you pass row & column
+  var column = position[1]; //get rid of these if you pass row & column
+  var checktoKill = this.selectChecker(row,column);
+  var indexofChecker = this.checker.indexOf(checktoKill);
+  this.checker.splice(indexofChecker,1)
+}
+
+
+
+
+
+  }
   this.grid = [];
   // creates an 8x8 array, filled with null values
   this.createGrid = function() {
@@ -53,14 +108,53 @@ function Board() {
   };
 
   // Your code here
+
+
 }
 function Game() {
 
   this.board = new Board();
 
   this.start = function() {
+    this.board.createCheckers(); //this was backwards in the spec  added in class 3/1/2017
     this.board.createGrid();
     // Your code here
+
+    // this.moveChecker = function(start, end) {
+    //   var checker = this.board.selectChecker(start[0], start[1]);
+    //   this.board.grid[start[0]][start[1]] = null;
+    //   this.board.grid[end[0]][end[1]] = checker;
+    //   if (Math.abs(start[0] - end[0]) === 2) {
+    //     var killPosition = [(parseInt(start[0],10) + parseInt(end[0],10)) / 2 , (parseInt(start[1],10) + parseInt(end[1],10)) / 2];
+    //     this.board.killChecker(killPosition);
+    //   }// end of function for moveChecker
+
+
+// notes from Class
+    this.moveChecker = function (start, end){
+
+      var startColumn = start[1];
+
+      var endRow = end[0];
+      var endColumn = end[1];
+
+      this.board.grid[startRow][startColumn] = null;
+
+      this.board.grid[endRow][endColumn] = null;
+
+      var check = this.selectChecker(startRow, startColumn);  //helper method to get a value instead of making it complex
+
+      var distance = Math.abs(startRow - endRow);
+
+      if(distance ==2){
+        var midRow = startRow + endRow /2;
+        var midColum = startColumn + endColumn /2;
+        this.killChecker([midRow,midColum]);  // no need to use an array see references on line 58
+        //this.killChecker(row, column)
+      }
+
+
+    }
   };
 }
 
--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -2,4 +2,79 @@
 
 $(document).on('ready', function() {
   // Put app logic in here
+  setup();
 });
+
+
+function setup(){
+  mystuff();
+}
+
+function mystuff(){
+
+  // Spec 1
+  // Set a variable playerTurn equal to 'X'.
+  // Using jQuery, set a click listener on all of the [data-cell]s that SETs playerTurn as .text() on $(this) by $(this).text(playerTurn)
+
+  var playerTurn = 'X';
+  $('[data-cell]').click(function(){
+    $(this).text(playerTurn);
+      // var data = $(this).text();
+      // console.log(data + ' here is data');  <---test what is in "this"
+
+    //  console.log(playerTurn)
+    var win =  checkForWin(playerTurn);
+    resetBoard(win);
+
+  //   Spec 2
+  // Also in your click event, toggle playerTurn between 'O' and 'X' using a ternary operator. You should now be able to change turns as you click around on your board.
+    playerTurn = (playerTurn==='X') ? 'O': 'X';  //set playerTurn to X or O
+  }); //end of JQuery function -----------------------
+}// end of mystuff function
+
+//    Reset the board for next game or play
+function resetBoard(){
+  $('#clear').click(function(){
+    var blank = '';
+    var data2 = $('[data-cell]');
+
+    for (var i=0; i<9; i++){
+      data2[i].innerText = '';
+    } //end of for loop
+  }); //end of JQUERY click function -----------------
+} //end of resetBoard
+
+
+function checkForWin(playerTurn){
+  var data2 = $('[data-cell]');  // data object with cell location & value
+//console.log(playerTurn);
+  if (
+//horizontal win
+       data2[0].innerText === playerTurn && data2[1].innerText === playerTurn && data2[2].innerText === playerTurn ||
+       data2[3].innerText === playerTurn && data2[4].innerText === playerTurn && data2[5].innerText === playerTurn ||
+       data2[6].innerText === playerTurn && data2[7].innerText === playerTurn && data2[8].innerText === playerTurn ||
+//vertical win
+      data2[0].innerText === playerTurn && data2[3].innerText === playerTurn && data2[6].innerText === playerTurn ||
+      data2[1].innerText === playerTurn && data2[4].innerText === playerTurn && data2[7].innerText === playerTurn ||
+      data2[2].innerText === playerTurn && data2[5].innerText === playerTurn && data2[8].innerText === playerTurn ||
+//diagonal win
+      data2[2].innerText === playerTurn && data2[4].innerText === playerTurn && data2[6].innerText === playerTurn ||
+      data2[0].innerText === playerTurn && data2[4].innerText === playerTurn && data2[8].innerText === playerTurn ){
+
+    console.log('success!')
+    return true;
+  }//end of horizontal IF
+} //end of checkForWin function
+
+
+//
+/*
+the "$" represents
+  var $ = function () {
+
+}
+*/
+
+// run tests against "test.js" not script.js
+
+// $("[data-cell]")  ----  this grabs attributes
