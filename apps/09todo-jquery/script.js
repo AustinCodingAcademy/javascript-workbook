d7dfe56d015007580e22560eeff1513f0276d13a
HippErger <berger.clayton@gmail.com>
2017-03-18 19:54:24 -0500

    appending texts

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -1,5 +1,17 @@
 'use strict';
 
 $(document).ready(function() {
-  // App logic goes here
+  $("form").submit(function(event){
+    event.preventDefault();
+
+    // assigns the value of the input field to toDoText to be used later
+    var toDoText = $(this).find('#todo').val();
+    //var newItem = document.createElement("li");
+    $('#todo-list').append('<li>' + toDoText + '</li>');
+  })
+
+
+
 });
+
+// Construct a string containing a list item <li></li> with your todoText in the middle. .append() the html string to the end of your #todo-list.
