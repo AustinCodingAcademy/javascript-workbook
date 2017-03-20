f1d29c3da286bf679ef746b0a52e1512f09dd89d
RX7RUSS <russ.savage5@gmail.com>
2017-03-19 21:53:06 -0500

    09 ToDo Started

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -2,4 +2,11 @@
 
 $(document).ready(function() {
   // App logic goes here
+
+  var todoText = $(this).find('input[type="text"]').val();
+
+  $('form').submit(function (event) { // target the form, and put a submit event listener on it. We can capture and pass in the event for us to use
+        event.preventDefault(); // this tells the browser to "prevent" the "default" "event" from occuring (sending and refreshing)
+        console.log( $(this) ); // let's log out our "this" context (our form element) as a jQuery object
+      });
 });
