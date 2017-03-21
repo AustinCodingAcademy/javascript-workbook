51deaafe5953249672a87d3dc2d437c0b933499d
RX7RUSS <russ.savage5@gmail.com>
2017-03-20 19:09:27 -0500

    ToDo Appp Progress

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -3,10 +3,15 @@
 $(document).ready(function() {
   // App logic goes here
 
+  $('form').submit(function (event) {
+  event.preventDefault();
+
   var todoText = $(this).find('input[type="text"]').val();
 
-  $('form').submit(function (event) { // target the form, and put a submit event listener on it. We can capture and pass in the event for us to use
-    event.preventDefault(); // this tells the browser to "prevent" the "default" "event" from occuring (sending and refreshing)
-    console.log( $(this) ); // let's log out our "this" context (our form element) as a jQuery object
+  $('#todo-list').append('<li><input type = "checkbox"/>' + todoText + '<li>');
+
+  $('#todo-list').sortable();
+
   });
+
 });
