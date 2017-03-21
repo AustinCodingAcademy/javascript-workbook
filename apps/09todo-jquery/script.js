b83f22c5f7b3885b6186b718ee6063e934fb0987
RX7RUSS <russ.savage5@gmail.com>
2017-03-21 05:31:55 -0500

    09TODO Complete

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -4,14 +4,15 @@ $(document).ready(function() {
   // App logic goes here
 
   $('form').submit(function (event) {
-  event.preventDefault();
+    event.preventDefault();
+    var todoText = $(this).find('input[type="text"]').val();
+    $('#todo-list').append('<li><input type = "checkbox"/>' + todoText + '<button></button></li>');
+    $('#todo-list').sortable();
 
-  var todoText = $(this).find('input[type="text"]').val();
+  });
 
-  $('#todo-list').append('<li><input type = "checkbox"/>' + todoText + '<li>');
-
-  $('#todo-list').sortable();
-
+  $('body').on('click','button',function(){
+    $(this).parent().remove();
   });
 
 });
--- apps/10address-book-ajax/script.js
+++ apps/10address-book-ajax/script.js
@@ -2,4 +2,11 @@
 
 $(document).ready(function() {
   // You code here
+    $.ajax('https://reqres-api.herokuapp.com/api/users', {
+    success: function(response) {
+      for (var i = 0; i < response.length; i++) {
+        console.log(response[i]['name']);
+      }
+    }
+  });
 });
