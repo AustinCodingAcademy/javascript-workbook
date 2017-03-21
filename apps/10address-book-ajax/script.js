a79dba6eddc0b9fbd157a69a40e41ba223dce8fb
RX7RUSS <russ.savage5@gmail.com>
2017-03-21 05:35:28 -0500

    10Address Book Started

--- apps/10address-book-ajax/script.js
+++ apps/10address-book-ajax/script.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
   // You code here
-    $.ajax('https://reqres-api.herokuapp.com/api/users', {
+  $.ajax('https://reqres-api.herokuapp.com/api/users', {
     success: function(response) {
       for (var i = 0; i < response.length; i++) {
         console.log(response[i]['name']);
