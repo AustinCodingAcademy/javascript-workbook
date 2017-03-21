c21bcada59389b3dc601849ce7cea2222e618f01
saf0911 <saf0911@gmail.com>
2017-03-20 19:30:50 -0500

    todo done

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -2,4 +2,19 @@
 
 $(document).ready(function() {
   // App logic goes here
+  $('form').submit(function(event) {
+    event.preventDefault();
+    var toDo = $(this).find('#todo').val();
+    $('#todo-list').append('<li><input type="checkbox" class="ckbx"/>' + toDo + '<button>Remove</button></li>');
+    $('#todo-list').sortable();
+    $('li').addClass('newLi');
+    $('li').css({'color': 'blue', 'height': '25px', 'width': '250px'});
+    
+  });  
+
+  $('body').on('click', 'button', function()  {
+   $(this).parent().remove();
+
+  });
+   
 });
