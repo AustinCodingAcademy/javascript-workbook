4407c978e9be4ab5d7b7175487b14b8d490215f5
UnimportantWank <Xeromancer@gmail.com>
2017-03-21 17:54:29 -0500

    pass

--- Functional JavaScript NODE school/07Basic-Recursion.js
+++ Functional JavaScript NODE school/07Basic-Recursion.js
@@ -1,7 +1,10 @@
 function reduce(arr, fn, initial) {
   // SOLUTION GOES HERE
-  return function ()
-}, 0)
+  return (function reduceOne(index, value) {
+    if (index > arr.length - 1) return value
+    return reduceOne(index + 1, fn(value, arr[index], index, arr))
+  })(0, initial)
+}
 module.exports = reduce
 
 
