6c334944be5f0f8fd9e1e1f44ba1612dbf9337a7
aleexga <aleexga@gmail.com>
2017-03-21 17:23:27 -0500

    To Do app finished

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -1,5 +1,10 @@
 'use strict';
 
 $(document).ready(function() {
-  // App logic goes here
+  $('form').submit(function (event) { // target the form, and put a submit event listener on it. We can capture and pass in the event for us to use
+    event.preventDefault(); // this tells the browser to "prevent" the "default" "event" from occuring (sending and refreshing)
+    var value = $(this).find('input[type="text"]').val();
+    $('#todo-list').append('<li><input type="checkbox"/>' + value + '</li>');
+    $('#todo-list').sortable();  
+  });
 });
