2b67b11669e89265cf14c1fbf51c418febf6d773
JustinWomack <womack.justin@rocketmail.com>
2017-03-21 23:48:28 -0500

    Todo coplete Ad book wip

--- apps/10address-book-ajax/script.js
+++ apps/10address-book-ajax/script.js
@@ -2,4 +2,38 @@
 
 $(document).ready(function() {
   // You code here
+
+  //Build the initial data.
+  $.ajax('https://reqres-api.herokuapp.com/api/users', {
+    success: function(users) {
+      for (var i = 0; i < users.length; i++) {
+        var firstName = users[i].first_name;
+        var lastName = users[i].last_name;
+        var studentID = users[i].id;
+        var theURL = 'https://reqres-api.herokuapp.com/api/users' + '/' + studentID;
+
+        var theHTML=      
+          '<tr>' +
+            '<td>' + studentID + '</td>' +
+            '<td>' + firstName + '</td>' +
+            '<td>' + lastName + '</td>' +
+            '<td> <a href="' + theURL + '" data-id="' + studentID + '">view</a></td>' +
+          '</tr>'
+
+        $('tbody').append(theHTML)
+      }
+    }
+  });
+
+
+  $('a').click(function (event) { // target the form, and put a submit event listener on it. We can capture and pass in the event for us to use
+      event.preventDefault(); // this tells the browser to "prevent" the "default" "event" from occuring (sending and refreshing)
+      //var theData = $(this).data;
+      //console.log(theData)
+    });
+
+  $('body').on("click", "tr td a", function() {
+    event.preventDefault();
+    });
+
 });
