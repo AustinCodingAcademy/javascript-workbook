a65c46f7b474b4e367d2c21745818321a9a4be13
rbmcintosh <ryanbmcintosh@gmail.com>
2017-03-22 17:09:41 -0500

    Address Book: Both Tests Passing

--- apps/10address-book-ajax/script.js
+++ apps/10address-book-ajax/script.js
@@ -8,6 +8,26 @@ $(document).ready(function() {
         var str = '<tr><td>' + user.id + '</td><td>' + user.first_name + '</td><td>' + user.last_name + '</td><td><a href="#" data-id="' + user.id + '">view</a></td></tr>';
         $('tbody').append(str);
       });
+
+      $('a').on('click', function(event) {
+        event.preventDefault();
+        var url = 'https://reqres-api.herokuapp.com/api/users/';
+        $.ajax(url + $(this).data('id'), {
+          success: function(user) {
+            var $details = $('#details');
+            
+            $details.empty();
+            
+            var str2 = "<h3>" + user.first_name + " " + user.last_name + "</h3>" +
+                       "<h4>" + user.occupation + "</h4>" +
+                       "<p>" + user.phone + "</p>" +
+                       "<p>" + user.address + "</p>" +
+                       "<img src='" + user.avatar + "'>"; 
+
+            $details.append(str2);
+          }
+        });
+      });
     }
   });
 });
