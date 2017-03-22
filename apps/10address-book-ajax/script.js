bacdbec33f1508971d3a3fb3ac0450e373f21096
cruzmndz <cruzalexmendez@gmail.com>
2017-03-21 21:06:35 -0500

    first commit
    spec 1 and 2 done.

--- apps/10address-book-ajax/script.js
+++ apps/10address-book-ajax/script.js
@@ -1,5 +1,20 @@
 'use strict';
 
 $(document).ready(function() {
-  // You code here
+  $.ajax('https://reqres-api.herokuapp.com/api/users', {
+    success: function(userArray) {
+      userArray.forEach(function(users) {
+        var str = "<tr>" + 
+                    "<td>" + users.id + "</td>" + 
+                    "<td>" + users.first_name + "</td>" +
+                    "<td>" + users.last_name + "</td>" +
+                    "<td>" + "<a href='#' data-id='" + users.id + "'>view</a>" + "</td>" +
+                  "</tr>";
+        $('tbody').append(str);
+      })
+    }
+
 });
+
+});
+
