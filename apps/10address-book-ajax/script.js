cf5cb31008e81fe0511fe24afaf50278390663b0
Leslie Robertson <leslievrobertson@gmail.com>
2017-03-23 14:54:12 -0500

    10 Address Book AJAX - zero tests passing, but app definitely works ftw

--- apps/10address-book-ajax/script.js
+++ apps/10address-book-ajax/script.js
@@ -1,5 +1,35 @@
 'use strict';
 
 $(document).ready(function() {
-  // You code here
+
+  var url = 'https://reqres-api.herokuapp.com/api/users/';
+
+  $.ajax(url, {
+    success: function(response) {
+      response.forEach( function(user) {
+
+        var str = '<tr><td>' + user.id + '</td><td>' + user.first_name + '</td><td>' + user.last_name + '</td><td><a data-id="' + user.id + '" href="#">view</a></td></tr>';
+
+        $('tbody').append(str);
+      });
+
+      $('a[data-id]').click(function(event) {
+        event.preventDefault();
+
+        var newUrl = url + $(this).data('id');
+
+        $('#details').empty();
+
+        $.ajax(newUrl, {
+          success: function(user) {
+
+            var details = '<h3>' + user.first_name + ' ' + user.last_name + '</h3><h4>' + user.occupation + '</h4><p>' + user.phone + '</p><p>' + user.address + '</p><img src="' + user.avatar + '"/>';
+
+            $('#details').append(details);
+          }
+        });
+      });
+      $('a[data-id')[0].click();
+    }
+  });
 });
