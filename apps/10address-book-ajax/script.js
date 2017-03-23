a5bf4b082290d2189c32cb001c7e522178963f21
cruzmndz <cruzalexmendez@gmail.com>
2017-03-22 20:20:12 -0500

    final
    -removed .on('click', and moved up function call to use .click(function
    -commented out place holders in html

--- apps/10address-book-ajax/script.js
+++ apps/10address-book-ajax/script.js
@@ -11,10 +11,34 @@ $(document).ready(function() {
                     "<td>" + "<a href='#' data-id='" + users.id + "'>view</a>" + "</td>" +
                   "</tr>";
         $('tbody').append(str);
-      })
+      })  
+      viewUser();  
     }
+  });
+
+  
 
 });
 
-});
+function viewUser() {
+  $('a').click(function() {
+    event.preventDefault();
+    $('#details').empty();
+    var url = 'https://reqres-api.herokuapp.com/api/users/';
+    url = url + $(this).data('id');
+    console.log('user was clicked');
+    $.ajax(url, {
+      success: function(userArray) {
+        var viewStr = "<h3>" + userArray.first_name + "</h3>" +
+                      "<h4>" + userArray.occupation + "</h4>" +
+                      "<p>" + userArray.phone + "</p>" +
+                      "<p>" + userArray.address + "</p>" +
+                      "<img src='" + userArray.avatar + "'>";
+        $('#details').append(viewStr);
+      }
+    });
+  });
+}
 
+
+ 
