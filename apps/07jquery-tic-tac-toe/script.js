9d7e39585989d05f8102f2dd6b9e5ed545b9975f
Arnold73 <acpwells@gmail.com>
2017-03-24 10:22:46 -0500

    tic tac toe

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -5,6 +5,12 @@ var playerTurn = 'X'
 $(document).on('ready', function() {
   $(['data-cell']).click(function() {
     $(this).text(playerTurn);
+    if (checkForWin()){
+		     $('#announce-winner').text('Player ' + playerTurn + ' Wins!');
+		     }
     playerTurn = (playerTurn === 'X') ? 'O' : 'X';
   })
+  $('#clear').click(function(){
+		     $('[data-cell]').text('');
+		   });
 });
