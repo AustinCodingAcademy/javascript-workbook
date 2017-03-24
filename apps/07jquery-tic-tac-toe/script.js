6ab1a29eb6de19fd12849f0c3ace4920cbe9c00e
Arnold73 <acpwells@gmail.com>
2017-03-24 11:06:11 -0500

    tic tac toe final

--- apps/07jquery-tic-tac-toe/script.js
+++ apps/07jquery-tic-tac-toe/script.js
@@ -1,16 +1,77 @@
 'use strict';
 
-var playerTurn = 'X'
+var playerTurn = 'X' //Setting the variable from spec 1. 
 
 $(document).on('ready', function() {
-  $(['data-cell']).click(function() {
+
+  $('[data-cell]').click(function() { //click listener from spec one.
     $(this).text(playerTurn);
-    if (checkForWin()){
+
+
+    if (checkForWin()){//This checks for the win and announces the winner if the criteria are met.
 		     $('#announce-winner').text('Player ' + playerTurn + ' Wins!');
-		     }
-    playerTurn = (playerTurn === 'X') ? 'O' : 'X';
+        }
+        
+    playerTurn = (playerTurn === 'X') ? 'O' : 'X'; //this flips the playerTurn from X to O depending on the last click.
   })
+
   $('#clear').click(function(){
 		     $('[data-cell]').text('');
 		   });
+
 });
+		 function checkForWin() {
+       //horizontal wins
+       
+       if ($("[data-cell='0']").text() === playerTurn &&
+           $("[data-cell='1']").text() === playerTurn &&
+           $("[data-cell='2']").text() === playerTurn) {
+            return true;
+        }
+
+      if ($("[data-cell='3']").text() === playerTurn &&
+          $("[data-cell='4']").text() === playerTurn &&
+          $("[data-cell='5']").text() === playerTurn) {
+            return true;
+          }
+
+      if ($("[data-cell='6']").text() === playerTurn &&
+          $("[data-cell='7']").text() === playerTurn &&
+          $("[data-cell='8']").text() === playerTurn) {
+            return true;
+          }
+
+      // vertical wins
+
+      if ($("[data-cell='0']").text() === playerTurn &&
+          $("[data-cell='3']").text() === playerTurn &&
+          $("[data-cell='6']").text() === playerTurn) {
+            return true;
+          }
+
+      if ($("[data-cell='1']").text() === playerTurn &&
+          $("[data-cell='4']").text() === playerTurn &&
+          $("[data-cell='7']").text() === playerTurn) {
+            return true;
+          }
+
+      if ($("[data-cell='2']").text() === playerTurn &&
+          $("[data-cell='5']").text() === playerTurn &&
+          $("[data-cell='8']").text() === playerTurn) {
+            return true;
+          }
+
+      // diagonal wins
+
+      if ($("[data-cell='0']").text() === playerTurn &&
+          $("[data-cell='4']").text() === playerTurn &&
+          $("[data-cell='8']").text() === playerTurn) {
+            return true;
+          }
+
+      if ($("[data-cell='2']").text() === playerTurn &&
+          $("[data-cell='4']").text() === playerTurn &&
+          $("[data-cell='6']").text() === playerTurn) {
+            return true;
+          }
+  }
