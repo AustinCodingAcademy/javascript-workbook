2ecac53119e59b28aa6782a47798e7803ef20ae2
Desmond Thomas <d.ishmael.thomas@gmail.com>
2017-03-26 12:46:04 -0500

    11gist-blog/script.js

--- apps/11gist-blog/script.js
+++ apps/11gist-blog/script.js
@@ -1,5 +1,33 @@
 'use strict';
 
 $(document).ready(function() {
-  // You code here
+  $.ajax('http://127.0.0.1:8080/apps/11gist-blog/api/gists.json', {
+    success: function(posts) {
+      posts.forEach(function(post) {
+        if (post.description.startsWith('#post')) {
+          var strPostDescription = post.description.slice(5);
+          var $post = $('<li><a href="#" data-url="' + post.url + '" data-comments="' + post.comments_url + '">' + strPostDescription + '</a></li>');
+          $('#posts').append($post);
+        };
+      })
+      $('body').on('click', 'a', function(event) {
+        event.preventDefault();
+        $.ajax($(this).data('url'), {
+          success: function(post) {
+            var postContent = post.files['post.md'].content;
+            $('#post').empty().append(marked(postContent))
+          }
+        });
+        $.ajax($(this).data('comments'), {
+          success: function(comments) {
+            $('#comments').empty();
+            comments.forEach(function(comment) {
+              var $comment = $('<li>' + comment.user.login + ' ' + comment.body + '</li>');
+              $('#comments').append($comment);
+            })
+          }
+        })
+      });
+    }
+  });
 });
