8ca64cb1ca3c40d9e192b87af02853114d26ca0e
MicahMonfrini <ffrock905@gmail.com>
2017-03-25 20:23:37 -0500

    Final: Spec 1.2 complete

--- apps/11gist-blog/script.js
+++ apps/11gist-blog/script.js
@@ -5,11 +5,20 @@ var getDescription = {
   success: function(gists) {
     // loop through each gist
     gists.forEach(function(gist) {
-      // variable containing the description of each gist
-      var links = 
-      ('<li>' + '<a href="#">' + gist.description + '</a>' + '</li>');
-      // add item in "links" variable to the "ul" with the id "posts"
-      $('#posts').append(links);
+      // variable for the description of each gist
+      var gistDesc = gist.description;
+      // variable containing the string "#post" in the gistDesc
+      var postStr = gistDesc.substring(0, 5);
+      // if the description begins with the string "#post"...
+      if (postStr === "#post") {
+        // ...remove the "#post" from the string...
+        gistDesc = gistDesc.replace("#post", "");
+        // (variable containing the description of each gist)
+        var links = 
+        ('<li>' + '<a href="#">' + gistDesc + '</a>' + '</li>');
+        // ...and add item in "links" variable to the ul with the id "posts"
+        $('#posts').append(links);
+      }
     });
   }
 }
