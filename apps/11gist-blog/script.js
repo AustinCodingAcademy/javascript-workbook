44f25e929cd55577753558759323f62514600eac
MicahMonfrini <ffrock905@gmail.com>
2017-03-25 19:07:45 -0500

    Final: Spec 1.1 complete

--- apps/11gist-blog/script.js
+++ apps/11gist-blog/script.js
@@ -1,5 +1,21 @@
 'use strict';
 
+// callback to get the description of each gist
+var getDescription = {
+  success: function(gists) {
+    // loop through each gist
+    gists.forEach(function(gist) {
+      // variable containing the description of each gist
+      var links = 
+      ('<li>' + '<a href="#">' + gist.description + '</a>' + '</li>');
+      // add item in "links" variable to the "ul" with the id "posts"
+      $('#posts').append(links);
+    });
+  }
+}
+
+
 $(document).ready(function() {
-  // You code here
+  // ajax call to generate a list of the descriptions in each gist
+  $.ajax("http://127.0.0.1:8080/apps/11gist-blog/api/gists.json", getDescription)
 });
