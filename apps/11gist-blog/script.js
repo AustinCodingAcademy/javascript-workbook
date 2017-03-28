9c0c16668c137edc55087afe5c625cbbdd5ccc63
RX7RUSS <russ.savage5@gmail.com>
2017-03-27 20:26:04 -0500

    11gist-blog started

--- apps/11gist-blog/script.js
+++ apps/11gist-blog/script.js
@@ -2,4 +2,10 @@
 
 $(document).ready(function() {
   // You code here
-});
+  $.ajax('http://127.0.0.1:8080/apps/11gist-blog/api/gists.json', {
+    success: function (#posts) {
+      posts.forEach(function ('description') {
+      })
+    }
+  }) 
+})
