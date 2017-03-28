02d02907fc33eb0af58d014879b09935fa856b8d
RX7RUSS <russ.savage5@gmail.com>
2017-03-27 19:38:12 -0500

    10address-book changes still failing one test

--- apps/10address-book-ajax/script.js
+++ apps/10address-book-ajax/script.js
@@ -1,7 +1,5 @@
 'use strict';
 
-'use strict';
-
 $(document).ready(function () {
   $.ajax('https://reqres-api.herokuapp.com/api/users', {
     success: function (users) {
@@ -14,22 +12,23 @@ $(document).ready(function () {
           <td><a href='#' data-id=${user.id}>view</a></td>
         </tr>`;
         $('tbody').append(str);
-        $('tbody a').on('click', function (link) {
-          link.preventDefault();
-          var url = 'https://reqres-api.herokuapp.com/api/users/' + $(this).data('id');
-          console.log(url);
-          $.ajax(url, {
-            success: function (user) {
-              var str2 =
-                `<h3>${user.first_name}</h3>
+
+      })
+      $('tbody a').on('click', function (link) {
+        link.preventDefault();
+        var URL = 'https://reqres-api.herokuapp.com/api/users/' + $(this).data('id');
+        console.log(URL);
+        $.ajax(URL, {
+          success: function (user) {
+            var str2 =
+              `<h3>${user.first_name}</h3>
                 <h4>${user.last_name}</h4>
                 <p>${user.occupation}</p>
                 <p>${user.address}</p>
                 <img src='${user.avatar}'>`;
-              console.log(str2);
-              $('#details').html(str2);
-            }
-          })
+            console.log(str2);
+            $('#details').html(str2);
+          }
         })
       })
     }
