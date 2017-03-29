b78d0cbcf64331e52e3f1056d90cf245c1ae5ea5
RX7RUSS <russ.savage5@gmail.com>
2017-03-29 15:00:08 -0500

    GIST Blog with no css formatting

--- apps/11gist-blog/script.js
+++ apps/11gist-blog/script.js
@@ -1,11 +1,58 @@
 'use strict';
 
-$(document).ready(function() {
-  // You code here
-  $.ajax('http://127.0.0.1:8080/apps/11gist-blog/api/gists.json', {
-    success: function (#posts) {
-      posts.forEach(function ('description') {
-      })
+var gistDescription = {
+  success: function(gists) {
+    gists.forEach(function(gist) {
+      var gistDesc = gist.description;
+      // .substring property to find "#post" in the gistDesc
+      var postStr = gistDesc.substring(0, 5);
+      if (postStr === "#post") {
+        // .replace property to remove "#post"
+        gistDesc = gistDesc.replace("#post", "");
+        var links = 
+        ('<li>' + '<a href="#" data-url="' + gist.url + '">' + gistDesc + '</a>' + '</li>');
+        $('#posts').append(links);
+      };
+    });
+  }
+};
+
+// callback to get the content of the gist that was clicked
+var gistContent = {
+  success: function(gist) {
+    var content = gist.files['post.md']['content'];
+    var commentURL = gist["comments_url"];
+    var str = 
+    ('<div id="post">' +
+    '<p>' + marked(content) + '</p>' +
+    '</div>')
+    $('#post').replaceWith(str);
+    var getComments = {
+      success: function(comments) {
+        comments.forEach(function(comment) {
+          var userName = comment["user"]["login"];
+          var userComment = comment["body"];
+          var str = 
+          ('<ul id="comments">' + 
+          '<li>' + userName + '</li>' +
+          '<li>' + userComment + '</li>' +
+          '</ul>')
+          $('#comments').html(str);
+        });
+      }
     }
-  }) 
-})
+    $.ajax(commentURL, getComments);
+  }
+};
+
+
+$(document).ready(function() {
+  // first ajax call
+  $.ajax("http://127.0.0.1:8080/apps/11gist-blog/api/gists.json", gistDescription)
+  $('body').on('click', 'a', function(event) {
+    event.preventDefault();
+    // variable to store the url in the data attribute
+    var gistURL = $(this).data('url');
+    $.ajax(gistURL, gistContent);
+  });
+});
