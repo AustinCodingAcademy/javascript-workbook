32133fd2155fd30d403670b33903644fc818c5a1
Leslie Robertson <leslievrobertson@gmail.com>
2017-03-30 14:52:21 -0500

    11 Gist Blog - commit with current comments and issues, for record-keeping; still need to fix comments not loading and duplicate post view glitches

--- apps/11gist-blog/script.js
+++ apps/11gist-blog/script.js
@@ -1,46 +1,65 @@
 'use strict';
 
 $(document).ready(function() {
-  var pathToGistBlog = 'http://127.0.0.1:8000/apps/11gist-blog/api/gists.json';
+  // var url = 'https://api.github.com/users/roberl54/gists';
+  var url = 'http://127.0.0.1:8080/apps/11gist-blog/api/gists.json'
   var $posts = $('#posts');
   var $post = $('#post');
+  // var postUrl = post.url;
+  var $commentsList = $('#comments')
 
-  $.ajax(pathToGistBlog, {
+
+  // three total AJAX calls: list of gists, the particular gist you click on, and then the comments
+
+
+  // First AJAX call, generating the list of gists/posts
+  $.ajax(url, {
     success: function(response) {
       response.forEach(function(post) {
         if (post.description.startsWith('#post')) {
-          var $hashTag = '#post ';
           var postTitle = post.description.substring(6);
-          // var postContent = post.files[post.md];
+          var $postDeets = $('<li><a href="#" data-url="' + post.url + '" data-comments="' + post.comments_url + '">' + postTitle + '</a></li>');
 
-          // var postListData = {
-          //   url: post.url,
-          //   title: postTitle
-          // };
+          $posts.append($postDeets);
+        };
 
-          $posts.append(`<li>${postTitle}</li>`);
-          // $post.append(`<li>${postContent}</li>`);
-        }
-      });
+      })
+
+      var $title = $('title').text();
+      var newTitleDiv = $('<div id="site_title" class="sm-col-4">' + $title + '</div>')
+      $posts.before(newTitleDiv);
+      $posts.wrap('<aside class="small-12 medium-3"></aside>');
+      $post.addClass('medium-9');
+
+
+      // or do I want this: $('a').data('url').click(function(event)) ???
+      // placed outside of the forEach loop so it only runs once per click
+      $('a').click(function(event) {
+        event.preventDefault();
+
+        // var postUrl = $(this).data('url')  ??
+
+        // Second AJAX call, expanding the gist that is clicked
+        // need to get postUrl to actually mean something...
+        $.ajax($(this).data('url'), {
+          success: function(response) {
+            var content = response.files['post.md'].content;
+            $post.append(marked(content))
+
+            //Now put the 3rd AJAX call here to add in any comments
+            $.ajax($(this).data('comments'), {
+              success: function(comments) {
+                comments.forEach(function(comment) {
+                  var $commentDeets = $('<li>' + comment.user.login + '</li><li>' + comment.body + '</li>');
+                  $commentsList.append($commentDeets);
+                })
+              }
+            }); // close 3rd AJAX
+
+          }
+        }); // close 2nd AJAX
+
+      }); // close click event listener
     }
-  });
-
-
-  // $.ajax(pathToGistBlog, {
-  //   success: function(response) {
-  //     // console.log(response.created_at);
-  //     // console.log(response.user);
-  //     response.forEach(function(post){
-  //           console.log(post.description);
-  //           $('#post').append(post.description);
-  //         })
-  //   }
-  // });
-
-  // $("#posts").text("new heading");
-
+  }); // close 1st AJAX
 });
-
-// response.forEach(function(user){
-//       console.log(user.name);
-//     })
