961939fe4e96b9c1ada23e9333b911ba8f0845ca
Dan Putnam <daniel.a.putnam@gmail.com>
2017-04-17 20:43:56 -0500

    lint files

--- 01week/javascripting/if-statement.js
+++ 01week/javascripting/if-statement.js
@@ -3,6 +3,6 @@ var fruit = 'orange';
 if(fruit.length > 5){
   console.log('The fruit name has more than five characters.');
 }
- else {
-   console.log('The fruit name has five characters or less.');
- }
+else {
+  console.log('The fruit name has five characters or less.');
+}
