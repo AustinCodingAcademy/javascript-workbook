e8736ecda4c937e887753d9e6079c3042e667e5f
Maggie <mymonstermaggie@Maggies-MacBook-Pro.local>
2017-04-19 00:01:45 -0500

    fix errors2

--- 01week/javascripting/scope.js
+++ 01week/javascripting/scope.js
@@ -7,13 +7,12 @@ var a = 1, b = 2, c = 3;
     var b = 8;
     console.log("a: "+a+", b: "+b+", c: "+c);
 
-      (function thirdFunction(){
-        var a = 7, c = 9;
+    (function thirdFunction(){
+      var a = 7, c = 9;
 
-        (function fourthFunction(){
-          var a = 1, c = 8;
-
-        })();
+      (function fourthFunction(){
+        var a = 1, c = 8;
       })();
     })();
+  })();
 })();
