9386d4109f4c369c693b06e125129116eaf9dafd
Ericka Emery <ericka.emery@gmail.com>
2017-04-19 18:44:16 -0500

    updated this file to show alternative exception

--- 01week/javascripting/looping-through-arrays.js
+++ 01week/javascripting/looping-through-arrays.js
@@ -1,5 +1,6 @@
 var pets = ['cat', 'dog', 'rat'];
-for (var i = 0; i < 3; i++) {
+// for (var i = 0; i < pets.; i++) {
+for (var i = 0; i < pets.length; i++) {
   pets [i] = pets[i] + 's';
 }
 console.log(pets);
