7feaf2dfd4a6b2b930d88ac4f99edd6f738f3efd
Eric Garcia <egarcia1868@gmail.com>
2017-04-19 20:33:50 -0500

    my latest rock paper scissors

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -7,33 +7,41 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+var hand2 = Math.random();
+console.log(hand2)
+if (hand2 < .34) {
+    hand2 = 'rock'
+    console.log(hand2)
+  } else if (hand2 > .66) {
+    hand2 = 'scissors'
+    console.log(hand2)
+  } else {
+    hand2 = 'paper'
+    console.log(hand2)
+  }
 
 function rockPaperScissors(hand1, hand2) {
-  // Write code here
-  if ((hand1 === 'rock' && hand2 === 'rock') || (hand1 === 'scissors' && hand2
+  if (hand1 !== 'rock' && hand1 !== 'paper' && hand1 !== 'scissors') {
+    // Had to use && ^here^ instead of ||
+  return 'Invalid entry.  Please enter rock, paper, or scissors.';
+} else if ((hand1 === 'rock' && hand2 === 'rock') || (hand1 === 'scissors' && hand2
   === 'scissors') || (hand1 === 'paper' && hand2 === 'paper')) {
     return 'It\'s a tie!'
   } else if ((hand1 === 'rock' && hand2 === 'scissors') || (hand1 === 'paper' &&
   hand2 === 'rock') || (hand1 === 'scissors' && hand2 === 'paper')) {
-    return
+    return 'Hand one wins!'
+  } else if ((hand1 === 'rock' && hand2 === 'paper') || (hand1 === 'paper' && hand2 === 'scissors') || (hand1 === 'scissors' && hand2 === 'rock'))
+    return 'Hand two wins!'
   }
-  }
-  //   if (hand2 === 'rock') {
-  //   return "It's a tie";
-  //   } else if (hand2 === 'scissors') {
-  //   return "Hand one wins!"
-  //   } else if (hand2 === 'paper') {
-  //   return "Hand two wins!"
-  //   }
-  // } else if (hand1 === '')
+
 
 
 function getPrompt() {
   rl.question('hand1: ', (answer1) => {
-    rl.question('hand2: ', (answer2) => {
-      console.log( rockPaperScissors(answer1, answer2) );
+    // rl.question('hand2: ', (answer2) => {
+      console.log( rockPaperScissors(answer1, hand2) );
       getPrompt();
-    });
+    // });
   });
 }
 
