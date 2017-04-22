14368f5e8096ae60d53022fe217b5f04082da746
meganroone <emailgena@gmail.com>
2017-04-22 17:20:08 -0500

    corrections to indentations

---
+++ 01week/javascripting/anotherRSP.js
@@ -0,0 +1,41 @@
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+  computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+  computerChoice = "paper";
+} else {
+  computerChoice = "scissors";
+} console.log("Computer: " + computerChoice);
+
+
+var compare = function (choice1, choice2) {
+  if (choice1 === choice2) {
+    return"The result is a tie!";
+  }
+  if(choice1 === "rock") {
+    if(choice2 === "scissors"){
+      return "rock wins";
+    }
+    else {
+      return "paper wins!";
+    }
+  }
+  if(choice1 === "paper") {
+    if(choice2 === "rock"){
+      return "paper wins";
+    }
+    else {
+      return "scissors wins";
+    }
+  }
+  if(choice1 === "scissors") {
+    if(choice2 === "rock"){
+      return "rock wins";
+    }
+    else {
+      return "scissors wins";
+    }
+  }
+};
+compare(userChoice,computerChoice);
