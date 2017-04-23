ff9ba855981b7f8b618d2f7cc3b4f7655409a458
lgrozas6 <lauragracep@live.com>
2017-04-23 17:35:23 -0500

    Rock Paper Scissors function

--- 01week/javascripting/looping-through-arrays.js
+++ 01week/javascripting/looping-through-arrays.js
@@ -1,7 +1,7 @@
 var pets = ['cat', 'dog', 'rat'];
 var length = pets.length;
 
-for (i = 0; i < length; i++) {
+for (i = 0; i < pets.length; i++) {
         pets[i] = pets[i] + 's';
 }
 
--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -10,7 +10,36 @@ const rl = readline.createInterface({
 
 function rockPaperScissors(hand1, hand2) {
 
-  // Write code here
+  //hand1 winners
+   if(hand1 == 'rock' && hand2 == 'scissors'){
+     return = "Hand one wins!";
+   }
+   else if(hand1 == 'paper' && hand2 == 'rock'){
+     return = "Hand one wins!";
+   }
+   else if(hand1 == 'scissors' && hand2 == 'paper'){
+     return = "Hand one wins!";
+   }
+    //hand2 winners
+   else if(hand1 == 'rock' && hand2 == 'paper'){
+     return = "Hand two wins!";
+   }
+   else if(hand1 == 'paper' && hand2 == 'scissors'){
+     return = "Hand two wins!";
+   }
+   else if(hand1 == 'scissors' && hand2 == 'rock'){
+     return = "Hand two wins!";
+   }
+  } //hand1 and hand2 ties
+  else if(hand1 === 'rock' && hand2 === 'rock'){
+    return "It's a tie!";
+  }
+  else if(hand1 === 'paper' && hand2 === 'paper'){
+    return "It's a tie!";
+  }
+  else if(hand1 === 'scissors' && hand2 === 'scissors'){
+    return "It's a tie!";
+  }
 
 }
 
