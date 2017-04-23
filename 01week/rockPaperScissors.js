ee5eb442944f1f6f427f232e80f2882036b7d68c
stackbaxter <stackbaxter@gmail.com>
2017-04-23 16:05:51 -0500

    this may do it!

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -40,25 +40,25 @@
   // Write code here
     if(hand1 === hand2){
       return "It's a tie!";
-  }
+    }
     else if (hand1 ==='rock') {
-    return (hand2 ==='scissors') ? ('Hand one wins!') : ('Hand two wins!')
-  }
+      return (hand2 ==='scissors') ? ('Hand one wins!') : ('Hand two wins!')
+    }
     else if (hand1 === 'paper') {
-    return (hand2 === 'rock') ? 'Hand one wins!' : 'Hand two wins!';
-  }
+      return (hand2 === 'rock') ? 'Hand one wins!' : 'Hand two wins!';
+    }
     else if (hand1 === 'scissors') {
-    return (hand2 === 'paper') ? 'Hand one wins!': 'Hand two wins!';
-  };
+      return (hand2 === 'paper') ? 'Hand one wins!': 'Hand two wins!';
+    };
   }
 
   function getPrompt() {
     rl.question('hand1: ', (answer1) => {
-    rl.question('hand2: ', (answer2) => {
+      rl.question('hand2: ', (answer2) => {
       console.log( rockPaperScissors(answer1, answer2) );
       getPrompt();
     });
-  });
+    });
   }
 
 // Tests
@@ -66,17 +66,17 @@
   if (typeof describe === 'function') {
 
     describe('#rockPaperScissors()', () => {
-    it('should detect a tie', () => {
+      it('should detect a tie', () => {
       assert.equal(rockPaperScissors('rock', 'rock'), "It's a tie!");
       assert.equal(rockPaperScissors('paper', 'paper'), "It's a tie!");
       assert.equal(rockPaperScissors('scissors', 'scissors'), "It's a tie!");
     });
-    it('should detect which hand won', () => {
+      it('should detect which hand won', () => {
       assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
       assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
       assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
     });
-  });
+    });
   }
   else {
 
