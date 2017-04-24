cba7ee655d7c70a35ba66720da2732212ddb8300
stackbaxter <stackbaxter@gmail.com>
2017-04-24 12:44:04 -0500

    working?

--- 01week/javascripting/testdummy.js
+++ 01week/javascripting/testdummy.js
@@ -1,7 +1,12 @@
 // <script>//html
 'use strict';
 
-//attempting to have terminal interface
+// let robot = new Object();
+//   robot.name = "ivan";
+//   robot.wins = 0;
+//   robot.win = function(){
+//     this.wins +=1;
+//   };
 const assert = require('assert');
 const readline = require('readline');
 const rl = readline.createInterface({
@@ -13,6 +18,7 @@ const rl = readline.createInterface({
 function robot(name){
   this.name = name;
   this.wins = 0;
+  // this.choice;
   this.win = function(){
     this.wins +=1;
   }
@@ -31,74 +37,71 @@ function robot(name){
   };
 };
 
-// let rob1 = new robot();
-// let rob2 = new robot();
-
-/*
-Exception: SyntaxError: redeclaration of let rob1
-@Scratchpad/1:1:1
-*/
+let rob1 = new robot();
+let rob2 = new robot ();
 
 // rob1.choice=turn();
 // rob2.choice=turn();
 
-function rockPaperScissors(hand1, hand2) {
+function rockPaperScissors(throw1, throw2) {
 
   // Write code here
-  if(hand1 === hand2){
+  if(throw1 === throw2){
     return "It's a tie!";
   }
-  else if (hand1 ==='rock') {
-    return (hand2 ==='scissors') ? (rob1) : (rob2);
+  else if (throw1 ==='rock') {
+    return (throw2 ==='scissors') ? (rob1) : (rob2);
     // return (throw2 ==='scissors') ? (rob1.name +' wins!')) : (rob2.name +' wins!'), (rob2.win())
   }
-  else if (hand1 === 'paper') {
-    return(hand2 === 'rock') ? (rob1) : (rob2);
+  else if (throw1 === 'paper') {
+    return(throw2 === 'rock') ? (rob1) : (rob2);
     // return (throw2 === 'rock') ? rob1.name +' wins!' : (rob2.name +' wins!'),(rob2.win());
   }
-  else if (hand1 === 'scissors') {
-    return(hand2 === 'paper') ? (rob1) : (rob2);
+  else if (throw1 === 'scissors') {
+    return(throw2 === 'paper') ? (rob1) : (rob2);
     // return (throw2 === 'paper') ? rob1.name + ' wins!': rob2.name +' wins!';
   };
 }
-//Game logic
+//just need to button this up in a for loop and write a declare winner function
 function game(){
   let winner = new Object();
   winner = (rockPaperScissors(rob1.hand(), rob2.hand()));
 
   if(winner != "It's a tie!"){
     winner.win();
-    console.log(winner.name +' won the game and has '+ winner.wins +' wins!'/n);
+    console.log(winner.name, winner.wins);
   }
 
   else{
     console.log("IT's a tie!");
   };
 };
-
+// let turns = 10;
+// for(var i = 0; i < turns; i++){
+//   console.log(game());
+// }
+// Announce Winner
 // announceWinner(rob1, rob2);
 function announceWinner(rob1, rob2){
   if(rob1.wins > rob2.wins){
     console.log(rob1.name +' is the Champion!');
   }
   else if(rob1.wins < rob2.wins){
-    console.log(rob2.name + ' is the Champion!)')
+    console.log(rob2.name + ' is the Champion!')
   }
   else if(rob1.wins === rob2.wins) {
     console.log('Both robots are evenly matched!');
   }
 }
 
-//changing the questions to robot names and number of games
 function getInfo() {
   rl.question('Robot 1 name: ', (name1) => {
     rl.question('Robot 2 name: ', (name2) => {
       rl.question('Number of games: ', (gameNum) => {
-
-        let rob1 = new robot(name1);
-        let rob2 = new robot(name2);
+        rob1.name = (name1);
+        rob2.name = (name2);
         let turns = gameNum;
-        for(var i = 0; i < 5; i++){
+        for(var i = 0; i < turns; i++){
           game(rob1, rob2);
         }
         announceWinner(rob1, rob2);
@@ -106,5 +109,4 @@ function getInfo() {
     });
   });
 })};
-
- getInfo();
+getInfo();
