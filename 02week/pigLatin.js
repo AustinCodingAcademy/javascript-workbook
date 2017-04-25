3c0bdcffc3bb54f7a66629eb60f6e1a5b73a2dae
Ericka Emery <ericka.emery@gmail.com>
2017-04-24 20:59:58 -0500

    passed all tests before lowercase

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -10,8 +10,18 @@ const rl = readline.createInterface({
 
 function pigLatin(word) {
 
-  // Your code here
-
+  // Your code here --- We are trying to determine how we can successfully utilize
+  // the .toLowercase function right after input
+let vowel = ['a', 'e', 'i', 'o', 'u'];
+  if (vowel.includes(word[0].toLowerCase())) {
+    return(word + 'yay');
+  } else {
+    for (var i = 0; i < word.length -1; i++) {
+      if (vowel.includes(word[i].toLowerCase())) {
+        return (word.substring(i) + word.substring(0,i) + 'ay');
+      }
+    }
+  }
 }
 
 
