5e261a3b62b747bc5e151517e9e3096222483623
meganroone <emailgena@gmail.com>
2017-04-26 17:56:01 -0500

    adding pigLatin

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -7,23 +7,35 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-
 function pigLatin(word) {
+// Your test code here
+  word = prompt('Enter a word to be Pig Latined!');
+  // Variables to be used
+  var pigLatined = '';
+  var vowel = /[aeiouy]/gi;
+  // Checking if the first character is a vowel
+  if (word[0].match(vowel)) {
+    pigLatined = word + 'yay';
 
-  // Your code here
-
+  } else {
+    // Finding how many consonants occur before the first vowel
+    var vowelIndice = word.indexOf(word.match(vowel)[0]);
+    // Take the string from the first vowel to the last character
+    // then add the consonants that were previously omitted and add the ending.
+    pigLatined = word.substr(vowelIndice) + word.substr(0, vowelIndice) + 'ay';
+  }
+  return pigLatined;
 }
+pigLatin();
 
-
-function getPrompt() {
-  rl.question('word ', (answer) => {
-    console.log( pigLatin(answer) );
-    getPrompt();
-  });
-}
+// function getPrompt() {
+//   rl.question('word ', (answer) => {
+//     console.log( pigLatin(answer) );
+//     getPrompt();
+//   });
+// }
 
 // Tests
-
 if (typeof describe === 'function') {
 
   describe('#pigLatin()', () => {
