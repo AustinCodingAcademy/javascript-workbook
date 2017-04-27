d516f85ead0f07a01a0a15f5dc1383d9d01bd20d
Greg Swan <greg.c.swan@gmail.com>
2017-04-27 13:25:41 -0500

    passing all tests in tic tac toe

--- 02week/ticTacToe.js
+++ 02week/ticTacToe.js
@@ -24,23 +24,63 @@ function printBoard() {
 }
 
 function horizontalWin() {
-  // Your code here
+  if ((board[0][0] === playerTurn) && (board[0][1] === playerTurn) && (board[0][2] === playerTurn)) {
+    return true;
+  }
+  else if ((board[1][0] === playerTurn) && (board[1][1] === playerTurn) && (board[1][2] === playerTurn)) {
+    return true;
+  }
+  else if ((board[2][0] === playerTurn) && (board[2][1] === playerTurn) && (board[2][2] === playerTurn)) {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
 
 function verticalWin() {
-  // Your code here
+  if ((board[0][0] === playerTurn) && (board[1][0] === playerTurn) && (board[2][0] === playerTurn)) {
+    return true;
+  }
+  else if ((board[0][1] === playerTurn) && (board[1][1] === playerTurn) && (board[2][1] === playerTurn)) {
+    return true;
+  }
+  else if ((board[0][2] === playerTurn) && (board[1][2] === playerTurn) && (board[2][2] === playerTurn)) {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
 
 function diagonalWin() {
-  // Your code here
+  if ((board[0][0] === playerTurn) && (board[1][1] === playerTurn) && (board[2][2] === playerTurn)) {
+    return true;
+  }
+  else if ((board[0][2] === playerTurn) && (board[1][1] === playerTurn) && (board[2][0] === playerTurn)) {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
 
 function checkForWin() {
-  // Your code here
+  if (horizontalWin() || verticalWin() || diagonalWin()){
+    return true;
+  }
 }
 
+
 function ticTacToe(row, column) {
-  // Your code here
+  board[row][column] = playerTurn;
+  checkForWin();
+  playerTurn =(playerTurn === 'X'?'O':'X');
+
+
 }
 
 function getPrompt() {
@@ -89,5 +129,4 @@ if (typeof describe === 'function') {
 } else {
 
   getPrompt();
-
 }
