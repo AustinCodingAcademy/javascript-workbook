0ed76f0ed2c78681aacb3946b1f857577c58502b
Greg Devany <greg.devany@yahoo.com>
2017-04-27 15:24:07 -0500

    TicTacToe completed but doesn't pass test(?)

--- 02week/ticTacToe.js
+++ 02week/ticTacToe.js
@@ -35,23 +35,21 @@ function horizontalWin() {
 
 function verticalWin() {
   //outer loop check each column
-    for (var i = 0; i < 3; i++) {
+  for (var i = 0; i < 3; i++) {
   //check each row in column
-      const j = 0;
-      if (board[j][i] === ' ') return false;
-      if (board[j][i] === board[j+1][i] && board[j][i] === board[j+2][i]) return true;
-    }
+    const j = 0;
+    if (board[j][i] === ' ') return false;
+    if (board[j][i] === board[j+1][i] && board[j][i] === board[j+2][i]) return true;
+  }
 }
 
 function diagonalWin() {
 // check for diagonal win
-  const i = 0;
-  const j = 0;
-  if (board[i][j] === ' ' || board[i+2][j] === ' ') return false;
-  if ((board[i][j] === board[i+1][j+1] && board[i][j] === board[i+2][j+2]) ||
-    (board[i+2][j] === board[i+1][j+1] && board[i+2][j] === board[i][j+2])) {
-      return true;
-    }
+  if (board[0][0] === ' ' || board[2][0] === ' ') return false;
+  if ((board[0][0] === board[1][1] && board[0][0] === board[2][2]) ||
+    (board[2][0] === board[1][1] && board[2][0] === board[0][2])) {
+    return true;
+  }
 }
 
 function checkForWin() {
