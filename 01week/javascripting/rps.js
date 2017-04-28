0285a813af479c267c078240560d57cc568aec3f
jdrowlett <jd@jdrowlett.com>
2017-04-28 08:07:10 -0500

    rock paper scissors final version of code

--- 01week/javascripting/rps.js
+++ 01week/javascripting/rps.js
@@ -11,25 +11,33 @@ function rockPaperScissors(hand1, hand2) {
 
   // Write code here
 
-  if (hand1 === "paper") {
-      if (hand2 === "rock") {
-          return "paper wins";
-      } else {
-          if (hand2 === "scissors") {
-              return "scissors wins";
-          }
-      }
-      if (hand1 === "scissors") {
-          if (hand2 === "rock") {
-              return "rock wins";
-          } else {
-              if (hand2 === "paper") {
-                  return "scissors wins";
-              }
-          }
-      }
+  if (hand1 === hand2) {
+    return "It's a tie!";
+    }
+  else if (hand1 === 'rock' {
+    if (hand2 === scissors) {
+      return "Hand one wins!";
+    }
+    else {
+      return "Hand two wins!";
+    }
   }
-
+  else if (hand1 === 'paper') {
+    if (hand2 === 'rock') {
+      return "Hand one wins!";
+    }
+    else {
+      return "Hand two wins!";
+    }
+  }
+  else if (hand2 === 'scissors') {
+    if (hand2 === 'paper') {
+      return "Hand one wins!";
+    }
+    else {
+      return "Hand two wins!";
+    }
+  }
 }
 
 function getPrompt() {
