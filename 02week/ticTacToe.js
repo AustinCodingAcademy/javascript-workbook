0fefe1935ee13a37e62b9e5ce7037166f7cbfb66
Andrew Hartford <Andrew Hartford>
2017-04-27 19:25:12 -0500

    tictactoe progress

--- 02week/ticTacToe.js
+++ 02week/ticTacToe.js
@@ -13,6 +13,7 @@ let board = [
 ];
 
 let playerTurn = 'X';
+playerTurn = (playerTurn === "X" ? "O" : "X");
 
 function printBoard() {
   console.log('   0  1  2');
@@ -24,23 +25,51 @@ function printBoard() {
 }
 
 function horizontalWin() {
-  // Your code here
+  if((board[0, 0] === playerTurn) && (board[0, 1] === playerTurn) && (board[0, 2] === playerTurn)) {
+    return true;
+  }
+  if((board[1, 0] === playerTurn) && (board[1, 1] === playerTurn) && (board[1, 2] === playerTurn)) {
+    return true;
+  }
+  if((board[2, 0] === playerTurn) && (board[2, 1] === playerTurn) && (board[2, 2] === playerTurn)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function verticalWin() {
-  // Your code here
+  if((board[0, 0] === playerTurn) && (board[1, 0] === playerTurn) && (board[2, 0] === playerTurn)) {
+    return true;
+  }
+  if((board[0, 1] === playerTurn) && (board[1, 1] === playerTurn) && (board[2, 1] === playerTurn)) {
+    return true;
+  }
+  if((board[0, 2] === playerTurn) && (board[1, 2] === playerTurn) && (board[2, 2] === playerTurn)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function diagonalWin() {
-  // Your code here
+  if((board[0, 2] === playerTurn) && (board[1, 1] === playerTurn) && (board[2, 0] === playerTurn)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function checkForWin() {
-  // Your code here
+  if(horizontalWin === true ||
+    verticalWin === true ||
+    diagonalWin === true) {
+      return true;
+    }
 }
 
 function ticTacToe(row, column) {
-  // Your code here
+  return board[row][column] = playerTurn;
 }
 
 function getPrompt() {
