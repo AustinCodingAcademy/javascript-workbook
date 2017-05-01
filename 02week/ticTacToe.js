0f2154de18045bfb8ecb5460b4e11a22da5d7bf0
Eric Garcia <egarcia1868@gmail.com>
2017-05-01 17:15:55 -0500

    adding tictactoe

--- 02week/ticTacToe.js
+++ 02week/ticTacToe.js
@@ -25,22 +25,58 @@ function printBoard() {
 
 function horizontalWin() {
   // Your code here
+  if ((board[0][0] === playerTurn && board[0][1] === playerTurn && board[0][2] === playerTurn) ||
+  (board[1][0] === playerTurn && board[1][1] === playerTurn && board[1][2] === playerTurn) ||
+  (board[2][0] === playerTurn && board[2][1] === playerTurn && board[2][2] === playerTurn)) {
+    return true
+  } else {
+    return false
+  }
 }
 
 function verticalWin() {
   // Your code here
+  if ((board[0][0] === playerTurn && board[1][0] === playerTurn && board[2][0] === playerTurn) ||
+  (board[0][1] === playerTurn && board[1][1] === playerTurn && board[2][1] === playerTurn) ||
+  (board[0][2] === playerTurn && board[1][2] === playerTurn && board[2][2] === playerTurn)) {
+    return true
+  } else {
+    return false
+  }
 }
 
 function diagonalWin() {
   // Your code here
+  if ((board[0][0] === playerTurn && board[1][1] === playerTurn && board[2][2] === playerTurn) ||
+  (board[0][2] === playerTurn && board[1][1] === playerTurn && board[2][0] === playerTurn)) {
+    return true
+  } else {
+    return false
+  }
 }
 
 function checkForWin() {
   // Your code here
+  if (diagonalWin() === true || horizontalWin() === true || verticalWin() === true) {
+  return true
+  }
+}
+
+function announce() {
+  if (checkForWin() === true) {
+  return (playerTurn + ' wins!')
+  }
 }
 
 function ticTacToe(row, column) {
   // Your code here
+  board[row][column] = playerTurn;
+
+  if (playerTurn === 'X') {
+    playerTurn = 'O'
+  } else {
+    playerTurn = 'X'
+  }
 }
 
 function getPrompt() {
