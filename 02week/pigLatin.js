13f06263c58dfe17bf9501d5c74f0e096ea158ba
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-05-02 18:05:51 -0500

    new code structure of Pig Latin Application

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -11,27 +11,21 @@ const rl = readline.createInterface({
 function pigLatin(word) {
 
   // Your code here
-  word = prompt('Please enter any word'); //Asking user to enter a word
-  var vowels = ['a','e','i','o','u'];
-
-  var firstLetter = word.substring(0,1); // here substring() method returns the first character
-
-  var secondLetter = word.substring(1,2); // here substring() method returns the second character
-
-
-  if(firstLetter === (firstLetter.toUpperCase())) { // check if first character is capitalized
-    firstLetter = firstLetter.toLowerCase(); // first character lowercase
-    word = word.toLowerCase(); // word lowercase
-  }
-  if((firstLetter === vowels[0]) || (firstLetter === vowels[1]) || (firstLetter === vowels[2]) || (firstLetter === vowels[3]) || (firstLetter === vowels[4])) { // if first character is a vowel
-    word = word + 'yay'; // add 'yay' to the end of the word
-    console.log(word);
-  }else if((secondLetter === vowels[0]) || (secondLetter === vowels[1]) || (secondLetter === vowels[2]) || (secondLetter === vowels[3]) || (secondLetter === vowels[4])) { //if the first character is not vowel and second character is vowel
-    word = word.slice(1) + firstLetter + 'ay'; //take out first character and add it to the end with 'ay'
-    console.log(word);
-  }else{ // if first and second characters are not vowels
-    word = word.slice(2) + word.slice(0,2) + 'ay'; //take out both 1st and 2nd characters and add them to the end with 'ay'
-    console.log(word);
+  word = word.toLowerCase(); // first auto lowercase word before translation
+  let vowels = ['a','e','i','o','u'], //declare all vowels in an array and assign to variable vowels
+      result = word.split(''); //split the string and assign to result
+      
+  if(vowels.includes(word.charAt(0))) { //if first letter is a vowel
+    return word += 'yay'; // add yay to the end of the word
+  } else { //if the first letter is not a vowel
+    for (var i=0; i < word.length; i++) { //loop through each letter
+      if(!vowels.includes(word[i])) { //if the letter isn't a vowel
+        result.push(result.shift()); //take the first letter and add it to the end
+      } else { //until you reach a vowel
+        result.push('ay'); //add ay to the end
+        return result.join(''); //make it a string
+      }
+    }
   }
 
 }
