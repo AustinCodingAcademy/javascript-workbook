23614365e1bce8e72e6f15fa3fc55be301ecaff3
Stu Kilgore <stuart.kilgore@gmail.com>
2017-05-03 12:16:38 -0500

    hanoi comments

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -19,40 +19,45 @@ function printStacks() {
   console.log("c: " + stacks.c + "\n");
 }
 
+//Moves last entry in startStack to last place in endStack
 function movePiece(startStack, endStack) {
   let start = stacks[startStack];
   let end = stacks[endStack];
   end.push(start.pop());
 }
 
+//Checks to assert legal moves
 function isLegal(startStack, endStack) {
   let start = stacks[startStack];
   let end = stacks[endStack];
+  //Setting x and y to the last entry of startStack and endStack respectively
+  //If startStack or endStack are empty, x/y are set to 5
   let x = (start.length===0?5:start[start.length - 1]);
   let y = (end.length===0?5:end[end.length - 1]);
-  if (x === 0) {
+  if (x === 0) { //Checks if there is anything to pick up
     console.log('Invalid move: nothing to pick up');
     return false;
-  } else if (x > y) {
+  } else if (x > y) { //Checks if picked up piece is better than end location
     console.log('Invalid move: can not place larger on smaller');
     return false;
-  } else if (x === y) {
+  } else if (x === y) { //Checks if both startStack and endStack are empty
     console.log('Invalid move: start and end are empty');
     return false;
-  } else {
+  } else { //If none of the above are triggered, move is legal
     return true;
   }
 }
 
+//Checks if either stack c has all pieces on it
 function checkForWin() {
   if (stacks.c.join('') === '4321') {
     return true;
   } else {
     return false;
   }
-  // return (stacks.c===[2,1]?true:false);
 }
 
+//Main function
 function towersOfHanoi(startStack, endStack) {
   if (isLegal(startStack, endStack)) {
     movePiece(startStack, endStack);
