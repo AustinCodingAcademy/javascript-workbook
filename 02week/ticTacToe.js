a834e3e81435fbc5658c0f8f73d16e9225daf62f
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-05-02 20:01:49 -0500

    new version of TicTacToe application

--- 02week/ticTacToe.js
+++ 02week/ticTacToe.js
@@ -24,53 +24,59 @@ function printBoard() {
 }
 
 function horizontalWin() {
-  if ((board[0][0] && board[0][1]) && (board[0][2] === 'X') || (board[1][0] && board[1][1]) && (board[1][2] === 'X') || (board[2][0] && board[2][1] && board[2][2] === 'X') ||
-      (board[0][0] && board[0][1]) && (board[0][2] === 'O') || (board[1][0] && board[1][1]) && (board[1][2] === 'O') || (board[2][0] && board[2][1] && board[2][2] === 'O')) {
+
+  for (var i=0; i < 3; i++) {
+    if ((board[i][0] !== '') && (board[i][0] === board[i][1] === board[i][2])) {
       return true;
 
     }
-      return false;
-    
+  }
+
 
 }
 
 function verticalWin() {
-  if ((board[0][0] && board[1][0]) && (board[2][0] === 'X') || (board[0][1] && board[1][1]) && (board[2][1] === 'X') || (board[0][2] && board[1][2] && board[2][2] === 'X') ||
-      (board[0][0] && board[1][0]) && (board[2][0] === 'O') || (board[0][1] && board[1][1]) && (board[2][1] === 'O') || (board[0][2] && board[1][2] && board[2][2] === 'O')) {
+
+  for (var i=0; i < 3; i++) {
+    if ((board[0][i] !== '') && (board[0][i] === board[1][i] === board[2][i])) {
       return true;
 
     }
-      return false;
-
   }
+  return false;
+}
 
-  function diagonalWin() {
-    if ((board[0][0] && board[1][1]) && (board[2][2] === 'X') || (board[0][2] && board[1][1]) && (board[2][0] === 'X')  ||
-        (board[0][0] && board[1][1]) && (board[2][2] === 'O') || (board[0][2] && board[1][1]) && (board[2][0] === 'O')) {
-        return true;
+function diagonalWin() {
+  for (var i=0; i < 3; i++) {
+    if ((board[0][0] === board[1][1] === board[2][2]) || (board[0][2] === board[1][1] === board[2][0])) {
+      return true;
 
-      }
-        return false;
-
-  }
-
-  function checkForWin() {
-
-    if (horizontalWin() || verticalWin() || diagonalWin()) {
-      console.log('You win!');
     }
-    return false;
+  }
+  return false;
+}
+
+function checkForWin() {
+
+  if (horizontalWin() || verticalWin() || diagonalWin()) {
+    return true;
+    console.log('You win!');
+  }
+  return false;
 
   function ticTacToe(row, column) {
     // Your code here
-    board[row][column] = playerTurn;
 
+    board[row][column] = playerTurn;
     checkForWin();
-    if(playerTurn === 'X'){
+
+    if (playerTurn === 'X') {
       playerTurn = 'O';
-    }else{
+    } else {
       playerTurn = 'X';
     }
+
+
   }
 
   function getPrompt() {
