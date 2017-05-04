15a8b45b4112fcac92b27c414ad85a638606a900
Juan Mata <juanmata@Juans-MacBook-Pro.local>
2017-05-04 18:52:01 -0500

    tictactoe

---
+++ 03week/loops.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var persons = {
+  firstName: "Jane",
+  lastName: "Doe",
+  birthDate: "Jan 5, 1925",
+  gender: "female"
+};
+
+function loops(){
+  //1. Write For Loop
+
+  //2. Write For..In Loop
+
+  //3. Write While Loop
+
+  //4. Write Do While Loop
+
+
+}
