eaa72b186686cade8854344c5e693a0655af0e7b
Kevin Colten <kevincolten@gmail.com>
2017-05-04 09:48:29 -0500

    Update mastermind.js

--- 03week/mastermind.js
+++ 03week/mastermind.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const assert = require('assert');
-const colors = require('colors/safe');
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
