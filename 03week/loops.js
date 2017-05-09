b0eed0507143e4245c9cd4cb0a2b4b17d4e8e1b3
Juan Mata <juanmata@Juans-MacBook-Pro.local>
2017-05-09 17:15:20 -0500

    loops

--- 03week/loops.js
+++ 03week/loops.js
@@ -9,12 +9,28 @@ var persons = {
 
 function loops(){
   //1. Write For Loop
+  for(var i=0 ; i<1001 ; i++){
+  console.log(i);
+}
 
   //2. Write For..In Loop
+  var x;
+  for(x in person){
+    console.log(person[x]);
+  }
+  console.log(person.birthDate);
 
   //3. Write While Loop
-
+  var i = 0;
+    while (i <= 1000) {
+      console.log(i);
+      i++;
+    }
+    
   //4. Write Do While Loop
-
+  do{
+    console.log(1)
+  }
+  while(false);
 
 }
