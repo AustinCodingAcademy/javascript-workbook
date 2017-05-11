aba83a6a3d47b9ba881021af5cf352fb51b3ffbe
Greg Swan <greg.c.swan@gmail.com>
2017-05-11 14:32:08 -0500

    Algorithyms

--- 03week/mastermind.js
+++ 03week/mastermind.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const assert = require('assert');
-const colors = require('colors/safe');
+// const colors = require('colors/safe');
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
@@ -9,7 +9,7 @@ const rl = readline.createInterface({
 });
 
 const board = [];
-let solution = '';
+let solution = 'abcd';
 const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
 function printBoard() {
@@ -29,13 +29,126 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateHint() {
-  // your code here
+function generateHint(solution, guess) {
+  console.log(guess, solution);
+  let redPegs = []
+  let whitePegs = []
+
+  //Determines Red Pegs
+  if (guess[0] === solution[0]){
+    whitePegs.push(-1);
+    redPegs.push(1);
+  }
+  if (guess[1] === solution[1]){
+    whitePegs.push(-1);
+    redPegs.push(1);
+  }
+  if (guess[2] === solution[2]){
+    whitePegs.push(-1);
+    redPegs.push(1);
+  }
+  if (guess[3] === solution[3]){
+    whitePegs.push(-1);
+    redPegs.push(1);
+  }
+
+
+  // function to add the elements of whitePegs together
+  function add(a, b) {
+      return a + b;
+  }
+
+  //Determines white pegs
+  if((solution.search('a') >= 0) & (guess.search('a') >= 0)){
+    if(guess.match(/a/gi).length < solution.match(/a/gi).length){
+      whitePegs.push(guess.match(/a/gi).length);
+    }
+    else{
+      whitePegs.push(solution.match(/a/gi).length);
+    }
+  }
+  if((solution.search('b') >= 0) & (guess.search('b') >= 0)){
+    if(guess.match(/b/gi).length < solution.match(/b/gi).length){
+      whitePegs.push(guess.match(/b/gi).length);
+    }
+    else{
+      whitePegs.push(solution.match(/b/gi).length);
+    }
+  }
+  if((solution.search('c') >= 0) & (guess.search('c') >= 0)){
+    if(guess.match(/c/gi).length < solution.match(/c/gi).length){
+      whitePegs.push(guess.match(/c/gi).length);
+    }
+    else{
+      whitePegs.push(solution.match(/c/gi).length);
+    }
+  }
+  if((solution.search('d') >= 0) & (guess.search('d') >= 0)){
+    if(guess.match(/d/gi).length < solution.match(/d/gi).length){
+      whitePegs.push(guess.match(/d/gi).length);
+    }
+    else{
+      whitePegs.push(solution.match(/d/gi).length);
+    }
+  }
+  if((solution.search('e') >= 0) & (guess.search('e') >= 0)){
+    if(guess.match(/e/gi).length < solution.match(/e/gi).length){
+      whitePegs.push(guess.match(/e/gi).length);
+    }
+    else{
+      whitePegs.push(solution.match(/e/gi).length);
+    }
+  }
+  if((solution.search('f') >= 0) & (guess.search('f') >= 0)){
+    if(guess.match(/f/gi).length < solution.match(/f/gi).length){
+      whitePegs.push(guess.match(/f/gi).length);
+    }
+    else{
+      whitePegs.push(solution.match(/f/gi).length);
+    }
+  }
+  if((solution.search('g') >= 0) & (guess.search('g') >= 0)){
+    if(guess.match(/g/gi).length < solution.match(/g/gi).length){
+      whitePegs.push(guess.match(/g/gi).length);
+    }
+    else{
+      whitePegs.push(solution.match(/g/gi).length);
+    }
+  }
+  if((solution.search('h') >= 0) & (guess.search('h') >= 0)){
+    if(guess.match(/h/gi).length < solution.match(/h/gi).length){
+      whitePegs.push(guess.match(/h/gi).length);
+    }
+    else{
+      whitePegs.push(solution.match(/h/gi).length);
+    }
+  }
+
+  let sumW = whitePegs.reduce(add, 0)
+  let sumR = redPegs.length
+  let empty = ' '
+  let dash = '-'
+
+  return (sumR + dash + sumW);
+
+  }
+
+
+
+
+  function mastermind(guess) {
+    console.log('solution :' + solution);
+    console.log('guess :' + guess);
+    board.push(guess)
+    if (guess === solution) {
+    return 'You guessed it!'
+  }
+    generateHint(solution, guess);
+
+
+
 }
 
-function mastermind(guess) {
-  // your code here
-}
 
 
 function getPrompt() {
@@ -73,6 +186,6 @@ if (typeof describe === 'function') {
 
 } else {
 
-  generateSolution();
+  // generateSolution();
   getPrompt();
 }
--- 04week/algorithms.js
+++ 04week/algorithms.js
@@ -15,45 +15,112 @@ for (let i = 0; i < 1000; i++) {
 }
 
 function bubbleSort(arr) {
-  // Your code here
+  let swapped;
+  do {
+    swapped = false
+    for (let i = 0; i < arr.length-1; i++){
+      let first = arr[i];
+      let second = arr[i+1];
+      // console.log(first + '&' + second )
+      if (first > second){
+        arr[i] = second;
+        arr[i+1] = first;
+        swapped = true;
+      }
+    }
+    // console.log(arr)
+  } while (swapped)
+    return arr;
 }
 
+
 function mergeSort(arr) {
-  // Your code here
+  if (arr.length < 2){
+    return arr;
+  }
+  const firstHalf = arr.slice(0, arr.length / 2);
+  const secondHalf = arr.slice(arr.length / 2);
+
+  const sortedFirst = mergeSort(firstHalf);
+  const sortedSecond = mergeSort(secondHalf);
+
+  let sortedArr = [];
+
+  while (sortedFirst.length && sortedSecond.length) {
+    if (sortedFirst[0] <= sortedSecond[0]){
+      sortedArr.push(sortedFirst.shift());
+    }
+    else {
+      sortedArr.push(sortedSecond.shift());
+    }
+  }
+
+  if (!sortedFirst.length){
+    sortedArr = sortedArr.concat(sortedSecond);
+  }
+  else {
+    sortedArr = sortedArr.concat(sortedFirst)
+  }
+  return sortedArr;
+
+
 }
 
 function binarySearch(arr, item) {
-  // Your code here
+  let firstHalf = item.slice(0, Math.floor(item.length / 2));
+  let secondHalf = item.slice(item.length / 2);
+  while (firstHalf.length > 1 && secondHalf.length > 1) {
+    if (needle < secondHalf[0]) {
+      firstHalf = firstHalf.slice(0, item.length / 2);
+      secondHalf = firstHalf.slice(item.length / 2);
+    } else {
+      firstHalf = secondHalf.slice(0, item.length / 2);
+      secondHalf = secondHalf.slice(item.length / 2);
+    }
+  }
+
+  if (firstHalf[0] === arr || secondHalf[0] === arr) {
+    return true;
+  }
+  return false;
 }
+
+// binarySearch(5, [10,22,33,44,55,66,76,87,98]);
+
 
 // Tests
 
 if (typeof describe === 'function') {
+  function comparator(a, b) {
+      if (Number(a) < Number(b)) return -1;
+      if (Number(a) > Number(b)) return 1;
+      return 0;
+    }
 
-  describe('#bubbleSort()', () => {
-    it('should sort array', () => {
-      const sorted = bubbleSort(arr);
-      assert.deepEqual(sorted, arr.sort());
+    describe('#bubbleSort()', () => {
+      it('should sort array', () => {
+        const sorted = bubbleSort(arr);
+        assert.deepEqual(sorted, arr.sort(comparator));
+      });
     });
-  });
 
-  describe('#mergeSort()', () => {
-    it('should sort array', () => {
-      const sorted = mergeSort(arr);
-      assert.deepEqual(sorted, arr.sort());
+    describe('#mergeSort()', () => {
+      it('should sort array', () => {
+        const sorted = mergeSort(arr);
+        assert.deepEqual(sorted, arr.sort(comparator));
+      });
     });
-  });
 
-  describe('#binarySearch()', () => {
-    it('should return the index of given item if sorted array contains it', () => {
-      const idx = binarySearch([2, 1, 4, 3], 3);
-      assert.equal(idx, 2);
+    describe('#binarySearch()', () => {
+      it('should return the index of given item if sorted array contains it', () => {
+        const idx = binarySearch([2, 1, 4, 3], 3);
+        assert.equal(idx, 2);
+      });
+      it('should return false if item not in sorted array', () => {
+        const idx = binarySearch([1, 2, 3, 4], 5);
+        assert.equal(idx, false);
+      });
     });
-    it('should return false if item not in sorted array', () => {
-      const idx = binarySearch([1, 2, 3, 4], 5);
-      assert.equal(idx, false);
-    });
-  });
 
 } else {
 
