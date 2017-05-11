ac952ed18862bf1405dcfc5bfc76891d2d294089
Juan Mata <juanmata@Juans-MacBook-Pro.local>
2017-05-11 10:49:05 -0500

    mastermind work now

--- 03week/mastermind.js
+++ 03week/mastermind.js
@@ -1,16 +1,18 @@
 'use strict';
 
 const assert = require('assert');
-const colors = require('colors/safe');
+//const colors = require('colors/safe');
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-const board = [];
+let board = [];
 let solution = '';
 const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+let hint = '';
+
 
 function printBoard() {
   for (let i = 0; i < board.length; i++) {
@@ -29,34 +31,36 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateHint() {
-  // your code here
-  let numberRight = 0;
-  let numberClose = 0;
-  function split(string) {
-  return string.split('')
-}
-solution = split(solution)
-guess = split(guess)
-for (let i = 0; i < solution.length; i++) {
-  if(guess[i] === solution[i]){
-      numberRight += 1;
-    } else if (guess[i].includes(solution[i])) {
-      numberClose += 1;
+function generateHint(guess) {
+  let g = guess.split('');
+  let s = solution.split('');
+  let arr = [];
+  let numCorrect = 0 ;
+  let numExist = 0 ;
+  for(let i=0; i<s.length; i++){
+    if(s[i]===g[i]){
+      arr.push(g[i]);
+      numCorrect++;
     }
-    let returnString = numberRight + '‐' + numberClose;
-    return returnString;
+    else if(s.includes(g[i]) && (arr.includes(g[i]) === false)){
+      arr.push(g[i]);
+      numExist++;
+    }
+  }
+  hint = `${numCorrect}-${numExist}`;
+  return hint;
 }
 
 function mastermind(guess) {
-  // your code here
-  (board.push(guess));
-  if(guess === solution) {
-    return "You guessed it!";
-  } else {
-    return generateHint(guess, solution);
+  generateHint(guess);
+  //check for win
+  if(guess===solution){
+    return 'You guessed it!';
   }
-}
+  else {
+    board.push(guess+': '+hint);
+    printBoard();
+  }
 }
 
 
@@ -85,10 +89,12 @@ if (typeof describe === 'function') {
 
   describe('#generateHint()', () => {
     it('should generate hints', () => {
-      assert.equal(generateHint('abcd', 'abdc'), '2-2');
+      solution = 'abcd';
+      assert.equal(generateHint('abdc'), '2-2');
     });
     it('should generate hints if solution has duplicates', () => {
-      assert.equal(generateHint('abcd', 'aabb'), '1-1');
+      solution = 'abcd';
+      assert.equal(generateHint('aabb'), '1-1');
     });
 
   });
