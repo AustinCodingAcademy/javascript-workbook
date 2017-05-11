bb36a46893d98557da835197b424dd886c510fed
stackbaxter <stackbaxter@yahoo.com>
2017-05-10 20:55:03 -0500

    higherorder

--- 04week/algorithms.js
+++ 04week/algorithms.js
@@ -187,7 +187,7 @@ if (typeof describe === 'function') {
 
   describe('#binarySearch()', () => {
     it('should return the index of given item if sorted array contains it', () => {
-      const idx = binarySearch(3, [1,2,3,4,6,7,8,9,10]);
+      const idx = binarySearch([1,2,3,4,6,7,8,9,10], 3);
       assert.equal(idx, 2);
     });
     it('should return false if item not in sorted array', () => {
@@ -196,8 +196,4 @@ if (typeof describe === 'function') {
     });
   });
 
-} else {
-
-
-
-}
+} 
