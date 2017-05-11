c7861f66ea303794a764ce1070607aa77ec37f6f
stackbaxter <stackbaxter@gmail.com>
2017-05-11 02:13:43 -0500

    !!!

--- 04week/algorithms.js
+++ 04week/algorithms.js
@@ -20,13 +20,13 @@ function bubbleSort(arr) {
   for (var i = 0; i < length; i++) { //Number of passes
     for (var j = 0; j < (length - i - 1); j++) { //Notice that j < (length - i)
         //Compare the adjacent positions
-        if(arr[j] > arr[j+1]) {
+      if(arr[j] > arr[j+1]) {
           //Swap the numbers
           var tmp = arr[j];  //Temporary variable to hold the current number
           arr[j] = arr[j+1]; //Replace current number with adjacent number
           arr[j+1] = tmp; //Replace adjacent number with current number
         }
-      }
+    }
   }
   return arr;
 }
