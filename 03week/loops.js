25c8b4a2fb1984511d1585c7faa642abcb1e1cf6
jdrowlett <jdrowlett@outlook.com>
2017-05-13 12:11:43 -0500

    Loops for cars, persons, numbers

--- 03week/loops.js
+++ 03week/loops.js
@@ -1,11 +1,25 @@
-function loops(){
-  //1. Write For Loop
+//1. Write For Loop
+  function loops() {
+    //1. Write For Loop
+    var carsInReverse = ['Ferrari', 'Bentley', 'Lamborghini', 'Maserati'];
+    for (i = 0; i < 4; i++) {
+      console.log(carsInReverse);
+    }
+  }
 
-  //2. Write For..In Loop
+//2. Write For..In Loop
+  var persons = {fname:"Jane", lname:"Doe", birthDate:"Jan 5, 1925", gender:"Female"};
+    console.log(persons);
 
-  //3. Write While Loop
+//3. Write While Loop
+  var i = 0;
+  while (i < 1000) {
+    console.log(i++);
+  }
 
-  //4. Write Do While Loop
+//4. Write Do While Loop
+  do {
+    i = 0; i < 1000; i++;
+  }
 
-  
-}
+  while (i < 1000);
