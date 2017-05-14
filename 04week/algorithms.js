18f92b3195fa3520e063ff560ae4143ea70acf68
Maggie <mymonstermaggie@Maggies-MacBook-Pro.local>
2017-05-14 18:08:26 -0500

    fix errors

--- 04week/algorithms.js
+++ 04week/algorithms.js
@@ -20,10 +20,10 @@ function bubbleSort(arr) {
     swapped = false;
     for(var i=0; i<arr.length; i++){
       if(arr[i] > arr[i+1]){
-       let temp = arr[i];
-       arr[i] = arr[i+1];
-       arr[i+1] = temp;
-       swapped = true;
+        let temp = arr[i];
+        arr[i] = arr[i+1];
+        arr[i+1] = temp;
+        swapped = true;
       }
     }
   } while (swapped);
