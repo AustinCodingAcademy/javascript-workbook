104417f3e1de88e51e768a70728d4a9800dab39f
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-05-15 16:44:24 -0500

    Fixing indentation errors

--- 02week/ticTacToe.js
+++ 02week/ticTacToe.js
@@ -80,49 +80,49 @@ function printBoard() {
 //   }
 
 function getPrompt() {
-    printBoard();
-    console.log("It's Player " + playerTurn + "'s turn.");
-    rl.question('row: ', (row) => {
-      rl.question('column: ', (column) => {
-        ticTacToe(row, column);
-        getPrompt();
-      });
+  printBoard();
+  console.log("It's Player " + playerTurn + "'s turn.");
+  rl.question('row: ', (row) => {
+    rl.question('column: ', (column) => {
+      ticTacToe(row, column);
+      getPrompt();
     });
+  });
 
-  }
+}
 
 
 
-  // Tests
+// Tests
 
 if (typeof describe === 'function') {
 
-    describe('#ticTacToe()', () => {
-      it('should place mark on the board', () => {
-        ticTacToe(1, 1);
-        assert.deepEqual(board, [ [' ', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' '] ]);
-      });
-      it('should alternate between players', () => {
-        ticTacToe(0, 0);
-        assert.deepEqual(board, [ ['O', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' '] ]);
-      });
-      it('should check for vertical wins', () => {
-        board = [ [' ', 'X', ' '], [' ', 'X', ' '], [' ', 'X', ' '] ];
-        assert.equal(verticalWin(), true);
-      });
-      it('should check for horizontal wins', () => {
-        board = [ ['X', 'X', 'X'], [' ', ' ', ' '], [' ', ' ', ' '] ];
-        assert.equal(horizontalWin(), true);
-      });
-      it('should check for diagonal wins', () => {
-        board = [ ['X', ' ', ' '], [' ', 'X', ' '], [' ', ' ', 'X'] ];
-        assert.equal(diagonalWin(), true);
-      });
-      it('should detect a win', () => {
-        assert.equal(checkForWin(), true);
-      });
+  describe('#ticTacToe()', () => {
+    it('should place mark on the board', () => {
+      ticTacToe(1, 1);
+      assert.deepEqual(board, [ [' ', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' '] ]);
     });
-  } else {
+    it('should alternate between players', () => {
+      ticTacToe(0, 0);
+      assert.deepEqual(board, [ ['O', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' '] ]);
+    });
+    it('should check for vertical wins', () => {
+      board = [ [' ', 'X', ' '], [' ', 'X', ' '], [' ', 'X', ' '] ];
+      assert.equal(verticalWin(), true);
+    });
+    it('should check for horizontal wins', () => {
+      board = [ ['X', 'X', 'X'], [' ', ' ', ' '], [' ', ' ', ' '] ];
+      assert.equal(horizontalWin(), true);
+    });
+    it('should check for diagonal wins', () => {
+      board = [ ['X', ' ', ' '], [' ', 'X', ' '], [' ', ' ', 'X'] ];
+      assert.equal(diagonalWin(), true);
+    });
+    it('should detect a win', () => {
+      assert.equal(checkForWin(), true);
+    });
+  });
+} else {
 
-    getPrompt();
-  }
+  getPrompt();
+}
