7d15ed3f639168daf32ea935a6e5091a6a0c292f
stackbaxter <stackbaxter@gmail.com>
2017-05-15 17:30:16 -0500

    higher order making more sense

---
+++ 04week/functional-javascript/basic-Call.js
@@ -0,0 +1,8 @@
+function duckCount() {
+
+  return Array.prototype.slice.call(arguments).filter(obj =>
+       Object.prototype.hasOwnProperty.call(obj, 'quack')).length}
+
+
+
+   module.exports = duckCount
