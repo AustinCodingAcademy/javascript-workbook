6bde49f97183bd9ec3cb5361e732111e5b647839
demarcogleason <demarcogleason@gmail.com>
2017-05-16 23:51:43 -0500

    higher order functions

--- 04week/higherOrder.js
+++ 04week/higherOrder.js
@@ -4,22 +4,55 @@ const assert = require('assert');
 
 function forEach(arr, callback) {
   // Your code here
+  for (let i=0; i<arr.length; i++){
+    callback(arr[i]);
+  }
 }
 
 function map(arr, callback) {
   // Your code here
+//   var double = arr.map(function(arr) {
+//   return arr * 2;
+// });
+let mapped = [];
+for (let i=0; i<arr.length; i++){
+  mapped.push(callback(arr[i]));
 }
+return mapped;
+}
+
 
 function filter(arr, callback) {
   // Your code here
+  let newArr = [];
+  for (let i=0; i < arr.length; i++){
+    if (callback(arr[i])){
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
 }
 
 function some(arr, callback) {
   // Your code here
+  for (let i=0; i < arr.length; i++){
+    if (callback(arr[i])){
+      return true;
+    }
+
+  }
+  return false;
 }
 
 function every(arr, callback) {
   // Your code here
+  for (let i=0; i < arr.length; i++){
+    if (!callback(arr[i])){
+      return false;
+    }
+
+  }
+  return true;
 }
 
 if (typeof describe === 'function') {
