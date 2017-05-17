3f4446a0988cf2bab54a442918cd6bce9f3dc018
Greg Swan <greg.c.swan@gmail.com>
2017-05-17 17:48:26 -0500

    fixing identation

--- 01week/javascripting/array-filtering.js
+++ 01week/javascripting/array-filtering.js
@@ -1,8 +1,8 @@
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-var filtered = numbers.filter(function evenNumbers(number) {  
-       return number % 2 === 0;
-     }
+var filtered = numbers.filter(function evenNumbers(number) {
+  return number % 2 === 0;
+}
 );
 
 
