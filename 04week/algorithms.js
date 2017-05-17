6c8736776d6120e0c32539d91c13e191a7707ba1
meganroone <emailgena@gmail.com>
2017-05-17 18:35:15 -0500

    indents again 3

--- 04week/algorithms.js
+++ 04week/algorithms.js
@@ -20,10 +20,10 @@ function bubbleSort(arr) {
   for (let i = (len-1); i >= 0; i--) {
     for (j = 1; j <= i; j++) {
       if (arr[j] < arr[j - 1]) {
-      let temp = arr[j-1];
-      arr[j-1] = arr[j];
-      arr[j] = temp;
-    }
+        let temp = arr[j-1];
+        arr[j-1] = arr[j];
+        arr[j] = temp;
+      }
     }
   }
   return arr;
