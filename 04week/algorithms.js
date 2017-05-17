afdf6bc7c8091e53a6a7836682a19b200ec1463f
meganroone <emailgena@gmail.com>
2017-05-17 18:27:58 -0500

    indents again

--- 04week/algorithms.js
+++ 04week/algorithms.js
@@ -18,13 +18,11 @@ function bubbleSort(arr) {
   // Your code here
   let len = arr.length;
   for (let i = (len-1); i >= 0; i--) {
-
-  for (j = 1; j <= i; j++) {
-
-        if (arr[j] < arr[j - 1]) {
-          let temp = arr[j-1];
-          arr[j-1] = arr[j];
-          arr[j] = temp;
+    for (j = 1; j <= i; j++) {
+    if (arr[j] < arr[j - 1]) {
+      let temp = arr[j-1];
+      arr[j-1] = arr[j];
+      arr[j] = temp;
       }
     }
   }
