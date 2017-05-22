45b7a6c944c5134ba0b45bff4c26a17856c1109d
Greg Swan <greg.c.swan@gmail.com>
2017-05-22 17:57:22 -0500

    passing aling all tests in checkers

--- 05week/checkers.js
+++ 05week/checkers.js
@@ -8,13 +8,33 @@ const rl = readline.createInterface({
 });
 
 
-function Checker() {
-  // Your code here
+
+
+
+
+
+
+function Checker(color) {
+  if (color === 'white'){
+    this.symbol = String.fromCharCode(0x125CB);
+  }
+  else {
+    this.symbol = String.fromCharCode(0x125CF);
+  }
 }
+
+
+
+
+
+
+
 
 function Board() {
   this.grid = [];
   // creates an 8x8 array, filled with null values
+
+
   this.createGrid = function() {
     // loop to create the 8 rows
     for (let row = 0; row < 8; row++) {
@@ -52,17 +72,94 @@ function Board() {
     console.log(string);
   };
 
-  // Your code here
+  this.checkers = [];
+
+  this.createCheckers = function(){
+    const whiteChecker = new Checker('white')
+    const blackChecker = new Checker('black')
+    // the index of all white positions
+    let whitePositions = [[0, 1], [0, 3], [0, 5], [0, 7],[1, 0], [1, 2], [1, 4],
+    [1, 6], [2, 1], [2, 3], [2, 5], [2, 7]]
+    // the index of all black positions
+    let blackPositions = [[5, 0], [5, 2], [5, 4], [5, 6], [6, 1], [6, 3], [6, 5],
+    [6, 7], [7, 0], [7, 2], [7, 4], [7, 6]]
+
+    for (let wp in whitePositions){
+      // uses a for in loop to index all of the positions to instatiate a checker
+      this.grid[whitePositions[wp][0]][whitePositions[wp][1]] = whiteChecker;
+      // pushes all instances of a checker to the array checkers
+      this.checkers.push(whiteChecker);
+    }
+
+    for (let bp in blackPositions){
+      // uses a for in loop to index all of the positions to instatiate a checker
+      this.grid[blackPositions[bp][0]][blackPositions[bp][1]] = blackChecker;
+      // pushes all instances of a checker to the array checkers
+      this.checkers.push(blackChecker);
+    }
+  }
+
+  this.selectChecker = function(row, col){
+    return this.grid[row][col];
+    // this.grid[end[0]][end[1]] = checker;
+
+
+  }
+
+  this.killChecker = function(position){
+    // selects the checker
+    let checker = this.selectChecker(position[0], position[1]);
+    // finds the index of the checker
+    let indexChecker = this.checkers.indexOf(checker);
+    // removes the selected checker from array checkers
+    this.checkers.splice(indexChecker, 1);
+    
+    this.grid[position[0]][position[1]] = null;
+
+  }
 }
+
+
+
+
+
+
+
+
 function Game() {
 
   this.board = new Board();
 
   this.start = function() {
     this.board.createGrid();
-    // Your code here
+    this.board.createCheckers()
   };
+
+
+
+  this.moveChecker = function(start, end) {
+
+     let checker = this.board.selectChecker(start[0], start[1]);
+     this.board.grid[end[0]][end[1]] = checker;
+     this.board.grid[start[0]][start[1]] = null;
+     let distance = start[0]-end[0];
+     if(Math.abs(distance) === 2){
+    // Calculates the midPoint if the absolute distance between two coordinates
+       let midRow = (Number(start[0]) + Number(end[0])) / 2;
+       let midCol = (Number(start[1]) + Number(end[1])) / 2;
+       let midPosition = [midRow, midCol];
+       this.board.killChecker(midPosition);
+     }
+
+
+  }
+
 }
+
+
+
+
+
 
 function getPrompt() {
   game.board.viewGrid();
