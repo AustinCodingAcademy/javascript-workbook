80c33110d420de3129ff16cfccd9f36c852ba58b
Ethan Crowley <Ethan Crowley>
2017-05-23 21:11:51 -0500

    checkers start

--- 05week/checkers.js
+++ 05week/checkers.js
@@ -3,75 +3,133 @@
 const assert = require('assert');
 const readline = require('readline');
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
 
-function Checker() {
-  // Your code here
+function Checker(color) {
+    this.color = color;
+    if (this.color === 'white') {
+        this.symbol = String.fromCharCode(0x125CB);
+    } else {
+        this.symbol = String.fromCharCode(0x125CF);
+    }
 }
+
 
 function Board() {
-  this.grid = [];
-  // creates an 8x8 array, filled with null values
-  this.createGrid = function() {
-    // loop to create the 8 rows
-    for (let row = 0; row < 8; row++) {
-      this.grid[row] = [];
-      // push in 8 columns of nulls
-      for (let column = 0; column < 8; column++) {
-        this.grid[row].push(null);
-      }
+    //this.killChecker
+    this.checkers = [];
+    this.grid = [];
+    this.selectChecker = function(row, column) {
+        return this.grid[row][column]
     }
-  };
+    //  this.createCheckers =
+    let whitePositions = [
+        [0, 1],
+        [0, 3],
+        [0, 5],
+        [0, 7],
+        [1, 0],
+        [1, 2],
+        [1, 4],
+        [1, 6],
+        [2, 1],
+        [2, 3],
+        [2, 5],
+        [2, 7]
+    ];
+    let blackPositions = [
+        [5, 0],
+        [5, 2],
+        [5, 4],
+        [5, 6],
+        [6, 1],
+        [6, 3],
+        [6, 5],
+        [6, 7],
+        [7, 0],
+        [7, 2],
+        [7, 4],
+        [7, 6]
+    ];
 
-  // prints out the board
-  this.viewGrid = function() {
-    // add our column numbers
-    let string = "  0 1 2 3 4 5 6 7\n";
-    for (let row = 0; row < 8; row++) {
-      // we start with our row number in our array
-      const rowOfCheckers = [row];
-      // a loop within a loop
-      for (let column = 0; column < 8; column++) {
-        // if the location is "truthy" (contains a checker piece, in this case)
-        if (this.grid[row][column]) {
-          // push the symbol of the check in that location into the array
-          rowOfCheckers.push(this.grid[row][column].symbol);
-        } else {
-          // just push in a blank space
-          rowOfCheckers.push(' ');
+
+    // creates an 8x8 array, filled with null values
+    this.createGrid = function() {
+        // loop to create the 8 rows
+        for (let row = 0; row < 8; row++) {
+            this.grid[row] = [];
+            // push in 8 columns of nulls
+            for (let column = 0; column < 8; column++) {
+                this.grid[row].push(null);
+            }
         }
-      }
-      // join the rowOfCheckers array to a string, separated by a space
-      string += rowOfCheckers.join(' ');
-      // add a 'new line'
-      string += "\n";
-    }
-    console.log(string);
-  };
+        for (let i = 0; i <= 11; i++) {
+            //place white checkers
+            let whiteChecker = new Checker('white');
+            // console.log(whitePositions);
+            this.checkers.push(whiteChecker);
+            let coordinate = whitePositions[i];
+            // console.log(this.grid);
 
-  // Your code here
+            this.grid[coordinate[0]][coordinate[1]] = whiteChecker;
+            //place black checker
+            let blackChecker = new Checker('black');
+            this.checkers.push(blackChecker);
+            let coordinate2 = blackPositions[i];
+            this.grid[coordinate2[0]][coordinate2[1]] = blackChecker;
+        }
+    };
+
+    // prints out the board
+    this.viewGrid = function() {
+        // add our column numbers
+        let string = "  0 1 2 3 4 5 6 7\n";
+        for (let row = 0; row < 8; row++) {
+            // we start with our row number in our array
+            const rowOfCheckers = [row];
+            // a loop within a loop
+            for (let column = 0; column < 8; column++) {
+                // if the location is "truthy" (contains a checker piece, in this case)
+                if (this.grid[row][column]) {
+                    // push the symbol of the check in that location into the array
+                    rowOfCheckers.push(this.grid[row][column].symbol);
+                } else {
+                    // just push in a blank space
+                    rowOfCheckers.push(' ');
+                }
+            }
+            // join the rowOfCheckers array to a string, separated by a space
+            string += rowOfCheckers.join(' ');
+            // add a 'new line'
+            string += "\n";
+        }
+        console.log(string);
+    };
+
+    // Your code here
 }
+
 function Game() {
 
-  this.board = new Board();
+    this.board = new Board();
 
-  this.start = function() {
-    this.board.createGrid();
-    // Your code here
-  };
+    this.start = function() {
+        this.board.createGrid();
+        // Your code here
+    };
 }
 
 function getPrompt() {
-  game.board.viewGrid();
-  rl.question('which piece?: ', (whichPiece) => {
-    rl.question('to where?: ', (toWhere) => {
-      game.moveChecker(whichPiece, toWhere);
-      getPrompt();
+    game.board.viewGrid();
+    rl.question('which piece?: ', (whichPiece) => {
+        rl.question('to where?: ', (toWhere) => {
+            game.moveChecker(whichPiece, toWhere);
+            getPrompt();
+        });
     });
-  });
 }
 
 const game = new Game();
@@ -81,32 +139,32 @@ game.start();
 // Tests
 
 if (typeof describe === 'function') {
-  describe('Game', () => {
-    it('should have a board', () => {
-      assert.equal(game.board.constructor.name, 'Board');
+    describe('Game', () => {
+        it('should have a board', () => {
+            assert.equal(game.board.constructor.name, 'Board');
+        });
+        it('board should have 24 checkers', () => {
+            assert.equal(game.board.checkers.length, 24);
+        });
     });
-    it('board should have 24 checkers', () => {
-      assert.equal(game.board.checkers.length, 24);
-    });
-  });
 
-  describe('Game.moveChecker()', function () {
-    it('should move a checker', function () {
-      assert(!game.board.grid[4][1]);
-      game.moveChecker('50', '41');
-      assert(game.board.grid[4][1]);
-      game.moveChecker('21', '30');
-      assert(game.board.grid[3][0]);
-      game.moveChecker('52', '43');
-      assert(game.board.grid[4][3]);
+    describe('Game.moveChecker()', function() {
+        it('should move a checker', function() {
+            assert(!game.board.grid[4][1]);
+            game.moveChecker('50', '41');
+            assert(game.board.grid[4][1]);
+            game.moveChecker('21', '30');
+            assert(game.board.grid[3][0]);
+            game.moveChecker('52', '43');
+            assert(game.board.grid[4][3]);
+        });
+        it('should be able to jump over and kill another checker', () => {
+            game.moveChecker('30', '52');
+            assert(game.board.grid[5][2]);
+            assert(!game.board.grid[4][1]);
+            assert.equal(game.board.checkers.length, 23);
+        });
     });
-    it('should be able to jump over and kill another checker', () => {
-      game.moveChecker('30', '52');
-      assert(game.board.grid[5][2]);
-      assert(!game.board.grid[4][1]);
-      assert.equal(game.board.checkers.length, 23);
-    });
-  });
 } else {
-  getPrompt();
+    getPrompt();
 }
--- 05week/spaceTravelToMars.js
+++ 05week/spaceTravelToMars.js
@@ -5,110 +5,111 @@ var assert = require('assert');
 
 
 // Your code here
-function CrewMember(name, job, specialSkill){
-  this.name = name;
-  this.job = job;
-  this.specialSkill = specialSkill;
-  this.ship = null;
-  this.enterShip = function (ship) {
-   // Assign our ship
-   this.ship = ship;
-   // Make sure we are part of the crew
-   this.ship.crew.push(this);
- }
+function CrewMember(name, job, specialSkill) {
+    this.name = name;
+    this.job = job;
+    this.specialSkill = specialSkill;
+    this.ship = null;
+    this.enterShip = function(ship) {
+        // Assign our ship
+        this.ship = ship;
+        // Make sure we are part of the crew
+        this.ship.crew.push(this);
+    }
 }
 
 var rick = new CrewMember('Rick Martinez', 'pilot', 'chemistry');
 rick;
+
 function Ship(name, type, ability) {
-  this.name = name;
-  this.type = type;
-  this.ability = ability;
-  this.crew = [];
+    this.name = name;
+    this.type = type;
+    this.ability = ability;
+    this.crew = [];
 }
 // Function to return a Ship object
 function Ship(name, type, ability, crewMember) {
-  this.name = name;
-  this.type = type;
-  this.ability = ability;
-  this.crew = [];
-  this.missionStatement = function () {
-  	// Let's define a default statement to return
-    // in case we don't have any of the right
-    // members on board
-  	var statement = "Can't perform a mission yet.";
+    this.name = name;
+    this.type = type;
+    this.ability = ability;
+    this.crew = [];
+    this.missionStatement = function() {
+        // Let's define a default statement to return
+        // in case we don't have any of the right
+        // members on board
+        var statement = "Can't perform a mission yet.";
 
-    // Here's some more missions
+        // Here's some more missions
 
-  	// Figure out the mission statement by looping
-    // through the array to make sure that we have
-    // the correct crew member on board
-    var job = '';
-    for (var i=0; i < this.crew.length; i++) {
-      job = this.crew[i].job;
-      if (jobTypes[job] == this.type) {
-      	statement = this.ability;
-        break;
-      }
+        // Figure out the mission statement by looping
+        // through the array to make sure that we have
+        // the correct crew member on board
+        var job = '';
+        for (var i = 0; i < this.crew.length; i++) {
+            job = this.crew[i].job;
+            if (jobTypes[job] == this.type) {
+                statement = this.ability;
+                break;
+            }
+        }
+        console.log(statement);
+        return statement;
     }
-    console.log(statement);
-    return statement;
-  }
 }
 
 
 var mav = new Ship('Mars Ascent Vehicle', 'MAV', 'Ascend into low orbit');
 mav;
 var jobTypes = {
-  pilot: 'MAV',
-  mechanic: 'Repair Ship',
-  commander: 'Main Ship',
-  programmer: 'Any Ship!'
+    pilot: 'MAV',
+    mechanic: 'Repair Ship',
+    commander: 'Main Ship',
+    programmer: 'Any Ship!'
 };
 
 //tests
-if (typeof describe === 'function'){
-  describe('CrewMember', function(){
-    it('should have a name, a job, a specialSkill and ship upon instantiation', function(){
-      var crewMember1 = new CrewMember('Rick Martinez', 'pilot', 'chemistry');
-      assert.equal(crewMember1.name, 'Rick Martinez');
-      assert.equal(crewMember1.job, 'pilot');
-      assert.equal(crewMember1.specialSkill, 'chemistry');
-      assert.equal(crewMember1.ship, null);
+if (typeof describe === 'function') {
+    describe('CrewMember', function() {
+        it('should have a name, a job, a specialSkill and ship upon instantiation', function() {
+            var crewMember1 = new CrewMember('Rick Martinez', 'pilot', 'chemistry');
+            assert.equal(crewMember1.name, 'Rick Martinez');
+            assert.equal(crewMember1.job, 'pilot');
+            assert.equal(crewMember1.specialSkill, 'chemistry');
+            assert.equal(crewMember1.ship, null);
+        });
+
+        it('can enter a ship', function() {
+            var mav = new Ship('Mars Ascent Vehicle', 'MAV', 'Ascend into low orbit');
+            var crewMember1 = new CrewMember('Rick Martinez', 'pilot', 'chemistry');
+            crewMember1.enterShip(mav);
+            assert.equal(crewMember1.ship, mav);
+            assert.equal(mav.crew.length, 1);
+            assert.equal(mav.crew[0], crewMember1);
+        });
     });
 
-    it('can enter a ship', function(){
-      var mav = new Ship('Mars Ascent Vehicle', 'MAV', 'Ascend into low orbit');
-      var crewMember1 = new CrewMember('Rick Martinez', 'pilot', 'chemistry');
-      crewMember1.enterShip(mav);
-      assert.equal(crewMember1.ship, mav);
-      assert.equal(mav.crew.length, 1);
-      assert.equal(mav.crew[0], crewMember1);
+    describe('Ship', function() {
+        it('should have a name, a type, an ability and an empty crew upon instantiation', function() {
+            var mav = new Ship('Mars Ascent Vehicle', 'MAV', 'Ascend into low orbit');
+            assert.equal(mav.name, 'Mars Ascent Vehicle');
+            assert.equal(mav.type, 'MAV');
+            assert.equal(mav.ability, 'Ascend into low orbit');
+            assert.equal(mav.crew.length, 0);
+        });
+
+        it('can return a mission statement correctly', function() {
+            var mav = new Ship('Mars Ascent Vehicle', 'MAV', 'Ascend into low orbit');
+            var crewMember1 = new CrewMember('Rick Martinez', 'pilot', 'chemistry');
+            var hermes = new Ship('Hermes', 'Main Ship', 'Interplanetary Space Travel');
+            var crewMember2 = new CrewMember('Commander Lewis', 'commander', 'geology');
+            assert.equal(mav.missionStatement(), "Can't perform a mission yet.");
+            assert.equal(hermes.missionStatement(), "Can't perform a mission yet.");
+
+            crewMember1.enterShip(mav);
+            assert.equal(mav.missionStatement(), "Ascend into low orbit");
+
+            crewMember2.enterShip(hermes);
+            assert.equal(hermes.missionStatement(), "Interplanetary Space Travel");
+        });
     });
-  });
-
-  describe('Ship', function(){
-    it('should have a name, a type, an ability and an empty crew upon instantiation', function(){
-      var mav = new Ship('Mars Ascent Vehicle', 'MAV', 'Ascend into low orbit');
-      assert.equal(mav.name, 'Mars Ascent Vehicle');
-      assert.equal(mav.type, 'MAV');
-      assert.equal(mav.ability, 'Ascend into low orbit');
-      assert.equal(mav.crew.length, 0);
-    });
-
-    it('can return a mission statement correctly', function(){
-      var mav = new Ship('Mars Ascent Vehicle', 'MAV', 'Ascend into low orbit');
-      var crewMember1 = new CrewMember('Rick Martinez', 'pilot', 'chemistry');
-      var hermes = new Ship('Hermes', 'Main Ship', 'Interplanetary Space Travel');
-      var crewMember2 = new CrewMember('Commander Lewis', 'commander', 'geology');
-      assert.equal(mav.missionStatement(), "Can't perform a mission yet.");
-      assert.equal(hermes.missionStatement(), "Can't perform a mission yet.");
-
-      crewMember1.enterShip(mav);
-      assert.equal(mav.missionStatement(), "Ascend into low orbit");
-
-      crewMember2.enterShip(hermes);
-      assert.equal(hermes.missionStatement(), "Interplanetary Space Travel");
-    });
-  });
 }
