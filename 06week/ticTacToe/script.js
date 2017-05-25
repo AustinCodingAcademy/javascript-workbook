d34b6ca728b3e9d23bca1921f62107ee41689cdd
Greg Devany <greg.devany@yahoo.com>
2017-05-25 09:32:28 -0500

    TicTacToe- checkWin() recoded with class example2

--- 06week/ticTacToe/script.js
+++ 06week/ticTacToe/script.js
@@ -32,40 +32,62 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 // Check to see if there's a winner function
+  // function checkWin() {
+  //
+  //   if((document.querySelector('[data-cell="0"]').innerText === playerTurn &&
+  //      document.querySelector('[data-cell="3"]').innerText === playerTurn &&
+  //      document.querySelector('[data-cell="6"]').innerText === playerTurn
+  //    ) || (document.querySelector('[data-cell="1"]').innerText === playerTurn &&
+  //         document.querySelector('[data-cell="4"]').innerText === playerTurn &&
+  //         document.querySelector('[data-cell="7"]').innerText === playerTurn
+  //       ) || (document.querySelector('[data-cell="2"]').innerText === playerTurn &&
+  //            document.querySelector('[data-cell="5"]').innerText === playerTurn &&
+  //            document.querySelector('[data-cell="8"]').innerText === playerTurn
+  //          ) || (document.querySelector('[data-cell="0"]').innerText === playerTurn &&
+  //               document.querySelector('[data-cell="1"]').innerText === playerTurn &&
+  //               document.querySelector('[data-cell="2"]').innerText === playerTurn
+  //             ) || (document.querySelector('[data-cell="3"]').innerText === playerTurn &&
+  //                  document.querySelector('[data-cell="4"]').innerText === playerTurn &&
+  //                  document.querySelector('[data-cell="5"]').innerText === playerTurn
+  //                ) || (document.querySelector('[data-cell="6"]').innerText === playerTurn &&
+  //                     document.querySelector('[data-cell="7"]').innerText === playerTurn &&
+  //                     document.querySelector('[data-cell="8"]').innerText === playerTurn
+  //                   ) || (document.querySelector('[data-cell="0"]').innerText === playerTurn &&
+  //                        document.querySelector('[data-cell="4"]').innerText === playerTurn &&
+  //                        document.querySelector('[data-cell="8"]').innerText === playerTurn
+  //                      ) || (document.querySelector('[data-cell="2"]').innerText === playerTurn &&
+  //                           document.querySelector('[data-cell="4"]').innerText === playerTurn &&
+  //                           document.querySelector('[data-cell="6"]').innerText === playerTurn
+  //                         )
+  //     )
+  //       {
+  //       return true;
+  //      } else {
+  //        return false;
+  //      }
+  //    }
+
+// });
+
   function checkWin() {
+    const winningCells = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ];
 
-    if((document.querySelector('[data-cell="0"]').innerText === playerTurn &&
-       document.querySelector('[data-cell="3"]').innerText === playerTurn &&
-       document.querySelector('[data-cell="6"]').innerText === playerTurn
-     ) || (document.querySelector('[data-cell="1"]').innerText === playerTurn &&
-          document.querySelector('[data-cell="4"]').innerText === playerTurn &&
-          document.querySelector('[data-cell="7"]').innerText === playerTurn
-        ) || (document.querySelector('[data-cell="2"]').innerText === playerTurn &&
-             document.querySelector('[data-cell="5"]').innerText === playerTurn &&
-             document.querySelector('[data-cell="8"]').innerText === playerTurn
-           ) || (document.querySelector('[data-cell="0"]').innerText === playerTurn &&
-                document.querySelector('[data-cell="1"]').innerText === playerTurn &&
-                document.querySelector('[data-cell="2"]').innerText === playerTurn
-              ) || (document.querySelector('[data-cell="3"]').innerText === playerTurn &&
-                   document.querySelector('[data-cell="4"]').innerText === playerTurn &&
-                   document.querySelector('[data-cell="5"]').innerText === playerTurn
-                 ) || (document.querySelector('[data-cell="6"]').innerText === playerTurn &&
-                      document.querySelector('[data-cell="7"]').innerText === playerTurn &&
-                      document.querySelector('[data-cell="8"]').innerText === playerTurn
-                    ) || (document.querySelector('[data-cell="0"]').innerText === playerTurn &&
-                         document.querySelector('[data-cell="4"]').innerText === playerTurn &&
-                         document.querySelector('[data-cell="8"]').innerText === playerTurn
-                       ) || (document.querySelector('[data-cell="2"]').innerText === playerTurn &&
-                            document.querySelector('[data-cell="4"]').innerText === playerTurn &&
-                            document.querySelector('[data-cell="6"]').innerText === playerTurn
-                          )
-      )
-        {
-        return true;
-       } else {
-         return false;
-       }
-     }
-
+    return winningCells.some((combo) => {
+      return combo.every((letter) => {
+        return document.querySelector(`[data-cell="${letter}"]`).innerText === playerTurn;
+      });
+    });
+  }
 });
+// stop
+
 // https://gdevany.github.io/javascript-workbook/06week/ticTacToe/index.html
