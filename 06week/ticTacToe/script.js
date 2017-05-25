b1bed1b138363879a2e634119e215518fcb5fb12
stackbaxter <stackbaxter@yahoo.com>
2017-05-24 20:23:53 -0500

    working the dom

--- 06week/ticTacToe/script.js
+++ 06week/ticTacToe/script.js
@@ -2,57 +2,41 @@
 //neeed a ) somewhere down there.
 document.addEventListener('DOMContentLoaded', () => {
   let playerTurn = 'X';
-    let cells = document.querySelectorAll('[data-cell]');
-  document.querySelectorAll('[data-cell]').forEach(cell => {cell.onclick = function(e){
-    this.innerText = (playerTurn === 'X') ? 'X': 'O';
-  //  i++;
-     checkForWin(playerTurn);
-    playerTurn === 'X'?playerTurn = 'O': playerTurn = 'X';
-    console.log(this);}})
+  let cells = document.querySelectorAll('[data-cell]');
+  document.querySelectorAll('[data-cell]').forEach(cell => {
+    cell.onclick = function (e) {
+      this.innerText = (playerTurn === 'X') ? 'X' : 'O';
+      //  i++;
+      console.log(checkForWin(playerTurn));
+      if (checkForWin(playerTurn)) {
+        document.querySelector('#announce-winner').innerText = `Player "${playerTurn}" Wins!`;
+      }
+      playerTurn === 'X' ? playerTurn = 'O' : playerTurn = 'X';
+      console.log(this);
+    }
+  })
 
-    function checkForWin(playerTurn){
+  function checkForWin(playerTurn) {
+    const winningCells = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+    return winningCells.some((combo) => {
+      return combo.every((idx) => {
+        return document.querySelector(`[data-cell="${idx}"]`).innerText === playerTurn;
+      })
+    })
+  }
 
-      // for(let i = 0; i < 3; i +
-      console.log(cells)
-      // let i = 0;
-      // while(i < 9){
-        if(
-          cells[0].innerText === playerTurn &&
-          cells[3].innerText === playerTurn &&
-          cells[6].innerText === playerTurn ||
-
-          cells[1].innerText === playerTurn &&
-          cells[4].innerText === playerTurn &&
-          cells[7].innerText === playerTurn ||
-
-          cells[2].innerText === playerTurn &&
-          cells[5].innerText === playerTurn &&
-          cells[8].innerText === playerTurn ||
-
-          cells[2].innerText === playerTurn &&
-          cells[1].innerText === playerTurn &&
-          cells[0].innerText === playerTurn ||
-
-          cells[5].innerText === playerTurn &&
-          cells[4].innerText === playerTurn &&
-          cells[3].innerText === playerTurn ||
-
-          cells[6].innerText === playerTurn &&
-          cells[7].innerText === playerTurn &&
-          cells[8].innerText === playerTurn ||
-
-          cells[0].innerText === playerTurn &&
-          cells[4].innerText === playerTurn &&
-          cells[8].innerText === playerTurn ||
-
-          cells[6].innerText === playerTurn &&
-          cells[4].innerText === playerTurn &&
-          cells[2].innerText ===playerTurn
-          ){
-              alert ('You Win!');
-          };
-}
-        }
-)
-
-document.getElementById('clear').onclick = function(e){document.querySelectorAll('[data-cell]').forEach(cell => cell.innerText = "")};
+  document.getElementById('clear').onclick = function (e) {
+    document.querySelectorAll('[data-cell]').forEach(cell => cell.innerText = "");
+    playerTurn = 'X';
+    document.querySelector('#announce-winner').innerText = '';
+  };
+})
--- 06week/towersOfHanoi/script.js
+++ 06week/towersOfHanoi/script.js
@@ -1,5 +1,30 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Your code here
-});
+  let stack = document.querySelectorAll('[data-stack]');
+  console.log(stack);
+  let ring = null;
+  stack.forEach(cell => {cell.onclick = function(e){
+
+      if(ring === 'null'){
+        ring = this.children().last().detach();
+        console.log($ring);
+        console.log($(this).children().length);
+        }
+
+      else if ($(this).children().length === 0 || ($ring.data('block') < $(this).children().last().data('block') ))
+       {
+         $(this).append($ring);
+         $ring = 'null';
+         checkWin();
+       }
+
+     }
+
+
+    if(this.length > 0){
+
+    }
+  }
+}))
+  );
