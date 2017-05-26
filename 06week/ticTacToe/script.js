62ca0756bb798a909858e65061d3f63a5d7d0f06
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-05-25 20:44:14 -0500

    Toggle player function is implemented

--- 06week/ticTacToe/script.js
+++ 06week/ticTacToe/script.js
@@ -2,4 +2,13 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   // Your code here
-});
+  let playerTurn = 'X'; //assign first player's move to X
+
+  document.querySelectorAll('[data-cell]').forEach(cell => {
+    cell.addEventListener ('click', function() { //target each div when it's clicked/selected
+      this.innerText = playerTurn; //assign current data cell to X
+      playerTurn = (playerTurn === 'X') ? 'O' : 'X';
+    }); //addEventListener function ends
+  }) //forEach function ends
+
+}); //addEventListener function ends
