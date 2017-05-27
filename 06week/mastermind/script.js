91dbfd011c2e0ecf6def2962c39445c59dde1f45
Greg Devany <greg.devany@yahoo.com>
2017-05-27 18:58:36 -0500

    Checkpoint2- WIP Almost done with functionality

--- 06week/mastermind/script.js
+++ 06week/mastermind/script.js
@@ -5,73 +5,72 @@ document.addEventListener('DOMContentLoaded', () => {
 
   document.querySelector('button').onclick = function (event) {
     event.preventDefault();
+
+    let board = [];
+    let solution = 'abcd';
+    const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
     let guess = document.querySelector('input').value;
     mastermind(guess);
-
     let newDiv = document.createElement('div');
     newDiv.innerText = printBoard() + message;
-    console.log(board);
-
+console.log(board);
     document.querySelector('#board').appendChild(newDiv);
-  }
 
-  let board = [];
-  let solution = 'abcd';
-  const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+    function mastermind(guess) {
+      let count = 0;
+      board = [];
+      let gs = [];
+      for (var i = 0; i < 4; i++) {
+        gs.push(guess[i]);
+      }
 
-  function mastermind(guess) {
-    let count = 0;
-    board = [];
-    let gs = [];
-    for (var i = 0; i < 4; i++) {
-      gs.push(guess[i]);
+      generateHint(solution, gs);
+
+      count++;
+
+      if (board[4] === '4-0') {
+        message = '  You guessed it!';
+      }
+      board = board.join('').split(',');
+      message = '  Try again';
     }
 
-    generateHint(solution, gs);
+    function generateHint(solution, gs) {
+      //check for reds
+      board = [];
+      let redCount = 0;
+      for (var i = 0; i <= gs.length-1; i++) {
+        if (solution[i] === gs[i]) redCount++;
+      }
 
-    count++;
-
-    if (board[4] === '4-0') {
-      message = '  You guessed it!';
-    }
-    board = board.join('').split(',');
-    message = '  Try again';
-  }
-
-  function generateHint(solution, gs) {
-    //check for reds
-    board = [];
-    let redCount = 0;
-    for (var i = 0; i <= gs.length-1; i++) {
-      if (solution[i] === gs[i]) redCount++;
-    }
-
-    //check for white
-    let whiteCount = 0;
-    for (var i = 0; i <= solution.length-1; i++) {
-      let current = '';
-      for (var j = 0; j <= gs.length-1; j++) {
-        if (current !== solution[i]) {
-          if (solution[i] === gs[j] && solution[i] !== gs[i]) {
-            whiteCount++;
-            current = solution[i];
+      //check for white
+      let whiteCount = 0;
+      for (var i = 0; i <= solution.length-1; i++) {
+        let current = '';
+        for (var j = 0; j <= gs.length-1; j++) {
+          if (current !== solution[i]) {
+            if (solution[i] === gs[j] && solution[i] !== gs[i]) {
+              whiteCount++;
+              current = solution[i];
+            }
           }
         }
       }
+
+      //log results
+      for (var i = 0; i <= gs.length-1; i++) {
+        board.push(gs[i]);
+      }
+      board.push(redCount + '-' + whiteCount);
+      return(board[4]);
     }
 
-    //log results
-    for (var i = 0; i <= gs.length-1; i++) {
-      board.push(gs[i]);
+  function printBoard() {
+    for (let i = 0; i <= board.length-1; i++) {
+      return(board[i]);
     }
-    board.push(redCount + '-' + whiteCount);
-    return(board[4]);
   }
 
-function printBoard() {
-  for (let i = 0; i <= board.length-1; i++) {
-    return(board[i]);
-  }
 }
   // let board = [];
   // let solution = 'abcd';
