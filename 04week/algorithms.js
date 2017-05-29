f3e5a95d989b45d44ba047823977da5726b745bf
Ericka Emery <ericka.emery@gmail.com>
2017-05-29 18:26:38 -0500

    Algorithms: MergeSort complete, tabs corrected

--- 04week/algorithms.js
+++ 04week/algorithms.js
@@ -38,23 +38,23 @@ function mergeSort(arr) {
   while (sortedFirst.length && sortedSecond.length) {
   // if the first item in the first array is smaller, push
   //it in the sorted array
-  if (sortedFirst[0] < sortedSecond[0]) {
+    if (sortedFirst[0] < sortedSecond[0]) {
     sortedArr.push(sortedFirst.shift());
   } else {
     // else push the first item in the second array
     sortedArr.push(sortedSecond.shift());
   }
-}
+  }
 
 // eventually one of the arrays are going to be emptied,
 // meaning the other one will be presorted and attachable
 // to the end
-if(!sortedFirst.length) {
+  if(!sortedFirst.length) {
   sortedArr = sortedArr.concat(sortedSecond);
 } else if (!sortedSecond.length) {
   sortedArr = sortedArr.concat(sortedFirst);
 }
-return sortedArr;
+  return sortedArr;
 }
 
 console.log(mergeSort([10,9,8,7,6,5]))
