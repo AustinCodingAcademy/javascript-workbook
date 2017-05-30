62a172cecc624c17ebdcc6c3831955dcec6f6145
stackbaxter <stackbaxter@gmail.com>
2017-05-30 01:22:06 -0500

    almost there
    need win condition and getting the smallest to sit on top of the
    biggest.

--- 06week/towersOfHanoi/script.js
+++ 06week/towersOfHanoi/script.js
@@ -12,30 +12,18 @@ document.addEventListener('DOMContentLoaded', () => {
   let ring = null;
 
   stack.forEach(pole => {pole.onclick = function(e){
-    stack.forEach( stacks => {stacks.onclick = function (e)  {
 //remove child, addchild and stacks[0].lastChild.dataset.block or something
-      if(this.length !== 0){
-        if(ring === null){
+      if(this.children.length !== 0 && ring === null){
         ring = this.removeChild(this.lastElementChild);
         }
-        console.log(this.length)
-        // ring = this.lastChild;
-        console.log(ring);
+        else if(this.children.length === 0 && ring != null){
+           this.appendChild(ring);
+           ring = null;
         }
-
-       else if (this.length === 0 && ring != null){
-         this.addchild(ring);
-
-       }
-    //|| ($ring.data('block') < $(this).children().last().data('block') ))
-      //  {
-      //    $(this).append($ring);
-      //    $ring = 'null';
-      //    checkWin();
-      //  }
-
-    }});
-  }});
-
-
-});
+        else if(ring != null && this.lastElementChild.getAttribute('data-block') > ring.getAttribute('data-block')){
+         this.appendChild(ring);
+         ring = null;
+        }
+    }
+     });
+   });
