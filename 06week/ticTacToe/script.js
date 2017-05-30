510f5ac22d1f5b85ff41d599a5ddfce4b2d0da5b
demarcogleason <demarcogleason@gmail.com>
2017-05-30 15:27:36 -0500

    tic tac toe

--- 06week/ticTacToe/script.js
+++ 06week/ticTacToe/script.js
@@ -2,18 +2,36 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   // Your code here
+  let playerTurn = 'X';
+  document.querySelectorAll('[data-cell]').forEach(cell => {
+    cell.addEventListener('click', function() {
+      this.innerText = playerTurn;
+      if (checkForWin()) {
+        document.querySelector('#announce-winner').innerText = `Player ${playerTurn} Wins!`;
+      }
+      playerTurn = (playerTurn === 'X') ? 'O' : 'X';
+    });
+  });
 
-
-function announceWinner(){
-if document.getElementById("announceWinner")
-}
-  function clear(){
-    var restart = document.getElementById("clear");
-    location.reload();
+  function checkForWin() {
+    const threeInRow = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    return threeInRow.some(order => {
+      if (
+      document.querySelector(`[data-cell="${order[0]}"]`).innerText === playerTurn &&
+      document.querySelector(`[data-cell="${order[1]}"]`).innerText === playerTurn &&
+      document.querySelector(`[data-cell="${order[2]}"]`).innerText === playerTurn
+    ) {
+      return true;
+      }
+    });
   }
-
-  function getelements(){
-    return document.querySelectorAll(".square");
-  }
-  //set inner html
 });
