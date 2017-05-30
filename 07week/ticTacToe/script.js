56ec18d9f768a22a405e48eb8e4f0ef193d8d2e2
stackbaxter <stackbaxter@yahoo.com>
2017-05-29 20:41:07 -0500

    react!

--- 07week/ticTacToe/script.js
+++ 07week/ticTacToe/script.js
@@ -3,29 +3,53 @@
 class TicTacToe extends React.Component {
   constructor(props) {
     super(props);
+    this.state ={
+      0: '',
+      1: '',
+      2: '',
+      3: '',
+      4: '',
+      5: '',
+      6: '',
+      7: '',
+      8: '',
+    playerTurn : 'X',
+    won: 'none'
+    };
+  }
+
+  clickCell = (e) => {
+    const obj = {};
+    obj[e.target.getAttribute('data-cell')] = this.state.playerTurn;
+    obj['playerTurn'] = this.state.playerTurn ==='X'? 'O': 'X';
+    this.setState(obj);
+    //
+    // this.setState({
+    //   playerTurn: this.state.playerTurn === 'X'?'O':'X'
+    // })
   }
 
   render() {
     return (
       <div>
         <div className="row">
-          <div data-cell="0"></div>
-          <div data-cell="1"></div>
-          <div data-cell="2"></div>
+          <div data-cell="0" onClick={this.clickCell}>{this.state[0]}</div>
+          <div data-cell="1" onClick={this.clickCell}>{this.state[1]}</div>
+          <div data-cell="2" onClick={this.clickCell}>{this.state[2]}</div>
         </div>
         <div className="row">
-          <div data-cell="3"></div>
-          <div data-cell="4"></div>
-          <div data-cell="5"></div>
+          <div data-cell="3" onClick={this.clickCell}>{this.state[3]}</div>
+          <div data-cell="4" onClick={this.clickCell}>{this.state[4]}</div>
+          <div data-cell="5" onClick={this.clickCell}>{this.state[5]}</div>
         </div>
         <div className="row">
-          <div data-cell="6"></div>
-          <div data-cell="7"></div>
-          <div data-cell="8"></div>
+          <div data-cell="6" onClick={this.clickCell}>{this.state[6]}</div>
+          <div data-cell="7" onClick={this.clickCell}>{this.state[7]}</div>
+          <div data-cell="8" onClick={this.clickCell}>{this.state[8]}</div>
         </div>
       </div>
     );
-  }
+  };
 }
 
 ReactDOM.render(<TicTacToe />, document.getElementById('tic-tac-toe'));
