30ab8d3dc242d0493ef438bc40a298696d7ebf3d
stackbaxter <stackbaxter@gmail.com>
2017-05-31 01:02:21 -0500

    got it, minor deets, polishing left

--- 06week/towersOfHanoi/script.js
+++ 06week/towersOfHanoi/script.js
@@ -1,25 +1,25 @@
 'use strict';
 
   document.addEventListener('DOMContentLoaded', () => {
-    let stack = document.querySelectorAll('[data-stack]');
-    let rings = document.querySelectorAll('[data-block]');
-    let ring = null;
-
+  let stack = document.querySelectorAll('[data-stack]');
+  let ring = null;
 //listen to the stacks, everything happens on the click
+
     stack.forEach(pole => {pole.onclick = function(e){
       //picking up a ring
     if(this.children.length !== 0 && ring === null){
       ring = this.removeChild(this.lastElementChild);
         console.log(ring);
     }
-    else if(ring != null){// putting down a ring
-      if(this.children.length === 0){
+    else if(this.children.length === 0 && ring != null){// putting down a ring
            this.appendChild(ring);
            ring = null;
          }//casting the data-block to int for compare
     else if (parseInt(this.lastElementChild.getAttribute('data-block')) > parseInt(ring.getAttribute('data-block'))) {
         this.appendChild(ring);
-        checkWin();
+        if(this.childElementCount === 4){
+            document.getElementById('announce-game-won').innerText = "You Won!"
+        };
         ring = null;
       }
     else{
@@ -28,14 +28,5 @@
         console.log('Illegal Move!');
       }
     }
-
-
-  }})
-  checkWin = function(){
-    if(this.children.length = 4 && this.lastElementChild.getAttribute('data-block') === 25){
-      document.getElementById('announce-game-won').innerText = "You Won!"
-    }
-
-  }
-
   })
+})
