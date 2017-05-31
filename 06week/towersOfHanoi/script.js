fde6a3634aa297e956dbdbf8cf58235345437d53
stackbaxter <stackbaxter@gmail.com>
2017-05-30 23:53:40 -0500

    need win

--- 06week/towersOfHanoi/script.js
+++ 06week/towersOfHanoi/script.js
@@ -5,17 +5,25 @@
     let rings = document.querySelectorAll('[data-block]');
     let ring = null;
 
+//listen to the stacks
     stack.forEach(pole => {pole.onclick = function(e){
+      //picking up a ring
     if(this.children.length !== 0 && ring === null){
       ring = this.removeChild(this.lastElementChild);
         console.log(ring);
       }
       else if(ring != null){
-         if(this.children.length === 0 || this.lastElementChild.getAttribute('data-block') > ring.getAttribute('data-block') ){
-        this.appendChild(ring);
-        ring = null;
+         if(this.children.length === 0){
+           this.appendChild(ring);
+           ring = null;
+         }
+        else if (parseInt(this.lastElementChild.getAttribute('data-block')) > parseInt(ring.getAttribute('data-block'))) {
+          this.appendChild(ring);
+          ring = null;
         }
       else{
+        console.log(this.lastElementChild.getAttribute('data-block'));
+        console.log(ring);
         console.log('Illegal Move!');
       }
     }
