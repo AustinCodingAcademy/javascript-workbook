5bc274fac6b3403e6bca9f6d608e168978c02121
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-06-01 16:13:36 -0500

    App works

--- 07week/ticTacToe/script.js
+++ 07week/ticTacToe/script.js
@@ -4,8 +4,8 @@ class TicTacToe extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      0: '',
+    this.state = { //create your state
+      0: '', //this each value will be either 'X' or 'O'
       1: '',
       2: '',
       3: '',
@@ -14,19 +14,34 @@ class TicTacToe extends React.Component {
       6: '',
       7: '',
       8: '',
-      playerTurn: 'X',
-      won: 'none'
+      playerTurn: 'X', //set player Turn to X in the beginning
     };
-  }
+  } //state ends
 
-  clickCell = (e) => {
+  clickCell = (event) => { //when a data-cell is clicked
     const obj = {}; //create an empty object in order to pass in attribute 'data-cell'
-    obj[e.target.getAttribute('data-cell')] = this.state.playerTurn; //target data-cell
-    obj['playerTurn'] = this.state.playerTurn === 'X' ? 'O' : 'X'; //toggle player between X and O
-    this.setState(obj);
+    obj[event.target.getAttribute('data-cell')] = this.state.playerTurn; //target data-cell
+    obj['playerTurn'] = (this.state.playerTurn === 'X') ? 'O' : 'X'; //toggle player between X and O
+    this.setState(obj); //pass in that object
+
+
   }//clickCell function ends
 
   render() {
+
+    const winningCells = [ [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]; /*declare winning cells and
+                                                                                              assign them to an array*/
+
+    let playerWon = '';
+    winningCells.some((combo) => { //go through each array and check whether the condition is true
+      if (this.state[combo[0]] && //if first cell is not empty and
+          this.state[combo[0]] === this.state[combo[1]] && //first cell is equal to second cell e.g. X===X and
+          this.state[combo[0]] === this.state[combo[2]]) {  //first cell is equal to third cell e.g. X===X
+            playerWon = this.state[combo[0]];
+          }
+    });//winningCells.some() function ends
+
+
     return (
       <div>
         <div>Player {this.state.playerTurn}'s Turn</div>
@@ -48,11 +63,11 @@ class TicTacToe extends React.Component {
           </div>
         </div>
         <br />
-        <div style={ {display: this.state.won} }>Player {this.state.playerTurn} Won!</div>
+        <div>Player {playerWon} won!</div>
       </div>
 
-    );
-  }
-}
+    ); //return ends
+  } //render ends
+} //class ends
 
 ReactDOM.render(<TicTacToe />, document.getElementById('tic-tac-toe'));
