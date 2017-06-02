7b52719d09201bf39327839e6c3463e357a31710
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-06-01 22:08:27 -0500

    Blocks can be removed from stack

--- 07week/towersOfHanoi/script.js
+++ 07week/towersOfHanoi/script.js
@@ -3,24 +3,42 @@
 class TowersOfHanoi extends React.Component {
   constructor(props) {
     super(props);
-  }
+    this.state = { //declare state
+      1: [100,75,50,25], //data-stack-1
+      2: [], //data-stack-2
+      3: [], //data-stack-3
+      block: null //declare block and assign to null in the beginning
+    };
+  } //constructor ends
+
+  moveBlock = (event) => { //when it's clicked
+    const stack = event.target.getAttribute('data-stack'); //select the stack
+    const newStack = this.state[stack].slice(); //make a copy of the array to remove items
+    const block = newStack.pop(); //remove the last element and assign to block
+    const obj = {}; //create an empty object to hold stack and block variables
+    obj[stack] = newStack;
+    obj[block] = block;
+    this.setState(obj); //pass the object in
+  }//moveBlock ends
 
   render() {
+
+    const block1 = this.state[1].map((block) => {
+      return (<div key={block} data-block={block}></div>);
+    });
+
     return (
       <div>
-        <div data-stack="1">
-          <div data-block="100"></div>
-          <div data-block="75"></div>
-          <div data-block="50"></div>
-          <div data-block="25"></div>
+        <div data-stack="1" onClick={this.moveBlock}>
+          {block1}
         </div>
         <div data-stack="2">
         </div>
         <div data-stack="3">
         </div>
       </div>
-    );
-  }
-}
+    ); //return ends
+  } //render ends
+}//class ends
 
 ReactDOM.render(<TowersOfHanoi />, document.getElementById('towers-of-hanoi'));
