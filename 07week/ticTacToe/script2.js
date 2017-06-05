d25438af2efe9117c19b6891f5737d27a84b0103
meganroone <emailgena@gmail.com>
2017-06-05 13:25:59 -0500

    TTT correct

--- 07week/ticTacToe/script2.js
+++ 07week/ticTacToe/script2.js
@@ -33,9 +33,7 @@ class TicTacToe extends React.Component {
 
     this.setState(obj);
 
-    this.checkWin();
-
-
+    //this.checkWin();
 
   }
 
@@ -90,52 +88,3 @@ class TicTacToe extends React.Component {
 }
 
 ReactDOM.render(<TicTacToe />, document.getElementById('tic-tac-toe'));
-
-
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-// 'use strict';
-//
-// class TicTacToe extends React.Component {
-//   constructor (props) {
-//     super(props);
-//     this.state ={}
-//   }
-//
-//   render() {
-//     return (
-//       <div>
-//       <div> Player {this.state.PlayerTurn}'s Turn</div>
-//       <div>
-//       <div className="row">
-//       <div data-cell="0" onClick={this.clickCell}>{this.state[0]}</div>
-//       <div data-cell="1" onClick={this.clickCell}>{this.state[1]}</div>
-//       <div data-cell="2" onClick={this.clickCell}>{this.state[2]}</div>
-//       </div>
-//       <div className="row">
-//       <div data-cell="3" onClick={this.clickCell}>{this.state[3]}</div>
-//       <div data-cell="4" onClick={this.clickCell}>{this.state[4]}</div>
-//       <div data-cell="5" onClick={this.clickCell}>{this.state[5]}</div>
-//       </div>
-//       <div className="row">
-//       <div data-cell="6" onClick={this.clickCell}>{this.state[6]}</div>
-//       <div data-cell="7" onClick={this.clickCell}>{this.state[7]}</div>
-//       <div data-cell="8" onClick={this.clickCell}>{this.state[8]}</div>
-//       </div>
-//       </div>
-//       <br/>
-//       <div style={{display: this.state.won}}>Player (this.state.PlayerTurn} Won!</div>
-//         </div>
-//       );
-//     }
-//   }
-//   ReactDOM.render(<TicTacToe />, document.getElementById('tic-tac-toe'));
