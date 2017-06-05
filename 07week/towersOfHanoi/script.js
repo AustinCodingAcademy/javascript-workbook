afe8d81f32cbf3880b0ed6dba1a2e89f4b04e529
Greg Devany <greg.devany@yahoo.com>
2017-06-05 00:04:56 -0500

    TowersOfHanoi- React - WIP; pickup-check, putDown-Need to think about it

--- 07week/towersOfHanoi/script.js
+++ 07week/towersOfHanoi/script.js
@@ -3,20 +3,51 @@
 class TowersOfHanoi extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+        a: [100,75,50,25],
+        b: [],
+        c: [],
+        blockHeld: null
+    }
+  }
+
+  moveBlock = (event) => {
+
+
+    const clickedStack = event.target.getAttribute('data-stack');
+    const blocks = this.state[clickedStack].slice();
+    const block = blocks.pop();
+    const obj = {};
+    obj[clickedStack] = blocks;
+    obj.block = block;
+    this.setState(obj);
+
   }
 
   render() {
+
+    const aBlocks = this.state.a.map((block) => {
+      return (<div data-block={block}></div>)
+    });
+
+    const bBlocks = this.state.b.map((block) => {
+      return (<div data-block={block}></div>)
+    });
+
+    const cBlocks = this.state.c.map((block) => {
+      return (<div data-block={block}></div>)
+    });
+
     return (
       <div>
-        <div data-stack="1">
-          <div data-block="100"></div>
-          <div data-block="75"></div>
-          <div data-block="50"></div>
-          <div data-block="25"></div>
+        <div data-stack="a" onClick={this.moveBlock}>
+          {aBlocks} <div className="clicker">click here</div>
         </div>
-        <div data-stack="2">
+        <div data-stack="b" onClick={this.moveBlock}>
+          {bBlocks} <div className="clicker">click here</div>
         </div>
-        <div data-stack="3">
+        <div data-stack="c" onClick={this.moveBlock}>
+          {cBlocks} <div className="clicker">click here</div>
         </div>
       </div>
     );
