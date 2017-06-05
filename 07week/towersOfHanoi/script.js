33cf71ddae0e777a9fa1503a1e48253a88f366f4
stackbaxter <stackbaxter@gmail.com>
2017-06-05 15:57:25 -0500

    very slow progress

--- 07week/towersOfHanoi/script.js
+++ 07week/towersOfHanoi/script.js
@@ -4,40 +4,37 @@ class TowersOfHanoi extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      a: [25,50,75,100],
+      a: [25, 50, 75, 100],
       b: [],
       c: [],
       block: null
-
     }
   }
 
-moveBlock(event) => {
-  const stack = event.target.getAttribute('data-stack');
-  const block = this.state[stack].slice();
-  const = block.pop();
-  const obj = {};
-  obj[stack] = blocks;
-  obj.block = block;
-  this.setState(obj);
-}
 
+  moveBlock = (event) => {
+    const stack = event.target.getAttribute('data-stack');
+    const blocks = this.state[stack].slice();
+    const block = blocks.pop();
+    const obj = {};
+    obj[stack] = blocks;
+    obj.block = block;
+    this.setState(obj);
+  }
 
   render() {
-    const aBlocks = this.state.a.map((block) ={
-      return(<div key = {block} data=block ={block}><)
-    })
+    const aBlocks = this.state.a.map((block) => {
+      return (<div key={block} data-block={block}></div>)
+    });
+
     return (
       <div>
-        <div data-stack="a">
-          <div data-block="100"></div>
-          <div data-block="75"></div>
-          <div data-block="50"></div>
-          <div data-block="25"></div>
+        <div data-stack="a" onClick={this.moveBlock}>
+          {aBlocks}
         </div>
-        <div data-stack="b">
+        <div data-stack="b" onClick={this.moveBlock}>
         </div>
-        <div data-stack="c">
+        <div data-stack="c" onClick={this.moveBlock}>
         </div>
       </div>
     );
