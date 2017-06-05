b34248077d76e2f350950b6ba1c2c12cae263947
jdrowlett <jdrowlett@outlook.com>
2017-06-04 20:19:47 -0500

    tic tac toe in DOM

--- 06week/ticTacToe/script.js
+++ 06week/ticTacToe/script.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', (index.html) => {
     for(i = 0; i < 10; i++); {
       console.log();
     }
-    //horizontal win
+    //Horizontal win
     document.getElementsByClassName('row'), () => {
       if(playerX === [1, 2, 3] || [4, 5, 6] || [7, 8, 9]) {
         return alert "Player X wins!";
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', (index.html) => {
         return alert "Player O wins!";
       }
     }
-    //vertical win
+    //Vertical win
     document.getElementsByTagName('column'), () => {
       if(playerX === [1, 4, 7] || [2, 5, 8] || [3, 6, 9]) {
         return alert "Player X wins!";
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', (index.html) => {
         return alert "Player O wins!";
       }
     }
-    //diagonal win
+    //Diagonal win
       if(playerX === [1, 5, 9] || [3, 5, 7]) {
         return alert "Player X wins!";
       }
