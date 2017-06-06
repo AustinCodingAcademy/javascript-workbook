cfae38a6c820fd1e4a0803c0475a9a9b4545f9e7
stackbaxter <stackbaxter@gmail.com>
2017-06-06 00:26:30 -0500

    win condition

--- 07week/towersOfHanoi/script.js
+++ 07week/towersOfHanoi/script.js
@@ -12,23 +12,32 @@ class TowersOfHanoi extends React.Component {
   }
 
   clickStack = (e) => {
+
     const letter = e.target.getAttribute('data-stack');
     const stack = this.state[letter].slice();//clone of the array (shallow clone)
+    let hand = this.state.block;
     const obj = {};//doing this so you can use a variable for the key in referencing the object
     obj[letter] = stack;
     if (!this.state.block){
-      this.state.block = stack.pop();
-      obj['block']= block;
+      hand = stack.pop();
+      obj['block'] = hand;
     }
-    else{
+    else if(stack.length === 0 || hand < stack[stack.length-1]){
       stack.push(this.state.block);
       obj['block'] = null;
+      this.gameWin(letter, stack);
     }
+
     this.setState(obj);
+
 
 }
 
-
+gameWin = (letter, stack) => {
+  if(letter === 'b' || letter === 'c' && stack.length === 4){
+  document.getElementById('gameWinner').innerHtml = "You Won!";
+  }
+}
   // moveBlock = (event) => {
   //   const stack = event.target.getAttribute('data-stack');
   //   const blocks = this.state[stack].slice();
