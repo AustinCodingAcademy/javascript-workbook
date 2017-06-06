5268038254e8726296bdd6c14fd23d8dab867ac0
Greg Swan <greg.c.swan@gmail.com>
2017-06-05 22:45:56 -0500

    finished towers of hanoi in react

--- 07week/towersOfHanoi/script.js
+++ 07week/towersOfHanoi/script.js
@@ -3,20 +3,76 @@
 class TowersOfHanoi extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      a:[100, 75, 50, 25],
+      b:[],
+      c:[],
+      block: null,
+      win: ''
+
+
+    }
   }
 
+  clickStack = (e) => {
+
+    const letter = e.target.getAttribute('data-stack');
+    const stack = this.state[letter].slice();
+    let block;
+
+    if (!this.state.block){
+      block = stack.pop();
+      console.log(block)
+    }
+    else if(!e.target.lastElementChild) {
+      stack.push(this.state.block);
+      block = null;
+    }
+    else if(e.target.lastElementChild && this.state.block < Number(e.target.lastElementChild.getAttribute('data-block'))) {
+      stack.push(this.state.block);
+      block = null;
+    }
+    else{
+      return false;
+    }
+
+    const update = {}
+    update[letter] = stack
+    update['block'] = block;
+
+    if (update[letter].length === 4) {
+     update['win'] = "YOU WON!"
+    }
+
+    this.setState(update);
+
+  }
+
+
   render() {
+    const aBlocks = this.state.a.map((size) => {
+      return (<div data-block={size}></div>);
+    });
+    const bBlocks = this.state.b.map((size) => {
+      return (<div data-block={size}></div>);
+    });
+    const cBlocks = this.state.c.map((size) => {
+      return (<div data-block={size}></div>);
+    });
+
     return (
       <div>
-        <div data-stack="1">
-          <div data-block="100"></div>
-          <div data-block="75"></div>
-          <div data-block="50"></div>
-          <div data-block="25"></div>
+        <div onClick={this.clickStack} data-stack="a">
+          {aBlocks}
         </div>
-        <div data-stack="2">
+        <div onClick={this.clickStack} data-stack="b">
+          {bBlocks}
         </div>
-        <div data-stack="3">
+        <div onClick={this.clickStack} data-stack="c">
+          {cBlocks}
+        </div>
+        <div>
+          {this.state.win}
         </div>
       </div>
     );
