c35358c28d96b2bb2d93c33e350738e6eb30001f
stuartkilgore <stuart.kilgore@gmail.com>
2017-06-05 20:40:12 -0500

    fetch stuff i guess

--- 08week/fetch/script.js
+++ 08week/fetch/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty').then((response) => {
+  response.json().then((storyIds) => {
+    storyIds.forEach((storyId) => {
+      fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json?print=pretty`).then((response) => {
+        response.json().then((story) => {
+          const a = document.createElement('a');
+          a.href = story.url;
+          a.innerText = story.title;
+          a.target = 'blank';
+          const div = document.createElement('div');
+          div.appendChild(a);
+          document.querySelector('#fetch').appendChild(div);
+        })
+      })
+    })
+  });
+});
