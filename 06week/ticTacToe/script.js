a81d26e311628a0d7f120b7461050460d9faa378
Eric <user@users-MacBook-Pro.local>
2017-06-06 18:28:12 -0500

    updating

--- 06week/ticTacToe/script.js
+++ 06week/ticTacToe/script.js
@@ -1,7 +1,16 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-
+let playerTurn = 'X';
+document.querySelectorAll('[data-cell]').forEach(cell => {
+  cell.addEventListener('click', function(){
+    this.innerText = playerTurn;
+    if (checkForWin()){
+      document.querySelector('#announce-winner').innerText = `Player ${playerTurn} Wins!`;
+    }
+    playerTurn = (playerTurn === 'X') ? 'O' : 'X';
+  });
+});
 });
 //
 
@@ -13,4 +22,26 @@ function clearBoard (){
 }
 //could accept inner HTML to each square, select all data-cell set inner Html to empty string
 //Search getElementByDataAttribute
-//set all squares to class = "something" 
+//set all squares to class = "something"
+
+function checkForWin(){
+const threeInRow = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+return threeInRow.some(order =>{
+  if(
+    document.querySelector(`[data-cell = "${order[0]}"]`).innerText === playerTurn &&
+    document.querySelector(`[data-cell = "${order[1]}"]`).innerText === playerTurn &&
+    document.querySelector(`[data-cell = "${order[2]}"]`).innerText === playerTurn
+  ){
+    return true;
+  }
+});
+}
--- 06week/towersOfHanoi/script.js
+++ 06week/towersOfHanoi/script.js
@@ -1,5 +1,36 @@
-'use strict';
 
-document.addEventListener('DOMContentLoaded', () => {
-  // Your code here
+
+  $(document).ready(function () {
+
+
+     let $block = null;
+
+    $('[data-stack]').click(function () {
+      let stack = this;
+      //var $children = $(this).childre();
+      //var $last = $children.last();
+      if ($block === null) {
+        $block = $(stack).children().last().detach();
+      } else if (isLegal() || isEmpty()){
+        $(stack).append($block); //maybe push?
+        $block = null;
+        if (checkForWin()){
+          $('annouce-game-won').text('You Won!');
+        };
+      }
+
+      });
+    function isLegal(stack){
+      return ($block.data('block') < $(stack).children().last()data('block'))
+    }
+    function isEmpty(stack){
+      return $(stack).children().length === 0;
+    }
+
+    function checkForWin(){
+      return $('[data-stack="2"]').children().length===4||
+        $('[data-stack="3"]').children().length===4;
+    }
+  });
+
 });
