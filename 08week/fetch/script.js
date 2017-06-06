d1c8c6792095e692b93a601af206d5bf55097041
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-06-05 20:47:07 -0500

    building top stories page in hackernews API by using fetch

--- 08week/fetch/script.js
+++ 08week/fetch/script.js
@@ -1,1 +1,17 @@
 'use strict';
+fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty').then(res => {
+  res.json().then (storyId => {
+    storyId.forEach(Id => {
+      fetch(`https://hacker-news.firebaseio.com/v0/item/${Id}.json?print=pretty`).then(res => {
+        res.json().then (story => {
+          const li = document.createElement('li');
+          const a = document.createElement('a');
+          a.href = story.url;
+          a.innerText = story.title;
+          li.appendChild(a);
+          document.getElementById('top-stories').appendChild(li);
+        });
+      });
+    });
+  });
+});
