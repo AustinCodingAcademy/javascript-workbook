b4ef69c26dab445394ab07dd2871921e462fc0fa
jdrowlett <jdrowlett@outlook.com>
2017-06-08 18:01:23 -0500

    spaceTravelToMars

--- 05week/spaceTravelToMars.js
+++ 05week/spaceTravelToMars.js
@@ -56,7 +56,9 @@ function(){
   assert.equal(oss.missionStatement(), "Can't perform a mission yet.");
   assert.equal(zoidberg.missionStatement(), "Can't perform a mission yet.");
 
-//tests
+
+//Tests
+
 if (typeof describe === 'function'){
   describe('CrewMember', function(){
     it('should have a name, a job, a specialSkill and ship upon instantiation', function(){
