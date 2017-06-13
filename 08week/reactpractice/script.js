348f4f299752972482a935f4a04daaa4999daaf8
Greg Devany <greg.devany@yahoo.com>
2017-06-13 14:03:54 -0500

    reactpractice- copying manny's and Stephan's code.  Working 100%

--- 08week/reactpractice/script.js
+++ 08week/reactpractice/script.js
@@ -5,45 +5,15 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      userlist: [],
-      fname: '',
-      lname: '',
-      id: '',
-      pic: '',
-      loc: ''
+      userlist: []
     };
   };
 
   componentDidMount() {
-    fetch('https://randomuser.me/api/?results=2').then((response) => {
+    fetch('https://randomuser.me/api/?results=20').then((response) => {
       response.json().then((data) => {
         this.setState({
-          userlist: data.results.map((person) => {
-          // let fname = person.name.first;
-          // let lname = person.name.last;
-          // let pic = person.picture.thumbnail;
-          // let id = person.id.value;
-          // let loc = person.location.city;
-
-          this.state.userlist.push(person.name.first);
-          this.state.userlist.push(person.name.last);
-          this.state.userlist.push(person.picture.thumbnail);
-          this.state.userlist.push(person.id.value);
-          this.state.userlist.push(person.location.city);
-
-          // fname: person.name.first;
-          // lname: person.name.last;
-          // pic: person.picture.thumbnail;
-          // id: person.id.value;
-          // loc: person.location.city;
-
-// console.log([fname, lname, pic, id, loc]);
-// console.log(this.state.fname);
-console.log(this.state.userlist);
-
-
-          // return [fname, lname, pic, id, loc];
-          })
+          userlist: data.results
         })
       })
     })
@@ -52,24 +22,21 @@ console.log(this.state.userlist);
   renderUsers = () => {
     return (
       this.state.userlist.map(user =>
-        <li key={user.location.city}>  <img alt="pic of user" src={user.pic}/> <br/>
-            Name:{user.fname} {user.lname} <br/> DOB: {user.dob}
+        <li key={user.name.last}>  <img alt="pic of user" src={user.picture.large}/> <br/>
+            Name:{user.name.first} {user.name.last} <br/> DOB: {user.dob}
         </li>
       )
     )
   }
 
-
   render() {
     return (
       <div>
-        Cold or Not:
+        Check Me Out:
         <ul>{this.renderUsers()}</ul>
       </div>
     )
   }
-
-
 
 }
 
