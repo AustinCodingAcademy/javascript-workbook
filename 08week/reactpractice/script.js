e622b122ac1855c8d335a5b6204577ec6ed05634
Greg Devany <greg.devany@yahoo.com>
2017-06-13 01:47:08 -0500

    reactpractice- copying manny's and Stephan's code.  Not working...yet

--- 08week/reactpractice/script.js
+++ 08week/reactpractice/script.js
@@ -1,50 +1,74 @@
 'use strict';
 
-const users = [
-  {
-    name: 'Steve',
-    phone: '512-555-3333',
-    city: 'Austin'
-  },
-  {
-    name: 'Bob',
-    phone: '512-555-4444',
-    city: 'Bastrop'
-  }
-];
-
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-        namer: '',
-        phone: '',
-        city: ''
+      userlist: [],
+      fname: '',
+      lname: '',
+      id: '',
+      pic: '',
+      loc: ''
     };
   };
 
-componentDidMount() {
-  users.map((user) => {
-    this.setState({
-      namer: user.name,
-      phone: user.phone,
-      city: user.city
+  componentDidMount() {
+    fetch('https://randomuser.me/api/?results=2').then((response) => {
+      response.json().then((data) => {
+        this.setState({
+          userlist: data.results.map((person) => {
+          // let fname = person.name.first;
+          // let lname = person.name.last;
+          // let pic = person.picture.thumbnail;
+          // let id = person.id.value;
+          // let loc = person.location.city;
+
+          this.state.userlist.push(person.name.first);
+          this.state.userlist.push(person.name.last);
+          this.state.userlist.push(person.picture.thumbnail);
+          this.state.userlist.push(person.id.value);
+          this.state.userlist.push(person.location.city);
+
+          // fname: person.name.first;
+          // lname: person.name.last;
+          // pic: person.picture.thumbnail;
+          // id: person.id.value;
+          // loc: person.location.city;
+
+// console.log([fname, lname, pic, id, loc]);
+// console.log(this.state.fname);
+console.log(this.state.userlist);
+
+
+          // return [fname, lname, pic, id, loc];
+          })
+        })
+      })
     })
-  })
-}
+  }
 
-  render() {
-
+  renderUsers = () => {
     return (
-      <div id="root">
-        <div>{this.state.namer}</div>
-        <div>{this.state.phone}</div>
-        <div>{this.state.city}</div>
-      </div>
-
+      this.state.userlist.map(user =>
+        <li key={user.location.city}>  <img alt="pic of user" src={user.pic}/> <br/>
+            Name:{user.fname} {user.lname} <br/> DOB: {user.dob}
+        </li>
+      )
     )
   }
+
+
+  render() {
+    return (
+      <div>
+        Cold or Not:
+        <ul>{this.renderUsers()}</ul>
+      </div>
+    )
+  }
+
 
 
 }
