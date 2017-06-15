e4112dfdff83fe286f8bce45f3cc8ea553c25f3c
jdrowlett <jdrowlett@outlook.com>
2017-06-15 12:41:03 -0500

    tictactoe updated

--- 07week/ticTacToe/script.js
+++ 07week/ticTacToe/script.js
@@ -3,25 +3,41 @@
 class TicTacToe extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      a: '',
+      b: '',
+      c: '',
+      d: '',
+      e: '',
+      f: '',
+      g: '',
+      h: '',
+      i: ''
+    }
+  }
+
+  clickCell - (event) => {
+    const cellLetter = event.target.getAttribute('data-cell'); 
+
   }
 
   render() {
-    return (
+    return ( //Board:
       <div>
         <div className="row">
-          <div data-cell="0"></div>
-          <div data-cell="1"></div>
-          <div data-cell="2"></div>
+          <div data-cell="a"></div>
+          <div data-cell="b"></div>
+          <div data-cell="c"></div>
         </div>
         <div className="row">
-          <div data-cell="3"></div>
-          <div data-cell="4"></div>
-          <div data-cell="5"></div>
+          <div data-cell="d"></div>
+          <div data-cell="e"></div>
+          <div data-cell="f"></div>
         </div>
         <div className="row">
-          <div data-cell="6"></div>
-          <div data-cell="7"></div>
-          <div data-cell="8"></div>
+          <div data-cell="g"></div>
+          <div data-cell="h"></div>
+          <div data-cell="i"></div>
         </div>
       </div>
     );
