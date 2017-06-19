639cb99c468dd3aa650945b1dbc014ce7eeed880
Greg Devany <greg.devany@yahoo.com>
2017-06-19 17:18:49 -0500

    reactpractice2- changing over to googlebooks api

--- 08week/reactpractice2/script.js
+++ 08week/reactpractice2/script.js
@@ -5,7 +5,7 @@ class App extends React.Component {
     super();
 
     this.state = {
-      winelist: []
+      listit: []
     }
   }
 
@@ -21,35 +21,70 @@ class App extends React.Component {
   // }
 
 
+  // componentDidMount() {
+  //   fetch('https://services.wine.com/api/beta2/service.svc/json/categorymap?filter=categories(490+124)+rating(90|95)&apikey=562422cbdaa35a13c2c2f4a44ba18688').then((response) => {
+  //     response.json().then((result) => {
+  //       this.setState({
+  //         // userlist: result.Categories[0][0]
+  //         listit: result
+  //       })
+  //       console.log(this.state.listit)
+  //     })
+  //   })
+  // }
+
+  // componentDidMount() {
+  //   fetch('https://api.bookshare.org/book/popular/page/1/limit/100/format/json?api_key=f7jn7uxywqmwwydy2rm9yq35').then((response) => {
+  //     response.json().then((result) => {
+  //       this.setState({
+  //         // userlist: result.Categories[0][0]
+  //         listit: result
+  //       })
+  //       console.log(this.state.listit)
+  //     })
+  //   })
+  // }
+
+  // componentDidMount() {
+  //   fetch('https://www.googleapis.com/books/v1/volumes?q=pride+prejudice&download=epub&key=AIzaSyCWCAyFHX_qCtN9SmHcgDk20ZmzIvWI5z4').then((response) => {
+  //     response.json().then((result) => {
+  //       this.setState({
+  //         listit: result
+  //       })
+  //       console.log(this.state.listit)
+  //     })
+  //   })
+  // }
+
   componentDidMount() {
-    fetch('https://services.wine.com/api/beta2/service.svc/json/categorymap?filter=categories(490+124)+rating(90|95)&apikey=562422cbdaa35a13c2c2f4a44ba18688').then((response) => {
-      response.json().then((result) => {
+    fetch('https://www.googleapis.com/books/v1/volumes?q=pride+prejudice&download=epub&filter=free-ebooks&key=AIzaSyCWCAyFHX_qCtN9SmHcgDk20ZmzIvWI5z4').then((response) => {
+      response.json().then((json) => {
         this.setState({
-          // userlist: result.Categories[0][0]
-          winelist: result
+          listit: json.items.map((post) => {
+            return post.accessInfo.pdf;
+          })
         })
-        // console.log(this.state.winelist)
+        console.log(this.state.listit)
       })
     })
   }
 
-  wineinfo = () => {
-    console.log(this.state.winelist)
-    // return (
-    //   this.state.winelist.map((wine) =>
-    //     <li key={wine.id}>
-    //       Name: {wine.name} {wine.type} <br/>
-    //       SCORE: {wine.score}
-    //     </li>
-    //   )
-    // )
+  listinfo = () => {
+    console.log(this.state.listit)
+    return (
+      this.state.listit.map((listitem) =>
+        <li key={listitem.downloadLink}>
+          Name: {listitem.downloadLink}<br/>
+        </li>
+      )
+    )
   }
 
   render() {
     return (
       <div>
-      Red wines (score 90-95)
-        <ul>{this.wineinfo()}</ul>
+      Here's the results:
+        <ul>{this.listinfo()}</ul>
       </div>
     )
   }
