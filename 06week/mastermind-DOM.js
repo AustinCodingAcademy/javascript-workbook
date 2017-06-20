c9e12640670d4b7322d8f1f15b36726c3646316b
jdrowlett <jdrowlett@outlook.com>
2017-06-20 18:57:58 -0500

    mastermind DOM revised

--- 06week/mastermind-DOM.js
+++ 06week/mastermind-DOM.js
@@ -24,6 +24,54 @@ $('form#mastermind').submit(function(data)) {
   checkForWin(guess);
 })
 
+function printBoard() {
+  for (let i = 0; i < board.length; i++) {
+    console.log(board[i]);
+  }
+}
+
+//Generating a Solution
+function generateSolution() {
+  for (let i = 0; i < 4; i++) {
+    const randomIndex = getRandomInt(0, letters.length);
+    solution += letters[randomIndex];
+  }
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+//Generating a Hint
+function generateHint() {
+
+  var guess = guess.split('');
+  var solution = solution.split('');
+    console.log(guess);
+    console.log(solution);
+  }
+
+//Entering a Guess
+function mastermind(guess) {
+  if (guess [''] ===solution ['']) {
+   return "You guessed it!";
+ }
+  if (guess [''] !=solution ['']) {
+    return generateHint(guess, solution);
+}
+ var solution = 'adbc';
+ var guess = 'bcda';
+}
+
+//Prompt whether Guess matches Solution's color, number or both
+function getPrompt() {
+  rl.question('guess: ', (guess) => {
+    console.log( mastermind(guess) );
+    printBoard();
+    getPrompt();
+  });
+}
+
 //Check Win
 
 function checkForWin(guess, solution) {
