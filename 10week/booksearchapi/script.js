78c29c0accdcf590ff09ccd01aeb4c81b2ec4beb
Greg Devany <greg.devany@yahoo.com>
2017-06-21 00:18:44 -0500

    bookshareapi -googlebooks -changed back to return only epub with downloadLink

--- 10week/booksearchapi/script.js
+++ 10week/booksearchapi/script.js
@@ -12,34 +12,28 @@ class App extends React.Component {
     this.changeInput = this.changeInput.bind(this)
   }
 
-  // componentDidMount() {
-  //   if (this.state.bookname) {
-  //     this.fetchBook(this.state.bookname);
-  //   }
-  // }
-
   fetchBook() {
     console.log(this.state.bookname);
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=${this.state.bookname}&filter=free-ebooks&maxResults=40&key=AIzaSyCWCAyFHX_qCtN9SmHcgDk20ZmzIvWI5z4`)
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${this.state.bookname}&download=epub&maxResults=40&key=AIzaSyCWCAyFHX_qCtN9SmHcgDk20ZmzIvWI5z4`)
     .then((response) => {
       response.json().then((data) => {
         var templist = [];
         data.items.map((withit) => {
-          //This will map and return free ebook with download link
-          // if (withit.accessInfo.hasOwnProperty('epub')) {
-          //   if (withit.accessInfo.epub.hasOwnProperty('downloadLink')) {
-          //     templist.push(withit)
-          //   }
-          // }
-
           // This will map and return free ebook with download link
-          // console.log(withit.volumeInfo.publishedDate.slice(0,4));
-          if (withit.volumeInfo.hasOwnProperty('publishedDate')) {
-            var year = parseInt(withit.volumeInfo.publishedDate.slice(0,4));
-            if (year > 1990) {
+          if (withit.accessInfo.hasOwnProperty('epub')) {
+            if (withit.accessInfo.epub.hasOwnProperty('downloadLink')) {
               templist.push(withit)
             }
           }
+
+          // This will map and return free ebook with download link
+          // console.log(withit.volumeInfo.publishedDate.slice(0,4));
+          // if (withit.volumeInfo.hasOwnProperty('publishedDate')) {
+          //   var year = parseInt(withit.volumeInfo.publishedDate.slice(0,4));
+          //   if (year > 1990) {
+          //     templist.push(withit)
+          //   }
+          // }
 
         })
         this.setState({
@@ -48,16 +42,14 @@ class App extends React.Component {
       })
     })
   }
-  // <a href={listitem.accessInfo.epub.downloadLink}>
-  // <img alt="dsf" src={listitem.volumeInfo.imageLinks.thumbnail}/>
 
   listinfo = () => {
     console.log(this.state.listit)
     return (
       this.state.listit.map((listitem) =>
         <li key={listitem.id}>
-          <a href={listitem.accessInfo.webReaderLink}>
-          <img alt="dsf" src={listitem.volumeInfo.imageLinks.thumbnail}/>
+          <a href={listitem.accessInfo.epub.downloadLink}>
+          <img alt="book cover" src={listitem.volumeInfo.imageLinks.thumbnail}/>
           </a><br/>
         </li>
       )
