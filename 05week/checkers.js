e6a372113e48a34eb73587f8fa39b803f5b00a19
Cam H <Cam H>
2017-06-22 18:40:20 -0500

    checkers 3/4

--- 05week/checkers.js
+++ 05week/checkers.js
@@ -18,65 +18,12 @@ function Checker(color) {
   }
 }
 
+
 function Board() {
   this.grid = [];
   // creates an 8x8 array, filled with null values
-  this.checkers = [];
+  this.checkers = new Array();
   // in-play checker pieces // creates checker pieces
-  this.createCheckers = function() {
-    var whitePositions = [];
-      // [0, 1],
-      // [0, 3],
-      // [0, 5],
-      // [0, 7],
-      // [1, 0],
-      // [1, 2],
-      // [1, 4],
-      // [1, 6],
-      // [2, 1],
-      // [2, 3],
-      // [2, 5],
-      // [2, 7]
-    
-
-    var blackPositions = [];
-      // [5, 0],
-      // [5, 2],
-      // [5, 4],
-      // [5, 6],
-      // [6, 1],
-      // [6, 3],
-      // [6, 5],
-      // [6, 7],
-      // [7, 0],
-      // [7, 2],
-      // [7, 4],
-      // [7, 6]
-    
-    let whiteChecker = new Checker('white');
-    let blackChecker = new Checker('black');
-// create and push white checkers
-    for (var i = 0; i <= 2; i++) {
-      for (var j = 1; j <= 7; j += 2) {
-        if ((i === 1) && (j % 2 !== 0)) { j = j - 1 }
-        this.grid[i][j] = whiteChecker;
-        this.checkers.push(whiteChecker)
-      }
-    };
-// create and push black checkers
-    for (var h = 5; h <= 7; h++) {
-      for (var k = 0; k <= 7; k += 2) {
-        if((h === 6) && (k % 2 !== 1)){k = k -1}
-          this.grid[h][k] = blackChecker;
-        this.checkers.push(blackChecker)
-      }
-    };
-
-    // console.log(whitePositions)
-      // console.log(blackPositions)
-      // this.grid.push(whitePositions, blackPositions)
-    // console.log(this.grid)
-  }
 
   this.createGrid = function() {
     // loop to create the 8 rows
@@ -87,59 +34,117 @@ function Board() {
         this.grid[row].push(null);
       }
     }
-// user chooses which checker to move
-    this.selectChecker = (row, column) =>{
+    // prints out the board
+    this.viewGrid = function() {
+      // add our column numbers
+      let string = "  0 1 2 3 4 5 6 7\n";
+      for (let row = 0; row < 8; row++) {
+        // we start with our row number in our array
+        const rowOfCheckers = [row];
+        // a loop within a loop
+        for (let column = 0; column < 8; column++) {
+          // if the location is "truthy" (contains a checker piece, in this case)
+          if (this.grid[row][column]) {
+            // push the symbol of the check in that location into the array
+            rowOfCheckers.push(this.grid[row][column].symbol);
+          } else {
+            // just push in a blank space
+            rowOfCheckers.push(' ');
+          }
+        }
+        // join the rowOfCheckers array to a string, separated by a space
+        string += rowOfCheckers.join(' ');
+        // add a 'new line'
+        string += "\n";
+      }
+      console.log(string);
+    };
+    // Your code here
+    this.createCheckers = function() {
+        let whiteChecker = new Checker('white');
+        let blackChecker = new Checker('black');
+
+        const whitePositions = [
+          [0, 1],
+          [0, 3],
+          [0, 5],
+          [0, 7],
+          [1, 0],
+          [1, 2],
+          [1, 4],
+          [1, 6],
+          [2, 1],
+          [2, 3],
+          [2, 5],
+          [2, 7]
+        ];
+
+        const blackPositions = [
+          [5, 0],
+          [5, 2],
+          [5, 4],
+          [5, 6],
+          [6, 1],
+          [6, 3],
+          [6, 5],
+          [6, 7],
+          [7, 0],
+          [7, 2],
+          [7, 4],
+          [7, 6]
+        ];
+
+        // create and push white checkers
+        // for (var i = 0; i <= 2; i++) {
+        //   for (var j = 1; j <= 7; j += 2) {
+        //     if ((i === 1) && (j % 2 !== 0)) { j = j - 1 }
+        //     this.grid[i][j] = whiteChecker;
+        //     // this.checkers.push(whiteChecker)
+        //   }
+        // };
+        for(var w = 0; w <= whitePositions.length-1; w++){
+          let x = whitePositions[w][0];
+          let y = whitePositions[w][1];
+          this.grid[x][y] = whiteChecker;
+          this.checkers.push(whiteChecker);
+
+        };
+
+        //TEST iterate thru hard-coded array
+
+        for (var b = 0; b <= blackPositions.length - 1; b++) {
+          let x = blackPositions[b][0];
+          let y = blackPositions[b][1];
+          this.grid[x][y] = blackChecker;
+          this.checkers.push(blackChecker)
+
+
+        };
+        // create and push black checkers
+
+        // for (var h = 5; h <= 7; h++) {
+        //   for (var k = 1; k <= 7; k += 2) {
+        //     if ((h === 6) && (k % 2 !== 1)) { k = k - 1 }
+        //     this.grid[h][k] = blackChecker;
+        //     this.checkers.push(blackChecker)
+        //   }
+        // };
+
+      }
+      // user chooses which checker to move
+    this.selectChecker = (row, column) => {
       return this.grid[row][column];
     };
-// KILL CHECKER
-this.killChecker = (position) => {
-// let positionX = position[0];
-// let positionY = position[1];
-// let kill = this.selectChecker(positionX, positionY);
-// let index = this.checkers.indexOf(kill);
-// index.splice([0],[1]);
-// this.grid[positionX][positionY] = null;
- 
-//  console.log(positionY);
-//  console.log(positionY);
-//  console.log(kill);
-let kill = this.selectChecker(position[0], position[1]);
-this.checkers.splice(indexOf(kill),1);
-this.grid[position[0]][position[1]] = null;
 
-
-};
-
-
+    // KILL CHECKER
+    this.killChecker = (position) => {
+      let kill = this.selectChecker(position[0], position[1]);
+      let index = this.checkers.indexOf(kill);
+      this.checkers.splice(index, 1);
+      this.grid[position[0]][position[1]] = null;
+    };
   };
-
-  // prints out the board
-  this.viewGrid = function() {
-    // add our column numbers
-    let string = "  0 1 2 3 4 5 6 7\n";
-    for (let row = 0; row < 8; row++) {
-      // we start with our row number in our array
-      const rowOfCheckers = [row];
-      // a loop within a loop
-      for (let column = 0; column < 8; column++) {
-        // if the location is "truthy" (contains a checker piece, in this case)
-        if (this.grid[row][column]) {
-          // push the symbol of the check in that location into the array
-          rowOfCheckers.push(this.grid[row][column].symbol);
-        } else {
-          // just push in a blank space
-          rowOfCheckers.push(' ');
-        }
-      }
-      // join the rowOfCheckers array to a string, separated by a space
-      string += rowOfCheckers.join(' ');
-      // add a 'new line'
-      string += "\n";
-    }
-    console.log(string);
-  };
-
-  // Your code here
+  console.log(this.checkers)
 }
 
 function Game() {
@@ -161,13 +166,17 @@ function Game() {
     this.board.grid[endRow][endCol] = checker;
     this.board.grid[startRow][startCol] = null;
     // console.log(this.board.checkers.indexOf(checker));
-    if(Math.abs(startRow)-Math.abs(endRow) === 2){
-      let midpointX = Math.abs(startRow) - Math.abs(endRow) / 2;
-      let midpointY = Math.abs(startCol) - Math.abs(endCol) / 2;
-      this.killChecker(midpointX,midpointY);
+
+    var killPosition = (midpointX, midpointY) => {
+      return midpointX = (Math.abs(startRow) - Math.abs(endRow)) / 2;
+      return midpointY = (Math.abs(startCol) - Math.abs(endCol)) / 2;
     };
 
-      };
+    if (Math.abs(startRow) - Math.abs(endRow) === 2) {
+
+      this.board.killChecker(killPosition);
+    };
+  };
 }
 
 function getPrompt() {
