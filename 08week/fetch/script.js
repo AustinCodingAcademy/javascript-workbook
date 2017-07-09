4cb4689eaeabb64e8b09118b42ba10838d78cb6d
Andrew Hartford <Andrew Hartford>
2017-07-08 20:03:06 -0500

    checkpoint3

--- 08week/fetch/script.js
+++ 08week/fetch/script.js
@@ -1,4 +1,22 @@
-// 'use strict';
+'use strict';
+
+var request = new Request('https://randomuser.me/api/?results=10');
+
+fetch(request).then(function(response) {
+  return response.json().then(function(json) {
+    console.dir(json);
+  });
+});
+
+
+
+
+
+
+
+
+
+
 //
 // fetchJsonp('https://api.instagram.com/v1/users/self/media/recent/?' + window.location.hash.split('#')[1],
 //   method: 'GET',
@@ -30,22 +48,3 @@
 //     })
 //   });
 // });
-
-fetch('https://randomuser.me/api/?results=5000}', {
-  method: 'GET'
-}).then((response) => {
-  response.json().then((storyIds) => {
-    storyIds.forEach((storyId) => {
-      fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json?print=pretty`).then((response) => {
-        response.json().then((story) => {
-          const a = document.createElement('a');
-          a.href = story.url;
-          a.innerText = story.title
-          const div = document.createElement('div');
-          div.appendChild(a);
-          document.querySelector('#fetch').appendChild(div);
-        })
-      })
-    })
-  });
-});
---
+++ 10week/checkpt3/script.js
@@ -0,0 +1,103 @@
+'use strict';
+
+class checkpoint extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      person: [],
+      filteredname: []
+    }
+
+  fetch('https://randomuser.me/api/?results=5')
+  .then(function(response) {
+    return response.json();
+  })
+     .then(function(person) {
+        const name = this.state.name.slice();
+        name.push(person);
+        this.setState({
+          name: name,
+          filtered: names
+        });
+      });
+
+    this.search = this.search.bind(this);
+  }
+
+  search(event) {
+    console.log(event.target.value);
+    const filtered = this.state.name.filter(function(person) {
+      return person.title.toLowerCase().includes(event.target.value.toLowerCase());
+    });
+    this.setState({ filtered: filtered })
+  }
+
+  render() {
+    const name = this.state.filtered.map(function(person) {
+      return (
+        <li key={person.name}>
+        </li>
+      )
+    });
+    return (
+      <div>
+        <input type="text" onChange={this.search} />
+        <ul>
+          {filtered}
+        </ul>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<checkpoint />, document.getElementById('checkpoint'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 'use strict';
+//
+//
+// class checkpoint extends React.Component {
+//   constructor(props) {
+//     super();
+//     this.state = {
+//       person: [],
+//       filteredName: []
+//     }
+//
+// var request = new Request('https://randomuser.me/api/?results=5')
+// var myImage = document.querySelector('nice');
+// var myURL = request.url;
+//
+//
+// fetch(request).then(function(response) {
+//   return response.json()
+// })
+// .then(function(json) {
+//   json.results.forEach((user) => {
+//     console.log(JSON.stringify(json, null, 2));
+//     const a = document.createElement('a');
+//     a.href = `mailto:${user.email}`
+//     a.innerText = user.name.first
+//     document.querySelector('#nice').appendChild(a)
+//   })
+// })
+//
+//
+//
+// render() {
+//   return()
+// }
+// ReactDOM.render(<checkpoint />, document.getElementById('nice'));
