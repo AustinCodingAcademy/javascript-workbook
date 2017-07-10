5c228bf9e67deb6c2b6ccb131dc8275f04e8f336
stuartkilgore <stuart.kilgore@gmail.com>
2017-07-10 15:44:17 -0500

    wrapping up

--- 08week/fetch/script.js
+++ 08week/fetch/script.js
@@ -1,19 +1,42 @@
 'use strict';
 
-fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty').then((response) => {
-  response.json().then((storyIds) => {
-    storyIds.forEach((storyId) => {
-      fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json?print=pretty`).then((response) => {
-        response.json().then((story) => {
-          const a = document.createElement('a');
-          a.href = story.url;
-          a.innerText = story.title;
-          a.target = 'blank';
-          const div = document.createElement('div');
-          div.appendChild(a);
-          document.querySelector('#fetch').appendChild(div);
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stories: []
+    }
+  }
+
+  setStories = () => {
+    //return [nums]
+  }
+
+  componentDidMount(){
+    fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty').then((response) => {
+      response.json().then((storyIds) => {
+        storyIds.slice(0,10).forEach((storyId) => {
+          fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json?print=pretty`).then((response) => {
+            response.json().then((story) => {
+              this.setState(previous => {
+                return {
+                  stories: [...previous.stories, story]
+                }
+              })
+            })
+          })
         })
-      })
+      });
+    });
+  }
+
+  render() {
+    let list = this.state.stories.map(story => {
+      return (<a key={story.id} href={story.url}>{story.title}</a>);
     })
-  });
-});
+
+    return (<div>{list}</div>);
+  }
+}
+
+ReactDOM.render(<List />, document.querySelector('#fetch'))
--- 09week/hackernews/script.js
+++ 09week/hackernews/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty').then((response) => {
+  console.log('response: ', response);
+  response.json().then((storyIds) => {
+    console.log('storyIds: ', storyIds);
+    for (var i = 0; i < 10; i++) {
+      fetch(`https://hacker-news.firebaseio.com/v0/item/${storyIds[i]}.json?print=pretty`).then((response) => {
+        response.json().then((story) => {
+          const a = document.createElement('a');
+          a.href = story.url;
+          a.innerText = story.title;
+          a.target = 'blank';
+          const div = document.createElement('div');
+          div.appendChild(a);
+          document.querySelector('#fetch').appendChild(div);
+        })
+      })
+    }    
+  });
+});
