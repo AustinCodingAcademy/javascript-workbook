1a9b5d5f645ffe18b818b1550a040807f35dee87
rjones1984 <rjones1984@users.noreply.github.com>
2017-07-10 21:44:00 -0500

    finished data-types

--- 01week/data-types.js
+++ 01week/data-types.js
@@ -30,7 +30,7 @@ function getType(data) {
   return typeof data;
 }
 
-getType(30);
+console.log(getType(30));
 
 // the following function satisfies item 5
 
@@ -38,17 +38,13 @@ function addNumbers (num1, num2) {
   return num1 + num2;
 }
 
-addNumbers(2,30.6);
+console.log(addNumbers(2,30.6));
 
 // the following function satisfies items 6-8
 
 function makeFood(beans, rice) {
   if (beans === 'y' && rice === 'y') {
     console.log('Burrito');
-  } else if (beans === 'n' || rice === 'n') {
-    console.log('Sorry, you have to like both of them.');
-  } else {
-    console.log('Try again, and say you like both ingredients.');
   }
 }
 
