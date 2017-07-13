7ec6ea7401148dd9439a1a9b56f754128e406e35
jdrowlett <jdrowlett@outlook.com>
2017-07-13 18:31:10 -0500

    Initial Commit

---
+++ 01week/javascripting/javascripting.js
@@ -0,0 +1,66 @@
+'use strict';
+
+//Display date and time//
+const currentDate = date;
+const currentTime = time;
+function displayDateAndTime(currentDate, currentTime) {
+  console.log(currentDate && currentTime);
+}
+return(displayDateAndTime);
+}
+
+//Convert number to string//
+const number = >0;
+const string = '';
+function convertNumber(number, string){
+  return string;
+}
+
+//Convert string to number//
+const number = >0;
+const string = '';
+function convertString(string, number){
+  return number;
+}
+
+//Print different data types
+function dataType(){
+  const boolean = "Boolean";
+  const null = "Null";
+  const und = "Undefined";
+  const num = "Number";
+  const nan = "NaN";
+  const str = "String";
+
+}
+
+//Add 2 numbers together
+var a = 7;
+var b = 13;
+function adding(7, 13){
+  return (a + b);
+}
+
+//Run if 2 things are true
+var x = blue;
+var y = red;
+function colors (x, y){
+  if x == blue && y == red;
+}
+  return "true";
+
+//Run if one of two things are true
+var x = blue;
+var y = dog;
+function oneThingTrue (x, y){
+  if x || y === blue
+  return "true";
+}
+
+//Run if both things are not true//
+var x = car;
+var y = dog;
+function bothNotTrue (x, y){
+  if x || y != color;
+  return "not true";
+}
