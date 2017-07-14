f599f298e13657f83a1aae02ebcbaacc3e14d790
matt_Huntsberry <mmhuntsberry@gmail.com>
2017-07-14 13:17:38 -0500

    First Commit -> rockPaperScissors program

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -7,28 +7,51 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+function rockPaperScissors (hand1) {
+  var words = ['rock', 'paper', 'scissors'];
+  var num = Math.floor(Math.random() * words.length);
+  var hand2 = words[num];
 
-function rockPaperScissors(hand1, hand2) {
+  hand1 = hand1.toLowerCase().trim();
+  hand2 = hand2.toLowerCase().trim();
 
-  // Write code here
-
+  if (hand1 === hand2) {
+    return "It's a tie!";
+  } else if (hand1 === 'rock') {
+    if (hand2 === 'paper') {
+      return 'Hand two wins!';
+    } else {
+      return 'Hand one wins!';
+    }
+  } else if (hand1 === 'scissors') {
+    if (hand2 === 'rock') {
+      return 'Hand two wins!';
+    } else {
+      return 'Hand one wins!';
+    }
+  } else if (hand1 === 'paper') {
+    if (hand2 === 'rock') {
+      return 'Hand one wins!';
+    } else {
+      return 'Hand two wins!';
+    }
+  }
 }
 
-function getPrompt() {
+function getPrompt () {
   rl.question('hand1: ', (answer1) => {
-    rl.question('hand2: ', (answer2) => {
-      console.log( rockPaperScissors(answer1, answer2) );
+    //rl.question('hand2: ', (answer2) => {
+      console.log(rockPaperScissors(answer1));
       getPrompt();
-    });
+    //});
   });
 }
 
 // Tests
 
 if (typeof describe === 'function') {
-
-  describe('#rockPaperScissors()', () => {
-    it('should detect a tie', () => {
+  describe ('#rockPaperScissors()', () => {
+    it ('should detect a tie', () => {
       assert.equal(rockPaperScissors('rock', 'rock'), "It's a tie!");
       assert.equal(rockPaperScissors('paper', 'paper'), "It's a tie!");
       assert.equal(rockPaperScissors('scissors', 'scissors'), "It's a tie!");
@@ -45,7 +68,5 @@ if (typeof describe === 'function') {
     });
   });
 } else {
-
   getPrompt();
-
 }
