46d2e031526162884da0bef00a90e32488bd23ab
Eddie Garcia <fasteddiegarcia@gmail.com>
2017-07-14 11:49:53 -0500

    Change 'console.log' to 'return' in the body of the functions

--- 01week/javascripting/data-types.js
+++ 01week/javascripting/data-types.js
@@ -3,25 +3,25 @@
 
 // Write a JavaScript program to display the current day and time.
 function CurrentDateTime() {
-  console.log(new Date());
+  return new Date();
 }
-CurrentDateTime();
+console.log(new CurrentDateTime());
 
 
 
 // Write a JavaScript program to convert a number to a string.
 function NumberToString(num) {
-  console.log(num.toString());
+  return num.toString();
 }
-NumberToString(37);
+console.log(new NumberToString(37));
 
 
 
 // Write a JavaScript program to convert a string to the number.
 function StringToNumber(nummer) {
-  console.log(parseInt(nummer));
+  return parseInt(nummer);
 }
-StringToNumber(23);
+console.log(new StringToNumber(23));
 
 
 
@@ -33,44 +33,44 @@ StringToNumber(23);
 // NaN
 // String
 function ReturnType(someData) {
-  console.log(typeof someData);
+  return typeof someData;
 }
-ReturnType(9);
+console.log(new ReturnType(9));
 
 
 
 // Write a JavaScript program that adds 2 numbers together.
 function Addem(num1, num2) {
-  console.log(num1 + num2);
+  return num1 + num2;
 }
-Addem(4,5);
+console.log(new Addem(4,5));
 
 
 
 // Write a JavaScript program that runs only when 2 things are true.
 function TwoTrue(x,y) {
   if (x === true && y === true) {
-    console.log("Yeah! Both things are true!");
+    return "Yeah! Both things are true!";
   }
 }
-TwoTrue(true,true);
+console.log(new TwoTrue(true,true));
 
 
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
 function OnlyOneisTrue(z,a) {
   if (z === true || a === true) {
-    console.log("Booyah! Only one thing is true!");
+    return "Booyah! Only one thing is true!";
   }
 }
-OnlyOneisTrue(true,false);
+console.log(new OnlyOneisTrue(true,false));
 
 
 
 // Write a JavaScript program that runs when both things are not true.
 function NoneTrue(b,c) {
   if (b !== true && c !== true) {
-  console.log("Neither statement evaluates to true!");
+    return "Neither statement evaluates to true!";
   }
 }
-NoneTrue(false, false);
+console.log(new NoneTrue(false, false));
