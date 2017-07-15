63d23f6863f4bde5098ede168fcf28706b40b3e6
situlillian <mslilliansitu@gmail.com>
2017-07-15 11:49:47 -0500

    added second hand AI

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -8,8 +8,8 @@ const rl = readline.createInterface({
 });
 
 function rockPaperScissors (hand1, hand2) {
-  hand1 = hand1.trim().toLowerCase();
-  hand2 = hand2.trim().toLowerCase();
+  hand1 = hand1.toLowerCase().trim();
+  hand2 = hand2.toLowerCase().trim();
 
   if (hand1 === hand2) {
     return 'It\'s a tie!';
