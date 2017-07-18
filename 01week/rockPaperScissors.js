a587b8d8a20a9b2b3bf6c11bb53b9afad10eb1ca
Chase Turner <chasethemigthy1@gmail.com>
2017-07-18 18:24:48 -0500

    RockPaperScissors 1st Commit

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -12,7 +12,11 @@ const rl = readline.createInterface({
 
   // Write code here
 function rockPaperScissors(hand1, hand2){
-  if (hand1 === hand2){
+  if (hand1 === 'rock' && hand2 === 'rock') {
+    return "It's a tie!";
+  }else if (hand1 === 'paper' && hand2 === 'paper') {
+    return "It's a tie!";
+  }else if (hand1 === 'scissors' && hand2 === 'scissors') {
     return "It's a tie!";
   }else if (hand1==='rock' && hand2==='paper') {
     return "Hand two wins!";
