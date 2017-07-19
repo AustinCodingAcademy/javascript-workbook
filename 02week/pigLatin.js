41f0f0df53f89498ea82600e3a7c7468dab4bfd1
TrungPTAustin <trungnguyendpt@gmail.com>
2017-07-19 17:22:32 -0500

    initial commit

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -9,11 +9,29 @@ const rl = readline.createInterface({
 
 
 function pigLatin(word) {
-
   // Your code here
+  //if vowel add yay at the end of the word; apple add appleyay
+  //if not a vowel slice letters until the vowel, then add ay; skirt = irtskay
+  word = word.toLowerCase();
+  word = word.trim();
 
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  var wordPreFix;
+
+  for (var i = 0; i < word.length; i++) {
+    //character of the word at position i
+    if (vowels.indexOf(word.charAt(i)) > -1) {
+      wordPreFix = word.slice(0, i);
+      word = word.slice(i);
+      break;
+    }
+  }
+
+  if (wordPreFix.length > 0) {
+    return word + wordPreFix + 'ay';
+  }
+  return word + 'yay';
 }
-
 
 function getPrompt() {
   rl.question('word ', (answer) => {
