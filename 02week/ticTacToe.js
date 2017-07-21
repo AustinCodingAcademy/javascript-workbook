5f8d8798ad6276ed0062e365629dd6921b033be0
situlillian <mslilliansitu@gmail.com>
2017-07-20 21:38:09 -0500

    added tictactoe solution

--- 02week/ticTacToe.js
+++ 02week/ticTacToe.js
@@ -24,23 +24,42 @@ function printBoard() {
 }
 
 function horizontalWin() {
-  // Your code here
+  return (
+    (board[0][0] === playerTurn && board[0][1] === playerTurn && board[0][2] === playerTurn) ||
+    (board[1][0] === playerTurn && board[1][1] === playerTurn && board[1][2] === playerTurn) ||
+    (board[2][0] === playerTurn && board[2][1] === playerTurn && board[2][2] === playerTurn)
+  );
 }
 
 function verticalWin() {
-  // Your code here
+  return (
+    (board[0][0] === playerTurn && board[1][0] === playerTurn && board[2][0] === playerTurn) ||
+    (board[0][1] === playerTurn && board[1][1] === playerTurn && board[2][1] === playerTurn) ||
+    (board[0][2] === playerTurn && board[1][2] === playerTurn && board[2][2] === playerTurn)
+  );
 }
 
 function diagonalWin() {
-  // Your code here
+  return (
+    (board[0][0] === playerTurn && board[1][1] === playerTurn && board[2][2] === playerTurn) ||
+    (board[2][0] === playerTurn && board[1][1] === playerTurn && board[0][2] === playerTurn)
+  );
 }
 
 function checkForWin() {
-  // Your code here
+  if ( horizontalWin() || verticalWin() || diagonalWin() ) {
+    console.log('Player ' +  playerTurn + ' Won!'); // announce to the world
+    return true;
+  }
+  return false;
 }
 
 function ticTacToe(row, column) {
   // Your code here
+  board[row][column] = playerTurn;
+  if (!checkForWin()) {
+    playerTurn = (playerTurn === 'X') ? 'O' : 'X';
+  }
 }
 
 function getPrompt() {
