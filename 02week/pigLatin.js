ce7b08ae9c245b188d3442c296f8333b36c8f480
Craig Copeland <copelandhouse2@yahoo.com>
2017-07-21 09:58:28 -0500

    Adding the pig latin program

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -10,10 +10,41 @@ const rl = readline.createInterface({
 
 function pigLatin(word) {
 
-  // Your code here
+  // First, define vowels array
+  // Turn string into array
+  // Compare each character to the vowels array.
+  //   if first letter AND vowel, add yay to end of string and return.
+  //   else if nth letter is vowel, add ay to string array and return.
+  //   else if nth letter is consonant, remove from front and move to end of array.
+  // When returning, turn string array string back into a string
 
+  // NOTE: This code only handles words that contain a traditional vowel.  Does not work for words using y as a vowel.
+  // such as: sky, fly, why, try
+
+  word = word.toLowerCase().trim();
+  let wordArray = word.split('');
+
+  // This piece of code tests if the current letter is a vowel or not
+  const isAVowel = (letter) => {
+    const vowelsArr = ['a','e','i','o','u']
+    return vowelsArr.some(vowel => letter === vowel);
+  }
+
+  // The while loop tests the current letter for a vowel.  If not, then move the consonant to the end.
+  // If it finds a vowel, then break.
+  let counter = 0;
+  while (!isAVowel(wordArray[0])) {
+    wordArray.push(wordArray.shift());  // This should move first char to last.
+    counter++;
+  }
+
+  // This code adds either ay or yay at the end to complete the piglatin conversion.
+  if (counter === 0) {
+    return wordArray.join('')+'yay';
+  } else {
+    return wordArray.join('')+'ay';
+  }
 }
-
 
 function getPrompt() {
   rl.question('word ', (answer) => {
