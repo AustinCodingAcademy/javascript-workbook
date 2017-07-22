745fb3668d8db4c32763b36403475de99bc137f2
Jason Kelley <jasonkelley@Jasons-MacBook-Pro.local>
2017-07-21 19:32:20 -0500

    project1

--- 02week/project1.js
+++ 02week/project1.js
@@ -1,30 +1,63 @@
 
 // length
-let cars = ['Ford', 'Chevrolet', 'Buick', 'Mustang'];
-console.log(cars.length);
+var cars = ['Ford', 'Chevrolet', 'Buick', 'Mustang'];
+// console.log(cars.length);
 
 // concat
-let moreCars = ['Buick', 'Mercedes', 'Dodge', 'Honda'];
-let totalCars = cars.concat(moreCars);
-console.log (totalCars);
+var moreCars = ['Civic', 'Mercedes', 'Dodge', 'Honda'];
+var totalCars = cars.concat(moreCars);
+// console.log (totalCars);
 
 // indexOf && lastIndexOf
 var x = totalCars.indexOf('Honda');
-console.log (x);
+// console.log (x);
 var y = totalCars.lastIndexOf('Ford');
-console.log (y);
+// console.log (y);
 
 // join
-let stringOfCars = totalCars.join();
-console.log(stringOfCars);
+var stringOfCars = totalCars.join();
+// console.log(stringOfCars);
 
-// toDo split
-let totalCars2 = stringOfCars.split();
-console.log (totalCars2);
-
+// split
+var totalCars = stringOfCars.split(',');
+// console.log (totalCars);
 
 // reverse
-let carsInReverse = totalCars.reverse();
-console.log(carsInReverse);
+var carsInReverse = totalCars.reverse();
+// console.log(carsInReverse);
 
 // sort
+var carsInReverse = carsInReverse.sort();
+// alert(carsInReverse.indexOf('Buick'));
+// console.log(carsInReverse);
+
+// slice
+var removedCars = carsInReverse.slice(4, 6);
+// console.log(removedCars);
+
+// splice
+var removed = carsInReverse.splice(1, 2, 'Ford', 'Honda');
+// console.log(carsInReverse);
+
+// push
+carsInReverse.push('Chevrolet', 'Civic');
+// console.log(carsInReverse);
+
+// pop
+carsInReverse.pop();
+// console.log(carsInReverse);
+
+// shift
+var shift = carsInReverse.shift();
+// console.log(shift);
+
+// unshift
+var unshift = carsInReverse.unshift('Rolls Royce');
+// console.log(unshift);
+// console.log(carsInReverse);
+
+// forEach
+var numbers = [23, 45, 0, 2];
+numbers.forEach(function(number){
+  console.log(number + 2);
+});
