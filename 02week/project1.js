821ba346752d68c9d111ec4da3157f933fe3fa69
Jason Kelley <jasonkelley@Jasons-MacBook-Pro.local>
2017-07-21 19:41:14 -0500

    project1 complete

--- 02week/project1.js
+++ 02week/project1.js
@@ -1,59 +1,59 @@
 
 // length
 var cars = ['Ford', 'Chevrolet', 'Buick', 'Mustang'];
-// console.log(cars.length);
+console.log(cars.length);
 
 // concat
 var moreCars = ['Civic', 'Mercedes', 'Dodge', 'Honda'];
 var totalCars = cars.concat(moreCars);
-// console.log (totalCars);
+console.log (totalCars);
 
 // indexOf && lastIndexOf
 var x = totalCars.indexOf('Honda');
-// console.log (x);
+console.log (x);
 var y = totalCars.lastIndexOf('Ford');
-// console.log (y);
+console.log (y);
 
 // join
 var stringOfCars = totalCars.join();
-// console.log(stringOfCars);
+console.log(stringOfCars);
 
 // split
 var totalCars = stringOfCars.split(',');
-// console.log (totalCars);
+console.log (totalCars);
 
 // reverse
 var carsInReverse = totalCars.reverse();
-// console.log(carsInReverse);
+console.log(carsInReverse);
 
 // sort
 var carsInReverse = carsInReverse.sort();
 // alert(carsInReverse.indexOf('Buick'));
-// console.log(carsInReverse);
+console.log(carsInReverse);
 
 // slice
 var removedCars = carsInReverse.slice(4, 6);
-// console.log(removedCars);
+console.log(removedCars);
 
 // splice
 var removed = carsInReverse.splice(1, 2, 'Ford', 'Honda');
-// console.log(carsInReverse);
+console.log(carsInReverse);
 
 // push
 carsInReverse.push('Chevrolet', 'Civic');
-// console.log(carsInReverse);
+console.log(carsInReverse);
 
 // pop
 carsInReverse.pop();
-// console.log(carsInReverse);
+console.log(carsInReverse);
 
 // shift
 var shift = carsInReverse.shift();
-// console.log(shift);
+console.log(shift);
 
 // unshift
 var unshift = carsInReverse.unshift('Rolls Royce');
-// console.log(unshift);
+console.log(unshift);
 // console.log(carsInReverse);
 
 // forEach
