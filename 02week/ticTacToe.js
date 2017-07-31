1d6e8b7908a61efd380c839d26fd1cfa904b03d3
McQueenMeghan <meghan.a.mcqueen@gmail.com>
2017-07-31 15:40:39 -0500

    mastermind, ckpt1

--- 02week/ticTacToe.js
+++ 02week/ticTacToe.js
@@ -22,25 +22,53 @@ function printBoard() {
   console.log('  ---------');
   console.log('2 ' + board[2].join(' | '));
 }
+let playerTurn = 'X';
+
+function printBoard() {
+  console.log('   0  1  2');
+  console.log('0 ' + board[0].join(' | '));
+  console.log('  ---------');
+  console.log('1 ' + board[1].join(' | '));
+  console.log('  ---------');
+  console.log('2 ' + board[2].join(' | '));
+}
 
 function horizontalWin() {
-  // Your code here
+  return (
+    (board[0][0] === playerTurn && board[0][1] === playerTurn && board[0][2] === playerTurn) ||
+    (board[1][0] === playerTurn && board[1][1] === playerTurn && board[1][2] === playerTurn) ||
+    (board[2][0] === playerTurn && board[2][1] === playerTurn && board[2][2] === playerTurn)
+  );
 }
 
 function verticalWin() {
-  // Your code here
+  return (
+    (board[0][0] === playerTurn && board[1][0] === playerTurn && board[2][0] === playerTurn) ||
+    (board[0][1] === playerTurn && board[1][1] === playerTurn && board[2][1] === playerTurn) ||
+    (board[0][2] === playerTurn && board[1][2] === playerTurn && board[2][2] === playerTurn)
+  );
 }
 
 function diagonalWin() {
-  // Your code here
+  return (
+    (board[0][0] === playerTurn && board[1][1] === playerTurn && board[2][2] === playerTurn) ||
+    (board[2][0] === playerTurn && board[1][1] === playerTurn && board[0][2] === playerTurn)
+  );
 }
 
 function checkForWin() {
-  // Your code here
+  if ( horizontalWin() || verticalWin() || diagonalWin() ) {
+    console.log('Player ' +  playerTurn + ' Won!'); // announce to the world
+    return true;
+  }
+  return false;
 }
 
 function ticTacToe(row, column) {
-  // Your code here
+  board[row][column] = playerTurn;
+  if (!checkForWin()) {
+    playerTurn = (playerTurn === 'X') ? 'O' : 'X';
+  }
 }
 
 function getPrompt() {
@@ -54,7 +82,6 @@ function getPrompt() {
   });
 
 }
-
 
 
 // Tests
--- 03week/mastermind.js
+++ 03week/mastermind.js
@@ -11,37 +11,63 @@ let board = [];
 let solution = '';
 let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
-function printBoard() {
+function printBoard () {
   for (let i = 0; i < board.length; i++) {
     console.log(board[i]);
   }
 }
 
-function generateSolution() {
+function generateSolution () {
   for (let i = 0; i < 4; i++) {
     const randomIndex = getRandomInt(0, letters.length);
     solution += letters[randomIndex];
-  } //*Don't do anything with this*
+  } // *Don't do anything with this*
 }
 
-function getRandomInt(min, max) {
+function getRandomInt (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateHint() {
-  // your code here
-  // most of your logic
-  // white point if it's right letter, wrong place 1-1 (R)
+function generateHint (solution, guess) {
+  // white point if it's right letter, wrong place 0-1 (R)
   // red point if it's right letter, right place 1-1 (L)
+  var white = ['almost-correct'];
+  var red = ['correct'];
+
+  white = 0;
+  red = 0;
+  guess = guess.split('white');
+  solution = solution.split('red');
+  for (var i = 0; i < 4; i++) {
+    if (guess[i] === solution[i]) {
+      white++;
+      solution[i] = null;
+    }
+  }
+
+  for (i = 0; i < 4; i++) {
+    var redIndex = solution.indexOf(guess[i]);
+    if (redIndex > -1) {
+      red++;
+
+      solution[redIndex] = null;
+    }
+  }
+  return (white + '' + red);
 }
 
-function mastermind(guess) {
+function mastermind (guess) {
   solution = 'abcd'; // Comment this out to generate a random solution
-  // your code here
+  if (guess === solution) {
+    console.log('We Have a Winner!');
+    return ('We Have a Winner!');
+  } else {
+    var hint = generateHint(solution, guess);
+    board.push(guess + '' + hint);
+  }
 }
 
-
-function getPrompt() {
+function getPrompt () {
   rl.question('guess: ', (guess) => {
     mastermind(guess);
     printBoard();
@@ -59,7 +85,7 @@ if (typeof describe === 'function') {
       assert.equal(board.length, 1);
     });
     it('should be able to detect a win', () => {
-      assert.equal(mastermind(solution), 'You guessed it!');
+      assert.equal(mastermind(solution), 'We Have a Winner!');
     });
   });
 
