d3484a97e5502935177fff59fcbcb112178df484
Eddie Garcia <fasteddiegarcia@gmail.com>
2017-08-03 18:38:03 -0500

    Add Algorithm code

--- 03week/mastermind.js
+++ 03week/mastermind.js
@@ -29,45 +29,46 @@ function getRandomInt(min, max) {
 }
 
 function generateHint(itemsArr) {
+  // **** Should this be 'correctItemsArr'? ****
   return `${itemsArr.length} are correct`
   //
 }
 
-
+// Guess should contain 4 items which would compare each item and its position to the solution's items and position
 function mastermind(guess) {
   solution = 'abcd'; // Comment this out to generate a random solution
-  // The guess should contain 4 items which would compare each item position to
-  // the solution's items and position
-  // Create solution into an array in order to compare the index of each item
+  // Turn solution into an array in order to compare the index of each item on the board
   const solutionArr = solution.split('');
-  // pushing the guess to the board to then check against solution
+  // Push the guess to the board to then check against solution
   const boardLength = board.push(guess);
-  // if boardLength is equal to 4 compare to solution
+  // Create an array to compare to the board, which will allow us to generate a hint; ie, "3 guesses are correct"
   const correctItemsArr = [];
-
+  // Check to see if the board has a valid answer of 4 pegs
+  // If boardLength is equal to 4, compare board pegs to solution pegs
   if (boardLength === 4) {
-    // console.log(solutionArr, 'arr')
-
+    // Iterate through all the indices / pegs in the BOARD array
     for (let i = 0; i < board.length; i++) {
-
+      // Iterate through all the indices / pegs in the SOLUTION array
       for (let e = 0; e < solutionArr.length; e++) {
-
+        // Beginning at the board[0] position, if each indices' peg in the BOARD matches the SOLUTION peg
         if (board[i] === solutionArr[e]) {
+          // Add the current BOARD peg to the correctItemsArr array
           correctItemsArr.push(i)
         }
       }
     }
+    // If the correctItemsArr is full of correct pegs
     if (correctItemsArr.length === 4) {
+      // Print the winning statement
       console.log('You are a BOSS! You guessed all 4 items correctly!')
       return 'You are a BOSS! You guessed all 4 items correctly!'
     } else {
+      // If the correctItemsArr isn't full and correct, run the generateHint function
       console.log(generateHint(correctItemsArr))
     }
+    // Clear the board after each guess
     board = [];
   }
-
-  // if true then check for win(items and position are eqaul)
-  // if items and posiitons are not equal run hint
   console.log(boardLength, 'length');
 }
 
