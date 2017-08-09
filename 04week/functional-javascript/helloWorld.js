779cf602c5e562defa820d61782c9ee7f526ba5f
CGottschallJ <Cameron.Gottschall@gmail.com>
2017-08-08 20:04:20 -0500

    functional Javascript

--- 04week/functional-javascript/helloWorld.js
+++ 04week/functional-javascript/helloWorld.js
@@ -2,4 +2,34 @@ function upperCaser(input) {
   return input.toUpperCase();
 }
 
-module.exports = upperCaser
+//module.exports = upperCaser
+
+//module.exports = upperCaser
+
+function repeat(operation, num) {
+  if (num <= 0) return
+  operation()
+  return repeat(operation, --num)
+}
+
+//module.exports = repeat
+
+
+
+function doubleAll(numbers) {
+  return numbers.map(element => element * 2);
+}
+
+//module.exports = doubleAll
+
+function getShortMessages(messages) {
+  return messages.map(messageObj => messageObj['message']).filter(message => message.length < 50);
+}
+//module.exports = getShortMessages
+
+function checkUsersValid(goodUsers) {
+  return function allUsersValid(submittedUsers) {
+    
+  };
+}
+module.exports = checkUsersValid
