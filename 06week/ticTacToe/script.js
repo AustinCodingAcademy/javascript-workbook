fe23ffe569a40829cd7fb21a4850a1faba27fcc6
situlillian <mslilliansitu@gmail.com>
2017-08-14 12:59:46 -0500

    Initial commit for Tic Tac Toe

--- 06week/ticTacToe/script.js
+++ 06week/ticTacToe/script.js
@@ -2,4 +2,20 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   // Your code here
+  (function playGame() {
+    const playerTurn = 1;
+    let dataCells = document.querySelectorAll('div[data-cell]');
+    console.log(dataCells);
+
+    dataCells.forEach(function (cell) {
+      cell.addEventListener('click', onGridClick);
+    });
+
+    function onGridClick(event) {
+      console.log(`${event.type} got fired`);
+    }
+
+    (function playerActions() {
+    })();
+  })();
 });
