9106ce0d667aa15a5839ec4d1d5a5f3887b9dd58
McQueenMeghan <meghan.a.mcqueen@gmail.com>
2017-08-16 18:10:33 -0500

    tic-tac-toe DOM

--- 06week/ticTacToe/script.js
+++ 06week/ticTacToe/script.js
@@ -1,5 +1,54 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Your code here
+  // YOUR CODE HERE
+  let toggle = true;
+
+  document.querySelectorAll('[data-cell]').forEach((div) => {
+    div.addEventListener('click', (e) => {
+      if (!e.target.innerHTML) {
+        e.target.innerHTML = toggle ? 'X' : 'O';
+        toggle = !toggle;
+      } else {
+      alert('One Attempt Per Square!');
+      }
+    });
+  });
+// CLEAR BOARD
+  document.querySelector('button').addEventListener('click', (e) => {
+    document.querySelectorAll('[data-cell]').forEach((div) => {
+      div.innerHTML = '';
+    });
+  });
 });
+
+// CHECK FOR WIN
+//function checkWinner () {
+     //if((datacell0.text() != '') && (datacell0.text () == datacell1.text())
+      //&& (datacell1.text() == datacell2.text())){
+       //return true;
+     //} else if((datacell3.text() != '') && (datacell3.text () == datacell4.text())
+        //&& (datacell4.text() == datacell5.text())){
+         //return true;
+       //} else if((datacell6.text() != '') && (datacell6.text () == datacell7.text())
+        //&& (datacell7.text() == datacell8.text())){
+         //return true;
+       //} else if((datacell0.text() != '') && (datacell0.text () == datacell3.text())
+        //&& (datacell3.text() == datacell6.text())){
+         //return true;
+       //} else if((datacell1.text() != '') && (datacell1.text () == datacell4.text())
+        //&& (datacell4.text() == datacell7.text())){
+         //return true;
+      //} else if((datacell2.text() != '') && (datacell2.text () == datacell5.text())
+        //&& (datacell5.text() == datacell8.text())){
+         //return true;
+       //} else if((datacell0.text() != '') && (datacell0.text () == datacell4.text())
+        //&& (datacell4.text() == datacell8.text())){
+         //return true;
+       //} else if((datacell2.text() != '') && (datacell2.text () == datacell4.text())
+        //&& (datacell4.text() == datacell6.text())){
+         //return true;
+       //} else {
+         //return false;
+       //}
+     //};
