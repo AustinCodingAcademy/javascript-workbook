2cb4d45a1c86bfc6398b4ddc61ebe916657bfba0
matt_Huntsberry <mmhuntsberry@gmail.com>
2017-08-25 21:25:50 -0500

    1st Commit --> Towers.js REACT

---
+++ 07week/towersOfHanoi/script.jsx
@@ -0,0 +1,54 @@
+'use strict';
+
+class TowersOfHanoi extends React.Component {
+  constructor (props) {
+    super(props);
+
+
+    this.state = { moved: {} };
+
+    this.removeBlock = this.removeBlock.bind(this);
+    this.setBlock = this.setBlock.bind(this);
+  }
+
+  render () {
+    return (
+      <div>
+        <div data-stack='1' onClick={this.setBlock}>
+          <div data-block='100' onClick={this.removeBlock}></div>
+          <div data-block='75' onClick={this.removeBlock}></div>
+          <div data-block='50' onClick={this.removeBlock}></div>
+          <div data-block='25' onClick={this.removeBlock}></div>
+        </div>
+        <div data-stack='2' onClick={this.setBlock}>
+        </div>
+        <div data-stack='3' onClick={this.setBlock}>
+        </div>
+      </div>
+    );
+  }
+
+  removeBlock (e) {
+    if (!this.state.moved.target) {
+      this.state.moved = { target: e.target, size: e.target.attributes[0].value, parent: e.target.parentNode };
+      e.target.parentNode.removeChild(e.target);
+    }
+  }
+  setBlock (e) {
+    let end = e.target.children[e.target.children.length - 1];
+    let endBlock = end && parseInt(end.attributes[0].value, 10);
+
+    if (!endBlock || endBlock > this.state.moved.size) {
+      e.target.appendChild(this.state.moved.target);
+      this.state.moved.target = 0;
+      if (e.target.children.length === 4) {
+        alert("You da man!");
+      }
+    } else {
+      this.state.moved.parent.appendChild(this.state.moved.target);
+      this.state.moved.target = 0;
+    }
+  }
+}
+
+ReactDOM.render(<TowersOfHanoi />, document.getElementById('towers-of-hanoi'));
