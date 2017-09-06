2404aac28f7659029ab321c2cfbef057b03306f8
Eddie Garcia <fasteddiegarcia@gmail.com>
2017-09-06 00:26:13 -0500

    Initial HackerNews app code

---
+++ 09week/hacker-news/src/App.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+// Child of Timeline
+class Story extends Component {
+  render() {
+    return (
+      <Details />
+    );
+  }
+}
+
+// Child of Story
+class Details extends Component {
+  render() {
+    return (
+      <div>
+        <p></p>
+      </div>
+    );
+  }
+}
+
+// Parent
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      // Begin topStories with empty array, so we can push into it
+      topStories: [],
+    };
+  }
+
+  componentDidMount() {
+    // Initial API call to HN for top stories, returns 8 digit number
+    fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
+    // Pass response to function that will JSONify the response
+    .then((res) => {res.json()
+      // Slice the JSON formatted response, called storyIds, to get the first 20
+      .then((storyIds) => {storyIds.slice(0, 20)
+        // For each story id in the array
+        .forEach((storyId) => {
+          // Call API, asking for specific story, with the story id in the URL. 1 - 20
+          fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json?print=pretty`)
+          // Pass response to function that will JSONify the response
+          .then((res) => {res.json()
+            // Each story is passed to setState function
+            .then((story) =>
+              //
+              this.setState({
+                // setState of topStories to the entirity of topStories + current story
+                topStories: [...this.state.topStories, story]
+              })
+            );
+          });
+        });
+      });
+    });
+  }
+  // Create method to render each story title
+  renderStories() {
+    // Return the map of topStories array, pass in 'story' as element, 'key' as iterator
+    return this.state.topStories.map((story, key) => {
+      // Multiline return statement
+      return (
+        // Define 'key' so React knows where to place element in relation to other elements
+        <div key={key}>
+          <h4>{key + 1}</h4>
+          <h5>{story.by}</h5>
+          <a className='link' href={story.url} target='_blank'> {story.title} </a>
+          <p>{story.score} points</p>
+          <hr></hr>
+
+        </div>
+      )
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.renderStories()}
+      </div>
+    );
+  }
+}
+
+export default App;
