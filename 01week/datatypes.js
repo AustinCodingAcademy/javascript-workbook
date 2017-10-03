1a70a60945ab67bf8558657f9ee9573dff69bde3
Leah Lawrence <lalawren@us.ibm.com>
2017-10-02 20:50:52 -0500

    adding a new version of datatypes.js to the forked repo...beginning of my homework

---
+++ 01week/datatypes.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// Write a JavaScript function to display the current day and time.
+let displayDate = function(){
+	let date = new Date();
+	console.log(date);
+}
+
+displayDate();
+
+// Write a JavaScript program to convert a number to a string.
+let convertNumbertoString = function() {
+	let num = 15;
+	let n = num.toString();
+}
+
+print(typeof convertNumbertoString());
+
+// Write a JavaScript program to convert a string to the number. 
