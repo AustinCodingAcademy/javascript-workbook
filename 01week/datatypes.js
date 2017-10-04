10f989e17b82d25201df00c05f6ce4895f8968be
walzer85 <walzer85@gmail.com>
2017-10-03 20:45:50 -0500

    wrote comments for first assignment

---
+++ 01week/datatypes.js
@@ -0,0 +1,17 @@
+//returns current date and time
+//make a today var, which is assigned a new date instance
+//make new var to store the today var and toDateString method
+//use var today with toTimeString method
+
+//create a var to convert.
+//input a number into var convert
+//use convert and toString() method to make the number a string
+
+//first method to convert string to number
+//input a string into parseInt() method and returns number
+//or...
+//set var to a string
+//use Number() method
+
+//tells what type of datatype your input is
+//use the typeOf method
