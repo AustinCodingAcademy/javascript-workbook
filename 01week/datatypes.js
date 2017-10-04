0c30f093e72623fc3d6d665142422eae5b6de7cb
Mark Hinojosa <markhinojosa1212@gmail.com>
2017-10-03 20:45:34 -0500

    questions

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -1,1 +1,40 @@
 'use strict'
+
+//Write a JavaScript program to display the current day and time.
+//function that receives time and date and displays upon request
+
+
+
+
+// Write a JavaScript program to convert a number to a string.
+//assign variable, write function that receives a number and converts it to a string, using num.toString();
+
+
+
+
+//Write a JavaScript program to convert a string to the number.
+//parseint a string argumqewriopnt and return an interger
+
+
+
+
+//Write a JavaScript program that takes in different datatypes and prints out whether they are a:
+// Boolean
+// Null
+// Undefined
+// Number
+// NaN
+// String
+//function takes in argument and use typeof() to declare what kind of datatype it is
+
+
+
+// Write a JavaScript program that adds 2 numbers together.
+//function that adds intergers and numbers and displays the result, use +,-,*,/
+
+
+
+
+
+//Write a JavaSc\gh'ript program that runs only when 2 things are true.
+//function that
