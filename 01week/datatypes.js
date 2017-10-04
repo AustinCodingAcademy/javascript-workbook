194c989f08cdb72a3fd2bb66606c4c36a9f7e4b7
Stephen Williams <stephen@wearetmbr.com>
2017-10-04 17:30:07 -0500

    datatypes project

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -30,12 +30,67 @@ convertString('6');
 
 // display datatype of variable
 
-let dataTypes = function(x) {
-	let type = typeof(x);
+let dataTypes = function(y) {
+	let type = typeof(y);
 	console.log(type);
 }
 
 dataTypes('true');
 
 // adding two integers
+
+let calculator = function(a, b){ 
+	let sum = a + b;
+	console.log(sum);
+}
+calculator(2, 6);
+
+
+// run if both things are true
+
+
+let twoTruths = function(c,d){
+
+	if ( c > 0 && d > 0 ) {
+		console.log('both true');
+	} else {
+		console.log('incorrect');
+	}
+}
+
+twoTruths(3, 6);
+
+// run if one of two things are true
+
+let oneTruth = function(e,f){
+
+	if ( e > 0 || f > 0 ) {
+		console.log('one is true');
+	} else {
+		console.log('incorrect');
+	}
+}
+
+oneTruth(3, -6);
+
+// run if both things are false
+
+let twoFalse = function(c,d){
+
+	if ( c < 0 && d < 0 ) {
+		console.log('incorrect');
+	} else {
+		console.log('both false');
+	}
+}
+
+twoFalse(3, 6);
+
+
+
+
+
+
+
+
 
