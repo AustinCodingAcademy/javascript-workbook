4dab1e69d05f96392308b1ccb716c7b3db747ebb
Trevor Charnquist <tcharnquist@gmail.com>
2017-10-03 19:52:39 -0500

    Added code to datatypes

---
+++ 01week/datatypes.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const day = new Date();
+console.log(day);
+
+const numToStr = 1337;
+const nTS = numToStr.toString();
+console.log(nTS);
+
+const strToNum = parseInt("112358");
+console.log(strToNum);
+
+console.log(typeof true);
+console.log(typeof null); //null is an Object for some reason//
+console.log(typeof undefined);
+console.log(typeof NaN);
+console.log(typeof "Words")
+
+const addi = 4+6;
+console.log(addi);
+
+
+const oner = 1;
+const twoer = 2;
+const fourer = 4;
+if (oner+oner === twoer && twoer+twoer === fourer) {
+  console.log("It Works");
+} else {
+  console.log("It doesn't work");
+}
+
+if (oner+oner === twoer || twoer+twoer === fourer) {
+  console.log("It Works");
+} else {
+  console.log("It doesn't work");
+}
+
+if (oner+twoer === fourer && fourer+fourer === oner) {
+  console.log("It works");
+} else {
+  console.log("It doesn't work");
+}
+
