59c2799e4fbc8e4d32b128dba575013f3a208817
reneedudley <4reneedudley@gmail.com>
2017-10-05 18:54:37 -0500

    renee added to name

--- 01week/javascripting/dataTypes.js
+++ 01week/javascripting/dataTypes.js
@@ -1,2 +1,19 @@
 'use strict';
 console.log('here');
+
+
+// Write a JavaScript program that runs only when 2 things are true.
+
+// function that recieves to items, if those two things are true.
+// methods: function, 2 arguments, if/ then statement
+
+function evaluatesToTrue(arg1, arg2) {
+    if(arg1 && arg2){
+        return 'both are true'
+    }else{
+        return 'nope'
+    }
+}
+
+evaluatesToTrue(4, 6);
+evaluatesToTrue(null, 6);
