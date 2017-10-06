e53a835cf792fd94444408daf79fb661e168f81a
Erika Ortiz <erikaortiz@Erikas-MacBook-Air.local>
2017-10-05 20:47:37 -0500

    class2-rps

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -9,10 +9,30 @@ const rl = readline.createInterface({
 
 
 function rockPaperScissors(hand1, hand2) {
+//compare hand 1 with hand2 to find a tie
+//compare hand1 with hand2 to find a winner
+//rock beats scissors; scissors beats paper; paper beats rock 
 
-  // Write code here
+if (hand1 === hand2) {
+  return("It's a tie!");
+} else if (hand1 === "rock" && hand2 === "paper") {
+  return("Hand two wins!");
+} else if (hand1 === 'paper' && hand2 === 'rock') {
+  return("Hand one wins!");
+} else if (hand1 === 'rock' && hand2 === 'scissors') {
+  return ("Hand one wins!");
+} else if (hand1 === 'scissors' && hand2 === 'rock') {
+  return ("Hand two wins!");
+} else if (hand1 === 'scissors' && hand2 === 'paper') {
+  return ("Hand one wins!");
+} else if (hand1 === 'paper' && hand2 === 'scissors') {
+  return ("Hand two wins!");
+} else {
+  return ("What are you saying? You have to type 'rock','paper', or 'scissors'!");
+}
 
 }
+
 
 function getPrompt() {
   rl.question('hand1: ', (answer1) => {
