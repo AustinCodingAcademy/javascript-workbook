14e50e783aeacaa21955571459382c2ce18cf493
Patrick Creamer <patrickseancreamer@gmail.com>
2017-10-10 17:42:37 -0500

    rockPaperScissors complete

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -9,7 +9,25 @@ const rl = readline.createInterface({
 
 
 function rockPaperScissors(hand1, hand2) {
-
+let h1 = hand1.toLowerCase().trim();
+//console.log("h1 is "+h1);
+let h2 = hand2.toLowerCase().trim();
+//console.log("h2 is "+h2);
+if ((h1===h2)&&
+((h1==="paper")||(h1==="scissors")||(h1==="rock"))&&
+((h2==="paper")||(h2==="scissors")||(h2==="rock"))){
+  return "It's a tie!";
+}else if (((h1==="scissors")&&(h2==="paper"))||
+((h1==="paper")&&(h2==="rock"))||
+((h1==="rock")&&(h2==="scissors"))){
+  return "Hand one wins!";
+}else if (((h2==="scissors")&&(h1==="paper"))||
+((h2==="paper")&&(h1==="rock"))||
+((h2==="rock")&&(h1==="scissors"))){
+  return "Hand two wins!";
+}else {
+  return "SOMEBODY---not gonna name any names---but somebody had an invalid input.";
+}
   // Write code here
 
 }
