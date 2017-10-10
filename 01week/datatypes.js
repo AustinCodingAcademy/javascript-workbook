38395d588196692acb740cec9856a504640762bd
Neil Brownell <neil@onnit.com>
2017-10-10 16:40:55 -0500

    simplifying boolean statement in checkIfOneofTwo(), making sure function use return instead of log for their result, and using ternary statements in some of the really simple functions

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -1,7 +1,6 @@
 'use strict';
 
 
-console.log('Problem 1.');
 // 1. Write a JavaScript program to display the current day and time.
 // ------------------------------------------------------------------
 // I'm going to declare a variable to store an instance of the Date() object
@@ -9,13 +8,12 @@ console.log('Problem 1.');
 // I'll console.log the result
 function displayDayAndTime() {
   var currentDate = new Date();
-  console.log(currentDate.toLocaleString());
+  return currentDate.toLocaleString();
 }
 displayDayAndTime();
 
 
 
-console.log('\nProblem 2.');
 // 2. Write a JavaScript program to convert a number to a string.
 // ------------------------------------------------------------------
 // I'm going to write a function that accepts a number as its argument
@@ -23,18 +21,15 @@ console.log('\nProblem 2.');
 // There are at least 2 other options I could use:
 // http://2ality.com/2012/03/converting-to-string.html
 function convertNumToString(arg) {
-  if (isNaN(arg)) {
-    console.log('error: argument provided was not a number');
-  } else {
-    console.log(String(arg));
-  }
+  return (isNaN(arg))
+    ? 'error: argument provided was not a number'
+    : String(arg);
 }
 convertNumToString('foo'); // returns an error
 convertNumToString(7); // returns '7'
 
 
 
-console.log('\nProblem 3.');
 // 3. Write a JavaScript program to convert a string to a number.
 // ------------------------------------------------------------------
 // I'm going to write a function that accepts one argument
@@ -42,24 +37,21 @@ console.log('\nProblem 3.');
 // If it can't be parsed as a number, the function will log an error
 function convertStringToNum(arg) {
   var ret = parseInt(arg);
-  if (isNaN(ret)) {
-    console.log('error, the provided argument cannot be parsed to a number');
-  } else {
-    console.log(ret);
-  }
+  return (isNaN(ret))
+    ? 'error, the provided argument cannot be parsed to a number'
+    : ret;
 }
 convertStringToNum('asdf'); // returns an error
 convertStringToNum('44'); // returns 44
 
 
 
-console.log('\nProblem 4.');
 // 4. Write a JavaScript program that takes in different datatypes and prints out their type
 // ------------------------------------------------------------------
 // I'm writing a function that accepts an argument and uses
 // typeof to output the argument's type
 function outputType(arg) {
-  console.log(typeof arg);
+  return typeof arg;
 }
 outputType(7); // returns 'number'
 outputType(true); // returns 'boolean'
@@ -67,17 +59,15 @@ outputType([5,6,7]); // returns 'object'
 
 
 
-console.log('\nProblem 5.');
 // 5. Write a JavaScript program that adds 2 numbers together.
 // ------------------------------------------------------------------
 // I'm going to write a function that accepts two arguments,
 // tries to ensure they are both numbers, and adds them together
 function addTwoNumbers(arg1, arg2) {
   if (isNaN(arg1) || isNaN(arg2)) {
-    console.log('error: one of more of your arguments was not a number');
+    return 'error: one of more of your arguments was not a number';
   } else {
-    var ret = parseInt(arg1) + parseInt(arg2);
-    console.log(ret);
+    return parseInt(arg1) + parseInt(arg2);
   }
 }
 addTwoNumbers(5, 9); // returns 14
@@ -85,18 +75,15 @@ addTwoNumbers(5, '9'); // returns 14
 addTwoNumbers(5, 'asdf'); // returns an error
 
 
-console.log('\nProblem 6.');
 // 6. Write a JavaScript program that runs only when 2 things are true.
 // ------------------------------------------------------------------
 // I'm going to write a function that accepts two arguments
 // and uses a conditional statement with an && operator to check if they are true
 // note: I'm not checking that they literally === true, just if they are truthy
 function checkIfBothTrue(arg1, arg2) {
-  if (arg1 && arg2) {
-    console.log('both true');
-  } else {
-    console.log('error');
-  }
+  return (arg1 && arg2)
+    ? 'both true'
+    : 'error';
 }
 checkIfBothTrue(5, {}); // returns 'both true'
 var foobar = true;
@@ -105,46 +92,34 @@ var whatever = null;
 checkIfBothTrue(whatever, 9); // returns 'error'
 
 
-console.log('\nProblem 7.');
+
 // 7. Write a JavaScript program that runs when 1 of 2 things are true.
 // ------------------------------------------------------------------
 // I'm going to write a function that accepts two arguments and uses conditionals 
 // to test the criteria that only one is true, using parentheses and && || ! operators
 // note: I'm not checking that they literally === true, just if they are truthy
 function checkIfOneofTwo(arg1, arg2) {
-  if ((arg1 && !arg2) || (!arg1 && arg2)) {
-    console.log('success: 1 of 2 arguments true');
-  } else {
-    console.log('error');
-  }
+  return (arg1 || arg2) 
+    ? 'success: 1 of 2 arguments true'
+    : 'error';
 }
 checkIfOneofTwo('some string', false); // returns 'success'
 checkIfOneofTwo(false, false); // returns 'error'
-checkIfOneofTwo(true, true); // returns 'error'
 
 
-console.log('\nProblem 8.');
+
 // 8. Write a JavaScript program that runs when both things are not true.
 // ------------------------------------------------------------------
 // I'm going to write a function that accepts two arguments
 // It will use a conditional statement, with the && and ! operators
 // note: I'm not checking that they literally === true or false, just if they are truthy
 function checkIfBothFalse(arg1, arg2) {
-  if (!arg1 && !arg2) {
-    console.log('success: both arguments are not true');
-  } else {
-    console.log('error');
-  }
+  return (!arg1 && !arg2)
+    ? 'success: both arguments are not true'
+    : 'error';
 }
 checkIfBothFalse(false, false); // returns 'success'
 checkIfBothFalse('Lorem ipsum', false); // returns 'error'
 checkIfBothFalse(true, true); // returns 'error'
 var testA = false, testB = null;
 checkIfBothFalse(testA, testB); // returns 'success'
-
-
-function bye() {
-  console.log('\n\n');
-  return "... end of assignment";
-}
-bye();
