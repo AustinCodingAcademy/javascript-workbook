a400b9bfce93e4ea2b222ba06984c0f433118bc1
Trevor Charnquist <tcharnquist@gmail.com>
2017-10-19 18:07:24 -0500

    Pig latin upload, everything works except trimming, gonna work on that.

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -8,10 +8,22 @@ const rl = readline.createInterface({
 });
 
 
+ //is the first letter a vowel? If so, just add 'yay' at end.
+  //test for complex first letters. 
+  // ch th cr ph 
+
 function pigLatin(word) {
 
-  // Your code here
 
+  if( (word.charAt(0) === 'e') || (word.charAt(0) === 'a') || (word.charAt(0) === 'i') || (word.charAt(0) === 'o') || (word.charAt(0) === 'u') ) {
+    return word + 'yay';
+    }else 
+      if((word.charAt(1) === 'r') || (word.charAt(1) === 'h') || (word.charAt(1) === 'l') || (word.slice(0, 2) === 'qu') ) {
+        return word.slice(2) + word.slice(0, 2) + 'ay';
+    }else{
+      return word.slice(1) + word.slice(0, 1) + 'ay';
+  }
+  //is there a way to modify the return after it has done the above? Can the '.trim' method work at the end of the function without putting it in each return?
 }
 
 
