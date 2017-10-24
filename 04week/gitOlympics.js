7d6c86886eae7e51532acba7f969c21f1cc58090
walzer85 <walzer85@gmail.com>
2017-10-24 18:57:02 -0500

    added one more city

--- 04week/gitOlympics.js
+++ 04week/gitOlympics.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const names = ['athens', 'berlin', 'atlanta', 'seoul']
+const names = ['athens', 'berlin', 'atlanta', 'seoul', 'los angeles']
 
 const printListOfOlympics=(arr)=>{
   arr.forEach(i => console.log(i))
