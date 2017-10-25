d9b6de24babee8ead7923e51759962440c43ab45
gormangit <contact@jongorman.top>
2017-10-24 19:03:48 -0500

    first

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const printlistOfOlympians = (myArr) => {
+    myArr.forEach((item, index) => {
+        console.log(item)
+    });
+}
+console.log(printlistOfOlympians(['steph', 'john', 'edward', 'renee', 'mia']));
