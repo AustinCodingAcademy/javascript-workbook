d6f5ae8ec038e75c8913826e7ff1cc7c7f262d32
Trevor Charnquist <tcharnquist@gmail.com>
2017-10-24 19:09:40 -0500

    Still don't work

--- 04week/gitOlympics.js
+++ 04week/gitOlympics.js
@@ -1,9 +1,11 @@
 'use strict';
 
-let arr1 = ['Zeus', 'Hades', 'Poseidon', 'Apollo', 'Hera']
+const arr1 = ['Zeus', 'Hades', 'Poseidon', 'Apollo', 'Hera'];
 
 const printListOfOlympians = (arr1) => {
-	arr1.forEach(olympians) => {
-		console.log(arr1);
-	}
+	arr1.forEach((olympians) => {
+		console.log(olympians);
+	})
 } 
+
+printListOfOlympians(arr1)
