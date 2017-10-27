f13846d5d38199f4ac147a5ebf70f6bc28222220
walzer85 <walzer85@gmail.com>
2017-10-27 06:59:13 -0500

    completeed for...in loop

--- 04week/loop.js
+++ 04week/loop.js
@@ -9,3 +9,16 @@ carsInReverse.forEach((item)=>{
 });
 
 carsInReverse;
+
+//create an object with the personal information from assignment
+const personalInfo = {
+  firstName: "Jane",
+  lastName: "Doe",
+  birthDate: "Jan 5, 1925",
+  gender: "female";
+}
+
+//create a for...in loop that prints out the contents of personalInfo
+for (const prop in personalInfo) {
+  console.log(`personalInfo.${prop} = ${personalInfo[prop]}`);
+}
