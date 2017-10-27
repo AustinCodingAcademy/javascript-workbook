74a2c5dbaf382135e6a44152537f6db5ece27199
walzer85 <walzer85@gmail.com>
2017-10-27 06:30:25 -0500

    finished first for loop

---
+++ 04week/loop.js
@@ -0,0 +1,11 @@
+'use strict'
+
+//create a variable with several car makers
+const carsInReverse ['toyota', 'honda', 'chevrolet', 'puegot']
+
+//using forEach, console log the items inside the array carsInReverse
+carsInReverse.forEach((item)=>{
+  console.log(item);
+});
+
+carsInReverse;
