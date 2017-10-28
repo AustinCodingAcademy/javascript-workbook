3e26c3e28b0abd1cdf6845cb962adce47210afb2
Mark Hinojosa <markhinojosa1212@gmail.com>
2017-10-28 11:38:28 -0500

    print last values in stacks

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -21,46 +21,84 @@ function printStacks() {
   console.log("a: " + stacks.a);
   console.log("b: " + stacks.b);
   console.log("c: " + stacks.c);
-}
+};
 
-function movePiece(startStack,endStack) {
+function movePiece(startStack, endStack){
   // target last number in array column
-
-}
-
-function isLegal() {
-  // Your code here
-  if(lastInStartStack < lastInEndStack){
-    console.log('ok to move');
-  }
-
-}
-
-function checkForWin() {
-  // Your code here
-
-}
-
-function towersOfHanoi(startStack, endStack) {
-  // Your code here
-console.log(startStack);
-console.log(stacks.a);
-  console.log(stacks[startStack][stacks.startStack.length]);
-
-
-  // let pieceEnd = stacks.startStack[(stacks.a.length) - 1];
 //
-// movePiece(startStack, endStack);
-}
+//  let endStackLength = stacks[endStack][(stacks[endStack].length
 
-function getPrompt() {
-  printStacks();
-  rl.question('start stack: ', (startStack) => {
-    rl.question('end stack: ', (endStack) => {
-      towersOfHanoi(startStack, endStack);
-      getPrompt();
-    });
-  });
-}
+        // if(startStackLength > 0){
+        //   let startStackLastNumber = stacks[startStack][(stacks[startStack].length - 1)]
+        //   console.log('start stack last value is ' + startStackLastNumber)
+        // } else {
+        //   return console.log('start stack is empty');
+        // }
+        //
+        // if(stacks[endStack][(stacks[endStack].length > 0){
+        //       let endStackLastNumber = stacks[endStack][(stacks[startStack].length - 1)]
+        //       return console.log('end stack last value is ' + endStackLastNumber)
+        //     } else {
+        //       return console.log('end stack is empty')
+        //     }
+        //
+        //
 
-getPrompt();
+      };
+
+          function isLegal() {
+            // Your code here
+            if (lastInStartStack < lastInEndStack) {
+              console.log('ok to move');
+            }
+
+          }
+
+          function checkForWin() {
+            // Your code here
+
+          }
+
+          function towersOfHanoi(startStack, endStack) {
+            // Your code here
+
+            // target last number in array column
+let startStackLength = stacks[startStack].length;
+ let endStackLength = stacks[endStack].length;
+console.log(startStackLength + '' + endStackLength)
+      if(startStackLength > 0){
+        let startStackLastNumber = stacks[startStack][(stacks[startStack].length - 1)];
+        console.log('start stack last value is ' + startStackLastNumber)
+      } else {
+        return console.log('start stack is empty');
+      }
+
+      if(endStackLength > 0){
+            let endStackLastNumber = stacks[endStack][(stacks[startStack].length - 1)];
+            return console.log('end stack last value is ' + endStackLastNumber)
+          } else {
+            return console.log('end stack is empty')
+          }
+            // let endStackLastNumber = stacks[endStack][(stacks[startStack].length - 1)];
+            //
+            // let startStackLastNumber = stacks[startStack][(stacks[startStack].length - 1)];
+            //
+            // console.log(endStackLastNumber)
+
+
+            // let pieceEnd = stacks.startStack[(stacks.a.length) - 1];
+            //
+            // movePiece(startStack, endStack);
+          }
+
+          function getPrompt() {
+            printStacks();
+            rl.question('start stack: ', (startStack) => {
+              rl.question('end stack: ', (endStack) => {
+                towersOfHanoi(startStack, endStack);
+                getPrompt();
+              });
+            });
+          }
+
+          getPrompt();
