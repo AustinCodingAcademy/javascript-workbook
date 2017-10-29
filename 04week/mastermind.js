727d7deee781ef0ed502d005c21448d41bf4356c
Andrew Thomas <athomas3@trinity.edu>
2017-10-29 14:48:28 -0500

    hel

--- 04week/mastermind.js
+++ 04week/mastermind.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const colors = require('colors');
 const assert = require('assert');
 const readline = require('readline');
 const rl = readline.createInterface({
@@ -28,13 +28,40 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateHint() {
+function generateHint(solution,guess) {
   // your code here
+  console.log(solution)
+  let solutionArray = solution.split('')
+  let guessArray= guess.split('')
+  var correctLetterLocations = 0
+  let correctLetters = 0
+  for(let x = 0; x < solutionArray.length; x++){
+    if(guessArray[x]===solutionArray[x]){
+      solutionArray[x]= null;
+      correctLetterLocations ++;
+    }
+    for (let s = 0; s < solutionArray.length; s++){
+      let targetIndex = solutionArray.indexOf(guessArray[s])
+      if(targetIndex > -1){
+        correctLetters++;
+        solutionArray[s] = null
+      }
+
+    }
+  }
+
+  return correctLetterLocations.red +'-' + correctLetters.white
+
+
+
 }
 
 function mastermind(guess) {
   solution = 'abcd'; // Comment this out to generate a random solution
   // your code here
+  if (guess === 'abcd'){
+    return "You guessed it!"
+  }
 }
 
 
@@ -62,12 +89,12 @@ if (typeof describe === 'function') {
 
   describe('#generateHint()', () => {
     it('should generate hints', () => {
-      let expected = ('2'.red)+"="('2'.white)
-      assert.equal(generateHint('abdc'),expected);
+      let expected = ('2'.red)+"-"+('2'.white)
+      assert.equal(generateHint('abcd', 'abdc'),expected);
     });
     it('should generate hints if solution has duplicates', () => {
-      let expected = ('1'.red)+"="('1'.white)
-      assert.equal(generateHint('aabb'), expected);
+      let expected = ('1'.red)+"-"+('1'.white)
+      assert.equal(generateHint('abcd', 'aabb'), expected);
     });
 
   });
