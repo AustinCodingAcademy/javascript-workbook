43990cfcf3f01ee9f741262a1ebb09430edaa2d7
Mark Hinojosa <markhinojosa1212@gmail.com>
2017-10-28 21:08:55 -0500

    finished towers

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -8,14 +8,13 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+
 let stacks = {
   a: [4, 3, 2, 1],
   b: [],
   c: []
 };
 
-// let pieceStart = stacks.startStack[(stacks.a.length) - 1];
-// let pieceEnd = stacks.startStack[(stacks.a.length) - 1];
 
 function printStacks() {
   console.log("a: " + stacks.a);
@@ -23,82 +22,73 @@ function printStacks() {
   console.log("c: " + stacks.c);
 };
 
-function movePiece(startStack, endStack){
-  // target last number in array column
-//
-//  let endStackLength = stacks[endStack][(stacks[endStack].length
+function movePiece(startStack, endStack) {
 
-        // if(startStackLength > 0){
-        //   let startStackLastNumber = stacks[startStack][(stacks[startStack].length - 1)]
-        //   console.log('start stack last value is ' + startStackLastNumber)
-        // } else {
-        //   return console.log('start stack is empty');
-        // }
-        //
-        // if(stacks[endStack][(stacks[endStack].length > 0){
-        //       let endStackLastNumber = stacks[endStack][(stacks[startStack].length - 1)]
-        //       return console.log('end stack last value is ' + endStackLastNumber)
-        //     } else {
-        //       return console.log('end stack is empty')
-        //     }
-        //
-        //
+};
 
-      };
+//if the move is legal, then run this function
+function isLegal(startStack, endStack) {
+  let storePopped = stacks[startStack].pop();
+  stacks[endStack].push(storePopped);
+  return checkForWin();
 
-          function isLegal() {
-            // Your code here
-            if (lastInStartStack < lastInEndStack) {
-              console.log('ok to move');
-            }
-
-          }
-
-          function checkForWin() {
-            // Your code here
-
-          }
-
-          function towersOfHanoi(startStack, endStack) {
-            // Your code here
-
-            // target last number in array column
-let startStackLength = stacks[startStack].length;
- let endStackLength = stacks[endStack].length;
-console.log(startStackLength + '' + endStackLength)
-      if(startStackLength > 0){
-        let startStackLastNumber = stacks[startStack][(stacks[startStack].length - 1)];
-        console.log('start stack last value is ' + startStackLastNumber)
-      } else {
-        return console.log('start stack is empty');
-      }
-
-      if(endStackLength > 0){
-            let endStackLastNumber = stacks[endStack][(stacks[startStack].length - 1)];
-            return console.log('end stack last value is ' + endStackLastNumber)
-          } else {
-            return console.log('end stack is empty')
-          }
-            // let endStackLastNumber = stacks[endStack][(stacks[startStack].length - 1)];
-            //
-            // let startStackLastNumber = stacks[startStack][(stacks[startStack].length - 1)];
-            //
-            // console.log(endStackLastNumber)
+}
 
 
-            // let pieceEnd = stacks.startStack[(stacks.a.length) - 1];
-            //
-            // movePiece(startStack, endStack);
-          }
-
-          function getPrompt() {
-            printStacks();
-            rl.question('start stack: ', (startStack) => {
-              rl.question('end stack: ', (endStack) => {
-                towersOfHanoi(startStack, endStack);
-                getPrompt();
-              });
-            });
-          }
-
+//check for all 4 items in the C stack
+function checkForWin() {
+  if (stacks.c.length === 1) {
+    return 'you win!',
+      //Reset game
+      rl.question(' Press Enter to reset ', (resetAnswer) => {
+        if (resetAnswer === '') {
+          stacks = {
+            a: [4, 3, 2, 1],
+            b: [],
+            c: []
+          };
           getPrompt();
+        }
+      })
+
+  }
+}
+
+function towersOfHanoi(startStack, endStack) {
+  //check for correct inputs, create an array with acceptable inputs and us indexOf
+  let checkForExistence = ['a', 'b', 'c']
+  if (checkForExistence.indexOf(startStack) != -1 && checkForExistence.indexOf(endStack) != -1) {
+    //use .length of arrays to verify a stack is empty
+    let startStackLength = stacks[startStack].length;
+    let endStackLength = stacks[endStack].length;
+
+    //use .length -1 for get the last value in a stack
+    let startStackLastNumber = stacks[startStack][(stacks[startStack].length - 1)];
+    let endStackLastNumber = stacks[endStack][(stacks[endStack].length - 1)];
+
+    if (startStackLength === 0) {
+      console.log('enter a correct start stack');
+    }
+    //if start stack is not empty, continue with logic if not then please re-enter starting stack
+    if (startStackLength > 0) {
+      if (startStackLastNumber < endStackLastNumber || endStackLength === 0) {
+        isLegal(startStack, endStack);
+      }
+    }
+  } else {
+    console.log('Enter another stack');
+  }
+
+};
+
+function getPrompt() {
+  printStacks();
+  rl.question('start stack: ', (startStack) => {
+    rl.question('end stack: ', (endStack) => {
+      towersOfHanoi(startStack, endStack);
+      getPrompt();
+    });
+  });
+}
+
+getPrompt();
