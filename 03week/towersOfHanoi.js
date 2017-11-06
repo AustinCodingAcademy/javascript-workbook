8d3c59860c5bd7decf0d9bce04a2f9d9fa32d62f
wilymon <wilhem@gmail.com>
2017-11-06 14:38:16 -0600

    updated towers file

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -19,23 +19,44 @@ function printStacks() {
   console.log("c: " + stacks.c);
 }
 
-function movePiece() {
-  // Your code here
-
+function movePiece(startStack, endStack) {
+  let myMove = stacks[startStack].pop();
+  stacks[endStack].push(myMove);
 }
 
-function isLegal() {
-  // Your code here
+function isLegal(startStack, endStack) {
+  let startTest = stacks[startStack][stacks[startStack].length - 1];
+  let endTest = stacks[endStack][stacks[endStack].length - 1];
+
+  if ((startTest < endTest) || (stacks[endStack].length == 0)){
+    return true;
+  } else {
+    return false;
+  }
 
 }
 
 function checkForWin() {
-  // Your code here
-
+  if ((stacks.b.length === 4) || (stacks.c.length === 4)) {
+    console.log('YOU WIN!');
+    stacks = {
+      a: [4, 3, 2, 1],
+      b: [],
+      c: []
+    };
+    return true;
+  } else {
+    return false;
+  };
 }
 
 function towersOfHanoi(startStack, endStack) {
-  // Your code here
+  if (isLegal(startStack, endStack)) {
+    movePiece(startStack, endStack);
+    checkForWin();
+  } else {
+    return console.log('Invalid move. You can only move a smaller piece on top of a larger piece, or to an empty stack.');
+  };
 
 }
 
@@ -50,5 +71,3 @@ function getPrompt() {
 }
 
 getPrompt();
-
-
