bd749eca957d8e6c38f3845ef15bee037db9f5a9
wilymon <wilhem@gmail.com>
2017-11-12 13:45:36 -0600

    first commit
    got my pieces to move

--- 05week/checkers.js
+++ 05week/checkers.js
@@ -8,8 +8,8 @@ const rl = readline.createInterface({
 });
 
 
-function Checker() {
-  // Your code here
+function Checker(piece) {
+  this.symbol = piece;
 }
 
 function Board() {
@@ -24,7 +24,44 @@ function Board() {
         this.grid[row].push(null);
       }
     }
+      this.addChecker(0,1,'X');
+      this.addChecker(0,3,'X');
+      this.addChecker(0,5,'X');
+      this.addChecker(0,7,'X');
+      this.addChecker(1,0,'X');
+      this.addChecker(1,2,'X');
+      this.addChecker(1,4,'X');
+      this.addChecker(1,6,'X');
+      this.addChecker(2,1,'X');
+      this.addChecker(2,3,'X');
+      this.addChecker(2,5,'X');
+      this.addChecker(2,7,'X');
+      this.addChecker(5,0,'O');
+      this.addChecker(5,2,'O');
+      this.addChecker(5,4,'O');
+      this.addChecker(5,6,'O');
+      this.addChecker(6,1,'O');
+      this.addChecker(6,3,'O');
+      this.addChecker(6,5,'O');
+      this.addChecker(6,7,'O');
+      this.addChecker(7,0,'O');
+      this.addChecker(7,2,'O');
+      this.addChecker(7,4,'O');
+      this.addChecker(7,6,'O');
   };
+
+  let checkers = [];
+
+  this.addChecker = function(xCoord, yCoord, playerType){
+    let xChecker = new Checker(playerType);
+    this.grid[xCoord][yCoord] = xChecker;
+    checkers.push('1');
+  }
+
+  this.removeChecker = function(xCoord, yCoord){
+    this.grid[xCoord][yCoord] = null;
+    checkers.pop();
+  }
 
   // prints out the board
   this.viewGrid = function() {
@@ -51,17 +88,22 @@ function Board() {
     }
     console.log(string);
   };
-
-  // Your code here
 }
+
 function Game() {
 
   this.board = new Board();
 
+  this.moveChecker = function(whichPiece,toWhere){
+    let myMove = this.board.grid[whichPiece[0]][whichPiece[1]];
+    this.board.grid[whichPiece[0]][whichPiece[1]] = null;
+    this.board.grid[toWhere[0]][toWhere[1]] = myMove;
+  }
+
   this.start = function() {
     this.board.createGrid();
-    // Your code here
-  };
+    this.board.addChecker(0);
+  }
 }
 
 function getPrompt() {
