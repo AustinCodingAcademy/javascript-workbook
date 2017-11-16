c0a55ed8128637bb3978aea5709a2611a14ca9f2
wilymon <wilhem@gmail.com>
2017-11-15 18:44:06 -0600

    I know I've been bad about commenting in my code. I tried to be better about that in this one.

--- 06week/higherOrder.js
+++ 06week/higherOrder.js
@@ -2,25 +2,134 @@
 
 const assert = require('assert');
 
-function forEach(arr, callback) {
-  // Your code here
+//random array of elements
+
+let array1 = [1,2,3,4,5,6]
+
+//this is my callback function that changes the array in some way, in this case to increase the array
+function incrimenter(num){
+  if(num<4){
+    return (num + 2)
+  } else {
+    return (num * 2)
+  }
 }
 
-function map(arr, callback) {
-  // Your code here
+//This function takes in the array and passes each value into the callback function to increase the array
+function forEach(array, callback) {
+  let changedElements = [];
+  for (let i=0; i < array.length; i++){
+    let converter = incrimenter(array[i]);
+    changedElements.push(converter);
+  }
+  return changedElements;
 }
 
-function filter(arr, callback) {
-  // Your code here
+console.log(forEach(array1, incrimenter));
+
+//another random array to test my Map function
+let array2 = [99,87,23,67,13,77];
+
+//This is my callback function, which will change the number grades to letter grades
+let gradeConverter = function(num){
+  if (num <= 100 && num >= 90){
+    return 'A';
+  } if (num <= 89 && num >= 80){
+    return 'B';
+  } if (num <= 79 && num >= 70){
+    return 'C';
+  } if (num <= 69) {
+    return 'F';
+  }
+};
+
+//The map function takes in the array, runs each element through the gradeConverter function and returns a new array
+function map(array, callback){
+  let convertedArray = [];
+  for (let i=0; i < array.length; i++){
+      let element = array[i];
+      let converted = callback(element);
+      convertedArray.push(converted);
+    }
+    return convertedArray;
 }
 
-function some(arr, callback) {
-  // Your code here
+let classGrades = map(array2, gradeConverter);
+
+console.log(classGrades);
+
+//my third random array of numbers
+let array3 = [1,2,3,4,5];
+
+//this function returns true when the number passed into it is even
+let evenFilter = function(num){
+  return (num%2 === 0);
+};
+
+//my filter function runs each item in my array through the evenFilter function, returning only the even numbers in a new array
+function filter(array, callback){
+  let savedOff = [];
+  for (let i=0; i < array.length; i++){
+    let element = array[i];
+    if (callback(element) === true){
+      converted2 = array[i];
+      savedOff.push(converted2);
+    }
+  }
+  return savedOff;
 }
 
-function every(arr, callback) {
-  // Your code here
+let filtered = filter(array, evenFilter);
+
+console.log(filtered);
+
+//forth random array of students by gender
+let array4 = ["Girl", "Boy", "Girl", "Boy", "Boy", "Boy"];
+
+//function to check if there are any boys in the class, returns true if true
+let areSomeStudentsBoys = function(student){
+  return student === "Boy";
+};
+
+//takes in the array4 elements and checks to see if any of them are === "Boy", If true function returns true without checking the rest of the values
+function some(array, callback) {
+  for (let i=0; i < array.length; i++){
+    let element = array[i];
+    console.log(element);
+    if (callback(element) === true){
+      return true;
+    }
+  }
+  return false;
 }
+
+let someCheck = some(array4, areSomeStudentsBoys);
+
+console.log(someCheck);
+
+//forth random array of students by gender
+let array5 = ["Boy", "Boy", "Girl", "Boy", "Boy", "Boy"];
+
+//function to check if there are any boys in the class, returns true if true
+let areAllStudentsBoys = function(student){
+  return student != "Boy"
+};
+
+//takes in the array5 elements and checks to see if all of them are === "Boy", returns true if all are boys, pops out false if it hits any girls
+function every(array, callback) {
+  for (let i=0; i < array.length; i++){
+    let element = array[i];
+    console.log(element);
+    if (callback(element) == true){
+      return false;
+    }
+  }
+  return true;
+}
+
+let everyCheck = every(array5, areAllStudentsBoys);
+
+console.log(everyCheck);
 
 if (typeof describe === 'function') {
 
