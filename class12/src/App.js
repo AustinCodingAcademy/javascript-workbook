5e218b3eae2f270d9bc77d5eb2b9f614189f4292
edvidaurri <edward@vidaurrimail.com>
2017-11-16 09:03:51 -0600

    changed renderPost

--- class12/src/App.js
+++ class12/src/App.js
@@ -8,14 +8,22 @@ class App extends Component {
          {name: 'renee', post: 'I love kitties', id: 1},
          {name: 'stevie', post: 'I love dogs', id: 2},
          {name: 'groot', post: 'I Am Groot', id: 3},
-         {name: 'luke', post: 'blah blah blah', id: 4},
-         {name: 'vader', post: 'I am your father', id: 5},
+        //  {name: 'luke', post: 'blah blah blah', id: 4},
+        //  {name: 'vader', post: 'I am your father', id: 5},
          {name: 'lea', post: 'Pizza', id: 6},
-         {name: 'tamra', post: 'Workout', id: 7},
-         {name: 'vicky', post: 'Coto Insurance', id: 8},
+        //  {name: 'tamra', post: 'Workout', id: 7},
+        //  {name: 'vicky', post: 'Coto Insurance', id: 8},
      ]
 
   };
+ //  handleLikeClick=(e)=>{
+ //    this.setState({timelineData: e.target.value})
+ // };
+ // handleILikeClick=()=>{
+ //    const newState = this.state.timelineData;
+ //    // newState.push({this.state.timelineData});
+ //    this.setState({timelineData: newList, ''})
+ // };
   renderPosts = () => {
     return this.state.timelineData.map((item, key) => {
       return (
