8404d97e90b454bd12f8a210e334fbbcda55acf6
Andrew Thomas <athomas3@trinity.edu>
2017-11-26 18:33:49 -0600

    tictactoereact

--- 07week/ticTacToe/script.js
+++ 07week/ticTacToe/script.js
@@ -4,24 +4,43 @@ class TicTacToe extends React.Component {
   constructor(props) {
     super(props);
   }
+  getInitialState: function() {
+    return{symbol:"x"}
+  }
+  var Symboll = react.createClass({
+    return
+  })
+  var hello = printSymbol: function(){
+    let i = 0
+    if(i=== 0 || i%2===0){
+      console.log(this.symbol)
+      return (<div data-cell="{i}">this.symbol</div>)
+      i++
+    } else{
+      symbol = "o"
+      console.log(this.symbol)
+      return this.symbol
+      i++
+    }
 
+  }
   render() {
     return (
       <div>
         <div className="row">
-          <div data-cell="0"></div>
-          <div data-cell="1"></div>
-          <div data-cell="2"></div>
+          <div onClick={this.printSymbol} data-cell="0">{this.state.symbol}</div>
+          <div onClick={this.printSymbol} data-cell="1">{this.state.symbol}</div>
+          <div onClick={this.printSymbol} data-cell="2">{this.state.symbol}</div>
         </div>
         <div className="row">
-          <div data-cell="3"></div>
-          <div data-cell="4"></div>
-          <div data-cell="5"></div>
+          <div onClick={this.printSymbol} data-cell="3">{this.state.symbol}</div>
+          <div onClick={this.printSymbol} data-cell="4">{this.state.symbol}</div>
+          <div onClick={this.printSymbol} data-cell="5">{this.state.symbol}</div>
         </div>
         <div className="row">
-          <div data-cell="6"></div>
-          <div data-cell="7"></div>
-          <div data-cell="8"></div>
+          <div onClick={this.printSymbol} data-cell="6">{this.state.symbol}</div>
+          <div onClick={this.printSymbol} data-cell="7">{this.state.symbol}</div>
+          <div onClick={this.printSymbol} data-cell="8">{this.state.symbol}</div>
         </div>
       </div>
     );
