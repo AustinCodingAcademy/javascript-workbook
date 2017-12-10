9430d9a0cc5ca424d66a9a9594904416c982838e
wilymon <wilhem@gmail.com>
2017-12-09 18:44:34 -0600

    styled address book

--- 08week/fetch/script.js
+++ 08week/fetch/script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+class ListUsers extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      users: []
+    }
+  }
+
+  componentWillMount(){
+    fetch('https://reqres.in/api/users?page=1').then((response)=>{
+      return response.json();
+    }).then((X)=>{
+      console.log(X);
+      this.setState({
+        users: X.data
+      })
+    })
+  }
+
+  render(){
+    let name1 = this.state.users.map(Contacts);
+
+    function Contacts(user){
+      return <div className="border m-2 rounded" key={user.id}> <span className="m-2">{user.first_name} {user.last_name}</span> <img className="m-4 rounded-circle" key={user.id} src={user.avatar} /></div>;
+    }
+
+    return(
+    <div>
+      {name1}
+    </div>
+    )
+
+  }
+}
+
+ReactDOM.render(<ListUsers />, document.getElementById("fetch"));
