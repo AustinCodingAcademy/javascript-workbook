f3bd84c99e92b062ec98b3216d33f5f848997606
Andrew Thomas <athomas3@trinity.edu>
2017-12-11 18:48:23 -0600

    hw17

--- 09week/hackernews/script.js
+++ 09week/hackernews/script.js
@@ -1,1 +1,43 @@
 'use strict';
+
+'use strict';
+class Hacker extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      item : []
+    }
+  }
+  componentWillMount(){
+    const stories = [];
+    for(let i=1; i < 111; i++){
+      const url = `https://hacker-news.firebaseio.com/v0/item/${i}.json?print=pretty`
+      fetch(url).then((result) => {
+        return result.json();
+      }).then((response) =>{
+        stories.push(response);
+        if(stories.length===100){
+      this.setState({
+        item: stories
+      })
+    }
+    });
+  }
+}
+  render(){
+    console.log(this.state.item)
+
+    var x =
+      this.state.item.map((item, index)=>{
+        return (<list key = {index}><a href ={item.url}>{item.title}</a><br></br></list >)
+      })
+    return(
+
+      <ul>
+        {x}
+      </ul>
+    )
+  }
+}
+ReactDOM.render(<Hacker/>, document.getElementById('hacker-news'));
