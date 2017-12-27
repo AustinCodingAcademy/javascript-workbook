8763de087fedf917bab4abe918db215e68fe3744
gormangit <contact@jongorman.top>
2017-12-26 21:32:57 -0600

    Console.logs everywhere

--- 07week/react-tictactoe/src/App.js
+++ 07week/react-tictactoe/src/App.js
@@ -11,50 +11,65 @@ class App extends Component {
     playerTurn: "X",
     grid: {},
     board: [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8]
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8]
     ],
+    // gameWone; false,
   };
 
 
-
   handleClick(boxes) {
-      if (!this.state.grid[boxes]) {
-        const playerTurn = this.state.playerTurn === 'X' ? 'O' : 'X';
-        const newGrid = {...this.state.grid};
-        this.setState({grid: newGrid, playerTurn});
-        newGrid[boxes] = this.state.playerTurn;
-
-      }
+    console.log('first line of handelClick')
+    if (!this.state.grid[boxes]) {
+      console.log(this.state);
+      const playerTurn = this.state.playerTurn === 'X' ? 'O' : 'X';
+      console.log('after turnary')
+      const newGrid = {...this.state.grid};
+      this.setState({grid: newGrid, playerTurn});
+      newGrid[boxes] = this.state.playerTurn;
+    }
+    // const currentGrid = this.state.grid;
+    // if(this.state.playerTurn === currentGrid[0]) {
+    //   console.log('if statement in handelClick')
+    //   this.setState({gameWone: true})
+    // }
   }
-renderBoard() {
+
+  renderBoard() {
+    console.log('this is renderBoard')
     return this.state.board.map((row, key) => {
       return (
         <div className="row" key={key}>
           {row.map((num, index) => {
-            return <div key={index} className="boardBox" onClick={()=> this.handleClick(num)}>{this.state.grid[num]}</div>
+            return <div key={index} className="boardBox"
+                        onClick={() => this.handleClick(num)}>{this.state.grid[num]}</div>
           })}
         </div>
       )
     })
-}
+  }
 
   render() {
-
+    console.log('render');
     console.log(this.state.grid)
     console.log(this.state.playerTurn)
     let games;
-    games = <h1> It is {this.state.playerTurn}'s turn</h1>
     const currentGrid = this.state.grid;
-    if(this.state.playerTurn === currentGrid[0]) {
+    if (this.state.playerTurn === currentGrid[0]) {
+      console.log('here it is!')
       games = <h1> The winner is: {this.state.playerTurn}</h1>;
       console.log('match')
+    }else {
+      games = <h1> It is {this.state.playerTurn}'s turn</h1>
+
+      console.log('else Statement')
     }
 
-
     return (
+
       <div>
+
         <div className="status">{games}</div>
         {this.renderBoard()}
       </div>
