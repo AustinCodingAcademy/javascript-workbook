88b8803a539f11fb59d2683a33b8079943f24830
Agata Drozdowska <Agata@agatas-mbp.attlocal.net>
2018-01-20 18:52:10 -0600

    Data Types - sum function

---
+++ 01week/datatypes.js
@@ -0,0 +1,7 @@
+'use strict';
+
+//Write a JavaScript program that adds 2 numbers together
+function sumOfTwoNumbers(num1, num2) {
+  return num1 + num2; //return sum of two given numbers
+}
+sumOfTwoNumbers(3, 4);
