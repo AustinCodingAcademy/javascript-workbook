c07fa472c60801a177658d0128cc928c15a102dd
mimitch2 <mike@melodix.com>
2018-01-24 09:54:41 -0600

    first RPS commit

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -8,25 +8,56 @@ const rl = readline.createInterface({
 });
 
 
-function rockPaperScissors(hand1, hand2) {
+// 1. create function that determins if user 1 wins, user 2 wins or it's a tie
+//2. Create fucntion that parses user inputs and formats it correctly
+//2. create a long if/ else/if  test for a tie first, then test for user1 win, ELSE user2 wins
+//3. return the result of the game
 
+// const possibleHands = ['rock', 'paper', 'scissors']
+
+
+
+const rockPaperScissors=(hand1, hand2)=> {
   // Write code here
+  const possibleHands = ["rock", "paper", "scissors"];
 
+  let newHand1 = hand1.toLowerCase().trim();//remove caps and white space
+  let newHand2 = hand2.toLowerCase().trim();//remove caps and white space
+
+  if(possibleHands.includes(newHand1) == true && possibleHands.includes(newHand2) === true) {
+  // if (hand1 === 'rock' || hand1 === 'paper' || hand1 === 'scissors' && //FIXME
+  // hand2 === 'rock' || hand1 === 'paper' || hand1 === 'scissors')
+  // {
+
+    if (newHand1 === newHand2) {
+      return 'It`s a tie!!'
+    } else if (newHand1 === 'rock' && newHand2 === 'scissors' ||
+    newHand1 === 'scissors' && newHand2 === 'paper' ||
+    newHand1 === 'paper' && newHand2 ==='rock'){
+      return 'Hand 1 wins!!'
+    }else{
+      return 'Hand 2 wins!!'
+    }
+
+  }else{
+    return 'Inavlid entry!! Please type rock, paper or scissors.'
+  }
 }
 
 function getPrompt() {
   rl.question('hand1: ', (answer1) => {
     rl.question('hand2: ', (answer2) => {
-      console.log( rockPaperScissors(answer1, answer2) );
+      console.log(rockPaperScissors(answer1, answer2));
       getPrompt();
     });
   });
 }
 
+
+
 // Tests
 
 if (typeof describe === 'function') {
-
   describe('#rockPaperScissors()', () => {
     it('should detect a tie', () => {
       assert.equal(rockPaperScissors('rock', 'rock'), "It's a tie!");
@@ -47,5 +78,39 @@ if (typeof describe === 'function') {
 } else {
 
   getPrompt();
-
 }
+
+//score
+
+
+// const scoreAnalyzer=(score)=> {
+//   if (score >= 80) {
+//     return 'High'
+//   }else if (score < 80 && score >= 60 ){
+//     return 'Average'
+//   }else{
+//     return 'Low'
+//   }
+// }
+
+// scoreAnalyzer(59);
+//
+//
+// //swith
+//
+// const analyzeDataType=(input)=> {
+//   const inputType = typeof input;
+//   switch (inputType) {
+//   case 'number':
+//   case 'boolean':
+//     return 'go ahead'
+//     break;
+//   case 'string':
+//     return false
+//     break;
+//   default:
+//     return 'no'
+//
+//   }
+// }
+// analyzeDataType(false);
