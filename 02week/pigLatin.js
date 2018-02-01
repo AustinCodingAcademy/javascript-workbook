42940f62006a609f783f0b7488f1da76227a9316
Sunnidino <sbriannaparker@gmail.com>
2018-02-01 17:53:35 -0600

    Pig Latin commit

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -10,7 +10,23 @@ const rl = readline.createInterface({
 
 function pigLatin(word) {
 
-  // Your code here
+  //create an array of vowels (a,e,i,o,u)
+  //constanant letter goes to the end
+  //add "ay" to the end.if the word begins with a vowel add "way"
+
+  //function simply ask to translate Pig Latin.  String will store the series of words
+  function translatePigLatin (str) {
+    const vowels = ['a','e','i','o','u'];
+    const result = str.split('');
+
+    if (vowels.includes(str.charAt(0))) {
+      return str += 'way';
+    }
+  }
+
+  console.log (translatePigLatin("apple"));
+
+  //If the word begins with a vowel will return true with the addition of way at the end.
 
 }
 
