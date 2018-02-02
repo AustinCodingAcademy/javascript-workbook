0e5529a8e45702296611788c398703d3e6122772
mimitch2 <mike@melodix.com>
2018-02-02 09:41:39 -0600

    updates based on teacher feedback

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -26,36 +26,38 @@ Scissors beats paper | paper beats rock | rock beats scissors | both hands equal
 
 */
 
+let newHand1
+let newHand2
 
-const isItValid=(hand1, hand2)=> {//check to see if answers are valid before comparing
+const isItValid=(newHand1, newHand2)=> {//check to see if answers are valid before comparing
   const possibleHands = ["rock", "paper", "scissors"];//array of valid inputs
-  const newHand1 = hand1.toLowerCase().trim();//remove caps and white space
-  const newHand2 = hand2.toLowerCase().trim();//remove caps and white space
-
-  if(possibleHands.includes(newHand1) === true && possibleHands.includes(newHand2) === true) {
-    return rockPaperScissors(newHand1, newHand2);//run rockPaperScissors() only if both entries are valid
-  }else{
-    return 'Inavlid entry!! Please type rock, paper or scissors.'//else tell user of invalid entry(ies)
+  if (possibleHands.indexOf(newHand1) !== -1 && possibleHands.indexOf(newHand2) !== -1){
+    return true
   }
 }
 
 
-const rockPaperScissors=(newHand1, newHand2)=> {//this runs ONLY if isItValid() passes
-  if (newHand1 === newHand2) {//first check for a tie
-    return 'It`s a tie!!'//if true, it's a tie, if false move on
+const rockPaperScissors=(hand1, hand2)=> {
+
+  newHand1 = hand1.toLowerCase().trim();//remove caps and white space
+  newHand2 = hand2.toLowerCase().trim();//remove caps and white space
+  if(isItValid(newHand1, newHand2) !==true){
+    return "Inavlid entry!! Please type rock, paper or scissors."
+  } else if (newHand1 === newHand2) {//first check for a tie
+    return 'It`s a tie!'//if true, it's a tie, if false move on
   } else if (newHand1 === 'rock' && newHand2 === 'scissors' ||//if no tie, check if hand1 is a winner
-    newHand1 === 'scissors' && newHand2 === 'paper' ||
-    newHand1 === 'paper' && newHand2 ==='rock'){
-    return 'Hand 1 wins!!'//if true, hand1 wins
+      newHand1 === 'scissors' && newHand2 === 'paper' ||
+      newHand1 === 'paper' && newHand2 ==='rock'){
+    return 'Hand 1 wins!'//if true, hand1 wins
   }else{
-    return 'Hand 2 wins!!'//else hand2 wins (no need to set conditions here)
+    return 'Hand 2 wins!'//else hand2 wins (no need to set conditions here)
   }
 }
 
 const getPrompt=()=> {//change to ES6 syntax
   rl.question('hand1: ', (answer1) => {
     rl.question('hand2: ', (answer2) => {
-      console.log(isItValid(answer1, answer2));//changed this to call the new validation function
+      console.log(rockPaperScissors(answer1, answer2));
       getPrompt();
     });
   });
@@ -63,24 +65,37 @@ const getPrompt=()=> {//change to ES6 syntax
 
 
 
-// Tests
+// Tests  mocha
 
 if (typeof describe === 'function') {
   describe('#rockPaperScissors()', () => {
     it('should detect a tie', () => {
-      assert.equal(rockPaperScissors('rock', 'rock'), "It's a tie!");
-      assert.equal(rockPaperScissors('paper', 'paper'), "It's a tie!");
-      assert.equal(rockPaperScissors('scissors', 'scissors'), "It's a tie!");
+      assert.equal(rockPaperScissors('rock', 'rock'), "It`s a tie!");
+      assert.equal(rockPaperScissors('paper', 'paper'), "It`s a tie!");
+      assert.equal(rockPaperScissors('scissors', 'scissors'), "It`s a tie!");
     });
     it('should detect which hand won', () => {
-      assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
-      assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
+      assert.equal(rockPaperScissors('rock', 'paper'), "Hand 2 wins!");
+      assert.equal(rockPaperScissors('paper', 'scissors'), "Hand 2 wins!");
+      assert.equal(rockPaperScissors('scissors', 'rock'), "Hand 2 wins!");
+      assert.equal(rockPaperScissors('rock', 'scissors'), "Hand 1 wins!");
+      assert.equal(rockPaperScissors('scissors', 'paper'), "Hand 1 wins!");
+      assert.equal(rockPaperScissors('paper', 'rock'), "Hand 1 wins!");
     });
+
+  });
+  describe('#rockPaperScissors()', () => {
     it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
-      assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
-      assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
+      assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand 2 wins!");
+      assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand 2 wins!");
+      assert.equal(rockPaperScissors('rock  ', '    sCiSsOrs'), "Hand 1 wins!");
+    });
+    it('should report invalid entries', () => {
+      assert.equal(rockPaperScissors('blue', 'red'), "Inavlid entry!! Please type rock, paper or scissors.");
+      assert.equal(rockPaperScissors('paper', 'yellow'), "Inavlid entry!! Please type rock, paper or scissors.");
+      assert.equal(rockPaperScissors('"rock"', '12345'), "Inavlid entry!! Please type rock, paper or scissors.");
+      assert.equal(rockPaperScissors('%@$%#', 'rock'), "Inavlid entry!! Please type rock, paper or scissors.");
+      assert.equal(rockPaperScissors('   ', 'pa per'), "Inavlid entry!! Please type rock, paper or scissors.");
     });
   });
 } else {
