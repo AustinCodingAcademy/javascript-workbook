d3482509fc289ab8199e1eaf2a9b7f8b9efb7471
kkannen <kristamkannen@gmail.com>
2018-02-06 20:35:21 -0600

    first commit with a few test cases (not actual tests)

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -19,6 +19,7 @@ function printStacks() {
   console.log("c: " + stacks.c);
 }
 
+
 function movePiece() {
   // Your code here
 
@@ -26,17 +27,20 @@ function movePiece() {
 
 function isLegal() {
   // Your code here
-
+  //if user tried to move from a to b but piece moved is bigger than piece on b, then move is not legal
+  //if piece on stack and if currentPiece > piece on stack, then move is illegal
+  //if stack empty or piece on stack is > than currentPiece, then move is legal
 }
 
 function checkForWin() {
   // Your code here
-
+  //if c: [4, 3, 2, 1] then you win
 }
 
 function towersOfHanoi(startStack, endStack) {
   // Your code here
-
+  //future test: TowersOfHanoi(a,c) should move the top piece from column a, to column c
+  //future test: TowersOfHanoi(b,c) should move the top piece from column b, to column c
 }
 
 function getPrompt() {
@@ -51,4 +55,6 @@ function getPrompt() {
 
 getPrompt();
 
-
+// Write 2 test cases that you will test for
+// For Example:
+// It should be able to move a block, if I ran towersOfHanoi(‘a’, ‘b’), the stacks would look like { a: [4, 3, 2], b: [1], c: [] }
