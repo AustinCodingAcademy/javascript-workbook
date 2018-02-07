6eac6194264025a17278d3f606484136e8f4f842
SavannahMerritt <merritts003424@gmail.com>
2018-02-06 19:06:11 -0600

    First Commit

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const printListOfOlympians = () => {
+  const olympain = ['Athena', 'Apollo', 'Aphrodite', 'Ares', 'Artemis']
+  olympain.forEach((olympian) => {
+    console.log(olympian, 'olympain')
+  })
+}
+
+printListOfOlympians()
