ddd6888562af24cb30259a9acc3c134005699ab2
TODD FREDERKING <toddfrederking@gmail.com>
2018-02-06 20:14:13 -0600

    can you hear me now

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,16 @@
+'use strict'
+
+let Olympians = ['Buce Jenner', 'Mary Lou Retton', 'Nadia', 'Jessie Owens', 'Michael Phelps'];
+
+// array1.forEach(function(element) {
+//   console.log(element);
+// });
+
+const printListOfOlympians = (OlympiansList) => {
+  Olympians.forEach(function(OlympiansList){
+    console.log(OlympiansList);
+  });
+}
+
+printListOfOlympians();
+// testing for correct branch
