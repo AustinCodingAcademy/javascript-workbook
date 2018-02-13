573489c77ee16d92851edaff46753ad1e87cf888
Justin Oswald <justinoswald95@gmail.com>
2018-02-13 17:19:13 -0600

    recreation

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -12,32 +12,75 @@ let stacks = {
   b: [],
   c: []
 };
+let moveCounter = 0
 
-function printStacks() {
+const printStacks = () => {
   console.log("a: " + stacks.a);
   console.log("b: " + stacks.b);
   console.log("c: " + stacks.c);
 }
 
-function movePiece() {
-  // Your code here
-
+const isItLegal = (startStack, endStack) => {
+  if((startStack === "a" || startStack === "b" || startStack === "c") &&
+     (endStack === "a" || endStack === "b" || endStack === "c")){
+    return true
+  }
 }
 
-function isLegal() {
-  // Your code here
-
+const movePiece = (startStack, endStack) => {
+  let blockToBeMoved = stacks[startStack].valueOf()[stacks[startStack].valueOf().length-1]
+  let lastBlockOfFutureStack = stacks[endStack].valueOf()[stacks[endStack].valueOf().length-1]
+  if((stacks[endStack].length === 0) || (blockToBeMoved < lastBlockOfFutureStack)){
+      stacks[endStack].push(stacks[startStack].pop())
+      moveCounter ++
+      return true
+  }
 }
 
-function checkForWin() {
-  // Your code here
-
+const checkMoveCounter = (moveCounter) => {
+  if(moveCounter <= 15){
+    return true
+  }else{
+    return false
+  }
 }
 
-function towersOfHanoi(startStack, endStack) {
-  // Your code here
-
+const compareWinningArrays = (finalArray, winningArray) => {
+  if(finalArray.length == winningArray.length){
+    for(var i = 0 ; i = finalArray.length; i++){
+      if(finalArray[i] === winningArray[i]){
+          return true;
+      }else return false
+    }
+  }
 }
+
+
+const checkForWin = (startStack, endStack) => {
+  let finalArray = stacks[endStack].valueOf()
+  let winningArray = [4, 3, 2, 1]
+  if(compareWinningArrays(finalArray, winningArray)){
+    if(checkMoveCounter(moveCounter)){
+      return true
+    }else{
+      console.log("Almost but you use too many moves, give it another trie!")
+    }
+  }
+}
+
+const towersOfHanoi = (startStack, endStack) => {
+  if(isItLegal(startStack, endStack)){
+    if(movePiece(startStack, endStack)){
+      if(checkForWin(startStack, endStack)){
+        return console.log("Congratulations you win!")
+      }
+      else console.log("Next Move") + printStacks()
+    }
+    else console.log("invalid move")
+  }
+  else console.log("invalid input")
+}
+
 
 function getPrompt() {
   printStacks();
@@ -50,5 +93,4 @@ function getPrompt() {
 }
 
 getPrompt();
-
-
+//commit
