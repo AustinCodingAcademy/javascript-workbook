87bb3cb4831a516a39f9641696818f575b795945
Andrew <32229265+aselee@users.noreply.github.com>
2018-02-14 19:24:14 -0600

    class4Loop - second commit

--- 04week/loop.js
+++ 04week/loop.js
@@ -25,3 +25,14 @@ for (info in persons) {
 // Then use a for...in loop and if state to console.log the value associated with the key birthDate.
 
 console.log(persons.birthDate)
+
+// while loop
+// Use a for loop to console.log the numbers 1 to 1000.
+
+var num = 1;
+for(num=1; num<=1000; num++) {
+  console.log(num)
+}
+
+// do...while loop
+// Use a do...while loop to console.log the numbers from 1 to 1000.
