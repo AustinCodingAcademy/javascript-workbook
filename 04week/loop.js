b17779a9ce3074b38c576f6b52026b9031995934
Andrew <32229265+aselee@users.noreply.github.com>
2018-02-15 16:51:00 -0600

    class4Loop - fourth commit and turning in assignment

--- 04week/loop.js
+++ 04week/loop.js
@@ -44,3 +44,16 @@ do {
   console.log(numWhile);
 
 } while (numWhile<=999);
+
+// When is a for loop better than a while loop? How is the readability of the code affected?
+// A for loop is better when you are generating for that specific number and know the specific number of iterations.
+// While, a while for loop is the opposite, you use it when you do not know the specific number of iterations.
+// So it will keep on looping until a pre-determined scenerio takes place.
+
+// What is the difference between a for loop and a for...in loop?
+// The difference between a for loop and a for...in loop is that a for...in loop loops over enumerable property names of an object.
+// Where a for loop are to iterate a specific number.
+
+// What is the difference between a while loop and a do...while loop?
+// The difference bwetween a while loop and a do...while loop is that a while loop checks the condition first before examining the content.
+// While a do while loop executes the content of that loop once before checking the condition of the while.
