6c31bf486c3b6ec65b5b875ad1cbe9cef60a016b
TODD FREDERKING <toddfrederking@gmail.com>
2018-03-13 19:34:15 -0500

    another

--- 07week/react-tictactoe/src/App.js
+++ 07week/react-tictactoe/src/App.js
@@ -55,7 +55,7 @@ class App extends Component {
 
             {row.map((box,i)=>{
               return <div key = {i} style = {boxStyle} > {row[i]}
-                <button onClick={this.handleclick}></button>
+
 
 
 
