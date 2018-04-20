d3f2ffc1535d0a83e78a3eb5302d5bb1b23245f5
Keith Dybvig <kdybvig@Keiths-MacBook-Pro.local>
2018-04-19 20:19:38 -0500

    fixed trim and case change

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -9,8 +9,10 @@ const rl = readline.createInterface({
 
 
 function rockPaperScissors(hand1, hand2) {
-    hand1.toLowerCase();
-    hand2.toLowerCase();
+    hand1 = hand1.toLowerCase();
+    hand2 = hand2.toLowerCase();
+    hand1 = hand1.trim();
+    hand2 = hand2.trim();
     if (hand1 === hand2) {
       return "It's a tie!"
     } else if ((hand1 === 'rock' && hand2 === 'scissors') || (hand1 === 'scissors' && hand2 === 'paper') || (hand1 === 'paper'
