10c07345c3d7a458d0af746109cf6db3dfecfd4b
roelcantu <roelhcantu@gmail.com>
2018-04-21 20:25:41 -0500

    added last if statement for rock

--- testBranch.js
+++ testBranch.js
@@ -23,3 +23,17 @@ if (hand1 === "scissors") {
        }
    }
 }
+
+// Determine the winner for rock as first hand and add the toLowerCase()
+// and trim() methods to pass the tests.
+if (hand1.toLowerCase().trim() === "rock") {
+  if (hand2.toLowerCase() === "scissors") {
+      return "Hand one wins!";
+  } else {
+      if (hand2.trim() === "paper") {
+        return "Hand two wins!";
+      } else if (hand1 === hand1){
+         return "It's a tie!";
+      }
+  }
+}
