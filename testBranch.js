f56864bdb46c71ffa58de13739b5fb448428dc9e
roelcantu <roelhcantu@gmail.com>
2018-04-21 20:20:53 -0500

    added another if statement

--- testBranch.js
+++ testBranch.js
@@ -10,3 +10,16 @@ if (hand1.toLowerCase() === "paper") {
       }
    }
 }
+
+// Create code for scissors as first hand to execute win, loss, or tie.
+if (hand1 === "scissors") {
+   if (hand2 === "rock") {
+       return "Hand two wins!!!";
+   } else {
+       if (hand2 === "paper") {
+        return "Hand one wins";
+       } else if (hand1 === hand1){
+          return "It's a tie!";
+       }
+   }
+}
