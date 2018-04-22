687141803c602ac5d770fac1abf919689b4569af
roelcantu <roelhcantu@gmail.com>
2018-04-21 19:15:31 -0500

    comments and made changes to the if statements

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -22,34 +22,34 @@ function rockPaperScissors(hand1, hand2) {
           return "It's a tie!";
         }
      }
-   }
+  }
 
-   // Create code for scissors as first hand to execute win, loss, or tie.
-   if (hand1 === "scissors") {
-       if (hand2 === "rock") {
-           return "Hand two wins!!!";
-       } else {
-           if (hand2 === "paper") {
-            return "Hand one wins";
-           } else if (hand1 === hand1){
-              return "It's a tie!";
-           }
-       }
-   }
+  // Create code for scissors as first hand to execute win, loss, or tie.
+  if (hand1 === "scissors") {
+     if (hand2 === "rock") {
+         return "Hand two wins!!!";
+     } else {
+         if (hand2 === "paper") {
+          return "Hand one wins";
+         } else if (hand1 === hand1){
+            return "It's a tie!";
+         }
+     }
+  }
 
-   // Determine the winner for rock as first hand and add the toLowerCase()
-   // and trim() methods to pass the tests.
-   if (hand1.toLowerCase().trim() === "rock") {
-      if (hand2.toLowerCase() === "scissors") {
-          return "Hand one wins!";
-      } else {
-          if (hand2.trim() === "paper") {
-            return "Hand two wins!";
-          } else if (hand1 === hand1){
-             return "It's a tie!";
-          }
-      }
-   }
+  // Determine the winner for rock as first hand and add the toLowerCase()
+  // and trim() methods to pass the tests.
+  if (hand1.toLowerCase().trim() === "rock") {
+    if (hand2.toLowerCase() === "scissors") {
+        return "Hand one wins!";
+    } else {
+        if (hand2.trim() === "paper") {
+          return "Hand two wins!";
+        } else if (hand1 === hand1){
+           return "It's a tie!";
+        }
+    }
+  }
 }
 
 function getPrompt() {
