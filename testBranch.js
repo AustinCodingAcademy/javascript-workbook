da348ecdf2d78511cf6960c0cb155de64a33efc6
roelcantu <roelhcantu@gmail.com>
2018-04-21 20:08:12 -0500

    test

---
+++ testBranch.js
@@ -0,0 +1,12 @@
+'use strict';
+if (hand1.toLowerCase() === "paper") {
+  if (hand2 === "rock") {
+      return "Hand one wins";
+  } else {
+      if (hand2.toLowerCase() === "scissors") {
+          return "Hand two wins!";
+      } else if(hand1 === hand1){
+        return "It's a tie!";
+      }
+   }
+}
