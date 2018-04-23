feacd75ffcc99161102c8dedae522a9943b4b6ae
roelcantu <roelhcantu@gmail.com>
2018-04-22 19:20:29 -0500

    test

--- testBranch.js
+++ testBranch.js
@@ -1,3 +1,6 @@
+'use strict';
+
+// Create function for paper
 if (hand1.toLowerCase() === "paper") {
   if (hand2 === "rock") {
       return "Hand one wins";
@@ -8,4 +11,33 @@ if (hand1.toLowerCase() === "paper") {
         return "It's a tie!";
       }
    }
+}
+
+//test
+
+// Create code for scissors as first hand to execute win, loss, or tie.
+if (hand1 === "scissors") {
+   if (hand2 === "rock") {
+       return "Hand two wins!!!";
+   } else {
+       if (hand2 === "paper") {
+        return "Hand one wins";
+       } else if (hand1 === hand1){
+          return "It's a tie!";
+       }
+   }
+}
+
+// Determine the winner for rock as first hand and add the toLowerCase()
+// and trim() methods to pass the tests.
+if (hand1.toLowerCase().trim() === "rock") {
+  if (hand2.toLowerCase() === "scissors") {
+      return "Hand one wins!";
+  } else {
+      if (hand2.trim() === "paper") {
+        return "Hand two wins!";
+      } else if (hand1 === hand1){
+         return "It's a tie!";
+      }
+  }
 }
