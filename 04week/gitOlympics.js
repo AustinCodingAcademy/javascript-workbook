dce77cfd7dacca4a9948176ed70eb09e1a8e1d07
roelcantu <roelhcantu@gmail.com>
2018-05-03 19:23:48 -0500

    test

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,9 @@
+'use strict'
+
+let arr = ['John', 'Jane', 'Tom', 'Jim', 'Tim'];
+const printListOfOlympics = (arr) => {
+  arr.forEach(function(item, index) {
+    console.log(item, index);
+  })
+}
+printListOfOlympics(arr
