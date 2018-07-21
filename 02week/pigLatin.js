32572b8ea32a06dec782269bcb40dbb62b6ed8fb
cynthiawilliamsa <cynthiawilliamsa@gmail.com>
2018-07-21 12:31:41 -0500

    initial commit

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -1,3 +1,19 @@
+//CODE PLAN//
+//write a function inputValid() to scrub code
+  //trim spaces using trim()
+  //make case consistent using toLowerCase()
+
+//write a function chkForVowelStart()
+  //use conditional to evaluate if input string starts with a,e,i,o,u
+  //if truthy return original string concatinating 'yay' to the end
+
+//write a function makePigLatin()
+  //create variables to hold sliced string from input
+    //assign strStartSlice to index 0 of slice to hold value
+    //assign strEndSlice to slice from index 1(inclusive) to the end of the string (word.length)
+  //return concat of strStartSlice, strEndSlice and 'ay'
+
+
 'use strict';
 
 const assert = require('assert');
