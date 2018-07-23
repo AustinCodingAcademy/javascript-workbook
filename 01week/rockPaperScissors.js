dcbce29f1414339724b1d38080454e4f8cf5469d
Marshall Slemp <marshall.slemp@gmail.com>
2018-07-23 11:42:31 -0500

    add formatHand function

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -7,11 +7,31 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-// Create a function that takes two arguments and returns 1 winner
-// Check that arguments are truthy
-// Remove whitespace from user inputs and convert toLowerCase
-// Create an array that holds ['rock', 'paper', 'scissors']
-// Check that arguments are in the array, indexOf()
+
+
+// formatHand takes one string argument and either returns 1 of 3 possble hands for rock paper scissors or undefined
+// Uses first letter of hand to determine if its rock || paper || scissors
+// Must trim() whitespace and toLowerCase()
+// methods: switch, charAt(), trim(), toLowerCase()
+const formatHand = (hand) => {
+  let setHand;
+  switch(hand.trim().toLowerCase().charAt(0)){
+    case 'r':
+      setHand = 'rock';
+      break;
+    case 'p':
+      setHand = 'paper';
+       break;
+    case 's':
+      setHand = 'scissors';
+      break;
+    default:
+      break;
+  }
+  return setHand;
+}
+
+// Create rockPaperScissors Parent function that takes two string arguments and returns 1 winner
 // if hand1 is the same as hand2,       return 'It's a tie!'
 // if hand1 = rock & hand2 = paper,     return 'Hand two wins!'
 // if hand1 = paper & hand2 = scissors, return 'Hand two wins!'
@@ -19,16 +39,15 @@ const rl = readline.createInterface({
 // if hand2 = rock & hand1 = paper,     return 'Hand one wins!' 
 // if hand2 = paper & hand1 = scissors, return 'Hand one wins!'
 // if hand2 = scissors & hand1 = rock,  return 'Hand one wins!' 
-// methods: if statements, toLowerCase(), trim(), indexOf()
+// methods: if statements
 function rockPaperScissors(hand1, hand2) {
 
   // Write code here
   
   if(hand1 && hand2){
-    const handOne = hand1.trim().toLowerCase();
-    const handTwo = hand2.trim().toLowerCase();
-    const possibleHands = ['rock', 'paper', 'scissors'];
-    if(possibleHands.indexOf(handOne) > -1 && possibleHands.indexOf(handTwo) > -1 ){
+    const handOne = formatHand(hand1);
+    const handTwo = formatHand(hand2);
+    if(handOne && handTwo){
       if(handOne === handTwo){
         return 'It\'s a tie!'
       }
@@ -41,7 +60,7 @@ function rockPaperScissors(hand1, hand2) {
       if(handOne == 'scissors'){
         return handTwo == 'rock' ? 'Hand two wins!' : 'Hand one wins!';
       } 
-    } 
+    }
   } 
   // One or more arguments are invalid
   return 'Invalid input';
