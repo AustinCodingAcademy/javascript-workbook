2eacac07cbbedb5912e29fba26623fbe07ef0442
sarahcheatham <sarah.cheatham14@gmail.com>
2018-07-26 11:16:17 -0500

    input valid function and simple word

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -7,14 +7,54 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+//function pigLatin() - parent function
+//isInputValid() - evaluation function
+//order of nested functions:
+//lowercase and trim
+//vowel loop? for yay
+//complex word loop until you find a vowel 
+//simple word
 
+// const checkForVowel = (word)=>{
+//   const vowel = ['a', 'e', 'i', 'o', 'u'];
+//   for (var i = 0; i < word.length; i++) {
+//     word.indexOf(vowel)
+//   }
+  
+// }
+
+//changes the input word into a pig latin word (simple)
+const changeWord = (word)=>{
+  const wordArr = word.split('');
+  const firstLetter = wordArr.shift();
+  const pushFirstLetter = wordArr.push(firstLetter);
+  const ay = 'ay';
+  const pushAy = wordArr.push(ay);
+  const completeWord = wordArr.join('');
+  return completeWord
+}
+
+//checks to see if the word is not a number
+const isInputValid = (word)=>{
+  return isNaN(word)
+}
+
+//the varible word is whatever string the user inputs
 function pigLatin(word) {
 
-  // Your code here
+  //change word to a lowercase trim word
+  word = word.toLowerCase().trim()
+  
+  //if word is not a number execute this code else return 'please enter a valid word'
+  if(isInputValid(word)) {
+    return changeWord(word)
+  } else {
+    return 'please enter a valid word'
+  }
 
 }
 
-
+//dont do anything to this
 function getPrompt() {
   rl.question('word ', (answer) => {
     console.log( pigLatin(answer) );
