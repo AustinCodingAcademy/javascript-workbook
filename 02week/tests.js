9b432eb3d6168ffcb4bbcc7cec1e29ee795a2b4f
sarahcheatham <sarah.cheatham14@gmail.com>
2018-08-01 17:57:33 -0500

    my tests added

--- 02week/tests.js
+++ 02week/tests.js
@@ -10,90 +10,48 @@ const rl = readline.createInterface({
 
 //Creating an array of choices
 const rpsArray = ['rock', 'paper', 'scissors'];
-const rock = rpsArray[0];
-const paper = rpsArray[1];
-const scissors = rpsArray[2];
 
-//Checking if 
+
+//Checking if the users input is either 'rock' 'paper' or 'scissors' for both hands
 const isInputValid = (hand1, hand2) =>{
 
   const isHandOneValid = hand1.match('rock') || hand1.match('paper') || hand1.match('scissors');
   
   const isHandTwoValid = hand2.match('rock') || hand2.match('paper') || hand2.match('scissors');
 
-  console.log(isHandOneValid)
-  console.log(isHandTwoValid)
+  // console.log(isHandOneValid)
+  // console.log(isHandTwoValid)
 
   if (isHandOneValid !== null && isHandTwoValid !== null) {
     return true
   }
-
-  // if (isHandOneValid && isHandTwoValid === null){
-  //   return 'please enter a valid word'
-  // } 
-
- 
 }
 
+//Checking if its a tie or which hand wins
+const checkForWin = (hand1, hand2) =>{
+  if(hand1 === hand2){
+    return 'It\'s a tie!'
+  }
 
-//function for player typing a word wrong
-//use charAt(0) to grab the first letter of the word
-// const spellCheck = (hand1, hand2) => {
-//   //return the word by the first key
-//   // const words = rpsArray;
-//   // const firstLetterRock = words[0].charCodeAt(0);
-//   // const firstLetterPaper = words[1].charCodeAt(0);
-//   // const firstLetterScissors = words[2].charCodeAt(0);
-//   // const userInput = [firstLetterRock, firstLetterPaper, firstLetterScissors];
-//   // return userInput
-//   //userInput = [114, 112, 115]
-// }
+  if(hand1 === rpsArray[0] && hand2 === rpsArray[2] || hand1 === rpsArray[1] && hand2 === rpsArray[0] || hand1 === rpsArray[2] && hand2 === rpsArray[1]){
+    return 'Hand one wins!'
+  } else {
+    return 'Hand two wins!'
+  }
+}
 
+//parent function
 function rockPaperScissors(hand1, hand2) {
   
   hand1 = hand1.toLowerCase().trim();
   hand2 = hand2.toLowerCase().trim();
 
   if(isInputValid(hand1, hand2)){
-    return true
+    return checkForWin(hand1, hand2)
   } else {
-    return false
-  }
-
-  // f(hand1 === hand2){
-  //   return 'Its a tie!'
-  // }
-
-  // if(hand1 === rpsArray[0] && hand2 === rpsArray[2]){
-  //   return 'Hand one wins!'
-  // }
-
-  // if(hand1 === rpsArray[1] && hand2 === rpsArray[0]) {
-  //   return 'Hand one wins!'
-  // }
-
-  // if(hand1 === rpsArray[2] && hand2 === rpsArray[1]) {
-  //   return 'Hand one wins!'
-  // } else {
-  //   return 'Hand two wins!'
-  // }
-  //if statement for a tie
-  // if (isInputValid(hand1, hand2)){
-  //   if (hand1 === rpsArray[0] && hand2 === rpsArray[2])
-  //     return 'Hand one wins!';
-  //   } 
-
-  //   if (hand1 === rpsArray[1] && hand2 === rpsArray[0]){
-  //     return 'Hand one wins!';
-  //   }
-
-  //   if (hand1 === rpsArray[2] && hand2 === rpsArray[1]){
-  //     return 'Hand one wins!';
-  //   } else {
-  //     return 'Hand two wins!'
-  //   }
-    
-  }
+    return 'please enter a valid word'
+  }  
+}
   
 
 //need an else if statement for all of these 
@@ -137,6 +95,17 @@ if (typeof describe === 'function') {
       assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
       assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
       assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
+    });
+    //my tests
+    it('should detect valid input', () => {
+      assert.equal(rockPaperScissors('rock', 'star'), "please enter a valid word");
+      assert.equal(rockPaperScissors('cat', 'cat'), "please enter a valid word");
+      assert.equal(rockPaperScissors('fish', 'paper'), "please enter a valid word");
+    });
+    it('should detect spelling errors in user input', () => {
+      assert.equal(rockPaperScissors('rok', 'paper'), "please enter a valid word");
+      assert.equal(rockPaperScissors('scisors', 'roc'), "please enter a valid word");
+      assert.equal(rockPaperScissors('rock', 'papar'), "please enter a valid word");
     });
   });
 } else {
