d0c329fe1571599f08a0b9e2c0224efac5ffd332
jwaltrip <jake.waltrip@gmail.com>
2018-08-21 12:45:19 -0500

    cleaned up code, comments, and whiteboard

--- 05week/checkers.js
+++ 05week/checkers.js
@@ -2,60 +2,92 @@
 
 const assert = require('assert');
 const readline = require('readline');
-const colors = require('colors');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-// TODO clean up whiteboard
+
 /* WHITEBOARD
 
 Checkers - 8x8
 
-METHODS:
-  [x] movePiece(begCoord, endCoor)
+class Board - METHODS:
+
+  [x] createGrid()
+    - method that creates an 8x8 array, filled with null values
+
+  [x] viewGrid()
+    - console.logs the grid
+
+  [x] initializeGrid()
+    - this sets the initial pieces of the board
+    - it runs on the beggining of the game
+
+  [x] isValidInput(whichPiece, endPos)
+    - check to make sure the move coordinates entered are inside the board perimeter
+    - checks to make sure that endPos is one of the 32 possible squares that can be occupied
+    - returns true/false
+
+  [x] isMoveLegal(whichPiece, endPos)
+    - makes sure the move entered by user is a legal move
+    - see method definition for detailed algorithm used
+
+  [x] calculateJumpedPiecePos(whichPiece, endPos)
+    - calculates the jumped piece position based on start and end coords
+    - returns a 2 char string of coords
+
+  [x] getMoveDirection(whichPiece, endPos)
+    - get move direction
+    - returns string: 'up-left', 'up-right', 'down-left', 'down-right'
+
+  [x] isValidSingleSpaceMove(whichPiece, endPos)
+    - determines if a move is a single space move
+    - returns true/false
+
+  [x] isDoubleSpaceMove(whichPiece, endPos)
+    - determines if a move is a double space move
+    - returns true/false
+
+  [x] movePiece(whichPiece, endPos)
     - can move forward and backwards from start of game
     - pieces have to move diagonally
-    - takes begining and end coordinates as pararms
-  [x] checkForWin()
-    - check to see if all pieces are gone for a team
-    - keep a counter of all pieces that have been jumped
-  [x] isMoveLegal(whichPiece, endPos)
-    - is the end position not occupied
-  [x] isValidInput(whichPiece, endPos)
-    - check to make sure the move coordinates entered are inside the board
-    - checks to make sure that endPos is one of the 32 possible squares that can be occupied
-  [x] killPiece()
-    - if an opponent piece is jumped, remove it from the board
-  [] resetGame()
-    - reset gloabl vars: board, playerTurn
-  [x] initializeBoard()
-    - initially setup the board
-  [] switchPlayer()
-    - will toggle current active player
-    - refer to tic tac toe app to see how this was implemented
 
+  [x] switchPlayer()
+    - this function toggles this.playerTurn
+
+  [x] isStartPieceValid(whichPiece)
+    - checks to make sure that piece being moved is valid based on this.playerTurn
+    - returns true/false
+
+  [x] checkForWin()
+    - iterates over this.checkers and checks to see if a player won
+    - returns true/false
  */
 
 function Checker(board, whichPiece, endPos) {
-  console.log("\nis valid input? ", board.isValidInput(whichPiece, endPos));
-  console.log(`Moving piece from ${whichPiece} to ${endPos}`);
-  console.log(`Is valid move? ${board.isMoveLegal(whichPiece, endPos)}`);
-  board.movePiece(whichPiece, endPos);
-  if ( board.checkForWin() ) {
-    console.log(`==============================`);
-    console.log(`PLAYER:  ${board.checkers[0]}  WINS!!!`);
-    console.log(`==============================`);
-    // TODO reset board
-  }
+  // console.log("\nis valid input? ", board.isValidInput(whichPiece, endPos));
+  // console.log(`Moving piece from ${whichPiece} to ${endPos}`);
+  // console.log(`Is valid move? ${board.isMoveLegal(whichPiece, endPos)}`);
 
+  // check for valid input
+  if ( board.isValidInput(whichPiece, endPos) ) {
+    // move piece
+    board.movePiece(whichPiece, endPos);
+    if ( board.checkForWin() ) {
+      console.log(`=======================`);
+      console.log(`PLAYER:  ${board.checkers[0]}  WINS!!!`);
+      console.log(`=======================`);
+      // exit the game
+      process.exit();
+    }
+  }
 }
 
 class Board {
   constructor() {
     this.grid = [];
-    this.checkers = [];
+    this.checkers = []; // array of pieces remaining on the board
     this.redPiece = 'R';
     this.blackPiece = 'B';
     this.playerTurn = this.blackPiece; // first players turn is black piece
@@ -72,6 +104,7 @@ class Board {
     }
   }
 
+  // console.logs the grid
   viewGrid() {
     // add our column numbers
     let string = "  0 1 2 3 4 5 6 7\n";
@@ -98,7 +131,6 @@ class Board {
     console.log(string);
   }
 
-
   // this sets the initial pieces of the board
   // it runs on the beggining of the game
   initializeGrid() {
@@ -108,11 +140,16 @@ class Board {
       // loop over cols of first 3 rows
       for (let col1=0; col1<8; col1++) {
         // populate first 3 rows of board
+        // if col is odd && row is even, then populate space
         if (col1 % 2 === 1 && row1 % 2 === 0) {
           this.grid[row1][col1] = this.redPiece;
+          // push piece to active checkers array
           this.checkers.push(this.redPiece);
-        } else if (col1 % 2 === 0 && row1 % 2 === 1) {
+        }
+        // if col is even && row is odd, then populate space
+        else if (col1 % 2 === 0 && row1 % 2 === 1) {
           this.grid[row1][col1] = this.redPiece;
+          // push piece to active checkers array
           this.checkers.push(this.redPiece);
         }
       }
@@ -124,11 +161,16 @@ class Board {
       // loop over cols of rows 5-7
       for (let col2=0; col2<8; col2++) {
         // populate rows 5-7 of board
+        // if col is even && row is odd, then populate space
         if(col2 % 2 === 0 && row2 % 2 === 1) {
           this.grid[row2][col2] = this.blackPiece;
+          // push piece to active checkers array
           this.checkers.push(this.blackPiece);
-        } else if (col2 % 2 === 1 && row2 % 2 === 0) {
+        }
+        // if col is odd && row is even, then populate space
+        else if (col2 % 2 === 1 && row2 % 2 === 0) {
           this.grid[row2][col2] = this.blackPiece;
+          // push piece to active checkers array
           this.checkers.push(this.blackPiece);
         }
       }
@@ -139,13 +181,16 @@ class Board {
   // checks to make sure that endPos is one of the 32 possible squares that can be occupied
   // returns true/false
   isValidInput(whichPiece, endPos) {
+    // valid row and col values
     const validRows = ['0', '1', '2', '3', '4', '5', '6', '7'];
     const validColsEven = ['0', '2', '4', '6'];
     const validColsOdd = ['1', '3', '5', '7'];
 
+    // split piece positions into arrays
     const pieceArr = whichPiece.split('');
     const endArr = endPos.split('');
 
+    // return values
     let isWhichPieceValid = false;
     let isEndPosValid = false;
 
@@ -172,7 +217,6 @@ class Board {
           isEndPosValid = true;
         }
       }
-
     }
 
     return isWhichPieceValid && isEndPosValid;
@@ -183,9 +227,9 @@ class Board {
   /*
     LEGAL MOVE REQ'S:
    - endPos must be empty (unoccupied)
-   - piece can move diagonally in any direction
+   - piece can move diagonally in any direction (from the start)
    - piece can move diagonally 1 space if endPos is empty
-   - piece can move diagonally 2 spaces if endPos is empty and space inbetween is an opponent piece
+   - piece can move diagonally 2 spaces if endPos is empty AND space inbetween is an opponent piece
 
     ALGORITHM TO CHECK FOR LEGALLY MOVING PIECES DIAGONALLY
    1. check to make sure endPos is empty
@@ -211,20 +255,23 @@ class Board {
 
    */
   isMoveLegal(whichPiece, endPos) {
+    // split pos coords into arrays
     const startArr = whichPiece.split('');
     const endArr = endPos.split('');
 
+    // get row/col numbers from arrays
     const startRow = Number(startArr[0]);
     const startCol = Number(startArr[1]);
     const endRow = Number(endArr[0]);
     const endCol = Number(endArr[1]);
 
+    // return value
     let isLegalMove = false;
 
     // check to make sure endPos is empty
     if ( !this.grid[endRow][endCol] ) {
-      // check if move is a single space move
-      if ( this.isSingleSpaceMove(whichPiece, endPos) ) {
+      // check if move is a valid diag single space move
+      if ( this.isValidSingleSpaceMove(whichPiece, endPos) ) {
         isLegalMove = true;
       }
       // check if move is a double space move
@@ -257,12 +304,16 @@ class Board {
   // returns a 2 char string of coords
   calculateJumpedPiecePos(whichPiece, endPos) {
     // get direction of the jumped piece
+    // will be a string of: 'up-left', 'up-right', 'down-left', 'down-right'
     const moveDir = this.getMoveDirection(whichPiece, endPos);
 
+    // holds numerical 2 digit coord of jumped piece
     let jumpedPiece = null;
+    // numerical 2 digit coords of piece being moved
     const startPiece = Number(whichPiece);
 
     // calculate coords of piece being jumped
+    // refer to algorithm outlined in isLegalMove() method
     if (moveDir === 'up-left') {
       jumpedPiece = startPiece - 11;
     } else if (moveDir === 'up-right') {
@@ -280,9 +331,11 @@ class Board {
   // get move direction
   // returns string: 'up-left', 'up-right', 'down-left', 'down-right'
   getMoveDirection(whichPiece, endPos) {
+    // get numerical values of start and end coords
     const start = Number(whichPiece);
     const end = Number(endPos);
 
+    // refer to algorithm outlined in isLegalMove() method
     if ((end - start === -22) || (end - start === -11)) return 'up-left';
     else if ((end - start === -18) || (end - start === -9)) return 'up-right';
     else if ((end - start === 18) || (end - start === 9)) return 'down-left';
@@ -291,19 +344,25 @@ class Board {
 
   // determines if a move is a single space move
   // returns true/false
-  isSingleSpaceMove(whichPiece, endPos) {
+  isValidSingleSpaceMove(whichPiece, endPos) {
+    // get numerical values of start and end coords
     const start = Number(whichPiece);
     const end = Number(endPos);
 
+    // returns true if piece is moved diagonally 1 space in any direction
+    // refer to algorithm outlined in isLegalMove() method
     return ((end - start === -11) || (end - start === 11) || (end - start === -9) || (end - start === 9));
   }
 
   // determines if a move is a double space move
   // returns true/false
   isDoubleSpaceMove(whichPiece, endPos) {
+    // get numerical values of start and end coords
     const start = Number(whichPiece);
     const end = Number(endPos);
 
+    // returns true if piece is moved diagonally 2 spaces in any direction
+    // refer to algorithm outlined in isLegalMove() method
     return ((end - start === -22) || (end - start === 22) || (end - start === -18) || (end - start === 18));
   }
 
@@ -315,18 +374,21 @@ class Board {
     if ( this.isMoveLegal(whichPiece, endPos) ) {
       // check to see if start piece is valid based on playerTurn
       if ( this.isStartPieceValid(whichPiece) ) {
+        // split string coords into array
         const startArr = whichPiece.split('');
         const endArr = endPos.split('');
 
+        // get numerical row/col values of coords
         const startRow = Number(startArr[0]);
         const startCol = Number(startArr[1]);
         const endRow = Number(endArr[0]);
         const endCol = Number(endArr[1]);
 
+        // get color of piece being moved
         const movingPieceColor = this.grid[startRow][startCol];
 
         // check to see if a piece was jumped
-        if (this.isDoubleSpaceMove(whichPiece, endPos) ) {
+        if ( this.isDoubleSpaceMove(whichPiece, endPos) ) {
           // get jumped piece coords
           const jumpedPiece = this.calculateJumpedPiecePos(whichPiece, endPos);
           const jumpedArr = jumpedPiece.split('');
@@ -357,27 +419,7 @@ class Board {
     }
   }
 
-  // iterates over this.checkers and checks to see if a player won
-  // returns true/false
-  checkForWin() {
-    let countRed = 0;
-    let countBlack = 0;
-
-    this.checkers.forEach(checker => {
-      // if find a red piece, add it to count
-      if (checker === this.redPiece) {
-        countRed += 1;
-      } else if (checker === this.blackPiece) {
-        countBlack += 1;
-      }
-    });
-
-    // if either red or black count = 0, then game over
-    return ((countRed === 0) || (countBlack === 0));
-  }
-
   // this function toggles this.playerTurn
-  // does not return a value
   switchPlayer() {
     return (this.playerTurn === this.blackPiece) ? this.playerTurn = this.redPiece : this.playerTurn = this.blackPiece;
   }
@@ -385,19 +427,39 @@ class Board {
   // checks to make sure that piece being moved is valid based on this.playerTurn
   // returns true/false
   isStartPieceValid(whichPiece) {
+    // get numerical coords of start piece
     const startArr = whichPiece.split('');
     const startRow = Number(startArr[0]);
     const startCol = Number(startArr[1]);
 
-    // get start piece value
-    const startPiece = this.grid[startRow][startCol];
+    // get start piece color
+    const startPieceColor = this.grid[startRow][startCol];
 
     // if redpiece turn and startpiece = red, then TRUE || blackpiece turn and startpiece = black, then TRUE
-    return ((startPiece === this.redPiece) && (this.playerTurn === this.redPiece)) || ((startPiece === this.blackPiece) && (this.playerTurn === this.blackPiece));
+    return ((startPieceColor === this.redPiece) && (this.playerTurn === this.redPiece)) || ((startPieceColor === this.blackPiece) && (this.playerTurn === this.blackPiece));
   }
 
-  // TODO resetGame fn after a win
+  // iterates over this.checkers and checks to see if a player won
+  // returns true/false
+  checkForWin() {
+    // holds a count of each red and black piece in play
+    let countRed = 0;
+    let countBlack = 0;
 
+    this.checkers.forEach(checker => {
+      // if find a red piece, add it to count
+      if (checker === this.redPiece) {
+        countRed += 1;
+      }
+      // if find a black piece, add it to count
+      else if (checker === this.blackPiece) {
+        countBlack += 1;
+      }
+    });
+
+    // if either red or black count = 0, then game over
+    return ((countRed === 0) || (countBlack === 0));
+  }
 }
 
 class Game {
@@ -411,11 +473,13 @@ class Game {
   }
 
   moveChecker(whichPiece, endPos) {
+    // call Checker function at the top of code
     Checker(this.board, whichPiece, endPos);
   }
 }
 
 function getPrompt() {
+  // display separator and current player turn
   console.log("-----------------------");
   console.log(`Player Turn: ${game.board.playerTurn}\n`);
   game.board.viewGrid();
@@ -432,6 +496,7 @@ game.start();
 
 
 // Tests
+// TODO write more tests
 if (typeof describe === 'function') {
   describe('Game', () => {
     it('should have a board', () => {
