6de6ce069dea9677f9b354675a09a64c8f2741dc
Christopher Willis <willis.christopher.a@gmail.com>
2018-10-11 01:54:15 -0500

    comments and readability

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -4,6 +4,12 @@ const currentTime = ()=>{
   const theDate = new Date();
   const hour = theDate.getHours();
   const mins = theDate.getMinutes();
+  //I wanted to get more use time in with const, though I would have liked to
+  //make these lets in general because I like to manipulate them to add to a string
+  //as I find this makes more flexable code as well as readable...and this turned
+  //out to be the case as the basic way I solved the problem would be better
+  //solved with modifying the hour variable directly in the if statements
+  //with a final outputString hurrah at the end. 
   console.log(hour);
   let outputString;
   if (hour>12){
@@ -30,6 +36,9 @@ const currentTime = ()=>{
 }
 
 const numberToString = (numberConvertString) =>{
+  //after watching your first data type video, I became obsessed with error checking!
+  //all instead of just using the toString, I wanted to catch problems that
+  //might arise with nonsense inputs  
   if(Number.isNaN(numberConvertString)){
     return "Technically not a number, don't listen to Javascript, it lies!";
   }
@@ -40,6 +49,7 @@ const numberToString = (numberConvertString) =>{
 }
 
 const stringToNumber = (stringConvertNumber) => {
+  //Using some good old nested truthyness to test for bad data.   
   if(typeof(stringConvertNumber)=="string"){
     const converted = Number(stringConvertNumber);
     if(converted ){
@@ -53,6 +63,11 @@ const stringToNumber = (stringConvertNumber) => {
 }
 
 const superTypeOf = (tester) =>{
+
+  //type of is already a really flexable primative, but null being an object
+  //and NaN technically being a number means we have to add in some special cases
+  //Tripple vs double equalls nullifies undefined accidently getting a null return value
+  //as both null and undefined are double equal to each other  
   if (tester===null){
     return null;
   }else if (Number.isNaN(tester)){
@@ -65,12 +80,15 @@ const superTypeOf = (tester) =>{
 const sumOfTwoNumbers = (num1, num2) =>{
   const numberConvert1 = Number(num1);
   const numberConvert2 = Number(num2);
-  if(typeof numberConvert1 == "number" && typeof numberConvert1 == "number" && !Number.isNaN(numberConvert1) && !Number.isNaN(numberConvert2)){
-  //yikes, but the logic is just convert the numbers that might be strings to numbers and if the aren numbers and don't turn into NaNs because of he conversion, then go ahead and add them.
+  if(typeof numberConvert1 == "number" && typeof numberConvert1 == "number" &&
+   !Number.isNaN(numberConvert1) && !Number.isNaN(numberConvert2)){
+  //yikes, but the logic is just convert the numbers that might be strings 
+  //to numbers and if the aren't 'numbers' and don't turn into NaNs 
+  //because of the conversion, then go ahead and add them.
     return numberConvert1 + numberConvert2;
 
   }
-  return "Not valid number or string numbers"
+  return "Not valid number or string of numbers"
 }
 
 const bothTrue = (bool1 , bool2 ) => {
@@ -88,6 +106,8 @@ const oneTrue = (bool1 , bool2) => {
 }
 
 const noneTrue = (bool1 , bool2) => {
+  //the oneTrue will only be false when both are false, so the not of it
+  //does what we want it to.   
   return !(oneTrue(bool1,bool2));
 }
 
