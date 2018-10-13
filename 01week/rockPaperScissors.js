2b441576121b9b079afe676d5c2ac9885f13b822
melfu <melular@gmail.com>
2018-10-13 12:55:01 -0500

    initial commit of rockpaperscissors game

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -7,12 +7,32 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-
-function rockPaperScissors(hand1, hand2) {
-
-  // Write code here
-
-}
+const rockPaperScissors=(hand1, hand2)=> {
+  if (hand1==hand2) {return "It's a tie!"
+  } else {
+    if (hand1=='rock' && hand2=='paper') {return 'Hand 2 Wins!'
+    } else {
+      if (hand1=='paper' && hand2=='rock') {return 'Hand 1 Wins!'
+    } else {
+      if (hand1=='paper' && hand2=='scissors') {return 'Hand 2 Wins!'
+    } else {
+      if (hand2=='paper' && hand1=='scissors') {return 'Hand 1 Wins!'
+    } else {
+      if (hand2=='rock' && hand1=='scissors') {return 'Hand 2 Wins!'
+    } else {
+      if (hand2=='scissors' && hand1=='rock') {return 'Hand 1 Wins!'
+    }
+    }}}}}}}
+//user enters move as hand1
+//user enters move as hand2
+//if move = rock, paper, or scissros then move is valid otherwise say enter valid move
+//if hand1 = hand2, it is a tie
+//if hand1 = rock & hand2 = paper, hand2 wins
+//if hand1 = paper & hand2 = rock, hand1 wins
+//if hand1 = paper and hand2 = scissors, hand2 wins
+//if hand2 = paper and hand1 = scissors, hand1 wins
+//if hand2 = rock and hand1 = scissors, hand2 wins
+//if hand2 = scissors and hand1 = rock, hand1 wins
 
 function getPrompt() {
   rl.question('hand1: ', (answer1) => {
