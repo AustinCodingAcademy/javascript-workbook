732681e6cf25e31dfb88b1de01fefd4c30e947db
Carla Wilson <cjwilson31@gmail.com>
2018-10-16 17:39:59 -0500

    corrected wrong answers

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -1,89 +1,78 @@
+
 // Write a JavaScript program to display the current day and time.
-
-const today = new Date(); //gets todays current date & time
-const day = today.getDay(); //gets a day as a number
-const dayOfWeek = ['Sunday', 'Monday','Tuesday','Wednesday', 'Thursday','Friday','Saturday'];  // array to change numbers to days
-const hour = today.getHours(); // gets current hour
-const minute  = today.getMinutes(); // gets current minute
-console.log(`Today is ${dayOfWeek[day]}`)
-console.log(`The time is: ${hour}:${minute}`);
-
-
+const getThatTime =()=> {
+  const today = new Date(); //gets todays current date & time
+  const day = today.getDay(); //gets a day as a number
+  const dayOfWeek = ['Sunday', 'Monday','Tuesday','Wednesday', 'Thursday','Friday','Saturday'];  // array to change numbers to days
+  const hour = today.getHours(); // gets current hour
+  const minute  = today.getMinutes(); // gets current minute
+  console.log(`Today is ${dayOfWeek[day]}`)
+  console.log(`The time is: ${hour}:${minute}`)}
+getThatTime();
+  
+  
 //Write a JavaScript program to convert a number to a string.
- const int = 20;
- const num = int.toString();
- console.log(num);
-
-
+const numtoString = (num) =>{ 
+  const digit = num.toString()
+  console.log(num);}
+numtoString(33);
+  
+  
+  
+  
 //Write a JavaScript program to convert a string to the number.
-var a = parseInt("88.99 is the number",10);
-console.log(a);
-
-
+const stringToNum =()=>{
+  const str = "39 years";
+  const parse = parseInt(str,10);
+  console.log(parse)
+}
+stringToNum();
+  
+  
 //Write a JavaScript program that takes in different datatypes and prints out whether they are a:
-const isBoolean = (val) => {
-   return typeof val
-} 
-console.log(isBoolean(false)) //Boolean
-
-const isNull = (val) => {
-  return typeof val
-   }
-  console.log(isNull(null));//Null
-
-const isundefined= (val) => {
-  return typeof val
-   }
-  console.log(isundefined())//Undefined
-
-const isNum = (val) => {
-  return typeof val
-   }
-  console.log(isNum(1));//Number
-
-const isNotNumber = (val) => {
-  if(isNaN(val)){return NaN}
-   }
-  console.log(isNotNumber(7)); //NaN how to get it to log NaN?
-
-
-const isString = (val) => {
-  return typeof val
-   }
-  console.log(isString("boo"));// string
-
-
+const whatTypes=()=> {
+  console.log(typeof false);
+  console.log(typeof null);
+  console.log(typeof undef);
+  console.log(typeof 55);
+  console.log(typeof "doggo");
+  console.log(typeof NaN)};
+whatTypes();
+  
+  
+  
+  
+  
+  
 //Write a JavaScript program that adds 2 numbers together.
 const addTwo = (one,two)=> {
-return one + two;
+  return one + two;
 }
 console.log(addTwo(1,2));
-
-
+  
+  
 //Write a JavaScript program that runs only when 2 things are true.
- const twoTrue = (num) => {
-  if(num > 1 && num < 100) {
-    return "you chose a number between 1 and 100";
+const twoTrue = (num1,num2) => {
+  if(num1 && num2) {
+    return "two true"
   }
 }
-console.log(twoTrue(99));
-
-
+console.log(twoTrue(6,8));
+  
 //Write a JavaScript program that runs when 1 of 2 things are true.
 const oneTrue = (num1,num2) => {
   if(num1 || num2) {
     return "1 of 2 things are true";
   }
 }
-console.log(oneTrue(1))
-
-
+console.log(oneTrue(1,7))
+  
+  
 //Write a JavaScript program that runs when both things are not true.
 const notTrue = (condition1,condition2)=> {
-  if(condition1===0 && condition2 <1) {
+  if(!condition1 && !condition2  ) {
     return "how";
-  }else {
-    return "it worked";
   }
 }
-console.log(notTrue(0))
+console.log(notTrue(false, NaN))
+  
