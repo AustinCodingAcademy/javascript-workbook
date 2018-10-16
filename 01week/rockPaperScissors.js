b0e2c68903cc14cf1457895fd9b8bd0ff5678600
Carla Wilson <cjwilson31@gmail.com>
2018-10-16 14:23:14 -0500

    completed rockPaperScissors

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -1,5 +1,17 @@
 'use strict';
 
+/*
+hand1 enters rock, paper, or scissors.
+hand2 enters  rock, paper, or scissors.
+if hand1 and hand2 enter same thing, return "its a tie"
+if hand1 enters rock and hand2 enters scissors, hand1 wins.
+if hand1 enters rock and hand2 enters paper, hand2 wins.
+if hand1 enters paper and hand2 enters rock, hand1 wins.
+if hand1 enters paper and hand2 enters scissors, hand2 wins.
+if hand1 enters scissors and hand2 enters paper, hand1 wins.
+if hand1 enters scissors and hand2 enters rock, hand2 wins.
+*/
+
 const assert = require('assert');
 const readline = require('readline');
 const rl = readline.createInterface({
@@ -9,16 +21,43 @@ const rl = readline.createInterface({
 
 
 function rockPaperScissors(hand1, hand2) {
-
   // Write code here
-
+  
+  if (hand1 === 'rock' && hand2 === 'paper') {     
+    return "Hand two wins!"
+  } else if (hand1 === 'rock' && hand2 === 'scissors') {
+    return "Hand one wins!"}
+   
+  if (hand1 === 'paper' && hand2 === 'rock') {
+    return "Hand one wins!"
+  } else if (hand1 === 'paper' && hand2 === 'scissors') {
+    return "Hand two wins!"}
+  
+  if (hand1 === 'scissors' && hand2 === 'rock') {
+    return "Hand two wins!"
+  } else if (hand1 === 'scissors' && hand2 === 'paper') {
+    return "Hand one wins!"}
+   
+  
+  if (hand1 === hand2) {
+    return "It's a tie!"
+  } 
+  
+  if  (hand1 && hand2 != 'rock'||'paper'||'scissors') {
+    return "please enter rock, paper, or scissors"
+  }
 }
+
+
 
 function getPrompt() {
   rl.question('hand1: ', (answer1) => {
     rl.question('hand2: ', (answer2) => {
-      console.log( rockPaperScissors(answer1, answer2) );
+      console.log(rockPaperScissors(answer1.trim().toLowerCase(), answer2.trim().toLowerCase()));
+      
+
       getPrompt();
+
     });
   });
 }
