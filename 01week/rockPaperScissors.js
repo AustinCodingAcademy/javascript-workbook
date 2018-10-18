5ad7a3c748487e760c10a3073dbff41a35ecee26
Jose Sanchez <josesanchez4512@gmail.com>
2018-10-18 17:12:17 -0500

    rockPaperScissors.js

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -6,11 +6,77 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
+//hand1 enters move, hands2 enters move
+//check to see if move is valid
+//if ove is invalid prompt user 
 
 function rockPaperScissors(hand1, hand2) {
 
   // Write code here
+ //Ask the user for a choice rock, paper, or scissor.
+
+
+// playerChoice = prompt("Make a selection: rock, paper, or scissors: ");
+
+// user choice 
+// computer choice
+// compare choice
+// if its a tie
+// compare if rocks,papers, scissors
+var userchoice = prompt('do you choose rock,paper,or scissors?')
+var computerChoice = Math.random()
+
+if (computerChoice < 0.34) {
+  computerChoice = 'paper'
+  
+} else if (computerChoice <= 0.67) {
+  computerChoice = 'scissors'
+  
+} else if (computerChoice <= 1) {
+  computerChoice = 'rock'
+}
+
+  const compare = function(choice1 ,choice2){
+  // if tie
+  if(choice1 === choice2){
+    return  "It is a tie!"
+  }
+  
+  // if user is rock
+  if(choice1 === 'rock') {
+    if(choice2 === 'scissors') {
+      return 'rock wins'
+    } else {
+      return 'paper wins'
+    }
+  }
+  
+  // if user is paper
+  if(choice1 === 'paper') {
+    if(choice2 === 'rock') {
+      return 'paper wins '
+    } else {
+      console.log('here')
+      return 'scissors win'
+    }
+  }
+  
+  // if user is scissors
+  if(choice1 === 'scissors') {
+    if(choice2 === 'paper') {
+      console.log('he')
+      return 'scissors win'
+    } else {
+      return 'rock wins'
+    }
+  }
+}
+
+compare(userchoice,computerChoice)
+
+
+
+
 
 }
 
