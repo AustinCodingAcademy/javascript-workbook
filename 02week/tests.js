5e6717c8ecb39e4490ef23ff76e553342a6fa54e
melfu <melular@gmail.com>
2018-10-19 23:39:15 -0500

    added tests for all hand 1 and hand 2 win possibilities

--- 02week/tests.js
+++ 02week/tests.js
@@ -10,14 +10,22 @@ if (typeof describe === 'function') {
       assert.equal(detect.rockPaperScissors('scissors', 'scissors'), "It's a tie!");
     });
     it('should detect which hand won', () => {
-      assert.equal(detect.rockPaperScissors('rock', 'paper'), "Hand two wins!");
-      assert.equal(detect.rockPaperScissors('paper', 'scissors'), "Hand two wins!");
-      assert.equal(detect.rockPaperScissors('rock', 'scissors'), "Hand one wins!");
+      assert.equal(detect.rockPaperScissors('rock', 'paper'), "Hand two Wins!");
+      assert.equal(detect.rockPaperScissors('paper', 'scissors'), "Hand two Wins!");
+      assert.equal(detect.rockPaperScissors('rock', 'scissors'), "Hand one Wins!");
+      assert.equal(detect.rockPaperScissors('scissors', 'paper'), "Hand one Wins!");
+      assert.equal(detect.rockPaperScissors('paper', 'rock'), "Hand one Wins!");
+      assert.equal(detect.rockPaperScissors('scissors', 'rock'), "Hand two Wins!");
     });
     it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
-      assert.equal(detect.rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
-      assert.equal(detect.rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
-      assert.equal(detect.rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
+      assert.equal(detect.rockPaperScissors('rOcK', ' paper '), "Hand two Wins!");
+      assert.equal(detect.rockPaperScissors('Paper', 'SCISSORS'), "Hand two Wins!");
+      assert.equal(detect.rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one Wins!");
+    });
+    it('should ask for a valid move', () => {
+      assert.equal(detect.rockPaperScissors('dragon', 'rock'), "Enter a valid move.");
+      assert.equal(detect.rockPaperScissors('paper', 'pepper'), "Enter a valid move.");
+      assert.equal(detect.rockPaperScissors('scissors', 'sciss0rs'), "Enter a valid move.");
     });
   });
 } else {
