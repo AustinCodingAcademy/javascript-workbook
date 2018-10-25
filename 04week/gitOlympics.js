a2d04acc63c4f118bc432f2cf26ef7f3383342d1
Christopher Willis <willis.christopher.a@gmail.com>
2018-10-25 18:54:09 -0500

    first commit

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const printListOfOlympians = (arr) =>{
+    array.forEach(element => {
+        console.log(element);
+    });
+}
+
+
+printListOfOlympians(["chris","alen","abby","dan","bj"]);
