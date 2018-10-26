70eafa4431b94fc4f26dd14df09e4b2085e01b61
Bireswar Banerjee <bireswar.infosys@gmail.com>
2018-10-25 19:20:38 -0500

    02WeekPigLatinExec

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -8,9 +8,56 @@ const rl = readline.createInterface({
 });
 
 
-function pigLatin(word) {
+function pigLatin(input) {
+  let inputStr = input.split('');
+  let counter  = 0;
 
-  // Your code here
+  inputStr.forEach(isVowel);
+  function isVowel(item,index){
+    if(index == 0)
+      console.log('Input letters are:');
+    console.log(item);
+  }
+  let index;
+  let newStr1;
+  let newStr2;
+  let vowelFound = false;
+  for(index = 0; index<inputStr.length;index++){
+    switch(inputStr[index]){
+      case 'a':
+      case 'e':
+      case 'i':
+      case 'o':
+      case 'u':
+      case 'A':
+      case 'E':
+      case 'I':
+      case 'O':
+      case 'U':
+           newStr1 = inputStr.slice(index);
+           newStr2 = inputStr.slice(0,index);
+           newStr1=newStr1.join('');
+           newStr2=newStr2.join('');
+           vowelFound = true;
+       break;
+    }
+    if(vowelFound)
+      break;
+  }
+  let returnStr;
+  if(vowelFound){
+    if(index == 0)
+      returnStr = (newStr1.concat(newStr2)).concat('yay');
+    else
+      returnStr = (newStr1.concat(newStr2)).concat('ay');
+    console.log('Input string in pig latin is:',returnStr);
+    return returnStr;
+  }
+  else
+  {
+    console.log('No vowels in the input.Try again');
+    return;
+  }  
 
 }
 
