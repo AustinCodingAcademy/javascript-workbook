6a9118e87f38a442abbf2d5604a3669dad4be2b3
Andy Olson <andyolson09@gmail.com>
2018-11-08 21:02:22 -0600

    all but current day and time done

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -4,11 +4,6 @@ const assert = require("assert");
 
 //____________________________________________________________________________________________________________
 // Write a JavaScript program to display the current day and time.
-function displayCurrentDayAndTime() {
-  let today = new Date();
-  console.log(today);
-}
-displayCurrentDayAndTime();
 
 //____________________________________________________________________________________________________________
 // Write a JavaScript program to convert a number to a string.
@@ -33,6 +28,23 @@ describe("numberToString", () => {
 //____________________________________________________________________________________________________________
 // Write a JavaScript program to convert a string to the number.
 
+// solution
+function stringToNumber(str) {
+  let num = Number(str);
+  return num;
+}
+
+// tests
+describe("stringToNumber", () => {
+  it("should take any string and turn it into a number", () => {
+    assert.equal(typeof stringToNumber("12345"), "number");
+  });
+  it("the number returned should be the same as the string inputted", () => {
+    assert.strictEqual(stringToNumber("12345"), 12345);
+    assert.notStrictEqual(stringToNumber("12345"), 54321);
+  });
+});
+
 //____________________________________________________________________________________________________________
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
 // Boolean
@@ -42,14 +54,97 @@ describe("numberToString", () => {
 // NaN
 // String
 
+// solution
+function printDataType(val) {
+  let type = typeof val;
+  return type;
+}
+
+describe("printDataType", () => {
+  it("should return a boolean", () => {
+    assert.equal(printDataType(true), "boolean");
+  });
+});
+
 //____________________________________________________________________________________________________________
 // Write a JavaScript program that adds 2 numbers together.
+
+// solution
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+describe("add", () => {
+  it("the value returned should be a number", () => {
+    assert.equal(typeof add(2, 2), "number");
+  });
+});
 
 //____________________________________________________________________________________________________________
 // Write a JavaScript program that runs only when 2 things are true.
 
+// solution
+function runWhenBothTrue(val1, val2) {
+  if (val1 && val2) return 1;
+  else return 0;
+}
+
+describe("runWhenBothTrue", () => {
+  it("should return 1 or 0 (number)", () => {
+    assert.equal(typeof runWhenBothTrue(true, true), "number");
+  });
+  it("should return 1 if condition is met", () => {
+    assert.equal(runWhenBothTrue(true, true), 1);
+  });
+  it("should return 0 if condition is not met", () => {
+    assert.equal(runWhenBothTrue(false, true), 0);
+    assert.equal(runWhenBothTrue(true, false), 0);
+    assert.equal(runWhenBothTrue(false, false), 0);
+  });
+});
+
 //____________________________________________________________________________________________________________
 // Write a JavaScript program that runs when 1 of 2 things are true.
 
+// solution
+function runWhenAnyTrue(val1, val2) {
+  if (val1 || val2) return 1;
+  else return 0;
+}
+
+describe("runWhenAnyTrue", () => {
+  it("should return 1 or 0 (number)", () => {
+    assert.equal(typeof runWhenAnyTrue(true, false), "number");
+  });
+  it("should return 1 if condition is met", () => {
+    assert.equal(runWhenAnyTrue(false, true), 1);
+    assert.equal(runWhenAnyTrue(true, false), 1);
+    assert.equal(runWhenAnyTrue(true, true), 1);
+  });
+  it("should return 0 if condition is not met", () => {
+    assert.equal(runWhenAnyTrue(false, false), 0);
+  });
+});
+
 //____________________________________________________________________________________________________________
 // Write a JavaScript program that runs when both things are not true.
+
+// solution
+function runWhenBothFalse(val1, val2) {
+  if (val1 === false && val2 === false) return 1;
+  else return 0;
+}
+
+describe("runWhenBothFalse", () => {
+  it("should return 1 or 0 (number)", () => {
+    assert.equal(typeof runWhenBothFalse(false, false), "number");
+  });
+  it("should return 1 if condition is met", () => {
+    assert.equal(runWhenBothFalse(false, false), 1);
+  });
+  it("should return 0 if condition is not met", () => {
+    assert.equal(runWhenBothFalse(false, true), 0);
+    assert.equal(runWhenBothFalse(true, false), 0);
+    assert.equal(runWhenBothFalse(true, true), 0);
+  });
+});
