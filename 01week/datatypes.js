848fba6f3869bf4f1bad992b4d93c71000c28c35
Tosin Awofeso <midastouchproductions@gmail.com>
2018-11-08 20:16:07 -0600

    finish number to string function

---
+++ 01week/datatypes.js
@@ -0,0 +1,55 @@
+"use strict";
+const assert = require("assert");
+
+// Write a JavaScript program to display the current day and time.
+
+// Write a JavaScript program to convert a number to a string.
+
+//Soultion
+function numberToString(anyNumberAtAll) {
+  return anyNumberAtAll.toString();
+}
+
+describe("numberToString", () => {
+  it("should take any number and turn it into a string", () => {
+    assert.equal(typeof numberToString(12345), "string");
+  });
+  it("the string should be the same as the number inputted", () => {
+    assert.strictEqual(numberToString(12345), "12345");
+    assert.notStrictEqual(numberToString(12345), "54321");
+  });
+});
+
+// Write a JavaScript program to convert a string to the number.
+
+function stringToNumber(num) {
+  return num;
+}
+
+describe("stringToNumber", () => {
+  it("should take any string and turn it into a number", () => {
+    assert.equal(typeof numberToString("12345"), "number");
+  });
+});
+
+// Write a JavaScript program that takes in different datatypes and prints out whether they are a:
+
+// Boolean
+
+// Null
+
+// Undefined
+
+// Number
+
+// NaN
+
+// String
+
+// Write a JavaScript program that adds 2 numbers together.
+
+// Write a JavaScript program that runs only when 2 things are true.
+
+// Write a JavaScript program that runs when 1 of 2 things are true.
+
+// Write a JavaScript program that runs when both things are not true.
