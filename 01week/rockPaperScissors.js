45e517d0405fa41776209cf0a2411c078e46abc7
Andy Olson <andyolson09@gmail.com>
2018-11-13 20:29:21 -0600

    added string variables, and cleaned up logic

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -7,41 +7,78 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+// game elements
+const rock = "rock";
+const paper = "paper";
+const scissors = "scissors";
+
+// win messages
+const result_error = "Invalid input.";
+const result_tie = "It's a tie!";
+const result_hand1Win = "Hand one wins!";
+const result_hand2Win = "Hand two wins!";
+
+/**
+ * Function: rockPaperScissors(hand1, hand2)
+ * Description: takes in two values and calls determineWinner() if we like the values, otherwise return error message
+ */
 function rockPaperScissors(hand1, hand2) {
   // Write code here
 
   // ensure valid input
   if (hand1 && hand2) {
-    console.log("received input of: ", hand1, hand2);
-
     // ensure we have strings
     if (typeof hand1 === "string" && typeof hand2 === "string") {
       // clean up input strings
       hand1 = scrubADubDub(hand1);
       hand2 = scrubADubDub(hand2);
-    } else return "Invalid input.";
 
-    console.log("comparing", hand1, "to", hand2);
+      //ensure hand1 and hand2 are specifically "rock", "paper", or "scissors"
+      if (
+        (hand1 === rock || hand1 === paper || hand1 === scissors) &&
+        (hand2 === rock || hand2 === paper || hand2 === scissors)
+      ) {
+        let winner = determineWinner(hand1, hand2);
+        return winner;
+      } else return result_error;
+    } else return result_error;
+  } else return result_error;
+}
 
-    // tie condition
-    if (hand1 === hand2) return "It's a tie!";
+/**
+ * Function: determineWinner(hand1, hand2)
+ * Description: takes two strings of "rock", "paper", or "scissors" and applies logic to determine the result
+ */
+function determineWinner(hand1, hand2) {
+  // tie condition
+  if (hand1 === hand2) return result_tie;
 
-    // rock vs x
-    if (hand1 === "rock") {
-      if (hand2 === "paper") return "Hand two wins!";
-      if (hand2 === "scissors") return "Hand one wins!";
+  switch (hand1) {
+    case rock: {
+      if (hand2 === scissors) return result_hand1Win;
+      else return result_hand2Win;
     }
-    // paper vs x
-    else if (hand1 === "paper") {
-      if (hand2 === "rock") return "Hand one wins!";
-      if (hand2 === "scissors") return "Hand two wins!";
+
+    case paper: {
+      if (hand2 === rock) return result_hand1Win;
+      else return result_hand2Win;
     }
-    // scissors vs x
-    else if (hand1 === "scissors") {
-      if (hand2 === "rock") return "Hand two wins!";
-      if (hand2 === "paper") return "Hand one wins!";
-    } else return "Invalid input.";
-  } else return "Invalid input.";
+
+    case scissors: {
+      if (hand2 === paper) return result_hand1Win;
+      else return result_hand2Win;
+    }
+  }
+}
+
+/**
+ * Function: scrubADubDub(str)
+ * Description: applies string methods to ensure the returned string is all lowercase and contains no whitespace
+ */
+function scrubADubDub(str) {
+  str = str.toLowerCase();
+  str = str.trim();
+  return str;
 }
 
 function getPrompt() {
@@ -51,12 +88,6 @@ function getPrompt() {
       getPrompt();
     });
   });
-}
-
-function scrubADubDub(str) {
-  str = str.toLowerCase();
-  str = str.trim();
-  return str;
 }
 
 // Tests
