44ef70a52e7c21843d99895c3e2b2b6744791251
Andy Olson <andyolson09@gmail.com>
2018-11-15 20:58:14 -0600

    applied code

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -1,45 +1,69 @@
 "use strict";
 
+// tests and cl stuff
 const assert = require("assert");
 const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
+// given variables
 const vowels = ["a", "e", "i", "o", "u"];
 const result_InputError = "Invalid Input.";
 
 function pigLatin(word) {
   // Your code here
-  // + should scrub word before searching for vowel (copy pasta function from rockPaperScissors.js)
+  //ensure valid input
+  if (word) {
+    // + should scrub word before searching for vowel (copy pasta function from rockPaperScissors.js)
+    word = scrubADubDub(word);
 
+    // + handle word that begins with a vowel (attach yay to end of word)
+    if (isVowel(word[0])) {
+      word += "yay";
+      return word;
+    }
 
-  // + find a way to determine if a letter is a vowel (make a function)
-  // + find a way to determine the location(index) of first vowel in word
+    // get the index of the first vowel in word
+    let firstVowelIndex = findVowel(word);
 
-  // + handle word that begins with a vowel (attach yay to end of word)
+    // + find a way to move any and all letters before vowel to end of word ( substring() ), then add "ay" to end
+    let lettersToMove = word.substring(0, firstVowelIndex);
+    word = word.substring(firstVowelIndex) + lettersToMove + "ay";
 
-  
-  // + find a way to move any and all letters before vowel to end of word ( substring() )
-
-
+    // return word
+    return word;
+  } else return result_InputError;
 }
 
-function getPrompt() {
-  rl.question("word ", answer => {
-    console.log(pigLatin(answer));
-    getPrompt();
-  });
-}
-
+// + find a way to determine if a letter is a vowel (make a function)
 /**
  * Function: isVowel(char)
  * Description: checks if given char matches any index of vowels array
  */
 function isVowel(char) {
   // traverse vowels array
-  // if char equals one of the vowels, return true
-  // else, return false
+  for (var i = 0; i < vowels.length; i++) {
+    // if char equals one of the vowels, return true
+    if (char === vowels[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// + find a way to determine the location(index) of first vowel in word (make a function)
+/**
+ * Function: findVowel(word)
+ * Description: traverses through word and returns index of first vowel
+ */
+function findVowel(word) {
+  for (var i = 0; i < word.length; i++) {
+    if (isVowel(word[i])) {
+      return i;
+    }
+  }
 }
 
 /**
@@ -50,6 +74,17 @@ function scrubADubDub(str) {
   return str.toLowerCase().trim();
 }
 
+/**
+ * Stuff we started with
+ * =================================================================
+ */
+
+function getPrompt() {
+  rl.question("word ", answer => {
+    console.log(pigLatin(answer));
+    getPrompt();
+  });
+}
 // Tests
 
 if (typeof describe === "function") {
