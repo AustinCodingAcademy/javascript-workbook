48d107b9a192e4872b77964386fb5d81908cd619
Ronald Hajewski <ronald.hajewski@gmail.com>
2018-11-20 15:45:58 -0600

    a few more tweaks to piglatin translator, removed some unecessary statements

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -6,29 +6,32 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+// Take the first letters of the word up to the first vowel, move them to the back, and add 'ay' to the end of it or 'yay' if word starts with vowel
 
-function pigLatin(word) {
-  // Your code here
-  // Take the first letters of the word up to the first vowel, move them to the back, and add 'ay' to the end of it
-  // split up the string so we can do something with it
-  const wordArray = word.split("");
-
+function firstVowel(word) {
   // find the first vowel
-  const vowel = [a, e, i, o, u];
-  for (let i = 0; i < wordArray.length; i++) {
-    if (wordArray[i] === vowel[i]) {
-      return "success";
+  for (let i = 0; i < word.length; i++) {
+    // loop through the word until you find 'if' (which are vowels)
+    const vowel = ["a", "e", "i", "o", "u"];
+    if (vowel.indexOf(word[i]) != -1) {
+      return i; // return the index of the first vowel and use firstVowel() with pigLatin function
     }
   }
-  // define all the vowels
+  // return -1;
+}
 
-  // iterate through the word till we find a vowel
-
-  // take everything before that vowel
-
-  // move it to the end
-
-  // add ay
+function pigLatin(word) {
+  word = word.toLowerCase().trim(); // convert word to lowercase
+  const firstLetter = firstVowel(word);
+  if (firstLetter > 0) {
+    // if the word doesn't start with a vowel slice it at the first vowel and attach to the end plus 'ay'
+    return word.slice(firstLetter) + word.slice(0, firstLetter) + "ay"; //
+  } else if (firstLetter === 0) {
+    // if the word starts with a vowel just add 'yay' to the end
+    return word + "yay";
+  } else {
+    console.log("you need to enter a vowel");
+  }
 }
 
 function getPrompt() {
@@ -62,3 +65,33 @@ if (typeof describe === "function") {
 } else {
   getPrompt();
 }
+
+// function pigLatin(word) {
+//   // Your code here
+//   // Take the first letters of the word up to the first vowel, move them to the back, and add 'ay' to the end of it
+
+//   // split up the string so we can do something with it
+//   const wordArray = word.split("");
+
+//   // find the first vowel
+//   let vowel = [a, e, i, o, u];
+
+//   // scan through the letters of the word inputted and stop when it gets to the first vowel
+//   for (let i = 0; i < wordArray.length; i++) {
+//     for (let i = 0; i < vowel.length; i++) {}
+//     if (wordArray[i] === vowel[i]) {
+//       const pLProgram = "success";
+//       return pLProgram;
+//     }
+//   }
+
+//   // define all the vowels
+
+//   // iterate through the word till we find a vowel
+
+//   // take everything before that vowel
+
+//   // move it to the end
+
+//   // add ay
+// }
