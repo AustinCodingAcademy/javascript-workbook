2397af899599ee8915ee4f215201984d74d08a51
Andy Olson <andyolson09@gmail.com>
2018-11-20 19:30:27 -0600

    rewrote tests

--- 02week/tests.js
+++ 02week/tests.js
@@ -25,13 +25,8 @@ const result_hand2Win = "Hand two wins!";
 function rockPaperScissors(hand1, hand2) {
   // Write code here
 
-  // ensure valid input
-  if (
-    hand1 &&
-    hand2 &&
-    typeof hand1 === "string" &&
-    typeof hand2 === "string"
-  ) {
+  // ensure input exists
+  if (hand1 && hand2) {
     // clean up input strings
     hand1 = scrubADubDub(hand1);
     hand2 = scrubADubDub(hand2);
@@ -41,8 +36,7 @@ function rockPaperScissors(hand1, hand2) {
       (hand1 === rock || hand1 === paper || hand1 === scissors) &&
       (hand2 === rock || hand2 === paper || hand2 === scissors)
     ) {
-      let winner = determineWinner(hand1, hand2);
-      return winner;
+      return determineWinner(hand1, hand2);
     } else return result_error;
   } else return result_error;
 }
@@ -56,17 +50,17 @@ function determineWinner(hand1, hand2) {
   if (hand1 === hand2) return result_tie;
 
   switch (hand1) {
-    case rock:
-      if (hand2 === scissors) return result_hand1Win;
-      else return result_hand2Win;
+  case rock:
+    if (hand2 === scissors) return result_hand1Win;
+    else return result_hand2Win;
 
-    case paper:
-      if (hand2 === rock) return result_hand1Win;
-      else return result_hand2Win;
+  case paper:
+    if (hand2 === rock) return result_hand1Win;
+    else return result_hand2Win;
 
-    case scissors:
-      if (hand2 === paper) return result_hand1Win;
-      else return result_hand2Win;
+  case scissors:
+    if (hand2 === paper) return result_hand1Win;
+    else return result_hand2Win;
   }
 }
 
@@ -91,6 +85,30 @@ function getPrompt() {
 
 if (typeof describe === "function") {
   // write tests here
+  describe("#rockPaperScissors()", () => {
+    it("should detect a tie", () => {
+      assert.equal(rockPaperScissors("rock", "rock"), result_tie);
+      assert.equal(rockPaperScissors("paper", "paper"), result_tie);
+    });
+    it("should detect when each hand wins", () => {
+      assert.equal(rockPaperScissors("rock", "scissors"), result_hand1Win);
+      assert.equal(rockPaperScissors("scissors", "paper"), result_hand1Win);
+      assert.equal(rockPaperScissors("rock", "paper"), result_hand2Win);
+      assert.equal(rockPaperScissors("scissors", "rock"), result_hand2Win);
+    });
+    it("should handle whitespace and capitilized letters", () => {
+      assert.equal(rockPaperScissors(" SCIssorS", "paPer "), result_hand1Win);
+      assert.equal(rockPaperScissors("PAPER", "SCISSORS"), result_hand2Win);
+      assert.equal(rockPaperScissors("paper", "rOck      "), result_hand1Win);
+    });
+    it("should handle if the user inputs something that is not 'rock', 'paper', or 'scissors'", () => {
+      assert.equal(rockPaperScissors("NotRock", "NotScissors"), result_error);
+      assert.equal(rockPaperScissors("rawk", "papper"), result_error);
+      assert.equal(rockPaperScissors(undefined, 123), result_error);
+      assert.equal(rockPaperScissors("rock", ""), result_error);
+      assert.equal(rockPaperScissors(".paper", "rock_"), result_error);
+    });
+  });
 } else {
   getPrompt();
 }
