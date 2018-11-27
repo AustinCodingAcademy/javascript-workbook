1805797560e6cd312b46a98b46dde6099050d24a
Taylor Colten <taylorwcolten@gmail.com>
2018-11-27 12:58:02 -0600

    scissors tie only rock paper scissors

--- 02week/tests.js
+++ 02week/tests.js
@@ -2,8 +2,20 @@
 if (typeof describe === 'function') {
 
     describe('#rockPaperScissors()', () => {
+     //should not accept any word other than paper rock scissors
+     it ('should only accept "rock" "paper" "scissor"',() => {
+         assert.equal(rockPaperScissors('') , 'invald entry!' )
      
-    });
+    
+    describe('#rockpaperScissors()', () => {
+        it ('should return tie on "scissors" "scissors"',() => {
+            assert.equal(rockPaperScissors("scissors", "scissors") ,'tie!')
+        })
+    })
+     })
+    
+    
+    })
 } else {
 
     getPrompt();
