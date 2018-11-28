1b68c14cf16c2adb9f3fb5a7dabe90f8f8a91fe5
Bireswar Banerjee <bireswar.infosys@gmail.com>
2018-11-27 20:13:38 -0600

    BireswarBanerjee

---
+++ 01week/rockPaperScissors.test.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const assert = require('assert');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+function rockPaperScissors(hand1, hand2) {
+
+  // Write code here
+
+}
+
+function getPrompt() {
+  rl.question('hand1: ', (answer1) => {
+    rl.question('hand2: ', (answer2) => {
+      console.log( rockPaperScissors(answer1, answer2) );
+      getPrompt();
+    });
+  });
+}
+
+// Tests
+
+if (typeof describe === 'function') {
+
+  describe('#rockPaperScissors()', () => {
+    it('should detect a tie', () => {
+      assert.equal(rockPaperScissors('rock', 'rock'), "It's a tie!");
+      assert.equal(rockPaperScissors('paper', 'paper'), "It's a tie!");
+      assert.equal(rockPaperScissors('scissors', 'scissors'), "It's a tie!");
+    });
+    it('should detect which hand won', () => {
+      assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
+      assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
+      assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
+    });
+    it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
+      assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
+      assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
+      assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
+    });
+  });
+} else {
+
+  getPrompt();
+
+}
