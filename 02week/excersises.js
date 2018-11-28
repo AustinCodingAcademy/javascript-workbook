f8f091a43f00a5012aa72a7b741f8c9dba61fc17
Brandon Turner <brandontheturner@gmail.com>
2018-11-27 18:40:59 -0600

    started exercises file

---
+++ 02week/excersises.js
@@ -0,0 +1,13 @@
+const cars = ["porsche", "ferrari", "lamborghini", "lotus"];
+
+console.log(cars.length);
+
+
+
+const moreCars = ["ford", "chevy", "toyota", "honda"];
+
+console.log(cars.concat(moreCars));
+
+console.log(cars.indexOf("honda"));
+
+console.log(cars.lastIndexOf("ford"));
