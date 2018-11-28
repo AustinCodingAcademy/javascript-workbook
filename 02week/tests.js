8b9b35723bd2887656a94aeed95ec6eb3500bc2f
Brandon Turner <brandontheturner@gmail.com>
2018-11-27 18:40:12 -0600

    made some tests

--- 02week/tests.js
+++ 02week/tests.js
@@ -9,18 +9,20 @@ const rl = readline.createInterface({
 
 function rockPaperScissors(hand1, hand2) {
   const tie = "It's a tie!";
-  const hand1Wins = "Hand one wins!";
-  const hand2Wins = "Hand two wins!";
+  const hand1Wins = "Player wins!";
+  const hand2Wins = "AI wins!";
   hand1 = hand1.toLowerCase().trim();
   hand2 = hand2.toLowerCase().trim();
+
   if (!hand1 || !hand2) {
-    return "Pick Rock Paper or Scissors....only";
+    return "Rock Paper or Scissors....only";
   }
   if (hand1 != "rock" && hand1 != "scissors" && hand1 != "paper") {
-    return "please enter a valid hand for hand 1";
+    return "please enter a valid hand Player";
   }
   if (hand2 != "rock" && hand2 != "scissors" && hand2 != "paper") {
-    return "please enter a valid hand for hand 2";
+
+    return "please enter a valid hand";
   }
   if (hand1 === hand2) {
     return tie;
@@ -30,19 +32,25 @@ function rockPaperScissors(hand1, hand2) {
   for (let i = 0; i < hand1WinConditions.length; i++) {
     if (hand1.toString() + "/" + hand2.toString() === hand1WinConditions[i]) {
       return hand1Wins;
-    } else if (hand1.tostring() + "/" + hand2.tostring() === hand2WinConditions[i]) {
+    } else if (hand1.toString() + "/" + hand2.toString() === hand2WinConditions[i]) {
       return hand2Wins;
     }
 
   }
 }
 
+function playerTwoAI() {
+  let hands = ["rock", "paper", "scissors"];
+  let chosen2 = hands[Math.floor(Math.random() * hands.length)];
+  console.log("AI player 2 " + chosen2);
+  return chosen2;
+
+}
+
 function getPrompt() {
   rl.question("hand1: ", answer1 => {
-    rl.question("hand2: ", answer2 => {
-      console.log(rockPaperScissors(answer1, answer2));
-      getPrompt();
-    })
+    console.log(rockPaperScissors(answer1, playerTwoAI()));
+    getPrompt();
   })
 }
 //Updated testing 
@@ -54,19 +62,28 @@ if (typeof describe === "function") {
       assert.equal(rockPaperScissors("paper", "paper"), "It's a tie!");
       assert.equal(rockPaperScissors("scissors", "scissors"), "It's a tie!")
     });
-    // Hand one winning conditions
-    it("should detect when hand 1 wins", () => {
-      assert.equal(rockPaperScissors("rock", "scissors"), "Hand one wins!");
-      assert.equal(rockPaperScissors("paper", "rock"), "Hand one wins!");
-      assert.equal(rockPaperScissors("scissors", "paper"), "Hand one wins!")
-    })
-    it("should detect when hand 2 wins", () => {
-      assert.equal(rockPaperScissors("scissors", "rock"), "Hand two wins!");
-      assert.equal(rockPaperScissors("rock", "paper"), "Hand two wins!");
-      assert.equal(rockPaperScissors("paper", "scissors"), "Hand two wins!")
+    // Detect when nothing has been entered
+    it("detect when nothing has been entered", () => {
+      assert.equal(rockPaperScissors("", ""), "Rock Paper or Scissors....only");
+    });
+    // Player winning conditions
+    it("should detect when Player wins", () => {
+      assert.equal(rockPaperScissors("rock", "scissors"), "Player wins!");
+      assert.equal(rockPaperScissors("paper", "rock"), "Player wins!");
+      assert.equal(rockPaperScissors("scissors", "paper"), "Player wins!");
     });
     // Hand 2 winning conditions
-
+    it("should detect when AI wins", () => {
+      assert.equal(rockPaperScissors("scissors", "rock"), "AI wins!");
+      assert.equal(rockPaperScissors("rock", "paper"), "AI wins!");
+      assert.equal(rockPaperScissors("paper", "scissors"), "AI wins!")
+    });
+    //valid hand
+    it("must be a valid hand selection", () => {
+      assert.equal(rockPaperScissors("sscires", "rrck"), "please enter a valid hand");
+      assert.equal(rockPaperScissors("rick", "popeR"), "please enter a valid hand");
+      assert.equal(rockPaperScissors("pipER", "Slissors"), "please enter a valid hand");
+    });
   });
 } else {
   getPrompt();
