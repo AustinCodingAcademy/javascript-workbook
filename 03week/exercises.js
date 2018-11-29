aaea75c50351c7c4567bcc30f785e4cfa290f060
Ronald Hajewski <ronald.hajewski@gmail.com>
2018-11-28 18:45:37 -0600

    working on tic tac toe

---
+++ 03week/exercises.js
@@ -0,0 +1,115 @@
+"use strict";
+
+// Complete each of the following exercises.
+// length
+// Create an array called cars which consists of 4 different types of cars. The first car type should be Ford.
+let cars = ["Ford", "Chevy", "Toyota", "Audi"];
+// Console.log the length of the array.
+console.log("Console.log the length of the array. ", cars.length);
+
+// concat
+// Create another array called more cars with 4 more different types of cars. The last car type should be Honda.
+let moreCars = ["Saab", "Mercedes", "Ferrari", "Honda"];
+// Use the concat method to combine the cars and moreCars arrays into an array called totalCars.
+let totalCars = cars.concat(moreCars);
+console.log(
+  "Use the concat method to combine the cars and moreCars arrays into an array called totalCars. ",
+  cars.concat(moreCars)
+);
+console.log(
+  "Use the concat method to combine the cars and moreCars arrays into an array called totalCars. ",
+  totalCars
+);
+
+// indexOf and lastIndexOf
+// Use the indexOf method to console.log the index of Honda.
+console.log(
+  "Use the indexOf method to console.log the index of Honda. ",
+  totalCars.indexOf("Honda")
+);
+// Use the lastIndexOf method to console.log the index of Ford.
+console.log(
+  "Use the lastIndexOf method to console.log the index of Ford. ",
+  totalCars.lastIndexOf("Ford")
+);
+// join
+// Use the join method to covert the array totalCars into a string called stringOfCars.
+let stringOfCars = totalCars.join(",");
+console.log(
+  "Use the join method to covert the array totalCars into a string called stringOfCars. ",
+  stringOfCars
+);
+// split
+// Use the split method to convert stringOfCars back intro an array called totalCars.
+totalCars = stringOfCars.split(",");
+console.log(
+  "Use the split method to convert stringOfCars back intro an array called totalCars. ",
+  totalCars
+);
+// reverse
+// Use the reverse method to create an array carsInReverse which is the array totalCars in reverse.
+let carsInReverse = totalCars.reverse();
+console.log(
+  "Use the reverse method to create an array carsInReverse which is the array totalCars in reverse. ",
+  carsInReverse
+);
+// sort
+// Use the sort method to put the array carsInReverse into alphabetical order.
+console.log(
+  "Use the reverse method to create an array carsInReverse which is the array totalCars in reverse. ",
+  carsInReverse.sort()
+);
+
+// Based on the types of cars you used, predict which item in the array should be at index 0.
+// Use the following code to confirm or reject your prediction:
+// alert(carsInReverse.indexOf("yourPrediction"));
+console.log(
+  "Based on the types of cars you used, predict which item in the array should be at index 0. I guessed ford, but it was actually Audi ",
+  totalCars.indexOf("Audi")
+);
+// slice
+// Use the slice method to remove Ford and Honda from the carsInReverse array and move them into a new array called removedCars.
+// splice
+// Use the splice method to remove the 2nd and 3rd items in the array carsInReverse and add Ford and Honda in their place.
+console.log(
+  "Use the splice method to remove the 2nd and 3rd items in the array carsInReverse and add Ford and Honda in their place. ",
+  totalCars.splice(1, 2)
+);
+// push
+// Use the push method to add the types of cars that you removed using the splice method to the carsInReverse array.
+console.log(
+  "Use the push method to add the types of cars that you removed using the splice method to the carsInReverse array. ",
+  carsInReverse.push("Chevy", "Ford")
+);
+console.log(carsInReverse);
+// pop
+// Use the pop method to remove and console.log the last item in the array carsInReverse.
+console.log(
+  "Use the pop method to remove and console.log the last item in the array carsInReverse. ",
+  carsInReverse.pop()
+);
+// shift
+// Use the shift method to remove and console.log the first item in the array carsInReverse.
+console.log(
+  "Use the shift method to remove and console.log the first item in the array carsInReverse. ",
+  carsInReverse.shift()
+);
+// unshift
+// Use the unshift method to add a new type of car to the array carsInReverse.
+console.log(
+  "Use the unshift method to add a new type of car to the array carsInReverse. ",
+  carsInReverse.unshift("Lambo")
+);
+console.log(carsInReverse);
+// forEach
+// Create an array called numbers with the following items: 23, 45, 0, 2 Write code that will add 2 to each item in the array numbers.
+const numbers = [23, 45, 0, 2];
+numbers.forEach(function(num, i) {
+  numbers[i] = 2 + num;
+  if (i === 3) {
+    console.log(
+      "Create an array called numbers with the following items: 23, 45, 0, 2 Write code that will add 2 to each item in the array numbers. ",
+      numbers
+    );
+  }
+});
--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -19,6 +19,12 @@ function printBoard() {
   console.log("2 " + board[2].join(" | "));
 }
 
+function playerChange() {
+  if (playerTurn === "X") {
+  } else playerTurn = "O";
+  return (playerTurn = "O");
+}
+
 function horizontalWin() {
   // Your code here
 }
@@ -89,80 +95,3 @@ if (typeof describe === "function") {
 } else {
   getPrompt();
 }
-
-$(document).ready(function() {
-  var move = 1;
-  var play = true;
-
-  $("#gameBoard tr td").click(function() {
-    if ($(this).text() == "" && play) {
-      if (move % 2 == 1) {
-        $(this)
-          .append("X")
-          .css({ fontSize: "50px", color: "pink" });
-      } else {
-        $(this)
-          .append("O")
-          .css({ fontSize: "50px", color: "#00ffff6f" });
-      }
-      move++;
-      if (checkForWin() != -1 && checkForWin() != "") {
-        // Write function: winner text
-        if (checkForWin() == "X") {
-          alert("Player 1 Winner!");
-        }
-        // Write function: winner text
-        else {
-          alert("Player 2 Winner!");
-        }
-        play = false;
-      }
-    }
-  });
-  // Write function: clear board
-
-  /* $('.reset').click (function () {
-    let clearBoard = $("#gameBoard");
-    let i = 0;
-    for (i = 0; i < clearBoard.length; i++) {
-      $(clearBoard[i]).text('');
-    }
-  } */
-
-  // Write function: check for win
-  function checkForWin() {
-    var box1 = $("#gameBoard tr:nth-child(1) td:nth-child(1)").text();
-    var box2 = $("#gameBoard tr:nth-child(1) td:nth-child(2)").text();
-    var box3 = $("#gameBoard tr:nth-child(1) td:nth-child(3)").text();
-    var box4 = $("#gameBoard tr:nth-child(2) td:nth-child(1)").text();
-    var box5 = $("#gameBoard tr:nth-child(2) td:nth-child(2)").text();
-    var box6 = $("#gameBoard tr:nth-child(2) td:nth-child(3)").text();
-    var box7 = $("#gameBoard tr:nth-child(3) td:nth-child(1)").text();
-    var box8 = $("#gameBoard tr:nth-child(3) td:nth-child(2)").text();
-    var box9 = $("#gameBoard tr:nth-child(3) td:nth-child(3)").text();
-    // check all rows
-    if (box1 == box2 && box2 == box3) {
-      return box3;
-    } else if (box4 == box5 && box5 == box6) {
-      return box6;
-    } else if (box7 == box8 && box8 == box9) {
-      return box9;
-    }
-    // check all columns
-    else if (box1 == box4 && box4 == box7) {
-      return box7;
-    } else if (box2 == box5 && box5 == box8) {
-      return box8;
-    } else if (box3 == box6 && box6 == box9) {
-      return box9;
-    }
-    // check both diagonals
-    else if (box1 == box5 && box5 == box9) {
-      return box9;
-    } else if (box3 == box5 && box5 == box7) {
-      return box7;
-    }
-    // no winner
-    return -1;
-  }
-});
