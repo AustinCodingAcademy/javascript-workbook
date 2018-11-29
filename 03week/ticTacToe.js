2a0968e3fe6b8809cc75d4b74029ac9e03f88a08
Andy Olson <andyolson09@gmail.com>
2018-11-28 22:52:56 -0600

    fixed win checking logic to function properly

--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -12,6 +12,7 @@ let sideLength = board.length;
 let playerOne = "X";
 let playerTwo = "O";
 let playerTurn = playerOne;
+let playerHasWon = false;
 
 function printBoard() {
   console.log("   0  1  2");
@@ -20,12 +21,19 @@ function printBoard() {
   console.log("1 " + board[1].join(" | "));
   console.log("  ---------");
   console.log("2 " + board[2].join(" | "));
+  if (checkForWin()) {
+    announceWinner();
+  }
 }
 
 function horizontalWin() {
   // Your code here
   for (let i = 0; i < sideLength; i++) {
-    if (board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+    if (
+      board[i][0] === board[i][1] &&
+      board[i][1] === board[i][2] &&
+      (board[i][0] === playerOne || board[i][0] === playerTwo)
+    ) {
       return true;
     }
   }
@@ -35,7 +43,11 @@ function horizontalWin() {
 function verticalWin() {
   // Your code here
   for (let i = 0; i < sideLength; i++) {
-    if (board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
+    if (
+      board[0][i] === board[1][i] &&
+      board[1][i] === board[2][i] &&
+      (board[0][i] === playerOne || board[0][i] === playerTwo)
+    ) {
       return true;
     }
   }
@@ -45,8 +57,9 @@ function verticalWin() {
 function diagonalWin() {
   // Your code here
   if (
-    (board[0][0] === board[1][1] && board[1][1] === board[2][2]) ||
-    (board[0][2] === board[1][1] && board[1][1] === board[2][0])
+    (board[1][1] === playerOne || board[1][1] === playerTwo) &&
+    ((board[0][0] === board[1][1] && board[1][1] === board[2][2]) ||
+      (board[0][2] === board[1][1] && board[1][1] === board[2][0]))
   )
     return true;
   else return false;
@@ -63,12 +76,8 @@ function ticTacToe(row, column) {
   let currentBoardPos = board[row][column];
 
   // assign current player symbol to designated row/column position if empty
-  if (currentBoardPos === " ") {
-    board[row][column] = playerTurn;
-    if (checkForWin()) {
-      announceWinner();
-    }
-  } else console.log("Invalid move.");
+  if (currentBoardPos === " ") board[row][column] = playerTurn;
+  else console.log("Invalid move.");
 
   // change playerTurn
   if (playerTurn === playerOne) playerTurn = playerTwo;
@@ -77,13 +86,15 @@ function ticTacToe(row, column) {
 
 function announceWinner() {
   if (playerTurn === playerOne) {
-    console.log("Player One wins!");
-  } else console.log("Player Two wins!");
+    console.log("Player Two wins!");
+  } else console.log("Player One wins!");
+  playerHasWon = true;
 }
 
 function getPrompt() {
   printBoard();
-  console.log("It's Player " + playerTurn + "'s turn.");
+  // exit if player has won
+  if (playerHasWon) return;
   rl.question("row: ", row => {
     rl.question("column: ", column => {
       ticTacToe(row, column);
