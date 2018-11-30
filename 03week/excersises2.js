a392b9ab69ceb953ec6909b4c5b6ca11f99a5338
Brandon Turner <brandontheturner@gmail.com>
2018-11-29 20:21:42 -0600

    putting in reps

--- 03week/excersises2.js
+++ 03week/excersises2.js
@@ -1,7 +1,14 @@
 let cars = ["ford", "chevy", "gmc", "dodge"];
+console.log("cars", cars);
+
 
 console.log(".length array", cars.length);
 
 let moreCars = ["bugatti", "lambo", "porche", "honda"];
+console.log("more cars", moreCars);
+
 
 let totalCars = (cars.concat(moreCars));
+console.log("totalCars", totalCars);
+
+console.log("indexof array to check for placement of honda", totalCars.indexOf);
