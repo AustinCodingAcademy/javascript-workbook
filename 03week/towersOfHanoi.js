c30fb895ea7293ae40a7e0fe3a11ca36bea9bb50
mdons <michaeljdonnell@gmail.com>
2018-11-29 23:21:46 -0600

    added comments

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -22,11 +22,17 @@ function printStacks() {
   console.log("Turns taken: " + turnsTaken);
 }
 
+// movePiece(startStack, endStack) takes in an origin stack and a destination
+// stack. It pops a value off the origin stack and pushes it onto the
+// destination stack.
 function movePiece(startStack, endStack) {
   let temp = stacks[startStack].pop();
   stacks[endStack].push(temp);
 }
 
+// isStack(startStack, endStack) verifies that the startStack and endStack
+// arguments provided by the user are valid keys in the stacks object. It
+// returns true if they are and false if they aren't.
 function isStack(startStack, endStack) {
   const validStacks = ["a", "b", "c"];
   if (validStacks.includes(startStack) && validStacks.includes(endStack)) {
@@ -35,6 +41,10 @@ function isStack(startStack, endStack) {
   return false;
 }
 
+// isLegal(startStack, endStack) takes in an origin stack and a destination
+// stack. It verifies that the origin stack is not empty and that the piece
+// to be moved is not larger than the piece it will be placed on top of. If
+// either is true, the move is not legal and the function returns false.
 function isLegal(startStack, endStack) {
   const startStackLength = stacks[startStack].length;
   const endStackLength = stacks[endStack].length;
@@ -48,6 +58,8 @@ function isLegal(startStack, endStack) {
   return true;
 }
 
+// checkForWin() checks if the tower of Hanoi has been reassembled correctly
+// in stack b or c (the win condition). It returns true if it has.
 function checkForWin() {
   const winningStack = "4,3,2,1";
   if (
@@ -59,6 +71,10 @@ function checkForWin() {
   return false;
 }
 
+// towersOfHanoi takes in an origin stack and a destination stack. It uses the
+// other functions to make sure the user has input valid stacks, verify the
+// move is legal, move a piece, increment the turn counter, and check for a win
+// condition.
 function towersOfHanoi(startStack, endStack) {
   if (isStack(startStack, endStack) && isLegal(startStack, endStack)) {
     movePiece(startStack, endStack);
