a3268af066fab86f0905143c7dfedd44eae6c311
Andy Olson <andyolson09@gmail.com>
2018-12-06 16:16:55 -0600

    removed console logs / general clean up

--- 04week/mastermind.js
+++ 04week/mastermind.js
@@ -23,8 +23,6 @@ function generateSolution() {
     const randomIndex = getRandomInt(0, letters.length);
     solution += letters[randomIndex];
   }
-
-  return solution;
 }
 
 function getRandomInt(min, max) {
@@ -35,14 +33,9 @@ function mastermind(guess) {
   console.log("LOG: In mastermind(): ");
 
   // your code here
-  let win = "You guessed it!";
-  let solution = "abcd";
-
-  const outOfTurns = "You ran out of turns! The solution was" + solution + ".";
+  const win = "You guessed it!";
+  const outOfTurns = "You ran out of turns! The solution was " + solution + ".";
   const guessAgain = "Guess again.";
-
-  console.log("LOG: solution = " + solution);
-  console.log("LOG: guess = " + guess);
 
   // if guess = solution return win
   if (guess === solution) {
@@ -51,24 +44,19 @@ function mastermind(guess) {
   } else {
     let hint = generateHint(solution, guess);
     let guessAndHint = guess + " " + hint;
-    console.log("LOG: hint after calling generateHint() = " + hint);
-    console.log("LOG: guessAndHint = " + guessAndHint);
+    board.push(guessAndHint);
   }
 
   // If the board length equals 10, return 'You ran out of turns! The solution was ' and the solution. Otherwise, return 'Guess again.'.
   if (board.length === 10) {
-    return outOfTurns;
-  } else return guessAgain;
+    console.log(outOfTurns);
+  } else console.log(guessAgain);
 }
 
 function generateHint(solution, guess) {
   // your code here
   let solutionArr = solution.split("");
   let guessArr = guess.split("");
-
-  console.log("LOG: In generateHint(): ");
-  console.log("LOG: solutionArr = " + solutionArr);
-  console.log("LOG: guessArr = " + guessArr);
 
   // Create a variable correctLetterLocations that will record how many correct "letter-locations" were guessed.
   // Set correctLetterLocations equal to 0.
@@ -84,33 +72,26 @@ function generateHint(solution, guess) {
     if (letter === guess) {
       correctLetterLocations++;
       solutionArr[i] = null;
-      console.log(
-        "LOG: We found a match at index " +
-          i +
-          ", correctLetterLocations = " +
-          correctLetterLocations
-      );
+    }
+  }
+  // Now that we have nulled the already counted correctLetterLocations, we can see if the guessArray contains any correctLetters that were not in the correct location.
+  // Set a variable correctLetters equal to 0, and in a for loop, again iterate over the solutionArray.
+  let correctLetters = 0;
+  for (let j = 0; j < solutionArr.length; j++) {
+    // Using .indexOf, determine if the item at the current index in guessArray appears inside of solutionArray. Save that index in a variable called targetIndex.
+    const letter = solutionArr[j];
+    let targetIndex = guessArr.indexOf(letter);
 
-      // Now that we have nulled the already counted correctLetterLocations, we can see if the guessArray contains any correctLetters that were not in the correct location.
-      // Set a variable correctLetters equal to 0, and in a for loop, again iterate over the solutionArray.
-      var correctLetters = 0;
-      for (let j = 0; j < solutionArr.length; j++) {
-        // Using .indexOf, determine if the item at the current index in guessArray appears inside of solutionArray. Save that index in a variable called targetIndex.
-        const letter = solutionArr[j];
-        let targetIndex = guessArr.indexOf(letter);
-
-        // Now, if targetIndex is greater that -1, increment correctLetters and set the item in solutionArray at that index equal to null.
-        if (targetIndex > -1) {
-          correctLetters++;
-          solutionArr[j] = null;
-        }
-      }
+    // Now, if targetIndex is greater that -1, increment correctLetters and set the item in solutionArray at that index equal to null.
+    if (targetIndex > -1) {
+      correctLetters++;
+      solutionArr[j] = null;
     }
   }
 
   // Using the colors package, return a string that prints out the hints you generated, with correctLetterLocations being red, correctLetters being white, and separated by a hyphen.
   return (
-    correctLetterLocations + "-" + correctLetters
+    colors.red(correctLetterLocations) + "-" + colors.white(correctLetters)
   );
 }
 
@@ -138,10 +119,10 @@ if (typeof describe === "function") {
 
   describe("#generateHint()", () => {
     it("should generate hints", () => {
-      assert.equal(generateHint("abcd", "abdc"), "2-2");
+      assert.equal(generateHint("abcd", "abdc"), "2".red + "-" + "2".white);
     });
     it("should generate hints if solution has duplicates", () => {
-      assert.equal(generateHint("abcd", "aabb"), "1-1");
+      assert.equal(generateHint("abcd", "aabb"), "1".red + "-" + "1".white);
     });
   });
 } else {
