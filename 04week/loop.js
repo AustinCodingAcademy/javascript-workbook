e673ae9b22cefbec5cf49e036e08922c35b7e142
Taylor Colten <taylorwcolten@gmail.com>
2018-12-07 01:44:03 -0600

    in for loop and while loop

--- 04week/loop.js
+++ 04week/loop.js
@@ -4,29 +4,42 @@ const cars = ["porsche", "ferrari", "lamborghini", "lotus"];
 //Complete each of the following exercises.
 
 //for loop
-for (let i = 0; i < cars.length; i++) {
-    const text = cars.array[i];
-    
-}
-
 //Use a for loop to console.log each item in the array carsInReverse.
-
+for (let x = 0; x < cars.length; x++) {
+    const text = cars[x];
+    console.log(x);
+}
 //for...in loop
-
 //Create an object (an array with keys and values) called persons with the following data:
 
-//firstName: "Jane"
-//lastName: "Doe"
-//birthDate: "Jan 5, 1925"
-//gender: "female"
+const person = {
+firstName: "Jane",
+lastName: "Doe",
+birthDate: "Jan 5, 1925",
+gender: "female",
+}
 
 //Use a for...in loop to console.log each key.
+for (const i in person) {
+    console.log([i]);
+  }
+  
 
 //Then use a for...in loop and if state to console.log the value associated with the key birthDate.
-
+for (const i in person) {
+    if (i === "birthDate") { 
+      console.log(person[i]);
+    }
+  }
 //while loop
-
 //Use a for loop to console.log the numbers 1 to 1000.
+let a = 0
+while (a < 1000) {
+    a += 1;
+    console.log(a);
+    
+}
+    
 
 //do...while loop
 
