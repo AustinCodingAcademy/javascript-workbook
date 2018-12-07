847bebfb8f4c9795a0dd4817a114ac8ad025c62d
Christopher Bergamo <chrstphrbrgmo@gmail.com>
2018-12-06 20:35:36 -0600

    LOOOOPS

---
+++ 04week/loops.js
@@ -0,0 +1,49 @@
+// for loop
+
+//     Use a for loop to console.log each item in the array cars.
+const cars = ["porsche", "ferrari", "lamborghini", "lotus"];
+let carsLength = cars.length;
+for (var i = 0; i < carsLength; i++) {
+  console.log(cars[i]);
+}
+// for...in loop
+
+//     Create an object (an array with keys and values) called persons with the following data:
+const persons = {
+  firstName: "Jane",
+  lastName: "Doe",
+  birthDate: "Jan 5, 1925",
+  gender: "female"
+};
+// [apple, orange, pear]
+
+//     Use a for...in loop to console.log each key.
+for (let elem in persons) {
+  console.log(persons[elem]);
+}
+//     Then use a for...in loop and if state to console.log the value associated with the key birthDate.
+console.log(persons.birthDate);
+
+// while loop
+
+//     Use a for loop to console.log the numbers 1 to 1000.
+let number = 0;
+while (number < 1000) {
+  number += 1;
+  console.log(number);
+}
+// do...while loop
+
+//     Use a do...while loop to console.log the numbers from 1 to 1000.
+let numberA = 0;
+do {
+  numberA += 1;
+  console.log(numberA);
+} while (numberA < 1000);
+
+// When is a for loop better than a while loop?
+////For loop will continually run, while stops when condition is met.
+//     How is the readability of the code affected?
+
+// What is the difference between a for loop and a for...in loop?
+// What is the difference between a while loop and a do...while loop?
--- 04week/mastermind.js
+++ 04week/mastermind.js
@@ -1,26 +1,30 @@
-'use strict';
+"use strict";
 
-const assert = require('assert');
-const readline = require('readline');
+const colors = require("colors");
+const assert = require("assert");
+const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
 let board = [];
-let solution = '';
-let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+let solution = "";
+let letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
 function printBoard() {
   for (let i = 0; i < board.length; i++) {
-    console.log(board[i]);
+    // console.log(board[i]);
   }
 }
+
+function isValid(guess) {}
 
 function generateSolution() {
   for (let i = 0; i < 4; i++) {
     const randomIndex = getRandomInt(0, letters.length);
     solution += letters[randomIndex];
+    // console.log(solution);
   }
 }
 
@@ -28,18 +32,82 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateHint() {
-  // your code here
+function generateHint(solution, guess) {
+  // console.log(solution);
+  // should take two args solution and guess
+  // split solution and guess into an array for comparison
+  let solutionArray = solution.split("");
+  let guessArray = guess.split("");
+  // Create a variable correctLetterLocations that will record how many correct "letter-locations" were guessed. For instance, a guess of aabc against a solution of deba would yield one correct "letter-location"
+  // (b). Set correctLetterLocations equal to 0.
+  let correctLetterLocations = 0;
+  let correctLetters = 0;
+  // In a for loop, iterate over the solutionArray,
+  // comparing each index of solutionArray against the same index of guessArray. If the item matches, increment correctLetterLocations, and set that index in solutionArray to null.
+  for (let i = 0; i < solutionArray.length; i++) {
+    let letter = solutionArray[i];
+    let guess = guessArray[i];
+    if (letter === guess) {
+      correctLetterLocations++;
+      solutionArray[i] = null;
+    } else {
+    }
+  }
+  // Now that we have nulled the already counted correctLetterLocations, we can see if the guessArray contains any correctLetters that were not in the correct location. Set a variable correctLetters equal to 0, and in a for loop, again iterate over the solutionArray. Using .indexOf, determine if the item at the current index in guessArray appears inside of solutionArray. Save that index in a variable called targetIndex. Now, if targetIndex is greater that -1, increment correctLetters and set the item in solutionArray at that index equal to null.
+  for (let j = 0; j < solutionArray.length; j++) {
+    let letter = solutionArray[j];
+    let targetIndex = guessArray.indexOf(letter);
+    if (targetIndex > -1) {
+      correctLetters++;
+      solutionArray[j] = null;
+    }
+  }
+  // Using the colors package, return a string that prints out the hints you generated, with correctLetterLocations being red, correctLetters being white, and separated by a hyphen.
+  return (
+    colors.red(correctLetterLocations) + " - " + colors.white(correctLetters)
+  );
+  // Using the colors package, return a string that prints out the hints you generated, with correctLetterLocations being red, correctLetters being white, and separated by a hyphen.
 }
-
 function mastermind(guess) {
-  solution = 'abcd'; // Comment this out to generate a random solution
-  // your code here
+  // solution = "abcd";
+  // comment this out to generate a random solution generateSolution()
+  // if guess != solution run the generate hint function with guess and solution as parameters
+  // Define a var called hint that collects the returned value of generateHint(solution, guess). .push the guess and the hint (as a combined string) into the board.
+  // If the board length equals 10, return 'You ran out of turns! The solution was ' and the solution. Otherwise, return 'Guess again.'.
+  // comment out the code above to generate a random solution generateSolution()
+  generateSolution();
+  if (isValid(guess)) {
+    const win = "You guessed it!";
+    const outOfTurns = "You ran out of turns! The solution is " + solution;
+    const guessAgain = "Try again";
+    if (solution === guess) {
+      let hint = generateHint(solution, guess);
+      let guessAndHint = guess + " " + hint;
+      board.push(guessAndHint);
+      console.log(win);
+      return win;
+    } else {
+      // if guess != solution run the generate hint function with guess and solution as parameters
+      // Define a var called hint that collects the returned value of generateHint(solution, guess). .push the guess and the hint (as a combined string) into the board.
+      let hint = generateHint(solution, guess);
+      let guessAndHint = guess + " " + hint;
+      board.push(guessAndHint);
+      console.log(hint);
+    }
+    // If the board length equals 10, return 'You ran out of turns! The solution was ' and the solution. Otherwise, return 'Guess again.'.
+    if (board.length === 10) {
+      console.log(outOfTurns);
+      return outOfTurns;
+    } else {
+      console.log(guessAgain);
+      return guessAgain;
+    }
+  } else {
+    console.log(colors.red("Not a valid entry. Guess Again."));
+  }
 }
-
-
 function getPrompt() {
-  rl.question('guess: ', (guess) => {
+  rl.question("guess: ", guess => {
     mastermind(guess);
     printBoard();
     getPrompt();
@@ -48,30 +116,33 @@ function getPrompt() {
 
 // Tests
 
-if (typeof describe === 'function') {
-  solution = 'abcd';
-  describe('#mastermind()', () => {
-    it('should register a guess and generate hints', () => {
-      mastermind('aabb');
+if (typeof describe === "function") {
+  solution = "abcd";
+  describe("#mastermind()", () => {
+    it("should register a guess and generate hints", () => {
+      mastermind("aabb");
       assert.equal(board.length, 1);
     });
-    it('should be able to detect a win', () => {
-      assert.equal(mastermind(solution), 'You guessed it!');
+    it("should be able to detect a win", () => {
+      assert.equal(mastermind(solution), "You guessed it!");
     });
   });
 
-  describe('#generateHint()', () => {
-    it('should generate hints', () => {
-      assert.equal(generateHint('abdc'), '2-2');
+  describe("#generateHint()", () => {
+    it("should generate hints", () => {
+      assert.equal(
+        generateHint("abdc", "abcd"),
+        colors.red("2") + " - " + colors.white("2")
+      );
     });
-    it('should generate hints if solution has duplicates', () => {
-      assert.equal(generateHint('aabb'), '1-1');
+    it("should generate hints if solution has duplicates", () => {
+      assert.equal(
+        generateHint("abee", "aabb"),
+        colors.red("1") + " - " + colors.white("1")
+      );
     });
-
   });
-
 } else {
-
   generateSolution();
   getPrompt();
 }
