607baa6b554ca18e5bb68066cf4a63f7e40268dc
Ronald Hajewski <ronald.hajewski@gmail.com>
2018-12-11 13:15:19 -0600

    loops.js questions

--- 04week/loop.js
+++ 04week/loop.js
@@ -66,6 +66,14 @@ for (const i = 0; i < 0; i++) {
 // }
 
 // When is a for loop better than a while loop?
+// a for loop is better than a while loop when you need to loop through a block of code a number of times and can't depend on the specified condtion being true.
+// While loop is used in situations where we do not know how many times loop needs to be excuted beforehand. // For loop is used where we already know about the number of times loop needs to be excuted. Typically for a index used in iteration.
+
 // How is the readability of the code affected?
+// for each defines a variable [i] or [j] in most cases - to increment until a condition is met. This can appear a little confusing especially if there are nested for loops. While loop is a little easier to read but behaves slightly different - once the value of the "while" statement is met then the loop ends.
+
 // What is the difference between a for loop and a for...in loop?
+// for...in loop is used to loop through an object's properties.
+
 // What is the difference between a while loop and a do...while loop?
+// a do...while loop will always execute once, even if the condition is never true.
