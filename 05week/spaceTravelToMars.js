9126d0c86140b79f09faf5ebf9ace3d7b1c3abaa
Joanna Filipowska <j.filipowska@gmail.com>
2018-12-13 11:24:05 -0600

    homework week 5

--- 05week/spaceTravelToMars.js
+++ 05week/spaceTravelToMars.js
@@ -1,30 +1,56 @@
-'use strict';
+"use strict";
 
-let assert = require('assert');
+let assert = require("assert");
 
 let jobTypes = {
-  pilot: 'MAV',
-  mechanic: 'Repair Ship',
-  commander: 'Main Ship',
-  programmer: 'Any Ship!'
+  pilot: "MAV",
+  mechanic: "Repair Ship",
+  commander: "Main Ship",
+  programmer: "Any Ship!"
 };
 
 // Your code here
+class CrewMember {
+  constructor(name, job, specialSkill, ship) {
+    this.name = name;
+    this.job = job;
+    this.specialSkill = specialSkill;
+    this.ship = ship;
+  }
+  enterShip(shipName) {
+    this.ship = shipName;
+    shipName.crew.push(this);
+  }
+}
 
-//tests
-if (typeof describe === 'function'){
-  describe('CrewMember', function(){
-    it('should have a name, a job, a specialSkill and ship upon instantiation', function(){
-      var crewMember1 = new CrewMember('Rick Martinez', 'pilot', 'chemistry');
-      assert.equal(crewMember1.name, 'Rick Martinez');
-      assert.equal(crewMember1.job, 'pilot');
-      assert.equal(crewMember1.specialSkill, 'chemistry');
+class Ship {
+  constructor(name, type, ability) {
+    this.name = name;
+    this.type = type;
+    this.ability = ability;
+    this.crew = [];
+  }
+  missionStatement() {
+    if (this.crew.length != 0) {
+      return this.ability;
+    }
+    return "Can't perform a mission yet.";
+  }
+}
+
+if (typeof describe === "function") {
+  describe("CrewMember", function() {
+    it("should have a name, a job, a specialSkill and ship upon instantiation", function() {
+      var crewMember1 = new CrewMember("Rick Martinez", "pilot", "chemistry");
+      assert.equal(crewMember1.name, "Rick Martinez");
+      assert.equal(crewMember1.job, "pilot");
+      assert.equal(crewMember1.specialSkill, "chemistry");
       assert.equal(crewMember1.ship, null);
     });
 
-    it('can enter a ship', function(){
-      let mav = new Ship('Mars Ascent Vehicle', 'MAV', 'Ascend into low orbit');
-      let crewMember1 = new CrewMember('Rick Martinez', 'pilot', 'chemistry');
+    it("can enter a ship", function() {
+      let mav = new Ship("Mars Ascent Vehicle", "MAV", "Ascend into low orbit");
+      let crewMember1 = new CrewMember("Rick Martinez", "pilot", "chemistry");
       crewMember1.enterShip(mav);
       assert.equal(crewMember1.ship, mav);
       assert.equal(mav.crew.length, 1);
@@ -32,20 +58,28 @@ if (typeof describe === 'function'){
     });
   });
 
-  describe('Ship', function(){
-    it('should have a name, a type, an ability and an empty crew upon instantiation', function(){
-      let mav = new Ship('Mars Ascent Vehicle', 'MAV', 'Ascend into low orbit');
-      assert.equal(mav.name, 'Mars Ascent Vehicle');
-      assert.equal(mav.type, 'MAV');
-      assert.equal(mav.ability, 'Ascend into low orbit');
+  describe("Ship", function() {
+    it("should have a name, a type, an ability and an empty crew upon instantiation", function() {
+      let mav = new Ship("Mars Ascent Vehicle", "MAV", "Ascend into low orbit");
+      assert.equal(mav.name, "Mars Ascent Vehicle");
+      assert.equal(mav.type, "MAV");
+      assert.equal(mav.ability, "Ascend into low orbit");
       assert.equal(mav.crew.length, 0);
     });
 
-    it('can return a mission statement correctly', function(){
-      let mav = new Ship('Mars Ascent Vehicle', 'MAV', 'Ascend into low orbit');
-      let crewMember1 = new CrewMember('Rick Martinez', 'pilot', 'chemistry');
-      let hermes = new Ship('Hermes', 'Main Ship', 'Interplanetary Space Travel');
-      let crewMember2 = new CrewMember('Commander Lewis', 'commander', 'geology');
+    it("can return a mission statement correctly", function() {
+      let mav = new Ship("Mars Ascent Vehicle", "MAV", "Ascend into low orbit");
+      let crewMember1 = new CrewMember("Rick Martinez", "pilot", "chemistry");
+      let hermes = new Ship(
+        "Hermes",
+        "Main Ship",
+        "Interplanetary Space Travel"
+      );
+      let crewMember2 = new CrewMember(
+        "Commander Lewis",
+        "commander",
+        "geology"
+      );
       assert.equal(mav.missionStatement(), "Can't perform a mission yet.");
       assert.equal(hermes.missionStatement(), "Can't perform a mission yet.");
 
