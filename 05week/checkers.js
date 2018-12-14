37b25fc66abc4ea3c38fa8e82518a5b245b3da5a
Brandon Turner <brandontheturner@gmail.com>
2018-12-14 12:15:21 -0600

    figuring out logic with class and functions

--- 05week/checkers.js
+++ 05week/checkers.js
@@ -1,20 +1,36 @@
-'use strict';
+"use strict";
 
-const assert = require('assert');
-const readline = require('readline');
+const assert = require("assert");
+const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+let checkers = "x";
 
 
-function Checker() {
-  // Your code here
+class Checker {
+  //put a piece in here
+  constructor(startPos) {
+    if (parseInt(startPos, 10) < 40) {
+      this.symbol = "O";
+    } else {
+      this.symbol = "X";
+    }
+  }
+  // Your code HE
+}
+
+
+function moveChecker(whichPiece, toWhere) {
+
 }
 
 class Board {
-  constructor() {
-    this.grid = []
+  constructor(checkers) {
+    this.grid = [],
+      this.checkers = []
+    // board needs an array of checkers
   }
   // method that creates an 8x8 array, filled with null values
   createGrid() {
@@ -41,15 +57,22 @@ class Board {
           rowOfCheckers.push(this.grid[row][column].symbol);
         } else {
           // just push in a blank space
-          rowOfCheckers.push(' ');
+          rowOfCheckers.push(" ");
         }
       }
       // join the rowOfCheckers array to a string, separated by a space
-      string += rowOfCheckers.join(' ');
+      string += rowOfCheckers.join("|");
       // add a 'new line'
       string += "\n";
     }
     console.log(string);
+  }
+  placePieces() {
+    for (let i = 0; i < 24; i++) {
+      const element = array[i];
+      // make checker pieces appear on board
+      // push this into board.checkers
+    }
   }
 
   // Your code here
@@ -57,55 +80,64 @@ class Board {
 
 class Game {
   constructor() {
-    this.board = new Board;
+    this.board = new Board();
+
   }
   start() {
     this.board.createGrid();
+    this.board.placePieces();
   }
-}
+  moveChecker(whichPiece, toWhere) {
+    // make sure move is legal
+    // we wanna make sure that no piece is in a legal adjacent
+    // if there is we need to check the next adjacent spot
+    // if that legal move allowed
+    // Extra Credit: King Logic
+    // Extra Credit: has to jump logic
+  }
 
-function getPrompt() {
-  game.board.viewGrid();
-  rl.question('which piece?: ', (whichPiece) => {
-    rl.question('to where?: ', (toWhere) => {
-      game.moveChecker(whichPiece, toWhere);
-      getPrompt();
+  function getPrompt() {
+    game.board.viewGrid();
+    rl.question("which piece?: ", whichPiece => {
+      rl.question("to where?: ", toWhere => {
+        game.moveChecker(whichPiece, toWhere);
+        getPrompt();
+      });
     });
-  });
-}
+  }
 
-const game = new Game();
-game.start();
+  const game = new Game();
+  game.start();
+
+  // Tests
+  if (typeof describe === "function") {
+    describe("Game", () => {
+      it("should have a board", () => {
+        assert.equal(game.board.constructor.name, "Board");
+      });
+      it("board should have 24 checkers", () => {
+        assert.equal(game.board.checkers.length, 24);
+      });
+    });
 
 
-// Tests
-if (typeof describe === 'function') {
-  describe('Game', () => {
-    it('should have a board', () => {
-      assert.equal(game.board.constructor.name, 'Board');
+    describe("Game.moveChecker()", () => {
+      it("should move a checker", () => {
+        assert(!game.board.grid[4][1]);
+        game.moveChecker("50", "41");
+        assert(game.board.grid[4][1]);
+        game.moveChecker("21", "30");
+        assert(game.board.grid[3][0]);
+        game.moveChecker("52", "43");
+        assert(game.board.grid[4][3]);
+      });
+      it("should be able to jump over and kill another checker", () => {
+        game.moveChecker("30", "52");
+        assert(game.board.grid[5][2]);
+        assert(!game.board.grid[4][1]);
+        assert.equal(game.board.checkers.length, 23);
+      });
     });
-    it('board should have 24 checkers', () => {
-      assert.equal(game.board.checkers.length, 24);
-    });
-  });
-
-  describe('Game.moveChecker()', () => {
-    it('should move a checker', () => {
-      assert(!game.board.grid[4][1]);
-      game.moveChecker('50', '41');
-      assert(game.board.grid[4][1]);
-      game.moveChecker('21', '30');
-      assert(game.board.grid[3][0]);
-      game.moveChecker('52', '43');
-      assert(game.board.grid[4][3]);
-    });
-    it('should be able to jump over and kill another checker', () => {
-      game.moveChecker('30', '52');
-      assert(game.board.grid[5][2]);
-      assert(!game.board.grid[4][1]);
-      assert.equal(game.board.checkers.length, 23);
-    });
-  });
-} else {
-  getPrompt();
-}
+  } else {
+    getPrompt();
+  }
