35e0df7d1f67626e24c98ba675228796248035b9
Brandon Turner <brandontheturner@gmail.com>
2018-12-18 19:11:14 -0600

    fixed jump logic and test everything working

--- 05week/checkers.js
+++ 05week/checkers.js
@@ -2,33 +2,40 @@
 
 const assert = require("assert");
 const readline = require("readline");
+const colors = require("colors");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-
-
 class Checker {
   //put a piece in here
-  constructor(symbol, color, row, column, king) {
-    this.symbol = symbol,
-      this.color = color,
-      this.row = row,
-      this.column = column,
-      this.king = false;
+  constructor(symbol, color, row, column) {
+    (this.symbol = symbol),
+      (this.color = color),
+      (this.row = row),
+      (this.column = column),
+      (this.king = false);
+  }
+  makeKing() {
+    if (!this.king) {
+      this.king = true;
+      if (this.symbol === "r") {
+        this.symbol = "R";
+      } else {
+        this.symbol = "B";
+      }
+    }
   }
 }
 // Your code HE
-
 
 //function moveChecker(whichPiece, toWhere) {
 //}
 
 class Board {
   constructor() {
-    this.grid = [],
-      this.checkers = []
+    (this.grid = []), (this.checkers = []);
     // board needs an array of checkers
   }
   // method that creates an 8x8 array, filled with null values
@@ -83,8 +90,7 @@ class Board {
       }
     });
     return win;
-  };
-
+  }
 
   removePiece(pieceToRemove) {
     const removeRow = parseInt(pieceToRemove.row);
@@ -95,8 +101,8 @@ class Board {
     // pieceToRemove.row = -1;
     // pieceToRemove.column = -1;
 
-    let removedIndex = this.checkers.indexOf(pieceToRemove)
-    this.checkers.splice(removedIndex, 1)
+    let removedIndex = this.checkers.indexOf(pieceToRemove);
+    this.checkers.splice(removedIndex, 1);
   }
   //set piece after jumped to is dead
 
@@ -108,23 +114,42 @@ class Board {
     const oldColumn = parseInt(currentPiece.column); // variable for current pieces column index before it is moved, logging location
 
     const valid = [0, 1, 2, 3, 4, 5, 6, 7];
-    if (!valid.includes(newRow) || !valid.includes(newColumn) || this.grid[newRow][newColumn]) { // limiting movement to the array valid 
+    if (
+      !valid.includes(newRow) ||
+      !valid.includes(newColumn) ||
+      this.grid[newRow][newColumn]
+    ) {
+      // limiting movement to the array valid
       console.log("Not a valid move");
       return false;
-    } else if (currentPiece.symbol === "r") {
-      if ((newRow === oldRow + 1 && newColumn === oldColumn - 1) || //limiting movement to within 1 space of starting position or oldRow
-        (newRow === oldRow + 1 && newColumn === oldColumn + 1)) {
+    }
+    if (currentPiece.symbol === "r") {
+      if (
+        (newRow === oldRow + 1 && newColumn === oldColumn - 1) || //limiting movement to within 1 space of starting position or oldRow
+        (newRow === oldRow + 1 && newColumn === oldColumn + 1)
+      ) {
         return true;
-      } else if (this.grid[oldRow + 1][oldColumn + 1] && this.grid[oldRow + 1][oldColumn + 1].symbol != "r" && //jumping piece logic in the + or ascending column direction, if piece does not match current symbol and is adjacent, 
-        newRow === oldRow + 2 && newColumn === oldColumn + 2) { //logic for jumping piece 2 spaces instead of one
+      } else if (
+        newRow === oldRow + 2 &&
+        newColumn === oldColumn + 2 &&
+        this.grid[oldRow + 1][oldColumn + 1] &&
+        this.grid[oldRow + 1][oldColumn + 1].symbol != "r" //jumping piece logic in the + or ascending column direction, if piece does not match current symbol and is adjacent,
+      ) {
+        //logic for jumping piece 2 spaces instead of one
         this.removePiece(this.grid[oldRow + 1][oldColumn + 1]); // removing the jumped piece logic
-        if (this.checkForWin("r")) { // checking for win conditions
+        if (this.checkForWin("r")) {
+          // checking for win conditions
           console.log("redWins");
         }
 
         return true;
-      } else if (this.grid[oldRow + 1][oldColumn - 1] && this.grid[oldRow + 1][oldColumn - 1].symbol != "r" && 
-        newRow === oldRow + 2 && newColumn === oldColumn - 2) {// jumping piece logic for jumping in the - column direction or descending direction
+      } else if (
+        newRow === oldRow + 2 &&
+        newColumn === oldColumn - 2 &&
+        this.grid[oldRow + 1][oldColumn - 1] &&
+        this.grid[oldRow + 1][oldColumn - 1].symbol != "r"
+      ) {
+        // jumping piece logic for jumping in the - column direction or descending direction
         this.removePiece(this.grid[oldRow + 1][oldColumn - 1]);
         if (this.checkForWin("r")) {
           console.log("redWins");
@@ -134,18 +159,29 @@ class Board {
         return false;
       }
     } else if (currentPiece.symbol === "b") {
-      if ((newRow === oldRow - 1 && newColumn === oldColumn - 1) ||
-        (newRow === oldRow - 1 && newColumn === oldColumn + 1)) {
+      if (
+        (newRow === oldRow - 1 && newColumn === oldColumn - 1) ||
+        (newRow === oldRow - 1 && newColumn === oldColumn + 1)
+      ) {
         return true;
-      } else if (this.grid[oldRow - 1][oldColumn - 1] && this.grid[oldRow - 1][oldColumn - 1].symbol != "b" &&
-        newRow === oldRow - 2 && newColumn === oldColumn - 2) { // same move logic as above just applied to the b symbol and moving the rows in the negative or up direction on the board, all row moves are negative 
-        this.removePiece(this.grid[oldRow - 1][oldColumn - 1]);// left coloumn moves are negative and right column moves are positive.
+      } else if (
+        newRow === oldRow - 2 &&
+        newColumn === oldColumn - 2 &&
+        this.grid[oldRow - 1][oldColumn - 1] &&
+        this.grid[oldRow - 1][oldColumn - 1].symbol != "b"
+      ) {
+        // same move logic as above just applied to the b symbol and moving the rows in the negative or up direction on the board, all row moves are negative
+        this.removePiece(this.grid[oldRow - 1][oldColumn - 1]); // left coloumn moves are negative and right column moves are positive.
         if (this.checkForWin("b")) {
           console.log("blueWins");
         }
         return true;
-      } else if (this.grid[oldRow - 1][oldColumn + 1] && this.grid[oldRow - 1][oldColumn + 1].symbol != "b" &&
-        newRow === oldRow - 2 && newColumn === oldColumn + 2) {
+      } else if (
+        newRow === oldRow - 2 &&
+        newColumn === oldColumn + 2 &&
+        this.grid[oldRow - 1][oldColumn + 1] &&
+        this.grid[oldRow - 1][oldColumn + 1].symbol != "b"
+      ) {
         this.removePiece(this.grid[oldRow - 1][oldColumn + 1]);
         if (this.checkForWin("b")) {
           console.log("blueWins");
@@ -155,43 +191,47 @@ class Board {
         return false;
       }
       //king movement
+      // } else if (currentPiece.king) {
+      //   if (newRow === oldRow - 1 && newColumn === oldColumn - 1) ||
+      //     (newRow === oldRow - 1 && newcolumn === oldColumn + 1) ||
+      //     (newRow === oldRow + 1 && newColumn === oldColumn - 1) ||
+      //     (newRow === oldRow + 1 && newColumn === oldColumn + 1)
+      // }
+      // return true;
+      //else if (this.grid[oldRow])
+      // jumping logic for king piece
     }
-    else if (currentPiece.king) {
-      if (newRow === oldRow - 1 && newColumn === oldColumn - 1) ||
-        (newRow === oldRow - 1 && newcolumn === oldColumn + 1) ||
-        (newRow === oldRow + 1 && newColumn === oldColumn - 1) ||
-        (newRow === oldRow + 1 && newColumn === oldColumn + 1)
-    } return true;
   }
-
   placePieces() {
     const redPosition = [
-      [0, 1],
-      [0, 3],
-      [0, 5],
-      [0, 7],
-      [1, 0],
-      [1, 2],
-      [1, 4],
-      [1, 6],
-      [2, 1],
-      [2, 3],
-      [2, 5],
-      [2, 7],
+      // [0, 1],
+      // [0, 3],
+      // [0, 5],
+      // [0, 7],
+      // [1, 0],
+      // [1, 2],
+      // [1, 4],
+      // [1, 6],
+      // [2, 1],
+      // [2, 3],
+      // [2, 5],
+      // [2, 7]
+      [6, 5]
     ];
     const bluePosition = [
-      [7, 0],
-      [7, 2],
-      [7, 4],
-      [7, 6],
-      [6, 1],
-      [6, 3],
-      [6, 5],
-      [6, 7],
-      [5, 0],
-      [5, 2],
-      [5, 4],
-      [5, 6],
+      // [7, 0],
+      // [7, 2],
+      // [7, 4],
+      // [7, 6],
+      // [6, 1],
+      // [6, 3],
+      // [6, 5],
+      // [6, 7],
+      // [5, 0],
+      // [5, 2],
+      // [5, 4],
+      // [5, 6]
+      [1, 2]
     ];
     for (let i = 0; i < redPosition.length; i++) {
       let square = redPosition[i].toString();
@@ -205,14 +245,14 @@ class Board {
       let row = square.charAt(0);
       let column = square.charAt(2);
       let c1 = new Checker("b", "blue", row, column);
-      this.checkers.push(c1)
+      this.checkers.push(c1);
     }
 
     this.checkers.forEach(square => {
       let row = square.row;
       let column = square.column;
       this.grid[row][column] = square;
-    })
+    });
     // make checker pieces appear on board
     // push this into board.checkers
     // Your code here
@@ -226,7 +266,6 @@ class Board {
 
   // math.abs gives you the absolute number. probably need later
   //moveChecker(start, end) {
-
 
   //}
 }
@@ -245,7 +284,7 @@ class Game {
 
     let currentPiece = this.board.checkers.find(checkers => {
       return checkers.row === currentRow && checkers.column === currentColumn;
-    })
+    });
     if (!currentPiece) {
       console.log("Not valid");
       return false;
@@ -261,20 +300,24 @@ class Game {
     currentPiece.row = whereRow;
     currentPiece.column = whereColumn;
     this.board.grid[currentRow][currentColumn] = null;
-    if (!currentPiece.king) {
-      if (currentPiece.color === "red" && whereRow === "7") {
-        console.log("red piece hit bottom of board");
-        currentPiece.king = true;
-        currentPiece.symbol = "R";
-      }
-      if (currentPiece.color === "blue" && whereRow === "0") {
-        console.log("blue piece on top row");
-        currentPiece.king = true;
-        currentPiece.symbol = "B";
-      }
+    if (whereRow === "0" || whereRow === "7") {
+      currentPiece.makeKing();
     }
+
+    // if (!currentPiece.king) {
+    //   if (currentPiece.color === "red" && whereRow === "7") {
+    //     console.log("red piece hit bottom of board");
+    //     currentPiece.king = true;
+    //     currentPiece.symbol = "R";
+    //   }
+    //   if (currentPiece.color === "blue" && whereRow === "0") {
+    //     console.log("blue piece on top row");
+    //     currentPiece.king = true;
+    //     currentPiece.symbol = "B";
+    //   }
+    // }
   }
-};
+}
 //moveChecker(whichPiece, toWhere) {
 // make sure move is legal
 // we wanna make sure that no piece is in a legal adjacent
@@ -305,7 +348,6 @@ if (typeof describe === "function") {
       assert.equal(game.board.checkers.length, 24);
     });
   });
-
 
   describe("Game.moveChecker()", () => {
     it("should move a checker", () => {
