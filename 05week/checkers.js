558be63e8e033cf4ad9f0760b1320c2db783dc03
Bireswar Banerjee <bireswar.infosys@gmail.com>
2019-01-16 00:50:03 -0600

    checkers-bireswar

--- 05week/checkers.js
+++ 05week/checkers.js
@@ -7,26 +7,32 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+let checkerPieceRed   = {symbol: 'red', onBorad: true};
+let checkerPieceBlack = {symbol: 'black', onBorad: true};
+
 
 function Checker() {
   // Your code here
+  initializeGrid();
 }
 
 class Board {
   constructor() {
-    this.grid = []
+    this.grid = [];
+    this.checkers = [];
   }
-  // method that creates an 8x8 array, filled with null values
-  createGrid() {
-    // loop to create the 8 rows
-    for (let row = 0; row < 8; row++) {
-      this.grid[row] = [];
-      // push in 8 columns of nulls
-      for (let column = 0; column < 8; column++) {
-        this.grid[row].push(null);
-      }
-    }
-  }
+   // method that creates an 8x8 array, filled with null values
+   createGrid() {
+     // loop to create the 8 rows
+     for (let row = 0; row < 8; row++) {
+       this.grid[row] = [];
+       // push in 8 columns of nulls
+       for (let column = 0; column < 8; column++) {
+         this.grid[row].push(null);
+       }
+     }
+   }
+
   viewGrid() {
     // add our column numbers
     let string = "  0 1 2 3 4 5 6 7\n";
@@ -53,6 +59,42 @@ class Board {
   }
 
   // Your code here
+  initializeGrid(){
+    let startIndex = 0;
+    for (let row = 0; row < 3; row++) {
+        for (let column = 0; column < 8; column++) {
+          if((row === 0) || (row === 2)){
+            if(column%2 !== 0){
+              this.grid[row][column] = checkerPieceRed;
+            }
+          }
+          else{
+            if(column%2 === 0)
+              this.grid[row][column] = checkerPieceRed;
+          }
+        }
+    }
+    for (let row = 5; row < 8; row++) {
+        for (let column = 0; column < 8; column++) {
+          if((row === 5) || (row === 7)){
+            if(column%2 === 0){
+              this.grid[row][column] = checkerPieceBlack;
+            }
+          }
+          else{
+            if(column%2 !== 0){
+              this.grid[row][column] = checkerPieceBlack;
+            }
+          }
+        }
+    }
+    for(let ctr=0; ctr<12;ctr++){
+      this.checkers.push(checkerPieceRed);
+    }
+    for(let ctr=0; ctr<12;ctr++){
+      this.checkers.push(checkerPieceBlack);
+    }
+  }
 }
 
 class Game {
@@ -61,6 +103,86 @@ class Game {
   }
   start() {
     this.board.createGrid();
+    this.board.initializeGrid();
+  }
+  moveChecker(fromPiece,toPiece){
+    let rowF    = ~~(fromPiece/10);
+    let columnF = fromPiece%10;
+    let rowT    = ~~(toPiece/10);
+    let columnT = toPiece%10;
+
+    if((game.board.grid[rowF][columnF] === null) ||
+       (game.board.grid[rowT][columnT] !== null))
+    {
+      console.log('Invalid move.');
+      return;
+    }
+    let fromColor = game.board.grid[rowF][columnF].symbol;
+    //let toColor = game.board.grid[rowT][columnT].symbol;
+    if(fromColor === 'black')
+    {
+       if((rowT === (rowF-1)) && 
+          ((columnT === (columnF-1)) || (columnT === (columnF+1))))
+       {
+        game.board.grid[rowT][columnT] = game.board.grid[rowF][columnF];
+        game.board.grid[rowF][columnF] = null;
+       }
+       else if(rowT === (rowF-2)) 
+       { 
+          if(columnT === (columnF-2)) 
+          {
+            if(game.board.grid[rowF-1][columnF-1].symbol === 'red')
+            {
+              game.board.grid[rowT][columnT] = game.board.grid[rowF][columnF];
+              game.board.grid[rowF][columnF] = null;
+              game.board.grid[rowF-1][columnF-1] = null;
+              game.board.checkers.shift();
+            }
+          }
+          else if(columnT === (columnF+2))
+          {
+            if(game.board.grid[rowF-1][columnF+1].symbol === 'red')
+            {
+              game.board.grid[rowT][columnT] = game.board.grid[rowF][columnF];
+              game.board.grid[rowF][columnF] = null;
+              game.board.grid[rowF-1][columnF+1] = null;
+              game.board.checkers.shift();
+            }
+          }
+       }
+    }
+    else if(fromColor === 'red')
+    {
+       if((rowT === (rowF+1)) && 
+          ((columnT === (columnF-1)) || (columnT === (columnF+1))))
+       {
+        game.board.grid[rowT][columnT] = game.board.grid[rowF][columnF];
+        game.board.grid[rowF][columnF] = null;
+       }
+       if(rowT === (rowF+2)) 
+       { 
+          if(columnT === (columnF-2)) 
+          {
+            if(game.board.grid[rowF+1][columnF-1].symbol === 'black')
+            {
+              game.board.grid[rowT][columnT] = game.board.grid[rowF][columnF];
+              game.board.grid[rowF][columnF] = null;
+              game.board.grid[rowF+1][columnF-1] = null;
+              game.board.checkers.pop();
+            }
+          }
+          else if(columnT === (columnF+2))
+          {
+            if(game.board.grid[rowF+1][columnF+1].symbol === 'black')
+            {
+              game.board.grid[rowT][columnT] = game.board.grid[rowF][columnF];
+              game.board.grid[rowF][columnF] = null;
+              game.board.grid[rowF+1][columnF+1] = null;
+              game.board.checkers.pop();
+            }
+          }
+       }
+    }
   }
 }
 
