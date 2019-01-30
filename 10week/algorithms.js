77efe788a807194751a94c1e0084052d52a3bf70
mdons <michaeljdonnell@gmail.com>
2019-01-29 20:34:23 -0600

    changed starting index to 0 for sake of readability

--- 10week/algorithms.js
+++ 10week/algorithms.js
@@ -19,11 +19,11 @@ function bubbleSort(arr) {
   let sorted;
   do {
     sorted = true;
-    for (let i = 1; i < arr.length; i++) {
-      if (arr[i - 1] > arr[i]) {
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
         let temp = arr[i];
-        arr[i] = arr[i - 1];
-        arr[i - 1] = temp;
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
         sorted = false;
       }
     }
