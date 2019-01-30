e9d6e6e9e2d4ffe42d4024794ab8f8800ccd82b2
mdons <michaeljdonnell@gmail.com>
2019-01-29 20:13:13 -0600

    bubble sort works

--- 10week/algorithms.js
+++ 10week/algorithms.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-const assert = require('assert');
+const assert = require("assert");
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -16,6 +16,21 @@ for (let i = 0; i < 1000; i++) {
 
 function bubbleSort(arr) {
   // Your code here
+  let sorted;
+  do {
+    sorted = true;
+    arr.reduce((prev, curr, i, arr) => {
+      // console.log(prev, curr, i, arr);
+      if (prev > curr) {
+        let temp = curr;
+        arr[i] = prev;
+        arr[i - 1] = temp;
+        sorted = false;
+      }
+      return arr[i];
+    });
+  } while (!sorted);
+  return arr;
 }
 
 function mergeSort(arr) {
@@ -28,41 +43,40 @@ function binarySearch(arr, item) {
 
 // Tests
 
-if (typeof describe === 'function') {
-
+if (typeof describe === "function") {
   function comparator(a, b) {
     if (Number(a) < Number(b)) return -1;
     if (Number(a) > Number(b)) return 1;
     return 0;
   }
 
-  describe('#bubbleSort()', () => {
-    it('should sort array', () => {
+  describe("#bubbleSort()", () => {
+    it("should sort array", () => {
       const sorted = bubbleSort(arr);
       assert.deepEqual(sorted, arr.sort(comparator));
     });
   });
 
-  describe('#mergeSort()', () => {
-    it('should sort array', () => {
+  xdescribe("#mergeSort()", () => {
+    it("should sort array", () => {
       const sorted = mergeSort(arr);
       assert.deepEqual(sorted, arr.sort(comparator));
     });
   });
 
-  describe('#binarySearch()', () => {
-    it('should return the index of given item if sorted array contains it', () => {
+  xdescribe("#binarySearch()", () => {
+    it("should return the index of given item if sorted array contains it", () => {
       const idx = binarySearch([1, 2, 3, 4], 3);
       assert.equal(idx, 2);
     });
-    it('should return false if item not in sorted array', () => {
+    it("should return false if item not in sorted array", () => {
       const idx = binarySearch([1, 2, 3, 4], 5);
       assert.equal(idx, false);
     });
   });
-
 } else {
-
-  console.log('Run the tests!')
-
+  let arr = [4, 3, 1, 2];
+  console.log(arr);
+  bubbleSort(arr);
+  console.log(arr);
 }
