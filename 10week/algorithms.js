53162c1bc09b921357f41ce3e77d7f20c19eea3a
Ronald Hajewski <ronald.hajewski@gmail.com>
2019-01-31 01:25:18 -0600

    bubble sort working, merge sort not working

--- 10week/algorithms.js
+++ 10week/algorithms.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-const assert = require('assert');
+const assert = require("assert");
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -10,17 +10,59 @@ function getRandomInt(min, max) {
 
 let arr = [];
 
-for (let i = 0; i < 1000; i++) {
-  arr.push(getRandomInt(0, 1000));
+for (let i = 0; i < 10; i++) {
+  arr.push(getRandomInt(0, 10));
 }
 
-function bubbleSort(arr) {
-  // Your code here
+// function bubbleSort(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     //Notice that j < (length - i)
+//     for (let j = 0; j < arr.length - i - 1; j++) {
+//       //compare positions
+//       if (arr[j] > arr[j + 1]) {
+//         //swich the numbers
+//         let temp = arr[j]; //variable to hold the current number
+//         arr[j] = arr[j + 1]; //swap current number with adjacent number
+//         arr[j + 1] = temp; //swap adjacent number with current number
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+// bubbleSort(arr);
+
+function mergeHelper(newLeftArr, newRightArr) {
+  if (newLeftArr[0] < newRightArr[0]) {
+    newRightArr.unshift(newLeftArr.shift());
+  } else {
+    newLeftArr.unshift(newRightArr.shift());
+  }
+  return newLeftArr.concat(newRightArr);
 }
 
 function mergeSort(arr) {
   // Your code here
+  let temp = [];
+  let leftArr;
+  let rightArr;
+  leftArr = arr.slice(0, arr.length / 2);
+  rightArr = arr.slice(arr.length / 2);
+  mergeHelper(leftArr, rightArr);
+  if (arr.length !== 1) {
+    newLeftArr = mergeSort(leftArr);
+    newRightArr = mergeSort(rightArr);
+  }
+  return arr;
 }
+
+function factorial(x) {
+  if (x < 0) return;
+  if (x === 0) return 1;
+  console.log(x, "*", x - 1);
+  return x * factorial(x - 1);
+}
+// console.log(factorial(7));
 
 function binarySearch(arr, item) {
   // Your code here
@@ -28,41 +70,41 @@ function binarySearch(arr, item) {
 
 // Tests
 
-if (typeof describe === 'function') {
-
+if (typeof describe === "function") {
   function comparator(a, b) {
+    // console.log("TCL: comparator -> a, b", a, b);
+
     if (Number(a) < Number(b)) return -1;
     if (Number(a) > Number(b)) return 1;
     return 0;
   }
 
-  describe('#bubbleSort()', () => {
-    it('should sort array', () => {
+  xdescribe("#bubbleSort()", () => {
+    it("should sort array", () => {
       const sorted = bubbleSort(arr);
       assert.deepEqual(sorted, arr.sort(comparator));
     });
   });
 
-  describe('#mergeSort()', () => {
-    it('should sort array', () => {
+  describe("#mergeSort()", () => {
+    it("should sort array", () => {
       const sorted = mergeSort(arr);
-      assert.deepEqual(sorted, arr.sort(comparator));
+      console.log("TCL: sorted", sorted);
+      // console.log("TCL: arr.sort(comparator)", arr.sort(comparator));
+      assert.strictEqual(sorted, arr.sort(comparator));
     });
   });
 
-  describe('#binarySearch()', () => {
-    it('should return the index of given item if sorted array contains it', () => {
+  xdescribe("#binarySearch()", () => {
+    it("should return the index of given item if sorted array contains it", () => {
       const idx = binarySearch([1, 2, 3, 4], 3);
       assert.equal(idx, 2);
     });
-    it('should return false if item not in sorted array', () => {
+    it("should return false if item not in sorted array", () => {
       const idx = binarySearch([1, 2, 3, 4], 5);
       assert.equal(idx, false);
     });
   });
-
 } else {
-
-  console.log('Run the tests!')
-
+  console.log("Run the tests!");
 }
