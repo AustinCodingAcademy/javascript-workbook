caff7bfdc08307e054dee3e116cd146237dd29d0
Christopher Bergamo <chrstphrbrgmo@gmail.com>
2019-01-31 19:12:42 -0600

    algorithms

--- 10week/algorithms.js
+++ 10week/algorithms.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-const assert = require('assert');
+const assert = require("assert");
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -16,7 +16,19 @@ for (let i = 0; i < 1000; i++) {
 
 function bubbleSort(arr) {
   // Your code here
+  var len = arr.length;
+  for (var i = len - 1; i >= 0; i--) {
+    for (var j = 1; j <= i; j++) {
+      if (arr[j - 1] > arr[j]) {
+        var temp = arr[j - 1];
+        arr[j - 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
 }
+console.log(arr);
 
 function mergeSort(arr) {
   // Your code here
@@ -28,41 +40,37 @@ function binarySearch(arr, item) {
 
 // Tests
 
-if (typeof describe === 'function') {
-
+if (typeof describe === "function") {
   function comparator(a, b) {
     if (Number(a) < Number(b)) return -1;
     if (Number(a) > Number(b)) return 1;
     return 0;
   }
 
-  describe('#bubbleSort()', () => {
-    it('should sort array', () => {
+  describe("#bubbleSort()", () => {
+    it("should sort array", () => {
       const sorted = bubbleSort(arr);
       assert.deepEqual(sorted, arr.sort(comparator));
     });
   });
 
-  describe('#mergeSort()', () => {
-    it('should sort array', () => {
+  describe("#mergeSort()", () => {
+    it("should sort array", () => {
       const sorted = mergeSort(arr);
       assert.deepEqual(sorted, arr.sort(comparator));
     });
   });
 
-  describe('#binarySearch()', () => {
-    it('should return the index of given item if sorted array contains it', () => {
+  describe("#binarySearch()", () => {
+    it("should return the index of given item if sorted array contains it", () => {
       const idx = binarySearch([1, 2, 3, 4], 3);
       assert.equal(idx, 2);
     });
-    it('should return false if item not in sorted array', () => {
+    it("should return false if item not in sorted array", () => {
       const idx = binarySearch([1, 2, 3, 4], 5);
       assert.equal(idx, false);
     });
   });
-
 } else {
-
-  console.log('Run the tests!')
-
+  console.log("Run the tests!");
 }
