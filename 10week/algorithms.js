cc780b0efda613630f920852d7a66066068e2482
Andy Olson <andyolson09@gmail.com>
2019-01-31 20:35:26 -0600

    working merge sort binary search

--- 10week/algorithms.js
+++ 10week/algorithms.js
@@ -35,10 +35,36 @@ const swap = (arr, a, b) => {
 
 function mergeSort(arr) {
   // Your code here
+  if (arr.length < 2) return arr;
+
+  let mid = Math.floor(arr.length / 2);
+  let left = arr.slice(0, mid);
+  let right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+function merge(arr1, arr2) {
+  let sortedArray = [];
+
+  while (arr1.length !== 0 && arr2.length !== 0) {
+    // compare first elements of arrays, shift smaller element and push to sortedArr
+    sortedArray.push(arr1[0] < arr2[0] ? arr1.shift() : arr2.shift());
+  }
+
+  return sortedArray.concat(arr1.length ? arr1 : arr2);
 }
 
 function binarySearch(arr, item) {
   // Your code here
+  let mid = Math.floor(arr.length / 2);
+  let midValue = arr[mid];
+
+  if (arr.length < 2) return false;
+  if (midValue === item) return mid;
+
+  return midValue < item
+    ? binarySearch(arr.slice(0, mid))
+    : binarySearch(arr.slice(mid));
 }
 
 // Tests
@@ -61,18 +87,22 @@ if (typeof describe === "function") {
 
   xdescribe("#mergeSort()", () => {
     it("should sort array", () => {
+      console.log("LOG: before sorting", arr);
       const sorted = mergeSort(arr);
+      console.log("LOG: after sorting ", sorted);
       assert.deepEqual(sorted, arr.sort(comparator));
     });
   });
 
-  xdescribe("#binarySearch()", () => {
+  describe("#binarySearch()", () => {
     it("should return the index of given item if sorted array contains it", () => {
       const idx = binarySearch([1, 2, 3, 4], 3);
+      console.log("LOG: idx", idx);
       assert.equal(idx, 2);
     });
     it("should return false if item not in sorted array", () => {
       const idx = binarySearch([1, 2, 3, 4], 5);
+      console.log("LOG: idx", idx);
       assert.equal(idx, false);
     });
   });
