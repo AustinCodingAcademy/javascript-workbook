2943cadea20bb59e8d15f2582ab11c576365d923
andrewjonesw <andrew.williamson.jones@gmail.com>
2019-03-05 16:45:49 -0600

    UnitTest

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -40,9 +40,6 @@ if(hand1 === 'scissors') {
   }
 }
 }
-
-
-
   
 function getPrompt() {
   rl.question('hand1: ', (answer1) => {
@@ -67,13 +64,19 @@ if (typeof describe === 'function') {
       assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
       assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
       assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
+      assert.equal(rockPaperScissors('paper', 'rock'), "Hand one wins!");
+      assert.equal(rockPaperScissors('scissors', 'paper'), "Hand one wins!");
+      assert.equal(rockPaperScissors('scissors', 'rock'), "Hand two wins!");
     });
     it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
       assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
       assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
       assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
     });
+    it('should make sure user must input a valid entry ', () => {
+      assert.equal(rockPaperScissors(!'rock' || !'paper' || !'scissors'), "Hand two wins!");
   });
+});
 } else {
 
   getPrompt();
