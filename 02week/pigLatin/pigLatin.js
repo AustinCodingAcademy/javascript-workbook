326b994d623487e49d3a141c3d992970c7f287cd
andrewjonesw <andrew.williamson.jones@gmail.com>
2019-03-10 13:42:11 -0500

    pigLatin

---
+++ 02week/pigLatin/pigLatin.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const assert = require('assert');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function pigLatin(str) {
+  var vowels = ['a','e','i','o','u'],
+  var result = str.split('');
+  
+  if (vowels.includes(str[0])) {
+    return str += 'way';
+  } else {
+    for (var i = 0; i < str.length; i++) {
+      if (!vowels.includes(str[i])) {
+      result.push(result.shift());
+    } else {
+      result.push('ay');
+      return result.join('');
+    }
+   }
+  }
+ }
+
+function getPrompt() {
+  rl.question('word ', (answer) => {
+    console.log( pigLatin(answer) );
+    getPrompt();
+  });
+}
+
+// Tests
+
+if (typeof describe === 'function') {
+
+  describe('#pigLatin()', () => {
+    it('should translate a simple word', () => {
+      assert.equal(pigLatin('car'), 'arcay');
+      assert.equal(pigLatin('dog'), 'ogday');
+    });
+    it('should translate a complex word', () => {
+      assert.equal(pigLatin('create'), 'eatecray');
+      assert.equal(pigLatin('valley'), 'alleyvay');
+    });
+    it('should attach "yay" if word begins with vowel', () => {
+      assert.equal(pigLatin('egg'), 'eggyay');
+      assert.equal(pigLatin('emission'), 'emissionyay');
+    });
+    it('should lowercase and trim word before translation', () => {
+      assert.equal(pigLatin('HeLlO '), 'ellohay');
+      assert.equal(pigLatin(' RoCkEt'), 'ocketray');
+    });
+  });
+} else {
+
+  getPrompt();
+
+}
