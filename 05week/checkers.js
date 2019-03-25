f20f98dfeac37548581f8481872c9965c05ebd31
Michael Snowden <mtsnowden@gmail.com>
2019-03-25 13:59:55 -0500

    got pieces on the board.

--- 05week/checkers.js
+++ 05week/checkers.js
@@ -8,36 +8,43 @@ const rl = readline.createInterface({
 });
 
 
-function Checker() {
+class Checker {
   // Your code here
+  constructor(symbol, row, col){
+    this.symbol = symbol;
+    this.row= row;
+    this.col = col;
+
+  }
 }
 
 class Board {
   constructor() {
     this.grid = []
+    this.checkers = []
   }
-  // method that creates an 8x8 array, filled with null values
+  // creates an 8x8 array, filled with null values
   createGrid() {
-    // loop to create the 8 rows
+    // loop to create the 8 rows of null values
     for (let row = 0; row < 8; row++) {
       this.grid[row] = [];
-      // push in 8 columns of nulls
+      // push in 8 columns of null values
       for (let column = 0; column < 8; column++) {
         this.grid[row].push(null);
       }
     }
   }
   viewGrid() {
-    // add our column numbers
+    // adding our column numbers
     let string = "  0 1 2 3 4 5 6 7\n";
     for (let row = 0; row < 8; row++) {
       // we start with our row number in our array
       const rowOfCheckers = [row];
       // a loop within a loop
       for (let column = 0; column < 8; column++) {
-        // if the location is "truthy" (contains a checker piece, in this case)
+        // if the location is "truthy" (contains a checker piece)
         if (this.grid[row][column]) {
-          // push the symbol of the check in that location into the array
+          // push the symbol of the checker in that location into the array
           rowOfCheckers.push(this.grid[row][column].symbol);
         } else {
           // just push in a blank space
@@ -52,8 +59,47 @@ class Board {
     console.log(string);
   }
 
+
   // Your code here
+createCheckers() {
+  for (let row = 0; row < 8; row++) {
+    for (let col = 0; col < 8; col++) {
+      // add the row and col coordinates and divide the sum by 2. if there is a remainder
+      // of 1 && the row value is less than 3, push an 'x' into that coordinate of the array. 
+      if ((row + col) % 2 === 1 && row < 3) {
+        const newChecker = new Checker("x", row, col);
+        this.grid[row][col] = newChecker;
+        this.checkers.push(newChecker);
+        // add the row and col coordinates and divide the sum by 2. if there is a remainder
+        // of 1 and the row value is greater than 4, push an 'o' into that coordinate of the array.
+      } else if ((row + col) % 2 === 1 && row > 4) {
+        const newChecker = new Checker("o", row, col);
+        this.grid[row][col] = newChecker;
+        this.checkers.push(newChecker);
+      }
+    }
+  }
 }
+
+findPiece(coordinate) {
+  const row = coordinate[0];
+  const col = coordinate[1];
+  const currentPiece = this.checkers.find(checker => {
+    return checker.row === row && checker.col === col;
+  });
+  return currentPiece;
+}
+
+removePiece(currentPiece) {
+  const row = currentPiece.row;
+  const col = currentPiece.col;
+  this.grid[row][col] = null;
+  const index = this.checkers.indexOf(currentPiece);
+  this.checkers.splice(index, 1);
+}
+}
+
+
 
 class Game {
   constructor() {
