a53057f67c05981fd3437d3dd709e8cf56b1d1d7
masharomanova <mariavromanova@yahoo.com>
2019-04-01 21:01:11 -0500

    1st class

---
+++ 01week/datatypes.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var d = new Date();
+console.log(d);
+
+
+var num = 15;
+var n = num.toString();
+console.log(n);
+console.log(typeof n);
+
+
+var x = "12";
+var y = Number(x);
+console.log(y);
+console.log(typeof y);
+
+
+
+
+var a = 10 > 9;
+console.log(typeof a);
+
+var b = null;
+console.log(b);
+
+var c;
+console.log(typeof c);
+
+var d = 7;
+console.log(typeof d);
+
+var e;
+if (isNaN(e) == true) {
+    console.log("NaN");
+}
+else {
+    console.log("no");
+}
+
+var f = "sixteen";
+console.log(typeof f);
+
+
+
+
+var g = 5;
+var h = 6;
+console.log(g + h);
+
+
+var j = 10 > 9;
+var k = 4 < 5;
+if (j && k == true) {
+    console.log("2 true");
+}
+else {
+    console.log("no");
+}
+
+
+
