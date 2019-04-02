e95245e0d4bf85447d22162a70fd8262134a6c33
Joseph William Hauger <joseph>
2019-04-01 20:58:25 -0500

    day 1

---
+++ 01week/datatypes.js
@@ -0,0 +1,60 @@
+'use strict'
+console.log("start here!"); 
+//dissplay datea and time
+var d = new Date();
+console.log(d);
+//convert a number, 7 to a string, "7" in javascripot
+var num = 7;
+var n = num.toString();
+console.log(typeof n);
+console.log(n);
+var text = "7";
+var integer = parseInt(text);
+console.log(typeof integer)
+console.log(integer);
+//boolean
+var x = 5;
+var z = 7;
+var y = (x == z);
+console.log(typeof y);
+console.log(y);
+//Null
+var person = 44;
+person = null;
+console.log(person);
+//Undefined
+var car;
+console.log(car);
+//Number
+var hi = 5 + 6;
+console.log(typeof hi);
+console.log(hi);
+//NaN
+var e;
+if(isNaN(e) == true){
+    console.log("NaN")
+}
+else{
+    console.log("no")
+    }
+//string
+var fun = "hey" + "  you!";
+console.log(typeof fun);
+console.log(fun);
+//add 2 numbers together
+var a = 7;
+var b = 7;
+var c = (a + b);
+console.log(c);
+//if 2 are true run
+var fun;
+var no;
+if(isNaN(fun && no) == true){
+    console.log("NaN")
+}
+else{
+    console.log("no")
+}
+//program runs when 1 of 2 things are true
+var 
+//runs when both things are not true
