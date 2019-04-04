dcaea152e03687750b18ec0f7e57be41c4f26772
Mikaela Rodriguez <mikrdgz@gmail.com>
2019-04-03 19:30:39 -0500

    some changes

---
+++ 01week/datatypes.js
@@ -0,0 +1,72 @@
+
+//Write a JS program to display the current date and time
+
+var today = new Date();
+
+function showDay() {
+console.log(today.toLocaleString('en-us'));
+}
+showDay();
+
+//convert number to string
+
+// let num = 5;
+
+// function makeString(){
+// let num = num.toString();
+// console.log(num);
+// }
+
+// makeString();
+
+//Write a JavaScript program to convert a string to the number.
+
+var newNum = parseInt(8);
+console.log(newNum);
+
+//Write a JavaScript program that takes in different datatypes and prints out whether they are a:
+// Boolean
+// Null
+// Undefined
+// Number
+// NaN
+// String
+
+function evalData (someData) {
+let n = typeof someData;
+console.log(n);
+}
+
+evalData("hi mom");
+
+// Write a JavaScript program that adds 2 numbers together.
+
+function addTwo(a,b) {
+let result = a + b;
+console.log(result);
+
+ }
+
+// Write a JavaScript program that runs only when 2 things are true.
+function evalStuff (thing1,thing2) {
+    if (thing1 == true && thing2 == true) {
+        console.log("run!");
+    }
+}
+
+// Write a JavaScript program that runs when 1 of 2 things are true.
+
+
+function evalMore (thing1,thing2) {
+    if (thing1 == true || thing2 == true) {
+        console.log("run!");
+    }
+}
+
+// Write a JavaScript program that runs when both things are not true.
+
+function evalFalse (thing1,thing2) {
+    if (thing1 != true && thing2 != true) {
+        console.log("run!");
+    }
+}
