db675cf74f5f15915830fcad356135325997682f
eamoses <e.a.moses@gmail.com>
2019-04-06 22:26:42 -0500

    adding tests

--- 02week/ttt-tests.js
+++ 02week/ttt-tests.js
@@ -1,34 +1,24 @@
-'use strict';
-
-const assert = require('assert');
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-
-function getPrompt() {
-    rl.question('word ', (answer) => {
-      const ttt = ticTacToe(answer);
-      console.log('Log: ', ttt);
-      getPrompt();
-    });
-}
-
 if (typeof describe === 'function') {
 
-    describe('#ticTacToe()', () => {
-      it('should start with an x', () => {
-        assert.equal(ticTacToe('x'), "x");
-      });
-      it('should detect winner', () => {
-        assert.equal(rockPaperScissors('x'), "X Wins!");
-        assert.equal(rockPaperScissors('o'), "O Wins!");
-      });
+  describe('#rockPaperScissors()', () => {
+    it('should detect a tie', () => {
+      assert.equal(rockPaperScissors('rock', 'rock'), "It's a tie!");
+      assert.equal(rockPaperScissors('paper', 'paper'), "It's a tie!");
+      assert.equal(rockPaperScissors('scissors', 'scissors'), "It's a tie!");
     });
-  } else {
-  
-    getPrompt();
-  
-  }
+    it('should detect which hand won', () => {
+      assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
+      assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
+      assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
+    });
+    it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
+      assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
+      assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
+      assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
+    });
+  });
+} else {
+
+  getPrompt();
+
+}
