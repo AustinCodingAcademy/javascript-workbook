04efa9639c87f6c3a594f650bc0d85fd7e51b2c4
eamoses <e.a.moses@gmail.com>
2019-04-06 21:38:48 -0500

    saving challenge

--- 02week/class/challenge.js
+++ 02week/class/challenge.js
@@ -1,5 +1,22 @@
 // PROMPT 
 // Take an array of numbers
-// raise them by the power of two
+// raise them by the power of two (the number times itself)
 // return the sum of the new array
 
+// function powerUp(n) {
+//     const powArray = [];
+//     for (let i = 0; i < n.length; i++) {
+//         pow = n[i] * n[i];
+//         powArray.push(pow);
+//     }
+//     return powArray;
+// }
+// const returnAnswer = powerUp([2, 4, 6, 8]);
+// console.log(returnAnswer);
+
+
+// const loopThruArray = [2, 4, 6, 8].map(function(num) {
+//     let pow = num * num;
+//     return pow;
+// });
+// console.log(loopThruArray);
