a99fa1b8289916962b149df9fb6a1611df21b49f
eamoses <e.a.moses@gmail.com>
2019-04-08 17:33:59 -0500

    save

--- 02week/class/challenge.js
+++ 02week/class/challenge.js
@@ -20,3 +20,23 @@
 //     return pow;
 // });
 // console.log(loopThruArray);
+
+function outer() {
+    var b = 10;
+    var c = 100;
+    function inner() {
+        var a = 20; 
+        console.log("a= " + a + " b= " + b);
+        a++;
+        b++;
+    }
+    return inner;
+}
+var X = outer();  // outer() invoked the first time
+var Y = outer();  // outer() invoked the second time
+//end of outer() function executions
+
+X(); // X() invoked the first time
+X(); // X() invoked the second time
+X(); // X() invoked the third time
+Y(); // Y() invoked the first time
