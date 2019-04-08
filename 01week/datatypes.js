febe452c61f83dd8e1dfb5892170ae75cfd4fe13
Mitchell Jolivette <jolivetteme@gmail.com>
2019-04-08 10:32:50 -0500

    Almost done with the UI

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -8,7 +8,6 @@ function getCurrentDate() {
   let currentDate = new Date();
   return currentDate;
 }
-
 /**
  * [convertNumToString description]
  * @param  {[type]} myNumber [description]
@@ -92,5 +91,45 @@ function ifFalse(oneThing, secondThing) {
   }
 }
 
-var myButton = document.getElementById("convertButton");
-myButton.addEventListener("click", myScript);
+var displayDate = document.getElementById("displayDate");
+var convertNumToStringButton = document.getElementById('convertNumToStringButton');
+var convertStringToNumButton = document.getElementById("convertNumToStringButton");
+var checkDataTypeButton = document.getElementById("checkDataTypeButton");
+var addButton = document.getElementById('addButton');
+//When the displayDate button is clicked
+displayDate.addEventListener("click", function(){
+  displayDate.innerHTML=getCurrentDate();
+});
+
+//When the "Convert Numbers" button is clicked
+convertNumToStringButton.addEventListener("click",function(){
+  var oldField = document.getElementById("inputNumber");
+  var newField = document.getElementById("outputString");
+  newField.value = oldField.value.toString()+" is now a "+typeof newField.value;
+});
+//When the "Convert String" button is clicked
+convertStringToNumButton.addEventListener("click",function(){
+  var oldField = document.getElementById("inputString");
+  var newField = document.getElementById("outputNumber");
+  newField.value = oldField.value+" is now a "+typeof parseInt(newField.value);
+});
+//Check what Datatype
+checkDataTypeButton.addEventListener('click',function(){
+  var myValue = document.getElementById("dataTypeName").valueOf();
+  console.log(typeof myValue.value);
+});
+
+// Add Two Numbers
+addButton.addEventListener("click",function(){
+  var num1 = document.getElementById("num1").value;
+  var num2 = document.getElementById("num2").value;
+  var sum = num1+num2;
+  document.getElementById("sum").value=parseInt(sum);
+});
+getResultButton.addEventListener("click",function(){
+  var item1 = document.getElementById("item1");
+  var item2 = document.getElementById("item2");
+  if (item1.checked==true) {
+    console.log(item1+' true');
+  }
+});
