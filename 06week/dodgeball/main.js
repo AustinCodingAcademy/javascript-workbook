8d61115ca2209b63071986d8375584766ebc2f91
Michael Snowden <mtsnowden@gmail.com>
2019-04-09 17:21:11 -0500

    started. so confused.

---
+++ 06week/dodgeball/main.js
@@ -0,0 +1,99 @@
+"use strict"
+const assert = require('assert');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const arrOfPeople = [
+    {
+      id: 2,
+      name: "Charles Young",
+      age: 55,
+      skillSet: "welding",
+      placeBorn: "Omaha, Nebraska"
+    },
+    {
+      id: 3,
+      name: "Judy Twilight",
+      age: 35,
+      skillSet: "fishing",
+      placeBorn: "Louisville, Kentucky"
+    },
+    {
+      id: 4,
+      name: "Cynthia Doolittle",
+      age: 20,
+      skillSet: "tic tac toe",
+      placeBorn: "Pawnee, Texas"
+    },
+    {
+      id: 5,
+      name: "John Willouby",
+      age: 28,
+      skillSet: "pipe fitting",
+      placeBorn: "New York, New York"
+    },
+    {
+      id: 6,
+      name: "Stan Honest",
+      age: 20,
+      skillSet: "boom-a-rang throwing",
+      placeBorn: "Perth, Australia"
+    },
+    {
+      id: 7,
+      name: "Mia Watu",
+      age: 17,
+      skillSet: "acrobatics",
+      placeBorn: "Los Angeles, California"
+    },
+    {
+      id: 8,
+      name: "Walter Cole",
+      age: 32,
+      skillSet: "jump rope",
+      placeBorn: "New Orleans, Louisiana"
+    },
+  ]
+
+//   creating arrays to move players in and out of..yea? 
+  const listOfPlayers = []
+  const blueTeam = []
+  const redTeam = []
+  
+//   
+  class player {
+    constructor(id, name, age, skillSet, placeBorn){
+        this.id = id,
+        this.name = name,
+        this.age = age,
+        this.skillSet = skillSet,
+        this.placeBorn = placeBorn
+  }
+
+  class blueTeammate {
+    constructor(){}
+  }
+
+  class redTeammate {
+    constructor(){}
+  }
+  
+  const signUpList = () => {
+    const listElement = document.getElementById('signUp')
+    arrOfPeople.map(person => {
+      const li = document.createElement("li")
+      const button = document.createElement("button")
+      button.innerHTML = "Make Player"
+      button.addEventListener('click', function() {makePlayer(person.id)} )
+      li.appendChild(button)
+      li.appendChild(document.createTextNode(person.name + " - " + person.skillSet))
+      listElement.append(li)
+    })
+  }
+  
+  const makePlayer = (id) => {
+    console.log(`li ${id} was clicked!`)
+  }
---
+++ 06week/dodgeball/readme.md
@@ -0,0 +1,5 @@
+20pts - Code Plan - Include this in a README.md file in your folder with comment in your code.
+20pts - Can add People to Players - When clicked the people are added to the Players column and removed from the People list while getting new values of a player added to them.
+20pts - Can add Players to different Teams - When we click on the blue button they Player is added to the blue team and removed from the Player list while also getting the keys color and mascot extended to them when they are moved to a team.
+20pts - Uses Class - This is not a hack job. You should use class to add the new properties you need and extend when you need.
+20pts - Minimum 3 Unit Tests - Use Mocha and Chai to give us at least 3 unit test that prove a person becomes a player and player becomes a teammate.
