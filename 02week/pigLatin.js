0ecff847af340a6d42b5912dbfc5794180cbf455
Aaron Gonzalez <ajgonzalez.93@gmail.com>
2019-04-09 16:24:15 -0500

    Pig Latin w2d1

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -8,11 +8,19 @@ const rl = readline.createInterface({
 });
 
 
-function pigLatin(word) {
-
-  // Your code here
-
+function pigLatin(str) {
+  function check(obj) {
+  	return ['a','i','u','e','o'].indexOf(str.charAt(obj)) == -1 ?
+      										  check(obj+1) : obj;}
+      										  
+  return str.substr(check(0))
+            .concat((check(0) === 0 ? 
+                        'w' : str.substr(0, check(0))) 
+                        + 'ayyyyyy');
 }
+
+// test here
+pigLatin("word");
 
 
 function getPrompt() {
