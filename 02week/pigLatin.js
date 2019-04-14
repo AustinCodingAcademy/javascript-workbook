1479b9d3ea44f877f56eac97fc11c8e657c0320e
Mikaela Rodriguez <mikrdgz@gmail.com>
2019-04-14 13:07:59 -0500

    add ay to words with vowels

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -9,32 +9,50 @@ const rl = readline.createInterface({
 //trim the word at the first letter
 // append "ay" to it
 
-function pigLatin(word) {
-    // Convert string to lowercase
-    word = word.toLowerCase()
-    // Initialize array of vowels
-    const vowels = ["a", "e", "i", "o", "u"];
-    // Initialize vowel index to 0
-    let vowelIndex = 0;
+// function pigLatin(word) {
+//     // Convert string to lowercase
+//     word = word.toLowerCase()
+//     // Initialize array of vowels
+//     const vowels = ["a", "e", "i", "o", "u"];
+//     // Initialize vowel index to 0
+//     let vowelIndex = 0;
   
-    if (vowels.includes(word[0])) {
-      // If first letter is a vowel
-      return word + "way";
-    } else {
-      // If the first letter isn't a vowel i.e is a consonant
-      for (let char of word) {
-        // Loop through until the first vowel is found
-        if (vowels.includes(word)) {
-          // Store the index at which the first vowel exists
-          vowelIndex = word.indexOf(char);
-          break;
-        }
-      }
-      // Compose final string
-      return word.slice(vowelIndex) + word.slice(0, vowelIndex) + "ay";
+//     if (vowels.includes(word[0])) {
+//       // If first letter is a vowel
+//       return word + "way";
+//     } else {
+//       // If the first letter isn't a vowel i.e is a consonant
+//       for (let char of word) {
+//         // Loop through until the first vowel is found
+//         if (vowels.includes(word)) {
+//           // Store the index at which the first vowel exists
+//           vowelIndex = word.indexOf(char);
+//           break;
+//         }
+//       }
+//       // Compose final string
+//       return word.slice(vowelIndex) + word.slice(0, vowelIndex) + "ay";
     
+//   }
+// }
+
+function pigLatin(word) {
+  word = word.toLowerCase();
+
+  const vowels = ["a", "e", "i", "o" , "u", "y"];
+
+  let vowelPos =  word[0];
+
+  if (vowels.includes(vowelPos)){
+    return word + "ay";
   }
-}
+
+  }
+
+
+
+
+
  
 
 
