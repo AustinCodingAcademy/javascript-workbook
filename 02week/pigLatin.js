51b103e48f7dc3b2d1078f22db4e116f9a8ad5a2
Aaron Gonzalez <ajgonzalez.93@gmail.com>
2019-04-15 20:26:38 -0500

    PL changes

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -7,24 +7,51 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-
-function pigLatin(str) {
-  function check(obj) {
-  	return ['a','i','u','e','o'].indexOf(str.charAt(obj)) == -1 ?
-      										  check(obj+1) : obj;}
+//old code
+//function pigLatin(str) {
+  //function check(obj) {
+  	//return ['a','i','u','e','o'].indexOf(str.charAt(obj)) == -1 ?
+      //										  check(obj+1) : obj;}
       										  
-  return str.substr(check(0))
-            .concat((check(0) === 0 ? 
-                        'w' : str.substr(0, check(0))) 
-                        + 'ayyyyyy');
-}
+  //return str.substr(check(0))
+    //        .concat((check(0) === 0 ? 
+      //                  'y' : str.substr(0, check(0))) 
+        //                + 'ay');
+//}
 
+function pigLatin(word){
+  let cut = word.substr(1).trim().toLowerCase() + word.charAt(0) + 'ay';
+  console.log(cut);
+  //convert to lowercase
+  word = word.trim().toLowerCase();
+  //initiliaze array of vowels
+  const vowels = ["a", "e", "i", "o", "u"];
+  //initialize vowel index to 0
+let vowelIndex = 0;
+
+if (vowels.includes(word[0])) {
+  //if first letter is a vowel
+  return word + "yay";
+} else{
+  //if ther first letter isnt a vowel
+  for (let char of word){
+    //loop through until the first vowel is found
+    if(vowels.includes(char)) {
+      //store the index at which the first vowel exists
+      vowelIndex = word.indexOf(char);
+      break;
+    }
+  }
+  //compose final string
+  return word.slice(vowelIndex) + word.slice(0, vowelIndex) + "ay";
+}
+}
 // test here
 pigLatin("word");
 
 
 function getPrompt() {
-  rl.question('word ', (answer) => {
+ rl.question('word ', (answer) => {
     console.log( pigLatin(answer) );
     getPrompt();
   });
