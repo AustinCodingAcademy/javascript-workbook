8e6a25922ab96d32237d4bcc49403680932a0cef
eamoses <e.a.moses@gmail.com>
2019-04-18 23:32:20 -0500

    Saving so far

--- 03week/towersOfHanoi/toh.js
+++ 03week/towersOfHanoi/toh.js
@@ -3,8 +3,9 @@ let stacks = {
     b: [],
     c: []
 }
-let startStack = 'a';
+let startStack = false;
 let gameStatus = true;
+let clickStack = false;
 
 function checkForWin() {
     let stackA = stacks.a
@@ -19,21 +20,75 @@ function checkForWin() {
     }
 }
 
-function towersOfHanoi(startStack, endStack) {
-    let movedFrom = stacks[startStack];
-    let movedTo = stacks[endStack];
-    if (movedFrom.length > 0) {
+function towersOfHanoi(clickedStack) {
+    if (startStack) {
+        let movedFrom = stacks[startStack];
+        let movedTo = stacks[clickedStack];
+        startStack = false;
+        if (movedFrom.length > 0) {
             let movedPiece = movedFrom[movedFrom.length-1];
             if (movedTo.length == 0 || movedPiece < movedTo[movedTo.length-1]) {
                 movedTo.push(movedPiece);
                 movedFrom.pop(movedPiece);
+                switch(movedTo) {
+                    case stacks.a:
+                        switch(movedPiece) {
+                            case 1:
+                                block_bc1.style.visibility = 'visible';
+                                break;
+                            case 2:
+                                block_bc2.style.visibility = 'visible';
+                                break;
+                            case 3:
+                                block_bc3.style.visibility = 'visible';
+                                break;
+                            case 4:
+                                block_bc4.style.visibility = 'visible';
+                                break;
+                        }
+                        break;
+                    case stacks.b:
+                        switch(movedPiece) {
+                            case 1:
+                                block_bc1.style.visibility = 'visible';
+                                break;
+                            case 2:
+                                block_bc2.style.visibility = 'visible';
+                                break;
+                            case 3:
+                                block_bc3.style.visibility = 'visible';
+                                break;
+                            case 4:
+                                block_bc4.style.visibility = 'visible';
+                                break;
+                        }
+                        break;
+                    case stacks.c:
+                        switch(movedPiece) {
+                            case 1:
+                                block_bc1.style.visibility = 'visible';
+                                break;
+                            case 2:
+                                block_bc2.style.visibility = 'visible';
+                                break;
+                            case 3:
+                                block_bc3.style.visibility = 'visible';
+                                break;
+                            case 4:
+                                block_bc4.style.visibility = 'visible';
+                                break;
+                        }
+                        break;
+                }
                 checkForWin();
-                console.log("Piece moved")
+            } else {
+                console.log("You cant move a bigger piece on a smaller piece");
+            }
         } else {
-            console.log("You cant move a bigger piece on a smaller piece");
+            console.log("There's nothing to move on that stack!");
         }
     } else {
-        console.log("There's nothing to move on that stack!");
+        startStack = clickedStack;
     }
 }
 
@@ -45,32 +100,33 @@ class BuildBlocks {
     addToDom() {
         const block = document.createElement('p');
         block.classList.add('blocks');
+        block.id = 'block_'+this.parentId;
         block.style.width = this.width+'%';
         block.style.height = '50px';
         block.style.backgroundColor = this.backgroundColor;
-        const parent = document.getElementById(this.parentId)
+        const parent = document.getElementById(this.parentId);
         parent.appendChild(block);
     }
 }
 const block1 = new BuildBlocks({
       backgroundColor: '#00f9ff',
-      width: 25, 
-      parentId: 'bc11'
+      width: 25,
+      parentId: 'bc1'
 });
 const block2 = new BuildBlocks({
       backgroundColor: '#FFFF00',
-      width: 50, 
-      parentId: 'bc12'
+      width: 50,
+      parentId: 'bc2'
 });
 const block3 = new BuildBlocks({
-      backgroundColor: '#00FF00', 
-      width: 75, 
-      parentId: 'bc13'
+      backgroundColor: '#00FF00',
+      width: 75,
+      parentId: 'bc3'
 });
 const block4 = new BuildBlocks({
-      backgroundColor: '#FF0066', 
-      width: 100, 
-      parentId: 'bc14'
+      backgroundColor: '#FF0066',
+      width: 100,
+      parentId: 'bc4'
 });
 
 const button = document.querySelector('button');
