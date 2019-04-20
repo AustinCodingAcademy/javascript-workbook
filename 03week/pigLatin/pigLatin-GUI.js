c15818303a38d63ab70598866962b82a4d6b5ae2
Kelly T Nguyen <Kelly.Nguyen@medspring.local>
2019-04-20 03:11:02 -0500

    saving just in case

--- 03week/pigLatin/pigLatin-GUI.js
+++ 03week/pigLatin/pigLatin-GUI.js
@@ -1,17 +1,16 @@
 'use strict';
 
-const assert = require('assert');
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-
 function pigLatin(word) {
-  let vowels = ['a', 'e', 'i', 'o', 'u'],
-    result = word.split('');
+  if(event.key === 'Enter') {
+  
+  word = word.value;
+  word = word.toLowerCase();
+  word = word.trim();
 
+  let vowels = ['a', 'e', 'i', 'o', 'u'],
+  result = word.split('');
+
+  
   if (vowels.includes(word.charAt(0))) {
     return word += 'yay';
   } else {
@@ -24,47 +23,17 @@ function pigLatin(word) {
       }
     }
   }
-}
 
-function pigLatin(word){
+function pigLatin(word) {
   return word.split(' ')
-    .map(function(w){return w.slice(1) + w.slice(0, 1) + 'ay'})
-    .join(' ');
+  .map(function(w){return w.slice(1) + w.slice(0, 1) + 'ay'})
+  .join(' ');
+  }
 }
 
-
-
-function getPrompt() {
-  rl.question('word ', (answer) => {
-    console.log( pigLatin(answer) );
-    getPrompt();
-  });
+let answer = result.join(' ');
+let inputBox = document.getElementById('text');
+inputBox.innerHTML = answer;
+return answer;
 }
-
-// Tests
-
-if (typeof describe === 'function') {
-
-  describe('#pigLatin()', () => {
-    it('should translate a simple word', () => {
-      assert.equal(pigLatin('car'), 'arcay');
-      assert.equal(pigLatin('dog'), 'ogday');
-    });
-    it('should translate a complex word', () => {
-      assert.equal(pigLatin('create'), 'eatecray');
-      assert.equal(pigLatin('valley'), 'alleyvay');
-    });
-    it('should attach "yay" if word begins with vowel', () => {
-      assert.equal(pigLatin('egg'), 'eggyay');
-      assert.equal(pigLatin('emission'), 'emissionyay');
-    });
-    it('should lowercase and trim word before translation', () => {
-      assert.equal(pigLatin('HeLlO '), 'ellohay');
-      assert.equal(pigLatin(' RoCkEt'), 'ocketray');
-    });
-  });
-} else {
-
-  getPrompt();
-
 }
