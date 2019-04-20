eb6c1ed2eba4d01686f1858a797acfc1d769f2d8
Kelly T Nguyen <Kelly.Nguyen@medspring.local>
2019-04-20 16:29:32 -0500

    saving gui

--- 03week/pigLatin/pigLatin-GUI.js
+++ 03week/pigLatin/pigLatin-GUI.js
@@ -1,60 +1,16 @@
 'use strict';
 
-function pigLatin(word) {
-  if(event.key === 'Enter') {
-  
-  word = word.value;
-  word = word.toLowerCase();
-  word = word.trim();
-
-  let vowels = ['a', 'e', 'i', 'o', 'u'],
-  result = word.split('');
-
-  
-  if (vowels.includes(word.charAt(0))) {
-    return word += 'yay';
-  } else {
-    for (let i = 0; i < word.length; i++) {
-      if (!vowels.includes(word[i])) {
-        result.push(result.shift());
-      } else {
-        result.push('ay');
-        return result.join('');
-      }
-    }
-  }
-
-function pigLatin(word) {
-  return word.split(' ')
-  .map(function(w){return w.slice(1) + w.slice(0, 1) + 'ay'})
-  .join(' ');
-  }
-}
-
-let answer = result.join(' ');
-let inputBox = document.getElementById('text');
-inputBox.innerHTML = answer;
-return answer;
-}
-}
-
-
-
-
-// 'use strict';
-
-// const assert = require('assert');
-// const readline = require('readline');
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
-
-
 // function pigLatin(word) {
-//   let vowels = ['a', 'e', 'i', 'o', 'u'],
-//     result = word.split('');
+//   if(event.key === 'Enter') {
+  
+//   word = word.value;
+//   word = word.toLowerCase();
+//   word = word.trim();
 
+//   let vowels = ['a', 'e', 'i', 'o', 'u'],
+//   result = word.split('');
+
+  
 //   if (vowels.includes(word.charAt(0))) {
 //     return word += 'yay';
 //   } else {
@@ -67,19 +23,62 @@ return answer;
 //       }
 //     }
 //   }
-// }
 
-// function pigLatin(word){
 //   return word.split(' ')
-//     .map(function(w){return w.slice(1) + w.slice(0, 1) + 'ay'})
-//     .join(' ');
+//   .map(function(w){return w.slice(1) + w.slice(0, 1) + 'ay'})
+//   .join(' ');
+//   }
+// }
+
+// let answer = result.join(' ');
+// let inputBox = document.getElementById('text');
+// inputBox.innerHTML = answer;
+// return answer;
+// }
 // }
 
 
 
-// function getPrompt() {
-//   rl.question('word ', (answer) => {
-//     console.log( pigLatin(answer) );
-//     getPrompt();
-//   });
-// }
+
+'use strict';
+
+const assert = require('assert');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+function pigLatin(word) {
+  let vowels = ['a', 'e', 'i', 'o', 'u'],
+    result = word.split('');
+
+  if (vowels.includes(word.charAt(0))) {
+    return word += 'yay';
+  } else {
+    for (let i = 0; i < word.length; i++) {
+      if (!vowels.includes(word[i])) {
+        result.push(result.shift());
+      } else {
+        result.push('ay');
+        return result.join('');
+      }
+    }
+  }
+}
+
+function pigLatin(word){
+  return word.split(' ')
+    .map(function(w){return w.slice(1) + w.slice(0, 1) + 'ay'})
+    .join(' ');
+}
+
+
+
+function getPrompt() {
+  rl.question('word ', (answer) => {
+    console.log( pigLatin(answer) );
+    getPrompt();
+  });
+}
