ba53788f55b89c6448f988910215cb4fd5e4ddd0
srajbhandari <srajbhandari@contractor.ea.com>
2019-04-20 22:50:23 -0500

    Array Of Cars Exercise

--- 03week/ArrayOfCars.js
+++ 03week/ArrayOfCars.js
@@ -8,22 +8,6 @@ let more_cars = ["Chevy", "Saab", "Tesla", "Honda"];
 let totalCars = cars.concat(more_cars);
 console.log("NEW TOTAL CARS:", totalCars);
 
-/*
-NOTE
-indexOf() function is match first occurance, LastindexOf() function is match last occurance
-Both function are using for finding index in String or Array;
-sample code with String operation
-
-var state = "abcdtmpefgtmp";
-var temp =state.indexOf("tmp"); //match first occurance
-console.log(temp);
-//>4
-
-var temp =state.lastIndexOf("tmp"); //match last occurance
-console.log(temp);
-//>10
-*/
-
 // 3.i indexOf() Honda item
 console.log("INDEX OF HONDA CAR:", totalCars.indexOf("Honda"));
 
@@ -36,7 +20,7 @@ console.log("STRING OF CARS: ", stringOfCars);
 
 // 5. Use the split method to convert stringOfCars back intro an array called totalCars.
 totalCars = stringOfCars.split(",");
-console.log("TOTAL CARS: ", totalCars);
+console.log("TOTAL CARS IN ARRAY: ", totalCars);
 
 // 6. Use the reverse method to create an array carsInReverse which is the array totalCars in reverse.
 let carsInReverse = totalCars.reverse();
@@ -46,37 +30,46 @@ console.log("CARS IN REVERSE: ", carsInReverse);
 // Use the sort method to put the array carsInReverse into alphabetical order.
 totalCars = carsInReverse.sort();
 console.log("CARS SORTED ALPHABETICALLY: ", totalCars);
-// Based on the types of cars you used, predict which item in the array should be at index 0.
-// Use the following code to confirm or reject your prediction:
-// alert(carsInReverse.indexOf('yourPrediction'));
-
 
 // 8. slice
 // Use the slice method to remove Ford and Honda from the carsInReverse array and move them into a new array called removedCars.
-// let carsInReverse1 = totalCars.reverse();
-// console.log("CARS IN REVERSE: ", carsInReverse1);
-// let removedCars = carsInReverse1.slice(4, 6);
-// console.log("REMOVED CARS: ", removedCars);
-
 
 carsInReverse = totalCars.reverse();
 console.log("CARS IN REVERSE: ", carsInReverse);
 
 let removedCars = carsInReverse.slice(3, 5);
-console.log("REMOVED CARS: ",removedCars);
+console.log("REMOVED CARS: ", removedCars);
 
-// // 9. splice
-// // Use the splice method to remove the 2nd and 3rd items in the array carsInReverse and add Ford and Honda in their place.
-// // NOTE: READ ABOUT SPLICE..................................
-// let carsInReverse2 = totalCars.reverse();
-// console.log("REMOVE 2ND AND 3RD CARS & ADD FORD & HONDA: ", carsInReverse2.splice(4, 6, "Ford", "Honda"));
+// 9. splice
+// Use the splice method to remove the 2nd and 3rd items in the array carsInReverse and add Ford and Honda in their place.
+let spliceCars = carsInReverse.splice(2, 2, "Ford", "Honda");
+console.log("NEW CARS ADDED: ", spliceCars);
+console.log(carsInReverse);
 
-// // Example
-// var fruits = ["Banana", "Orange", "Apple", "Mango"];
-// fruits.splice(0, 0, "Lemon", "Kiwi");
-// console.log(fruits);
+//READ MORE ABOUT SPLICE
 
+// 10. push
+// Use the push method to add the types of cars that you removed using the splice method to the carsInReverse array.
+console.log("PUSH NEW CAR:", carsInReverse.push("Ford"));
+console.log(carsInReverse);
 
+// 11 pop
+// Use the pop method to remove and console.log the last item in the array carsInReverse.
+console.log(carsInReverse.pop());
 
+// 12 shift
+// Use the shift method to remove and console.log the first item in the array carsInReverse.
+console.log(carsInReverse.shift());
 
+// 13 unshift
+// Use the unshift method to add a new type of car to the array carsInReverse.
+console.log(carsInReverse.unshift('Kia'));
+console.log(carsInReverse);
 
+// 14 forEach
+// Create an array called numbers with the following items: 23, 45, 0, 2 Write code that will add 2 to each item in the array numbers.
+let numbers = [23, 45, 0, 2];
+numbers.forEach(function(number){
+    numbers = number + 2;
+    console.log(numbers);
+}) 
