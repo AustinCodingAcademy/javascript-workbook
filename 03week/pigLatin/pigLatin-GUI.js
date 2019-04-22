21ba7b89428bb24e16f5e1740c20951977ecc4a9
Kelly T Nguyen <Kelly.Nguyen@medspring.local>
2019-04-21 23:04:06 -0500

    final piglatingui

--- 03week/pigLatin/pigLatin-GUI.js
+++ 03week/pigLatin/pigLatin-GUI.js
@@ -3,27 +3,30 @@
 function pigLatin(word) {
   if(event.key === 'Enter') {
 
-  word = word.value;
-  word = word.toLowerCase();
-  word = word.trim();
+    word = word.value;
+    word = word.trim().toLowerCase();
 
-  let vowels = ['a', 'e', 'i', 'o', 'u'],
-  result = word.split('');
+    let vowels = ['a', 'e', 'i', 'o', 'u'],
+      result = word.split('');
 
-  if(vowels.includes(word.charAt(0))) {
-  return word += 'yay';
-  } else {
-    for (let i = 0; i < word.length; i++) {
-      if (!vowels.includes(word[i])) {
-        result.push(result.shift());
-        let finish = result.join('');
-        document.getElementById('text').innerHTML = finish;
-      } else {
-        result.push('ay');
-        let answer = result.join('');
-        document.getElementById('text').innerHTML = answer;
+    let firstChar = result[0];
+
+    if (vowels.includes(word.charAt(0))) {
+      result.shift();
+      result.push(firstChar);
+      let answer = result.join('');
+      document.getElementById('text').innerHTML = answer += 'yay';
+    } else {
+      for (let i = 0; i < word.length; i++) {
+        if (!vowels.includes(word[i])) {
+          result.shift();
+          result.push(word[i]);
+        } else {
+          result.push('ay');
+          let answer = result.join('');
+          document.getElementById('text').innerHTML = answer;
+        }
       }
     }
   }
-}
 }
