6eb0a5432f7ce8e254bd2537fde72b83812e3a3f
eamoses <e.a.moses@gmail.com>
2019-04-21 19:56:45 -0500

    whitebaording

---
+++ 04week/class/practice1.js
@@ -0,0 +1,34 @@
+// Return unsorted scores in a descending sorted array.
+
+/*Example*/
+const unsortedScores = [37, 89, 41, 65, 91, 53];
+
+// function sortedScores(n){
+//     let y = n.sort();
+//     let x = y.reverse();
+//     console.log(x);
+// }
+
+function sortedScores(n) {
+    let change = false;
+    for (let index = 0; index < n.length; index++) {
+        const element = n[index];
+        const nextelement = n[index+1]
+        if(element > nextelement) {
+            n[index] = nextelement;
+            n[index+1] = element;
+            change = true;
+        }
+    }
+    console.log(n);
+    if(change){
+        sortedScores(n);
+    } else {
+        return n;
+    }
+}
+
+const sortedArray = sortedScores(unsortedScores);
+console.log(sortedArray);
+
+  // [91, 89, 65, 53, 41, 37]
---
+++ 04week/class/practice2.js
@@ -0,0 +1,8 @@
+console.log(0.1 + 0.2 == 0.3);
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.3);
+console.log(0.2 + 0.4);
+console.log(0.4 + 0.4);
+console.log(0.5 + 0.5);
+// false
+// 0.30000000000000004
