660dbbb2e68e55e3099a9415ed9f18bc7ddd67ed
eamoses <e.a.moses@gmail.com>
2019-04-24 16:05:12 -0500

    saving regex

--- 04week/regex.js
+++ 04week/regex.js
@@ -1,3 +1,13 @@
 // let re = /p+l/;
 // const re = new RegEx('p+l')
-let expression = /^hack/;
+let firstPart = /^austin/;
+let lastPart = /t!$/;
+let fullWord = /^austin coding academy$/;
+let middle = /a+c/;
+
+let startingWord = "austin coding academy is the best!";
+let section = startingWord.search(/coding/i);
+console.log(section);
+let replaceIt = startingWord.replace("coding", "running");
+console.log(replaceIt);
+
