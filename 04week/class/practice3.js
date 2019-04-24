ff9ae66775191e0fde5f03a4322e9534a53ad4a6
eamoses <e.a.moses@gmail.com>
2019-04-24 14:14:01 -0500

    save practice

---
+++ 04week/class/practice3.js
@@ -0,0 +1,9 @@
+let array = [3,2,1];
+
+function reverse(a) {
+    let x = a.reverse();
+    return x;
+}
+
+const reversal = reverse(array);
+console.log(reversal);
--- 04week/loop.js
+++ 04week/loop.js
@@ -1,35 +1,35 @@
-// http://awanderingreader.com/javascript-under-the-hood-building-our-own-foreach-and-map-functions/
+// // http://awanderingreader.com/javascript-under-the-hood-building-our-own-foreach-and-map-functions/
 
-// Challenge 3
-function map(array, callback) {
-    var newArr = [];
-    for (var i = 0; i < array.length; i++) {
-      newArr.push(callback(array[i]));
-    }
-    return newArr;
-}
-  
-function multiplyByTwo(num) {
-return num * 2;
-}
-console.log(map([1, 2, 3], multiplyByTwo));
-/////////////////////////////
-
-// Challenge 4
-function forEach(array, callback) {
-	for(let i = 0; i < array.length; i++) {
-		callback(array[i]);
-	}
-}
-// //Extension 1
-// function mapWith(array, callback) {
-// 	return forEach(array, callback)
+// // Challenge 3
+// function map(array, callback) {
+//     var newArr = [];
+//     for (var i = 0; i < array.length; i++) {
+//       newArr.push(callback(array[i]));
+//     }
+//     return newArr;
 // }
-// see for yourself if your forEach works!
-var alphabet = '';
-var letters = ['a', 'b', 'c', 'd'];
+  
+// function multiplyByTwo(num) {
+// return num * 2;
+// }
+// console.log(map([1, 2, 3], multiplyByTwo));
+// /////////////////////////////
 
-forEach(letters, function(char) {
-  alphabet += char;
-});
-console.log(alphabet);   //prints 'abcd'
+// // Challenge 4
+// function forEach(array, callback) {
+// 	for(let i = 0; i < array.length; i++) {
+// 		callback(array[i]);
+// 	}
+// }
+// // //Extension 1
+// // function mapWith(array, callback) {
+// // 	return forEach(array, callback)
+// // }
+// // see for yourself if your forEach works!
+// var alphabet = '';
+// var letters = ['a', 'b', 'c', 'd'];
+
+// forEach(letters, function(char) {
+//   alphabet += char;
+// });
+// console.log(alphabet);   //prints 'abcd'
