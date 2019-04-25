880f57ee531aa63df590f150b2a1837efbf44d1b
Mschweda <tahoe27@gmail.com>
2019-04-25 17:21:50 -0500

    loop js almost complete

---
+++ 04week/loop.js
@@ -0,0 +1,64 @@
+'use strict';
+
+//Use a do...while loop to console.log the numbers from 1 to 1000.
+
+// let counter = 1;
+// while(counter <= 1000) {
+//     console.log(counter);
+//     counter++;
+// }
+
+let counter = " ";
+let  i = 0;
+do {
+    i = i + 1;
+    counter = counter + i;
+}   while (i < 1000);
+console.log(counter);
+
+//Create an object (an array with keys and values) called person with the following data:
+
+const person = {
+firstName: "Jane",
+lastName: "Doe",
+birthDate: "Jan 5, 1925",
+gender: "female"
+};
+
+//Use a for...in loop and if statement to console.log the value associated with the key birthDate if the birth year is an odd number.
+
+let text = "";
+let x;
+for (let x in person) {
+    if (x === 'birthdate') {
+       console.log(person[x]) 
+    }
+}
+
+//Create an arrayOfPersons that contains mulitiple objects. You can simply copy/paste the person object you made above multiple times. Feel free to change the values to reflect multiple people you might have in your database.
+
+const arrayOfPersons = [{firstName: "Jane",
+lastName: "Doe",
+birthDate: "Jan 5, 1925",
+gender: "female"}, 
+{firstName: "John",
+lastName: "Smith",
+birthDate: "Jan 1, 1955",
+gender: "male"}
+];
+
+//Use .map() to map over the arrayOfPersons and console.log() their information.
+
+const arrayOfPeople = arrayOfPersons.map(newList => { 
+    return newList;
+});
+console.log(arrayOfPersons);
+
+//Use .filter() to filter the persons array and console.log only males in the array.
+
+const guys =  arrayOfPersons.filter(John => John.firstName === 'John'); 
+    console.log(guys);
+
+//Use .filter() to filter the persons array and console.log only people that were born before Jan 1, 1990.
+
+const oldPeople = arrayOfPersons.filter(birthDate => (birthDate.start < 1999 && birthDate.end > 1900));
