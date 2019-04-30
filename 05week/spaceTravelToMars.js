804326c043960c7556b76fe55432ebae2903ee80
Kelly T Nguyen <Kelly.Nguyen@medspring.local>
2019-04-29 20:50:48 -0500

    prepare for takeoff!

--- 05week/spaceTravelToMars.js
+++ 05week/spaceTravelToMars.js
@@ -10,8 +10,36 @@ let jobTypes = {
 };
 
 // Your code here
+class CrewMember {
+  constructor (name, job, specialSkill, ship) {
+    this.name = name;
+    this.job = job;
+    this.specialSkill = specialSkill;
+    this.ship = ship;
+  }
+  enterShip(ship)  {
+    this.ship = ship;
+    ship.crew.push(this);
+  }
+}
 
-//tests
+class Ship {
+  constructor (name, type, ability) {
+    this.name = name;
+    this.type = type;
+    this.ability = ability;
+    this.crew = [];
+  }
+  missionStatement () {
+    if (this.crew.length === 0) {
+      return `Can't perform a mission yet.`;
+    } else {
+      return this.ability;
+    }
+  }
+}
+
+//Tests
 if (typeof describe === 'function'){
   describe('CrewMember', function(){
     it('should have a name, a job, a specialSkill and ship upon instantiation', function(){
