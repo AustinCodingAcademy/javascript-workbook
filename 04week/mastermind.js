bff0614d3c3dc273bbce1d6510d6df5d96053604
Mikaela Rodriguez <mikrdgz@gmail.com>
2019-04-29 20:27:54 -0500

    finish mastermind

--- 04week/mastermind.js
+++ 04week/mastermind.js
@@ -11,11 +11,13 @@
 
 // Spec 2.2 - Determine correct "letter-locations" Create a variable correctLetterLocations that will record how many correct "letter-locations" were guessed. For instance, a guess of aabc against a solution of deba would yield one correct "letter-location" (b). Set correctLetterLocations equal to 0. In a for loop, iterate over the solutionArray, comparing each index of solutionArray against the same index of guessArray. If the item matches, increment correctLetterLocations, and set that index in solutionArray to null.
 
-// Spec 2.3 - Determine correct "letters" Now that we have nulled the already counted correctLetterLocations, we can see if the guessArray contains any correctLetters that were not in the correct location. Set a variable correctLetters equal to 0, and in a for loop, again iterate over the solutionArray. Using .indexOf, determine if the item at the current index in guessArray appears inside of solutionArray. Save that index in a variable called targetIndex. Now, if targetIndex is greater that -1, increment correctLetters and set the item in solutionArray at that index equal to null.
+// Spec 2.3 - Determine correct "letters" 
+// Now that we have nulled the already counted correctLetterLocations, we can see if the guessArray contains any correctLetters that were not in the correct location. Set a variable correctLetters equal to 0, and in a for loop, again iterate over the solutionArray. Using .indexOf, determine if the item at the current index in guessArray appears inside of solutionArray. Save that index in a variable called targetIndex. Now, if targetIndex is greater that -1, increment correctLetters and set the item in solutionArray at that index equal to null.
 
 // Spec 2.4 - return hint string Using the colors package, return a string that prints out the hints you generated, with correctLetterLocations being red, correctLetters being white, and separated by a hyphen.
 
-// Spec 3 - Add guess and hint to the board Define a var called hint that collects the returned value of generateHint(solution, guess). .push the guess and the hint (as a combined string) into the board.
+// Spec 3 - Add guess and hint to the board 
+// Define a var called hint that collects the returned value of generateHint(solution, guess). .push the guess and the hint (as a combined string) into the board.
 
 // Spec 4 - End the game After 10 incorrect guesses, if the board length equals 10, return 'You ran out of turns! The solution was ' and the solution. Otherwise, return 'Guess again.'.
 
@@ -50,34 +52,45 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateHint(solution, guess) {
+function generateHint(guess) {
   let solutionArray = solution.split("");
-  let guessArray = solution.split("");
+  let guessArray = guess.split("");
+  
   let correctLetterLocations = 0;
 
-  for (let i = 0; i <= solutionArray.length; i++){
-    if(solutionArray[i]==guessArray[i]){
-      
+  for (let i = 0; i < solutionArray.length; i++){
+    if(solutionArray[i] == guessArray[i]){
       correctLetterLocations += 1;
-      solutionArray[i] = null
+      solutionArray[i] = null;
     }
   }
 
+  let correctLetters = 0;
+
+  for (let a = 0; a <= solutionArray.length; a++) {
+    let targetIndex = guessArray.indexOf(solutionArray[a]);
+    if (targetIndex > -1) {
+        correctLetters = correctLetters + 1;
+        solutionArray[a] = null;
+    }
+  }
+
+    return correctLetterLocations + "-" + correctLetters
 }
 
 function mastermind(guess) {
  
-  const solution = ['abcd'];
-
-  if (guess == solution) {
-    console.log("You won!")
+   solution = 'abcd';
+  if (board.length > 10){
+    console.log("You ran out of turns!");
+  } else if (guess == solution) {
+    return ("You guessed it!");
   } else {
-    console.log("Keep guessing...")
-    generateHint(solution, guess);
+    let hint = generateHint(guess);
+    board.push(hint + guess);
+    console.log(board);
+    return ("Guess again.")
   }
-
- // solution = 'abcd'; // Comment this out to generate a random solution
-  // your code here
 }
 
 
