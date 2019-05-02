cde3544d7e36853b46517fb2f85631e658b9e9e4
Vinoth Kumar Nanda Kumar <vinothkumar.nk@gmail.com>
2019-05-01 20:13:06 -0500

    rockpaperscissors final

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -8,11 +8,38 @@ const rl = readline.createInterface({
 });
 
 
-function rockPaperScissors(hand1, hand2) {
+function rockPaperScissors(a, b) {
 
-  // Write code here
+    // Write code here
+    var hand1 = a.toLowerCase().trim();
+    var hand2 = b.toLowerCase().trim();
 
+if (hand1 === hand2)
+return "It's a tie!"
+
+else if ((hand1 == "rock" && hand2 =="scissors") || (hand1 == "scissors" && hand2 == "rock")){
+  if(hand1=="rock")
+  return "Hand one wins!"
+  else 
+  return "Hand two wins!"
 }
+
+else if ((hand1 == "rock" && hand2 =="paper") || (hand1 == "paper" && hand2 == "rock")){
+  if(hand1=="paper")
+  return "Hand one wins!"
+  else 
+  return "Hand two wins!"
+  }
+
+  else if ((hand1 == "paper" && hand2 =="scissors") || (hand1 == "scissors" && hand2 == "paper")){
+    if(hand1=="scissors")
+    return "Hand one wins!"
+    else 
+    return "Hand two wins!"
+  }
+}
+
+
 
 function getPrompt() {
   rl.question('hand1: ', (answer1) => {
