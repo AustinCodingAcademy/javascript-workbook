3afec1f03e87fa4335691e71eb3940db78947fab
Dan Brockman <dbrockman28@gmail.com>
2019-05-01 20:28:23 -0500

    Rock Paper Scissors

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -8,10 +8,26 @@ const rl = readline.createInterface({
 });
 
 
-function rockPaperScissors(hand1, hand2) {
+function rockPaperScissors(a, b) {
 
-  // Write code here
+  var hand1 = a.toLowerCase().trim();
+  var hand2 = b.toLowerCase().trim();
 
+  if (hand1 === hand2) {
+    return "It's a tie!";
+  } else if (hand1 === "rock" && hand2 === "paper") {
+    return "Hand two wins!";
+  } else if (hand1 === "rock" && hand2 === "scissors") {
+    return "Hand one wins!";
+  } else if (hand1 === "paper" && hand2 === "rock") {
+    return "Hand one wins!";
+  } else if (hand1 === "paper" && hand2 === "scissors") {
+    return "Hand two wins!";
+  } else if (hand1 === "scissors" && hand2 === "paper") {
+    return "Hand one wins!";
+  } else if (hand1 === "scissors" && hand2 === "rock") {
+    return "Hand two wins!";
+  }
 }
 
 function getPrompt() {
