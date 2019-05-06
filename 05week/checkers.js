d707f50b2713d6d2c067e620d3de28c438e600e1
masharomanova <mariavromanova@yahoo.com>
2019-05-05 23:05:26 -0500

    checkers not finished

--- 05week/checkers.js
+++ 05week/checkers.js
@@ -8,13 +8,20 @@ const rl = readline.createInterface({
 });
 
 
-function Checker() {
-  // Your code here
+class Checker {
+  constructor(color) {
+    if (color === 'white') {
+      this.symbol = '○';
+    } else {
+      this.symbol = '●';
+    }
+  }
 }
 
 class Board {
   constructor() {
     this.grid = []
+    this.checkers = []
   }
   // method that creates an 8x8 array, filled with null values
   createGrid() {
@@ -37,7 +44,7 @@ class Board {
       for (let column = 0; column < 8; column++) {
         // if the location is "truthy" (contains a checker piece, in this case)
         if (this.grid[row][column]) {
-          // push the symbol of the check in that location into the array
+          // push the symbol of the checker in that location into the array
           rowOfCheckers.push(this.grid[row][column].symbol);
         } else {
           // just push in a blank space
@@ -53,6 +60,41 @@ class Board {
   }
 
   // Your code here
+  createCheckers() {
+    // will create the pieces and adding them to array of checkers
+    for (let row = 0; row < 8; row++) {
+      // loop through board in order to find spots where checkers need to be placed
+      if (row != 3 && row != 4) {
+      for (let column = 0; column < 8; column++) {
+        if (row == 0 || row == 2) {
+          if (column % 2 != 0) {
+            this.checkers.push(new Checker('white'))
+            this.grid[row][column] = new Checker('white')
+          }
+        } else if (row == 1) {
+          if (column % 2 == 0) {
+            this.checkers.push(new Checker('white'))
+            this.grid[row][column] = new Checker('white')
+          }
+        } else if (row == 5 || row == 7) {
+          if (column % 2 == 0) {
+            this.checkers.push(new Checker('black'))
+            this.grid[row][column] = new Checker('black')
+          }
+        } else if (row == 6) {
+          if (column % 2 != 0) {
+            this.checkers.push(new Checker('black'))
+            this.grid[row][column] = new Checker('black')
+          }
+        }
+      }
+    }
+    }
+  }
+
+  selectChecker(row, column) {
+    return this.grid[row][column];
+  }
 }
 
 class Game {
@@ -61,6 +103,12 @@ class Game {
   }
   start() {
     this.board.createGrid();
+    this.board.createCheckers(); // adding pieces before the game starts
+  }
+  moveChecker(whichPiece, toWhere) {
+    let checker = this.board.selectChecker(whichPiece.charAt(0), whichPiece.charAt(1))
+    this.board.grid[whichPiece.charAt(0)][whichPiece.charAt(1)] = null
+    this.board.grid[toWhere.charAt(0)][toWhere.charAt(1)] = checker
   }
 }
 
