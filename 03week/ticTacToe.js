a47297650e5edc66090c09130cdccc4b77681503
Gina Gabriel <aldjounie12@yahoo.com>
2019-05-08 20:49:51 -0500

    stil working on ticTacToe

--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -12,7 +12,31 @@ let board = [
   [' ', ' ', ' ']
 ];
 
+
 let playerTurn = 'X';
+
+// JavaScript
+// function drawBoard()
+// {
+//     var parent = document.getElementById("game");
+//     var counter = 1;
+
+//     for (var i = 0; i < 3; i++)
+//     {
+//         var row = document.createElement("tr");
+
+//         for(var x = 0; x < size; x++)
+//         {
+//             var col = document.createElement("td");
+//             col.innerHTML = counter;
+
+//             row.appendChild(col);
+//         }
+//         parent.appendChild(row);
+//     }
+// }'
+
+
 
 function printBoard() {
   console.log('   0  1  2');
@@ -25,10 +49,15 @@ function printBoard() {
 
 function horizontalWin() {
   // Your code here
+  
+if( board[0][0]=='X'&& board[0][1]=='X' && board [0][2]=='X') 
+return true; 
+else return false;
 }
 
 function verticalWin() {
   // Your code here
+  if( board[0][])
 }
 
 function diagonalWin() {
@@ -41,6 +70,13 @@ function checkForWin() {
 
 function ticTacToe(row, column) {
   // Your code here
+  if (playerTurn ==='X'){
+  board[row][column]='X';
+  playerTurn = 'O';
+  } else{
+  board[row][column]='O';
+  playerTurn= 'X';
+}
 }
 
 function getPrompt() {
