5bde03e7d2b64201c2b41b83bca13575acc576c5
Gina Gabriel <aldjounie12@yahoo.com>
2019-05-12 18:51:05 -0500

    rockPaperScissors

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -14,37 +14,38 @@ const rl = readline.createInterface({
  
 
 function rockPaperScissors(hand1, hand2) {
-
-  if ( hand1===hand2) 
-  return "it's a tie"
-   else if (hand1==="rock"&& hand2==="rock")
-   return "it's a tie"
-    else if (hand1==="paper"&& hand2==="paper")
-   return "it's a tie"
-    else if (hand1==="scissors"&& hand2==="scissors")
-   return "it's a tie"
-   else if (hand1==="rock" && hand2==="scissor")
+console.log( hand1)
+hand1 = hand1.toLowerCase().trim();
+hand2 = hand2.toLowerCase().trim();
+  if ( hand1===hand2) {
+  return "It's a tie!"
+  }else if (hand1==="rock"&& hand2==="rock"){
+   return "It's a tie!"
+  }else if (hand1==="paper"&& hand2==="paper"){
+   return "It's a tie!"
+  }else if (hand1==="scissors"&& hand2==="scissors"){
+   return "It's a tie!"
+}else if (hand1==="rock" && hand2==="scissor"){
    return "Hand one wins!"
-   else if (hand1==="rock" && hand2==="paper")
-   return "Hand two Wins!"
-   else if (hand1==="paper" && hand2==="rock")
-   return "Hand one Wins!"
-   else if (hand1==="scissors" && hand2==="rock")
-   return "Hand two Wins! "
-   else if (hand1==="paper" && hand2==="scissors")
-   return "Hand two Wins!"
-   else if (hand1==="rock" && hand2==="scissors")
-   return "Hand one Wins" 
- else if ( hand1==="paper"&& hand2==="rock")
-  return " Hand one Wins"
- else if (hand1==="scissors"&& hand2==="paper")
- return "Hand one Wins"
- 
-   else
-   return "type : rock, paper or Scissors"
+}else if (hand1==="rock" && hand2==="paper"){
+   return "Hand two wins!"
+}else if (hand1==="paper" && hand2==="rock"){
+   return "Hand one wins!"
+}else if (hand1==="scissors" && hand2==="rock"){
+   return "Hand two wins"
+}else if (hand1==="paper" && hand2==="scissors"){
+   return "Hand two wins!"
+}else if (hand1==="rock" && hand2==="scissors"){
+   return "Hand one wins!" 
+} else if ( hand1==="paper"&& hand2==="rock"){
+  return " Hand one wins!"
+}else if (hand1==="scissors"&& hand2==="paper"){
+ return "Hand one wins!"
+ } else{
+   return "Hand two wins!"
 
 }
-
+}
 function getPrompt() {
   rl.question('hand1: ', (answer1) => {
     rl.question('hand2: ', (answer2) => {
@@ -53,39 +54,11 @@ function getPrompt() {
     });
   });
 }
-
-Tests
-function showTime(){
-    var date = new Date();
-    var h = date.getHours();// 0 - 23
-    var h = date.getMinutes();// 0 - 59
-    var h = date.getSeconds();// 0 - 59
-
-    if(h == 0){
-        h = 12;
-    }
-    if(h > 12){
-        h = h - 12;
-        session = "PM";
-    }
-
-    h = (h < 10) ? "0" + h : h;
-    h = (m < 10) ? "0" + m : m;
-    h = (s < 10) ? "0" + s : s;
-
-    var time = h + ":" + m + ":" + 5;
-    document.getElementById("MyClockDisplay").innerText = time;
-    document.getElementById("MyClockDisplay").innerContent = time;
-
-    startTimeOut("showTime, 1000");
-
-    showTime();
-}
-
+   
 
 if (typeof describe === 'function') {
 
-  describe('#rockPaperScissors()', () => {
+  describe('#rockPaperScissors', () => {
     it('should detect a tie', () => {
       assert.equal(rockPaperScissors('rock', 'rock'), "It's a tie!");
       assert.equal(rockPaperScissors('paper', 'paper'), "It's a tie!");
@@ -101,6 +74,12 @@ if (typeof describe === 'function') {
       assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
       assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
     });
+    // this my test//
+    it('should detect spelling errors in user input ', () => {
+      assert.equal(rockPaperScissors('rok', 'paper'), "Please enter a valid word");
+      assert.equal(rockPaperScissors('rock', 'peper'), "Please enter a valid word");
+      assert.equal(rockPaperScissors('ro2k', 'paper'), "Please enter a valid word");
+   })
   });
 } else {
 
