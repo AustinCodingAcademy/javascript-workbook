9ca681553a65ffefd8a2c15e83050f408f4a2dba
Alex Borsbach <alexborsbach@Alexs-MacBook-Pro.local>
2019-05-12 17:29:33 -0500

    passes added tets

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -10,7 +10,7 @@ const rl = readline.createInterface({
 function rockPaperScissors(hand1, hand2) {
   hand1 = hand1.toLowerCase().trim();
   hand2 = hand2.toLowerCase().trim();
-  if (hand1 === hand2) return "It's a tie";
+  if (hand1 === hand2) return "It's a tie!";
   else if (hand1 === "rock" && hand2 === "scissors") return "Hand one wins!";
   else if (hand1 === "rock" && hand2 === "paper") return "Hand two wins!";
   else if (hand1 === "scissor" && hand2 === "rock") return "Hand two wins!";
@@ -18,6 +18,20 @@ function rockPaperScissors(hand1, hand2) {
   else if (hand1 === "paper" && hand2 === "rock") return "Hand one wins!";
   else if (hand1 === "paper" && hand2 === "scissors") return "Hand two wins!";
   else return "Type: rock, paper or scissors";
+}
+
+//new function for new unit test
+function checkInput(hand1, hand2) {
+  if (hand1 === "rock" && hand2 === "scissors") return true;
+  else if (hand1 === "rock" && hand2 === "paper") return true;
+  else if (hand1 === "rock" && hand2 === "rock") return true;
+  else if (hand1 === "paper" && hand2 === "scissors") return true;
+  else if (hand1 === "paper" && hand2 === "rock") return true;
+  else if (hand1 === "paper" && hand2 === "paper") return true;
+  else if (hand1 === "scissors" && hand2 === "scissors") return true;
+  else if (hand1 === "scissors" && hand2 === "rock") return true;
+  else if (hand1 === "scissors" && hand2 === "paper") return true;
+  else return false;
 }
 
 function getPrompt() {
@@ -50,7 +64,8 @@ if (typeof describe === "function") {
     });
     //added test for assignment week 2 day 2.
     it("should check input for rock paper or scissors", () => {
-      assert.equal(rockPaperScissors())
+      rockPaperScissors("rock", "bowl");
+      assert.equal(checkInput("rock", "bowl"), false);
     });
   });
 } else {
