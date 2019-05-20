86b7c7bacc035814fd95f5420e3de12d00531939
Alex Borsbach <alexborsbach@Alexs-MacBook-Pro.local>
2019-05-20 18:21:10 -0500

    Tic Tac Toe GUI final

--- 03week/ticTacToev2.js
+++ 03week/ticTacToev2.js
@@ -1,107 +1,157 @@
-var player;
-var prevPlayer;
 
-function selectX() {
-  player = "X";
-}
+let player = "X";
+let canMove = true;
 
-function selectO() {
-  player = "O";
-}
+const box1 = document.getElementById("0,0");
+const box2 = document.getElementById("0,1");
+const box3 = document.getElementById("0,2");
+const box4 = document.getElementById("1,0");
+const box5 = document.getElementById("1,1");
+const box6 = document.getElementById("1,2");
+const box7 = document.getElementById("2,0");
+const box8 = document.getElementById("2,1");
+const box9 = document.getElementById("2,2");
 
-function horizontalWin() {
-  // Your code here
+// Your code
+
+// checks all possibilities for horizontal win
+const horizontalWin = () => {
   if (
-    document.getElementById("0,0").innerText == document.getElementById("0,1").innerText &&
-    document.getElementById("0,1").innerText == document.getElementById("0,2").innerText 
+    box1.innerText &&
+    box1.innerText === box2.innerText &&
+    box2.innerText === box3.innerText
   ) {
     return true;
   } else if (
-    document.getElementById("1,0").innerText == document.getElementById("1,1").innerText &&
-    document.getElementById("1,1").innerText == document.getElementById("1,2").innerText
+    box4.innerText &&
+    box4.innerText === box5.innerText &&
+    box5.innerText === box6.innerText
   ) {
     return true;
   } else if (
-    document.getElementById("2,0").innerText == document.getElementById("2,1").innerText &&
-    document.getElementById("2,1").innerText == document.getElementById("2,2").innerText
+    box7.innerText &&
+    box7.innerText === box8.innerText &&
+    box8.innerText === box9.innerText
   ) {
     return true;
-  } else return false;
-}
+  } else {
+    return false;
+  }
+};
+// Your code here
 
-function verticalWin() {
-  // Your code here
+// checks all possibilities for vertical wins
+const verticalWin = () => {
   if (
-    document.getElementById("0,0").innerText == document.getElementById("1,0").innerText &&
-    document.getElementById("1,0").innerText == document.getElementById("2,0").innerText
+    box1.innerText &&
+    box1.innerText === box4.innerText &&
+    box4.innerText === box7.innerText
   ) {
     return true;
   } else if (
-    document.getElementById("0,1").innerText == document.getElementById("1,1").innerText &&
-    document.getElementById("1,1").innerText == document.getElementById("2,1").innerText
+    box2.innerText &&
+    box2.innerText === box5.innerText &&
+    box5.innerText === box8.innerText
   ) {
     return true;
   } else if (
-    document.getElementById("0,2").innerText == document.getElementById("1,2").innerText &&
-    document.getElementById("1,2").innerText == document.getElementById("2,2").innerText
+    box3.innerText &&
+    box3.innerText === box6.innerText &&
+    box6.innerText === box9.innerText
   ) {
     return true;
-  } else return false;
-}
+  } else {
+    return false;
+  }
+};
 
-function diagonalWin() {
-  // Your code here
+// Your code here
+
+// checks all possibilties for diagonal wins
+const diagonlaWin = () => {
   if (
-    document.getElementById("0,0").innerText == document.getElementById("1,1").innerText &&
-    document.getElementById("1,1").innerText == document.getElementById("2,2").innerText
+    box1.innerText &&
+    box1.innerText === box5.innerText &&
+    box5.innerText === box9.innerText
   ) {
     return true;
   } else if (
-    document.getElementById("0,2").innerText == document.getElementById("1,1").innerText &&
-    document.getElementById("1,1").innerText == document.getElementById("2,0").innerText
+    box3.innerText &&
+    box3.innerText === box5.innerText &&
+    box5.innerText === box7.innerText
   ) {
     return true;
-  } else return false;
-}
+  } else {
+    return false;
+  }
+};
 
-function checkForWin() {
-  // Your code here
-  if (diagonalWin() || horizontalWin() || verticalWin()) {
-    //This displays that someone won
-    console.log("You won!");
-    //This is checking for win and stops game
+const checkForWin = () => {
+  const announceWinner = document.getElementById("announce-winner");
+  if (diagonlaWin() || horizontalWin() || verticalWin()) {
+    announceWinner.innerHTML = `Player ${player} won!`;
     return true;
   }
   return false;
+};
+
+function checkForDraw() {
+  // evaluates if all boxes have been clicked on 
+  if (
+    box1.innerHTML &&
+    box2.innerHTML &&
+    box3.innerHTML &&
+    box4.innerHTML &&
+    box5.innerHTML &&
+    box6.innerHTML &&
+    box7.innerHTML &&
+    box8.innerHTML &&
+    box9.innerHTML 
+  ) {
+    const announceWinner = document.getElementById("announce-winner");
+    announceWinner.innerHTML = "Draw!";
+    return true;
+  }
+
+  return false;
 }
 
-var counter = 0;
-
-function ticTacToe(el) {
-  counter ++;
-  console.log(counter)
-  if (!player) {
-    alert("Select a player");
-  } else if (prevPlayer === player) {
-    alert("Please select different player!");
+// switches players after each move
+const switchPlayer = () => {
+  if (player === "X") {
+    player = "O";
   } else {
-    el.innerText = player;
-    prevPlayer = player;
+    player = "X";
   }
-  if (counter > 4){
-    if (checkForWin()) {
-      console.log("player " + player + " wins");
-    } else {
-      console.log("keep playing");
-    }
+};
+
+function ticTacToe(id) {
+  if (!canMove) return;
+  const square = document.getElementById(id);
+  // if square has content do nothing
+  if (square.innerHTML) return 
+  square.innerHTML = player;
+  if (checkForWin() || checkForDraw()) {
+    gameOver();
   }
+  switchPlayer(); 
 }
 
-// function ticTacToe(event) {
-//   let dom = document.getElementById("cell"+event.target.value)
-//   console.log(dom)
-//   arr = event.target.value.split(',')
-//   let row=arr[0]
-//   let col=arr[1]
-//   console.log(arr)
-// }
+function gameOver() {
+  let player = "X";
+  // stops player from picking another square
+  canMove = false;
+}
+
+function startOver() {
+  const announceWinner = document.getElementById("announce-winner");
+  announceWinner.innerHTML = "";
+  canMove = true;
+  player = "X";
+  clearBoard();
+}
+
+function clearBoard() {
+  box1.innerHTML = box2.innerHTML = box3.innerHTML = box4.innerHTML = box5.innerHTML = box6.innerHTML = box7.innerHTML = box8.innerHTML = box9.innerHTML =
+    "";
+}
