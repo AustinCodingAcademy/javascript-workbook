7db3a809eaf2371c55f295a3b5547f4a1f00a335
aCodeReader <jacobs0408@yahoo.com>
2019-05-25 13:19:46 -0500

    playableMM

--- 04week/mastermind.js
+++ 04week/mastermind.js
@@ -1,19 +1,24 @@
-'use strict';
+"use strict";
 
-const assert = require('assert');
-const readline = require('readline');
+const assert = require("assert");
+const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
 let board = [];
-let solution = '';
-let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+let solution = "";
+let letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+function ReStartGame(gameStart){
+  board.length = 0
+}
+
 
 function printBoard() {
   for (let i = 0; i < board.length; i++) {
-    console.log(board[i]);
+    console.log("guess number " + (i + 1) + " **** " + board[i] + " ****");
   }
 }
 
@@ -29,55 +34,48 @@ function getRandomInt(min, max) {
 }
 
 function generateHint(guess) {
-let correctLetterLocations = 0 , correctLetters = 0;
-let solutionArray = solution.split('');
-let guessArray = guess.split('');
-guessArray.forEach((item,index) =>
-{ 
-  if (item === solutionArray[index])
-  {
-    correctLetterLocations +=1;
-    solutionArray[index] = '';
-    guessArray[index] = '';
-  }
-})
-guessArray.forEach((item,index) =>
-{ 
-  if ( item !==''){
-  let findIndex = solutionArray.indexOf(item);
-    if (findIndex >-1)
-    {
-      correctLetters+=1;
-      solutionArray[findIndex] = '';
-      guessArray[index] = '';
+  let correctLetterLocations = 0,
+    correctLetters = 0;
+  let solutionArray = solution.split("");
+  let guessArray = guess.split("");
+  guessArray.forEach((item, index) => {
+    if (item === solutionArray[index]) {
+      correctLetterLocations += 1;
+      solutionArray[index] = "";
+      guessArray[index] = "";
     }
-  }
-})
-return `${correctLetterLocations}-${correctLetters}`
+  });
+  guessArray.forEach((item, index) => {
+    if (item !== "") {
+      let findIndex = solutionArray.indexOf(item);
+      if (findIndex > -1) {
+        correctLetters += 1;
+        solutionArray[findIndex] = "";
+        guessArray[index] = "";
+      }
+    }
+  });
+  console.log(`${correctLetterLocations}-${correctLetters}`);
+  return `${correctLetterLocations}-${correctLetters}`;
 }
 
 function mastermind(guess) {
-  solution = 'abcd'; // Comment this out to generate a random solution
+  board.push(guess);
+  solution = "abcd"; // Comment this out to generate a random solution
   // your code here
-let hint = generateHint(guess); 
-
-
-if (solution == guess){
-  return 'You guessed it!'
-}
-  if(solution !== guess){
-
-    return  board.length++
+  let hint = generateHint(guess);
+  if (board.length >= 10) {
+    console.log('FAIL')
+    return ReStartGame;
   }
-  else (board.length > 10)
-    return ('FAIL')
-  
-
+  if (solution == guess) {
+    console.log("You guessed it!");
+    return "You guessed it!";
+  }
 }
-
 
 function getPrompt() {
-  rl.question('guess: ', (guess) => {
+  rl.question("guess: ", guess => {
     mastermind(guess);
     printBoard();
     getPrompt();
@@ -86,30 +84,27 @@ function getPrompt() {
 
 // Tests
 
-if (typeof describe === 'function') {
-  solution = 'abcd';
-  describe('#mastermind()', () => {
-    it('should register a guess and generate hints', () => {
-      mastermind('aabb');
+if (typeof describe === "function") {
+  solution = "abcd";
+  describe("#mastermind()", () => {
+    it("should register a guess and generate hints", () => {
+      mastermind("aabb");
       assert.equal(board.length, 1);
     });
-    it('should be able to detect a win', () => {
-      assert.equal(mastermind(solution), 'You guessed it!');
+    it("should be able to detect a win", () => {
+      assert.equal(mastermind(solution), "You guessed it!");
     });
   });
 
-  describe('#generateHint()', () => {
-    it('should generate hints', () => {
-      assert.equal(generateHint('abdc'), '2-2');
+  describe("#generateHint()", () => {
+    it("should generate hints", () => {
+      assert.equal(generateHint("abdc"), "2-2");
     });
-    it('should generate hints if solution has duplicates', () => {
-      assert.equal(generateHint('aabb'), '1-1');
+    it("should generate hints if solution has duplicates", () => {
+      assert.equal(generateHint("aabb"), "1-1");
     });
-
   });
-
 } else {
-
   generateSolution();
   getPrompt();
 }
