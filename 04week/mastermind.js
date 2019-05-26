ef6ebf867897f1279539d46d43a382d48b36b87e
Alex Borsbach <alexborsbach@Alexs-MacBook-Pro.local>
2019-05-26 10:22:05 -0500

    mastermind 2nd upload

--- 04week/mastermind.js
+++ 04week/mastermind.js
@@ -12,16 +12,20 @@ let board = [];
 let solution = "";
 let letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
+//generates game bord for terminal game
 function printBoard() {
-  for (let i = 0; i < board.length; i++) {
-    console.log(board[i]);
-  }
+  console.log(board)
+  // for (let i = 0; i < board.length; i++) {
+  //   console.log(board[i]);
+  // }
 }
-
+//generates a random code to be solved for
 function generateSolution() {
-  for (let i = 0; i < 4; i++) {
-    const randomIndex = getRandomInt(0, letters.length);
-    solution += letters[randomIndex];
+  if (solution == ""){
+    for (let i = 0; i < 4; i++) {
+      const randomIndex = getRandomInt(0, letters.length);
+      solution += letters[randomIndex];
+    }
   }
 }
 
@@ -29,50 +33,65 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateHint(guess) {
+const generateHint = (guess) => {
   // your code here
-  let correctLetterLocations = 0,
-    correctLetters = 0;
+  let correctLetterLocations = 0;
+  let correctLetters = 0;
   let solutionArray = solution.split("");
   let guessArray = guess.split("");
   // search letter same position
   guessArray.forEach((item, index) => {
     if (item === solutionArray[index]) {
       correctLetterLocations += 1;
-      solutionArray[index] = "";
+      solutionArray[index] = null;
       guessArray[index] = "";
     }
   });
   guessArray.forEach((item, index) => {
     if (item !== "") {
-      let findIndex = solutionArray.indexOf(item);
-      if (findIndex > -1) {
+      let targetIndex = solutionArray.indexOf(item);
+      if (targetIndex > -1) {
         correctLetters += 1;
-        solutionArray[findIndex] = "";
-        guessArray[index] = "";
+        solutionArray[targetIndex] = null;
+        // guessArray[index] = "";
       }
     }
   });
-  return `${correctLetterLocations}-${correctLetters}`;
+  // console.log(correctLetterLocations + '-' + correctLetters)
+  return `${correctLetterLocations} - ${correctLetters}`;
 }
 
 function mastermind(guess) {
-  solution = "abcd"; // Comment this out to generate a random solution
-  // your code here
+  checkForWin(guess, solution);
   let hint = generateHint(guess);
-  board.push[`${guess} ${hint}`];
+  let hintGuess = `${hint} - ${guess}`;
+  board.push(hintGuess);
+}
+
+function checkForWin(guess) {
+  if (guess === solution){
+    console.log("You guessed it!")
+    return "You guessed it!";
+  }
+    else if (board.length == 9){
+    console.log("You Lose, the correct answer was " + solution)
+  } else {
+    console.log("Guess again")
+  }
 }
 
 function getPrompt() {
   rl.question("guess: ", guess => {
+    generateSolution();
     mastermind(guess);
     printBoard();
     getPrompt();
   });
 }
 
+// getPrompt();
 // Tests
-//check for win or not
+// check for win or not
 //return value, increase hint to board
 if (typeof describe === "function") {
   solution = "abcd";
@@ -88,13 +107,13 @@ if (typeof describe === "function") {
 
   describe("#generateHint()", () => {
     it("should generate hints", () => {
-      assert.equal(generateHint("abdc"), "2-2");
+      assert.equal(generateHint("abdc"), "2 - 2");
     });
     it("should generate hints if solution has duplicates", () => {
-      assert.equal(generateHint("aabb"), "1-1");
+      assert.equal(generateHint("aabb"), "1 - 1");
     });
   });
 } else {
   generateSolution();
   getPrompt();
-}
+};
