d3d3a5b4b12894e966a24f22d16187ff1895d6f0
Casey Calkins <c.angelcalkins@outlook.com>
2019-06-18 19:57:23 -0500

    Week 1 Day 1 assignment

---
+++ 01week/datatypes.js
@@ -0,0 +1,35 @@
+let date = new Date();
+console.log(date);
+
+let myNum = 10;
+let numToString = myNum.toString();
+console.log(numToString);
+
+let myText = '100';
+let textToString = parseInt(myText, 10);
+console.log(textToString);
+
+function getType(dataType) {
+    console.log(typeof dataType);
+}
+
+function getSum(num1, num2) {
+    let sumOfNums = num1 + num2;
+    console.log(sumOfNums);
+}
+
+if (5 > 1 && 1 < 5) {
+    console.log('This is true');
+} else {
+    console.log('This is false');
+}
+
+if (5 > 1 || 1 > 5) {
+    console.log('5 is greater than 1');
+} else {
+    console.log('1 is not greater than 5');
+}
+
+if (1 > 5 && 5 < 1) {
+    console.log('Both things are not true');
+}
