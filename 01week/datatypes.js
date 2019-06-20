6a387abb281fd781e2144c0aa08082c39a3fb4cd
Urpiccha <ur.picha.hartman@gmail.com>
2019-06-20 14:05:53 -0500

    Data Types

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -1,1 +1,71 @@
-console.log("Hail Satan!");
+// Write a JavaScript program to display the current day and time.
+
+	var today = new Date();
+	var date =
+		today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+	var time =
+		today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date + "" + time;
+    
+// Write a JavaScript program to convert a number to a string.
+    
+    var num = 666;
+	var n = num.toString();
+    }
+    
+// Write a JavaScript program to convert a string to the number.
+
+    let str = 100
+    str * 1;
+
+// Write a JavaScript program that takes in different datatypes and prints out whether they are a:
+
+// Boolean
+
+var myBoo = true;
+var myBool = false;
+
+// Null
+
+var noValue = null
+
+// Undefined
+
+var huh = undefined
+
+// Number
+
+var nummer = 666
+
+// NaN
+
+var dogTail = NaN
+
+// String
+
+var hailSatan = "Hail Satan, Hail Knowledge"
+
+// Write a JavaScript program that adds 2 numbers together.
+
+var x = 1
+var y = 2
+var z = x + y
+
+
+// Write a JavaScript program that runs only when 2 things are true.
+
+let a = 3;
+let b = 5;
+if(a>0 && b>0) {
+}
+
+// Write a JavaScript program that runs when 1 of 2 things are true.
+
+let a = 3;
+let b = 5;
+if(a>0 || b>7) {  
+}
+
+// Write a JavaScript program that runs when both things are not true.
+
+if(!(a>0 && b>7))
