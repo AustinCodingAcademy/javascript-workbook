160c373913e95075bdef7f3a14495e8278391418
Casey Calkins <c.angelcalkins@outlook.com>
2019-06-20 20:39:06 -0500

    Commiting rockPaperScissors project.

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -9,9 +9,18 @@ const rl = readline.createInterface({
 
 
 function rockPaperScissors(hand1, hand2) {
+  hand1 = hand1.trim();
+  hand1 = hand1.toLowerCase();
+  hand2 = hand2.trim();
+  hand2 = hand2.toLowerCase();
 
-  // Write code here
-
+  if (hand1 == hand2) {
+    return("It\'s a tie!");
+  } else if (hand1 == 'rock' && hand2 == 'scissors' || hand1 == 'paper' && hand2 == 'rock' || hand1 == 'scissors' && hand2 == 'paper') {
+    return('Hand one wins!');
+  } else {
+    return('Hand two wins!');
+  }
 }
 
 function getPrompt() {
