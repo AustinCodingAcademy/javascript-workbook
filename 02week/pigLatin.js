7b2a129cdb7882cc3165755b1189c3aae4cd7578
grantkee <grant.d.kee@gmail.com>
2019-07-02 22:00:08 -0500

    final final version for sure ready to be graded

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -1,5 +1,12 @@
 'use strict';
 
+const assert = require('assert');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 let finalPhrase = [];
 
 function pigLatin(word) {
