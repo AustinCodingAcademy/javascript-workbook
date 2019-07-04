54226b56e9e3aa8b3f907cfbabd46ffff23b06b5
State Tyler <statetyler@State-Tylers-MacBook-Pro.local>
2019-07-03 19:28:30 -0500

    Pig latin final

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -10,8 +10,51 @@ const rl = readline.createInterface({
 
 function pigLatin(word) {
 
-  
+	// scrub the word and convert word to arry
+	word = word.trim().toLowerCase().split('');
 
+	// vowles arry
+	let vowels = [ 'a', 'e', 'i', 'o', 'u' ];
+
+	// newWord arry
+	let newWord = [];
+
+	// check if first letter is a vowel
+    if ( vowels.includes(word[0]) ) {
+
+        // make the newWord, the word but not as an arry
+        newWord = word.join('');
+
+        // add our pig latin
+        newWord += "yay";
+
+    } else {
+
+        // loop through the word arry
+        for ( let i = 0; i <= word.length; i++ ) {
+
+            // if the word contains a vowel
+            if ( !(vowels.includes(word[i]) ) ) {
+
+                // add consonants to new word
+                newWord.push(word[i]);
+
+            } else {
+
+                // slice the word from the vowel to the end, concat the newWord, and join it to a string
+				newWord = word.slice( i, word.length ).concat(newWord).join('');
+
+                // add our pig latin
+                newWord += "ay";
+
+                // break from loop
+				break;
+				
+            }
+        }
+    }
+        
+    return newWord;
 }
 
 
