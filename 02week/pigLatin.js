a896fc0ea92df367cb7c49aa9b9302dd274f4487
grantkee <grant.d.kee@gmail.com>
2019-07-07 14:07:21 -0500

    saving progress after tutoring today

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -2,12 +2,12 @@
 
 //I dont need this block of code because I'm taking user input from a textarea instead of a readline
 
-const assert = require('assert');
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// const assert = require('assert');
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
 
 let finalPhrase = [];
@@ -34,80 +34,80 @@ function pigLatin(word) {
     for (let i = 0; i < pigString.length; i++) {
       let yArray = ['y'];
       let ohArray = ['o', 'u']
-
-      if (! (vowels.includes(pigString[i]))){
-         newWord.push(pigString[i]);
-       } else if ( (yArray.includes(pigString[0])) && (ohArray.includes(pigString[1])) ) {
-         newWord = pigString.slice(1, pigString.length).concat(newWord).join('') + 'ay';
+      // if i is not a vowel
+      if ((! (vowels.includes(pigString[i]))) && (! yArray.includes(pigString[0]))){
+        // push the consonant to newWord
+        if (yArray.includes(pigString[i])) {
+          newWord = pigString.slice(i, pigString.length).concat(newWord).join('') + 'ay';
+          return newWord;
+        } else {
+          newWord.push(pigString[i]);
+        }
+      } else if ( (yArray.includes(pigString[0])) && (ohArray.includes(pigString[1])) ) {
+        console.log(2)
+         newWord = pigString.slice(1, pigString.length).concat(newWord).join('') + 'yay';
+         return newWord;
+         break;
       } else {
+        console.log(3)
         newWord = pigString.slice(i, pigString.length).concat(newWord).join('') + 'ay';
         break;    
       };
     };
-  finalPhrase.push(newWord);
+    finalPhrase.push(newWord);
+    return newWord;
   };
-  finalPhrase.join(' ');
 };
-};
-//I won't need this function anymore because I am taking user input from a textarea on a webpage.
-
-function getPrompt() {
-  rl.question('phrase: ', (answer) => {
-    pigLatin(answer);
-    console.log( finalPhrase.join(' ') );
-    getPrompt();
-    function empty(){
-      finalPhrase.length = 0;
-    };
-    empty();
-  });
 };
 
 
 
 //this area is where I will work on an online version of my pig latin translator. I will need to manipulate the DOM
 
-//first, I need to take text from the user's input and run it through my pig latin function. this will only work with an onClick function. Other examples I've seen convert in real-time, but I want my users to submit via a button to run the function
-
-// let userPhrase = document.getElementById("input").value;
-
 //i've created a variable called userPhrase that takes in the value of whatever the user types into my textarea box on the webpage. Now, I want to make a function that runs my pig latin translator whenever the user clicks the translate button
 
-// document.getElementById("translateButton").onclick = function () {returnTranslation()};
+document.getElementById("translateButton").onclick = function () {returnTranslation()};
 
-// function returnTranslation() {
-//   pigLatin(userPhrase);
-//   document.getElementById("output").innerHTML = finalPhrase.join(' ');
-//   function empty(){
-//     finalPhrase.length = 0;
-//   };
-//   empty();  
-// }
+function returnTranslation() {
+  let userPhrase = document.getElementById("input").value;
+  console.log('1 ' + userPhrase);
+  pigLatin(userPhrase);
+  console.log('word: ' + pigLatin(userPhrase))
+  console.log('finalPhrase: ' + finalPhrase.join(' '))
+  document.getElementById("output").innerHTML = pigLatin(userPhrase);
+  function empty(){
+    finalPhrase.length = 0;
+  };
+  empty();  
+};
+
+
+//since this isn't returning anything, maybe I need to try String(object);
 
 // Tests - I'm not using these for the website I'm building
 
-if (typeof describe === 'function') {
+// if (typeof describe === 'function') {
 
-  describe('#pigLatin()', () => {
-    it('should translate a simple word', () => {
-      assert.equal(pigLatin('car'), 'arcay');
-      assert.equal(pigLatin('dog'), 'ogday');
-    });
-    it('should translate a complex word', () => {
-      assert.equal(pigLatin('create'), 'eatecray');
-      assert.equal(pigLatin('valley'), 'alleyvay');
-    });
-    it('should attach "yay" if word begins with vowel', () => {
-      assert.equal(pigLatin('egg'), 'eggyay');
-      assert.equal(pigLatin('emission'), 'emissionyay');
-    });
-    it('should lowercase and trim word before translation', () => {
-      assert.equal(pigLatin('HeLlO '), 'ellohay');
-      assert.equal(pigLatin(' RoCkEt'), 'ocketray');
-    });
-  });
-} else {
+//   describe('#pigLatin()', () => {
+//     it('should translate a simple word', () => {
+//       assert.equal(pigLatin('car'), 'arcay');
+//       assert.equal(pigLatin('dog'), 'ogday');
+//     });
+//     it('should translate a complex word', () => {
+//       assert.equal(pigLatin('create'), 'eatecray');
+//       assert.equal(pigLatin('valley'), 'alleyvay');
+//     });
+//     it('should attach "yay" if word begins with vowel', () => {
+//       assert.equal(pigLatin('egg'), 'eggay');
+//       assert.equal(pigLatin('emission'), 'emissionay');
+//     });
+//     it('should lowercase and trim word before translation', () => {
+//       assert.equal(pigLatin('HeLlO '), 'ellohay');
+//       assert.equal(pigLatin(' RoCkEt'), 'ocketray');
+//     });
+//   });
+// } else {
 
-  getPrompt();
+//   getPrompt();
 
-}
+// }
