a97cc2545034feab464174ed833d61cf58c53f1f
unknown <seglifabian@gmail.com>
2019-07-10 18:04:19 -0500

    completed pigLatin assignment

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -6,17 +6,26 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+const vowel = "aeiou";
 
+const detectVowel = (word) => {
+  for  (let i=0; i < word.length; i++) {
+    if (vowel.indexOf(word[i])!== -1)
+    return[i]
+  }
+}
 
-function pigLatin(word) {
-
-  // Your code here
-
+const pigLatin = (word) => { 
+  const firstVowel = detectVowel(word);
+  if(firstVowel > 0) {
+    return word.slice(firstVowel) + word.slice(0, firstVowel) + "ay";
+  }
+  return word + "way";
 }
 
 
 function getPrompt() {
-  rl.question('word ', (answer) => {
+  rl.question('Please enter a word to convert to pig latin: ', (answer) => {
     console.log( pigLatin(answer) );
     getPrompt();
   });
@@ -35,9 +44,9 @@ if (typeof describe === 'function') {
       assert.equal(pigLatin('create'), 'eatecray');
       assert.equal(pigLatin('valley'), 'alleyvay');
     });
-    it('should attach "yay" if word begins with vowel', () => {
-      assert.equal(pigLatin('egg'), 'eggyay');
-      assert.equal(pigLatin('emission'), 'emissionyay');
+    it('should attach "way" if word begins with vowel', () => {
+      assert.equal(pigLatin('egg'), 'eggway');
+      assert.equal(pigLatin('emission'), 'emissionway');
     });
     it('should lowercase and trim word before translation', () => {
       assert.equal(pigLatin('HeLlO '), 'ellohay');
