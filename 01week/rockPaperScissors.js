ebd32e4016ee59dd338388884337b3842fbb978d
Connor Yates <connorwyates24@gmail.com>
2019-08-13 19:45:44 -0500

    adding rockPaperScissors

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -1,51 +1,47 @@
-'use strict';
+function playFunction() {
+  var userChoice = prompt("Do you choose rock, paper or scissors?");
+  var computerChoice = Math.random();
+  if (computerChoice < 0.34) {
+      computerChoice = "rock";
+  } else if(computerChoice <= 0.67) {
+      computerChoice = "paper";
+  } else {
+      computerChoice = "scissors";
+  }
+  
+  var compare = function(choice1, choice2) {
+      if(choice1 === choice2) {
+        alert("The result is a tie!");
+  }
+  if(choice1 === "rock") {
+      if(choice2 === "scissors") {
+          alert("rock wins");
+      } else {
+          alert("paper wins");
+      }
+  }
+  if(choice1 === "paper") {
+      if(choice2 === "rock") {
+          alert("paper wins");
+      } else {
+          if(choice2 === "scissors") {
+              alert("scissors wins");
+      }
+  }
+  if(choice1 === "scissors") {
+      if(choice2 === "rock") {
+          alert("rock wins");
+      } else {
+          if(choice2 === "paper") {
+             alert("scissors wins");
+          }
+      }
+  }
+  }
+  };
+  console.log("User Choice: " + userChoice);
+  console.log("Computer Choice: " + computerChoice);
+  compare(userChoice, computerChoice)
+  } 
 
-const assert = require('assert');
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-
-function rockPaperScissors(hand1, hand2) {
-
-  // Write code here
-
-}
-
-function getPrompt() {
-  rl.question('hand1: ', (answer1) => {
-    rl.question('hand2: ', (answer2) => {
-      console.log( rockPaperScissors(answer1, answer2) );
-      getPrompt();
-    });
-  });
-}
-
-// Tests
-
-if (typeof describe === 'function') {
-
-  describe('#rockPaperScissors()', () => {
-    it('should detect a tie', () => {
-      assert.equal(rockPaperScissors('rock', 'rock'), "It's a tie!");
-      assert.equal(rockPaperScissors('paper', 'paper'), "It's a tie!");
-      assert.equal(rockPaperScissors('scissors', 'scissors'), "It's a tie!");
-    });
-    it('should detect which hand won', () => {
-      assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
-      assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
-    });
-    it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
-      assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
-      assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
-    });
-  });
-} else {
-
-  getPrompt();
-
-}
+  
