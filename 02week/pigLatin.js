1a38c906b955a8f684b830d5217aa8ed1f9132a9
David Bille <d_bille@yahoo.com>
2019-08-23 19:16:51 -0500

    pig latin final version

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -10,10 +10,31 @@ const rl = readline.createInterface({
 
 function pigLatin(word) {
 
-  // Your code here
+  var word = word.toLowerCase().trim();
 
+  // Holds index of the first vowel, found with the findVowel function
+  var firstVowelIndex = findVowel(word);
+
+  if (firstVowelIndex > 0){
+
+    /* Returns word minus first consonant(s) and returns at end of word with 'ay', or returns word with 'way' */
+    return word.slice(firstVowelIndex) + word.slice(0, firstVowelIndex) + 'ay';
+  } else {
+    return word + 'way';
+  }
 }
 
+function findVowel(word) {
+  /* Finds first vowel in the word and assigns its index to i, which is then held in the findVowel function */
+  for(var i=0; i<word.length; i++){
+
+    /* if any character in 'aeiou' is found in the word, and its index is not -1, its index will be held and assigned to i */
+    if('aeiou'.indexOf(word[i]) !== -1){
+      return i;
+    }
+  }
+}
+pigLatin('');
 
 function getPrompt() {
   rl.question('word ', (answer) => {
