08ba5f663dbe63de7d907c84a5dacacee2eab633
vincentnqc <vincentnqc92@gmail.com>
2019-08-26 14:31:13 -0500

    basic commit

--- 02week/ticTacToe.js
+++ 02week/ticTacToe.js
@@ -34,7 +34,13 @@ function horizontalWin() {
 }
 
 function verticalWin() {
-  // Your code here
+  if((board[0][0] === board[0][1] && board[0][2])
+  || (board[1][0] === board[1][1] && board[1][2])
+  || (board[2][0] === board[2][1] && board[2][2]))
+
+  console.log("Horizontal Win!");
+
+  return true;
 }
 
 function diagonalWin() {
