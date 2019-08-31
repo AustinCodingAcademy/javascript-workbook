689536c88e1ac4049be8cd0e7193f1ad27abf747
Alex Munoz <anmunoz24@gmail.com>
2019-08-31 16:02:48 -0700

    in progress

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -19,25 +19,63 @@ function printStacks() {
   console.log("c: " + stacks.c);
 }
 
-function movePiece() {
-  // Your code here
+
+
+function movePiece(startStack, endStack) {
+
+let remove = stacks[startStack].pop()
+
+stacks[endStack].push(remove)
+
 
 }
 
-function isLegal() {
-  // Your code here
+function isLegal(startStack, endStack) {
 
+if(stacks[startStack][stacks[startStack].length -1] < stacks[endStack][stacks[endStack].length -1]){
+
+return true }  
+
+else if (stacks[endStack].length === 0){
+  return true
 }
 
-function checkForWin() {
-  // Your code here
+else return false
+}
+
+function checkForWin(endStack) {
+  let winStack = [4, 3, 2, 1]
+
+  // console.log(endStack)
+
+  if (endStack === 'b' || 'c'){
+
+    for(let i = 0; i < winStack.length; i++){
+
+      // console.log(winStack[i], stacks[endStack][i])
+      if(stacks[endStack][i] !== winStack[i]){
+        return false
+      }
+    }
+
+      return true
+  }
 
 }
 
 function towersOfHanoi(startStack, endStack) {
-  // Your code here
-
+if (isLegal(startStack, endStack)){
+movePiece (startStack, endStack) 
+if(checkForWin(endStack)){
+  console.log("We've Got A Winner!")
 }
+}
+else {
+  console.log('Not A Valid Move')
+}
+}
+
+
 
 function getPrompt() {
   printStacks();
