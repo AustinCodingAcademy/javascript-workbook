8422cdc2176641dd84bbb21f884db42096f7f2ce
devarcher <archershot@hotmail.com>
2019-09-03 11:15:17 -0500

    added comments and moved movePiece console log to appropriate place in code

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -20,20 +20,25 @@ function printStacks() {
 }
 
 function movePiece(startStack, endStack) {
+  // Ensuring input is all lower case and with no spaces. Created from (array) to to (array) variables.
   const moveFrom = startStack.toLowerCase().trim();
   const moveTo = endStack.toLowerCase().trim();
+
+  // Created variable that represents the startStack array input from user. Pop takes last element from that array and removes it.
   const lastElement = stacks[moveFrom].pop();
 
+  // statement pushes the above element that was popped off the start array and pushes it onto the endStack (moveTo) array.
   stacks[moveTo].push(lastElement);
 }
 
 function isValid(startStack, endStack) {
   const moveFrom = startStack.toLowerCase().trim();
   const moveTo = endStack.toLowerCase().trim();
+  // Defines what inputs are acceptable.
   const validInput = ["a", "b", "c"];
 
+  // if the validInput array contains the input from the user, then it is ok to continue.
   if (validInput.includes(moveFrom) && validInput.includes(moveTo)) {
-    console.log(`Item moved from stack ${moveFrom} to ${moveTo}.`);
     return true;
   } else {
     console.log(
@@ -46,21 +51,30 @@ function isValid(startStack, endStack) {
 function isLegal(startStack, endStack) {
   const moveFrom = startStack.toLowerCase().trim();
   const moveTo = endStack.toLowerCase().trim();
+
+  // Getting the last index, and second to last index of moveFrom array and moveTo array respectively.
   const lastElementIndex = stacks[moveFrom].length - 1;
   const secondToLastElementIndex = stacks[moveTo].length - 2;
+
+  // creating a variable that is the lastElement and secondToLastElement in moveFrom and moveTo arrays.
   const lastElement = stacks[moveFrom][lastElementIndex];
   const secondToLastElement = stacks[moveTo][secondToLastElementIndex];
 
-  console.log(stacks[moveTo][0], lastElement);
+  // console.log(stacks[moveTo][0], lastElement);
   // console.log(stacks[moveTo][1], lastElement);
 
   // Value of is getting Index number.  Need to get the value of the dyanmic index.
 
   if (
+    // First moveTo element is greater than lastElement
     stacks[moveTo][0] > lastElement ||
+    // First Element is Undefined (Because there is no element in the array)
     stacks[moveTo][0] === undefined ||
+    // If there are two or more elements in the moveTo array, 
+    // this comparison weights that value against the moveFrom lastElement value
     secondToLastElement > lastElement
   ) {
+    console.log(`*** ${lastElement} moved from stack ${moveFrom} to stack ${moveTo}.***`);
     return true;
   } else {
     console.log(
@@ -71,10 +85,13 @@ function isLegal(startStack, endStack) {
 }
 
 function checkForWin() {
+  // Defines variables for arrays b and c
   const arrB = stacks.b;
   const arrC = stacks.c;
 
+  // A win is having 4 valid and legal values in stacks.b or stacks.c **I changed the win test to include C**
   if (arrB.length === 4 || arrC.length === 4) {
+    console.log("!!!You Win!!!");
     return true;
   } else {
     return false;
@@ -82,12 +99,16 @@ function checkForWin() {
 }
 
 function towersOfHanoi(startStack, endStack) {
+  // Checks Valid Input logic and Legal Move logic.
   if (isValid(startStack, endStack) && isLegal(startStack, endStack)) {
-    console.log("is ValidHere and isLegalHere");
+    // console.log("is ValidHere and isLegalHere");
+    // If Valid and Legal, movePiece function is called.
     movePiece(startStack, endStack);
   } else {
+    // If not legal and valid, no move is allowed.
     return false;
   }
+  // Check for Win is called for array B & C. I altered the win scenario in tests.
   checkForWin();
 }
 
