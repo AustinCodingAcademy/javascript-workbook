9d203ba6dce659b33b227be5a3cd5d4d44322bdd
Alex Munoz <anmunoz24@gmail.com>
2019-09-07 14:43:00 -0700

    clean up code

--- 04week/mastermind.js
+++ 04week/mastermind.js
@@ -28,11 +28,14 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+//Call or pass in guess, which would be the users input.
+//let variables which that split the string into an array
 
-//Create a function that compares the users input to the solution.
-//Run through the array, and compare each index.
-//console.log or generate hint to output how many letters are correct, and if location is correct. 
-//Example 2-2
+//let a variable correctLetterLocations equal 0 by default, so you it will increment when the user guesses a correct letter in the correct location.
+//(for loop)
+//Compare solution to the users guess by checking each index. 
+//Increment correct letter location when guess index matches solution index.
+//Set the rest of the solution to null.
 
 function generateHint(guess) {
 
@@ -47,12 +50,22 @@ function generateHint(guess) {
      solutionArray[i] = null 
     }
   }
+
+//let a variable correctLetters equal 0 by default, so it will increment when the user guesses a correct letter.
+//(for loop)
+//Compare solution to the users guess by running through and checking each index.
+//let targetIndex 
+//Compare the indexes of guessArray to the solutionArray, if greater than -1, in other words if any indexes match, increment correct letter by 1
+//what doesnt equal set to null.
+
+//return the number of correct locations dash correct letters
+
   let correctLetters = 0;
 
   for(let i=0; i<solutionArray.length; i++){
    let targetIndex = solutionArray.indexOf(guessArray[i]);
    if(targetIndex > -1){
-     console.log(targetIndex)
+    //  console.log(targetIndex)
      correctLetters++;
      solutionArray[targetIndex] = null 
    }
@@ -60,33 +73,67 @@ function generateHint(guess) {
   return `${correctLetterLocations}-${correctLetters}`
 }
 
-// // function isValid(guess) {
-// //   let guessArray = guess.split("").trim().toLowerCase()
-// //   if(guessArray.length === 4){
-// //     return guess
-// //   } else {
-// //     console.log("Please Enter Valid Guess")
-// //   }
-// }
+//Create function to check and make sure every users input is valid by removing extra  spaces and turning to lowercase.
+//Guess has to be equal to 4.
 
-//Detect a win by all letters, and indexes match up.
+  function isValid(guess)  { console.log(guess)
+   if(guess.length === 4){
+     return true
+   } else {
+     return false
+   }
+ }
+
+//Create counter function to allow user to input 10 tries max.
+
+ function maxTries(guesses) {
+   if (guesses.length <= 10) {
+     return true 
+   } else {
+     return false
+   }
+ }
+
+//Play Game
+//Keep track of users guesses maxTries().
+
+//Check to make sure users input isValis()
+
+//If users guess equals solution, console.log YOU WIN!
+//If not generateHint().
+//let variable hint equal generateHint() pass in the users guess
+//Pass in the guess and hint and push it to the board, print.
+
+//When users guess does not equal solution console log try again!
+//console log # of try user is on. 
+
+//if input is not valid console log enter valid  guess
+
+//if users inputs are greater than 10 console log you lost the game!
 
 function mastermind(guess) {
-  // solution = 'abcd'; // Comment this out to generate a random solution
+  // Comment this out to generate a random solution
+  guess.toLowerCase().trim()
 
-if(guess === solution){
+  if (maxTries(board)) {
+
+  if(isValid(guess)){
+
+  if(guess === solution){
   console.log("You guessed it!")
   return "You guessed it!"
 }   else{
   let hint = generateHint(guess);
   board.push(`${guess} ${hint}`)
-  console.log(board.length) 
   console.log("Try Again")
+  console.log(board.length) 
 }
-
-
-
-
+  } else  {
+    console.log("Please Enter Valid Guess")
+  }
+} else {
+  console.log("You Ran Out Of Turns!")
+}
 }
 
 
