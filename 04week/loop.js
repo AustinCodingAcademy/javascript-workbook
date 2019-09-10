16e99876791c9e422a4a6639069bce078624500f
devarcher <archershot@hotmail.com>
2019-09-10 15:31:07 -0500

    fixed for in loop and number 7. assignment complete

--- 04week/loop.js
+++ 04week/loop.js
@@ -23,11 +23,15 @@ person = {
 // 3. Use a for...in loop and if statement to console.log the value associated with the key birthDate if the birth year is an odd number.
 // RegEx for the Year (YYYY)
 let regexYear = /\d{4,}/g;
+let str = person.birthDate.match(regexYear);
+let year = parseInt(str);
+console.log(year);
+// console.log(person.birthDate[regexYear]);
 
 // For Loop for the
 for (key in person) {
-  console.log('RegEx Type: ', Object.prototype.toString.call(regexYear));
-  if (person.birthDate[regexYear] % 2 !== 1) {
+  // console.log('RegEx Type: ', Object.prototype.toString.call(regexYear));
+  if (year % 2 !== 0) {
     console.log(`${person.birthDate}`);
   }
   break;
@@ -35,7 +39,7 @@ for (key in person) {
 
 console.log("==================================================");
 
-// 4. Create an arrayOfPersons that contains mulitiple objects. 
+// 4. Create an arrayOfPersons that contains mulitiple objects.
 arrayOfPersons = [
   {
     firstName: "Jane",
@@ -60,24 +64,26 @@ arrayOfPersons = [
 ];
 
 // 5. Use .map() to map over the arrayOfPersons and console.log() their information.
-const arrayOfPersons2 = arrayOfPersons.map((person) => person)
+const arrayOfPersons2 = arrayOfPersons.map(person => person);
 console.log(arrayOfPersons);
 
 console.log("==================================================");
-
 
 console.log(arrayOfPersons2);
 
 console.log("==================================================");
 
 // 6. Use .filter() to filter the persons array and console.log only males in the array.
-const malesArr = arrayOfPersons2.filter((men) => men.gender === 'male');
+const malesArr = arrayOfPersons2.filter(men => men.gender === "male");
 console.log(malesArr);
 
 console.log("==================================================");
 
 // 7. Use .filter() to filter the persons array and console.log only people that were born before Jan 1, 1990.
-const oldPeople = arrayOfPersons2.filter((oldest) => oldest.birthDate[regexYear] > 1990);
+
+const oldPeople = arrayOfPersons2.filter(oldest => {
+  let oldStr = oldest.birthDate.match(regexYear);
+  let birthYear = parseInt(oldStr);
+  return birthYear < 1990;
+});
 console.log(oldPeople);
-
-
