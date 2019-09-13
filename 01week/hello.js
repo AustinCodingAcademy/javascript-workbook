b7790d7ca0f3aac0f9d9dca0b616a2899a720a97
JoshuaGutie <Josh.Gvtie@gmail.com>
2019-09-12 19:52:37 -0500

    hello.js

--- 01week/hello.js
+++ 01week/hello.js
@@ -1,5 +1,7 @@
-let color = "green";
-let otherColor = "black";
-
-console.log("my color is", color);
-console.log("my other color is", otherColor);
+const addMe = function(num1, num2) {
+  const sum = num1 + num2;
+  if (sum > 10) {
+    console.log("greater than 10");
+  }
+};
+addMe(6, 10);
