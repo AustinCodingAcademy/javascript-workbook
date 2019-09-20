ce56175abcdc429e421b1f2c08680d707f658d8c
Yousif Seedhom <yousif.seedhom@gmail.com>
2019-09-19 19:34:20 -0500

    This is my practice assignment

---
+++ 02week/practice.js
@@ -0,0 +1,11 @@
+let word = "mead";
+
+// write some code 
+let firstVowel = 1;
+
+
+let firstHalf = word.substring(1, 3);
+let secondHalf = word.substring(firstVowel);
+
+console.log("first half: ", firstHalf);
+console.log("second half: ", secondHalf);
