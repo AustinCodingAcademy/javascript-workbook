8909ab54bf509a513af167105c081c1bbb370315
Alex Munoz <anmunoz24@gmail.com>
2019-09-23 14:47:51 -0700

    Final Commit

--- 05week/checkers.js
+++ 05week/checkers.js
@@ -6,12 +6,6 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-
-function checkers() {
-  // Your code here
-
-}
-
 //checkers class to define key value pairs, color and symbol to create black and red piece
 class Checkers {
   constructor(color, symbol) {
@@ -20,11 +14,11 @@ class Checkers {
   }
 }
 
-//new instances of Checkers class
+//new instances of Checkers class, black piece, and red piece, color and symbol
 const blackPiece = new Checkers('black', 'b')
 const redPiece = new Checkers('red', 'r')
 
-//switch player
+//switch player, black piece starts
 let playerTurn = blackPiece;
 
 function switchPlayer() {
@@ -35,6 +29,7 @@ function switchPlayer() {
   }
 }
 
+//define the grid and checkers array, pass into the Board 
 class Board {
   constructor(grid, checkers) {
     this.grid = [],
@@ -44,7 +39,7 @@ class Board {
     return this.grid[row][column];
   }
 
-  //Find the index of that checker in the this.checkers array. then remove it by .splice()ing it out
+  //assign the position on this.grid to null. That checker is now gone.
   killChecker(position) {
     this.board.grid[position[0]][position[1]] = null;   
   }
@@ -86,7 +81,7 @@ class Board {
     console.log(string);
   }
 
-  //push red pieces to board
+  //push red pieces to board, rows 0 through 3, all columns, odds and evens
   initializeGrid () {
     for (let row1 = 0; row1 < 3; row1++) {
       for(let col1 = 0; col1 < 8; col1++) {
@@ -100,7 +95,7 @@ class Board {
       }
     }
 
-    //push black pieces to board
+  //push black pieces to board, rows 5 through 8, all columns, odds and evens
     for (let row2 = 5; row2 < 8; row2++) {
       for(let col2 = 0; col2 < 8; col2++) {
         if(row2 % 2 == 0 & col2 % 2 == 1) {
@@ -124,53 +119,58 @@ class Game {
     this.board.initializeGrid(); 
   }
   moveChecker(whichPiece, toWhere) {
-    let whichPieceArray = whichPiece.split("");//start; create variable and turn a string into an array
+    let whichPieceArray = whichPiece.split("");//start position; create variable and turn a string into an array
 
-    let toWhereArray = toWhere.split("");//end;
+    let toWhereArray = toWhere.split("");//end position; create variable and turn a string into an array
 
     if (this.legalMove(whichPiece, toWhere)) {
       //create new variable to pick up a piece to move
       let selectedPiece = this.board.selectChecker(whichPieceArray[0], whichPieceArray[1])
 
-      this.board.grid[toWhereArray[0]][toWhereArray[1]] = selectedPiece//place selectedPiece and move those coordinates
-      this.board.grid[whichPieceArray[0]][whichPieceArray[1]] = null//remove selectedPiece that moved from those coordinates
+      this.board.grid[toWhereArray[0]][toWhereArray[1]] = selectedPiece//select piece to move
+      this.board.grid[whichPieceArray[0]][whichPieceArray[1]] = null//set position back to null
       switchPlayer();
       // console.log("condition", toWhereArray[0], whichPieceArray[0])
     };
     
     let toWhereNumber = toWhere.split("").map(Number);//turn the end string into an array of numbers
 
-    let row1 = toWhereNumber[0];
-    let column1 = toWhereNumber[1];
+    let row1 = toWhereNumber[0];//define row
+    let column1 = toWhereNumber[1];//define column
 
     let endPosition = this.board.grid[row1][column1];//end postion equals end row, and end column 
 
+        //to jump piece, start position, needs to be 18 or 22 away from end position, end position must be empty. 
+        //use Number() to convert variables to numbers.
+        //use selectedPiece to access and move piece, and set (remove) position back to null.
         if(playerTurn.symbol == "b") {
           if(Number(whichPiece) - 18 === Number(toWhere) || Number(whichPiece) - 22 === Number(toWhere)) {
             if(endPosition === null){
               let selectedPiece = this.board.selectChecker(whichPieceArray[0], whichPieceArray[1])
+              
 
-              this.board.grid[toWhereArray[0]][toWhereArray[1]] = selectedPiece//place selectedPiece and move those coordinates
-              this.board.grid[whichPieceArray[0]][whichPieceArray[1]] = null//remove selectedPiece that moved from those coordinates
+              this.board.grid[toWhereArray[0]][toWhereArray[1]] = selectedPiece//select piece to move
+              this.board.grid[whichPieceArray[0]][whichPieceArray[1]] = null//set position back to null
 
               this.board.grid[Number(whichPieceArray[0]) - 1][Number(whichPieceArray[1]) + 1] = null;
-              this.board.checkers.pop();
+              this.board.checkers.pop();//remove from checkers array, change length
               // console.log(this.board.checkers.length)
             }     
           }
        }
 
-       if(playerTurn.symbol == "r") {
+        //to jump piece, start position, needs to be 18 or 22 away from end position, end position must be empty. 
+        //use Number() to convert variables to numbers.
+        //use selectedPiece to access and move piece, and set (remove) position back to null.
+      if(playerTurn.symbol == "r") {
         if(Number(whichPiece) + 18 === Number(toWhere) || Number(whichPiece) + 22 === Number(toWhere)) {
           if(endPosition === null){
             let selectedPiece = this.board.selectChecker(whichPieceArray[0], whichPieceArray[1])
-            //create new variable to pick up a piece to move
       
-            this.board.grid[toWhereArray[0]][toWhereArray[1]] = selectedPiece//place selectedPiece and move those coordinates
-            this.board.grid[whichPieceArray[0]][whichPieceArray[1]] = null//remove selectedPiece that moved from those coordinates
-
+            this.board.grid[toWhereArray[0]][toWhereArray[1]] = selectedPiece//select piece to move
+            this.board.grid[whichPieceArray[0]][whichPieceArray[1]] = null//set position back to null
             this.board.grid[Number(whichPieceArray[0]) + 1][Number(whichPieceArray[1]) +1] = null;
-            this.board.checkers.pop()
+            this.board.checkers.pop()//remove from checkers array, change length
             // console.log(this.board.checkers.length)
           }           
         }
@@ -182,6 +182,7 @@ class Game {
     let whichPieceArray = whichPiece.split(""); 
     let toWhereArray = toWhere.split("");
 
+    //loop through start and end array(position) and make sure input is only 2
     if(whichPieceArray.length === 2 && toWhereArray.length === 2) {
       if(whichPieceArray[0] >= 0 && whichPieceArray[0] <= 7 && whichPieceArray[1] >= 0 && whichPieceArray[1] <= 7 && toWhereArray[0] >= 0 && toWhereArray[0] <= 7 && toWhereArray[1] >= 0 && toWhereArray[1] <= 7) {
         return true
@@ -203,7 +204,7 @@ class Game {
           if(Number(whichPiece) - 9 === Number(toWhere) || Number(whichPiece) - 11 === Number(toWhere)) {
             if(endPosition === null){
             console.log("Legal Move")
-            return true 
+            return true
             }     
             console.log("Not a Legal Move")
           }
