2886e5f71c3f5998480f64bda94dffa816d36f2a
devarcher <archershot@hotmail.com>
2019-09-25 13:36:49 -0500

    first commit: populated assignment with give code

---
+++ 07week/firstFetch/index.html
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Fetch Practice</title>
+</head>
+<body>
+
+  <!-- When you click this button, the posts collected in the fetch request will be logged in the console in your devtools tray -->
+  <button onclick="consolePosts()">Console Posts</button>
+
+  <!-- When you click this button the posts collected from the fetch request will be displayed below in the ul element with the id='all-posts' -->
+  <button onclick="displayPost()">Display Posts</button>
+  <button onclick="">Fetch 5 Posts</button>
+  <button onclick="">Fetch Comments</button>
+  <button onclick="">Fetch Users</button>
+  <div>
+    <h3>All Posts</h3>
+    <ul id='all-posts'></ul>
+  </div>
+  <script src="./main.js"></script>
+</body>
+</html>
---
+++ 07week/firstFetch/main.js
@@ -0,0 +1,32 @@
+let arrayOfPosts;
+
+// this function waits for the web page to be loaded, when it does it will run the code inside of it which happen to be getPosts()
+window.onload = function() {
+  getPosts()
+
+}
+
+// this function is going to make a fetch request to the url inside it's parameter brackets (). Then it will turn the response (data it's getting back), saved here as res. The res.json will not be saved as posts and saved into the variable, arrayOfPosts
+const getPosts = () => {
+  fetch('http://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(posts => arrayOfPosts = posts)
+}
+
+// this function logs the results in your browsers console
+const consolePosts = () => {
+  console.log(arrayOfPosts)
+}
+
+// this function creates elements inside the all-posts ul, then appends text inside it with the posts that were returned in the request.
+const displayPost = () => {
+  const allPosts = document.getElementById('all-posts')
+  arrayOfPosts.map((post, index) => {
+    const li = document.createElement('li')
+    const text = document.createTextNode(`#${index}, Title: ${post.title}:  ${post.body}, by user: ${post.userId}`)
+    li.appendChild(text)
+    allPosts.append(li)
+  })
+}
+
+// Your job now is to follow the functions above and use them as templates to build the functionality the buttons in the index.html file already have laid out in it. This way you can learn how to build fetch requests and work with other apis and become a real developer!!
