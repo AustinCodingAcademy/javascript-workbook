38493db26892629a98bd76a3d7cba5cc2ed29640
devarcher <archershot@hotmail.com>
2019-09-26 13:09:38 -0500

    attempted changes unsuccessfully

--- 07week/addressBook/main.js
+++ 07week/addressBook/main.js
@@ -1,19 +1,23 @@
-let userArr; 
+let userArr;
 
 window.onload = function() {
   getUsers();
-}
+};
 
 const getUsers = () => {
   fetch("https://randomuser.me/api/")
-  .then(data => data.json())
-  .then(users => (userArr = users));
+    .then(data => data.json())
+    .then(users => (userArr = users));
 };
 
-const displayUsers = () => {
-  const user = document.getElementsByClassName(pBox);
-  userArr.map((user) => {
-    let img = document.getElementById(img);
-    let name = document.getElementById(name);
-  })
-};
+// const displayUsers = () => {
+//   const user = document.getElementsByClassName("pBox");
+//   userArr.map(user => {
+//     let img = document.getElementById(img);
+//     let name = document.getElementById(name);
+//     user.append(users.img);
+//   });
+// };
+
+getUsers();
+// displayUsers()
