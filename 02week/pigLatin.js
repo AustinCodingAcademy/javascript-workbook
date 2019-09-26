69d80a4f1fba4a3144530d35e841916fb7587c8a
JoshuaGutie <Josh.Gvtie@gmail.com>
2019-09-26 01:26:46 -0500

    pigLatin

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -7,16 +7,35 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-function pigLatin(word) {
+// function pigLatin(word) {
+//   let positionA = word.indexOf("a");
+//   let positionE = word.indexOf("e");
+//   let positionI = word.indexOf("i");
+//   let positionO = word.indexOf("o");
+//   let positionU = word.indexOf("u");
+
+  //make an array
   const vowels = ["a", "e", "i", "o", "u"];
+  //only add to the array values greater than -1
+  //math.min
+  //week2-1
+
   //sets all the words to lowercase and trims them
   word = word.trim().toLowerCase();
   //splitting word into indivudual letters
   var splitWord = word.split("");
-  //
-  const firstLetter = word.indexOf(filtered[0]);
-
-  pigLatin(" create");
+  let filtered = splitWord.filter(letter => vowels.indexOf(letter) > -1);
+  console.log(filtered);
+  const indexFirstVowel = word.indexOf(filtered[0]);
+  if (indexFirstVowel == 0) {
+    return word + "yay";
+  } else {
+    return (
+      splitWord.slice(indexFirstVowel).join("") +
+      splitWord.slice(0, indexFirstVowel).join("") +
+      "ay"
+    );
+  }
 }
 
 function getPrompt() {
