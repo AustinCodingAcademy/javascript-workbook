c7bc36a2d2156b5aa2eb358c298f00a2aa167390
devarcher <archershot@hotmail.com>
2019-09-25 20:50:55 -0500

    first commit: added html, css, some js

---
+++ 07week/addressBook/main.js
@@ -0,0 +1,19 @@
+let userArr; 
+
+window.onload = function() {
+  getUsers();
+}
+
+const getUsers = () => {
+  fetch("https://randomuser.me/api/")
+  .then(data => data.json())
+  .then(users => (userArr = users));
+};
+
+const displayUsers = () => {
+  const user = document.getElementsByClassName(pBox);
+  userArr.map((user) => {
+    let img = document.getElementById(img);
+    let name = document.getElementById(name);
+  })
+};
