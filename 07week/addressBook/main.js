81b42a2650514713a28d9b9a5170e8002bf13371
devarcher <archershot@hotmail.com>
2019-09-26 20:25:00 -0500

    names exist

--- 07week/addressBook/main.js
+++ 07week/addressBook/main.js
@@ -1,23 +1,30 @@
-let userArr;
+let userObj;
 
 window.onload = function() {
   getUsers();
 };
 
 const getUsers = () => {
-  fetch("https://randomuser.me/api/")
-    .then(data => data.json())
-    .then(users => (userArr = users));
+  fetch("https://randomuser.me/api/?results=5")
+    .then(res => res.json())
+    .then(user => (userObj = user));
 };
 
-// const displayUsers = () => {
-//   const user = document.getElementsByClassName("pBox");
-//   userArr.map(user => {
-//     let img = document.getElementById(img);
-//     let name = document.getElementById(name);
-//     user.append(users.img);
-//   });
-// };
+const consoleUsers = () => {
+  console.log(userObj.results);
+  console.log(userObj.results[0].name);
+};
 
-getUsers();
-// displayUsers()
+const displayUsers = () => {
+  // const user = document.getElementsByClassName("pBox");
+  for (let i = 0; i < userObj.results.length; i++) {
+    let nameDiv = document.getElementById("name");
+    let newParagraph = document.createElement("p");
+    // let img = document.getElementById(img);
+    const text = document.createTextNode(`${userObj.results[i].name.first} ${userObj.results[i].name.last}`);
+    nameDiv.appendChild(newParagraph);
+    newParagraph.appendChild(text);
+  }
+};
+
+// getUseUsers
