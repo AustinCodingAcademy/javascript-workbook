7566e1c2352b0d0867609bf22e63d2294c946f3c
devarcher <archershot@hotmail.com>
2019-09-26 21:07:59 -0500

    added images and styling for css

--- 07week/addressBook/main.js
+++ 07week/addressBook/main.js
@@ -13,17 +13,33 @@ const getUsers = () => {
 const consoleUsers = () => {
   console.log(userObj.results);
   console.log(userObj.results[0].name);
+  console.log(userObj.results[0].picture.large)
 };
 
 const displayUsers = () => {
-  // const user = document.getElementsByClassName("pBox");
   for (let i = 0; i < userObj.results.length; i++) {
+    // Grabbing Location to push name info.
     let nameDiv = document.getElementById("name");
+
+    // Creating a new Element Paragraph to place text into
     let newParagraph = document.createElement("p");
-    // let img = document.getElementById(img);
-    const text = document.createTextNode(`${userObj.results[i].name.first} ${userObj.results[i].name.last}`);
+
+    // Creating a TextNode with appropraite info
+    const text = document.createTextNode(
+      `${userObj.results[i].name.first} ${userObj.results[i].name.last}`
+    );
+
+    // Placing new paragram into #name div
     nameDiv.appendChild(newParagraph);
+
+    // Appending Text variable into newParagraph
     newParagraph.appendChild(text);
+
+    // Make Image Place and Append
+    let imgDiv = document.getElementById('img');
+    let newImg = document.createElement("IMG");
+    newImg.src = userObj.results[i].picture.large;
+    imgDiv.append(newImg);
   }
 };
 
