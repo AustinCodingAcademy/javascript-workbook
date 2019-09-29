2981823ca36296537e82fe25274305d915d6d354
Alex Munoz <anmunoz24@gmail.com>
2019-09-29 14:04:58 -0700

    working on more info button

--- 07week/fetch-Address Book/main.js
+++ 07week/fetch-Address Book/main.js
@@ -1,14 +1,47 @@
+"use strict"
+console.log("hello")
+
+document.getElementsById('apiButton').addEventListener('onclick', getUsers);
+document.getElementById('infoButton').addEventListener('onclick', displayInfo);
+
 let arrayOfUsers;
 
-
 function getUsers() {
-  fetch('https://randomuser.me/api/')
+  fetch('https://randomuser.me/api/?results=10')
     .then(res => res.json())
-    .then(data => arrayOfUsers = data)
-    console.log(data) 
-}
+    .then(data => {
+      console.log(data.results)
+      let output = "<h2>Contatcs</h2>";
+      data.results.forEach(function(results){
+        output += `
+        <ul>
+          <li>${results.name.last}, ${results.name.first}</li>
+          <li>${results.location.city}</li>
+          <img src="${results.picture.large}">
+          <button id='infoButton' onclick="displayInfo()">More Info</button>
+        `
+      })
+      document.getElementById("users").innerHTML=output
+      })
+
+      function displayInfo() {
+        fetch('https://randomuser.me/api/?results=3')
+        .then(res => res.json())
+        .then(info => {
+          console.log(info.results)
+          let display = 
+          info.results.forEach(function(results) {
+            display += `
+            <ul>
+            <li>${results.dob}</li>
+            <li>${results.location.street}</li>
+            <li>${results.location.city}</li>
+            `
+          })
+          document.getElementById("more-info").innerHTML=display
+        })
+      }
+};
 
 
-document.getElementById("users").addEventListener("click", function () {
-  getUsers()
-})
+
