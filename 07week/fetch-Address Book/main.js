d16528e2af14d9853946c5946eaf536f1bc5e107
Alex Munoz <anmunoz24@gmail.com>
2019-09-29 10:38:59 -0700

    addressBook

--- 07week/fetch-Address Book/main.js
+++ 07week/fetch-Address Book/main.js
@@ -1,2 +1,14 @@
+let arrayOfUsers;
 
 
+function getUsers() {
+  fetch('https://randomuser.me/api/')
+    .then(res => res.json())
+    .then(data => arrayOfUsers = data)
+    console.log(data) 
+}
+
+
+document.getElementById("users").addEventListener("click", function () {
+  getUsers()
+})
