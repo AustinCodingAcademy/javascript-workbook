d0ac2e234c3b1ce21293e99ae226d56ab7ee29ea
Alex Munoz <anmunoz24@gmail.com>
2019-09-30 14:17:05 -0700

    Final Commit

--- 07week/fetch-Address Book/Address-Book/main.js
+++ 07week/fetch-Address Book/Address-Book/main.js
@@ -1,29 +1,27 @@
 "use strict"
 
+//fetch multiple users on window onload
 window.onload = function() {
   getUsers();
 };
 
-console.log("hello")
-
-// document.getElementById('apiButton').addEventListener('onclick', getUsers);
-
+//create empty array of users
 let arrayOfUsers = [];
 
+//fetch users from API given, JSON file
 function getUsers() {
   fetch('https://randomuser.me/api/?results=10')
     .then(res => res.json())
     .then(data => {
-      console.log(data.results)
-
+      //map through array of users and push users into array
       data.results.map(user => {
         arrayOfUsers.push(user)
       })
 
+      //output equals an h2 element and print data(results) for each user
       let output = "<h2>Contacts</h2>";
       data.results.forEach(function(results){
-        // let li = document.createElement('li')
-        // let button = document.createElement('button')
+        //let output equal ul- a list of user information, name, country, img tag, and more info button
         output += `
         <ul id='${results.dob.age}'>
           <li><b>Name:</b> ${results.name.first} ${results.name.last}</li>
@@ -34,21 +32,23 @@ function getUsers() {
           </ul>
           `
       })
+      //get element by user id and insert into the DOM by variable name 
       document.getElementById("users").innerHTML=output
       })
 };
 
+//get more info button function, pass in objects
 function displayInfo(age, city, state, cell) {
-  console.log('results', age)
-    let input = `
+    //let display equal another ul- a list of more user information, city, state, img age, and cell
+    let display = `
       <ul>
       <li><b>Location:</b>${city}, ${state}</li>
       <li><b>Age:</b>${age}</li>
       <li><b>Phone #:</b>${cell}</li><br>
       </ul>
       `
-      console.log(input)
-    document.getElementById(`${age}`).innerHTML += input
+      //get element by user id and insert into the DOM by variable name 
+      document.getElementById(`${age}`).innerHTML += display
 }
 
 
