5071a41b2e03a800a265b1762eedbdd5ab54f298
Alex Munoz <anmunoz24@gmail.com>
2019-09-30 12:20:39 -0700

    address book with more info button

--- 07week/fetch-Address Book/Address-Book/main.js
+++ 07week/fetch-Address Book/Address-Book/main.js
@@ -22,37 +22,33 @@ function getUsers() {
 
       let output = "<h2>Contacts</h2>";
       data.results.forEach(function(results){
+        // let li = document.createElement('li')
+        // let button = document.createElement('button')
         output += `
-        <ul>
+        <ul id='${results.dob.age}'>
           <li><b>Name:</b> ${results.name.first} ${results.name.last}</li>
-          <li><b>Location:</b> ${results.location.country}</li>
+          <li><b>Country:</b> ${results.location.country}</li>
           <img src="${results.picture.large}">
-          <br><button id="infoButton" onclick="displayInfo()">More Info</button>
-        `
+          <br><button id="infoButton" onclick="displayInfo(${results.dob.age}, '${results.location.city}', '${results.location.state}', '${results.cell}')">
+          More Info</button>
+          </ul>
+          `
       })
       document.getElementById("users").innerHTML=output
-      document.getElementById('infoButton').addEventListener('onclick', displayInfo);
       })
 };
 
-
-
-function displayInfo() {
-  fetch('https://randomuser.me/api/?results=10')
-  .then(res => res.json())
-  .then(data => {
-    console.log(data.results)
-    let input = 
-    data.results.forEach(function(results){
-      input += `
+function displayInfo(age, city, state, cell) {
+  console.log('results', age)
+    let input = `
       <ul>
-      <li>${results.location.city}, ${results.location.state}</li>
-      <li>${results.dob}</li>
-      <li>${results.location.phone}</li>
+      <li><b>Location:</b>${city}, ${state}</li>
+      <li><b>Age:</b>${age}</li>
+      <li><b>Phone #:</b>${cell}</li><br>
+      </ul>
       `
-    })
-    document.getElementById("users").innerHTML=input
-  })
+      console.log(input)
+    document.getElementById(`${age}`).innerHTML += input
 }
 
 
