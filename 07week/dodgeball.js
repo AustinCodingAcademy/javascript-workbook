7105b1efc0ead701a26ecfc2f40c17b4f3eaf67d
JustinBarsalou <justin.barsalou@g.austincc.edu>
2019-10-06 11:18:56 -0500

    finished dodgeball file, just need to comment

--- 07week/dodgeball.js
+++ 07week/dodgeball.js
@@ -98,30 +98,98 @@ const listPeopleChoices = () => {
 
 
 const makePlayer = (id) => {
-  console.log(`li ${id} was clicked!`)
-  var p = arrOfPeople.find(element => {
-
+  // console.log(`li ${id} was clicked!`)
+  var p = arrOfPeople.findIndex(element => {
+    
+    // console.log(element)
     return element.id == id
-  
+
+    
   }) 
-  console.log(arrOfPeople[id])
-  listOfPlayers.push(p)
-  console.log(listOfPlayers)
-}
+  document.getElementById("people").innerHTML = null;
+  const q = arrOfPeople.splice(p, 1);
+  // console.log(q, 'qqqqqq')
 
-
-const choseTeam = (id) => {
-  const elementList = document.getElementById('players')
-  const list = document.createElement("li")
+  listOfPlayers.push(q[0]);
+  const playerList = document.getElementById('players')
+  const li = document.createElement("li");
   const button = document.createElement("button")
   const button2 = document.createElement("button")
   button.innerHTML = "Red Team"
   button2.innerHTML = 'Blue Team'
-  button.addEventListener('click', function() {makePlayer(person.id)} )
-  button2.addEventListener('click', function() {makePlayer(person.id)} )
-  list.appendChild(button)
-  list.appendChild(button2)
-  list.appendChild(document.createTextNode(p.name + " - " + p.skillSet))
-  elementList.append(list)
-  
+  console.log(q[0], 'qqqqqq')
+
+  button.addEventListener('click', function() {
+    moveToRed(q[0].id)
+    playerList.removeChild(li)
+  } )
+  button2.addEventListener('click', function() {
+    moveToBlack(q[0].id)
+    playerList.removeChild(li)
+
+  } )
+  li.appendChild(button)
+  li.appendChild(button2)
+  li.appendChild(document.createTextNode(q[0].name + "-" + q[0].skillSet))
+  playerList.appendChild(li)
+  listPeopleChoices()
+}
+
+const moveToRed = (id) => {
+  console.log(`li ${id} was clicked!`)
+  var r = listOfPlayers.find(element => {
+    
+    console.log(element)
+    return element.id == id
+    
+  }) 
+  let index = listOfPlayers.indexOf(r)
+  console.log("index: " +index)
+  // listOfPlayers.splice(index, 1)
+  // console.log(r,'arr')
+  const a = listOfPlayers.splice(index, 1);
+  // console.log(listOfPlayers);
+  redTeam.push(a[0]);
+  console.log(listOfPlayers)
+  const redList = document.getElementById('red')
+  const li = document.createElement("li");
+  const button = document.createElement("button")
+  button.innerHTML = "Remove"
+  // button.addEventListener('click', function() {moveToRed(p.id)} )
+  li.appendChild(button)
+  li.appendChild(document.createTextNode(a[0].name + "-" + a[0].skillSet))
+  redList.appendChild(li)
+}
+
+// const moveToBlack = () => {
+//   var b = listOfPlayers.find(element => {
+
+//     return element.id == id
+    
+//   }) 
+//   blueTeam.push(b)
+// }
+
+
+const moveToBlack = (id) => {
+  console.log(`li ${id} was clicked!`)
+  var b = listOfPlayers.find(element => {
+    
+    console.log(element)
+    return element.id == id
+    
+  }) 
+
+  // console.log(r,'arr')
+  const c = listOfPlayers.splice(b, 1);
+  // console.log(listOfPlayers);
+  blueTeam.push(c);
+  const redList = document.getElementById('blue')
+  const li = document.createElement("li");
+  const button = document.createElement("button")
+  button.innerHTML = "Remove"
+  // button.addEventListener('click', function() {moveToRed(p.id)} )
+  li.appendChild(button)
+  li.appendChild(document.createTextNode(b.name + "-" + b.skillSet))
+  redList.appendChild(li)
 }
