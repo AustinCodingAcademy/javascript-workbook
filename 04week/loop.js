c29414022d5e3ff80872f2446f693933ddb3482a
coltonnewton89 <52718393+coltonnewton89@users.noreply.github.com>
2019-10-14 12:25:56 -0500

    change things up

--- 04week/loop.js
+++ 04week/loop.js
@@ -10,5 +10,74 @@ do {
 
 console.log(x); */
 
+let person = {
+    firstName: "Jane",
+    lastName: "Doe",
+    birthDate: "Jan 5, 1925",
+    gender: "female"
+}
 
+function forInLoop() {
+    for (var i in person) {
+        if (i === 'birthdate') {
+            console.log(person[i])
+        }
+    }
+}
 
+forInLoop();
+
+const arrayOfPersons = [
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+        birthDate: "Jan 5, 1925",
+        gender: "female"
+    },
+
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+        birthDate: "Jan 5, 1925",
+        gender: "female"
+    },
+
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+        birthDate: "Jan 5, 1925",
+        gender: "female"
+    },
+
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+        birthDate: "Jan 5, 1925",
+        gender: "female"
+    }
+]
+
+arrayOfPersons.map(function (items) {
+    console.log(items);
+})
+
+function checkGender() {
+    arrayOfPersons.filter(function (person) {
+        if (person.gender === 'male') {
+            //coment note console.log(person)
+        }
+    })
+}
+
+checkGender();
+
+function checkAge() {
+    arrayOfPersons.filter(function (person) {
+        var lastIndex = person.birthDate.length - 1;
+        var date = person.birthDate.slice(lastIndex - 4, lastIndex + 1)
+        if (date <= 1990) {
+            //comment note dose  console.log(person)
+        }
+    })
+
+}
