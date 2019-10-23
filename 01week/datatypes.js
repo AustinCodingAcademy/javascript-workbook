f9119d5e6eeb2c491b2f94c261986d6f4922428f
EmiKusaba <eeemi615@gmail.com>
2019-10-23 13:15:17 -0500

    added code

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -28,9 +28,61 @@ var theFu; // theFu has been declared, but its value is undefined
 console.log(typeof theFu);
 
 // Number
-NaN
-String
+var thisIsANumber = 64;
+console.log(thisIsANumber);
+
+// NaN
+function sanitise(x) {
+  if (isNaN(x)) {
+    return NaN;
+  }
+  return x;
+}
+console.log(sanitise('NotANumber'));
+// expected output: NaN
+
+// String
+let longString = "This is a very long string which needs " +
+"to wrap across multiple lines " 
+console.log(longString);
+
 //Write a JavaScript program that adds 2 numbers together.
+myVar = 5 + 10; // assigned 15
+console.log(myVar);
+
 //Write a JavaScript program that runs only when 2 things are true.
+var a = 3;
+var b = -2;
+
+console.log(a > 0 && b > -5);
+// expected output: true
+
+console.log(a > 0 || b > 0);
+// expected output: true
+
+console.log(!(a > 0 || b > 0));
+// expected output: false
+
 //Write a JavaScript program that runs when 1 of 2 things are true.
+var a = 3;
+var b = -2;
+
+console.log(a > 0 && b > 0);
+// expected output: false
+
+console.log(a > 0 || b > 0);
+// expected output: true
+
+console.log(!(a > 0 || b > 0));
+// expected output: false
+
 //Write a JavaScript program that runs when both things are not true.
+
+var a = 3;
+var b = -2;
+
+console.log(a > 0 && b > 0);
+// expected output: false
+
+console.log(!(a > 0 || b > 0));
+// expected output: false
