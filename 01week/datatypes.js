af50b5821a20bd8a8863521fb9bcabdb6fb0c534
bwdurst <bwdurst@gmail.com>
2019-10-22 20:37:38 -0500

    adding comments to assignment

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -1,29 +1,37 @@
+// Task 1
 let d = new Date();
 console.log(d);
 
+// Task 2
 let num = 15;
 let n = num.toString();
 console.log(typeof n);
 
+// Task 3
 let text = '42px';
 let integer = parseInt(text);
 console.log(integer);
 
+// Task 4
 let whatDataType = true;
 console.log(typeof whatDataType)
 
+// Task 5
 let num1 = 10;
 let num2 = 32;
 console.log(num1 + num2);
 
+// Task 6
 if (11===11 && 19===19) {
   console.log("Both are truthy!")
 };
 
+// Task 7
 if (1 > 10 || 1 < 20) {
   console.log("One is truthy!")
 };
 
+// Task 8
 let test5 = 5;
 let test6 = 5;
 if (test5===10 && test6===20) {
