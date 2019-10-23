24f8c3f2a5383fc92a7be02e09be468913553075
EmiKusaba <eeemi615@gmail.com>
2019-10-22 20:53:03 -0500

    first js

---
+++ 01week/datatypes.js
@@ -0,0 +1,36 @@
+//Write a JavaScript program to display the current day and time.
+var now = new Date();
+console.log(now);
+
+//Write a JavaScript program to convert a number to a string.
+var num = 15;
+var n = num.toString();
+console.log(n);
+
+//Write a JavaScript program to convert a string to the number.
+var str = '123';
+str = Number('123');
+console.log(str);
+
+//Write a JavaScript program that takes in different datatypes and prints out whether they are a:
+//Boolean
+var x = 0;
+
+// Boolean(x); 
+console.log(Boolean(x)); 
+
+// Null
+var foo = null;
+console.log(foo);
+
+// Undefined
+var theFu; // theFu has been declared, but its value is undefined
+console.log(typeof theFu);
+
+// Number
+NaN
+String
+//Write a JavaScript program that adds 2 numbers together.
+//Write a JavaScript program that runs only when 2 things are true.
+//Write a JavaScript program that runs when 1 of 2 things are true.
+//Write a JavaScript program that runs when both things are not true.
