ecbbcc316937130e4c103b4388406242caf4902e
stephensilva <ssilva755@gmail.com>
2019-10-24 20:19:09 -0500

    RPS work in progress

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -8,11 +8,29 @@ const rl = readline.createInterface({
 });
 
 
+
+
+
+
 function rockPaperScissors(hand1, hand2) {
 
   // Write code here
+   if (hand1 === hand2) {
+     return "It's a tie!";
+   } else if (hand1 === 'rock' && hand2 === 'scissors') {
+     return "Hand one wins!";
+   } else if (hand1 === 'rock' && hand2 === 'paper') {
+     return "Hand two wins!";
+   } else if (hand1 === 'paper' && hand2 === 'rock') {
+     return "Hand one wins!";
+   } 
+  }
 
 }
+
+
+
+
 
 function getPrompt() {
   rl.question('hand1: ', (answer1) => {
