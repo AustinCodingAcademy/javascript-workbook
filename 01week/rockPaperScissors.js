56182c15d986ec3dce9966c0af95f38fbb2cba93
AndrewSimons3 <simdrew07@gmail.com>
2019-10-24 19:52:35 -0500

    started rockpaperscissors

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -12,6 +12,26 @@ function rockPaperScissors(hand1, hand2) {
 
   // Write code here
 
+  if (hand1 === hand2) {
+    return "It's a tie!"
+
+  }else if (hand1 === 'rock' && hand2 === 'scissors') {
+    return "Hand one wins!"
+
+  } else if (hand1 === 'scissors' && hand2 === 'paper') {
+    return "Hand one wins!"
+
+  } else if (hand1 === 'paper' && hand2 === 'rock') {
+    return "Hand one wins!"
+
+  } else if (hand2 === 'rock' && hand1 === 'scissors') {
+    return "Hand two wins!"
+
+  } else if (hand2 === 'scissors' && hand1 === 'paper') {
+    return "Hand two wins!"
+
+  } else if (hand2 === 'paper' && hand2 === 'rock') 
+    return "Hand two wins!"
 }
 
 function getPrompt() {
