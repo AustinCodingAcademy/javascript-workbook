f32ed0626389db86be2cb1dec329e5e8d75f86e7
dgasch512 <gaschdav208@gmail.com>
2019-10-27 21:09:37 -0500

    rockpaperscissors

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -5,9 +5,24 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
+
+
 function rockPaperScissors(hand1, hand2) {
   // Write code here
+  if (hand1 === hand2) {
+  return "It's a tie!";
+} else if (hand1 === 'rock' && hand2 === 'scissors')
+           {return "Hand one wins!";}
+ else if (hand1 === 'paper' && hand2 === 'rock')
+           {return "Hand one wins!";}
+  else if (hand1 === 'scissors' && hand2 === 'paper')
+           {return "Hand one wins!";}
+  else {
+    return "Hand two wins!"
+  }
 }
+  
 function getPrompt() {
   rl.question('hand1: ', (answer1) => {
     rl.question('hand2: ', (answer2) => {
