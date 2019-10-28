45e056cb8e0ef77fb42da0d4163bbdf7cdbd260e
EmiKusaba <eeemi615@gmail.com>
2019-10-28 15:00:24 -0500

    added code

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -10,9 +10,34 @@ const rl = readline.createInterface({
 
 function pigLatin(word) {
 
-  // Your code here
+  // Convert string to lowercase
+  word = word.toLowerCase()
+  //remove white space
+  word = word.trim()
+  // Initialize array of vowels
+  const vowels = ["a", "e", "i", "o", "u"];
+  // Initialize vowel index to 0
+  let vowelIndex = 0;
 
+  if (vowels.includes(word[0])) {
+    // If first letter is a vowel
+    return word + "yay";
+  } else {
+    // If the first letter isn't a vowel i.e is a consonant
+    for (let char of word) {
+      // Loop through until the first vowel is found
+      if (vowels.includes(char)) {
+        // Store the index at which the first vowel exists
+        vowelIndex = word.indexOf(char);
+        break;
+      }
+    }
+    // Compose final string
+    return word.slice(vowelIndex) + word.slice(0, vowelIndex) + "ay";
+  }
 }
+
+
 
 
 function getPrompt() {
