6c47497833845f4fc10aa5110a513424953a45cc
Pace Davis <pacemdavis@gmail.com>
2019-10-29 20:42:54 -0500

    passed all the tests

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -9,9 +9,30 @@ const rl = readline.createInterface({
 
 
 function pigLatin(word) {
-
+  //lowerCase and trim word
+  word = word.toLowerCase().trim();
+  
   // Your code here
-
+  // create a variable for vowels
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  //create a variable for word position 0
+  const firstLetter = word[0];
+  //most basic case where word starts with a vowel
+  //If it does start with a vowel, attach "yay" on the end
+  if(vowels.includes(firstLetter)) {
+    return word + "yay";
+  }
+  //create else condition for all remaining words that don't start with vowels
+  //Use a for loop to run through the word until it finds the first vowel
+  //Once it finds the first vowel return the word cut from the first vowel on
+  //Add that to the word from position 0 until the first vowel and add "ay"
+  else {
+    for (let i = 1; i < word.length; i++) {
+      if (vowels.includes(word[i])) {
+        return word.slice(i) + word.slice(0, i) + "ay";
+      }
+    }
+  }
 }
 
 
