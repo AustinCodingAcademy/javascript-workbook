8ed137c67ad5c3178222d8f3934b8051ad635dd3
Pace Davis <pacemdavis@gmail.com>
2019-10-31 19:13:44 -0500

    working on new test

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -17,23 +17,38 @@ function pigLatin(word) {
   const vowels = ['a', 'e', 'i', 'o', 'u'];
   //create a variable for word position 0
   const firstLetter = word[0];
+  const space = ' ';
   //most basic case where word starts with a vowel
   //If it does start with a vowel, attach "yay" on the end
-  if(vowels.includes(firstLetter)) {
-    return word + "yay";
-  }
-  //create else condition for all remaining words that don't start with vowels
-  //Use a for loop to run through the word until it finds the first vowel
-  //Once it finds the first vowel return the word cut from the first vowel on
-  //Add that to the word from position 0 until the first vowel and add "ay"
-  else {
-    for (let i = 1; i < word.length; i++) {
-      if (vowels.includes(word[i])) {
-        return word.slice(i) + word.slice(0, i) + "ay";
+  // for (let j = 0; j < space.length; j++) {
+
+    // if (word.includes(space[j])) {
+    //   let newWord = word.split(" ").toString(j);
+    //   return newWord + "ay";
+
+    // }
+     if (vowels.includes(firstLetter)) {
+      return word + "yay";
+      
+    }
+    //create else condition for all remaining words that don't start with vowels
+    //Use a for loop to run through the word until it finds the first vowel
+    //Once it finds the first vowel return the word cut from the first vowel on
+    //Add that to the word from position 0 until the first vowel and add "ay"
+    else {
+      for (let i = 1; i < word.length; i++) {
+        if (vowels.includes(word[i])) {
+          return word.slice(i) + word.slice(0, i) + "ay";
+        }
+  
       }
     }
-  }
-}
+    }
+  
+  // }
+
+  
+
 
 
 function getPrompt() {
@@ -63,6 +78,10 @@ if (typeof describe === 'function') {
     it('should lowercase and trim word before translation', () => {
       assert.equal(pigLatin('HeLlO '), 'ellohay');
       assert.equal(pigLatin(' RoCkEt'), 'ocketray');
+    });
+    it('Should separate two words and return them together', () => {
+      assert.equal(pigLatin('Hop Fest'), 'ophay estfay');
+      assert.equal(pigLatin('Beer Time'), 'eerbay imetay');
     });
   });
 } else {
