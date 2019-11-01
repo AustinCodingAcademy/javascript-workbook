2d8c38b621c18fd3c30f20cad9780e19cf1c67cd
bwdurst <bwdurst@gmail.com>
2019-10-31 19:28:31 -0500

    rps.tests 2

--- 02week/tests.js
+++ 02week/tests.js
@@ -57,18 +57,18 @@ if (typeof describe === 'function') {
       assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
       assert.equal(rockPaperScissors('scissors', 'paper'), "Hand one wins!");
       assert.equal(rockPaperScissors('paper', 'rock'), "Hand one wins!");
-
       assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
       assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
       assert.equal(rockPaperScissors('scissors', 'rock'), "Hand two wins!");
-
-
     });
     it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
       assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
       assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
       assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
     });
+    it('should ensure correct input', () => {
+      assert.equal(rockPaperScissors())
+    })
   });
 } else {
 
