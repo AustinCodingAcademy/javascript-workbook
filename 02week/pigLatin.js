62a3b37dfab2e768155c2e3904832ce7c65ab4de
Rivasjon <Rivasjon000@Gmail.com>
2019-10-31 19:19:06 -0500

    added test and branch

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -8,18 +8,73 @@ const rl = readline.createInterface({
 });
 
 
-function pigLatin(word) {
+//function pigLatin(str){
+   // str = str.trim().toLowerCase();
+    
 
-  // Your code here
+  //const vowels = ['a','e','i','o','u', 'y'];
+  //let vowelIndex = 0 ;
+  //const firstLetter = str[0];
+  //if (vowels.includes(str[0])) {
+   // return str + 'yay';
+  //} else { 
+   // for (let char of str){
+      //vowelIndex = str.indexOf(char);
+      //break;
+    // }
+    //}
+  //return str.slice(vowelIndex) + str.slice(0, vowelIndex) + "yay";
+    //}
+    function toPigLatin(part) {
+      str=part.toLowerCase();
+      var n =part.search(/[aeiuo]/);
+      switch (n){
+       case 0: str = str+"way"; break;
+       case -1: str = str+"ay"; break;
+       default :
+          //str= str.substr(n)+str.substr(0,n)+"ay";
+          str=str.replace(/([^aeiou]*)([aeiou])(\w+)/, "$2$3$1ay");
+       break;
+     }
+     return str; 
+ 
+ 
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ function letters(word) {
+      return word.split('');
+  }
+ 
+  function pigLatinizeWord(word) {
+      var chars = letters(word);
+      return chars.slice(1).join('') + chars[0] + 'ay';
+  }
+ 
+ function pigLatinizeSentence(sentence) {
+     return sentence.replace(/\w+/g, pigLatinizeWord);
+ }
+ 
+ 
 
-}
+  
+  
+  
+  
+
 
 
 function getPrompt() {
   rl.question('word ', (answer) => {
     console.log( pigLatin(answer) );
     getPrompt();
-  });
+  }); 
 }
 
 // Tests
