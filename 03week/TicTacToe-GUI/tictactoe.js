27f35bb2efa967fd01c94860d2f6f8972fed312b
EmiKusaba <eeemi615@gmail.com>
2019-11-05 15:39:30 -0600

    added code

--- 03week/TicTacToe-GUI/tictactoe.js
+++ 03week/TicTacToe-GUI/tictactoe.js
@@ -1,45 +1,46 @@
 
-var players =[];
-var markers = ["X","O"];
+var players = [];
+var markers = ["X", "O"];
 players[0] = "Player One";
 players[1] = "Player Two";
 var whoseTurn = 0;
-var scores = [0,0];
-var winValues = [7,56,73,84,146,273,292,448];
+var scores = [0, 0];
+var winValues = [7, 56, 73, 84, 146, 273, 292, 448];
 var gameOver = false;
 
 //check for wins
-function winCheck(){
-for (var i =0; i < winValues.length; i++)
-{
-  if ((scores[whoseTurn] & winValues[i]) == winValues[i])
+function winCheck() {
+  for (var i = 0; i < winValues.length; i++) {
+    if ((scores[whoseTurn] & winValues[i]) == winValues[i]) {
+      document.getElementById("title").innerText = players[whoseTurn] + "'s Win!";
+      gameOver = true;
+    }
+  }
+  if (((scores[0] + scores[1]) == 511))
   {
-  document.getElementById("title").innerText = players[whoseTurn] + "'s Win!";
-  gameOver = true;
-}
-}
+document.getElementById("title").innerText = "Cat wins!"
+gaveOver =true;
+  }
 }
 //Draw Board
 
-function play(clickedDiv, divValue)
-{
-  if (clickedDiv.innerHTML == "&nbsp;")
-{
-  scores[whoseTurn] += divValue;
-  clickedDiv.onclick = "";
-  clickedDiv.innerHTML = "<span>" + markers[whoseTurn] + "</span>";
-  winCheck();
-  if (!gameOver){toggleplayer();}
-}
+function play(clickedDiv, divValue) {
+  // if (clickedDiv.innerHTML == "&nbsp;")
+  if (!gameOver) {
+    scores[whoseTurn] += divValue;
+    clickedDiv.onclick = "";
+    clickedDiv.innerHTML = "<span>" + markers[whoseTurn] + "</span>";
+    winCheck();
+    if (!gameOver) { toggleplayer(); }
+  }
 }
 
-function toggleplayer()
-{
-  if(whoseTurn == 0) whoseTurn = 1;
+function toggleplayer() {
+  if (whoseTurn == 0) whoseTurn = 1;
   else whoseTurn = 0;
 
   document.getElementById("title").innerText =
-  players[whoseTurn] + "'s Turn";
+    players[whoseTurn] + "'s Turn";
 }
 //Track Player
 
