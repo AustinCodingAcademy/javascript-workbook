05b779742263de270525b22b06945ecdcd13b64a
ryan-blizzard <blizzy34@hotmail.com>
2019-11-12 20:49:31 -0600

    mastermind update

--- 04week/mastermind.js
+++ 04week/mastermind.js
@@ -28,9 +28,46 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateHint() {
-  // your code here
-}
+function generateHint(guess) {
+  const solutionArray = solution.split('');
+  // should be ['a', 'b', 'c', 'd']
+
+  const guessArray = guess.split ('');
+  //if guess was 'abdc'
+  // should be ['a', 'b', 'c', 'd']
+
+  let redPegs = 0;
+  let whitePegs = 0;
+
+// checking for redPegs
+  for (let i = 0; i < solutionArray.length; i++){
+    if (solutionArray[i] === guessArray[i]) {
+      redPegs++;
+      solutionArray[i] = null;
+    }
+    // solutionArray[i]
+    // guessArray [i]
+  }
+
+  let targetIndex = null;
+  // checking for whitePegs
+  for (let i = 0; i < guessArray.length; i++){
+    targetIndex = solutionArray.indexOf(guessArray[i]);
+   
+    if (targetIndex > -1) {
+      whitePegs++;
+      solutionArray[targetIndex]=null;
+    }
+    }
+
+
+    // return a string representation of redPegs and whitePegs variables
+    return `${redPegs}-${whitePegs}`;
+  }
+  
+  // return a string representation of redPegs and whitePegs variables
+  // return 'x-x';
+
 
 function mastermind(guess) {
   solution = 'abcd'; // Comment this out to generate a random solution
