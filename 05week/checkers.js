736d7fd690530b18cd69d7a94a78a8002f2d01ef
Rivasjon <Rivasjon000@Gmail.com>
2019-12-01 21:26:06 -0600

    pushing checkers

--- 05week/checkers.js
+++ 05week/checkers.js
@@ -1,21 +1,30 @@
-'use strict';
+"use strict";
 
-const assert = require('assert');
-const readline = require('readline');
+const assert = require("assert");
+const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-
-function Checker() {
-  // Your code here
+class Checker {
+  // Constructor for the checkers to be placed on the board.
+  constructor(color) {
+    //If the passed in color is white, create a white checker, else create black
+    if (color === "white") {
+      this.symbol = "W";
+    } else if (color === "black") {
+      this.symbol = "B";
+    }
+  }
 }
 
 class Board {
   constructor() {
-    this.grid = []
+    this.grid = [];
+    this.checkers = [];
   }
+
   // method that creates an 8x8 array, filled with null values
   createGrid() {
     // loop to create the 8 rows
@@ -41,11 +50,11 @@ class Board {
           rowOfCheckers.push(this.grid[row][column].symbol);
         } else {
           // just push in a blank space
-          rowOfCheckers.push(' ');
+          rowOfCheckers.push(" ");
         }
       }
       // join the rowOfCheckers array to a string, separated by a space
-      string += rowOfCheckers.join(' ');
+      string += rowOfCheckers.join(" ");
       // add a 'new line'
       string += "\n";
     }
@@ -53,21 +62,144 @@ class Board {
   }
 
   // Your code here
+  createCheckers() {
+    // Coordinates for the white and black checkers
+    var whitePositions = [
+      [0, 1],
+      [0, 3],
+      [0, 5],
+      [0, 7],
+      [1, 0],
+      [1, 2],
+      [1, 4],
+      [1, 6],
+      [2, 1],
+      [2, 3],
+      [2, 5],
+      [2, 7]
+    ];
+    var blackPositions = [
+      [5, 0],
+      [5, 2],
+      [5, 4],
+      [5, 6],
+      [6, 1],
+      [6, 3],
+      [6, 5],
+      [6, 7],
+      [7, 0],
+      [7, 2],
+      [7, 4],
+      [7, 6]
+    ];
+    for (let i = 0; i <= 11; i++) {
+      //row and column variables created from white starting positions array
+      const row = whitePositions[i][0];
+      const column = whitePositions[i][1];
+      // create white checkers from whitePositions array
+      this.grid[row][column] = new Checker("white");
+      this.checkers.push([whitePositions[i][0], whitePositions[i][1]]);
+      // row and column variables created from black starting positions array
+      const rowBlack = blackPositions[i][0];
+      const colBlack = blackPositions[i][1];
+      // create black checkers from blackPositions array
+      this.grid[rowBlack][colBlack] = new Checker("black");
+      this.checkers.push([blackPositions[i][0], blackPositions[i][1]]);
+    }
+  }
+  // helper function created to find the checker at a given location.
+  selectChecker(row, column) {
+    // returns found value (black or white character)
+    return this.grid[row][column];
+  }
+
+  // Kill checker function 
+  killChecker(startPos, endPos) {
+    // empty variable created for the mid point between the starting and ending positions
+    var middle = [];
+
+    // if the starting row and colunm are greater than the end row and column
+    if (startPos[1] > endPos[1] && startPos[0] > endPos[0]) {
+      // then middle variable equals the row and column -1
+      middle = [startPos[0] - 1, startPos[1] - 1];
+      
+      this.grid[middle[0]][middle[1]] = null;
+      
+      var middleIndex = this.checkers.findIndex(
+        item => item[0] === middle[0] && item[1] === middle[1]
+      );
+      this.checkers.splice(middleIndex, 1);
+    }
+
+    // checks for the 2nd of 4 possible jumps
+    if (startPos[1] > endPos[1] && startPos[0] < endPos[0]) {
+      middle = [startPos[0] + 1, startPos[1] - 1];
+      // set the middle position to null
+      this.grid[middle[0]][middle[1]] = null;
+      // find and remove the checker in the checker array
+      var middleIndex = this.checkers.findIndex(
+        item => item[0] === middle[0] && item[1] === middle[1]
+      );
+      this.checkers.splice(middleIndex, 1);
+    }
+
+    // checks for the 3rd of 4 possible jumps
+    if (startPos[1] < endPos[1] && startPos[0] > endPos[0]) {
+      middle = [startPos[0] - 1, startPos[1] + 1];
+      // set the middle position to null
+      this.grid[middle[0]][middle[1]] = null;
+      // find and remove the checker in the checker array
+      var middleIndex = this.checkers.findIndex(
+        item => item[0] === middle[0] && item[1] === middle[1]
+      );
+      this.checkers.splice(middleIndex, 1);
+    }
+
+    // checks for the 4th of 4 possible jumps
+    if (startPos[1] < endPos[1] && startPos[0] < endPos[0]) {
+      middle = [startPos[0] + 1, startPos[1] + 1];
+      // set the middle position to null
+      this.grid[middle[0]][middle[1]] = null;
+      // find and remove the checker in the checker array
+      var middleIndex = this.checkers.findIndex(
+        item => item[0] === middle[0] && item[1] === middle[1]
+      );
+      this.checkers.splice(middleIndex, 1);
+    }
+  }
 }
 
 class Game {
   constructor() {
-    this.board = new Board;
+    this.board = new Board();
   }
   start() {
     this.board.createGrid();
+    this.board.createCheckers();
+  }
+
+  moveChecker(start, end) {
+    var startCoords = start.split("").map(Number);
+    var endCoords = end.split("").map(Number);
+    var checker = this.board.selectChecker(startCoords[0], startCoords[1]);
+    var checkerIndex = this.board.checkers.findIndex(item => {
+      return item[0] === startCoords[0] && item[1] === startCoords[1];
+    });
+
+    this.board.grid[endCoords[0]][endCoords[1]] = checker;
+    this.board.grid[startCoords[0]][startCoords[1]] = null;
+    this.board.checkers.splice(checkerIndex, 1, [endCoords[0], endCoords[1]]);
+
+    if (Math.abs(startCoords[0] - endCoords[0]) === 2) {
+      this.board.killChecker(startCoords, endCoords);
+    }
   }
 }
 
 function getPrompt() {
   game.board.viewGrid();
-  rl.question('which piece?: ', (whichPiece) => {
-    rl.question('to where?: ', (toWhere) => {
+  rl.question("which piece?: ", whichPiece => {
+    rl.question("to where?: ", toWhere => {
       game.moveChecker(whichPiece, toWhere);
       getPrompt();
     });
@@ -77,30 +209,29 @@ function getPrompt() {
 const game = new Game();
 game.start();
 
-
 // Tests
-if (typeof describe === 'function') {
-  describe('Game', () => {
-    it('should have a board', () => {
-      assert.equal(game.board.constructor.name, 'Board');
+if (typeof describe === "function") {
+  describe("Game", () => {
+    it("should have a board", () => {
+      assert.equal(game.board.constructor.name, "Board");
     });
-    it('board should have 24 checkers', () => {
+    it("board should have 24 checkers", () => {
       assert.equal(game.board.checkers.length, 24);
     });
   });
 
-  describe('Game.moveChecker()', () => {
-    it('should move a checker', () => {
+  describe("Game.moveChecker()", () => {
+    it("should move a checker", () => {
       assert(!game.board.grid[4][1]);
-      game.moveChecker('50', '41');
+      game.moveChecker("50", "41");
       assert(game.board.grid[4][1]);
-      game.moveChecker('21', '30');
+      game.moveChecker("21", "30");
       assert(game.board.grid[3][0]);
-      game.moveChecker('52', '43');
+      game.moveChecker("52", "43");
       assert(game.board.grid[4][3]);
     });
-    it('should be able to jump over and kill another checker', () => {
-      game.moveChecker('30', '52');
+    it("should be able to jump over and kill another checker", () => {
+      game.moveChecker("30", "52");
       assert(game.board.grid[5][2]);
       assert(!game.board.grid[4][1]);
       assert.equal(game.board.checkers.length, 23);
