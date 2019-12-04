015ec2d922181f2f925e6691e6e874c13b6de48f
bwdurst <bwdurst@gmail.com>
2019-12-03 19:42:09 -0600

    Created assignment template

--- 06week/higherOrder.js
+++ 06week/higherOrder.js
@@ -2,9 +2,9 @@
 
 const assert = require('assert');
 
-function forEach(arr, callback) {
-  // Your code here
-}
+// function forEach(arr, callback) {
+//   // Your code here
+// }
 
 function map(arr, callback) {
   // Your code here
@@ -14,25 +14,31 @@ function filter(arr, callback) {
   // Your code here
 }
 
-function some(arr, callback) {
-  // Your code here
+function reduce(arr, callback) {
+  for( let i = 0; i < arr.length; i++) {
+    
+  }
 }
 
-function every(arr, callback) {
-  // Your code here
-}
+// function some(arr, callback) {
+//   // Your code here
+// }
+
+// function every(arr, callback) {
+//   // Your code here
+// }
 
 if (typeof describe === 'function') {
 
-  describe('#forEach()', () => {
-    it('should call the callback the array.length number of times', () => {
-      let count = 0;
-      forEach([1, 2, 3], () => {
-        count++;
-      });
-      assert.equal(count, 3);
-    });
-  });
+  // describe('#forEach()', () => {
+  //   it('should call the callback the array.length number of times', () => {
+  //     let count = 0;
+  //     forEach([1, 2, 3], () => {
+  //       count++;
+  //     });
+  //     assert.equal(count, 3);
+  //   });
+  // });
 
   describe('#map()', () => {
     const arr = [1, 2, 3];
@@ -56,45 +62,54 @@ if (typeof describe === 'function') {
     });
   });
 
-  describe('#some()', () => {
-    let count = 0;
-    const somed = some([1, 2, 3, 4], (num) => {
-      count++;
-      return num % 2 === 0;
-    });
-    it('should return true if at least one item passes the predicate test', () => {
-      assert.equal(somed, true);
-    });
-    it('should stop at the first item that passes the predicate test', () => {
-      assert.equal(count, 2);
-    });
-    it('should return false if no items pass the predicate test', () => {
-      const somed = some([1, 3, 5], (num) => {
-        return num % 2 === 0;
+  describe('#reduce()', () => {
+    it('should return the total of all the numbers in an array', () => {
+      const reduced = reduce([1, 2, 3], (accumulator, currentValue) => {
+        return accumulator + currentValue;
       });
-      assert.equal(somed, false);
+      assert.deepEqual(reduced, 6);
     });
   });
 
-  describe('#every()', () => {
-    it('should return true if at all passes the predicate test', () => {
-      const everied = every([2, 4, 6], (num) => {
-        return num % 2 === 0;
-      });
-      assert.equal(everied, true);
-    });
-    let count = 0;
-    const everied = every([2, 3, 4, 5], (num) => {
-      count++;
-      return num % 2 === 0;
-    });
-    it('should return false if any item fails the predicate test', () => {
-      assert.equal(everied, false);
-    });
-    it('should stop at the first item that fails the predicate test', () => {
-      assert.equal(count, 2);
-    });
-  });
+  // describe('#some()', () => {
+  //   let count = 0;
+  //   const somed = some([1, 2, 3, 4], (num) => {
+  //     count++;
+  //     return num % 2 === 0;
+  //   });
+  //   it('should return true if at least one item passes the predicate test', () => {
+  //     assert.equal(somed, true);
+  //   });
+  //   it('should stop at the first item that passes the predicate test', () => {
+  //     assert.equal(count, 2);
+  //   });
+  //   it('should return false if no items pass the predicate test', () => {
+  //     const somed = some([1, 3, 5], (num) => {
+  //       return num % 2 === 0;
+  //     });
+  //     assert.equal(somed, false);
+  //   });
+  // });
+
+  // describe('#every()', () => {
+  //   it('should return true if at all passes the predicate test', () => {
+  //     const everied = every([2, 4, 6], (num) => {
+  //       return num % 2 === 0;
+  //     });
+  //     assert.equal(everied, true);
+  //   });
+  //   let count = 0;
+  //   const everied = every([2, 3, 4, 5], (num) => {
+  //     count++;
+  //     return num % 2 === 0;
+  //   });
+  //   it('should return false if any item fails the predicate test', () => {
+  //     assert.equal(everied, false);
+  //   });
+  //   it('should stop at the first item that fails the predicate test', () => {
+  //     assert.equal(count, 2);
+  //   });
+  // });
 
 } else {
 
