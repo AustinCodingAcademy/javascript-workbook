c2a91084c4969cc47e5aa2ca63d682142952f793
Justin Essler <justinessler@fastmail.com>
2019-12-14 12:14:31 -0600

    tutorials

---
+++ 02week/word.js
@@ -0,0 +1,12 @@
+
+// function pigLatin(word) {
+
+  // Your code here
+  let word = "elephant";
+
+  let firstHalf = word.substring(3,5);
+  let secondHalf = word.substring(2);
+
+  console.log("first half = ", firstHalf);
+  console.log("the original word is", word);
+//   console.log("the position of the firs vowel is", positionOfVowel);
