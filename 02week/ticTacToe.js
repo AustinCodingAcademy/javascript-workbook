0b5bb19167a4f59621fd925e10e577663d9c13c3
Justin Essler <justinessler@fastmail.com>
2019-12-14 20:21:06 -0600

    saving loops

--- 02week/ticTacToe.js
+++ 02week/ticTacToe.js
@@ -23,6 +23,19 @@ function printBoard() {
   console.log('2 ' + board[2].join(' | '));
 }
 
+
+// ======================TickTacToe Notes ==============================
+// return true if you have won, other wse return
+
+// accept the row and column, the user wants to put a mark on ....
+// place the approptiate mark on the borad
+// check if they have won ?s
+// if they have not won, and still open slots, do nothing
+// else if they have not won, and thereare no open slots
+// else it they have won, print out the winning players name and message
+// make sure to switch the playerturn varialble before you return from the function (but after you print any message )
+
+
 function horizontalWin() {
   for(let i=0; i<=2; i++){
     if(board[i][0] == board[i][1] && board[i][1] == board [i][2] && board [i][0] !=""){
@@ -31,33 +44,38 @@ function horizontalWin() {
   } return false
 }
 
-
-
-
-
 function verticalWin() {
-  // Your code here
+  for(let i=0; i<=2; i++){
+    if(board[0][i] == board[1][i] && board[1][i] == board [2][i] && board [0][i] !=""){
+      return true;
+    }
+  } return false;
 }
 
-
-//retuen true if win diagonally, other wise return....
 function diagonalWin() {
-  // Your code here
+  for(let i=0; i<=2; i++){
+    if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[i][i] !=""){
+      return true;
+    }
+    if(board[2][0] == board[1][1] && board[1][1] == board[0][2] && board[1][1] !=""){
+      return true;
+    }
+  } return false;
 }
 
-
-//return true if you have won, other wse return
 function checkForWin() {
-  // Your code here
+
+// if(horizontalWin(true) || verticalWin(true) || diagonalWin(true)) {
+//   window.alert(`Player ${currentMarker} won!`)
+//   } else {
+//     changeMarker()
+//   }
+// }
 }
 
-// acceot the row and column, the usedr want to put a mark on
-// place the approptiate mark on the borad
-// check if they have won ?
-// if they have not won, and still open slots , do nothing
-// else if they have not won, and thereare no open slot
-// else it they have won, print out the winning players name and message
-//make sure to switch the playerturn varialble before you return from the function(but after you print any message ); 
+
+
+
 function ticTacToe(row, column) {
   board[row][colunm] = playerTurn;
 }
