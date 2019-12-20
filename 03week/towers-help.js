7540a11dfa1c3b023427577e3b5b6997c479de50
Justin Essler <justinessler@fastmail.com>
2019-12-19 22:18:04 -0600

    hanoi

---
+++ 03week/towers-help.js
@@ -0,0 +1,11 @@
+let person = {
+    name: "Justin",
+    dob: "8/4/1973",
+    gender: "male"
+}
+
+console.log(person.age);
+console.log(person.gender);
+
+let attribute = "gender";
+
--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -19,24 +19,57 @@ function printStacks() {
   console.log("c: " + stacks.c);
 }
 
-function movePiece() {
+function movePiece(startStack, endStack) {
+// move the top piece from startStack to top of endStack
   // Your code here
 
 }
 
-function isLegal() {
-  // Your code here
+
+
+function isLegal(startStack, endStack) {
+// if the move is legal return true
+// if the move is not legal return false
+// A legal move is....
+  // when at least 1 piece in the start stack
+    // 1. when the endStack is empty
+    // 2. when the top pieve in the start stack is smaller than the top piece in the endstack
+
+// Your code here
+
+
+
+
 
 }
 
 function checkForWin() {
+// if the player won, return true
+// if the player did not win, return false
+// a win is when all 4 blocks are in stack b or stack c
+
+
   // Your code here
 
 }
 
 function towersOfHanoi(startStack, endStack) {
+  // takes in the userInput for startStack and endStack
+  // and digitally plays the round
+  
+
+  
   // Your code here
 
+  // check to see if the move is legal
+    // move the piece
+  // esle if the move is not legal
+    // tell user the move is not legal
+
+  // check if they won
+    // if they won, tell them they won,
+    // if they did not win, do nothing
+    
 }
 
 function getPrompt() {
