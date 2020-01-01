b776796bd9c8e26ce1c32e9afd1c7f7cfeaf6bc6
Justin Essler <justinessler@fastmail.com>
2020-01-01 16:26:10 -0600

    lubys cars

--- 03week/arrayOfCars.js
+++ 03week/arrayOfCars.js
@@ -1,9 +1,10 @@
 // Off of master create a new branch called 'ArrayOfCars'
+    console.log("======Start of the Program=====")
 
 // length
     // Create an array called cars which consists of 4 different types of cars. The first car type should be Ford.
     // Console.log the length of the array.
-    myCars = ['Ford', 'GM', 'Chrysler', 'Dodge'];
+    myCars = ['Ford', 'Chrysler', 'Dodge'];
     console.log(myCars.length);
 
 // concat
@@ -41,21 +42,41 @@
     // Based on the types of cars you used, predict which item in the array should be at index 0.
     let yourPrediction = "Buick"
     // Use the following code to confirm or reject your prediction: 
-    // alert(carsInReverse.indexOf(['Buick']));
+    console.log(carsInReverse.indexOf(['Buick']));
+    // this one is not working, not sure what I am doing wrong
 // slice
     // Use the slice method to remove Ford and Honda from the carsInReverse array and move them into a new array called removedCars.
-    carsInReverse.slice();
+    let removedCars = carsInReverse.slice(-4,-2);
+    console.log(removedCars);
 // splice
     // Use the splice method to remove the 2nd and 3rd items in the array carsInReverse and add Ford and Honda in their place.
+    // array.splice(index, howmany, item1, ....., itemX)
+    carsInReverse.splice(2,2,"Ford", "Honda");
+    console.log(carsInReverse);
 // push
     // Use the push method to add the types of cars that you removed using the splice method to the carsInReverse array.
-// pop
+    // array.push(item1, item2, ..., itemX)
+    carsInReverse.push("Dodge", "Ford");
+    console.log(carsInReverse);
+// pops
     // Use the pop method to remove and console.log the last item in the array carsInReverse.
+    console.log(carsInReverse.pop());
+    console.log(carsInReverse);
+
 // shift
     // Use the shift method to remove and console.log the first item in the array carsInReverse.
+    console.log(carsInReverse.shift());
 // unshift
     // Use the unshift method to add a new type of car to the array carsInReverse.
-// forEach
+    carsInReverse.unshift("Tesla");
+    console.log(carsInReverse);
 
-// Create an array called numbers with the following items: 23, 45, 0, 2 Write code that will add 2 to each item in the array numbers.
-// .forEach() requires a function to be passed into it as its first argument. Build a function that will add 2 and then use .forEach() to pass each number into your freshly built function. const numbers = [23, 45, 0 , 2, 8, 44, 100, 1, 3, 91, 34]
+// forEach
+    // Create an array called numbers with the following items: 23, 45, 0, 2 Write code that will add 2 to each item in the array numbers.
+    let numbers = [23,45,0,2];
+    let summedNumbers = numbers.map(number => number + 2);
+    console.log(summedNumbers);
+    // .forEach() requires a function to be passed into it as its first argument. Build a function that will add 2 and then use .forEach() to pass each number into your freshly built function. 
+    const numbers2 = [23, 45, 0 , 2, 8, 44, 100, 1, 3, 91, 34];
+    let summedNumbers2 = numbers2.map(number => number + 2);
+    console.log(summedNumbers2);
