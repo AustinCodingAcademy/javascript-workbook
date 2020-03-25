896c2ddbf09443415937aad9321245d9a86184d0
CodingGiraffe <adamthomas03@gmail.com>
2020-03-24 20:28:01 -0500

    initial request

---
+++ 01week/datatypes.js
@@ -0,0 +1,35 @@
+'use strict'
+
+//current day and time
+let now = new Date();
+console.log("the current date and time")
+
+//Write a JavaScript program to convert a number to a string
+const theNumber = 5;
+const theString = theNumber.toString();
+
+console.log("the string is five")
+console.log("the number is", theString)
+
+//Write a JavaScript program to convert a string to the number.
+const theOtherString = "4.5";
+const theOtherNumber = parseInt(theOtherString, 10)
+console.log("the otherString type", typeof theOtherString)
+console.log("the otherNumber type", typeof theOtherNumber)
+
+//Write a JavaScript program that takes in different datatypes and prints out whether they are a:
+//Boolean
+
+//Null
+
+//Undefined
+
+//Number
+
+//NaN
+
+//String
+
+//Write a JavaScript program that adds 2 numbers together
+
+//
