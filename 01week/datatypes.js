885084add41b53d36ec0b496017f1bcff8d79d95
erincrogers <erin.c.rogers@gmail.com>
2020-03-24 20:28:20 -0500

    test commit for datatypes branch

---
+++ 01week/datatypes.js
@@ -0,0 +1,20 @@
+'use strict'
+//write a javascript program to display the current date and time.
+let now = new Date ();
+
+console.log (now);
+
+//JS program to convert a number to a string
+const theNumber = 5;
+const theString = theNumber.toString();
+
+console.log("The type of theString variable is", (typeof theString));
+console.log("The type of theNumber variable is", (typeof theNumber));
+console.log("The string is", theNumber.toString());
+console.log("the number is ", theNumber);
+
+//write a JS program to convert a string to a number
+const theOtherString = "11";
+const theOtherNumber = parseInt(theOtherString, 10);
+console.log("The otherString type is ", (typeof theOtherString));
+console.log("The otherString type is ", (typeof theOtherNumber));
