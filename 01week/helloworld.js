1c9da5e9e6cf2d82406837697725acf977d2d3b0
aaron-kane-w <aaron.kane.w@gmail.com>
2020-03-24 20:27:09 -0500

    working on datatypes assignment

--- 01week/helloworld.js
+++ 01week/helloworld.js
@@ -1,3 +1,22 @@
 'use strict'
 
-console.log('Hello World!')
+// Write a JavaScript program to display the current day and time.
+let now = new Date()
+console.log('the current date is', now)
+
+// Write a JavaScript program to convert a number to a string.
+const theNumber = 5
+const theString = theNumber.toString()
+
+console.log('The string is', theString)
+console.log('the number is', theNumber)
+
+
+// Write a JavaScript program to convert a string to the number.
+const theOtherString = '5'
+const theOtherNumber = parseInt(theOtherString, 10)
+  // 
+  const theOtherNumber1 = Number(theOtherString)
+
+console.log('The otherString type is', (typeof theOtherString))
+console.log('The otherNumber type is', (typeof theOtherNumber))
