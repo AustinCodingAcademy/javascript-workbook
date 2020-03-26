95f9c15d9a3ee55b3e50b872a778d2ca70905a8a
patfitz95 <patfitz95@gmail.com>
2020-03-26 18:19:33 -0500

    moved over to main computer but updated datatype assignment

---
+++ 01week/datatypes.js
@@ -0,0 +1,7 @@
+'use strict'
+let now = new Date ();
+console.log ('date and time', now)
+const theNumber = 5;
+const theString = theNumber.toString();
+const theOtherString = parseInt(theNumber);
+console.log (theString, theOtherString)
