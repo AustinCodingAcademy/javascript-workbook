9ea3f795fef16641438ddf44e8332a388643b511
CodingGiraffe <adamthomas03@gmail.com>
2020-04-01 10:38:09 -0500

    finished pig latin game

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -6,13 +6,51 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+//make function to find first vowel
+function findFirstVowel(word) {
+//make variable with vowels for loop to search for
+  const vowel = ["a", "e", "i", "o", "u"];
+//make a loop to go through each letter of each word
+  for (let i = 0; i < word.length; i++) {
+//set new variable to find vowels 
+    let firstVowel = vowel.indexOf(word[i]);
+  
+  console.log(firstVowel);
+//make condition of how to act if a word does or does not have a vowel
+  if (vowel.indexOf(word[i]) !== -1) {
+  return i;
+  }
+  }
+  }
 
-
+//make a varible for pig latin word
 const pigLatin = (word)  => {
+//trim and lowercase input
+  word = word.toLowerCase().trim();
+//set loop to search for first vowel in a word
+  let firstVowel = findFirstVowel(word);
+  //set conditions to split word and add "ay" at the end
+  if (firstVowel > 0) {
+  return word.substring(firstVowel) + word.substring(0, firstVowel) + "ay";
+  } else {
+  return word + "yay";
+  }
+  }
 
-  // Your code here
 
-}
+// 1. if the word starts with a vowel, add yay to end
+//2. if the word has a vowel: 
+  //find the first vowel
+  //split word into two parts before the first vowel
+  //   - starting at the begining and stopping just before the first vowel
+  //   - starting at the first vowel to the end of the word
+  //c. make a new word, where you flip the 2 parts
+  //d. add "ay" to the end of the new word
+
+  
+  
+
+
 
 
 const getPrompt = () => {
