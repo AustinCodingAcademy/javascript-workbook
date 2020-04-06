377f2a94d1ee13fdd7921ebc65e4a7a5b08914a4
Chris Trevino <chrisdadef@gmail.com>
2020-04-06 17:58:39 -0500

    finally someting that works

---
+++ 02week/tests.js
@@ -0,0 +1,18 @@
+          'use strict'
+
+// for loop practice
+let fruits = ["mangos", "apple", "pineapple", "grapes"]
+
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i])
+  
+  
+}
+
+let data {
+  name: Christopher
+  age: 28,
+  maritalStatus: true
+}
+
+
