9504700b939bd7ac6e21158a6ae27edde537a836
Chris Trevino <chrisdadef@gmail.com>
2020-04-07 10:42:33 -0500

    changes

--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -28,22 +28,22 @@ function printBoard() {
 function horizontalWin() {
   // Your code here
     if(board[0][0] === 'X' && board [0][1] === 'X' && board[0][2] === 'X') {
-      return true;
+      return "Player one wins!";
   } 
     if(board[1][0] === 'X' && board [1][1] === 'X' && board[1][2] === 'X') {
-      return true;
+      return "Player one wins!";
   }  
     if(board[2][0] === 'X' && board [2][1] === 'X' && board[2][2] === 'X') {
-      return true;
+      return "Player one wins!";
   }
   if(board[0][0] === 'O' && board [0][1] === 'O' && board[0][2] === 'O') {
-    return true;
+    return  "Player two wins!";
 } 
   if(board[1][0] === 'O' && board [1][1] === 'O' && board[1][2] === 'O') {
-    return true;
+    return  "Player two wins!';
 }  
   if(board[2][0] === 'O' && board [2][1] === 'O' && board[2][2] === 'O') {
-    return true;
+    return  "Player two wins!';
 }
 }
 
