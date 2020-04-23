f404dedd5c67bde9c8a1e8972ddcb4ced271f729
Chris Trevino <chrisdadef@gmail.com>
2020-04-23 01:36:33 -0500

    chages '

--- 04week/higherFunc.js
+++ 04week/higherFunc.js
@@ -1,54 +1,83 @@
 //make a  lists of student
 //each student should have a name, and a average grade
 
-let s1 = {
-  name: "Yousif",
-  avg: 85
+// let s1 = {
+//   name: "Yousif",
+//   avg: 85
+// }
+
+// let s2 = {
+//   name: "Mike",
+//   avg: 98
+// }
+
+// let s3 ={
+//   name: "Bob",
+//   avg: 90
+// }
+
+// let s4 = {
+//   name: "Bridget",
+//   avg:103
+// }
+
+// let students = [s1,s2,s3,s4]
+
+// students.filter(function(element, index){
+//   if (element.avg >= 90) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+
+// }).map((element, index) => {
+//   return element.avg
+// }).forEach((element, index) => {
+//   console.log(element);
+// });
+
+// let firstA = students.find((element, index) => {
+//   if (element.avg >= 90) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// });
+
+// let numbers = [3, 73, 21, 19, 4];
+
+// let sum = numbers.reduce(function(prev, curr) {
+//   return prev + curr;
+// });
+
+// console.log("the sum is ", sum);
+
+'use strict'
+
+
+let result = "";
+let i = 0;
+
+do {
+  i = i + 1;
+  result = result + i;
+} while (i <= 100);
+
+
+ let person = {
+
+  firstName: "Jane",
+  lastName : "Doe",
+  birthDate: "Jan 5, 1925",
+  gender: "female"
+ }
+
+ for (const birth in person) {
+  console.log(`${person['birthDate']}`);
 }
-
-let s2 = {
-  name: "Mike",
-  avg: 98
-}
-
-let s3 ={
-  name: "Bob",
-  avg: 90
-}
-
-let s4 = {
-  name: "Bridget",
-  avg:103
-}
-
-let students = [s1,s2,s3,s4]
-
-students.filter(function(element, index){
-  if (element.avg >= 90) {
-    return true;
-  } else {
-    return false;
-  }
-
-}).map((element, index) => {
-  return element.avg
-}).forEach((element, index) => {
-  console.log(element);
-});
-
-let firstA = students.find((element, index) => {
-  if (element.avg >= 90) {
-    return true;
-  } else {
-    return false;
-  }
-});
-
-let numbers = [3, 73, 21, 19, 4];
-
-let sum = numbers.reduce(function(prev, curr) {
-  return prev + curr;
-});
-
-console.log("the sum is ", sum);
+ if(person.birthDate == false) {
+  
+  return  person.birthDate
+ }
+ console.log(person.birthDate)
 
