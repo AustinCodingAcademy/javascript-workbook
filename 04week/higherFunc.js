ecfc02ef4fe25634d3e99d0b60bbeac2f6f4a73b
Chris Trevino <chrisdadef@gmail.com>
2020-04-29 20:43:20 -0500

    changes

--- 04week/higherFunc.js
+++ 04week/higherFunc.js
@@ -57,12 +57,13 @@
 
 let result = "";
 let i = 0;
-
+// .1 Use a do...while loop to console.log the numbers from 1 to 1000.
 do {
   i = i + 1;
   result = result + i;
 } while (i <= 100);
 
+// .2 Create an object (an array with keys and values) called person with the following data:
 
  let person = {
 
@@ -71,8 +72,58 @@ do {
   birthDate: "Jan 5, 1925",
   gender: "female"
  }
+// .3Use a for...in loop and if statement to console.log the value associated with the key birthDate if the birth year is an odd number.
 
  for (let x in person) {
-  console.log(`${person[x]}`);
+  //console.log(`${person[x]}`);
 }
+//.4 Create an arrayOfPersons that contains multiple objects. You can simply copy/paste the person object you made above multiple times. Feel free to change the values to reflect multiple people you might have in your database.
+
+const arrayOfPersons = [
+  {
+    firstname: 'Chris',
+    lastname: 'Trevino',
+    gender: 'male',
+    dob: 'October 10, 1995'
+  },
+  {
+    firstname: 'Christina',
+    lastname: 'Hamilton',
+    gender: 'female',
+    dob: 'January 05, 1979'
+  },
+  {
+    firstname: 'John',
+    lastname: 'Ginger',
+    gender: 'male',
+    dob: 'April 20, 1990'
+  },
+  {
+    firstname: 'Lola',
+    lastname: 'Moana',
+    gender: 'female',
+    dob: 'July 4, 1985'
+  }
+]
+
+
+//.5 Use .map() to map over the arrayOfPersons and console.log() their information.
+// didn't really understand if im not modifying anything?????
+const map1 = arrayOfPersons.map(arrayOfPersons);
+//console.log(map1);
+// 6.Use .filter() to filter the persons array and console.log only males in the array.
+//Wow I fudging go it.
+const onlyMales = arrayOfPersons.filter(word => (word.gender === 'male'));
+//console.log(onlyMales)
+const onlyFemales = arrayOfPersons.filter(word => (word.gender === 'female'))
+//console.log(onlyFemales);
+//7.Use .filter() to filter the persons array and console.log only people that were born before Jan 1, 1990.
+const bornBefore = arrayOfPersons.filter(word => word.dob < 'Jan 1, 1990' );
+console.log(bornBefore);
+
+
+
+
+
+
 
